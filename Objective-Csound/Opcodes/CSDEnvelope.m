b5afcfcd5b266287811974d0504c2d2574e9b7bd
Aurelius Prochazka <aure@aure.com>
2012-05-24 00:23:18 -0700

    Envelope template

---
+++ Objective-Csound/Opcodes/CSDEnvelope.m
@@ -0,0 +1,13 @@
+//
+//  CSDEnvelope.m
+//  AudioPong
+//
+//  Created by Aurelius Prochazka on 5/17/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import "CSDEnvelope.h"
+
+@implementation CSDEnvelope
+
+@end
