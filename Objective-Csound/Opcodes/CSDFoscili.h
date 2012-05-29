634bc002e6c3712d03a07d54e0f159cba362036d
Adam Boulanger <adamb@media.mit.edu>
2012-05-29 10:53:17 -0700

    Added reset (reinit) to csdsynth. Fixed header init to get rid of distortion.

---
+++ Objective-Csound/Opcodes/CSDFoscili.h
@@ -0,0 +1,13 @@
+//
+//  CSDFoscili.h
+//  Missilez
+//
+//  Created by Adam Boulanger on 5/29/12.
+//  Copyright (c) 2012 MIT Media Lab. All rights reserved.
+//
+
+#import "CSDOpcode.h"
+
+@interface CSDFoscili : CSDOpcode
+
+@end
---
+++ Objective-Csound/Opcodes/CSDFoscili.m
@@ -0,0 +1,13 @@
+//
+//  CSDFoscili.m
+//  Missilez
+//
+//  Created by Adam Boulanger on 5/29/12.
+//  Copyright (c) 2012 MIT Media Lab. All rights reserved.
+//
+
+#import "CSDFoscili.h"
+
+@implementation CSDFoscili
+
+@end
