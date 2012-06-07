5acdf20c5e4282ea7b43d1f8c67859c071de6f79
Aurelius Prochazka <aure@aure.com>
2012-06-07 00:38:59 -0700

    Introducing the concept of a CSD Parameter Array, CSDParamArray, which is right now just a list of floats.  Also adding a basic helper function for the most common sine function table.

--- Objective-Csound/FunctionTables/CSDSineTable.m
+++ Objective-Csound/FunctionTables/CSDSineTable.m
@@ -11,9 +11,14 @@
 @implementation CSDSineTable
 
 //TODO: Should make the partial sizes an array of floats
--(id) initWithOutput:(NSString *)output TableSize:(int) tableSize PartialStrengths:(NSString *)parameters
+-(id) initWithOutput:(NSString *)output TableSize:(int) tableSize PartialStrengths:(CSDParamArray *)partials
 {
-    return [self initWithOutput:output TableSize:tableSize GenRouting:kGenRoutineSines AndParameters:parameters];
+    return [self initWithOutput:output TableSize:tableSize GenRouting:kGenRoutineSines AndParameters:[partials parameterString]];
+}
+
+-(id) initDefaultsWithOutput:(NSString *)output
+{
+    return [self initWithOutput:output TableSize:4096 GenRouting:kGenRoutineSines AndParameters:@"1"];
 }
 
 
