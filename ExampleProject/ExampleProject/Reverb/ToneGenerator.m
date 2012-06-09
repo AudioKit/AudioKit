0aaeeaae5d67526989276d0f7a206770d2d9645d
Aurelius Prochazka <aure@aure.com>
2012-06-09 15:29:23 -0700

    Added the tone generator to the reverb example

--- ExampleProject/ExampleProject/Reverb/ToneGenerator.m
+++ ExampleProject/ExampleProject/Reverb/ToneGenerator.m
@@ -8,6 +8,39 @@
 
 #import "ToneGenerator.h"
 
+typedef enum
+{
+    kPValuePitchTag=4,
+}kPValueTag;
+
 @implementation ToneGenerator
+
+-(id) initWithOrchestra:(CSDOrchestra *)newOrchestra {
+    self = [super initWithOrchestra:newOrchestra];
+    if (self) {                                                   
+        CSDSineTable * sineTable = [[CSDSineTable alloc] init];
+        [self addFunctionTable:sineTable];
+        
+        //H4Y - ARB: This assumes that CSDFunctionTable is ftgentmp
+        //  and will look for [CSDFunctionTable output] during csd conversion
+        CSDOscillator * myOscillator = [[CSDOscillator alloc] 
+                                        initWithAmplitude:[CSDParamConstant paramWithFloat:0.4]
+                                        Pitch:[CSDParamConstant paramWithPValue:kPValuePitchTag]
+                                        FunctionTable:sineTable];
+        [self addOpcode:myOscillator];
+
+        CSDOutputStereo * stereoOutput = 
+        [[CSDOutputStereo alloc] initWithInputLeft:[myOscillator output] 
+                                        InputRight:[myOscillator output]]; 
+        [self addOpcode:stereoOutput];
+    }
+    return self;
+}
+
+-(void) playNoteForDuration:(float)dur Pitch:(float)pitch {
+    int instrumentNumber = [[orchestra instruments] indexOfObject:self] + 1;
+    NSString * note = [NSString stringWithFormat:@"%0.2f %0.2f", dur, pitch];
+    [[CSDManager sharedCSDManager] playNote:note OnInstrument:instrumentNumber];
+}
 
 @end
