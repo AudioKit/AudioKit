205305e919c6cb89137026554a2b60be166d374b
Adam Boulanger <adamb@media.mit.edu>
2012-06-15 21:06:21 -0700

    untested, almost the right organization, not building due to strange linker errors

--- Objective-Csound/Parameters/CSDContinuous.h
+++ Objective-Csound/Parameters/CSDContinuous.h
@@ -20,15 +20,22 @@
     Float32 value;
     
     NSString * uniqueIdentifier;
+    
+    CSDParam * output;
+    BOOL isControl;
 }
 
 @property (nonatomic, readwrite) Float32 value;
 @property (nonatomic, readonly) NSString * uniqueIdentifier;
+@property (nonatomic, strong) CSDParam * output;
 @property (nonatomic, assign) Float32 maximumValue;
 @property (nonatomic, assign) Float32 minimumValue;
 @property (nonatomic, assign) Float32 initValue;
 
+
 -(id)init:(float)aInitValue Max:(float)maxValue Min:(float)minValue Tag:(int)aTag;
+-(id)init:(float)aInitValue Max:(float)maxValue Min:(float)minValue Tag:(int)aTag isControlRate:(BOOL)control;
 -(NSString *)convertToCsd;
+-(NSString *)uniqueName;
 
 @end
