9541e259bb1efc1ac7c129e0849450248d9b2440
Adam Boulanger <adamb@media.mit.edu>
2012-06-15 21:06:21 -0700

    untested, almost the right organization, not building due to strange linker errors

--- Objective-Csound/Parameters/CSDContinuous.m
+++ Objective-Csound/Parameters/CSDContinuous.m
@@ -8,6 +8,7 @@
 //
 
 #import "CSDContinuous.h"
+#import "CSDParamControl.h"
 
 @implementation CSDContinuous
 @synthesize maximumValue;
@@ -15,6 +16,7 @@
 @synthesize initValue;
 @synthesize value;
 @synthesize uniqueIdentifier;
+@synthesize output;
 
 -(id)init:(float)aInitValue Max:(float)maxValue Min:(float)minValue Tag:(int)aTag
 {
@@ -32,9 +34,34 @@
     return self;
 }
 
+-(id)init:(float)aInitValue Max:(float)maxValue Min:(float)minValue Tag:(int)aTag isControlRate:(BOOL)control 
+{
+    self = [super init];
+    
+    maximumValue = maxValue;
+    minimumValue = minValue;
+    initValue = aInitValue;
+    value = aInitValue;
+    
+    if (self) {
+        if(isControl)
+        {
+            output = [CSDParamControl paramWithString:[self uniqueName]];
+        } else {
+            output = [CSDParam paramWithString:[self uniqueName]];
+        }
+    }
+    return self;
+}
+
 -(NSString *)convertToCsd
 {
-    return [NSString stringWithFormat:@"%@ chnget %@", , uniqueIdentifier];
+    return [NSString stringWithFormat:@"%@ chnget %@", output, uniqueIdentifier];
+}
+                      
+-(NSString *) uniqueName
+{
+    return [NSString stringWithFormat:@"%@%p", [self class], self];
 }
 
 
