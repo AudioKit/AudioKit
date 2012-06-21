617b9c49beeb8eba46c2e718c57ca27ed6819bc4
Adam Boulanger <adamb@media.mit.edu>
2012-06-21 13:11:39 -0700

    expseg untested

---
+++ Objective-Csound/Opcodes/CSDExpSegment.m
@@ -0,0 +1,78 @@
+//
+//  CSDExpSegment.m
+//  ExampleProject
+//
+//  Created by Adam Boulanger on 6/21/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import "CSDExpSegment.h"
+
+@implementation CSDExpSegment
+@synthesize output;
+@synthesize firstSegmentStartValue;
+@synthesize firstSegmentDuration;
+@synthesize firstSegmentTargetValue;
+@synthesize segmentArray;
+
+-(id)initWithFirstSegmentStartValue:(CSDParamConstant *) start
+               FirstSegmentDuration:(CSDParamConstant *) dur
+           FirstSegementTargetValue:(CSDParamConstant *) targ
+                       SegmentArray:(CSDParamArray *)aSegmentArray
+{
+    self = [super init];
+    
+    if (self) {
+        output = [CSDParamControl paramWithString:[self uniqueName]];
+        firstSegmentStartValue  = start;
+        firstSegmentDuration    = dur;
+        firstSegmentTargetValue = targ;
+        segmentArray            = aSegmentArray;
+    }
+    
+    return self;
+}
+
+-(id)initWithFirstSegmentStartValue:(CSDParamConstant *) start
+               FirstSegmentDuration:(CSDParamConstant *) dur
+           FirstSegementTargetValue:(CSDParamConstant *) targ
+{
+    if (self) {
+        output = [CSDParamControl paramWithString:[self uniqueName]];
+        firstSegmentStartValue  = start;
+        firstSegmentDuration    = dur;
+        firstSegmentTargetValue = targ;
+    }
+    
+    return self;
+}
+
+-(NSString *)convertToCsd
+{
+    if (segmentArray == nil) {
+        return [NSString stringWithFormat:@"%@ expseg %@, %@, %@\n", 
+                output, 
+                firstSegmentStartValue, 
+                firstSegmentDuration, 
+                firstSegmentTargetValue];
+    } else {
+        /* NSMutableString *s = [NSString stringWithFormat:@", "];
+         for (int i = 0; i < [segmentArray count]; i++) {
+         [s appendFormat:@"%@,", [segmentArray ob
+         }
+         return [NSString stringWithFormat:@"%@ line %@, %@, %@", 
+         output, firstSegmentStartValue, firstSegmentDuration, firstSegmentTargetValue];*/
+        return [NSString stringWithFormat:@"%@ expseg %@, %@, %@, %@\n", 
+                output, 
+                firstSegmentStartValue, 
+                firstSegmentDuration, 
+                firstSegmentTargetValue, 
+                [segmentArray parameterString]];
+    }
+}
+
+-(NSString *) description {
+    return [output parameterString];
+}
+
+@end
