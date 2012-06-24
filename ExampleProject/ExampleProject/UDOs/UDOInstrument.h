f01bb0558d71dbdb3c24184202f959bcc14f6479
Aurelius Prochazka <aure@aure.com>
2012-06-24 00:00:04 -0700

    Add the skeleton for the UDO example

---
+++ ExampleProject/ExampleProject/UDOs/UDOInstrument.h
@@ -0,0 +1,19 @@
+//
+//  UDOInstrument.h
+//  ExampleProject
+//
+//  Created by Aurelius Prochazka on 6/23/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import "OCSInstrument.h"
+
+@interface UDOInstrument : OCSInstrument {
+    OCSProperty *frequency;
+}
+
+@property (nonatomic, strong) OCSProperty *frequency;
+
+- (void)playNoteForDuration:(float)dur Frequency:(float)freq;
+
+@end
