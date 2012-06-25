66cefdf47412609671d7dffa4c9a7ea08f1d8577
Adam Boulanger <adamb@media.mit.edu>
2012-06-25 11:29:44 -0700

    compiling csound, fixed xib, working towards sound.

--- ExampleProject/ExampleProject/More Grain/MoreGrainViewController.m
+++ ExampleProject/ExampleProject/More Grain/MoreGrainViewController.m
@@ -27,4 +27,54 @@
     [[OCSManager sharedOCSManager] runOrchestra:orch];
 }
 
+-(IBAction)hit1:(id)sender
+{
+    [[myGrainBirds grainDensity] setValue:12];
+    [[myGrainBirds grainDuration] setValue:0.01f];
+
+    [[myGrainBirds pitchOffsetStartValue] setValue:0];
+    [[myGrainBirds pitchOffsetFirstTarget] setValue:100];
+        
+    [[myGrainBirds reverbSend] setValue:0.1];
+    
+    [[myGrainBirds pitchClass] setValue:10.6f];
+    [myGrainBirds playNoteForDuration:0.1];
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(playNote1Again:) userInfo:nil repeats:NO];
+}
+
+-(void)playNote1Again:(NSTimer *)aTimer
+{
+    [[myGrainBirds grainDuration] setValue:0.096];
+    
+    [[myGrainBirds pitchOffsetStartValue] setValue:100];
+    [[myGrainBirds pitchOffsetFirstTarget] setValue:0];
+    
+    [[myGrainBirds reverbSend] setValue:0.2f];
+    
+    [[myGrainBirds pitchClass] setValue:11];
+    [myGrainBirds playNoteForDuration:8];
+    
+    [timer invalidate];
+    timer = nil;
+}
+
+-(IBAction)hit2:(id)sender
+{
+    
+}
+-(IBAction)hit3:(id)sender
+{}
+-(IBAction)hit4:(id)sender
+{}
+-(IBAction)hit5:(id)sender
+{}
+-(IBAction)hit6:(id)sender
+{}
+
+-(IBAction)startFx:(id)sender
+{
+    [fx start];
+}
+
 @end
