991360106d3c27d6defe8050edea4ce5b251312e
Aurelius Prochazka <aure@aure.com>
2012-06-27 13:05:06 -0700

    Starting Convolve Opcode

---
+++ Objective-Csound/Opcodes/OCSConvolve.h
@@ -0,0 +1,12 @@
+//
+//  OCSConvolve.h
+//
+//  Created by Aurelius Prochazka on 6/27/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import "OCSOpcode.h"
+
+@interface OCSConvolve : OCSOpcode
+
+@end
