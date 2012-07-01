7426643ce2d6282fa9ef67bcdc8ec5f33733fc91
Aurelius Prochazka <aure@aure.com>
2012-07-01 13:42:08 -0700

    Added OCSScale opcode in current-best-practices way.

---
+++ Objective-Csound/Core Classes/OCSScore.h
@@ -0,0 +1,13 @@
+//
+//  OCSScore.h
+//  ExampleProject
+//
+//  Created by Aurelius Prochazka on 7/1/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCSScore : NSObject
+
+@end
---
+++ Objective-Csound/Opcodes/Mathematical Operations/OCSScale.m
@@ -0,0 +1,52 @@
+//
+//  OCSScale.m
+//
+//  Created by Aurelius Prochazka on 7/1/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import "OCSScale.h"  
+
+@interface OCSScale () {
+    OCSParamControl *kscl;
+    OCSParamControl *kin;
+    OCSParamControl *kmax;
+    OCSParamControl *kmin;
+}
+@end
+
+@implementation OCSScale
+
+@synthesize output        = kscl;
+@synthesize input         = kin;
+@synthesize minimumOutput = kmin;
+@synthesize maximumOutput = kmax;
+
+- (id)initWithInput:(OCSParamControl *)input
+      minimumOutput:(OCSParamControl *)minimumOutput
+      maximumOutput:(OCSParamControl *)maximumOutput;
+{
+    self = [super init];
+    
+    if (self) {
+        kscl = [OCSParamControl paramWithString:[self opcodeName]];      
+        kin  = input;
+        kmax = maximumOutput;
+        kmin = minimumOutput;
+    }
+    return self; 
+}
+
+/// CSD Representation: kscl scale kinput, kmax, kmin
+- (NSString *)stringForCSD 
+{
+    return [NSString stringWithFormat:@"%@ scale %@, %@, %@\n", kscl, kin, kmax, kmin];
+}
+
+/// Gives the CSD string for the output parameter.  
+- (NSString *)description {
+    return [kscl parameterString];
+}
+
+@end
+
