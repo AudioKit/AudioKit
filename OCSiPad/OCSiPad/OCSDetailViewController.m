aa3bb7485354175fa7229e4f88c661e996060996
Aurelius Prochazka <aure@aure.com>
2012-08-30 03:34:46 -0700

    moved examples

---
+++ OCSiPad/OCSiPad/OCSDetailViewController.m
@@ -0,0 +1,77 @@
+//
+//  OCSDetailViewController.m
+//  OCSiPad
+//
+//  Created by Aurelius Prochazka on 8/30/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import "OCSDetailViewController.h"
+
+@interface OCSDetailViewController ()
+@property (strong, nonatomic) UIPopoverController *masterPopoverController;
+- (void)configureView;
+@end
+
+@implementation OCSDetailViewController
+
+#pragma mark - Managing the detail item
+
+- (void)setDetailItem:(id)newDetailItem
+{
+    if (_detailItem != newDetailItem) {
+        _detailItem = newDetailItem;
+        
+        // Update the view.
+        [self configureView];
+    }
+
+    if (self.masterPopoverController != nil) {
+        [self.masterPopoverController dismissPopoverAnimated:YES];
+    }        
+}
+
+- (void)configureView
+{
+    // Update the user interface for the detail item.
+
+    if (self.detailItem) {
+        self.detailDescriptionLabel.text = [self.detailItem description];
+    }
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    [self configureView];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+#pragma mark - Split view
+
+- (void)splitViewController:(UISplitViewController *)splitController willHideViewController:(UIViewController *)viewController withBarButtonItem:(UIBarButtonItem *)barButtonItem forPopoverController:(UIPopoverController *)popoverController
+{
+    barButtonItem.title = NSLocalizedString(@"Master", @"Master");
+    [self.navigationItem setLeftBarButtonItem:barButtonItem animated:YES];
+    self.masterPopoverController = popoverController;
+}
+
+- (void)splitViewController:(UISplitViewController *)splitController willShowViewController:(UIViewController *)viewController invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem
+{
+    // Called when the view is shown again in the split view, invalidating the button and popover controller.
+    [self.navigationItem setLeftBarButtonItem:nil animated:YES];
+    self.masterPopoverController = nil;
+}
+
+@end
---
+++ OCSiPad/OCSiPad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OCSiPad
+//
+//  Created by Aurelius Prochazka on 8/30/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OCSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OCSAppDelegate class]));
+    }
+}
