8b1768f25989a76c7c64cf1416c85ff56c94ae78
Aurelius Prochazka <aure@aure.com>
2012-09-03 03:41:39 -0700

    Better FM Oscillator EE

--- Shared/Instruments/Operation Examples/FM Oscillator/FMOscillatorInstrument.h
+++ Shared/Instruments/Operation Examples/FM Oscillator/FMOscillatorInstrument.h
@@ -12,8 +12,8 @@
 
 @property (nonatomic, strong) OCSInstrumentProperty *frequency;
 #define kFrequencyInit 440
-#define kFrequencyMin  110
-#define kFrequencyMax  880
+#define kFrequencyMin  20
+#define kFrequencyMax  20000
 
 @property (nonatomic, strong) OCSInstrumentProperty *amplitude;
 #define kAmplitudeInit 0.2
@@ -22,13 +22,13 @@
 
 @property (nonatomic, strong) OCSInstrumentProperty *carrierMultiplier;
 #define kCarrierMultiplierInit 1.0
-#define kCarrierMultiplierMin  0.5
-#define kCarrierMultiplierMax  2.0
+#define kCarrierMultiplierMin  0.0
+#define kCarrierMultiplierMax  3.0
 
 @property (nonatomic, strong) OCSInstrumentProperty *modulatingMultiplier;
 #define kModulatingMultiplierInit 1.0
-#define kModulatingMultiplierMin 0.5
-#define kModulatingMultiplierMax 2.0
+#define kModulatingMultiplierMin 0.0
+#define kModulatingMultiplierMax 3.0
 
 @property (nonatomic, strong) OCSInstrumentProperty *modulationIndex;
 #define kModulationIndexInit 15
