5a6f68123f5f637223643679ca360cf8e1fa560d
Aurelius Prochazka <aure@aure.com>
2012-09-18 21:37:12 -0700

    Starting on the path to notes and note properties

---
+++ Objective-C Sound/Core Classes/OCSNote.h
@@ -0,0 +1,13 @@
+//
+//  OCSNote.h
+//  OCS iPad Examples
+//
+//  Created by Aurelius Prochazka on 9/18/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCSNote : NSObject
+
+@end
---
+++ Shared/Instruments/Sequences/SeqInstrument.h
@@ -0,0 +1,13 @@
+//
+//  SeqInstrument.h
+//  OCS iPad Examples
+//
+//  Created by Aurelius Prochazka on 9/18/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import "OCSInstrument.h"
+
+@interface SeqInstrument : OCSInstrument
+
+@end
