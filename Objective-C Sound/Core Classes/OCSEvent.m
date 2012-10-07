fd299c31fa23394c0da197703b3c1070bea6d99f
Aurelius Prochazka <aure@aure.com>
2012-10-07 14:22:48 -0700

    Cleaned out event model to have only the note and block based events

--- Objective-C Sound/Core Classes/OCSEvent.m
+++ Objective-C Sound/Core Classes/OCSEvent.m
@@ -12,61 +12,18 @@
 typedef void (^MyBlockType)();
 
 @interface OCSEvent () {
-    NSMutableString *scoreLine;
+    //NSMutableString *scoreLine;
     MyBlockType block;
-    int _myID;
-    float eventNumber;
-    OCSInstrument *instr;    
+    //int _myID;
+    //float eventNumber;
+    //OCSInstrument *instr;
 }
 @end
 
 @implementation OCSEvent
 
-@synthesize eventNumber;
-@synthesize instrument = instr;
-
 // -----------------------------------------------------------------------------
 #  pragma mark - Initialization
-// -----------------------------------------------------------------------------
-
-static int currentID = 1;
-+ (void)resetID { currentID = 1; }
-
-- (id)init {
-    self = [super init];
-    if (self) {
-        if (currentID > 99000) {
-            [OCSEvent resetID];
-        }
-        _myID = currentID++;
-        scoreLine  = [[NSMutableString alloc] init];
-    }
-    return self;
-}
-
-// -----------------------------------------------------------------------------
-#  pragma mark - Instrument Based Events
-// -----------------------------------------------------------------------------
-
-- (id)initWithInstrument:(OCSInstrument *)instrument duration:(float)duration;
-{
-    self = [self init];
-    if (self) {
-        instr = instrument;
-        eventNumber  = [instrument instrumentNumber] + _myID/100000.0;
-        scoreLine = [NSMutableString stringWithFormat:@"i %0.5f 0 %g", eventNumber, duration];
-    }
-    return self;
-}
-
-- (id)initWithInstrument:(OCSInstrument *)instrument;
-{
-    return [self initWithInstrument:instrument duration:-1];
-}
-
-
-// -----------------------------------------------------------------------------
-#  pragma mark - Note Based Events
 // -----------------------------------------------------------------------------
 
 @synthesize note;
@@ -75,15 +32,9 @@ static int currentID = 1;
     self = [self init];
     if (self) {
         note = newNote;
-        eventNumber  = [note.instrument instrumentNumber] + _myID/100000.0;
-        scoreLine = [NSMutableString stringWithFormat:@"i %0.5f 0 -1", eventNumber];
     }
     return self;
 }
-
-// -----------------------------------------------------------------------------
-#  pragma mark - Block Based Events
-// -----------------------------------------------------------------------------
 
 - (id)initWithNote:(OCSNote *)newNote block:(void (^)())aBlock {
     self = [self initWithNote:newNote];
@@ -93,7 +44,6 @@ static int currentID = 1;
     return self;
 }
 
-
 - (id)initWithBlock:(void (^)())aBlock {
     self = [self init];
     if (self) {
@@ -102,65 +52,21 @@ static int currentID = 1;
     return self;
 }
 
+// -----------------------------------------------------------------------------
+#  pragma mark - event Actions
+// -----------------------------------------------------------------------------
+
+- (void)playNote {
+    if (self->note) [note play];
+}
+
 - (void)runBlock {
     if (self->block) block();
 }
 
-// -----------------------------------------------------------------------------
-#  pragma mark - Event Based Events
-// -----------------------------------------------------------------------------
-
-- (id)initWithEvent:(OCSEvent *)event 
+- (void)trigger;
 {
-    self = [self init];
-    if (self) {
-        instr = [event instrument];
-        eventNumber  = [event eventNumber];
-        scoreLine = [NSMutableString stringWithFormat:@"i %0.5f 0 0.1", eventNumber];
-        if (event.note) {
-            note = event.note;
-        }
-    }
-    return self;
-}
-
-
-- (id)initDeactivation:(OCSEvent *)event
-         afterDuration:(float)delay;
-{
-    self = [self init];
-    if (self) {
-        scoreLine = [NSMutableString stringWithFormat:@"i -%0.5f %f 0.1", 
-                     [event eventNumber], delay ];
-
-        // This next method uses the turnoff2 opcode which might prove advantageous 
-        // so I won't delete it just yet.
-//        scoreLine = [NSString stringWithFormat:@"i \"Deactivator\" %f 0.1 %0.3f\n", 
-//                     delay, [event eventNumber]];
-    }
-    return self;
-}
-
-
-// -----------------------------------------------------------------------------
-#  pragma mark - Csound Implementation
-// -----------------------------------------------------------------------------
-
-- (void)start;
-{
-    [[OCSManager sharedOCSManager] startEvent:self];
-}
-
-- (void)stop;
-{
-    OCSEvent *stoppage = [[OCSEvent alloc] initDeactivation:self afterDuration:0.0];
-    [[OCSManager sharedOCSManager] startEvent:stoppage];
-}
-
-- (NSString *)stringForCSD;
-{
-    if (![scoreLine isEqual:@""]) NSLog(@"Event Scoreline: %@\n", scoreLine);
-    return [NSString stringWithFormat:@"%@",scoreLine];
+    [[OCSManager sharedOCSManager] triggerEvent:self];
 }
 
 @end
