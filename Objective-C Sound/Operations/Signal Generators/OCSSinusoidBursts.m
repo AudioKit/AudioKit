172a739260c521471a1db41297af279278e39ee7
Aurelius Prochazka <aure@aure.com>
2012-10-09 15:43:31 -0700

    Starting FOF

---
+++ Objective-C Sound/Operations/Signal Generators/OCSSinusoidBursts.m
@@ -0,0 +1,13 @@
+//
+//  OCSSinusoidBursts.m
+//  OCS iPad Examples
+//
+//  Created by Aurelius Prochazka on 10/9/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import "OCSSinusoidBursts.h"
+
+@implementation OCSSinusoidBursts
+
+@end
