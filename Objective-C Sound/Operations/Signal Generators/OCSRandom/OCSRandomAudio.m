e1d037377c8db4d327c935523156d0e3485926af
Aurelius Prochazka <aure@aure.com>
2012-10-12 00:43:24 -0700

    Split up OCSRandom

---
+++ Objective-C Sound/Operations/Signal Generators/OCSRandom/OCSRandomAudio.m
@@ -0,0 +1,34 @@
+//
+//  OCSRandomAudio.m
+//  Objective-C Sound
+//
+//  Created by Adam Boulanger on 9/29/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import "OCSRandomAudio.h"
+
+@interface OCSRandomAudio() {
+    OCSControl *min;
+    OCSControl *max;
+}
+@end
+
+@implementation OCSRandomAudio
+
+-(id)initWithMinimum:(OCSControl *)minimum
+             maximum:(OCSControl *)maximum
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        min = minimum;
+        max = maximum;
+    }
+    return self;
+}
+
+- (NSString *)stringForCSD {
+    return [NSString stringWithFormat:@"%@ random %@, %@", self, min, max];
+}
+
+@end
