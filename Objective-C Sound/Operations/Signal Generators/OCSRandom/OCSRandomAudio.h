94d05b37979ad20961c57caa277eae45dc58575b
Aurelius Prochazka <aure@aure.com>
2012-10-12 00:43:24 -0700

    Split up OCSRandom

---
+++ Objective-C Sound/Operations/Signal Generators/OCSRandom/OCSRandomAudio.h
@@ -0,0 +1,23 @@
+//
+//  OCSRandomAudio.h
+//  Objective-C Sound
+//
+//  Created by Adam Boulanger on 9/29/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import "OCSParameter+Operation.h"
+
+/**
+ Generates noise based on a controlled pseudo-random number series between minimum and maximum values.
+ */
+
+@interface OCSRandomAudio : OCSParameter
+
+/// Instantiates the oscillator with an initial phase of sampling.
+/// @param minimum minimum range limit
+/// @param maximum maximum range limit
+- (id)initWithMinimum:(OCSControl *)minimum
+              maximum:(OCSControl *)maximum;
+
+@end
