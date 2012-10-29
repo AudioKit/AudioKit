2dd79637a686c7f718e423b09113f9f852b48133
Aurelius Prochazka <aure@aure.com>
2012-10-29 12:27:36 -0700

    Building Physical Model Example

---
+++ OCS iPad Examples/OCS iPad Examples/Physical Models/PhysicalModelViewController.m
@@ -0,0 +1,38 @@
+//
+//  PhysicalModelViewController.m
+//  OCS iPad Examples
+//
+//  Created by Aurelius Prochazka on 10/29/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import "PhysicalModelViewController.h"
+
+@interface PhysicalModelViewController ()
+
+@end
+
+@implementation PhysicalModelViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Shared/Instruments/Operation Examples/Marimba/MarimbaInstrument.h
@@ -0,0 +1,38 @@
+//
+//  MarimbaInstrument.h
+//  OCS iPad Examples
+//
+//  Created by Aurelius Prochazka on 10/29/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import "OCSInstrument.h"
+
+@interface MarimbaInstrument : OCSInstrument
+
+@end
+
+
+@interface MarimbaNote : OCSNote
+
+@property (nonatomic, strong) OCSNoteProperty *frequency;
+#define kFrequencyInit 440
+#define kFrequencyMin  110
+#define kFrequencyMax  880
+
+@property (nonatomic, strong) OCSNoteProperty *amplitude;
+#define kAmplitudeInit 0.2
+#define kAmplitudeMin  0
+#define kAmplitudeMax  1
+
+@property (nonatomic, strong) OCSNoteProperty *vibratoFrequency;
+#define kVibratoFrequencyInit 0
+#define kVibratoFrequencyMin  0
+#define kVibratoFrequencyMax  12
+
+@property (nonatomic, strong) OCSNoteProperty *vibratoAmplitude;
+#define kVibratoAmplitudeInit 0
+#define kVibratoAmplitudeMin  0
+#define kVibratoAmplitudeMax  10
+
+@end
