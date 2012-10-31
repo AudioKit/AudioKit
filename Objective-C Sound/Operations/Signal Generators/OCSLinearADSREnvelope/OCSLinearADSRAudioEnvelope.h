bbdfeb29dec29e2e3b6cd72c50c5d8e50755a4bf
Aurelius Prochazka <aure@aure.com>
2012-10-31 15:16:20 -0700

    Added ADSR Envelopes

---
+++ Objective-C Sound/Operations/Signal Generators/OCSLinearADSREnvelope/OCSLinearADSRAudioEnvelope.h
@@ -0,0 +1,35 @@
+//
+//  OCSLinearADSRAudioEnvelope.h
+//  Objective-C Sound
+//
+//  Created by Aurelius Prochazka on 10/31/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import "OCSAudio.h"
+#import "OCSParameter+Operation.h"
+
+/** Calculates the classical ADSR envelope using linear segments.
+ 
+ The envelope generated is the range 0 to 1 and may need to be scaled further, depending on the amplitude required. The length of the sustain is calculated from the length of the note. This means this operation is not suitable for use with MIDI events.
+ */
+
+@interface OCSLinearADSRAudioEnvelope : OCSAudio
+
+/// Instantiates the linear adsr audio envelope
+/// @param attackDuration Duration of attack phase
+/// @param decayDuration Duration of decay
+/// @param sustainLevel Level for sustain phase
+/// @param releaseDuration Duration of release phase
+- (id)initWithAttackDuration:(OCSConstant *)attackDuration
+               decayDuration:(OCSConstant *)decayDuration
+                sustainLevel:(OCSConstant *)sustainLevel
+             releaseDuration:(OCSConstant *)releaseDuration;
+
+
+/// Set an optional delay
+/// @param delay Period of zero before the envelope starts
+- (void)setOptionalDelay:(OCSConstant *)delay;
+
+
+@end
