08a391407d289f142e8591f7298bb1fe8b591eee
Aurelius Prochazka <aure@aure.com>
2012-11-04 00:56:14 -0700

    More Physical Models

---
+++ Objective-C Sound/Operations/Signal Generators/Physical Models/PhISEM/OCSDroplet.m
@@ -0,0 +1,79 @@
+//
+//  OCSDroplet.m
+//  Objective-C Sound
+//
+//  Created by Aurelius Prochazka on 10/28/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+//  Implementation of Csound's dripwater:
+//  http://www.csounds.com/manual/html/dripwater.html
+//
+
+#import "OCSDroplet.h"
+
+@interface OCSDroplet () {
+    OCSConstant *idettack;
+    OCSControl *kamp;
+    OCSConstant *inum;
+    OCSConstant *idamp;
+    OCSConstant *imaxshake;
+    OCSConstant *ifreq;
+    OCSConstant *ifreq1;
+    OCSConstant *ifreq2;
+}
+@end
+
+@implementation OCSDroplet
+
+- (id)initWithDuration:(OCSConstant *)duration
+             amplitude:(OCSControl *)amplitude
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        idettack = duration;
+        kamp = amplitude;
+        
+        inum = ocsp(10);
+        idamp = ocsp(0);
+        imaxshake = ocsp(0);
+        ifreq = ocsp(450);
+        ifreq1 = ocsp(600);
+        ifreq2 = ocsp(750);
+        
+        
+    }
+    return self;
+}
+
+
+- (void)setOptionalCount:(OCSConstant *)count {
+	inum = count;
+}
+
+- (void)setOptionalDampingFactor:(OCSConstant *)dampingFactor {
+	idamp = dampingFactor;
+}
+
+- (void)setOptionalEnergyReturn:(OCSConstant *)energyReturn {
+	imaxshake = energyReturn;
+}
+
+- (void)setOptionalMainResonantFrequency:(OCSConstant *)mainResonantFrequency {
+	ifreq = mainResonantFrequency;
+}
+
+- (void)setOptionalFirstResonantFrequency:(OCSConstant *)firstResonantFrequency {
+	ifreq1 = firstResonantFrequency;
+}
+
+- (void)setOptionalSecondResonantFrequency:(OCSConstant *)secondResonantFrequency {
+	ifreq2 = secondResonantFrequency;
+}
+
+- (NSString *)stringForCSD {
+    return [NSString stringWithFormat:
+            @"%@ dripwater %@, %@, %@, %@, %@, %@, %@, %@",
+            self, kamp, idettack, inum, idamp, imaxshake, ifreq, ifreq1, ifreq2];
+}
+
+@end
---
+++ Objective-C Sound/Operations/Signal Generators/Physical Models/PhISEM/OCSSekere.m
@@ -0,0 +1,57 @@
+//
+//  OCSSekere.m
+//  Objective-C Sound
+//
+//  Created by Aurelius Prochazka on 10/30/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+//  Implementation of Csound's sekere:
+//  http://www.csounds.com/manual/html/sekere.html
+//
+
+#import "OCSSekere.h"
+
+@interface OCSSekere () {
+    OCSConstant *idettack;
+    OCSConstant *iamp;
+    OCSConstant *inum;
+    OCSConstant *idamp;
+    OCSConstant *imaxshake;
+}
+@end
+
+@implementation OCSSekere
+
+- (id)initWithDuration:(OCSConstant *)duration
+             amplitude:(OCSConstant *)amplitude
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        idettack = duration;
+        iamp = amplitude;
+        inum = ocsp(64);
+        idamp = ocsp(0.5);
+        imaxshake = ocsp(0);
+    }
+    return self;
+}
+
+- (void)setOptionalCount:(OCSConstant *)count {
+	inum = count;
+}
+
+- (void)setOptionalDampingFactor:(OCSConstant *)dampingFactor {
+	idamp = dampingFactor;
+}
+
+- (void)setOptionalEnergyReturn:(OCSConstant *)energyReturn {
+	imaxshake = energyReturn;
+}
+
+- (NSString *)stringForCSD {
+    return [NSString stringWithFormat:
+            @"%@ sekere %@, %@, %@, %@, %@",
+            self, iamp, idettack, inum, idamp, imaxshake];
+}
+
+@end
