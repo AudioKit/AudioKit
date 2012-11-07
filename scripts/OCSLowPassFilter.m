a85bd5025c966125b7313b40850d967c37385c3b
Aurelius Prochazka <aure@aure.com>
2012-11-06 16:30:43 -0800

    Added first order low and high pass filters

---
+++ scripts/OCSLowPassFilter.m
@@ -0,0 +1,39 @@
+//
+//  OCSLowPassFilter.m
+//  Objective-C Sound
+//
+//  Auto-generated from database on 11/6/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+//  Implementation of Csound's tone:
+//  http://www.csounds.com/manual/html/tone.html
+//
+
+#import "OCSLowPassFilter.h"
+
+@interface OCSLowPassFilter () {
+    OCSAudio *asig;
+    OCSControl *khp;
+}
+@end
+
+@implementation OCSLowPassFilter 
+
+- (id)initWithSourceAudio:(OCSAudio *)sourceAudio
+           halfPowerPoint:(OCSControl *)halfPowerPoint
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+            asig = sourceAudio;    
+                khp = halfPowerPoint;    
+                }
+    return self; 
+}
+
+- (NSString *)stringForCSD {
+    return [NSString stringWithFormat: 
+            @"%@ tone %@, %@", 
+            self, asig, khp];
+}
+
+@end
