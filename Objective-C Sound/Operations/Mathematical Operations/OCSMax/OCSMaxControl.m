c8ee17742b0b79a5834103c10e7e133591a15db7
Aurelius Prochazka <aure@aure.com>
2012-12-22 02:45:14 -0800

    Fixes to example projects and removing the auto-generated comments from max and min since they were not auto-generated

--- Objective-C Sound/Operations/Mathematical Operations/OCSMax/OCSMaxControl.m
+++ Objective-C Sound/Operations/Mathematical Operations/OCSMax/OCSMaxControl.m
@@ -2,7 +2,7 @@
 //  OCSMaxControl.m
 //  Objective-C Sound
 //
-//  Auto-generated from database on 12/22/12.
+//  Created by Aurelius Prochazka on 12/22/12.
 //  Copyright (c) 2012 Hear For Yourself. All rights reserved.
 //
 //  Implementation of Csound's max:
