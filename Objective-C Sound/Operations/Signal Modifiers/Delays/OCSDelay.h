888fc38db9660bec5169b577ddcd5c662dbb0347
Aurelius Prochazka <aure@aure.com>
2012-12-26 05:27:53 -0800

    Added a simple delay operation

---
+++ Objective-C Sound/Operations/Signal Modifiers/Delays/OCSDelay.h
@@ -0,0 +1,25 @@
+//
+//  OCSDelay.h
+//  Objective-C Sound
+//
+//  Auto-generated from database on 12/26/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+
+#import "OCSDelay.h"
+#import "OCSParameter+Operation.h"
+
+/** Simple audio delay
+ 
+ Delays an input signal by some time interval.
+ */
+
+@interface OCSDelay : OCSDelay
+
+/// Instantiates the delay
+/// @param audioSource Audio signal
+/// @param delayTime Requested delay time in seconds.
+- (id)initWithAudioSource:(OCSAudio *)audioSource
+                delayTime:(OCSConstant *)delayTime;
+
+@end
---
+++ Objective-C Sound/Operations/Signal Modifiers/Delays/OCSDelay.m
@@ -0,0 +1,39 @@
+//
+//  OCSDelay.m
+//  Objective-C Sound
+//
+//  Auto-generated from database on 12/26/12.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+//  Implementation of Csound's delay:
+//  http://www.csounds.com/manual/html/delay.html
+//
+
+#import "OCSDelay.h"
+
+@interface OCSDelay () {
+    OCSAudio *asig;
+    OCSConstant *idlt;
+}
+@end
+
+@implementation OCSDelay
+
+- (id)initWithAudioSource:(OCSAudio *)audioSource
+                delayTime:(OCSConstant *)delayTime
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        asig = audioSource;
+        idlt = delayTime;
+    }
+    return self;
+}
+
+- (NSString *)stringForCSD {
+    return [NSString stringWithFormat:
+            @"%@ delay %@, %@",
+            self, asig, idlt, idlt];
+}
+
+@end
