0f17f5f672d8bd984826a836dbee51bf3c6fb379
Aurelius Prochazka <aure@aure.com>
2013-03-28 20:16:20 -0700

    Corrected inheritance of OCSAudioFromFSignal

--- Objective-C Sound/Operations/Phase Vocoder Streaming/OCSAudioFromFSignal.h
+++ Objective-C Sound/Operations/Phase Vocoder Streaming/OCSAudioFromFSignal.h
@@ -6,13 +6,14 @@
 //  Copyright (c) 2012 Hear For Yourself. All rights reserved.
 //
 
+#import "OCSAudio.h"
 #import "OCSParameter+Operation.h"
 #import "OCSFSignal.h"
 
 /** Resynthesise phase vocoder data (f-signal) using a FFT overlap-add.
  */
 
-@interface OCSAudioFromFSignal : OCSParameter
+@interface OCSAudioFromFSignal : OCSAudio
 
 /// Create audio from an f-signal
 /// @param source Input f-signal
