742d476e7e53390ba3d7646d3761c6bb77fc9316
Aurelius Prochazka <aure@aure.com>
2013-09-26 16:13:26 -0700

    Accidentally obliterated Schouten implementation, brought it back

--- Objective-C Sound/Operations/Analysis/OCSTrackedFrequencySchouten.m
+++ Objective-C Sound/Operations/Analysis/OCSTrackedFrequencySchouten.m
@@ -1,1 +1,36 @@
-z
+//
+//  OCSTrackedPitchSchouten.m
+//  SocialMedia Sounds
+//
+//  Created by Adam Boulanger on 9/18/13.
+//  Copyright (c) 2013 Adam Boulanger. All rights reserved.
+//
+
+#import "OCSTrackedFrequencySchouten.h"
+
+@interface OCSTrackedFrequencySchouten ()
+{
+    OCSFSignal *asig;
+    OCSControl *iampThresh;
+}
+@end
+
+@implementation OCSTrackedFrequencySchouten
+
+-(id)initWithFSignalSource:(OCSFSignal *)fSignalSource
+        amplitudeThreshold:(OCSControl *)amplitudeThreshold
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        asig = fSignalSource;
+        iampThresh = amplitudeThreshold;
+    }
+    return self;
+}
+
+-(NSString *)stringForCSD
+{
+    return [NSString stringWithFormat:@"%@, kUnused pvspitch %@, %@", self, asig, iampThresh];
+}
+
+@end
