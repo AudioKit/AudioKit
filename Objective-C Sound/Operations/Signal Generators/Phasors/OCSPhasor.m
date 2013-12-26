f60d3dfba2d422c217d7f12cd58771142e3a80aa
Aurelius Prochazka <aure@aure.com>
2013-12-26 02:10:27 -0800

    Fixed a typo in Adam's XCode template assigning improper copyright

--- Objective-C Sound/Operations/Signal Generators/Phasors/OCSPhasor.m
+++ Objective-C Sound/Operations/Signal Generators/Phasors/OCSPhasor.m
@@ -2,17 +2,19 @@
 //  OCSPhasor.m
 //  Objective-C Sound
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 10/11/12.
-//  Copyright (c) 2012 Adam Boulanger. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/26/13.
+//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//
+//  Implementation of Csound's phasor:
+//  http://www.csounds.com/manual/html/phasor.html
 //
 
 #import "OCSPhasor.h"
 
 @interface OCSPhasor () {
-    OCSParameter *freq;
-    OCSConstant *phs;
+    OCSParameter *xcps;
+    OCSConstant *iphs;
 }
-
 @end
 
 @implementation OCSPhasor
@@ -21,20 +23,20 @@
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        freq = frequency;
-        phs  = ocsp(0);
+        xcps = frequency;
+        iphs = ocsp(0);
     }
     return self;
 }
 
 - (void)setOptionalPhase:(OCSConstant *)phase {
-    phs = phase;
+	iphs = phase;
 }
 
 - (NSString *)stringForCSD {
     return [NSString stringWithFormat:
             @"%@ phasor %@, %@",
-            self, freq, phs];
+            self, xcps, iphs];
 }
 
 @end
