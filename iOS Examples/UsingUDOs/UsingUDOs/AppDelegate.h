4cb25efae2c83e74a267f5c96cf8fe1da7e3152e
Aurelius Prochazka <aure@aure.com>
2014-07-01 02:32:34 -0700

    Moving the UDO example into its own project

---
+++ iOS Examples/UsingUDOs/UsingUDOs/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UsingUDOs
+//
+//  Created by Aurelius Prochazka on 7/1/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
