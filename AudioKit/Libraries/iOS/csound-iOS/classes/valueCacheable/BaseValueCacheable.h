43c0091edbc9684f4da5dca01645e18963cbe9d8
Aurelius Prochazka <aure@aure.com>
2014-07-05 17:13:27 -0700

    Removed duplicate classes

--- AudioKit/Libraries/iOS/csound-iOS/classes/valueCacheable/BaseValueCacheable.h
+++ AudioKit/Libraries/iOS/csound-iOS/classes/valueCacheable/BaseValueCacheable.h
@@ -20,7 +20,7 @@
  License along with Csound; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  02111-1307 USA
- 
+
  */
 
 #import <Foundation/Foundation.h>
