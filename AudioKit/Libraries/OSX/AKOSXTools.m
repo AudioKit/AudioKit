e93f1a0ae54de945a0c4bcc41288aab57a4826a0
Aurelius Prochazka <aure@aure.com>
2014-08-03 18:04:29 -0700

    Moved the iOS Examples into their own folder to parallel the OSX Examples.

--- AudioKit/Libraries/OSX/AKOSXTools.m
+++ AudioKit/Libraries/OSX/AKOSXTools.m
@@ -5,6 +5,8 @@
 //  Created by Aurelius Prochazka on 7/27/14.
 //  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
+#if TARGET_OS_IPHONE
+#elif TARGET_OS_MAC
 
 #import "AKOSXTools.h"
 
@@ -145,6 +147,5 @@
     return [self scaleValue:value fromMinimum:0 fromMaximum:127 toMinimum:minimum toMaximum:maximum];
 }
 
-
-
 @end
+#endif
