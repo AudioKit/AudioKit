c9bfa86a6d288b2ea0d685a54baa020728d8a8b1
Aurelius Prochazka <aure@aure.com>
2014-08-03 21:25:34 -0700

    Simplified the comb filter.

--- AudioKit/Operations/Signal Modifiers/Filters/AKCombFilter.h
+++ AudioKit/Operations/Signal Modifiers/Filters/AKCombFilter.h
@@ -2,16 +2,14 @@
 //  AKCombFilter.h
 //  AudioKit
 //
-//  Created by Adam Boulanger on 4/10/13.
-//  Copyright (c) 2013 Hear For Yourself. All rights reserved.
+//  Created by Aurelius Prochazka on 8/13/14
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 
 #import "AKAudio.h"
 #import "AKParameter+Operation.h"
 
-/** Comb Filter
- 
- Reverberates an input signal with a “colored” frequency response.
+/** Reverberates an input signal with a “colored” frequency response.
  
  This filter reiterates input with an echo density determined by loopTime. The attenuation rate is independent and is determined by reverbTime, the reverberation time (defined as the time in seconds for a signal to decay to 1/1000, or 60dB down from its original amplitude). Output from a comb filter will appear only after loopTime seconds.
  
@@ -20,28 +18,12 @@
 @interface AKCombFilter : AKAudio
 
 /// Instantiates the comb filter
-/// @param audioSource Input Signal
+/// @param audioSource Input Audio Signal
 /// @param reverbTime  The time in seconds for a signal to decay to 1/1000, or 60dB from its original amplitude.
 /// @param loopTime    Determines frequency response curve, loopTime * sr/2 peaks spaced evenly between 0 and sr/2.
 - (instancetype)initWithAudioSource:(AKAudio *)audioSource
                          reverbTime:(AKControl *)reverbTime
                            loopTime:(AKConstant *)loopTime;
-
-/// Instantiates the comb filter
-/// @param audioSource        Input Signal
-/// @param reverbTime         The time in seconds for a signal to decay to 1/1000, or 60dB from its original amplitude.
-/// @param loopTime           Determines frequency response curve, loopTime * sr/2 peaks spaced evenly between 0 and sr/2.
-/// @param delayAmount        Delay amount, in samples.
-/// @param isFeedbackRetained Initial disposition of delay-loop data space. True retains previous information in feedback loop.
-- (instancetype)initWithAudioSource:(AKAudio *)audioSource
-                         reverbTime:(AKControl *)reverbTime
-                           loopTime:(AKConstant *)loopTime
-                        delayAmount:(AKConstant *)delayAmount
-                 isFeedbackRetained:(BOOL)isFeedbackRetained;
-
-/// Set an optional delay amount.
-/// @param delayAmount Delay amount, in samples.
-- (void)setOptionalDelayAmount:(AKConstant *)delayAmount;
 
 /// Set an optional delay offset.
 /// @param isFeedbackRetained Initial disposition of delay-loop data space. True retains previous information in feedback loop.
--- AudioKit/Operations/Signal Modifiers/Filters/AKCombFilter.m
+++ AudioKit/Operations/Signal Modifiers/Filters/AKCombFilter.m
@@ -2,8 +2,8 @@
 //  AKCombFilter.m
 //  AudioKit
 //
-//  Created by Adam Boulanger on 4/10/13.
-//  Copyright (c) 2013 Hear For Yourself. All rights reserved.
+//  Created by Aurelius Prochazka on 8/13/14
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 
 #import "AKCombFilter.h"
@@ -29,43 +29,20 @@
         krvt = reverbTime;
         ilpt = loopTime;
         iskip = akp(0);
-        insmps = akp(0);
     }
     return self;
     
 }
 
-- (instancetype)initWithAudioSource:(AKAudio *)audioSource
-                         reverbTime:(AKControl *)reverbTime
-                           loopTime:(AKConstant *)loopTime
-                        delayAmount:(AKConstant *)delayAmount
-                 isFeedbackRetained:(BOOL)isFeedbackRetained
-{
-    self = [super initWithString:[self operationName]];
-    if(self) {
-        asig = audioSource;
-        krvt = reverbTime;
-        ilpt = loopTime;
-        iskip = akp( isFeedbackRetained);
-        insmps = delayAmount;
-    }
-    return self;
-}
-
-- (void)setOptionalDelayAmount:(AKConstant *)delayAmount
-{
-    insmps = delayAmount;
-}
-
 - (void)setOptionalRetainFeedbackFlag:(BOOL)isFeedbackRetained
 {
-    iskip = akp( isFeedbackRetained);
+    iskip = akp(isFeedbackRetained);
 }
 
 - (NSString *)stringForCSD {
     return [NSString stringWithFormat:
-            @"%@ comb %@, %@, %@, %@, %@",
-            self, asig, krvt, ilpt, iskip, insmps];
+            @"%@ comb %@, %@, %@, %@",
+            self, asig, krvt, ilpt, iskip];
 }
 
 @end
