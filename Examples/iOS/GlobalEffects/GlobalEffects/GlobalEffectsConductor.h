f5209d7b2d2dd4c9357762f4ee89352940adf0e5
Aurelius Prochazka <aure@aure.com>
2014-08-04 15:44:48 -0700

    Updated Global Effects example in both iOS and OSX to use the same conductor code.

---
+++ Examples/iOS/GlobalEffects/GlobalEffects/GlobalEffectsConductor.h
@@ -0,0 +1,13 @@
+//
+//  GlobalEffectsConductor.h
+//  GlobalEffects
+//
+//  Created by Aurelius Prochazka on 8/4/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GlobalEffectsConductor : NSObject
+
+@end
