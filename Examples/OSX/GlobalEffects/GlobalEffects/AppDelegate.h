695676cb0cc6ada9ef384c20d7827b3c8195d5e5
Aurelius Prochazka <aure@aure.com>
2014-08-04 01:04:44 -0700

    Add the Global Effects example to the OSX Examples.

---
+++ Examples/OSX/GlobalEffects/GlobalEffects/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GlobalEffects
+//
+//  Created by Aurelius Prochazka on 8/4/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
