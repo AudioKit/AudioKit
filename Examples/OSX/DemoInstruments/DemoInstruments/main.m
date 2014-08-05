766a7f357761afec95ee8cc6c768635676033989
Aurelius Prochazka <aure@aure.com>
2014-08-04 18:11:35 -0700

    Added the DemoInstruments example to OSX Examples.

---
+++ Examples/OSX/DemoInstruments/DemoInstruments/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DemoInstruments
+//
+//  Created by Aurelius Prochazka on 8/4/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
