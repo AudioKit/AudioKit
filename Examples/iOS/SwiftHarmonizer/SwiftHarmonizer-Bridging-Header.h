21c1fa5fcbaf8b633c9f3d6a13bd174e9ee2f240
Nicholas Arner <nicholasarner@gmail.com>
2014-10-12 14:26:13 -0400

    Added SwiftVersion of Harmonizer example

---
+++ Examples/iOS/SwiftHarmonizer/SwiftHarmonizer-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  SwiftHarmonizer-Bridging-Header.h
+//  SwiftHarmonizer
+//
+//  Created by Nicholas Arner on 10/7/14.
+//  Copyright (c) 2014 Nicholas Arner. All rights reserved.
+//
+
+#import "AKFoundation.h"
+#import "AKTools.h"
---
+++ Examples/iOS/SwiftHarmonizer/SwiftHarmonizer/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  SwiftHarmonizer
+//
+//  Created by Nicholas Arner on 10/7/14.
+//  Copyright (c) 2014 Nicholas Arner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let harmonizer = HarmonizerInstrument();
+    let sampler = AKSampler();
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let orchestra = AKOrchestra()
+        orchestra.addInstrument(harmonizer)
+        let manager = AKManager.sharedAKManager()
+        manager.runOrchestra(orchestra)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func startRecording(sender: AnyObject) {
+        harmonizer.play()
+        sampler.startRecordingToTrack("harmonizer")
+    }
+    
+    @IBAction func stopRecording(sender: AnyObject) {
+        harmonizer.stop()
+        sampler.stopRecordingToTrack("harmonizer")
+    }
+    
+    @IBAction func startPlaying(sender: AnyObject) {
+        sampler.startPlayingTrack("harmonizer")
+    }
+
+    @IBAction func stopPlaying(sender: AnyObject) {
+        sampler.stopPlayingTrack("harmonizer")
+    }
+}
