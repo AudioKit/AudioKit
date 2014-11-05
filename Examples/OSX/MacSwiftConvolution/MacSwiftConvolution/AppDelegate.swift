c5fbe8f0f12e40b8e764da8cadc24f45d0eb84ac
Aurelius Prochazka <aure@aure.com>
2014-11-04 22:52:22 -0800

    Mac Swift Convolution Example added.

---
+++ Examples/OSX/MacSwiftConvolution/MacSwiftConvolution/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacSwiftConvolution
+//
+//  Created by Aurelius Prochazka on 11/4/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
