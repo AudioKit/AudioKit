a6e19323620b08ea62e9506097822aaaf5eae0c5
Aurelius Prochazka <aure@aure.com>
2014-11-10 22:57:44 -0800

    Parameters get initial, min, and max values.
    Pushed up the chain from properties, this reduces the amount of code and sets up future improvements.

--- AudioKit/Parameters/AKControl.h
+++ AudioKit/Parameters/AKControl.h
@@ -12,6 +12,39 @@
  */
 @interface AKControl : AKParameter
 
+/// Current value of the parameter.
+@property (nonatomic, assign) float value;
+
+/// Start value for initialization.
+@property (nonatomic, assign) float initialValue;
+
+/// Minimum Value allowed.
+@property (nonatomic, assign) float minimum;
+
+/// Maximum Value allowed.
+@property (nonatomic, assign) float maximum;
+
+/// Initialize the control with bounds.
+/// @param minimum Minimum value.
+/// @param maximum Maximum value.
+- (instancetype)initWithMinimum:(float)minimum
+                        maximum:(float)maximum;
+
+/// Initialize the control with an initial value and bounds.
+/// @param initialValue Initial value.
+/// @param minimum Minimum value.
+/// @param maximum Maximum value.
+- (instancetype)initWithValue:(float)initialValue
+                      minimum:(float)minimum
+                      maximum:(float)maximum;
+
+/// Sets the current value to the initial value.
+- (void)reset;
+
+/// Randomize the current value between the minimum and maximum values
+- (void)randomize;
+
 /// Converts pitch to frequency
 - (instancetype)toCPS;
+
 @end
