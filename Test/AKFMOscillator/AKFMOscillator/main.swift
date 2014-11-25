03998d1882d6713d64d8d1c34bf1d13f6235d9ab
Aurelius Prochazka <aure@aure.com>
2014-11-25 15:41:57 -0800

    First Test script for AKFMOscillator

---
+++ Test/AKFMOscillator/AKFMOscillator/main.swift
@@ -0,0 +1,40 @@
+//
+//  main.swift
+//  AudioKit
+//
+//  Auto-generated from scripts by Aurelius Prochazka on 11/22/14.
+//  Modified by Aurelius Prochazka on 11/23/14 to include sine table.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
+//
+
+import Foundation
+
+class Instrument : AKInstrument {
+
+    override init() {
+        super.init()
+
+        let sine = AKSineTable()
+        connect(sine)
+
+        let operation = AKFMOscillator(
+            FTable: sine
+        )
+        operation.setOptionalCarrierMultiplier(4.4.ak)
+        operation.carrierMultiplier = 2.4.ak
+        operation.carrierMultiplier.value()
+        connect(operation)
+        connect(AKAudioOutput(audioSource:operation))
+    }
+}
+
+// Set Up
+let orchestra = AKOrchestra()
+let instrument = Instrument()
+orchestra.addInstrument(instrument)
+let manager = AKManager.sharedAKManager()
+
+// Run Test
+manager.runTestOrchestra(orchestra)
+while(manager.isRunning) {} //do nothing
+println("Test complete!")
