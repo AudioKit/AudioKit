af49a14cd9fa8dee33f4239017b7aaea570030c8
Aurelius Prochazka <aure@aure.com>
2014-11-26 01:54:55 -0800

    Improved and tested AKCabasa

---
+++ Test/AKCabasa/AKCabasa/Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Bridging-Header.h
+//  AudioKit
+//
+//  Auto-generated from scripts by Aurelius Prochazka on 11/25/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
+//
+
+#import "AKFoundation.h"
+#import "AKTools.h"
---
+++ Test/AKCabasa/AKCabasa/main.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  AudioKit
+//
+//  Auto-generated from scripts by Aurelius Prochazka on 11/25/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
+//
+
+import Foundation
+
+class Instrument : AKInstrument {
+
+    override init() {
+        super.init()
+
+        let operation = AKCabasa(
+                    )
+        connect(operation)
+        connect(AKAudioOutput(audioSource:operation))
+    }
+}
+
+// Set Up
+let orchestra = AKOrchestra()
+let instrument = Instrument()
+orchestra.addInstrument(instrument)
+let manager = AKManager.sharedAKManager()
+
+// Run Test
+manager.runTestOrchestra(orchestra)
+while(manager.isRunning) {} //do nothing
+println("Test complete!")
