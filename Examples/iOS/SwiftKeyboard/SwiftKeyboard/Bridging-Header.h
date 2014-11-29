c45de170e3d9e5fe0a0645e7ebda4d30ada801ce
Aurelius Prochazka <aure@aure.com>
2014-11-28 19:36:17 -0800

    Started Swift version of Keyboard App

---
+++ Examples/iOS/SwiftKeyboard/SwiftKeyboard/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  SwiftKeyboard
+//
+//  Created by Aurelius Prochazka on 11/28/14.
+//  Copyright (c) 2014 AudioKit. All rights reserved.
+//
+
+#ifndef SwiftKeyboard_Bridging_Header_h
+#define SwiftKeyboard_Bridging_Header_h
+
+
+#endif
---
+++ Examples/iOS/SwiftKeyboard/SwiftKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftKeyboard
+//
+//  Created by Aurelius Prochazka on 11/28/14.
+//  Copyright (c) 2014 AudioKit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
