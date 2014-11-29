5eb52eff11e996dbda5ff43583d625174ed5fcac
Aurelius Prochazka <aure@aure.com>
2014-11-28 20:07:44 -0800

    Added Tone Generation to Swift Keyboard Example

--- Examples/iOS/SwiftKeyboard/SwiftKeyboard/Instruments/ToneGenerator.swift
+++ Examples/iOS/SwiftKeyboard/SwiftKeyboard/Instruments/ToneGenerator.swift
@@ -9,28 +9,45 @@
 class ToneGenerator: AKInstrument {
 
     // Instrument Properties
-    var pan  = AKInstrumentProperty(value: 0.0, minimum: 0.0, maximum: 1.0)
-
+    var toneColor  = AKInstrumentProperty(value: 0.5, minimum: 0.1, maximum: 1.0)
+    var auxilliaryOutput = AKAudio()
+    
     override init() {
         super.init()
 
         // Instrument Properties
-        addProperty(pan)
+        addProperty(toneColor)
 
         // Note Properties
-        note = ToneGeneratorNote()
-
-        // Place instrument code here
+        let note = ToneGeneratorNote()
+        addNoteProperty(note.frequency)
+        
+        let sine = AKSineTable()
+        addFTable(sine)
+        
+        let fmOscillator = AKFMOscillator(
+            FTable: sine,
+            baseFrequency: note.frequency,
+            carrierMultiplier: toneColor.scaledBy(2.ak),
+            modulatingMultiplier: toneColor.scaledBy(1.2.ak),
+            modulationIndex: toneColor.scaledBy(1.5.ak),
+            amplitude: 0.15.ak,
+            phase: 0.ak)
+        connect(fmOscillator)
+        
+        let output = AKAudioOutput(audioSource: fmOscillator)
+        connect(output)
     }
 }
 
-class ToneGeneratorNote: AKInstrumentNote {
 
-    // Place Note Properties here
-
+class ToneGeneratorNote: AKNote {
+    
+    // Note Properties
+    var frequency = AKNoteProperty(minimum: 440, maximum: 880)
+    
     override init() {
         super.init()
-
-        // Place instrument code here
+        addProperty(frequency)
     }
 }
