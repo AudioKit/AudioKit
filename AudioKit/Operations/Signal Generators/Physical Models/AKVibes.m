38a6df2ca194feda585b1391d97dcc7a7053a5a5
Nicholas Arner <nicholasarner@gmail.com>
2014-11-30 15:30:27 -0500

    Start of AKVibes V2 Tests and Defaults

--- AudioKit/Operations/Signal Generators/Physical Models/AKVibes.m
+++ AudioKit/Operations/Signal Generators/Physical Models/AKVibes.m
@@ -2,63 +2,108 @@
 //  AKVibes.m
 //  AudioKit
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 11/3/12.
-//  Improved from database version by Aurelius Prochazka on 12/27/12.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 11/30/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 //  Implementation of Csound's vibes:
 //  http://www.csounds.com/manual/html/vibes.html
 //
 
 #import "AKVibes.h"
-#import "AKSoundFileTable.h"
+#import "AKManager.h"
 
 @implementation AKVibes
-{
-    AKControl *kfreq;
-    AKConstant *idec;
-    AKConstant *ihrd;
-    AKConstant *ipos;
-    AKControl *kamp;
-    AKFTable *ifnvib;
-    AKControl *kvibf;
-    AKControl *kvamp;
-}
 
 - (instancetype)initWithFrequency:(AKControl *)frequency
                   maximumDuration:(AKConstant *)maximumDuration
                     stickHardness:(AKConstant *)stickHardness
                    strikePosition:(AKConstant *)strikePosition
-                        amplitude:(AKControl *)amplitude
                 tremoloShapeTable:(AKFTable *)tremoloShapeTable
                  tremoloFrequency:(AKControl *)tremoloFrequency
                  tremoloAmplitude:(AKControl *)tremoloAmplitude
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        kfreq = frequency;
-        idec = maximumDuration;
-        ihrd = stickHardness;
-        ipos = strikePosition;
-        kamp = amplitude;
-        ifnvib = tremoloShapeTable;
-        kvibf = tremoloFrequency;
-        kvamp = tremoloAmplitude;
+            _frequency = frequency;
+                _maximumDuration = maximumDuration;
+                _stickHardness = stickHardness;
+                _strikePosition = strikePosition;
+                _tremoloShapeTable = tremoloShapeTable;
+                _tremoloFrequency = tremoloFrequency;
+                _tremoloAmplitude = tremoloAmplitude;
+        
     }
     return self;
 }
 
-- (NSString *)stringForCSD {
-    NSString *file;
-    file = [[NSBundle mainBundle] pathForResource:@"marmstk1" ofType:@"wav"];
-    AKSoundFileTable *fileTable;
-    fileTable = [[AKSoundFileTable alloc] initWithFilename:file];
-    
-    return [NSString stringWithFormat:
-            @"%@\n"
-            @"%@ vibes %@, %@, %@, %@, %@, %@, %@, %@",
-            [fileTable stringForCSD],
-            self, kamp, kfreq, ihrd, ipos, kvibf, kvamp, ifnvib, idec];
+- (instancetype)init
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        
+    // Default Values   
+            _frequency = akp(440);        
+            _maximumDuration = akp(0.5);        
+            _stickHardness = akp(0.5);        
+            _strikePosition = akp(0);        
+           _tremoloShapeTable = [AKManager sharedAKManager].standardSineTable;
+            
+            _tremoloFrequency = akp(6);        
+            _tremoloAmplitude = akp(0);            
+    }
+    return self;
 }
+
++ (instancetype)audio
+ {
+    return [[AKVibes alloc] init];
+}
+
+- (void)setOptionalFrequency:(AKControl *)frequency {
+    _frequency = frequency;
+}
+
+- (void)setOptionalMaximumDuration:(AKConstant *)maximumDuration {
+    _maximumDuration = maximumDuration;
+}
+
+- (void)setOptionalStickHardness:(AKConstant *)stickHardness {
+    _stickHardness = stickHardness;
+}
+
+- (void)setOptionalStrikePosition:(AKConstant *)strikePosition {
+    _strikePosition = strikePosition;
+}
+
+- (void)setOptionalTremoloShapeTable:(AKFTable *)tremoloShapeTable {
+    _tremoloShapeTable = tremoloShapeTable;
+}
+
+- (void)setOptionalTremoloFrequency:(AKControl *)tremoloFrequency {
+    _tremoloFrequency = tremoloFrequency;
+}
+
+- (void)setOptionalTremoloAmplitude:(AKControl *)tremoloAmplitude {
+    _tremoloAmplitude = tremoloAmplitude;
+}
+
+- (NSString *)stringForCSD {
+        // Constant Values  
+        AKConstant *_amplitude = akp(1.0);    
+        AKConstant *_strikeImpulseTable = akp();    
+        return [NSString stringWithFormat:
+            @"%@ vibes %@, %@, %@, %@, %@, %@, %@, %@, %@",
+            self,
+            _amplitude,
+            _frequency,
+            _stickHardness,
+            _strikePosition,
+            _strikeImpulseTable,
+            _tremoloFrequency,
+            _tremoloAmplitude,
+            _tremoloShapeTable,
+            _maximumDuration];
+}
+
 
 @end
