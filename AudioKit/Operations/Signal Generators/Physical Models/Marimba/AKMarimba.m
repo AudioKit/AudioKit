4247207fd95fb70b117f4333a6fe1530217fefa8
Aurelius Prochazka <aure@aure.com>
2014-11-30 22:26:08 -0800

    AKMarimba V2 with Custom Tests and Defaults

--- AudioKit/Operations/Signal Generators/Physical Models/Marimba/AKMarimba.m
+++ AudioKit/Operations/Signal Generators/Physical Models/Marimba/AKMarimba.m
@@ -2,81 +2,133 @@
 //  AKMarimba.m
 //  AudioKit
 //
-//  Created by Aurelius Prochazka on 10/31/12.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 11/30/14.
+//  Customized by Aurelius Prochazka on 11/30/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 //  Implementation of Csound's marimba:
 //  http://www.csounds.com/manual/html/marimba.html
 //
 
 #import "AKMarimba.h"
-
-
-@interface AKMarimba () {
-    AKControl *kfreq;
-    AKConstant *idec;
-    AKConstant *ihrd;
-    AKConstant *ipos;
-    AKControl *kamp;
-    AKFTable *ifnvib;
-    AKControl *kvibf;
-    AKControl *kvamp;
-    AKConstant *idoubles;
-    AKConstant *itriples;
-    AKSoundFileTable *fileTable;
-}
-@end
+#import "AKManager.h"
+#import "AKSoundFileTable.h"
 
 @implementation AKMarimba
 
 - (instancetype)initWithFrequency:(AKControl *)frequency
-                  maximumDuration:(AKConstant *)maximumDuration
                     stickHardness:(AKConstant *)stickHardness
                    strikePosition:(AKConstant *)strikePosition
-                        amplitude:(AKControl *)amplitude
                 vibratoShapeTable:(AKFTable *)vibratoShapeTable
                  vibratoFrequency:(AKControl *)vibratoFrequency
                  vibratoAmplitude:(AKControl *)vibratoAmplitude
+           doubleStrikePercentage:(AKConstant *)doubleStrikePercentage
+           tripleStrikePercentage:(AKConstant *)tripleStrikePercentage
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        kfreq = frequency;
-        idec = maximumDuration;
-        ihrd = stickHardness;
-        ipos = strikePosition;
-        kamp = amplitude;
-        ifnvib = vibratoShapeTable;
-        kvibf = vibratoFrequency;
-        kvamp = vibratoAmplitude;
-        idoubles = akp(40);
-        itriples = akp(20);
+        _frequency = frequency;
+        _stickHardness = stickHardness;
+        _strikePosition = strikePosition;
+        _vibratoShapeTable = vibratoShapeTable;
+        _vibratoFrequency = vibratoFrequency;
+        _vibratoAmplitude = vibratoAmplitude;
+        _doubleStrikePercentage = doubleStrikePercentage;
+        _tripleStrikePercentage = tripleStrikePercentage;
         
-        NSString *file = [[NSBundle mainBundle] pathForResource:@"marmstk1" ofType:@"wav"];
-        fileTable = [[AKSoundFileTable alloc] initWithFilename:file];
     }
     return self;
 }
 
+- (instancetype)init
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        
+        // Default Values
+        _frequency = akp(220);
+        _stickHardness = akp(0.5);
+        _strikePosition = akp(0.5);
+        _vibratoShapeTable = [AKManager standardSineTable];
+        
+        _vibratoFrequency = akp(0);
+        _vibratoAmplitude = akp(0);
+        _doubleStrikePercentage = akp(40);
+        _tripleStrikePercentage = akp(20);
+    }
+    return self;
+}
+
++ (instancetype)audio
+{
+    return [[AKMarimba alloc] init];
+}
+
+- (void)setOptionalFrequency:(AKControl *)frequency {
+    _frequency = frequency;
+}
+
+- (void)setOptionalStickHardness:(AKConstant *)stickHardness {
+    _stickHardness = stickHardness;
+}
+
+- (void)setOptionalStrikePosition:(AKConstant *)strikePosition {
+    _strikePosition = strikePosition;
+}
+
+- (void)setOptionalVibratoShapeTable:(AKFTable *)vibratoShapeTable {
+    _vibratoShapeTable = vibratoShapeTable;
+}
+
+- (void)setOptionalVibratoFrequency:(AKControl *)vibratoFrequency {
+    _vibratoFrequency = vibratoFrequency;
+}
+
+- (void)setOptionalVibratoAmplitude:(AKControl *)vibratoAmplitude {
+    _vibratoAmplitude = vibratoAmplitude;
+}
+
 - (void)setOptionalDoubleStrikePercentage:(AKConstant *)doubleStrikePercentage {
-	idoubles = doubleStrikePercentage;
+    _doubleStrikePercentage = doubleStrikePercentage;
 }
 
 - (void)setOptionalTripleStrikePercentage:(AKConstant *)tripleStrikePercentage {
-	itriples = tripleStrikePercentage;
+    _tripleStrikePercentage = tripleStrikePercentage;
 }
-
-- (void)setOptionalStrikeImpulseTable:(AKSoundFileTable *)strikeImpulseTable {
-    fileTable = strikeImpulseTable;
-}
-
-
 
 - (NSString *)stringForCSD {
+    // Constant Values
+    AKConstant *_maximumDuration = akp(1);
+    AKConstant *_amplitude = akp(1);
+    
+    NSString *file;
+    if ([[[AKManager sharedAKManager] fullPathToAudioKit] isKindOfClass:[NSString class]]) {
+        file = [[AKManager sharedAKManager] fullPathToAudioKit];
+        file = [file stringByAppendingPathComponent:@"AudioKit/Operations/Signal Generators/Physical Models/Marimba/marmstk1.wav"];
+    } else {
+        file = [[NSBundle mainBundle] pathForResource:@"marmstk1" ofType:@"wav"];
+    }
+    NSLog(@"file %@", file);
+    AKSoundFileTable *_strikeImpulseTable;
+    _strikeImpulseTable = [[AKSoundFileTable alloc] initWithFilename:file];
+    
     return [NSString stringWithFormat:
             @"%@\n"
             @"%@ marimba %@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@",
-            [fileTable stringForCSD],
-            self, kamp, kfreq, ihrd, ipos, fileTable, kvibf, kvamp, ifnvib, idec, idoubles, itriples];
+            [_strikeImpulseTable stringForCSD],
+            self,
+            _amplitude,
+            _frequency,
+            _stickHardness,
+            _strikePosition,
+            _strikeImpulseTable,
+            _vibratoFrequency,
+            _vibratoAmplitude,
+            _vibratoShapeTable,
+            _maximumDuration,
+            _doubleStrikePercentage,
+            _tripleStrikePercentage];
 }
+
 
 @end
