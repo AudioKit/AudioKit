23629617983baff94ae932359e9ede297c81c6d1
Aurelius Prochazka <aure@aure.com>
2014-11-30 23:26:46 -0800

    AKMandolin v2 Customized Tests and Defaults

---
+++ Tests/AKMandolin/AKMandolin/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  AudioKit
+//
+//  Auto-generated from scripts by Aurelius Prochazka on 11/30/14.
+//  Customized by Aurelius Prochazka on 11/30/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
+//
+
+import Foundation
+
+class Instrument : AKInstrument {
+
+    override init() {
+        super.init()
+
+        let operation = AKMandolin()
+        connect(operation)
+        connect(AKAudioOutput(audioSource:operation))
+    }
+}
+
+// Set Up
+AKManager.sharedAKManager().fullPathToAudioKit = "/Users/aure/Developer/h4y/AudioKit/"
+let instrument = Instrument()
+AKOrchestra.addInstrument(instrument)
+AKOrchestra.test()
+
+while(AKManager.sharedAKManager().isRunning) {} //do nothing
+println("Test complete!")
