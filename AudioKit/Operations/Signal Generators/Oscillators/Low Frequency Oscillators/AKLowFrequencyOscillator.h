eac947eec319b1cf22ed929f1ca44308696bf497
Nicholas Arner <nicholasarner@gmail.com>
2014-12-02 11:45:18 -0500

    AKLowFrequencyOscillator V2 Tests and Defaults Description will need to be re-written

--- AudioKit/Operations/Signal Generators/Oscillators/Low Frequency Oscillators/AKLowFrequencyOscillator.h
+++ AudioKit/Operations/Signal Generators/Oscillators/Low Frequency Oscillators/AKLowFrequencyOscillator.h
@@ -2,32 +2,50 @@
 //  AKLowFrequencyOscillator.h
 //  AudioKit
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 11/4/12.
-//  Modified by Aurelius Prochazka on 11/4/12 to enumerate types.
-//
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/2/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 
 #import "AKAudio.h"
 #import "AKParameter+Operation.h"
-#import "AKLowFrequencyOscillatorConstants.h"
 
-/** A low frequency oscillator of various shapes.s
+/** A low frequency oscillator of various shapes.
+
+ More detailed description from http://www.csounds.com/manual/html/
  */
 
 @interface AKLowFrequencyOscillator : AKAudio
 
-/// Instantiates the low frequency oscillator
+/// Instantiates the low frequency oscillator with all values
 /// @param frequency Frequency of the note.
-/// @param amplitude Amplitude of output.
+/// @param type Waveform of the oscillator, can be sine, triangle, square (bipolar), square (unipolar), saw-tooth, saw-tooth (down).
 - (instancetype)initWithFrequency:(AKControl *)frequency
-                        amplitude:(AKControl *)amplitude;
+                             type:(AKConstant *)type;
+
+/// Instantiates the low frequency oscillator with default values
+- (instancetype)init;
 
 
+/// Instantiates the low frequency oscillator with default values
++ (instancetype)audio;
+
+
+
+
+/// Frequency of the note. [Default Value: 440]
+@property AKControl *frequency;
+
+/// Set an optional frequency
+/// @param frequency Frequency of the note. [Default Value: 440]
+- (void)setOptionalFrequency:(AKControl *)frequency;
+
+
+/// Waveform of the oscillator, can be sine, triangle, square (bipolar), square (unipolar), saw-tooth, saw-tooth (down). [Default Value: 0]
+@property AKConstant *type;
 
 /// Set an optional type
-/// @param type Waveform of the oscillator, can be sine, triangle, square (bipolar), square (unipolar), saw-tooth, saw-tooth (down).
-- (void)setOptionalType:(LFOType)type;
+/// @param type Waveform of the oscillator, can be sine, triangle, square (bipolar), square (unipolar), saw-tooth, saw-tooth (down). [Default Value: 0]
+- (void)setOptionalType:(AKConstant *)type;
 
 
 @end
