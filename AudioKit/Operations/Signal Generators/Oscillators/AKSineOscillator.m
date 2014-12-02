faee847f889a95a5602578efec26d254249c743d
Nicholas Arner <nicholasarner@gmail.com>
2014-12-02 11:08:23 -0500

    AKSineOscillator V2 Tests and Defaults

--- AudioKit/Operations/Signal Generators/Oscillators/AKSineOscillator.m
+++ AudioKit/Operations/Signal Generators/Oscillators/AKSineOscillator.m
@@ -2,42 +2,65 @@
 //  AKSineOscillator.m
 //  AudioKit
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 11/11/12.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/2/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 //  Implementation of Csound's oscils:
 //  http://www.csounds.com/manual/html/oscils.html
 //
 
 #import "AKSineOscillator.h"
+#import "AKManager.h"
 
 @implementation AKSineOscillator
-{
-    AKConstant *icps;
-    AKConstant *iamp;
-    AKConstant *iphs;
-}
 
 - (instancetype)initWithFrequency:(AKConstant *)frequency
-                        amplitude:(AKConstant *)amplitude
+                            phase:(AKConstant *)phase
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        icps = frequency;
-        iamp = amplitude;
-        iphs = akp(0);
+            _frequency = frequency;
+                _phase = phase;
+        
     }
     return self;
 }
 
+- (instancetype)init
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        
+    // Default Values   
+            _frequency = akp(440);        
+            _phase = akp(0);            
+    }
+    return self;
+}
+
++ (instancetype)audio
+ {
+    return [[AKSineOscillator alloc] init];
+}
+
+- (void)setOptionalFrequency:(AKConstant *)frequency {
+    _frequency = frequency;
+}
+
 - (void)setOptionalPhase:(AKConstant *)phase {
-	iphs = phase;
+    _phase = phase;
 }
 
 - (NSString *)stringForCSD {
-    return [NSString stringWithFormat:
+        // Constant Values  
+                    AKConstant *_amplitude = akp(1);        
+        return [NSString stringWithFormat:
             @"%@ oscils %@, %@, %@",
-            self, iamp, icps, iphs];
+            self,
+            _amplitude,
+            _frequency,
+            _phase];
 }
+
 
 @end
