947de070b87aa3d56556205b2cabe1e02876c883
Aurelius Prochazka <aure@aure.com>
2014-12-06 23:33:27 -0800

    Added Xcode AudioKit file templates

---
+++ Templates/File Templates/AudioKit/AudioKit Processor.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,15 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : AKInstrument
+
+@property (nonatomic, strong) AKInstrumentProperty *reverb;
+
+- (instancetype)initWithAudioSource:(AKAudio *)audioSource;
+
+@end
