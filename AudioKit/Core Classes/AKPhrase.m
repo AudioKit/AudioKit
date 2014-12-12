2f62f576c46504999fc780a7d04775e65cb828f7
Aurelius Prochazka <aure@aure.com>
2014-12-12 02:13:43 -0800

    Implemented first version of AKPhrase

--- AudioKit/Core Classes/AKPhrase.m
+++ AudioKit/Core Classes/AKPhrase.m
@@ -7,7 +7,43 @@
 //
 
 #import "AKPhrase.h"
+#import "AKNote.h"
+#import "AKInstrument.h"
 
-@implementation AKPhrase
+@interface AKPhrase() {
+    NSMutableArray *timeNotePairs;
+}
+@end
+
+@implementation AKPhrase 
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        timeNotePairs = [[NSMutableArray alloc] init];
+    }
+    return self;
+    
+}
+
+- (void)addNote:(AKNote *)note {
+    [timeNotePairs addObject:@[@0, note]];
+}
+- (void)addNote:(AKNote *)note atTime:(float)time {
+    NSNumber *startTime = [NSNumber numberWithFloat:time];
+    [timeNotePairs addObject:@[startTime, note]];
+}
+
+- (void)playUsingInstrument:(AKInstrument *)instrument
+{
+    for (NSArray *timeNotePair in timeNotePairs) {
+        float time = [(NSNumber *)[timeNotePair objectAtIndex:0] floatValue];
+        AKNote *note = (AKNote *)[timeNotePair objectAtIndex:1];
+        [instrument playNote:note afterDelay:time];
+    }
+}
+
+
+
 
 @end
