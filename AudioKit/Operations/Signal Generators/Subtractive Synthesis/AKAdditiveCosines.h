cbf6461474424c8429a8423a262bf6c5946e4993
Aurelius Prochazka <aure@aure.com>
2014-12-14 01:10:44 -0800

    AKAdditiveCosines Tests and Defaults

--- AudioKit/Operations/Signal Generators/Subtractive Synthesis/AKAdditiveCosines.h
+++ AudioKit/Operations/Signal Generators/Subtractive Synthesis/AKAdditiveCosines.h
@@ -2,35 +2,96 @@
 //  AKAdditiveCosines.h
 //  AudioKit
 //
-//  Created by Aurelius Prochazka on 12/22/12.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/14/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
+
 
 #import "AKAudio.h"
 #import "AKParameter+Operation.h"
 
-/** Generate an additive set of harmonically related cosine partials of a fundamental frequency,
- and whose amplitudes are scaled so their summation peak equal the given amplitude property.
- Properties determine the selection and strength of partials. This is a band-limited pulse train:
- if the partials extend to the Nyquist, i.e. harmonicsCount = int (sr / 2 / fundamental freq.),
- the result is a real pulse train of amplitude (amplitude).
- 
+/** A set of harmonically related cosine partials.
+
+ More detailed description from http://www.csounds.com/manual/html/
  */
 
 @interface AKAdditiveCosines : AKAudio
 
-/// Create a set of harmonically related cosine partials
-/// @param cosineTable A cosine table with at least 8192 points is recommended
+/// Instantiates the additive cosines with all values
+/// @param cosineTable A cosine table with at least 8192 points is recommended.
 /// @param harmonicsCount Total number of harmonics requested.
 /// @param firstHarmonicIndex The lowest harmonic present.
 /// @param partialMultiplier The multiplier in the series of amplitude coefficients.
-/// @param fundamentalFrequency Thefrequency which can be modulated at any rate.
-/// @param amplitude Total amplitude which can be modulated at any rate.
--  (instancetype)initWithFTable:(AKFTable *)cosineTable
-                 harmonicsCount:(AKControl *)harmonicsCount
-             firstHarmonicIndex:(AKControl *)firstHarmonicIndex
-              partialMultiplier:(AKControl *)partialMultiplier
-           fundamentalFrequency:(AKParameter *)fundamentalFrequency
-                      amplitude:(AKParameter *)amplitude;
+/// @param fundamentalFrequency The fundamental frequency which can be modulated at any rate.
+/// @param amplitude The total amplitude of the output of all the cosines.
+/// @param phase Initial phase of the fundamental frequency, expressed as a fraction of a cycle (0 to 1).
+- (instancetype)initWithCosineTable:(AKFTable *)cosineTable
+                     harmonicsCount:(AKControl *)harmonicsCount
+                 firstHarmonicIndex:(AKControl *)firstHarmonicIndex
+                  partialMultiplier:(AKControl *)partialMultiplier
+               fundamentalFrequency:(AKParameter *)fundamentalFrequency
+                          amplitude:(AKParameter *)amplitude
+                              phase:(AKConstant *)phase;
+
+
+/// Instantiates the additive cosines with default values
+/// @param cosineTable A cosine table with at least 8192 points is recommended.
+- (instancetype)initWithCosineTable:(AKFTable *)cosineTable;
+
+
+/// Instantiates the additive cosines with default values
+/// @param cosineTable A cosine table with at least 8192 points is recommended.
++ (instancetype)audioWithCosineTable:(AKFTable *)cosineTable;
+
+
+
+
+/// Total number of harmonics requested. [Default Value: 10]
+@property AKControl *harmonicsCount;
+
+/// Set an optional harmonics count
+/// @param harmonicsCount Total number of harmonics requested. [Default Value: 10]
+- (void)setOptionalHarmonicsCount:(AKControl *)harmonicsCount;
+
+
+/// The lowest harmonic present. [Default Value: 1]
+@property AKControl *firstHarmonicIndex;
+
+/// Set an optional first harmonic index
+/// @param firstHarmonicIndex The lowest harmonic present. [Default Value: 1]
+- (void)setOptionalFirstHarmonicIndex:(AKControl *)firstHarmonicIndex;
+
+
+/// The multiplier in the series of amplitude coefficients. [Default Value: 0.5]
+@property AKControl *partialMultiplier;
+
+/// Set an optional partial multiplier
+/// @param partialMultiplier The multiplier in the series of amplitude coefficients. [Default Value: 0.5]
+- (void)setOptionalPartialMultiplier:(AKControl *)partialMultiplier;
+
+
+/// The fundamental frequency which can be modulated at any rate. [Default Value: 220]
+@property AKParameter *fundamentalFrequency;
+
+/// Set an optional fundamental frequency
+/// @param fundamentalFrequency The fundamental frequency which can be modulated at any rate. [Default Value: 220]
+- (void)setOptionalFundamentalFrequency:(AKParameter *)fundamentalFrequency;
+
+
+/// The total amplitude of the output of all the cosines. [Default Value: 1]
+@property AKParameter *amplitude;
+
+/// Set an optional amplitude
+/// @param amplitude The total amplitude of the output of all the cosines. [Default Value: 1]
+- (void)setOptionalAmplitude:(AKParameter *)amplitude;
+
+
+/// Initial phase of the fundamental frequency, expressed as a fraction of a cycle (0 to 1). [Default Value: 0]
+@property AKConstant *phase;
+
+/// Set an optional phase
+/// @param phase Initial phase of the fundamental frequency, expressed as a fraction of a cycle (0 to 1). [Default Value: 0]
+- (void)setOptionalPhase:(AKConstant *)phase;
+
 
 @end
