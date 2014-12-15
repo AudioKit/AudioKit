083859b596aea95e067d138da3893b8b52240195
Aurelius Prochazka <aure@aure.com>
2014-12-14 20:24:49 -0800

    AKLine Tests and Defaults (and bug fix)

--- AudioKit/Operations/Signal Generators/Lines/AKLine.h
+++ AudioKit/Operations/Signal Generators/Lines/AKLine.h
@@ -2,28 +2,52 @@
 //  AKLine.h
 //  AudioKit
 //
-//  Created by Adam Boulanger on 6/7/12.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/14/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 
 #import "AKAudio.h"
 #import "AKParameter+Operation.h"
 
-/** Creates a line that extends from a starting to a second point over the given time duration.
- After that duration, the line continues at the same slope until
- the note event ends.  Can be an audio signal or control rate parameter.
+/** Creates an infinitely long line that connects a starting to an ending point over the given time duration.
+
+ If this value is used past the duration point, the line continues indefinitely.
  */
 
 @interface AKLine : AKAudio
+/// Instantiates the line with all values
+/// @param firstPoint Value to start the line from.
+/// @param secondPoint Value to end the line at.
+/// @param durationBetweenPoints Duration in seconds between the defining points.
+- (instancetype)initWithFirstPoint:(AKConstant *)firstPoint
+                       secondPoint:(AKConstant *)secondPoint
+             durationBetweenPoints:(AKConstant *)durationBetweenPoints;
 
-/// Initialize a linear transition from one value to another over specified time.
-/// @param startingValue Value to start the line from.
-/// @param endingValue   Value to end the line at.
-/// @param duration      Duration of linear transition in seconds.
-/// @return An opcode to perform a linear transition over a given duration.
-- (instancetype)initFromValue:(AKConstant *)startingValue
-                      toValue:(AKConstant *)endingValue
-                     duration:(AKConstant *)duration;
+/// Instantiates the line with default values
+- (instancetype)init;
+
+/// Instantiates the line with default values
++ (instancetype)audio;
+
+/// Value to start the line from. [Default Value: 0]
+@property AKConstant *firstPoint;
+
+/// Set an optional first point
+/// @param firstPoint Value to start the line from. [Default Value: 0]
+- (void)setOptionalFirstPoint:(AKConstant *)firstPoint;
+/// Value to end the line at. [Default Value: 1]
+@property AKConstant *secondPoint;
+
+/// Set an optional second point
+/// @param secondPoint Value to end the line at. [Default Value: 1]
+- (void)setOptionalSecondPoint:(AKConstant *)secondPoint;
+/// Duration in seconds between the defining points. [Default Value: 1]
+@property AKConstant *durationBetweenPoints;
+
+/// Set an optional duration between points
+/// @param durationBetweenPoints Duration in seconds between the defining points. [Default Value: 1]
+- (void)setOptionalDurationBetweenPoints:(AKConstant *)durationBetweenPoints;
+
 
 
 @end
