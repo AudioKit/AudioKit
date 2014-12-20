fc1cce36484b7b9427d9f7c1639b677c667cf67b
Nicholas Arner <nicholasarner@gmail.com>
2014-12-20 11:05:13 -0500

    Sensible defaults for AKLowPassButterworthFilter

--- AudioKit/Operations/Signal Modifiers/Filters/Butterworth Filters/AKLowPassButterworthFilter.h
+++ AudioKit/Operations/Signal Modifiers/Filters/Butterworth Filters/AKLowPassButterworthFilter.h
@@ -2,24 +2,39 @@
 //  AKLowPassButterworthFilter.h
 //  AudioKit
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 12/26/13.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/20/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 
 #import "AKAudio.h"
 #import "AKParameter+Operation.h"
 
 /** A low-pass Butterworth filter.
- 
+
  These filters are Butterworth second-order IIR filters. They offer an almost flat passband and very good precision and stopband attenuation.
  */
 
 @interface AKLowPassButterworthFilter : AKAudio
-
-/// Instantiates the low pass butterworth filter
-/// @param audioSource     Input signal to be filtered.
-/// @param cutoffFrequency Cutoff frequency for each of the filters.
+/// Instantiates the low pass butterworth filter with all values
+/// @param audioSource Input signal to be filtered. [Default Value: ]
+/// @param cutoffFrequency Cutoff frequency for each of the filters. [Default Value: 1000]
 - (instancetype)initWithAudioSource:(AKAudio *)audioSource
                     cutoffFrequency:(AKControl *)cutoffFrequency;
+
+/// Instantiates the low pass butterworth filter with default values
+/// @param audioSource Input signal to be filtered.
+- (instancetype)initWithAudioSource:(AKAudio *)audioSource;
+
+/// Instantiates the low pass butterworth filter with default values
+/// @param audioSource Input signal to be filtered.
++ (instancetype)audioWithAudioSource:(AKAudio *)audioSource;
+/// Cutoff frequency for each of the filters. [Default Value: 1000]
+@property AKControl *cutoffFrequency;
+
+/// Set an optional cutoff frequency
+/// @param cutoffFrequency Cutoff frequency for each of the filters. [Default Value: 1000]
+- (void)setOptionalCutoffFrequency:(AKControl *)cutoffFrequency;
+
+
 
 @end
--- AudioKit/Operations/Signal Modifiers/Filters/Butterworth Filters/AKLowPassButterworthFilter.m
+++ AudioKit/Operations/Signal Modifiers/Filters/Butterworth Filters/AKLowPassButterworthFilter.m
@@ -2,19 +2,19 @@
 //  AKLowPassButterworthFilter.m
 //  AudioKit
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 12/26/13.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/20/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 //  Implementation of Csound's butterlp:
 //  http://www.csounds.com/manual/html/butterlp.html
 //
 
 #import "AKLowPassButterworthFilter.h"
+#import "AKManager.h"
 
 @implementation AKLowPassButterworthFilter
 {
-    AKAudio *asig;
-    AKControl *kfreq;
+    AKAudio *_audioSource;
 }
 
 - (instancetype)initWithAudioSource:(AKAudio *)audioSource
@@ -22,16 +22,38 @@
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        asig = audioSource;
-        kfreq = cutoffFrequency;
+        _audioSource = audioSource;
+        _cutoffFrequency = cutoffFrequency;
     }
     return self;
+}
+
+- (instancetype)initWithAudioSource:(AKAudio *)audioSource
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        _audioSource = audioSource;
+        // Default Values
+        _cutoffFrequency = akp(1000);    
+    }
+    return self;
+}
+
++ (instancetype)audioWithAudioSource:(AKAudio *)audioSource
+{
+    return [[AKLowPassButterworthFilter alloc] initWithAudioSource:audioSource];
+}
+
+- (void)setOptionalCutoffFrequency:(AKControl *)cutoffFrequency {
+    _cutoffFrequency = cutoffFrequency;
 }
 
 - (NSString *)stringForCSD {
     return [NSString stringWithFormat:
             @"%@ butterlp %@, %@",
-            self, asig, kfreq];
+            self,
+            _audioSource,
+            _cutoffFrequency];
 }
 
 @end
