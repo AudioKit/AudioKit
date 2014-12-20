5a4e72fb326578152009feeff27d6cd526a50042
Nicholas Arner <nicholasarner@gmail.com>
2014-12-19 21:57:18 -0500

    Test for AKEqualizerFilter

---
+++ Tests/AKEqualizerFilter/AKEqualizerFilter/main.swift
@@ -0,0 +1,72 @@
+//
+//  main.swift
+//  AudioKit
+//
+//  Auto-generated from scripts by Aurelius Prochazka on 12/19/14.
+//  Customized by Nick Arner on 12/19/14.
+//
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
+//
+
+import Foundation
+
+class Instrument : AKInstrument {
+    
+    var auxilliaryOutput = AKAudio()
+    
+    override init() {
+        super.init()
+        
+        let operation = AKSleighbells()
+        connect(operation)
+        
+        auxilliaryOutput = AKAudio.globalParameter()
+        assignOutput(auxilliaryOutput, to:operation)
+    }
+}
+
+class Processor : AKInstrument {
+    
+    init(audioSource: AKAudio) {
+        super.init()
+        
+        let line = AKLinearControl(firstPoint: 200.ak, secondPoint: 1500.ak, durationBetweenPoints: 11.ak)
+        connect(line)
+        
+        let operation = AKEqualizerFilter(audioSource: audioSource)
+        operation.centerFrequency = line
+        connect(operation)
+        
+        connect(AKAudioOutput(audioSource:operation))
+    }
+}
+
+// Set Up
+let instrument = Instrument()
+let processor = Processor(audioSource: instrument.auxilliaryOutput)
+AKOrchestra.addInstrument(instrument)
+AKOrchestra.addInstrument(processor)
+AKManager.sharedManager().isLogging = true
+AKOrchestra.testForDuration(10)
+
+processor.play()
+let note1 = AKNote()
+// specify properties and create more notes here
+
+let phrase = AKPhrase()
+phrase.addNote(note1, atTime:1.0)
+phrase.addNote(note1, atTime:1.25)
+phrase.addNote(note1, atTime:1.5)
+phrase.addNote(note1, atTime:2.0)
+phrase.addNote(note1, atTime:2.25)
+phrase.addNote(note1, atTime:2.5)
+phrase.addNote(note1, atTime:3.0)
+phrase.addNote(note1, atTime:3.25)
+phrase.addNote(note1, atTime:3.5)
+phrase.addNote(note1, atTime:3.875)
+phrase.addNote(note1, atTime:4.0)
+
+instrument.playPhrase(phrase)
+
+while(AKManager.sharedManager().isRunning) {} //do nothing
+println("Test complete!")
