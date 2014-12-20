d34639a3ce18b2b371b63db6381c7932d3754239
Nicholas Arner <nicholasarner@gmail.com>
2014-12-19 21:57:01 -0500

    Sensible defaults for AKEqualizerFilter

--- AudioKit/Operations/Signal Modifiers/Filters/AKEqualizerFilter.h
+++ AudioKit/Operations/Signal Modifiers/Filters/AKEqualizerFilter.h
@@ -2,29 +2,56 @@
 //  AKEqualizerFilter.h
 //  AudioKit
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 11/11/12.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/19/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 
 #import "AKAudio.h"
 #import "AKParameter+Operation.h"
 
 /** Second-order tunable equalisation filter based on Regalia and Mitra design.
- 
+
  Provides a peak/notch filter for building parametric/graphic equalisers.
- The amplitude response for this filter will be flat (=1) for gain=1. With gain bigger than 1, there will be a peak at the center frequency, whose width is given by the bandwidth parameter, but outside this band, the response will tend towards 1. Conversely, if gain is smaller than 1, a notch will be created around the CF.
+The amplitude response for this filter will be flat (=1) for gain=1. With gain bigger than 1, there will be a peak at the center frequency, whose width is given by the bandwidth parameter, but outside this band, the response will tend towards 1. Conversely, if gain is smaller than 1, a notch will be created around the CF.
  */
 
 @interface AKEqualizerFilter : AKAudio
-
-/// Instantiates the equalizer filter
-/// @param audioSource     Input signal.
-/// @param centerFrequency Filter center frequency in Hz.
-/// @param bandwidth       Peak/notch bandwidth in Hz.
-/// @param gain            Peak/notch gain.
+/// Instantiates the equalizer filter with all values
+/// @param audioSource Input signal. [Default Value: ]
+/// @param centerFrequency Filter center frequency in Hz. [Default Value: 1000]
+/// @param bandwidth Peak/notch bandwidth in Hz. [Default Value: 100]
+/// @param gain Peak/notch gain. [Default Value: 10]
 - (instancetype)initWithAudioSource:(AKAudio *)audioSource
                     centerFrequency:(AKControl *)centerFrequency
                           bandwidth:(AKControl *)bandwidth
                                gain:(AKControl *)gain;
+
+/// Instantiates the equalizer filter with default values
+/// @param audioSource Input signal.
+- (instancetype)initWithAudioSource:(AKAudio *)audioSource;
+
+/// Instantiates the equalizer filter with default values
+/// @param audioSource Input signal.
++ (instancetype)audioWithAudioSource:(AKAudio *)audioSource;
+/// Filter center frequency in Hz. [Default Value: 1000]
+@property AKControl *centerFrequency;
+
+/// Set an optional center frequency
+/// @param centerFrequency Filter center frequency in Hz. [Default Value: 1000]
+- (void)setOptionalCenterFrequency:(AKControl *)centerFrequency;
+/// Peak/notch bandwidth in Hz. [Default Value: 100]
+@property AKControl *bandwidth;
+
+/// Set an optional bandwidth
+/// @param bandwidth Peak/notch bandwidth in Hz. [Default Value: 100]
+- (void)setOptionalBandwidth:(AKControl *)bandwidth;
+/// Peak/notch gain. [Default Value: 10]
+@property AKControl *gain;
+
+/// Set an optional gain
+/// @param gain Peak/notch gain. [Default Value: 10]
+- (void)setOptionalGain:(AKControl *)gain;
+
+
 
 @end
--- AudioKit/Operations/Signal Modifiers/Filters/AKEqualizerFilter.m
+++ AudioKit/Operations/Signal Modifiers/Filters/AKEqualizerFilter.m
@@ -2,21 +2,19 @@
 //  AKEqualizerFilter.m
 //  AudioKit
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 11/11/12.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/19/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 //  Implementation of Csound's eqfil:
 //  http://www.csounds.com/manual/html/eqfil.html
 //
 
 #import "AKEqualizerFilter.h"
+#import "AKManager.h"
 
 @implementation AKEqualizerFilter
 {
-    AKAudio *ain;
-    AKControl *kcf;
-    AKControl *kbw;
-    AKControl *kgain;
+    AKAudio *_audioSource;
 }
 
 - (instancetype)initWithAudioSource:(AKAudio *)audioSource
@@ -26,18 +24,51 @@
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        ain = audioSource;
-        kcf = centerFrequency;
-        kbw = bandwidth;
-        kgain = gain;
+        _audioSource = audioSource;
+        _centerFrequency = centerFrequency;
+        _bandwidth = bandwidth;
+        _gain = gain;
     }
     return self;
 }
 
+- (instancetype)initWithAudioSource:(AKAudio *)audioSource
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        _audioSource = audioSource;
+        // Default Values
+        _centerFrequency = akp(1000);    
+        _bandwidth = akp(100);    
+        _gain = akp(10);    
+    }
+    return self;
+}
+
++ (instancetype)audioWithAudioSource:(AKAudio *)audioSource
+{
+    return [[AKEqualizerFilter alloc] initWithAudioSource:audioSource];
+}
+
+- (void)setOptionalCenterFrequency:(AKControl *)centerFrequency {
+    _centerFrequency = centerFrequency;
+}
+- (void)setOptionalBandwidth:(AKControl *)bandwidth {
+    _bandwidth = bandwidth;
+}
+- (void)setOptionalGain:(AKControl *)gain {
+    _gain = gain;
+}
+
 - (NSString *)stringForCSD {
     return [NSString stringWithFormat:
-            @"%@ eqfil %@, %@, %@, %@",
-            self, ain, kcf, kbw, kgain];
+            @"%@ eqfil %@, %@, %@, %@, %@",
+            self,
+            _audioSource,
+            _centerFrequency,
+            _bandwidth,
+            _gain,
+            _gain];
 }
 
 @end
