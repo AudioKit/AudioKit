3ed92de34ddbe7ee0d7133d3c7ef7fe79be1fe66
Aurelius Prochazka <aure@aure.com>
2014-12-21 12:24:54 -0800

    Sensible defaults and tests for AKOscillatingControl

--- AudioKit/Operations/Signal Generators/Oscillators/AKOscillatingControl.h
+++ AudioKit/Operations/Signal Generators/Oscillators/AKOscillatingControl.h
@@ -2,32 +2,64 @@
 //  AKOscillatingControl.h
 //  AudioKit
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 11/2/12.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/21/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 
 #import "AKControl.h"
 #import "AKParameter+Operation.h"
 
 /** A simple oscillator with linear interpolation.
- 
+
  Reads from the function table sequentially and repeatedly at given frequency. Linear interpolation is applied for table look up from internal phase values.
  */
 
 @interface AKOscillatingControl : AKControl
-
-/// Instantiates the oscillating control
-/// @param fTable Requires a wrap-around guard point
-/// @param frequency Frequency in cycles per second
-/// @param amplitude Amplitude of the output
+/// Instantiates the oscillating control with all values
+/// @param fTable Requires a wrap-around guard point [Default Value: sine]
+/// @param frequency Frequency in cycles per second [Default Value: 1]
+/// @param amplitude Amplitude of the output [Default Value: 1]
+/// @param phase Initial phase of sampling, expressed as a fraction of a cycle (0 to 1). A negative value will cause phase initialization to be skipped. The default value is 0. [Default Value: 0]
 - (instancetype)initWithFTable:(AKFTable *)fTable
                      frequency:(AKControl *)frequency
-                     amplitude:(AKControl *)amplitude;
+                     amplitude:(AKControl *)amplitude
+                         phase:(AKConstant *)phase;
 
+/// Instantiates the oscillating control with default values
+- (instancetype)init;
+
+/// Instantiates the oscillating control with default values
++ (instancetype)control;
+
+
+/// Requires a wrap-around guard point [Default Value: sine]
+@property AKFTable *fTable;
+
+/// Set an optional f table
+/// @param fTable Requires a wrap-around guard point [Default Value: sine]
+- (void)setOptionalFTable:(AKFTable *)fTable;
+
+/// Frequency in cycles per second [Default Value: 1]
+@property AKControl *frequency;
+
+/// Set an optional frequency
+/// @param frequency Frequency in cycles per second [Default Value: 1]
+- (void)setOptionalFrequency:(AKControl *)frequency;
+
+/// Amplitude of the output [Default Value: 1]
+@property AKControl *amplitude;
+
+/// Set an optional amplitude
+/// @param amplitude Amplitude of the output [Default Value: 1]
+- (void)setOptionalAmplitude:(AKControl *)amplitude;
+
+/// Initial phase of sampling, expressed as a fraction of a cycle (0 to 1). A negative value will cause phase initialization to be skipped. The default value is 0. [Default Value: 0]
+@property AKConstant *phase;
 
 /// Set an optional phase
-/// @param phase Initial phase of sampling, expressed as a fraction of a cycle (0 to 1). A negative value will cause phase initialization to be skipped. The default value is 0.
+/// @param phase Initial phase of sampling, expressed as a fraction of a cycle (0 to 1). A negative value will cause phase initialization to be skipped. The default value is 0. [Default Value: 0]
 - (void)setOptionalPhase:(AKConstant *)phase;
+
 
 
 @end
