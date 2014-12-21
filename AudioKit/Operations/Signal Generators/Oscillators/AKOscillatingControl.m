72a471c6e817fa20cf8f5b00318076b4e4ff82e6
Aurelius Prochazka <aure@aure.com>
2014-12-21 12:24:54 -0800

    Sensible defaults and tests for AKOscillatingControl

--- AudioKit/Operations/Signal Generators/Oscillators/AKOscillatingControl.m
+++ AudioKit/Operations/Signal Generators/Oscillators/AKOscillatingControl.m
@@ -2,45 +2,73 @@
 //  AKOscillatingControl.m
 //  AudioKit
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 11/2/12.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/21/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 //  Implementation of Csound's oscili:
 //  http://www.csounds.com/manual/html/oscili.html
 //
 
 #import "AKOscillatingControl.h"
+#import "AKManager.h"
 
 @implementation AKOscillatingControl
-{
-    AKFTable *ifn;
-    AKControl *kcps;
-    AKControl *kamp;
-    AKConstant *iphs;
-}
 
 - (instancetype)initWithFTable:(AKFTable *)fTable
                      frequency:(AKControl *)frequency
                      amplitude:(AKControl *)amplitude
+                         phase:(AKConstant *)phase
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        ifn = fTable;
-        kcps = frequency;
-        kamp = amplitude;
-        iphs = akp(0);
+        _fTable = fTable;
+        _frequency = frequency;
+        _amplitude = amplitude;
+        _phase = phase;
     }
     return self;
 }
 
+- (instancetype)init
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        // Default Values
+        _fTable = [AKManager standardSineTable];
+        
+        _frequency = akp(1);    
+        _amplitude = akp(1);    
+        _phase = akp(0);    
+    }
+    return self;
+}
+
++ (instancetype)control
+{
+    return [[AKOscillatingControl alloc] init];
+}
+
+- (void)setOptionalFTable:(AKFTable *)fTable {
+    _fTable = fTable;
+}
+- (void)setOptionalFrequency:(AKControl *)frequency {
+    _frequency = frequency;
+}
+- (void)setOptionalAmplitude:(AKControl *)amplitude {
+    _amplitude = amplitude;
+}
 - (void)setOptionalPhase:(AKConstant *)phase {
-	iphs = phase;
+    _phase = phase;
 }
 
 - (NSString *)stringForCSD {
     return [NSString stringWithFormat:
-            @"%@ poscil %@, %@, %@, %@",
-            self, kamp, kcps, ifn, iphs];
+            @"%@ oscili %@, %@, %@, %@",
+            self,
+            _amplitude,
+            _frequency,
+            _fTable,
+            _phase];
 }
 
 @end
