5e3b1c79ff4908c11d8831deb51a1983fe19ced3
Aurelius Prochazka <aure@aure.com>
2014-12-21 12:51:29 -0800

    Sensible defaults and tests for AKPanner.

--- AudioKit/Operations/Signal Modifiers/Volume and Spatialization/AKPanner.h
+++ AudioKit/Operations/Signal Modifiers/Volume and Spatialization/AKPanner.h
@@ -2,42 +2,49 @@
 //  AKPanner.h
 //  AudioKit
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 11/24/12.
-//  Modified by Aurelius Prochazka to add pan methods.
-//
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/21/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 
 #import "AKStereoAudio.h"
 #import "AKParameter+Operation.h"
 
 /** Distribute an audio signal across two channels with a choice of methods.
- 
- Panning methods include equal power, square root, simple linear, and an
- alternative equal power method based on the MIDI Association Recommend
- Practice for GM2 RP036 (Default Pan Curve).
+
+ Panning methods include equal power, square root, simple linear, and an alternative equal power method based on the MIDI Association Recommend Practice for GM2 RP036 (Default Pan Curve).
  */
 
-typedef enum
-{
-    kPanEqualPower = 0,
-    kPanSquareRoot = 1,
-    kPanLinear = 2,
-    kPanAltEqualPower = 3,
-} PanMethod;
-
 @interface AKPanner : AKStereoAudio
-
-/// Instantiates the panner
-/// @param audioSource Source signal.
-/// @param pan From hard left (0) to hard right (1).
+/// Instantiates the panner with all values
+/// @param audioSource Source signal. [Default Value: ]
+/// @param pan From hard left (-1) to middle (0) to hard right (1). [Default Value: 0]
+/// @param panMethod Pan Method: EqualPower = 0, SquareRoot = 1, Linear = 2, AltEqualPower = 3 [Default Value: 0]
 - (instancetype)initWithAudioSource:(AKAudio *)audioSource
-                                pan:(AKParameter *)pan;
+                                pan:(AKParameter *)pan
+                          panMethod:(AKConstant *)panMethod;
 
+/// Instantiates the panner with default values
+/// @param audioSource Source signal.
+- (instancetype)initWithAudioSource:(AKAudio *)audioSource;
+
+/// Instantiates the panner with default values
+/// @param audioSource Source signal.
++ (instancetype)stereoaudioWithAudioSource:(AKAudio *)audioSource;
+
+/// From hard left (-1) to middle (0) to hard right (1). [Default Value: 0]
+@property AKParameter *pan;
+
+/// Set an optional pan
+/// @param pan From hard left (-1) to middle (0) to hard right (1). [Default Value: 0]
+- (void)setOptionalPan:(AKParameter *)pan;
+
+/// Pan Method: EqualPower = 0, SquareRoot = 1, Linear = 2, AltEqualPower = 3 [Default Value: 0]
+@property AKConstant *panMethod;
 
 /// Set an optional pan method
-/// @param panMethod Pan Method
-- (void)setOptionalPanMethod:(PanMethod)panMethod;
+/// @param panMethod Pan Method: EqualPower = 0, SquareRoot = 1, Linear = 2, AltEqualPower = 3 [Default Value: 0]
+- (void)setOptionalPanMethod:(AKConstant *)panMethod;
+
 
 
 @end
