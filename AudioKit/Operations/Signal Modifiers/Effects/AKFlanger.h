e42c3441ab403998ab177c646aff54998fcb9fd3
Nicholas Arner <nicholasarner@gmail.com>
2014-12-21 17:16:48 -0500

    Sensible defaults for AKFlanger

--- AudioKit/Operations/Signal Modifiers/Effects/AKFlanger.h
+++ AudioKit/Operations/Signal Modifiers/Effects/AKFlanger.h
@@ -2,28 +2,45 @@
 //  AKFlanger.h
 //  AudioKit
 //
-//  Created by Aurelius Prochazka on 10/30/12.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/21/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 
 #import "AKAudio.h"
 #import "AKParameter+Operation.h"
 
 /** Flange effect
- 
- This is useful for generating choruses and flangers. The delay must be varied at
- audio-rate.
+
+ This is useful for generating choruses and flangers. The delay must be varied at audio-rate connecting delay to an oscillator output.
  */
 
 @interface AKFlanger : AKAudio
+/// Instantiates the flanger with all values
+/// @param audioSource Input signal. [Default Value: ]
+/// @param delayTime Delay in seconds [Default Value: ]
+/// @param feedback Feedback amount (in normal tasks this should not exceed 1, even if bigger values are allowed) [Default Value: 0]
+- (instancetype)initWithAudioSource:(AKAudio *)audioSource
+                          delayTime:(AKAudio *)delayTime
+                           feedback:(AKControl *)feedback;
 
-/// Instantiates the flanger
-/// @param sourceSignal Input signal.
+/// Instantiates the flanger with default values
+/// @param audioSource Input signal.
 /// @param delayTime Delay in seconds
-/// @param feedback Feedback amount (in normal tasks this should not exceed 1, even if bigger values are allowed)
+- (instancetype)initWithAudioSource:(AKAudio *)audioSource
+                          delayTime:(AKAudio *)delayTime;
 
-- (instancetype)initWithSourceSignal:(AKAudio *)sourceSignal
-                           delayTime:(AKAudio *)delayTime
-                            feedback:(AKControl *)feedback;
+/// Instantiates the flanger with default values
+/// @param audioSource Input signal.
+/// @param delayTime Delay in seconds
++ (instancetype)audioWithAudioSource:(AKAudio *)audioSource                           delayTime:(AKAudio *)delayTime;
+
+/// Feedback amount (in normal tasks this should not exceed 1, even if bigger values are allowed) [Default Value: 0]
+@property AKControl *feedback;
+
+/// Set an optional feedback
+/// @param feedback Feedback amount (in normal tasks this should not exceed 1, even if bigger values are allowed) [Default Value: 0]
+- (void)setOptionalFeedback:(AKControl *)feedback;
+
+
 
 @end
