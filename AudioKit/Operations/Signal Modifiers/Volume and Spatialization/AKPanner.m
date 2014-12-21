a1fb1886287bfdd18a351760e15798185f9ac466
Aurelius Prochazka <aure@aure.com>
2014-12-21 12:51:29 -0800

    Sensible defaults and tests for AKPanner.

--- AudioKit/Operations/Signal Modifiers/Volume and Spatialization/AKPanner.m
+++ AudioKit/Operations/Signal Modifiers/Volume and Spatialization/AKPanner.m
@@ -2,44 +2,65 @@
 //  AKPanner.m
 //  AudioKit
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 11/24/12.
-//  Modified by Aurelius Prochazka to add pan methods.
-//
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/21/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 //  Implementation of Csound's pan2:
 //  http://www.csounds.com/manual/html/pan2.html
 //
 
 #import "AKPanner.h"
+#import "AKManager.h"
 
 @implementation AKPanner
 {
-    AKAudio *asig;
-    AKParameter *xp;
-    AKConstant *imode;
+    AKAudio *_audioSource;
 }
 
 - (instancetype)initWithAudioSource:(AKAudio *)audioSource
                                 pan:(AKParameter *)pan
+                          panMethod:(AKConstant *)panMethod
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        asig = audioSource;
-        xp = pan;
-        imode = akpi(kPanEqualPower);
+        _audioSource = audioSource;
+        _pan = pan;
+        _panMethod = panMethod;
     }
     return self;
 }
 
-- (void)setOptionalPanMethod:(PanMethod)panMethod {
-	imode = akpi(panMethod);
+- (instancetype)initWithAudioSource:(AKAudio *)audioSource
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        _audioSource = audioSource;
+        // Default Values
+        _pan = akp(0);    
+        _panMethod = akp(0);    
+    }
+    return self;
+}
+
++ (instancetype)stereoaudioWithAudioSource:(AKAudio *)audioSource
+{
+    return [[AKPanner alloc] initWithAudioSource:audioSource];
+}
+
+- (void)setOptionalPan:(AKParameter *)pan {
+    _pan = pan;
+}
+- (void)setOptionalPanMethod:(AKConstant *)panMethod {
+    _panMethod = panMethod;
 }
 
 - (NSString *)stringForCSD {
     return [NSString stringWithFormat:
-            @"%@ pan2 %@, %@, %@",
-            self, asig, xp, imode];
+            @"%@ pan2 %@, 0.5 * (%@+1), %@",
+            self,
+            _audioSource,
+            _pan,
+            _panMethod];
 }
 
 @end
---
+++ Tests/AKPanner/AKPanner/main.swift
@@ -0,0 +1,65 @@
+//
+//  main.swift
+//  AudioKit
+//
+//  Auto-generated from scripts by Aurelius Prochazka on 12/21/14.
+//  Customized by Aurelius Prochazka on 12/21/14.
+//
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
+//
+
+import Foundation
+
+class Instrument : AKInstrument {
+
+    override init() {
+        super.init()
+
+        let note = Note()
+        addNoteProperty(note.pan)
+        addNoteProperty(note.panMethod)
+        
+        let oscillator = AKOscillator()
+        connect(oscillator)
+        
+        let pan = AKOscillatingControl()
+        connect(pan)
+
+        let operation = AKPanner(audioSource: oscillator)
+        operation.pan = pan
+        connect(operation)
+
+        connect(AKAudioOutput(stereoAudioSource:operation))
+    }
+}
+
+class Note: AKNote {
+    var pan = AKNoteProperty()
+    var panMethod = AKNoteProperty()
+
+    override init() {
+        super.init()
+        addProperty(pan)
+        self.pan.setValue(0)
+        addProperty(panMethod)
+        self.panMethod.setValue(0)
+    }
+}
+
+// Set Up
+let instrument = Instrument()
+AKOrchestra.addInstrument(instrument)
+AKManager.sharedManager().isLogging = true
+AKOrchestra.testForDuration(10)
+
+let note1 = Note()
+// specify properties and create more notes here
+
+let phrase = AKPhrase()
+phrase.addNote(note1, atTime:0.5)
+// add more phrase notes here
+
+instrument.playPhrase(phrase)
+
+while(AKManager.sharedManager().isRunning) {} //do nothing
+println("Test complete!")
