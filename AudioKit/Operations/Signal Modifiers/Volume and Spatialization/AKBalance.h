47b798951ef95569a6ec1059ff66ba1be9400c22
Nicholas Arner <nicholasarner@gmail.com>
2014-12-21 15:42:59 -0500

    Sensible defaults for AKBalance

--- AudioKit/Operations/Signal Modifiers/Volume and Spatialization/AKBalance.h
+++ AudioKit/Operations/Signal Modifiers/Volume and Spatialization/AKBalance.h
@@ -2,30 +2,44 @@
 //  AKBalance.h
 //  AudioKit
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 12/24/12.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/21/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 
 #import "AKAudio.h"
 #import "AKParameter+Operation.h"
 
 /** Adjusts one audio signal according to the values of another.
- 
+
  This operation outputs a version of the audio source, amplitude-modified so that its rms power is equal to that of the comparator audio source. Thus a signal that has suffered loss of power (eg., in passing through a filter bank) can be restored by matching it with, for instance, its own source. It should be noted that this modifies amplitude only; output signal is not altered in any other respect.
  */
 
 @interface AKBalance : AKAudio
+/// Instantiates the balance with all values
+/// @param audioSource Input audio signal [Default Value: ]
+/// @param comparatorAudioSource The comparator signal [Default Value: ]
+/// @param halfPowerPoint Half-power point (in Hz) of a special internal low-pass filter. The default value is 10. [Default Value: 10]
+- (instancetype)initWithAudioSource:(AKAudio *)audioSource
+              comparatorAudioSource:(AKAudio *)comparatorAudioSource
+                     halfPowerPoint:(AKConstant *)halfPowerPoint;
 
-/// Instantiates the balance
+/// Instantiates the balance with default values
 /// @param audioSource Input audio signal
 /// @param comparatorAudioSource The comparator signal
 - (instancetype)initWithAudioSource:(AKAudio *)audioSource
               comparatorAudioSource:(AKAudio *)comparatorAudioSource;
 
+/// Instantiates the balance with default values
+/// @param audioSource Input audio signal
+/// @param comparatorAudioSource The comparator signal
++ (instancetype)audioWithAudioSource:(AKAudio *)audioSource               comparatorAudioSource:(AKAudio *)comparatorAudioSource;
+/// Half-power point (in Hz) of a special internal low-pass filter. The default value is 10. [Default Value: 10]
+@property AKConstant *halfPowerPoint;
 
 /// Set an optional half power point
-/// @param halfPowerPoint Half-power point (in Hz) of a special internal low-pass filter. The default value is 10.
+/// @param halfPowerPoint Half-power point (in Hz) of a special internal low-pass filter. The default value is 10. [Default Value: 10]
 - (void)setOptionalHalfPowerPoint:(AKConstant *)halfPowerPoint;
+
 
 
 @end
