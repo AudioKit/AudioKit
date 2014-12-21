88e0236da5ce5f6f5a515358c348fc912c800565
Nicholas Arner <nicholasarner@gmail.com>
2014-12-21 17:16:48 -0500

    Sensible defaults for AKFlanger

--- AudioKit/Operations/Signal Modifiers/Effects/AKFlanger.m
+++ AudioKit/Operations/Signal Modifiers/Effects/AKFlanger.m
@@ -2,39 +2,66 @@
 //  AKFlanger.m
 //  AudioKit
 //
-//  Created by Aurelius Prochazka on 10/30/12.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/21/14.
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
 //
 //  Implementation of Csound's flanger:
 //  http://www.csounds.com/manual/html/flanger.html
 //
 
 #import "AKFlanger.h"
+#import "AKManager.h"
 
 @implementation AKFlanger
 {
-    AKAudio *asig;
-    AKAudio *adel;
-    AKControl *kfeedback;
+    AKAudio *_audioSource;
+    AKAudio *_delayTime;
 }
 
-- (instancetype)initWithSourceSignal:(AKAudio *)sourceSignal
-                           delayTime:(AKAudio *)delayTime
-                            feedback:(AKControl *)feedback
+- (instancetype)initWithAudioSource:(AKAudio *)audioSource
+                          delayTime:(AKAudio *)delayTime
+                           feedback:(AKControl *)feedback
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        asig = sourceSignal;
-        adel = delayTime;
-        kfeedback = feedback;
+        _audioSource = audioSource;
+        _delayTime = delayTime;
+        _feedback = feedback;
     }
     return self;
+}
+
+- (instancetype)initWithAudioSource:(AKAudio *)audioSource
+                          delayTime:(AKAudio *)delayTime
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        _audioSource = audioSource;
+        _delayTime = delayTime;
+        // Default Values
+        _feedback = akp(0);    
+    }
+    return self;
+}
+
++ (instancetype)audioWithAudioSource:(AKAudio *)audioSource
+                          delayTime:(AKAudio *)delayTime
+{
+    return [[AKFlanger alloc] initWithAudioSource:audioSource
+                          delayTime:delayTime];
+}
+
+- (void)setOptionalFeedback:(AKControl *)feedback {
+    _feedback = feedback;
 }
 
 - (NSString *)stringForCSD {
     return [NSString stringWithFormat:
             @"%@ flanger %@, %@, %@",
-            self, asig, adel, kfeedback];
+            self,
+            _audioSource,
+            _delayTime,
+            _feedback];
 }
 
 @end
