e4090760cc1eabfe0c91c084334e787b1c2bbc26
Aurelius Prochazka <aure@aure.com>
2014-12-21 11:47:17 -0800

    Sensible defaults and tests for AKRandom operations.

---
+++ Tests/AKRandomAudio/AKRandomAudio/main.swift
@@ -0,0 +1,37 @@
+//
+//  main.swift
+//  AudioKit
+//
+//  Auto-generated from scripts by Aurelius Prochazka on 12/21/14.
+//  Customized by Aurelius Prochazka on 12/21/14.
+//
+//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
+//
+
+import Foundation
+
+class Instrument : AKInstrument {
+    
+    override init() {
+        super.init()
+        
+        let operation = AKRandomControl()
+        connect(operation)
+        
+        let oscillator = AKOscillator()
+        oscillator.frequency = operation.scaledBy(100.ak).plus(440.ak)
+        connect(oscillator)
+        
+        connect(AKAudioOutput(audioSource:oscillator))
+    }
+}
+
+// Set Up
+let instrument = Instrument()
+AKOrchestra.addInstrument(instrument)
+AKManager.sharedManager().isLogging = true
+AKOrchestra.testForDuration(10)
+instrument.play()
+
+while(AKManager.sharedManager().isRunning) {} //do nothing
+println("Test complete!")
