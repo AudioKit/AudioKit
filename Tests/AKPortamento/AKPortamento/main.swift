52da60737c0f3bf6bc3aea3760b729346dc67c23
Nicholas Arner <nicholasarner@gmail.com>
2014-12-22 16:16:51 -0500

    Test for AKPortamento

---
+++ Tests/AKPortamento/AKPortamento/main.swift
@@ -0,0 +1,46 @@
+//
+//  main.swift
+//  AudioKit
+//
+//  Auto-generated from scripts by Aurelius Prochazka on 12/22/14.
+//  Customized by Nick Arner on 12/22/14.
+//
+//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
+//
+
+import Foundation
+
+class Instrument : AKInstrument {
+
+    override init() {
+        super.init()
+        
+        let frequencyShifter = AKLowFrequencyOscillatingControl()
+        frequencyShifter.type = AKLowFrequencyOscillatorType.BipolarSquare
+        frequencyShifter.amplitude = 100.ak;
+        frequencyShifter.frequency = 0.25.ak
+        connect(frequencyShifter)
+
+        let operation = AKPortamento(controlSource: frequencyShifter)
+        connect(operation)
+        
+        let sine = AKOscillator()
+        sine.frequency  = operation.plus(880.ak)
+        connect(sine)
+
+        connect(AKAudioOutput(audioSource:sine))
+    }
+}
+
+
+
+// Set Up
+let instrument = Instrument()
+AKOrchestra.addInstrument(instrument)
+AKManager.sharedManager().isLogging = true
+AKOrchestra.testForDuration(10)
+
+instrument.play()
+
+while(AKManager.sharedManager().isRunning) {} //do nothing
+println("Test complete!")
