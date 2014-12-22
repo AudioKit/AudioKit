70a97a311075d00c9e186c6a1097753951caa5bf
Nicholas Arner <nicholasarner@gmail.com>
2014-12-22 15:51:49 -0500

    Sensible defaults fork AKLowPassControlFilter

--- AudioKit/Operations/Signal Modifiers/Filters/AKLowPassControlFilter.h
+++ AudioKit/Operations/Signal Modifiers/Filters/AKLowPassControlFilter.h
@@ -2,22 +2,40 @@
 //  AKLowPassControlFilter.h
 //  AudioKit
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 11/6/12.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/22/14.
+//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
 //
 
 #import "AKControl.h"
 #import "AKParameter+Operation.h"
 
 /** A first-order recursive low-pass filter with variable frequency response.
+
+ More detailed description from http://www.csounds.com/manual/html/tonek.html
  */
 
 @interface AKLowPassControlFilter : AKControl
-
-/// Instantiates the low pass control filter
-/// @param sourceControl  The control signal to be filtered
-/// @param halfPowerPoint The response curve's half-power point, in Hertz. Half power is defined as peak power / root 2.
+/// Instantiates the low pass control filter with all values
+/// @param sourceControl The control to be filtered [Default Value: ]
+/// @param halfPowerPoint The response curve's half-power point, in Hertz. Half power is defined as peak power / root 2. [Default Value: 100]
 - (instancetype)initWithSourceControl:(AKControl *)sourceControl
                        halfPowerPoint:(AKControl *)halfPowerPoint;
+
+/// Instantiates the low pass control filter with default values
+/// @param sourceControl The control to be filtered
+- (instancetype)initWithSourceControl:(AKControl *)sourceControl;
+
+/// Instantiates the low pass control filter with default values
+/// @param sourceControl The control to be filtered
++ (instancetype)controlWithSourceControl:(AKControl *)sourceControl;
+
+/// The response curve's half-power point, in Hertz. Half power is defined as peak power / root 2. [Default Value: 100]
+@property AKControl *halfPowerPoint;
+
+/// Set an optional half power point
+/// @param halfPowerPoint The response curve's half-power point, in Hertz. Half power is defined as peak power / root 2. [Default Value: 100]
+- (void)setOptionalHalfPowerPoint:(AKControl *)halfPowerPoint;
+
+
 
 @end
--- AudioKit/Operations/Signal Modifiers/Filters/AKLowPassControlFilter.m
+++ AudioKit/Operations/Signal Modifiers/Filters/AKLowPassControlFilter.m
@@ -2,19 +2,19 @@
 //  AKLowPassControlFilter.m
 //  AudioKit
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 11/6/12.
-//  Copyright (c) 2012 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/22/14.
+//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
 //
-//  Implementation of Csound's tone:
-//  http://www.csounds.com/manual/html/tone.html
+//  Implementation of Csound's tonek:
+//  http://www.csounds.com/manual/html/tonek.html
 //
 
 #import "AKLowPassControlFilter.h"
+#import "AKManager.h"
 
 @implementation AKLowPassControlFilter
 {
-    AKControl *ksig;
-    AKControl *khp;
+    AKControl * _sourceControl;
 }
 
 - (instancetype)initWithSourceControl:(AKControl *)sourceControl
@@ -22,16 +22,38 @@
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        ksig = sourceControl;
-        khp = halfPowerPoint;
+        _sourceControl = sourceControl;
+        _halfPowerPoint = halfPowerPoint;
     }
     return self;
+}
+
+- (instancetype)initWithSourceControl:(AKControl *)sourceControl
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        _sourceControl = sourceControl;
+        // Default Values
+        _halfPowerPoint = akp(100);    
+    }
+    return self;
+}
+
++ (instancetype)controlWithSourceControl:(AKControl *)sourceControl
+{
+    return [[AKLowPassControlFilter alloc] initWithSourceControl:sourceControl];
+}
+
+- (void)setOptionalHalfPowerPoint:(AKControl *)halfPowerPoint {
+    _halfPowerPoint = halfPowerPoint;
 }
 
 - (NSString *)stringForCSD {
     return [NSString stringWithFormat:
             @"%@ tonek %@, %@",
-            self, ksig, khp];
+            self,
+            _sourceControl,
+            _halfPowerPoint];
 }
 
 @end
