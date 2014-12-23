788dc04efefde59c067c332c041688faea431b8b
Aurelius Prochazka <aure@aure.com>
2014-12-22 21:48:59 -0800

    Type for AKStruckMetalBarBoundaryCondition

--- AudioKit/Operations/Signal Generators/Physical Models/AKStruckMetalBar.h
+++ AudioKit/Operations/Signal Generators/Physical Models/AKStruckMetalBar.h
@@ -2,8 +2,8 @@
 //  AKStruckMetalBar.h
 //  AudioKit
 //
-//  Auto-generated from scripts by Aurelius Prochazka on 12/15/14.
-//  Copyright (c) 2014 Hear For Yourself. All rights reserved.
+//  Auto-generated from scripts by Aurelius Prochazka on 12/22/14.
+//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
 //
 
 #import "AKAudio.h"
@@ -22,8 +22,8 @@
 /// @param strikePosition Position along the bar that the strike occurs. [Default Value: 0.2]
 /// @param strikeVelocity Normalized strike velocity. [Default Value: 800]
 /// @param strikeWidth Spatial width of strike. [Default Value: 0.2]
-/// @param leftBoundaryCondition Boundary condition at left end of bar (1 is clamped; 2 pivoting and 3 free). [Default Value: 1]
-/// @param rightBoundaryCondition Boundary condition at right end of bar (1 is clamped; 2 pivoting and 3 free). [Default Value: 1]
+/// @param leftBoundaryCondition Boundary condition at left end of bar. [Default Value: AKStruckMetalBarBoundaryConditionClamped]
+/// @param rightBoundaryCondition Boundary condition at right end of bar. [Default Value: AKStruckMetalBarBoundaryConditionClamped]
 /// @param scanSpeed Speed of scanning the output location. [Default Value: 0.23]
 - (instancetype)initWithDecayTime:(AKConstant *)decayTime
            dimensionlessStiffness:(AKConstant *)dimensionlessStiffness
@@ -31,8 +31,8 @@
                    strikePosition:(AKConstant *)strikePosition
                    strikeVelocity:(AKConstant *)strikeVelocity
                       strikeWidth:(AKConstant *)strikeWidth
-            leftBoundaryCondition:(AKControl *)leftBoundaryCondition
-           rightBoundaryCondition:(AKControl *)rightBoundaryCondition
+            leftBoundaryCondition:(AKStruckMetalBarBoundaryCondition)leftBoundaryCondition
+           rightBoundaryCondition:(AKStruckMetalBarBoundaryCondition)rightBoundaryCondition
                         scanSpeed:(AKControl *)scanSpeed;
 
 /// Instantiates the struck metal bar with default values
@@ -41,54 +41,63 @@
 /// Instantiates the struck metal bar with default values
 + (instancetype)audio;
 
+
 /// 30 db decay time in seconds. [Default Value: 2.0]
 @property AKConstant *decayTime;
 
 /// Set an optional decay time
 /// @param decayTime 30 db decay time in seconds. [Default Value: 2.0]
 - (void)setOptionalDecayTime:(AKConstant *)decayTime;
+
 /// Dimensionless stiffness parameter. If this parameter is negative then the initialization is skipped and the previous state of the bar is continued. [Default Value: 100]
 @property AKConstant *dimensionlessStiffness;
 
 /// Set an optional dimensionless stiffness
 /// @param dimensionlessStiffness Dimensionless stiffness parameter. If this parameter is negative then the initialization is skipped and the previous state of the bar is continued. [Default Value: 100]
 - (void)setOptionalDimensionlessStiffness:(AKConstant *)dimensionlessStiffness;
+
 /// High-frequency loss parameter (keep this small). [Default Value: 0.001]
 @property AKConstant *highFrequencyLoss;
 
 /// Set an optional high frequency loss
 /// @param highFrequencyLoss High-frequency loss parameter (keep this small). [Default Value: 0.001]
 - (void)setOptionalHighFrequencyLoss:(AKConstant *)highFrequencyLoss;
+
 /// Position along the bar that the strike occurs. [Default Value: 0.2]
 @property AKConstant *strikePosition;
 
 /// Set an optional strike position
 /// @param strikePosition Position along the bar that the strike occurs. [Default Value: 0.2]
 - (void)setOptionalStrikePosition:(AKConstant *)strikePosition;
+
 /// Normalized strike velocity. [Default Value: 800]
 @property AKConstant *strikeVelocity;
 
 /// Set an optional strike velocity
 /// @param strikeVelocity Normalized strike velocity. [Default Value: 800]
 - (void)setOptionalStrikeVelocity:(AKConstant *)strikeVelocity;
+
 /// Spatial width of strike. [Default Value: 0.2]
 @property AKConstant *strikeWidth;
 
 /// Set an optional strike width
 /// @param strikeWidth Spatial width of strike. [Default Value: 0.2]
 - (void)setOptionalStrikeWidth:(AKConstant *)strikeWidth;
-/// Boundary condition at left end of bar (1 is clamped; 2 pivoting and 3 free). [Default Value: 1]
-@property AKControl *leftBoundaryCondition;
+
+/// Boundary condition at left end of bar. [Default Value: AKStruckMetalBarBoundaryConditionClamped]
+@property AKStruckMetalBarBoundaryCondition leftBoundaryCondition;
 
 /// Set an optional left boundary condition
-/// @param leftBoundaryCondition Boundary condition at left end of bar (1 is clamped; 2 pivoting and 3 free). [Default Value: 1]
-- (void)setOptionalLeftBoundaryCondition:(AKControl *)leftBoundaryCondition;
-/// Boundary condition at right end of bar (1 is clamped; 2 pivoting and 3 free). [Default Value: 1]
-@property AKControl *rightBoundaryCondition;
+/// @param leftBoundaryCondition Boundary condition at left end of bar. [Default Value: AKStruckMetalBarBoundaryConditionClamped]
+- (void)setOptionalLeftBoundaryCondition:(AKStruckMetalBarBoundaryCondition)leftBoundaryCondition;
+
+/// Boundary condition at right end of bar. [Default Value: AKStruckMetalBarBoundaryConditionClamped]
+@property AKStruckMetalBarBoundaryCondition rightBoundaryCondition;
 
 /// Set an optional right boundary condition
-/// @param rightBoundaryCondition Boundary condition at right end of bar (1 is clamped; 2 pivoting and 3 free). [Default Value: 1]
-- (void)setOptionalRightBoundaryCondition:(AKControl *)rightBoundaryCondition;
+/// @param rightBoundaryCondition Boundary condition at right end of bar. [Default Value: AKStruckMetalBarBoundaryConditionClamped]
+- (void)setOptionalRightBoundaryCondition:(AKStruckMetalBarBoundaryCondition)rightBoundaryCondition;
+
 /// Speed of scanning the output location. [Default Value: 0.23]
 @property AKControl *scanSpeed;
 
