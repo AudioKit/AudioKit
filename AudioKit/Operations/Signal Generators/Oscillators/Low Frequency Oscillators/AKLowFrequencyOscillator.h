9f185b700e5d9a29e2e1b46868d1a78518eafada
Aurelius Prochazka <aure@aure.com>
2014-12-23 03:36:55 -0800

    Include control-rate comment.

--- AudioKit/Operations/Signal Generators/Oscillators/Low Frequency Oscillators/AKLowFrequencyOscillator.h
+++ AudioKit/Operations/Signal Generators/Oscillators/Low Frequency Oscillators/AKLowFrequencyOscillator.h
@@ -17,8 +17,8 @@
 @interface AKLowFrequencyOscillator : AKAudio
 /// Instantiates the low frequency oscillator with all values
 /// @param type Waveform of the oscillator, can be sine, triangle, square (bipolar), square (unipolar), saw-tooth, saw-tooth (down). [Default Value: AKLowFrequencyOscillatorTypeSine]
-/// @param frequency Frequency of the note. [Default Value: 110]
-/// @param amplitude Amplitude of output. [Default Value: 1]
+/// @param frequency Frequency of the note. Updated at Control-rate. [Default Value: 110]
+/// @param amplitude Amplitude of output. Updated at Control-rate. [Default Value: 1]
 - (instancetype)initWithType:(AKLowFrequencyOscillatorType)type
                    frequency:(AKParameter *)frequency
                    amplitude:(AKParameter *)amplitude;
@@ -41,14 +41,14 @@
 @property AKParameter *frequency;
 
 /// Set an optional frequency
-/// @param frequency Frequency of the note. [Default Value: 110]
+/// @param frequency Frequency of the note. Updated at Control-rate. [Default Value: 110]
 - (void)setOptionalFrequency:(AKParameter *)frequency;
 
 /// Amplitude of output. [Default Value: 1]
 @property AKParameter *amplitude;
 
 /// Set an optional amplitude
-/// @param amplitude Amplitude of output. [Default Value: 1]
+/// @param amplitude Amplitude of output. Updated at Control-rate. [Default Value: 1]
 - (void)setOptionalAmplitude:(AKParameter *)amplitude;
 
 
