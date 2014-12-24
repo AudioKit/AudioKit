9430c847a4a56de70bee5e4ddd095316006a481a
Aurelius Prochazka <aure@aure.com>
2014-12-24 00:29:07 -0800

    Rate-agnostic AKMarimba

--- AudioKit/Operations/Signal Generators/Physical Models/Marimba/AKMarimba.h
+++ AudioKit/Operations/Signal Generators/Physical Models/Marimba/AKMarimba.h
@@ -2,7 +2,7 @@
 //  AKMarimba.h
 //  AudioKit
 //
-//  Auto-generated on 11/30/14.
+//  Auto-generated on 12/23/14.
 //  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
 //
 
@@ -15,42 +15,46 @@
  */
 
 @interface AKMarimba : AKAudio
-
 /// Instantiates the marimba with all values
-/// @param frequency Frequency of note played.
-/// @param stickHardness The hardness of the stick used in the strike. A range of 0 to 1 is used.
-/// @param strikePosition Where the block is hit, in the range 0 to 1.
-/// @param vibratoShapeTable Shape of vibrato, usually a sine table, created by a function
-/// @param vibratoFrequency Frequency of vibrato in Hertz. Suggested range is 0 to 12
-/// @param vibratoAmplitude Amplitude of the vibrato
-/// @param doubleStrikePercentage Percentage of double strikes. Default is 40%.
-/// @param tripleStrikePercentage Percentage of triple strikes. Default is 20%.
-- (instancetype)initWithFrequency:(AKControl *)frequency
+/// @param frequency Frequency of note played. Updated at Control-rate. [Default Value: 220]
+/// @param amplitude Amplitude of note. Updated at Control-rate. [Default Value: 1]
+/// @param stickHardness The hardness of the stick used in the strike. A range of 0 to 1 is used. [Default Value: 0.5]
+/// @param strikePosition Where the block is hit, in the range 0 to 1. [Default Value: 0.5]
+/// @param vibratoShapeTable Shape of vibrato, usually a sine table, created by a function [Default Value: sine]
+/// @param vibratoFrequency Frequency of vibrato in Hertz. Suggested range is 0 to 12 Updated at Control-rate. [Default Value: 0]
+/// @param vibratoAmplitude Amplitude of the vibrato Updated at Control-rate. [Default Value: 0]
+/// @param doubleStrikePercentage Percentage of double strikes. Default is 40%. [Default Value: 40]
+/// @param tripleStrikePercentage Percentage of triple strikes. Default is 20%. [Default Value: 20]
+- (instancetype)initWithFrequency:(AKParameter *)frequency
+                        amplitude:(AKParameter *)amplitude
                     stickHardness:(AKConstant *)stickHardness
                    strikePosition:(AKConstant *)strikePosition
                 vibratoShapeTable:(AKFTable *)vibratoShapeTable
-                 vibratoFrequency:(AKControl *)vibratoFrequency
-                 vibratoAmplitude:(AKControl *)vibratoAmplitude
+                 vibratoFrequency:(AKParameter *)vibratoFrequency
+                 vibratoAmplitude:(AKParameter *)vibratoAmplitude
            doubleStrikePercentage:(AKConstant *)doubleStrikePercentage
            tripleStrikePercentage:(AKConstant *)tripleStrikePercentage;
 
 /// Instantiates the marimba with default values
 - (instancetype)init;
 
-
 /// Instantiates the marimba with default values
 + (instancetype)audio;
 
 
-
-
 /// Frequency of note played. [Default Value: 220]
-@property AKControl *frequency;
+@property AKParameter *frequency;
 
 /// Set an optional frequency
-/// @param frequency Frequency of note played. [Default Value: 220]
-- (void)setOptionalFrequency:(AKControl *)frequency;
+/// @param frequency Frequency of note played. Updated at Control-rate. [Default Value: 220]
+- (void)setOptionalFrequency:(AKParameter *)frequency;
 
+/// Amplitude of note. [Default Value: 1]
+@property AKParameter *amplitude;
+
+/// Set an optional amplitude
+/// @param amplitude Amplitude of note. Updated at Control-rate. [Default Value: 1]
+- (void)setOptionalAmplitude:(AKParameter *)amplitude;
 
 /// The hardness of the stick used in the strike. A range of 0 to 1 is used. [Default Value: 0.5]
 @property AKConstant *stickHardness;
@@ -59,14 +63,12 @@
 /// @param stickHardness The hardness of the stick used in the strike. A range of 0 to 1 is used. [Default Value: 0.5]
 - (void)setOptionalStickHardness:(AKConstant *)stickHardness;
 
-
 /// Where the block is hit, in the range 0 to 1. [Default Value: 0.5]
 @property AKConstant *strikePosition;
 
 /// Set an optional strike position
 /// @param strikePosition Where the block is hit, in the range 0 to 1. [Default Value: 0.5]
 - (void)setOptionalStrikePosition:(AKConstant *)strikePosition;
-
 
 /// Shape of vibrato, usually a sine table, created by a function [Default Value: sine]
 @property AKFTable *vibratoShapeTable;
@@ -75,22 +77,19 @@
 /// @param vibratoShapeTable Shape of vibrato, usually a sine table, created by a function [Default Value: sine]
 - (void)setOptionalVibratoShapeTable:(AKFTable *)vibratoShapeTable;
 
-
 /// Frequency of vibrato in Hertz. Suggested range is 0 to 12 [Default Value: 0]
-@property AKControl *vibratoFrequency;
+@property AKParameter *vibratoFrequency;
 
 /// Set an optional vibrato frequency
-/// @param vibratoFrequency Frequency of vibrato in Hertz. Suggested range is 0 to 12 [Default Value: 0]
-- (void)setOptionalVibratoFrequency:(AKControl *)vibratoFrequency;
-
+/// @param vibratoFrequency Frequency of vibrato in Hertz. Suggested range is 0 to 12 Updated at Control-rate. [Default Value: 0]
+- (void)setOptionalVibratoFrequency:(AKParameter *)vibratoFrequency;
 
 /// Amplitude of the vibrato [Default Value: 0]
-@property AKControl *vibratoAmplitude;
+@property AKParameter *vibratoAmplitude;
 
 /// Set an optional vibrato amplitude
-/// @param vibratoAmplitude Amplitude of the vibrato [Default Value: 0]
-- (void)setOptionalVibratoAmplitude:(AKControl *)vibratoAmplitude;
-
+/// @param vibratoAmplitude Amplitude of the vibrato Updated at Control-rate. [Default Value: 0]
+- (void)setOptionalVibratoAmplitude:(AKParameter *)vibratoAmplitude;
 
 /// Percentage of double strikes. Default is 40%. [Default Value: 40]
 @property AKConstant *doubleStrikePercentage;
@@ -99,13 +98,13 @@
 /// @param doubleStrikePercentage Percentage of double strikes. Default is 40%. [Default Value: 40]
 - (void)setOptionalDoubleStrikePercentage:(AKConstant *)doubleStrikePercentage;
 
-
 /// Percentage of triple strikes. Default is 20%. [Default Value: 20]
 @property AKConstant *tripleStrikePercentage;
 
 /// Set an optional triple strike percentage
 /// @param tripleStrikePercentage Percentage of triple strikes. Default is 20%. [Default Value: 20]
 - (void)setOptionalTripleStrikePercentage:(AKConstant *)tripleStrikePercentage;
+
 
 
 @end
