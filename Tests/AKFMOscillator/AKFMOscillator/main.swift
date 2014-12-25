be331f5fa8cc63c7f38347ced99849c76bdf4f6a
Aurelius Prochazka <aure@aure.com>
2014-12-25 00:04:00 -0800

    More diverse range of source from AKFMOscillator test

--- Tests/AKFMOscillator/AKFMOscillator/main.swift
+++ Tests/AKFMOscillator/AKFMOscillator/main.swift
@@ -12,8 +12,24 @@ class Instrument : AKInstrument {
 
     override init() {
         super.init()
+        
+        let frequencyLine = AKLine(firstPoint: 10.ak, secondPoint: 880.ak, durationBetweenPoints: 11.ak)
+        connect(frequencyLine)
 
+        let carrierMultiplierLine = AKLine(firstPoint: 2.ak, secondPoint: 0.ak, durationBetweenPoints: 11.ak)
+        connect(carrierMultiplierLine)
+
+        let modulatingMultiplierLine = AKLine(firstPoint: 0.ak, secondPoint: 2.ak, durationBetweenPoints: 11.ak)
+        connect(modulatingMultiplierLine)
+        
+        let indexLine = AKLine(firstPoint: 0.ak, secondPoint: 30.ak, durationBetweenPoints: 11.ak)
+        connect(indexLine)
+        
         let operation = AKFMOscillator()
+        operation.baseFrequency = frequencyLine
+        operation.carrierMultiplier = carrierMultiplierLine
+        operation.modulatingMultiplier = modulatingMultiplierLine
+        operation.modulationIndex = indexLine
         connect(operation)
         connect(AKAudioOutput(audioSource:operation))
     }
