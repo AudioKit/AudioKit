09e1291d5f392e5d261d42df622910205ed6faf0
Aurelius Prochazka <aure@aure.com>
2014-12-24 22:08:55 -0800

    Sensible Defaults for AKVCOscillator

--- AudioKit/Operations/Signal Generators/Oscillators/AKVCOscillator.h
+++ AudioKit/Operations/Signal Generators/Oscillators/AKVCOscillator.h
@@ -2,7 +2,7 @@
 //  AKVCOscillator.h
 //  AudioKit
 //
-//  Rewritten by Aurelius Prochazka on 7/4/14.
+//  Auto-generated on 12/24/14.
 //  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
 //
 
@@ -10,33 +10,74 @@
 #import "AKParameter+Operation.h"
 
 /** Implementation of a band-limited oscillator using pre-calculated tables.  Meant to model vintage analog synthesizers.
- 
+
  Different modes require different inputs so this could be a reason to break this up into separate classes, or use more custom initializers.
  */
 
 @interface AKVCOscillator : AKAudio
+/// Instantiates the vc oscillator with all values
+/// @param waveformType Choose from various waveform types. [Default Value: AKVCOscillatorWaveformTypeSawtooth]
+/// @param bandwidth Bandwidth of the generated waveform, as percentage (0 to 1) of the sample rate. The expected range is 0 to 0.5 [Default Value: 0.5]
+/// @param frequency Frequency in Hz Updated at Control-rate. [Default Value: 440]
+/// @param amplitude Amplitude scale. In the case of a imode waveform value of a pulse waveform, the actual output level can be a lot higher than this value. Updated at Control-rate. [Default Value: 1]
+/// @param pulseWidth The pulse width of the square wave or the ramp characteristics of the triangle wave. It is required only by these waveforms and ignored in all other cases. The expected range is 0 to 1, any other value is wrapped to the allowed range. Updated at Control-rate. [Default Value: 0]
+/// @param phase Oscillator phase (depending on imode, this can be either an optional i-rate parameter that defaults to zero or required k-rate). Similarly to pulseWidth, the expected range is 0 to 1. Updated at Control-rate. [Default Value: 0]
+- (instancetype)initWithWaveformType:(AKVCOscillatorWaveformType)waveformType
+                           bandwidth:(AKConstant *)bandwidth
+                           frequency:(AKParameter *)frequency
+                           amplitude:(AKParameter *)amplitude
+                          pulseWidth:(AKParameter *)pulseWidth
+                               phase:(AKParameter *)phase;
 
-/// Instantiates the VC Oscillator
-/// @param frequency Frequency in Hz
-/// @param amplitude Amplitude scale. In the case of a imode waveform value of a pulse waveform, the actual output level can be a lot higher than this value.
-- (instancetype)initWithFrequency:(AKParameter *)frequency
-                        amplitude:(AKParameter *)amplitude;
+/// Instantiates the vc oscillator with default values
+- (instancetype)init;
+
+/// Instantiates the vc oscillator with default values
++ (instancetype)audio;
+
+
+/// Choose from various waveform types. [Default Value: AKVCOscillatorWaveformTypeSawtooth]
+@property AKVCOscillatorWaveformType waveformType;
 
 /// Set an optional waveform type
-/// @param waveformType Choose from various waveform types.
+/// @param waveformType Choose from various waveform types. [Default Value: AKVCOscillatorWaveformTypeSawtooth]
 - (void)setOptionalWaveformType:(AKVCOscillatorWaveformType)waveformType;
 
-/// Set an optional pulse width
-/// @param pulseWidth The pulse width of the square wave or the ramp characteristics of the triangle wave. It is required only by these waveforms and ignored in all other cases. The expected range is 0 to 1, any other value is wrapped to the allowed range.
-- (void)setOptionalPulseWidth:(AKControl *)pulseWidth;
-
-/// Set an optional phase
-/// @param phase Oscillator phase (depending on imode, this can be either an optional i-rate parameter that defaults to zero or required k-rate). Similarly to pulseWidth, the expected range is 0 to 1.
-- (void)setOptionalPhase:(AKControl *)phase;
+/// Bandwidth of the generated waveform, as percentage (0 to 1) of the sample rate. The expected range is 0 to 0.5 [Default Value: 0.5]
+@property AKConstant *bandwidth;
 
 /// Set an optional bandwidth
-/// @param bandwidth Bandwidth of the generated waveform, as percentage (0 to 1) of the sample rate. The expected range is 0 to 0.5
+/// @param bandwidth Bandwidth of the generated waveform, as percentage (0 to 1) of the sample rate. The expected range is 0 to 0.5 [Default Value: 0.5]
 - (void)setOptionalBandwidth:(AKConstant *)bandwidth;
+
+/// Frequency in Hz [Default Value: 440]
+@property AKParameter *frequency;
+
+/// Set an optional frequency
+/// @param frequency Frequency in Hz Updated at Control-rate. [Default Value: 440]
+- (void)setOptionalFrequency:(AKParameter *)frequency;
+
+/// Amplitude scale. In the case of a imode waveform value of a pulse waveform, the actual output level can be a lot higher than this value. [Default Value: 1]
+@property AKParameter *amplitude;
+
+/// Set an optional amplitude
+/// @param amplitude Amplitude scale. In the case of a imode waveform value of a pulse waveform, the actual output level can be a lot higher than this value. Updated at Control-rate. [Default Value: 1]
+- (void)setOptionalAmplitude:(AKParameter *)amplitude;
+
+/// The pulse width of the square wave or the ramp characteristics of the triangle wave. It is required only by these waveforms and ignored in all other cases. The expected range is 0 to 1, any other value is wrapped to the allowed range. [Default Value: 0]
+@property AKParameter *pulseWidth;
+
+/// Set an optional pulse width
+/// @param pulseWidth The pulse width of the square wave or the ramp characteristics of the triangle wave. It is required only by these waveforms and ignored in all other cases. The expected range is 0 to 1, any other value is wrapped to the allowed range. Updated at Control-rate. [Default Value: 0]
+- (void)setOptionalPulseWidth:(AKParameter *)pulseWidth;
+
+/// Oscillator phase (depending on imode, this can be either an optional i-rate parameter that defaults to zero or required k-rate). Similarly to pulseWidth, the expected range is 0 to 1. [Default Value: 0]
+@property AKParameter *phase;
+
+/// Set an optional phase
+/// @param phase Oscillator phase (depending on imode, this can be either an optional i-rate parameter that defaults to zero or required k-rate). Similarly to pulseWidth, the expected range is 0 to 1. Updated at Control-rate. [Default Value: 0]
+- (void)setOptionalPhase:(AKParameter *)phase;
+
 
 
 @end
--- AudioKit/Operations/Signal Generators/Oscillators/AKVCOscillator.m
+++ AudioKit/Operations/Signal Generators/Oscillators/AKVCOscillator.m
@@ -2,7 +2,7 @@
 //  AKVCOscillator.m
 //  AudioKit
 //
-//  Rewritten by Aurelius Prochazka on 7/4/14.
+//  Auto-generated on 12/24/14.
 //  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
 //
 //  Implementation of Csound's vco2:
@@ -10,52 +10,78 @@
 //
 
 #import "AKVCOscillator.h"
+#import "AKManager.h"
 
 @implementation AKVCOscillator
-{
-    AKParameter *kcps;
-    AKParameter *kamp;
-    AKConstant *imode;
-    AKControl *kpw;
-    AKControl *kphs;
-    AKConstant *inyx;
-}
 
-- (instancetype)initWithFrequency:(AKParameter *)frequency
-                        amplitude:(AKParameter *)amplitude
+- (instancetype)initWithWaveformType:(AKVCOscillatorWaveformType)waveformType
+                           bandwidth:(AKConstant *)bandwidth
+                           frequency:(AKParameter *)frequency
+                           amplitude:(AKParameter *)amplitude
+                          pulseWidth:(AKParameter *)pulseWidth
+                               phase:(AKParameter *)phase
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        kcps = frequency;
-        kamp = amplitude;
-        imode = akp(0);
-        kpw = akp(0);
-        kphs = akp(0);
-        inyx = akp(0.5);
+        _waveformType = waveformType;
+        _bandwidth = bandwidth;
+        _frequency = frequency;
+        _amplitude = amplitude;
+        _pulseWidth = pulseWidth;
+        _phase = phase;
     }
     return self;
 }
 
+- (instancetype)init
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        // Default Values
+        _waveformType = AKVCOscillatorWaveformTypeSawtooth;    
+        _bandwidth = akp(0.5);    
+        _frequency = akp(440);    
+        _amplitude = akp(1);    
+        _pulseWidth = akp(0);    
+        _phase = akp(0);    
+    }
+    return self;
+}
+
++ (instancetype)audio
+{
+    return [[AKVCOscillator alloc] init];
+}
+
 - (void)setOptionalWaveformType:(AKVCOscillatorWaveformType)waveformType {
-	imode = akp(waveformType);
+    _waveformType = waveformType;
 }
-
-- (void)setOptionalPulseWidth:(AKControl *)pulseWidth {
-	kpw = pulseWidth;
-}
-
-- (void)setOptionalPhase:(AKControl *)phase {
-	kphs = phase;
-}
-
 - (void)setOptionalBandwidth:(AKConstant *)bandwidth {
-	inyx = bandwidth;
+    _bandwidth = bandwidth;
+}
+- (void)setOptionalFrequency:(AKParameter *)frequency {
+    _frequency = frequency;
+}
+- (void)setOptionalAmplitude:(AKParameter *)amplitude {
+    _amplitude = amplitude;
+}
+- (void)setOptionalPulseWidth:(AKParameter *)pulseWidth {
+    _pulseWidth = pulseWidth;
+}
+- (void)setOptionalPhase:(AKParameter *)phase {
+    _phase = phase;
 }
 
 - (NSString *)stringForCSD {
     return [NSString stringWithFormat:
-            @"%@ vco2 AKControl(%@), AKControl(%@), %@, %@, %@, %@",
-            self, kamp, kcps, imode, kpw, kphs, inyx];
+            @"%@ vco2 AKControl(%@), AKControl(%@), %@, AKControl(%@), AKControl(%@), %@",
+            self,
+            _amplitude,
+            _frequency,
+            akpi(_waveformType),
+            _pulseWidth,
+            _phase,
+            _bandwidth];
 }
 
 @end
