c35c607fc1c4b447a720c75fb6e53c883510246c
Aurelius Prochazka <aure@aure.com>
2014-12-24 21:14:22 -0800

    Linear Envelope tests

---
+++ Tests/AKLinearADSRAudioEnvelope/AKLinearADSRAudioEnvelope/main.swift
@@ -0,0 +1,49 @@
+//
+//  main.swift
+//  AudioKit
+//
+//  Auto-generated on 12/24/14.
+//  Customized by Aurelius Prochazka on 12/24/14 (Yes, Christmas Eve!)
+//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
+//
+
+import Foundation
+
+class Instrument : AKInstrument {
+
+    override init() {
+        super.init()
+
+        let operation = AKLinearADSRAudioEnvelope()
+        connect(operation)
+        
+        let oscillator = AKOscillator()
+        oscillator.amplitude = operation
+        connect(oscillator)
+
+        connect(AKAudioOutput(audioSource:oscillator))
+    }
+}
+
+// Set Up
+let instrument = Instrument()
+AKOrchestra.addInstrument(instrument)
+AKManager.sharedManager().isLogging = true
+AKOrchestra.testForDuration(15)
+
+let note1 = AKNote()
+note1.duration.setValue(1.5)
+// specify properties and create more notes here
+
+let note2 = AKNote()
+note2.duration.setValue(5)
+
+let phrase = AKPhrase()
+phrase.addNote(note1, atTime:0.5)
+phrase.addNote(note2, atTime:3.5)
+// add more phrase notes here
+
+instrument.playPhrase(phrase)
+
+while(AKManager.sharedManager().isRunning) {} //do nothing
+println("Test complete!")
