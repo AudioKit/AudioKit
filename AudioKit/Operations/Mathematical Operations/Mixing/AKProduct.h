83aa61e21080a19c8d9c0c30ea7a0817d576be46
Aurelius Prochazka <aure@aure.com>
2014-12-24 23:20:21 -0800

    A typo and indentation issue

--- AudioKit/Operations/Mathematical Operations/Mixing/AKProduct.h
+++ AudioKit/Operations/Mathematical Operations/Mixing/AKProduct.h
@@ -9,22 +9,22 @@
 #import "AKParameter+Operation.h"
 #import "AKAudio.h"
 
-/** Multiplies any number of audio signals. 
+/** Multiplies any number of audio signals.
  */
 
-@interface AKProduct : AKAudio    
+@interface AKProduct : AKParameter
 
 @property NSArray *inputs;
 
 /// Initialization Statement
 /// @param firstOperand At least one input is required
 /// @param ...          End the list with a nil.
-- (instancetype)initWithOperands:(AKAudio *)firstOperand,...;
+- (instancetype)initWithOperands:(AKParameter *)firstOperand,...;
 
 /// Create a new signal as a product of exactly two given signals.
 /// @param firstOperand First signal to be summed.
 /// @param secondOperand Second Signal to be summed.
 - (instancetype)initWithFirstOperand:(AKParameter *)firstOperand
-                    secondOperand:(AKParameter *)secondOperand;
+                       secondOperand:(AKParameter *)secondOperand;
 
 @end
