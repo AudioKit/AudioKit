6729fff841a7afc3683d7383ef645f6278f5b007
Aurelius Prochazka <aure@aure.com>
2014-12-24 22:51:49 -0800

    Sensible defaults for AKReverb

--- AudioKit/Operations/Signal Modifiers/Reverbs/AKReverb.h
+++ AudioKit/Operations/Signal Modifiers/Reverbs/AKReverb.h
@@ -2,35 +2,73 @@
 //  AKReverb.h
 //  AudioKit
 //
-//  Created by Aurelius Prochazka on 6/9/12.
-//  Copyright (c) 2012 Aurelius Prochazka. All rights reserved.
+//  Auto-generated on 12/24/14.
+//  Customized by Aurelius Prochazka on 12/24/14.
+//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
 //
 
 #import "AKStereoAudio.h"
 #import "AKParameter+Operation.h"
-#import "AKControl.h"
 
-/** 8 delay line stereo FDN reverb, with feedback matrix based upon physical
- modeling scattering junction of 8 lossless waveguides of equal characteristic impedance.
+/** 8 delay line stereo FDN reverb
+
+ 8 delay line stereo FDN reverb, with feedback matrix based upon physical modeling scattering junction of 8 lossless waveguides of equal characteristic impedance.
  */
 
 @interface AKReverb : AKStereoAudio
+/// Instantiates the reverb with all values
+/// @param audioSourceLeftChannel Input for the left channel [Default Value: ]
+/// @param audioSourceRightChannel Input for the right channel [Default Value: ]
+/// @param feedback Feedback level in the range 0 to 1. 0.6 gives a good small 'live' room sound, 0.8 a small hall, and 0.9 a large hall. A setting of exactly 1 means infinite length, while higher values will make the opcode unstable. Updated at Control-rate. [Default Value: 0.6]
+/// @param cutoffFrequency comment Updated at Control-rate. [Default Value: 4000]
+- (instancetype)initWithAudioSourceLeftChannel:(AKParameter *)audioSourceLeftChannel
+                       audioSourceRightChannel:(AKParameter *)audioSourceRightChannel
+                                      feedback:(AKParameter *)feedback
+                               cutoffFrequency:(AKParameter *)cutoffFrequency;
 
-/// Apply reverb to a stereo signal
-/// @param sourceStereo    Input to the left and right channel.
-/// @param feedbackLevel   Degree of feedback, in the range 0 to 1. 0.6 gives a good small "live" room sound, 0.8 a small hall, and 0.9 a large hall. A setting of exactly 1 means infinite length, while higher values will make the opcode unstable.
-/// @param cutoffFrequency Cutoff frequency of simple first order lowpass filters in the feedback loop of delay lines, in Hz.  A lower value means faster decay in the high frequency range.
-- (instancetype)initWithStereoAudioSource:(AKStereoAudio *)sourceStereo
-                            feedbackLevel:(AKControl *)feedbackLevel
-                          cutoffFrequency:(AKControl *)cutoffFrequency;
+/// Instantiates the reverb with default values
+/// @param audioSourceLeftChannel Input for the left channel
+/// @param audioSourceRightChannel Input for the right channel
+- (instancetype)initWithAudioSourceLeftChannel:(AKParameter *)audioSourceLeftChannel
+                       audioSourceRightChannel:(AKParameter *)audioSourceRightChannel;
 
-/// Apply reverb to a mono signal
-/// @param audioSource       Input to both channels.
-/// @param feedbackLevel   Degree of feedback, in the range 0 to 1. 0.6 gives a good small "live" room sound, 0.8 a small hall, and 0.9 a large hall. A setting of exactly 1 means infinite length, while higher values will make the opcode unstable.
-/// @param cutoffFrequency Cutoff frequency of simple first order lowpass filters in the feedback loop of delay lines, in Hz.  A lower value means faster decay in the high frequency range.
-- (instancetype)initWithAudioSource:(AKAudio *)audioSource
-                      feedbackLevel:(AKControl *)feedbackLevel
-                    cutoffFrequency:(AKControl *)cutoffFrequency;
+/// Instantiates the reverb with default values
+/// @param audioSourceLeftChannel Input for the left channel
+/// @param audioSourceRightChannel Input for the right channel
++ (instancetype)stereoAudioWithAudioSourceLeftChannel:(AKParameter *)audioSourceLeftChannel
+                              audioSourceRightChannel:(AKParameter *)audioSourceRightChannel;
+
+/// Instantiates the reverb with all values
+/// @param audioSourceLeftChannel Input for the left channel [Default Value: ]
+/// @param audioSourceRightChannel Input for the right channel [Default Value: ]
+/// @param feedback Feedback level in the range 0 to 1. 0.6 gives a good small 'live' room sound, 0.8 a small hall, and 0.9 a large hall. A setting of exactly 1 means infinite length, while higher values will make the opcode unstable. Updated at Control-rate. [Default Value: 0.6]
+/// @param cutoffFrequency comment Updated at Control-rate. [Default Value: 4000]
+- (instancetype)initWithStereoAudioSource:(AKStereoAudio *)audioSource
+                  audioSourceRightChannel:(AKParameter *)audioSourceRightChannel
+                                 feedback:(AKParameter *)feedback
+                          cutoffFrequency:(AKParameter *)cutoffFrequency;
+
+/// Instantiates the reverb with default values
+/// @param audioSource Input to the reverberator.
+- (instancetype)initWithStereoAudioSource:(AKStereoAudio *)audioSource;
+
+/// Instantiates the reverb with default values
+/// @param audioSource Input to the reverberator.
++ (instancetype)stereoAudioWithStereoAudioSource:(AKStereoAudio *)audioSource;
+
+/// Feedback level in the range 0 to 1. 0.6 gives a good small 'live' room sound, 0.8 a small hall, and 0.9 a large hall. A setting of exactly 1 means infinite length, while higher values will make the opcode unstable. [Default Value: 0.6]
+@property AKParameter *feedback;
+
+/// Set an optional feedback
+/// @param feedback Feedback level in the range 0 to 1. 0.6 gives a good small 'live' room sound, 0.8 a small hall, and 0.9 a large hall. A setting of exactly 1 means infinite length, while higher values will make the opcode unstable. Updated at Control-rate. [Default Value: 0.6]
+- (void)setOptionalFeedback:(AKParameter *)feedback;
+
+/// comment [Default Value: 4000]
+@property AKParameter *cutoffFrequency;
+
+/// Set an optional cutoff frequency
+/// @param cutoffFrequency comment Updated at Control-rate. [Default Value: 4000]
+- (void)setOptionalCutoffFrequency:(AKParameter *)cutoffFrequency;
 
 
 
--- AudioKit/Operations/Signal Modifiers/Reverbs/AKReverb.m
+++ AudioKit/Operations/Signal Modifiers/Reverbs/AKReverb.m
@@ -2,50 +2,110 @@
 //  AKReverb.m
 //  AudioKit
 //
-//  Created by Aurelius Prochazka on 6/9/12.
-//  Copyright (c) 2012 Aurelius Prochazka. All rights reserved.
+//  Auto-generated on 12/24/14.
+//  Customized by Aurelius Prochazka on 12/24/14.
+//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
 //
-//  This is an incomplete port of the Csound's reverbsc:
+//  Implementation of Csound's reverbsc:
 //  http://www.csounds.com/manual/html/reverbsc.html
 //
 
 #import "AKReverb.h"
+#import "AKManager.h"
 
 @implementation AKReverb
 {
-    AKStereoAudio *aInLR;
-    AKControl *kFbLvl;
-    AKControl *kFCo;
+    AKParameter * _audioSourceLeftChannel;
+    AKParameter * _audioSourceRightChannel;
 }
 
-- (instancetype)initWithStereoAudioSource:(AKStereoAudio *)sourceStereo
-                            feedbackLevel:(AKControl *)feedbackLevel
-                          cutoffFrequency:(AKControl *)cutoffFrequency;
-
+- (instancetype)initWithAudioSourceLeftChannel:(AKParameter *)audioSourceLeftChannel
+                       audioSourceRightChannel:(AKParameter *)audioSourceRightChannel
+                                      feedback:(AKParameter *)feedback
+                               cutoffFrequency:(AKParameter *)cutoffFrequency
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        aInLR  = sourceStereo;
-        kFbLvl = feedbackLevel;
-        kFCo   = cutoffFrequency;
+        _audioSourceLeftChannel = audioSourceLeftChannel;
+        _audioSourceRightChannel = audioSourceRightChannel;
+        _feedback = feedback;
+        _cutoffFrequency = cutoffFrequency;
     }
     return self;
 }
 
-- (instancetype)initWithAudioSource:(AKAudio *)audioSource
-                      feedbackLevel:(AKControl *)feedbackLevel
-                    cutoffFrequency:(AKControl *)cutoffFrequency;
+- (instancetype)initWithAudioSourceLeftChannel:(AKParameter *)audioSourceLeftChannel
+                       audioSourceRightChannel:(AKParameter *)audioSourceRightChannel
 {
-    return [self initWithStereoAudioSource:[AKStereoAudio stereoFromMono:audioSource]
-                             feedbackLevel:feedbackLevel
-                           cutoffFrequency:cutoffFrequency];
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        _audioSourceLeftChannel = audioSourceLeftChannel;
+        _audioSourceRightChannel = audioSourceRightChannel;
+        // Default Values
+        _feedback = akp(0.6);    
+        _cutoffFrequency = akp(4000);    
+    }
+    return self;
 }
 
-- (NSString *)stringForCSD
++ (instancetype)stereoAudioWithAudioSourceLeftChannel:(AKParameter *)audioSourceLeftChannel
+                              audioSourceRightChannel:(AKParameter *)audioSourceRightChannel
 {
+    return [[AKReverb alloc] initWithAudioSourceLeftChannel:audioSourceLeftChannel
+                             audioSourceRightChannel:audioSourceRightChannel];
+}
+
+
+- (instancetype)initWithStereoAudioSource:(AKStereoAudio *)audioSource
+                  audioSourceRightChannel:(AKParameter *)audioSourceRightChannel
+                                 feedback:(AKParameter *)feedback
+                          cutoffFrequency:(AKParameter *)cutoffFrequency
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        _audioSourceLeftChannel = audioSource.leftOutput;
+        _audioSourceRightChannel = audioSource.rightOutput;
+        _feedback = feedback;
+        _cutoffFrequency = cutoffFrequency;
+    }
+    return self;
+}
+
+- (instancetype)initWithStereoAudioSource:(AKStereoAudio *)audioSource
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        _audioSourceLeftChannel = audioSource.leftOutput;
+        _audioSourceRightChannel = audioSource.rightOutput;
+        // Default Values
+        _feedback = akp(0.6);
+        _cutoffFrequency = akp(4000);
+    }
+    return self;
+}
+
++ (instancetype)stereoAudioWithStereoAudioSource:(AKStereoAudio *)audioSource
+{
+    return [[AKReverb alloc] initWithAudioSourceLeftChannel:audioSource.leftOutput
+                                    audioSourceRightChannel:audioSource.rightOutput];
+}
+
+
+- (void)setOptionalFeedback:(AKParameter *)feedback {
+    _feedback = feedback;
+}
+- (void)setOptionalCutoffFrequency:(AKParameter *)cutoffFrequency {
+    _cutoffFrequency = cutoffFrequency;
+}
+
+- (NSString *)stringForCSD {
     return [NSString stringWithFormat:
-            @"%@ reverbsc %@, %@, %@",
-            self , aInLR, kFbLvl, kFCo];
+            @"%@ reverbsc AKAudio(%@), AKAudio(%@), AKControl(%@), AKControl(%@)",
+            self,
+            _audioSourceLeftChannel,
+            _audioSourceRightChannel,
+            _feedback,
+            _cutoffFrequency];
 }
 
 @end
