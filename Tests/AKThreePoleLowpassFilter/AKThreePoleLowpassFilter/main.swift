7a95eafea95bc2ce6a4fd3a39aaed2d7241ac643
Nicholas Arner <nicholasarner@gmail.com>
2014-12-26 18:12:56 -0500

    Fixed parameters in AKThreePoleLowpassFilter test

--- Tests/AKThreePoleLowpassFilter/AKThreePoleLowpassFilter/main.swift
+++ Tests/AKThreePoleLowpassFilter/AKThreePoleLowpassFilter/main.swift
@@ -30,19 +30,19 @@ class Processor : AKInstrument {
     init(audioSource: AKAudio) {
         super.init()
         
-        let line1 = AKLinearControl(firstPoint: 0.1.ak, secondPoint: 0.9.ak, durationBetweenPoints: 11.ak)
-        connect(line1)
+        let distortion = AKLinearControl(firstPoint: 0.1.ak, secondPoint: 0.9.ak, durationBetweenPoints: 11.ak)
+        connect(distortion)
         
-        let line2 = AKLinearControl(firstPoint: 300.ak, secondPoint: 3000.ak, durationBetweenPoints: 11.ak)
-        connect(line2)
+        let cutoffFrequency = AKLinearControl(firstPoint: 300.ak, secondPoint: 3000.ak, durationBetweenPoints: 11.ak)
+        connect(cutoffFrequency)
 
-        let line3 = AKLinearControl(firstPoint: 0.ak, secondPoint: 1.ak, durationBetweenPoints: 11.ak)
-        connect(line3)
+        let resonance = AKLinearControl(firstPoint: 0.ak, secondPoint: 1.ak, durationBetweenPoints: 11.ak)
+        connect(resonance)
 
         let operation = AKThreePoleLowpassFilter(input: audioSource)
-        operation.distortion = line1
-        operation.cutoffFrequency = line2
-        operation.resonance = line3
+        operation.distortion = distortion
+        operation.cutoffFrequency = cutoffFrequency
+        operation.resonance = resonance
         connect(operation)
         
         connect(AKAudioOutput(audioSource:operation))
