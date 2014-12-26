0de7cbe9a1ca474ccbf28c3ed2c93b9b1680761c
Aurelius Prochazka <aure@aure.com>
2014-12-25 21:58:45 -0800

    New operation for loading MP3 files

---
+++ AudioKit/Operations/Signal Input and Output/AKMP3FileInput.m
@@ -0,0 +1,44 @@
+//
+//  AKMP3FileInput.m
+//  AudioKit
+//
+//  Auto-generated on 12/25/14.
+//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
+//
+//  Implementation of Csound's mp3in:
+//  http://www.csounds.com/manual/html/mp3in.html
+//
+
+#import "AKMP3FileInput.h"
+#import "AKManager.h"
+
+@implementation AKMP3FileInput
+{
+    AKConstant * _filename;
+}
+
+- (instancetype)initWithFilename:(AKConstant *)filename
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        _filename = filename;
+    }
+    return self;
+}
+
++ (instancetype)stereoaudioWithFilename:(AKConstant *)filename
+{
+    return [[AKMP3FileInput alloc] initWithFilename:filename];
+}
+
+
+- (NSString *)stringForCSD {
+    NSMutableString *csdString = [[NSMutableString alloc] init];
+
+    [csdString appendFormat:@"%@ mp3in ", self];
+
+    [csdString appendFormat:@"\"%@\"", _filename];
+    return csdString;
+}
+
+@end
