ba5ab8342b6042eaa66592bcdd57656fc7e87e65
Aurelius Prochazka <aure@aure.com>
2014-12-25 21:58:45 -0800

    New operation for loading MP3 files

---
+++ AudioKit/Operations/Signal Input and Output/AKMP3FileInput.h
@@ -0,0 +1,29 @@
+//
+//  AKMP3FileInput.h
+//  AudioKit
+//
+//  Auto-generated on 12/25/14.
+//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
+//
+
+#import "AKStereoAudio.h"
+#import "AKParameter+Operation.h"
+
+/** Reads stereo audio data from an external MP3 file.
+
+ Implementation of http://www.csounds.com/manual/html/mp3in.html
+ */
+
+@interface AKMP3FileInput : AKStereoAudio
+
+/// Instantiates the mp3 file input with default values
+/// @param filename Input MP3 Filename.
+- (instancetype)initWithFilename:(AKConstant *)filename;
+
+/// Instantiates the mp3 file input with default values
+/// @param filename Input MP3 Filename.
++ (instancetype)stereoaudioWithFilename:(AKConstant *)filename;
+
+
+
+@end
