be7a59000547fde415d0e6736a2597e34429fe4c
Aurelius Prochazka <aure@aure.com>
2014-12-26 22:33:02 -0800

    Removing table size from sound file

--- AudioKit/Operations/Function Tables/Generators/AKSoundFile.h
+++ AudioKit/Operations/Function Tables/Generators/AKSoundFile.h
@@ -29,12 +29,6 @@
 /// @param filename Audio file to load.  Most types are supported.
 - (instancetype)initWithFilename:(NSString *)filename;
 
-/// Store file with a filename using a specifically sized function table
-/// @param filename  Audio file to load.  Most types are supported.
-/// @param tableSize Size of the table to use.  Necessary for some opcodes.
-- (instancetype)initWithFilename:(NSString *)filename
-                       tableSize:(int)tableSize;
-
 /// Returns the string to retrieve the number of channels of a sound file table
 - (AKConstant *)channels;
 
