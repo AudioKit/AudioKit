c451d7af74621b8a708b716bdc053bc1eae91a79
Aurelius Prochazka <aure@aure.com>
2014-12-27 11:39:03 -0800

    Moving detailed comments into implementation

--- AudioKit/Operations/Function Tables/Generators/AKSoundFile.m
+++ AudioKit/Operations/Function Tables/Generators/AKSoundFile.m
@@ -9,6 +9,20 @@
 #import "AKSoundFile.h"
 #import "AKArray.h"
 
+/** 
+ 
+ `tableSize` is the number of points in the table. Ordinarily a power of 2 or a power-of-2 plus 1
+ The maximum tableSize is 16777216 (224) points. The allocation of table memory can be deferred
+ by setting this parameter to 0; the size allocated is then the number of points in the file
+ (probably not a power-of-2), and the table is not usable by normal oscillators, but it is usable
+ by an AKLoopoingOscillator. The soundfile can also be mono or stereo.
+ 
+ *Important:* Reading stops at end-of-file or when the table is full.
+ Table locations not filled will contain zeros.
+ 
+ */
+
+
 @implementation AKSoundFile
 
 - (instancetype)initWithFilename:(NSString *)filename
