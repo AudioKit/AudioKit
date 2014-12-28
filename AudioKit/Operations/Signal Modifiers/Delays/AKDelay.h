7798b61fc9d5189c313853f2f944a188f8aadf42
Aurelius Prochazka <aure@aure.com>
2014-12-27 22:59:12 -0800

    Sensible defaults for AKDelay

--- AudioKit/Operations/Signal Modifiers/Delays/AKDelay.h
+++ AudioKit/Operations/Signal Modifiers/Delays/AKDelay.h
@@ -2,27 +2,47 @@
 //  AKDelay.h
 //  AudioKit
 //
-//  Auto-generated on 12/26/12.
-//  Customized by Aurelius Prochazka on 12/28/13
-//
-//  Copyright (c) 2012 Aurelius Prochazka. All rights reserved.
+//  Auto-generated on 12/27/14.
+//  Customized by Aurelius Prochazka on 12/27/14.
+//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
 //
 
 #import "AKAudio.h"
 #import "AKParameter+Operation.h"
 
-/** Simple delay
- 
+/** Simple audio delay
+
  Delays an input signal by some time interval.
  */
 
 @interface AKDelay : AKAudio
+/// Instantiates the delay with all values
+/// @param input Input signal, usually audio. [Default Value: ]
+/// @param delayTime Requested delay time in seconds. [Default Value: ]
+/// @param feedback How much of the signal is sent back into the delay line.  Usually values range from 0-1. Updated at Control-rate. [Default Value: 0.0]
+- (instancetype)initWithInput:(AKParameter *)input
+                    delayTime:(AKConstant *)delayTime
+                     feedback:(AKParameter *)feedback;
 
-/// Instantiates the delay
-/// @param audioSource Audio signal
+/// Instantiates the delay with default values
+/// @param input Input signal, usually audio.
 /// @param delayTime Requested delay time in seconds.
 - (instancetype)initWithInput:(AKParameter *)input
                     delayTime:(AKConstant *)delayTime;
 
-- (void)setOptionalFeedback:(AKControl *)feedback;
+/// Instantiates the delay with default values
+/// @param input Input signal, usually audio.
+/// @param delayTime Requested delay time in seconds.
++ (instancetype)audioWithInput:(AKParameter *)input
+                     delayTime:(AKConstant *)delayTime;
+
+/// How much of the signal is sent back into the delay line.  Usually values range from 0-1. [Default Value: 0.0]
+@property AKParameter *feedback;
+
+/// Set an optional feedback
+/// @param feedback How much of the signal is sent back into the delay line.  Usually values range from 0-1. Updated at Control-rate. [Default Value: 0.0]
+- (void)setOptionalFeedback:(AKParameter *)feedback;
+
+
+
 @end
--- AudioKit/Operations/Signal Modifiers/Delays/AKDelay.m
+++ AudioKit/Operations/Signal Modifiers/Delays/AKDelay.m
@@ -2,22 +2,34 @@
 //  AKDelay.m
 //  AudioKit
 //
-//  Auto-generated on 12/26/12.
-//  Customized by Aurelius Prochazka on 12/28/13
-//
-//  Copyright (c) 2012 Aurelius Prochazka. All rights reserved.
+//  Auto-generated on 12/27/14.
+//  Customized by Aurelius Prochazka on 12/27/14.
+//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
 //
 //  Implementation of Csound's delay:
 //  http://www.csounds.com/manual/html/delay.html
 //
 
 #import "AKDelay.h"
+#import "AKManager.h"
 
 @implementation AKDelay
 {
-    AKParameter *asig;
-    AKControl *kFeedback;
-    AKConstant *idlt;
+    AKParameter * _input;
+    AKConstant * _delayTime;
+}
+
+- (instancetype)initWithInput:(AKParameter *)input
+                    delayTime:(AKConstant *)delayTime
+                     feedback:(AKParameter *)feedback
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        _input = input;
+        _delayTime = delayTime;
+        _feedback = feedback;
+    }
+    return self;
 }
 
 - (instancetype)initWithInput:(AKParameter *)input
@@ -25,23 +37,40 @@
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        asig = input;
-        idlt = delayTime;
-        kFeedback = akp(0);
+        _input = input;
+        _delayTime = delayTime;
+        // Default Values
+        _feedback = akp(0.0);
     }
     return self;
 }
-- (void)setOptionalFeedback:(AKControl *)feedback {
-    kFeedback = feedback;
+
++ (instancetype)audioWithInput:(AKParameter *)input
+                    delayTime:(AKConstant *)delayTime
+{
+    return [[AKDelay alloc] initWithInput:input
+                    delayTime:delayTime];
+}
+
+- (void)setOptionalFeedback:(AKParameter *)feedback {
+    _feedback = feedback;
 }
 
 - (NSString *)stringForCSD {
-    NSString *initialization = [NSString stringWithFormat:@"%@ init 0", self];
+    NSMutableString *csdString = [[NSMutableString alloc] init];
+
+    [csdString appendFormat:@"%@ init 0\n", self];
     
-    NSString *opcodeLine = [NSString stringWithFormat:
-                            @"%@ delay %@ + (%@*%@), %@",
-                            self, asig, self, kFeedback, idlt];
-    return [NSString stringWithFormat:@"%@\n%@", initialization, opcodeLine];
+    [csdString appendFormat:@"%@ delay ", self];
+
+    if ([_feedback class] == [AKAudio class]) {
+        [csdString appendFormat:@"%@ + (%@*%@), ", _input, self, _feedback];
+    } else {
+        [csdString appendFormat:@"AKAudio(%@ + (%@*%@)), ", _input, self, _feedback];
+    }
+
+    [csdString appendFormat:@"%@", _delayTime];
+    return csdString;
 }
 
 @end
