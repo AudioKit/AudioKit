04941a11d81f752e30a6c5819e242d340217014d
Aurelius Prochazka <aure@aure.com>
2014-12-27 22:16:32 -0800

    Updating granular synthesis examples

--- Examples/iOS/SwiftGranularSynthesis/SwiftGranularSynthesis/GranularInstrument.swift
+++ Examples/iOS/SwiftGranularSynthesis/SwiftGranularSynthesis/GranularInstrument.swift
@@ -12,10 +12,10 @@ class GranularSynth: AKInstrument
 {
     // INSTRUMENT CONTROLS =====================================================
     
-    var averageGrainDuration        = AKInstrumentProperty(value: 0.05, minimum: 0.001, maximum: 0.05)
-    var grainDensity                = AKInstrumentProperty(value: 25,   minimum: 0,     maximum: 2000)
-    var granularFrequencyDeviation  = AKInstrumentProperty(value: 1500, minimum: 1,     maximum: 3000)
-    var granularAmplitude           = AKInstrumentProperty(value: 1,    minimum: 0,     maximum: 2)
+    var averageGrainDuration        = AKInstrumentProperty(value: 0.25, minimum: 0.1,  maximum: 0.4)
+    var grainDensity                = AKInstrumentProperty(value: 300,  minimum: 10,   maximum: 600)
+    var granularFrequencyDeviation  = AKInstrumentProperty(value: 0.05, minimum: 0,    maximum: 0.1)
+    var granularAmplitude           = AKInstrumentProperty(value: 0.1,  minimum: 0.01, maximum: 0.2)
     
     // INSTRUMENT DEFINITION ===================================================
     
@@ -40,13 +40,14 @@ class GranularSynth: AKInstrument
         
         let grainTexture =  AKGranularSynthesisTexture (grainFunctionTable: fileTable,
                                                         windowFunctionTable: hamming,
-                                                        maximumGrainDuration: 0.05.ak,
+                                                        maximumGrainDuration: 0.5.ak,
                                                         averageGrainDuration: averageGrainDuration,
                                                         maximumFrequencyDeviation: granularFrequencyDeviation,
                                                         grainFrequency: baseFrequency,
-                                                        maximumAmplitudeDeviation: 0.05.ak,
+                                                        maximumAmplitudeDeviation: 0.1.ak,
                                                         grainAmplitude: granularAmplitude,
-                                                        grainDensity: grainDensity)
+                                                        grainDensity: grainDensity,
+                                                        useRandomGrainOffset: true)
         
         connect(grainTexture)
         connect(AKAudioOutput(audioSource: grainTexture))
