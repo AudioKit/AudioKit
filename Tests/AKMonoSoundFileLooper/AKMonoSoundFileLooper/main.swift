6b6467ccad73d0dfd24b4e5a8db983c40dd67df2
Aurelius Prochazka <aure@aure.com>
2014-12-28 02:46:39 -0800

    Tests for SoundFile Loopers

--- Tests/AKMonoSoundFileLooper/AKMonoSoundFileLooper/main.swift
+++ Tests/AKMonoSoundFileLooper/AKMonoSoundFileLooper/main.swift
@@ -3,47 +3,35 @@
 //  AudioKit
 //
 //  Auto-generated on 12/28/14.
+//  Customized by Aurelius Prochazka on 12/28/14.
+//
 //  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
 //
 
 import Foundation
 
 class Instrument : AKInstrument {
-
+    
     override init() {
         super.init()
-
-        let note = Note()
-        addNoteProperty(note.frequencyRatio)
-        addNoteProperty(note.amplitude)
-        addNoteProperty(note.playLoopMode)
-
-        let operation = AKMonoSoundFileLooper(        soundFile: requiredValue
-)
-        operation.frequencyRatio = note.frequencyRatio
-        operation.amplitude = note.amplitude
-        operation.playLoopMode = note.playLoopMode
+        
+        let filename = "CsoundLib64.framework/Sounds/mandpluk.aif"
+        
+        let soundFile = AKSoundFile(filename: filename)
+        connect(soundFile)
+        
+        let speed = AKLine(firstPoint: 10.ak, secondPoint: 0.2.ak, durationBetweenPoints: 10.ak)
+        connect(speed)
+        
+        let operation = AKMonoSoundFileLooper(soundFile: soundFile)
+        operation.frequencyRatio = speed
+        operation.loopMode = AKSoundFileLooperMode.Normal
         connect(operation)
-
+        
         connect(AKAudioOutput(audioSource:operation))
     }
 }
 
-class Note: AKNote {
-    var frequencyRatio = AKNoteProperty()
-    var amplitude = AKNoteProperty()
-    var playLoopMode = AKNoteProperty()
-
-    override init() {
-        super.init()
-        addProperty(frequencyRatio)
-        self.frequencyRatio.setValue(1)
-        addProperty(amplitude)
-        self.amplitude.setValue(1)
-        addProperty(playLoopMode)
-        self.playLoopMode.setValue(AKSoundFileLooperModeNormalLooping)
-    }
-}
 
 // Set Up
 let instrument = Instrument()
@@ -51,14 +39,7 @@ AKOrchestra.addInstrument(instrument)
 AKManager.sharedManager().isLogging = true
 AKOrchestra.testForDuration(10)
 
-let note1 = Note()
-// specify properties and create more notes here
-
-let phrase = AKPhrase()
-phrase.addNote(note1, atTime:0.5)
-// add more phrase notes here
-
-instrument.playPhrase(phrase)
+instrument.play()
 
 while(AKManager.sharedManager().isRunning) {} //do nothing
 println("Test complete!")
