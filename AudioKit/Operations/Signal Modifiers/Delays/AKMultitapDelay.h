e732546c06ce8f4d92fa03d41caa6cee3516c525
Aurelius Prochazka <aure@aure.com>
2014-12-27 18:18:19 -0800

    Added the AKMultitapDelay operation

---
+++ AudioKit/Operations/Signal Modifiers/Delays/AKMultitapDelay.h
@@ -0,0 +1,41 @@
+//
+//  AKMultitapDelay.h
+//  AudioKit
+//
+//  Auto-generated on 12/27/14.
+//  Customized by Aurelius Prochazka on 12/27/14.
+//
+//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
+//
+
+#import "AKAudio.h"
+#import "AKParameter+Operation.h"
+
+/** Multitap delay line implementation.
+
+ 
+ */
+
+@interface AKMultitapDelay : AKAudio
+/// Instantiates the multitap delay
+/// @param input Input signal to be delayed. [Default Value: ]
+/// @param firstEchoTime Time in seconds to delay the firsted delayed playback. [Default Value: ]
+/// @param firstEchoGain The relative amplitude of the first echo. [Default Value: ]
+- (instancetype)initWithInput:(AKParameter *)input
+                firstEchoTime:(AKConstant *)firstEchoTime
+                firstEchoGain:(AKConstant *)firstEchoGain;
+
+/// Instantiates the multitap delay
+/// @param input Input signal to be delayed.
+/// @param firstEchoTime Time in seconds to delay the firsted delayed playback.
+/// @param firstEchoGain The relative amplitude of the first echo.
++ (instancetype)audioWithInput:(AKParameter *)input
+                 firstEchoTime:(AKConstant *)firstEchoTime
+                 firstEchoGain:(AKConstant *)firstEchoGain;
+
+/// Adds an echo or tap to the multi-tap delay line
+/// @param time Time in seconds to delay the firsted delayed playback.
+/// @param gain The relative amplitude of the first echo.
+- (void)addEchoAtTime:(AKConstant *)time gain:(AKConstant *)gain;
+
+@end
---
+++ AudioKit/Operations/Signal Modifiers/Delays/AKMultitapDelay.m
@@ -0,0 +1,79 @@
+//
+//  AKMultitapDelay.m
+//  AudioKit
+//
+//  Auto-generated on 12/27/14.
+//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
+//  Customized by Aurelius Prochazka on 12/27/14.
+//
+//  Implementation of Csound's multitap:
+//  http://www.csounds.com/manual/html/multitap.html
+//
+
+#import "AKMultitapDelay.h"
+#import "AKManager.h"
+
+@implementation AKMultitapDelay
+{
+    AKParameter * _input;
+    AKConstant * _firstEchoTime;
+    AKConstant * _firstEchoGain;
+    
+    NSMutableArray *timesAndGains;
+}
+
+- (instancetype)initWithInput:(AKParameter *)input
+                firstEchoTime:(AKConstant *)firstEchoTime
+                firstEchoGain:(AKConstant *)firstEchoGain
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        _input = input;
+        _firstEchoTime = firstEchoTime;
+        _firstEchoGain = firstEchoGain;
+        
+        timesAndGains = [[NSMutableArray alloc] init];
+        [self addEchoAtTime:firstEchoTime gain:firstEchoGain];
+        
+    }
+    return self;
+}
+
++ (instancetype)audioWithInput:(AKParameter *)input
+                firstEchoTime:(AKConstant *)firstEchoTime
+                firstEchoGain:(AKConstant *)firstEchoGain
+{
+    return [[AKMultitapDelay alloc] initWithInput:input
+                firstEchoTime:firstEchoTime
+                firstEchoGain:firstEchoGain];
+}
+
+- (void)addEchoAtTime:(AKConstant *)time gain:(AKConstant *)gain
+{
+    [timesAndGains addObject:@[time, gain]];
+}
+
+
+
+- (NSString *)stringForCSD {
+    NSMutableString *csdString = [[NSMutableString alloc] init];
+
+    [csdString appendFormat:@"%@ multitap ", self];
+
+    if ([_input class] == [AKAudio class]) {
+        [csdString appendFormat:@"%@, ", _input];
+    } else {
+        [csdString appendFormat:@"AKAudio(%@), ", _input];
+    }
+    
+    NSMutableArray *flattenedTimesAndGains = [[NSMutableArray alloc] init];
+    for (NSArray *timeAndGain in timesAndGains) {
+        [flattenedTimesAndGains addObject:[timeAndGain componentsJoinedByString:@", "]];
+    }
+
+    [csdString appendFormat:@"%@", [flattenedTimesAndGains componentsJoinedByString:@", "]];
+    
+    return csdString;
+}
+
+@end
