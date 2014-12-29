d818af7d28edd3454c037d808719d6953e8bc3b4
Aurelius Prochazka <aure@aure.com>
2014-12-29 15:03:24 -0800

    AKCabasa test with more variation

--- Tests/AKCabasa/AKCabasa/main.swift
+++ Tests/AKCabasa/AKCabasa/main.swift
@@ -8,21 +8,66 @@
 
 import Foundation
 
+let testDuration: Float = 11.0
+
 class Instrument : AKInstrument {
 
     override init() {
         super.init()
+        let note = CabasaNote()
+        addNoteProperty(note.count)
+        addNoteProperty(note.dampingFactor)
+        
+        let cabasa = AKCabasa()
+        cabasa.count = note.count
+        cabasa.dampingFactor = note.dampingFactor
+        connect(cabasa)
+        
+        enableParameterLog(
+            "Count = ",
+            parameter: cabasa.count,
+            frequency:1
+        )
+        enableParameterLog(
+            "Damping Factor = ",
+            parameter: cabasa.dampingFactor,
+            frequency:1
+        )
+        
+        connect(AKAudioOutput(audioSource:cabasa))
+    }
+}
 
-        let operation = AKCabasa()
-        connect(operation)
-        connect(AKAudioOutput(audioSource:operation))
+class CabasaNote: AKNote {
+    var count = AKNoteProperty()
+    var dampingFactor = AKNoteProperty()
+    
+    override init() {
+        super.init()
+        addProperty(count)
+        addProperty(dampingFactor)
+    }
+    
+    convenience init(count: Int, dampingFactor: Float) {
+        self.init()
+        self.count.setValue(Float(count))
+        self.dampingFactor.setValue(dampingFactor)
     }
 }
 
 let instrument = Instrument()
 AKOrchestra.addInstrument(instrument)
-AKOrchestra.testForDuration(2)
-instrument.play()
+AKOrchestra.testForDuration(testDuration)
+
+let phrase = AKPhrase()
+
+for i in 1...10 {
+    let note = CabasaNote(count: i*20, dampingFactor: 1.1-Float(i)/10.0)
+    note.duration.setValue(1.0)
+    phrase.addNote(note, atTime: Float(i-1))
+}
+
+instrument.playPhrase(phrase)
 
 while(AKManager.sharedManager().isRunning) {} //do nothing
 println("Test complete!")
