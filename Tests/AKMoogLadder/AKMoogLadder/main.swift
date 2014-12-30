9580be3f3ddd08522aba6f3a75b27e1170d52422
Nicholas Arner <nicholasarner@gmail.com>
2014-12-30 16:51:39 -0500

    Updated AKHighPassButterworthFilter test to add logging

--- Tests/AKMoogLadder/AKMoogLadder/main.swift
+++ Tests/AKMoogLadder/AKMoogLadder/main.swift
@@ -44,12 +44,14 @@ class Processor : AKInstrument {
         enableParameterLog(
             "Resonance = ",
             parameter: moogLadder.resonance,
-            frequency:0.1)
+            frequency:0.1
+        )
         
         enableParameterLog(
             "Cutoff Frequency = ",
             parameter: moogLadder.cutoffFrequency,
-            frequency:0.1)
+            frequency:0.1
+        )
         
         connect(AKAudioOutput(audioSource:moogLadder))
     }
