99d6910424f52cfb2272ef9727a7ee737338c687
Nicholas Arner <nicholasarner@gmail.com>
2014-12-31 10:23:18 -0500

    Updated AKFMOscillator test to add logging

--- Tests/AKFMOscillator/AKFMOscillator/main.swift
+++ Tests/AKFMOscillator/AKFMOscillator/main.swift
@@ -2,42 +2,69 @@
 //  main.swift
 //  AudioKit
 //
-//  Created by Aurelius Prochazka on 12/1/14.
+//  Created by Nick Arner and Aurelius Prochazka on 12/1/14.
 //  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
 //
 
 import Foundation
+
+let testDuration: Float = 10.0
 
 class Instrument : AKInstrument {
 
     override init() {
         super.init()
 
-        let frequencyLine = AKLine(firstPoint: 10.ak, secondPoint: 880.ak, durationBetweenPoints: 11.ak)
+        let frequencyLine = AKLine(firstPoint: 10.ak, secondPoint: 880.ak, durationBetweenPoints: testDuration.ak)
         connect(frequencyLine)
 
-        let carrierMultiplierLine = AKLine(firstPoint: 2.ak, secondPoint: 0.ak, durationBetweenPoints: 11.ak)
+        let carrierMultiplierLine = AKLine(firstPoint: 2.ak, secondPoint: 0.ak, durationBetweenPoints: testDuration.ak)
         connect(carrierMultiplierLine)
 
-        let modulatingMultiplierLine = AKLine(firstPoint: 0.ak, secondPoint: 2.ak, durationBetweenPoints: 11.ak)
+        let modulatingMultiplierLine = AKLine(firstPoint: 0.ak, secondPoint: 2.ak, durationBetweenPoints: testDuration.ak)
         connect(modulatingMultiplierLine)
 
-        let indexLine = AKLine(firstPoint: 0.ak, secondPoint: 30.ak, durationBetweenPoints: 11.ak)
+        let indexLine = AKLine(firstPoint: 0.ak, secondPoint: 30.ak, durationBetweenPoints: testDuration.ak)
         connect(indexLine)
 
-        let operation = AKFMOscillator()
-        operation.baseFrequency = frequencyLine
-        operation.carrierMultiplier = carrierMultiplierLine
-        operation.modulatingMultiplier = modulatingMultiplierLine
-        operation.modulationIndex = indexLine
-        connect(operation)
-        connect(AKAudioOutput(audioSource:operation))
+        let fmOscillator = AKFMOscillator()
+        fmOscillator.baseFrequency = frequencyLine
+        fmOscillator.carrierMultiplier = carrierMultiplierLine
+        fmOscillator.modulatingMultiplier = modulatingMultiplierLine
+        fmOscillator.modulationIndex = indexLine
+        
+        enableParameterLog(
+            "Base Frequency = ",
+            parameter: fmOscillator.baseFrequency,
+            frequency:0.1
+        )
+        
+        enableParameterLog(
+            "Carrier Multiplier = ",
+            parameter: fmOscillator.carrierMultiplier,
+            frequency:0.1
+        )
+        
+        enableParameterLog(
+            "Modulating Multiplier = ",
+            parameter: fmOscillator.modulatingMultiplier,
+            frequency:0.1
+        )
+        
+        enableParameterLog(
+            "Modulation Index = ",
+            parameter: fmOscillator.modulationIndex,
+            frequency:0.1
+        )
+        
+        connect(fmOscillator)
+        connect(AKAudioOutput(audioSource:fmOscillator))
     }
 }
 
 let instrument = Instrument()
 AKOrchestra.addInstrument(instrument)
-AKOrchestra.testForDuration(10)
+AKOrchestra.testForDuration(testDuration)
 instrument.play()
 
 while(AKManager.sharedManager().isRunning) {} //do nothing
