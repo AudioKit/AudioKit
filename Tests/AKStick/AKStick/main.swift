c39b366e84089e91b529bbc77d047db4a6f75b20
Nicholas Arner <nicholasarner@gmail.com>
2014-12-31 11:12:51 -0500

    Added testDuration to AKStick test

--- Tests/AKStick/AKStick/main.swift
+++ Tests/AKStick/AKStick/main.swift
@@ -2,11 +2,13 @@
 //  main.swift
 //  AudioKit
 //
-//  Created by Aurelius Prochazka on 11/30/14.
+//  Created by Nick Arner and Aurelius Prochazka on 11/30/14.
 //  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
 //
 
 import Foundation
+
+let testDuration: Float = 2.0
 
 class Instrument : AKInstrument {
 
@@ -24,7 +26,7 @@ class Instrument : AKInstrument {
 
 let instrument = Instrument()
 AKOrchestra.addInstrument(instrument)
-AKOrchestra.testForDuration(1)
+AKOrchestra.testForDuration(testDuration)
 
 instrument.play()
 
