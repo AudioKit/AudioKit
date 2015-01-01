8c337c60ec6896c263e41d6fd46a2120bcb369fb
Aurelius Prochazka <aure@aure.com>
2014-12-31 17:15:06 -0800

    Variable Delay changed to use seconds.

--- AudioKit/Operations/Signal Modifiers/Delays/AKVariableDelay.h
+++ AudioKit/Operations/Signal Modifiers/Delays/AKVariableDelay.h
@@ -2,7 +2,7 @@
 //  AKVariableDelay.h
 //  AudioKit
 //
-//  Auto-generated on 12/27/14.
+//  Auto-generated on 12/31/14.
 //  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
 //
 
@@ -17,29 +17,32 @@
 @interface AKVariableDelay : AKAudio
 /// Instantiates the variable delay with all values
 /// @param input Input signal. [Default Value: ]
-/// @param delayTime Current value of delay in milliseconds. Note that linear functions have no pitch change effects. Fast changing values will cause discontinuities in the waveform resulting noise. [Default Value: ]
-/// @param maximumDelayTime Maximum value of delay in milliseconds. [Default Value: 50]
+/// @param delayTime Current value of delay in seconds. Note that linear functions have no pitch change effects. Fast changing values will cause discontinuities in the waveform resulting noise. [Default Value: 0]
+/// @param maximumDelayTime Maximum value of delay in seconds. [Default Value: 5]
 - (instancetype)initWithInput:(AKParameter *)input
                     delayTime:(AKParameter *)delayTime
              maximumDelayTime:(AKConstant *)maximumDelayTime;
 
 /// Instantiates the variable delay with default values
 /// @param input Input signal.
-/// @param delayTime Current value of delay in milliseconds. Note that linear functions have no pitch change effects. Fast changing values will cause discontinuities in the waveform resulting noise.
-- (instancetype)initWithInput:(AKParameter *)input
-                    delayTime:(AKParameter *)delayTime;
+- (instancetype)initWithInput:(AKParameter *)input;
 
 /// Instantiates the variable delay with default values
 /// @param input Input signal.
-/// @param delayTime Current value of delay in milliseconds. Note that linear functions have no pitch change effects. Fast changing values will cause discontinuities in the waveform resulting noise.
-+ (instancetype)audioWithInput:(AKParameter *)input
-                     delayTime:(AKParameter *)delayTime;
++ (instancetype)audioWithInput:(AKParameter *)input;
 
-/// Maximum value of delay in milliseconds. [Default Value: 50]
+/// Current value of delay in seconds. Note that linear functions have no pitch change effects. Fast changing values will cause discontinuities in the waveform resulting noise. [Default Value: 0]
+@property AKParameter *delayTime;
+
+/// Set an optional delay time
+/// @param delayTime Current value of delay in seconds. Note that linear functions have no pitch change effects. Fast changing values will cause discontinuities in the waveform resulting noise. [Default Value: 0]
+- (void)setOptionalDelayTime:(AKParameter *)delayTime;
+
+/// Maximum value of delay in seconds. [Default Value: 5]
 @property AKConstant *maximumDelayTime;
 
 /// Set an optional maximum delay time
-/// @param maximumDelayTime Maximum value of delay in milliseconds. [Default Value: 50]
+/// @param maximumDelayTime Maximum value of delay in seconds. [Default Value: 5]
 - (void)setOptionalMaximumDelayTime:(AKConstant *)maximumDelayTime;
 
 
