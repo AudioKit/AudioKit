0e193a01b28e77e852b6163126479d55383a76ef
Nicholas Arner <nicholasarner@gmail.com>
2015-01-01 21:44:36 -0500

    Added logging and phrases to AKStruckMetalBar test

--- Tests/AKStruckMetalBar/AKStruckMetalBar/main.swift
+++ Tests/AKStruckMetalBar/AKStruckMetalBar/main.swift
@@ -2,28 +2,72 @@
 //  main.swift
 //  AudioKit
 //
-//  Created by Aurelius Prochazka on 11/29/14.
+//  Created by Nick Arner and Aurelius Prochazka on 12/26/14.
 //  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
 //
 
 import Foundation
 
-class Instrument : AKInstrument {
+let testDuration: Float = 10.0
 
+class Instrument : AKInstrument {
+    
     override init() {
         super.init()
+        
+        let note = StruckMetalBarNote()
+        addNoteProperty(note.strikePosition)
+        addNoteProperty(note.strikeWidth)
+        
+        let struckMetalBar = AKStruckMetalBar()
+        struckMetalBar.strikePosition = note.strikePosition
+        struckMetalBar.strikeWidth = note.strikeWidth
+        connect(struckMetalBar)
+        
+        enableParameterLog(
+            "Strike Position = ",
+            parameter: struckMetalBar.strikePosition,
+            timeInterval:1
+        )
+        enableParameterLog(
+            "Strike Width = ",
+            parameter: struckMetalBar.strikeWidth,
+            timeInterval:1
+        )
+        connect(AKAudioOutput(audioSource:struckMetalBar))
+    }
+}
 
-        let operation = AKStruckMetalBar()
-        connect(operation)
-        connect(AKAudioOutput(audioSource:operation))
+class StruckMetalBarNote: AKNote {
+    var strikePosition = AKNoteProperty()
+    var strikeWidth = AKNoteProperty()
+    
+    override init() {
+        super.init()
+        addProperty(strikePosition)
+        addProperty(strikeWidth)
+    }
+    
+    
+    convenience init(strikePostion: Float, strikeWidth: Float) {
+        self.init()
+        self.strikePosition.setValue(strikePostion)
+        self.strikeWidth.setValue(strikeWidth)
     }
 }
 
 let instrument = Instrument()
 AKOrchestra.addInstrument(instrument)
-AKOrchestra.testForDuration(2)
+AKOrchestra.testForDuration(testDuration)
+let phrase = AKPhrase()
 
-instrument.play()
+for i in 1...10 {
+    let note = StruckMetalBarNote(strikePostion: Float(i)/20.0, strikeWidth: Float(i)/50)
+    note.duration.setValue(1.0)
+    phrase.addNote(note, atTime: Float(i-1))
+}
+
+instrument.playPhrase(phrase)
 
 while(AKManager.sharedManager().isRunning) {} //do nothing
 println("Test complete!")
