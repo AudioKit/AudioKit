e0f527f98327c602bf47886b88e2bd9ce7c4f20d
Aurelius Prochazka <aure@aure.com>
2015-01-04 19:26:59 -0800

    Turned off logging in swift keyboard example

--- Examples/iOS/SwiftKeyboard/SwiftKeyboard/Conductor.swift
+++ Examples/iOS/SwiftKeyboard/SwiftKeyboard/Conductor.swift
@@ -14,7 +14,6 @@ class Conductor {
     let frequencies = [523.25, 554.37, 587.33, 622.25, 659.26, 698.46, 739.99, 783.99, 830.61, 880, 932.33, 987.77, 1046.5]
 
     init() {
-        AKManager.sharedManager().isLogging = true
         AKOrchestra.addInstrument(toneGenerator)
         fx = EffectsProcessor(audioSource: toneGenerator.auxilliaryOutput)
         AKOrchestra.addInstrument(fx)
