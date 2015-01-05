21af62f0d7524b40bd25d6a34220a9aedc724ff3
Aurelius Prochazka <aure@aure.com>
2015-01-04 18:31:34 -0800

    Improved Segment Array Operations

---
+++ AudioKit/Operations/Signal Generators/Segment Arrays/AKExponentialSegmentArrayLoop.m
@@ -0,0 +1,47 @@
+//
+//  AKExponentialSegmentArrayLoop.m
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 1/14/15
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+//  Implementation of Csound's loopseg:
+//  http://www.csounds.com/manual/html/loopxseg.html
+//
+
+#import "AKExponentialSegmentArrayLoop.h"
+
+@implementation AKExponentialSegmentArrayLoop
+{
+    AKParameter *loopFrequency;
+    NSMutableArray *segments;
+}
+
+- (instancetype)initWithFrequency:(AKParameter *)frequency
+                     initialValue:(AKParameter *)initialValue;
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        loopFrequency = frequency;
+        segments = [[NSMutableArray alloc] init];
+        [segments addObject:initialValue];
+    }
+    return self;
+}
+
+- (void)addValue:(AKParameter *)value
+   afterDuration:(AKParameter *)duration
+{
+    [segments addObject:duration];
+    [segments addObject:value];
+}
+
+- (NSString *)stringForCSD {
+    return [NSString stringWithFormat:
+            @"%@ loopxseg %@, 0, 0, %@",
+            self,
+            loopFrequency,
+            [segments componentsJoinedByString:@", "]];
+}
+
+@end
