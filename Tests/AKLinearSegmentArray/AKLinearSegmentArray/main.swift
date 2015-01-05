98e9fdff6fbb29ea890d7ee4c0070c743f78fae7
Aurelius Prochazka <aure@aure.com>
2015-01-04 18:32:44 -0800

    Segment Array tests

---
+++ Tests/AKLinearSegmentArray/AKLinearSegmentArray/main.swift
@@ -0,0 +1,48 @@
+//
+//  main.swift
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 1/4/15.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+
+import Foundation
+
+let testDuration: Float = 10
+
+class Instrument : AKInstrument {
+
+    override init() {
+        super.init()
+        
+        let segments = AKLinearSegmentArray(
+            initialValue: 440.ak,
+            targetValue: 660.ak,
+            afterDuration: 2.ak
+        )
+        segments.addValue(550.ak, afterDuration: 0.ak)
+        segments.addValue(550.ak, afterDuration: 1.ak)
+        segments.addValue(880.ak, afterDuration: 0.ak)
+        segments.addValue(220.ak, afterDuration: 6.ak)
+        connect(segments)
+        
+        enableParameterLog("segment value = ", parameter: segments, timeInterval: 0.1)
+        
+        let oscillator = AKOscillator()
+        oscillator.frequency = segments
+        connect(oscillator)
+        
+        let output = AKAudioOutput(audioSource: oscillator)
+        connect(output)
+    }
+}
+
+let instrument = Instrument()
+AKOrchestra.addInstrument(instrument)
+AKManager.sharedManager().isLogging = true
+AKOrchestra.testForDuration(testDuration)
+
+instrument.play()
+
+while(AKManager.sharedManager().isRunning) {} //do nothing
+println("Test complete!")
