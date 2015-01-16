6d65f144323d20b1a847f1300ccef1a9f48e6d8e
Aurelius Prochazka <aure@aure.com>
2015-01-15 22:49:06 -0800

    Allowing note properties values to be set after a delay time

--- AudioKit/Parameters/Properties/AKNoteProperty.h
+++ AudioKit/Parameters/Properties/AKNoteProperty.h
@@ -25,6 +25,12 @@
 /// Internal reference number
 @property (assign) int pValue;
 
+
+/// Set the value at a future time
+/// @param floatValue The value that will be set
+/// @param time The delay time after which the value will be set.
+- (void)setValue:(float)floatValue afterDelay:(float)time;
+
 + (instancetype)duration;
 
 @end
