53e91e682867eca97ef2aaac94107d9409f2ec45
Adam Boulanger <adam@hearforyourself.com>
2015-01-20 14:21:14 -0800

    Exponential curve with parameter for amount of growth and decay.

---
+++ AudioKit/Operations/Function Tables/Generators/AKExponentialCurvesVariableGrowth.h
@@ -0,0 +1,13 @@
+//
+//  AKExponentialCurvesVariableGrowth.h
+//  EasyExponentialCurves
+//
+//  Created by Adam Boulanger on 1/20/15.
+//  Copyright (c) 2015 Adam Boulanger. All rights reserved.
+//
+
+#import "AKFunctionTable.h"
+
+@interface AKExponentialCurvesVariableGrowth : AKFunctionTable
+
+@end
---
+++ AudioKit/Operations/Function Tables/Generators/AKExponentialCurvesVariableGrowth.m
@@ -0,0 +1,13 @@
+//
+//  AKExponentialCurvesVariableGrowth.m
+//  EasyExponentialCurves
+//
+//  Created by Adam Boulanger on 1/20/15.
+//  Copyright (c) 2015 Adam Boulanger. All rights reserved.
+//
+
+#import "AKExponentialCurvesVariableGrowth.h"
+
+@implementation AKExponentialCurvesVariableGrowth
+
+@end
