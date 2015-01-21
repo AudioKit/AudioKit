2ce34c15669088f4e1637fddb340fd4a10b51771
Aurelius Prochazka <aure@aure.com>
2015-01-21 14:47:04 -0800

    Updating the convolution example with a level meter.

--- Examples/iOS/Convolution/Convolution/ViewController.m
+++ Examples/iOS/Convolution/Convolution/ViewController.m
@@ -10,19 +10,41 @@
 #import "AKFoundation.h"
 #import "AKTools.h"
 #import "ConvolutionInstrument.h"
+#import "LevelMeter.h"
+#import "AKAudioAnalyzer.h"
 
 @implementation ViewController
 {
     ConvolutionInstrument *conv;
+    
+    AKAudioAnalyzer *analyzer;
+    IBOutlet LevelMeter *levelMeter;
+    AKSequence *continuouslyUpdateLevelMeter;
+    AKEvent *updateLevelMeter;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    continuouslyUpdateLevelMeter = [AKSequence sequence];
+    updateLevelMeter = [[AKEvent alloc] initWithBlock:^{
+        [levelMeter setLevel:analyzer.trackedAmplitude.value];
+        [levelMeter setNeedsDisplay];
+        [continuouslyUpdateLevelMeter addEvent:updateLevelMeter afterDuration:0.04];
+        
+    }];
+    [continuouslyUpdateLevelMeter addEvent:updateLevelMeter];
+    [continuouslyUpdateLevelMeter play];
     
     conv = [[ConvolutionInstrument alloc] init];
     [AKOrchestra addInstrument:conv];
+    [[AKManager sharedManager] setIsLogging:YES];
+    analyzer = [[AKAudioAnalyzer alloc] initWithAudioSource:conv.auxilliaryOutput];
+    [AKOrchestra addInstrument:analyzer];
+    
     [AKOrchestra start];
+    [analyzer play];
 }
 
 - (IBAction)start:(id)sender {
