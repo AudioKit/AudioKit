9b7a1b2d4d962aabc7dc07111959ec8ba3bd0dc9
Aurelius Prochazka <aure@aure.com>
2015-01-30 00:27:58 -0800

    Testing out a new way to define types

--- AudioKit/Operations/Signal Generators/Oscillators/AKVCOscillator.h
+++ AudioKit/Operations/Signal Generators/Oscillators/AKVCOscillator.h
@@ -15,13 +15,23 @@
  */
 
 @interface AKVCOscillator : AKAudio
+
+///Type Helpers
++ (AKConstant *)waveformTypeForSawtooth;
++ (AKConstant *)waveformTypeForSquareWithPWM;
++ (AKConstant *)waveformTypeForTriangleWithRamp;
++ (AKConstant *)waveformTypeForUnnormalizedPulse;
++ (AKConstant *)waveformTypeForIntegratedSawtooth;
++ (AKConstant *)waveformTypeForSquare;
++ (AKConstant *)waveformTypeForTriangle;
+
 /// Instantiates the vc oscillator with all values
-/// @param waveformType Choose from various waveform types. [Default Value: AKVCOscillatorWaveformTypeSawtooth]
+/// @param waveformType Valid types are given by class functions starting with waveformTypeFor... [Default Value: waveformTypeForSawtooth]
 /// @param bandwidth Bandwidth of the generated waveform, as percentage (0 to 1) of the sample rate. The expected range is 0 to 0.5 [Default Value: 0.5]
 /// @param pulseWidth The pulse width of the square wave or the ramp characteristics of the triangle wave. It is required only by these waveforms and ignored in all other cases. The expected range is 0 to 1, any other value is wrapped to the allowed range. Updated at Control-rate. [Default Value: 0]
 /// @param frequency Frequency in Hz Updated at Control-rate. [Default Value: 440]
 /// @param amplitude Amplitude scale. In the case of a imode waveform value of a pulse waveform, the actual output level can be a lot higher than this value. Updated at Control-rate. [Default Value: 1]
-- (instancetype)initWithWaveformType:(AKVCOscillatorWaveformType)waveformType
+- (instancetype)initWithWaveformType:(AKConstant *)waveformType
                            bandwidth:(AKConstant *)bandwidth
                           pulseWidth:(AKParameter *)pulseWidth
                            frequency:(AKParameter *)frequency
@@ -34,12 +44,12 @@
 + (instancetype)oscillator;
 
 
-/// Choose from various waveform types. [Default Value: AKVCOscillatorWaveformTypeSawtooth]
-@property AKVCOscillatorWaveformType waveformType;
+/// Valid types are given by class functions starting with waveformTypeFor... [Default Value: waveformTypeForSawtooth]
+@property AKConstant *waveformType;
 
 /// Set an optional waveform type
-/// @param waveformType Choose from various waveform types. [Default Value: AKVCOscillatorWaveformTypeSawtooth]
-- (void)setOptionalWaveformType:(AKVCOscillatorWaveformType)waveformType;
+/// @param waveformType Valid types are given by class functions starting with waveformTypeFor... [Default Value: waveformTypeForSawtooth]
+- (void)setOptionalWaveformType:(AKConstant *)waveformType;
 
 /// Bandwidth of the generated waveform, as percentage (0 to 1) of the sample rate. The expected range is 0 to 0.5 [Default Value: 0.5]
 @property AKConstant *bandwidth;
