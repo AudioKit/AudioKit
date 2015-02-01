f480fae9ee9eef684e1e8e2df27a0117cd416d71
Aurelius Prochazka <aure@aure.com>
2015-02-01 14:10:35 -0800

    Starting a new class for multiple Input math operations

--- AudioKit/Operations/Mathematical Operations/Mixing/AKSum.h
+++ AudioKit/Operations/Mathematical Operations/Mixing/AKSum.h
@@ -3,29 +3,13 @@
 //  AudioKit
 //
 //  Created by Aurelius Prochazka on 6/9/12.
-//  Copyright (c) 2012 Aurelius Prochazka. All rights reserved.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
 //
 
-#import "AKParameter+Operation.h"
-#import "AKAudio.h"
 
 /** Sums any number of a-rate signals.
 */
 
-@interface AKSum : AKAudio
-
-@property NSArray *inputs;
-
-/// Create a new signal as a sum of given signals.
-/// @param firstOperand At least one input is required
-/// @param ...          End the list with a nil.
-- (instancetype)initWithOperands:(AKParameter *)firstOperand,...;
-
-
-/// Create a new signal as a sum of exactly two given signals.
-/// @param firstOperand First signal to be summed.
-/// @param secondOperand Second Signal to be summed.
-- (instancetype)initWithFirstOperand:(AKParameter *)firstOperand
-                       secondOperand:(AKParameter *)secondOperand;
+@interface AKSum : AKMultipleInputMathOperation
 
 @end
