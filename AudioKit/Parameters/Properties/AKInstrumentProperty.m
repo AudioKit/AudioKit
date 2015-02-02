7a3d9cc495dabd5978a7863dde832f5a26d66cbf
Aurelius Prochazka <aure@aure.com>
2015-02-02 12:49:18 -0800

    Reverting back to previous method of setting pointers

--- AudioKit/Parameters/Properties/AKInstrumentProperty.m
+++ AudioKit/Parameters/Properties/AKInstrumentProperty.m
@@ -61,12 +61,13 @@
 
 - (void)updateValuesToCsound
 {
-    *channelPtr = self.value;
+    if (isCacheDirty) *channelPtr = self.value;
 }
+
 - (void)updateValuesFromCsound
 {
     if ((isCacheDirty) && (*channelPtr == self.value)) isCacheDirty = NO;
-    if (*channelPtr) self.value = *channelPtr;
+    if ((!isCacheDirty) && (*channelPtr)) self.value = *channelPtr;
 }
 
 @end
