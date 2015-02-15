54733c5cbc4218a436c37f4c3733e789a0f86d72
Aurelius Prochazka <aure@aure.com>
2015-02-14 19:57:52 -0800

    Added speed and start time functionality to mono file input

--- AudioKit/Operations/Signal Input and Output/AKMonoFileInput.h
+++ AudioKit/Operations/Signal Input and Output/AKMonoFileInput.h
@@ -18,4 +18,26 @@
 /// @param fileName Location of the file on disk.
 - (instancetype)initWithFilename:(NSString *)fileName;
 
+/// Create a file input.
+/// @param fileName Location of the file on disk.
+/// @param speed Speed of the playback relative to 1 [Default Value: 1]
+/// @param startTime Time in second to start the playback (useful for pause/resume)
+- (instancetype)initWithFilename:(NSString *)fileName
+                           speed:(AKParameter *)speed
+                       startTime:(AKConstant *)startTime;
+
+/// Speed of the playback relative to 1 [Default Value: 1]
+@property AKParameter *speed;
+
+/// Set an optional speed
+/// @param speed Speed of the playback relative to 1 [Default Value: 1]
+- (void)setOptionalSpeed:(AKParameter *)speed;
+
+/// Set the start time (useful for pause/resume
+@property AKConstant *startTime;
+
+/// Set an optional speed
+/// @param startTime Time in second to start the playback (useful for pause/resume)
+- (void)setOptionalStartTime:(AKConstant *)startTime;
+
 @end
