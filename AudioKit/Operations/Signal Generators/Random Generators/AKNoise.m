f03cc81773f0bdd470fa8bc09c553383785ee47c
Aurelius Prochazka <aure@aure.com>
2015-02-19 03:12:52 -0800

    Recursive and inline connecting AKNoise

--- AudioKit/Operations/Signal Generators/Random Generators/AKNoise.m
+++ AudioKit/Operations/Signal Generators/Random Generators/AKNoise.m
@@ -2,8 +2,7 @@
 //  AKNoise.m
 //  AudioKit
 //
-//  Auto-generated on 2/13/15.
-//  Customized by Aurelius Prochazka on 2/13/15
+//  Auto-generated on 2/19/15.
 //  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
 //
 //  Implementation of Csound's noisepinky:
@@ -24,7 +23,8 @@
         _amplitude = amplitude;
         _pinkBalance = pinkBalance;
         _beta = beta;
-    }
+        [self setUpConnections];
+}
     return self;
 }
 
@@ -36,6 +36,7 @@
         _amplitude = akp(1);
         _pinkBalance = akp(0);
         _beta = akp(0);
+        [self setUpConnections];
     }
     return self;
 }
@@ -45,33 +46,95 @@
     return [[AKNoise alloc] init];
 }
 
-- (void)setOptionalAmplitude:(AKParameter *)amplitude {
+- (void)setAmplitude:(AKParameter *)amplitude {
     _amplitude = amplitude;
-}
-- (void)setOptionalPinkBalance:(AKParameter *)pinkBalance {
-    _pinkBalance = pinkBalance;
-}
-- (void)setOptionalBeta:(AKParameter *)beta {
-    _beta = beta;
+    [self setUpConnections];
 }
 
-- (NSString *)stringForCSD {
+- (void)setOptionalAmplitude:(AKParameter *)amplitude {
+    [self setAmplitude:amplitude];
+}
+
+- (void)setPinkBalance:(AKParameter *)pinkBalance {
+    _pinkBalance = pinkBalance;
+    [self setUpConnections];
+}
+
+- (void)setOptionalPinkBalance:(AKParameter *)pinkBalance {
+    [self setPinkBalance:pinkBalance];
+}
+
+- (void)setBeta:(AKParameter *)beta {
+    _beta = beta;
+    [self setUpConnections];
+}
+
+- (void)setOptionalBeta:(AKParameter *)beta {
+    [self setBeta:beta];
+}
+
+
+- (void)setUpConnections
+{
+    self.state = @"connectable";
+    self.dependencies = @[_amplitude, _pinkBalance, _beta];
+}
+
+- (NSString *)inlineStringForCSD
+{
+    NSMutableString *inlineCSDString = [[NSMutableString alloc] init];
+
+    [inlineCSDString appendString:@"noisepinky("];
+    [inlineCSDString appendString:[self inputsString]];
+    [inlineCSDString appendString:@")"];
+
+    return inlineCSDString;
+}
+
+
+- (NSString *)stringForCSD
+{
     NSMutableString *csdString = [[NSMutableString alloc] init];
 
-    
-    [csdString appendFormat:@"aWhiteNoise noise %@, ", _amplitude];
-    
-    if ([_beta class] == [AKControl class]) {
-        [csdString appendFormat:@"%@\n", _beta];
-    } else {
-        [csdString appendFormat:@"AKControl(%@)\n", _beta];
-    }
-    
-    [csdString appendFormat:
-     @"%@ = (1-%@) * aWhiteNoise + %@ * pinkish(aWhiteNoise, 1)\n",
-     self, _pinkBalance, _pinkBalance];
-    
+    [csdString appendFormat:@"%@ noisepinky ", self];
+    [csdString appendString:[self inputsString]];
     return csdString;
 }
+
+- (NSString *)inputsString {
+    NSMutableString *inputsString = [[NSMutableString alloc] init];
+
+    
+    if ([_amplitude class] == [AKAudio class]) {
+        [inputsString appendFormat:@"%@, ", _amplitude];
+    } else {
+        [inputsString appendFormat:@"AKAudio(%@), ", _amplitude];
+    }
+
+    if ([_beta class] == [AKControl class]) {
+        [inputsString appendFormat:@"%@, ", _beta];
+    } else {
+        [inputsString appendFormat:@"AKControl(%@), ", _beta];
+    }
+
+    if ([_pinkBalance class] == [AKAudio class]) {
+        [inputsString appendFormat:@"%@", _pinkBalance];
+    } else {
+        [inputsString appendFormat:@"AKAudio(%@)", _pinkBalance];
+    }
+return inputsString;
+}
+
+- (NSString *)udoString {
+    return @"\n"
+    "opcode  noisepinky, a, aka\n"
+    "aOut init 0\n"
+    "aAmplitude, kBeta, aPink xin\n"
+    "aWhiteNoise noise aAmplitude, kBeta\n"
+    "aOut = (1-aPink) * aWhiteNoise + aPink*pinkish(aWhiteNoise,1)\n"
+    "xout aOut\n"
+    "endop\n";
+}
+
 
 @end
