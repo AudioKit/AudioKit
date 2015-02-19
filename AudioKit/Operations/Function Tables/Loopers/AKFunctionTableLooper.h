7953f3ecb4b37f1374bdf48aec1ed4e138a95ae2
Aurelius Prochazka <aure@aure.com>
2015-02-19 00:43:25 -0800

    Added recursive connection to AKFunctionTableLooper

--- AudioKit/Operations/Function Tables/Loopers/AKFunctionTableLooper.h
+++ AudioKit/Operations/Function Tables/Loopers/AKFunctionTableLooper.h
@@ -2,7 +2,9 @@
 //  AKFunctionTableLooper.h
 //  AudioKit
 //
-//  Auto-generated on 1/3/15.
+//  Auto-generated on 2/19/15.
+//  Customized by Aurelius Prochazka to include types as class methods
+
 //  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
 //
 
@@ -52,42 +54,42 @@
 + (instancetype)looperWithFunctionTable:(AKFunctionTable *)functionTable;
 
 /// Loop start point in seconds. [Default Value: 0]
-@property AKParameter *startTime;
+@property (nonatomic) AKParameter *startTime;
 
 /// Set an optional start time
 /// @param startTime Loop start point in seconds. Updated at Control-rate. [Default Value: 0]
 - (void)setOptionalStartTime:(AKParameter *)startTime;
 
 /// Playback end position in seconds.  Defaults to end of function table(0). [Default Value: 0]
-@property AKParameter *endTime;
+@property (nonatomic) AKParameter *endTime;
 
 /// Set an optional end time
 /// @param endTime Playback end position in seconds.  Defaults to end of function table(0). Updated at Control-rate. [Default Value: 0]
 - (void)setOptionalEndTime:(AKParameter *)endTime;
 
 /// Pitch control by way of transposition ratio. [Default Value: 1]
-@property AKParameter *transpositionRatio;
+@property (nonatomic) AKParameter *transpositionRatio;
 
 /// Set an optional transposition ratio
 /// @param transpositionRatio Pitch control by way of transposition ratio. Updated at Control-rate. [Default Value: 1]
 - (void)setOptionalTranspositionRatio:(AKParameter *)transpositionRatio;
 
 /// Amplitude of loop. [Default Value: 1]
-@property AKParameter *amplitude;
+@property (nonatomic) AKParameter *amplitude;
 
 /// Set an optional amplitude
 /// @param amplitude Amplitude of loop. Updated at Control-rate. [Default Value: 1]
 - (void)setOptionalAmplitude:(AKParameter *)amplitude;
 
 /// crossfade length in seconds, updated once per loop cycle and limited to loop length. [Default Value: 0]
-@property AKParameter *crossfadeDuration;
+@property (nonatomic) AKParameter *crossfadeDuration;
 
 /// Set an optional crossfade duration
 /// @param crossfadeDuration crossfade length in seconds, updated once per loop cycle and limited to loop length. Updated at Control-rate. [Default Value: 0]
 - (void)setOptionalCrossfadeDuration:(AKParameter *)crossfadeDuration;
 
 /// Loop modes are forward, backward, and back and forth. [Default Value: AKFunctionTableLooperModeNormal]
-@property AKConstant *loopMode;
+@property (nonatomic) AKConstant *loopMode;
 
 /// Set an optional loop mode
 /// @param loopMode Loop modes are forward, backward, and back and forth. [Default Value: AKFunctionTableLooperModeNormal]
