e6b856d6c566e02c1091688a0167c174a3e94a78
Aurelius Prochazka <aure@aure.com>
2015-02-20 15:20:40 -0800

    Recursive and inline AKFFTProcessor

--- AudioKit/Operations/FFT/AKFFTProcessor.m
+++ AudioKit/Operations/FFT/AKFFTProcessor.m
@@ -2,7 +2,7 @@
 //  AKFFTProcessor.m
 //  AudioKit
 //
-//  Auto-generated on 1/27/15.
+//  Auto-generated on 2/20/15.
 //  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
 //
 //  Implementation of Csound's pvstanal:
@@ -36,7 +36,8 @@
         _sizeOfFFT = sizeOfFFT;
         _hopSize = hopSize;
         _dbthresh = dbthresh;
-    }
+        [self setUpConnections];
+}
     return self;
 }
 
@@ -53,6 +54,7 @@
         _sizeOfFFT = akp(2048);
         _hopSize = akp(512);
         _dbthresh = akp(1);
+        [self setUpConnections];
     }
     return self;
 }
@@ -62,73 +64,141 @@
     return [[AKFFTProcessor alloc] initWithFunctionTable:functionTable];
 }
 
-- (void)setOptionalFrequencyRatio:(AKParameter *)frequencyRatio {
+- (void)setFrequencyRatio:(AKParameter *)frequencyRatio {
     _frequencyRatio = frequencyRatio;
-}
-- (void)setOptionalTimeRatio:(AKParameter *)timeRatio {
-    _timeRatio = timeRatio;
-}
-- (void)setOptionalAmplitude:(AKParameter *)amplitude {
-    _amplitude = amplitude;
-}
-- (void)setOptionalFunctionTableOffset:(AKConstant *)functionTableOffset {
-    _functionTableOffset = functionTableOffset;
-}
-- (void)setOptionalSizeOfFFT:(AKConstant *)sizeOfFFT {
-    _sizeOfFFT = sizeOfFFT;
-}
-- (void)setOptionalHopSize:(AKConstant *)hopSize {
-    _hopSize = hopSize;
-}
-- (void)setOptionalDbthresh:(AKConstant *)dbthresh {
-    _dbthresh = dbthresh;
+    [self setUpConnections];
 }
 
-- (NSString *)stringForCSD {
+- (void)setOptionalFrequencyRatio:(AKParameter *)frequencyRatio {
+    [self setFrequencyRatio:frequencyRatio];
+}
+
+- (void)setTimeRatio:(AKParameter *)timeRatio {
+    _timeRatio = timeRatio;
+    [self setUpConnections];
+}
+
+- (void)setOptionalTimeRatio:(AKParameter *)timeRatio {
+    [self setTimeRatio:timeRatio];
+}
+
+- (void)setAmplitude:(AKParameter *)amplitude {
+    _amplitude = amplitude;
+    [self setUpConnections];
+}
+
+- (void)setOptionalAmplitude:(AKParameter *)amplitude {
+    [self setAmplitude:amplitude];
+}
+
+- (void)setFunctionTableOffset:(AKConstant *)functionTableOffset {
+    _functionTableOffset = functionTableOffset;
+    [self setUpConnections];
+}
+
+- (void)setOptionalFunctionTableOffset:(AKConstant *)functionTableOffset {
+    [self setFunctionTableOffset:functionTableOffset];
+}
+
+- (void)setSizeOfFFT:(AKConstant *)sizeOfFFT {
+    _sizeOfFFT = sizeOfFFT;
+    [self setUpConnections];
+}
+
+- (void)setOptionalSizeOfFFT:(AKConstant *)sizeOfFFT {
+    [self setSizeOfFFT:sizeOfFFT];
+}
+
+- (void)setHopSize:(AKConstant *)hopSize {
+    _hopSize = hopSize;
+    [self setUpConnections];
+}
+
+- (void)setOptionalHopSize:(AKConstant *)hopSize {
+    [self setHopSize:hopSize];
+}
+
+- (void)setDbthresh:(AKConstant *)dbthresh {
+    _dbthresh = dbthresh;
+    [self setUpConnections];
+}
+
+- (void)setOptionalDbthresh:(AKConstant *)dbthresh {
+    [self setDbthresh:dbthresh];
+}
+
+
+- (void)setUpConnections
+{
+    self.state = @"connectable";
+    self.dependencies = @[_functionTable, _frequencyRatio, _timeRatio, _amplitude, _functionTableOffset, _sizeOfFFT, _hopSize, _dbthresh];
+}
+
+- (NSString *)inlineStringForCSD
+{
+    NSMutableString *inlineCSDString = [[NSMutableString alloc] init];
+
+    [inlineCSDString appendString:@"pvstanal("];
+    [inlineCSDString appendString:[self inputsString]];
+    [inlineCSDString appendString:@")"];
+
+    return inlineCSDString;
+}
+
+
+- (NSString *)stringForCSD
+{
     NSMutableString *csdString = [[NSMutableString alloc] init];
+
+    [csdString appendFormat:@"%@ pvstanal ", self];
+    [csdString appendString:[self inputsString]];
+    return csdString;
+}
+
+- (NSString *)inputsString {
+    NSMutableString *inputsString = [[NSMutableString alloc] init];
 
     // Constant Values  
     AKConstant *_wrap = akp(1);        
-    [csdString appendFormat:@"%@ pvstanal ", self];
-
+    
     if ([_timeRatio class] == [AKControl class]) {
-        [csdString appendFormat:@"%@, ", _timeRatio];
+        [inputsString appendFormat:@"%@, ", _timeRatio];
     } else {
-        [csdString appendFormat:@"AKControl(%@), ", _timeRatio];
+        [inputsString appendFormat:@"AKControl(%@), ", _timeRatio];
     }
 
     if ([_amplitude class] == [AKControl class]) {
-        [csdString appendFormat:@"%@, ", _amplitude];
+        [inputsString appendFormat:@"%@, ", _amplitude];
     } else {
-        [csdString appendFormat:@"AKControl(%@), ", _amplitude];
+        [inputsString appendFormat:@"AKControl(%@), ", _amplitude];
     }
 
     if ([_frequencyRatio class] == [AKControl class]) {
-        [csdString appendFormat:@"%@, ", _frequencyRatio];
+        [inputsString appendFormat:@"%@, ", _frequencyRatio];
     } else {
-        [csdString appendFormat:@"AKControl(%@), ", _frequencyRatio];
+        [inputsString appendFormat:@"AKControl(%@), ", _frequencyRatio];
     }
 
     if ([_functionTable class] == [AKControl class]) {
-        [csdString appendFormat:@"%@, ", _functionTable];
+        [inputsString appendFormat:@"%@, ", _functionTable];
     } else {
-        [csdString appendFormat:@"AKControl(%@), ", _functionTable];
+        [inputsString appendFormat:@"AKControl(%@), ", _functionTable];
     }
 
     if ([_wrap class] == [AKControl class]) {
-        [csdString appendFormat:@"%@, ", _wrap];
+        [inputsString appendFormat:@"%@, ", _wrap];
     } else {
-        [csdString appendFormat:@"AKControl(%@), ", _wrap];
+        [inputsString appendFormat:@"AKControl(%@), ", _wrap];
     }
 
-    [csdString appendFormat:@"%@, ", _functionTableOffset];
+    [inputsString appendFormat:@"%@, ", _functionTableOffset];
     
-    [csdString appendFormat:@"%@, ", _sizeOfFFT];
+    [inputsString appendFormat:@"%@, ", _sizeOfFFT];
     
-    [csdString appendFormat:@"%@, ", _hopSize];
+    [inputsString appendFormat:@"%@, ", _hopSize];
     
-    [csdString appendFormat:@"%@", _dbthresh];
-    return csdString;
+    [inputsString appendFormat:@"%@", _dbthresh];
+    return inputsString;
 }
 
 @end
