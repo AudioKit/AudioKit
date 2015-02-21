44b9957bd9c16d7c415d42c9704bf7ff376b5685
Aurelius Prochazka <aure@aure.com>
2015-02-20 22:39:02 -0800

    Recursive, inline connection and tests for convolutions

--- AudioKit/Operations/Signal Modifiers/Convolutions/AKStereoConvolution.h
+++ AudioKit/Operations/Signal Modifiers/Convolutions/AKStereoConvolution.h
@@ -1,29 +1,26 @@
 //
-//  AKConvolution.h
+//  AKStereoConvolution.h
 //  AudioKit
 //
-//  Auto-generated on 12/27/14.
-//  Customized by Aurelius Prochazka on 12/27/14.
-//
-//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
+//  Auto-generated on 2/20/15.
+//  Customized by Aurelius Prochazka to simplify the interface.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
 //
 
 #import "AKStereoAudio.h"
 #import "AKParameter+Operation.h"
 
 /** Convolution based on a uniformly partitioned overlap-save algorithm.
- 
- 
  */
 
 @interface AKStereoConvolution : AKStereoAudio
-/// Instantiates the convolution with all values
+/// Instantiates the stereo convolution with all values
 /// @param input Input to the convolution, usually audio. [Default Value: ]
 /// @param impulseResponseFilename File contain the impulse response audio.  Usually a very short impulse sound. [Default Value: ]
 - (instancetype)initWithInput:(AKParameter *)input
       impulseResponseFilename:(NSString *)impulseResponseFilename;
 
-/// Instantiates the convolution with default values
+/// Instantiates the stereo convolution with default values
 /// @param input Input to the convolution, usually audio.
 /// @param impulseResponseFilename File contain the impulse response audio.  Usually a very short impulse sound.
 + (instancetype)convolutionWithInput:(AKParameter *)input
---
+++ Tests/AKConvolution/AKConvolution/main.swift
@@ -0,0 +1,73 @@
+//
+//  main.swift
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 2/20/15.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+
+import Foundation
+
+let testDuration: Float = 10.0
+
+class Instrument : AKInstrument {
+    
+    var auxilliaryOutput = AKAudio()
+    
+    override init() {
+        super.init()
+        
+        let filename = "CsoundLib64.framework/Sounds/808loop.wav"
+        let audio = AKFileInput(filename: filename)
+        let mono = AKMix(monoAudioFromStereoInput: audio)
+        
+        auxilliaryOutput = AKAudio.globalParameter()
+        assignOutput(auxilliaryOutput, to:mono)
+    }
+}
+
+class Processor : AKInstrument {
+    
+    init(audioSource: AKAudio) {
+        super.init()
+
+        let mixLine = AKLine(
+            firstPoint:  0.ak,
+            secondPoint: 1.ak,
+            durationBetweenPoints: testDuration.ak
+        )
+        let dishFilename      = "CsoundLib64.framework/Sounds/dish.wav"
+        let stairwellFilename = "CsoundLib64.framework/Sounds/Stairwell.wav"
+        
+        let dishConvolution      = AKConvolution(input: audioSource, impulseResponseFilename: dishFilename)
+        let stairwellConvolution = AKConvolution(input: audioSource, impulseResponseFilename: stairwellFilename)
+
+        let dishMix      = AKMix(input1: audioSource, input2: dishConvolution,      balance: 0.2.ak)
+        let stairwellMix = AKMix(input1: audioSource, input2: stairwellConvolution, balance: 0.2.ak)
+        
+        let mix  = AKMix(input1: dishMix, input2: stairwellMix, balance: mixLine)
+        setAudioOutput(mix)
+        
+        resetParameter(audioSource)
+        
+        enableParameterLog(
+            "Dish / Stairwell Mix = ",
+            parameter: mixLine,
+            timeInterval:0.1
+        )
+    }
+}
+
+AKOrchestra.testForDuration(testDuration)
+
+let instrument = Instrument()
+let processor = Processor(audioSource: instrument.auxilliaryOutput)
+AKOrchestra.addInstrument(instrument)
+AKOrchestra.addInstrument(processor)
+
+processor.play()
+instrument.play()
+
+let manager = AKManager.sharedManager()
+while(manager.isRunning) {} //do nothing
+println("Test complete!")
