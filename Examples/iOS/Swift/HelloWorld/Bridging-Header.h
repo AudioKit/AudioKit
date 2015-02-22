cbdcc0a73317358160452fb983b738e17451fcee
Nicholas Arner <nicholasarner@gmail.com>
2015-02-22 15:20:26 -0500

    Added Bridging-Header file

---
+++ Examples/iOS/Swift/HelloWorld/Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Bridging-Header.h
+//  HelloWorld
+//
+//  Created by Nicholas Arner on 2/22/15.
+//  Copyright (c) 2015 AudioKit. All rights reserved.
+//
+
+#import "AKFoundation.h"
+#import "AKTools.h"
