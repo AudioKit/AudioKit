6bad60054894afdb538d85fc96bd82bdac6a2a2b
Aurelius Prochazka <aure@aure.com>
2015-02-24 00:23:35 -0800

    Replaced AudioKitDemo with a project paralleling iOS AudioKitDemo

---
+++ Examples/OSX/AudioKitDemo/AudioKitDemo/Synthesis/SynthesisViewController.m
@@ -0,0 +1,67 @@
+//
+//  SynthesisViewController.m
+//  AudioKitDemo
+//
+//  Created by Aurelius Prochazka on 2/23/15.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+
+#import "SynthesisViewController.h"
+#import "AKFoundation.h"
+#import "Tambourine.h"
+#import "FMSynthesizer.h"
+
+
+@implementation SynthesisViewController
+{
+    IBOutlet NSView *fmSynthesizerTouchView;
+    IBOutlet NSView *tambourineTouchView;
+    
+    Tambourine *tambourine;
+    FMSynthesizer *fmSynthesizer;
+}
+
+- (void)viewDidAppear {
+    [super viewDidAppear];
+    tambourine = [[Tambourine alloc] init];
+    [AKOrchestra addInstrument:tambourine];
+    
+    fmSynthesizer = [[FMSynthesizer alloc] init];
+    [AKOrchestra addInstrument:fmSynthesizer];
+    
+    [AKOrchestra start];
+}
+
+- (void)viewWillDisappear {
+    [super viewWillDisappear];
+    [AKOrchestra reset];
+    [[AKManager sharedManager] stop];
+}
+
+- (IBAction)tapTambourine:(NSClickGestureRecognizer *)sender
+{
+    CGPoint touchPoint = [sender locationInView:tambourineTouchView];
+    float scaledX = touchPoint.x / tambourineTouchView.bounds.size.width;
+    float scaledY = 1.0 - touchPoint.y / tambourineTouchView.bounds.size.height;
+    
+    float intensity = scaledY*4000 + 20;
+    float dampingFactor = scaledX / 2.0;
+    TambourineNote *note = [[TambourineNote alloc] initWithIntensity:intensity
+                                                       dampingFactor:dampingFactor];
+    [tambourine playNote:note];
+}
+
+
+- (IBAction)tapFMOscillator:(NSClickGestureRecognizer *)sender {
+    
+    CGPoint touchPoint = [sender locationInView:fmSynthesizerTouchView];
+    float scaledX = touchPoint.x / fmSynthesizerTouchView.bounds.size.width;
+    float scaledY = 1.0 - touchPoint.y / fmSynthesizerTouchView.bounds.size.height;
+    
+    float frequency = scaledY*4000 + 20;
+    float color = scaledX;
+    FMSynthesizerNote *note = [[FMSynthesizerNote alloc] initWithFrequency:frequency color:color];
+    [fmSynthesizer playNote:note];
+}
+
+@end
