f0a6cd44dce91966a67c549570f25174fc87b665
Aurelius Prochazka <aure@aure.com>
2015-02-24 00:23:35 -0800

    Replaced AudioKitDemo with a project paralleling iOS AudioKitDemo

--- Examples/OSX/AudioKitDemo/AudioKitDemo/ViewController.h
+++ Examples/OSX/AudioKitDemo/AudioKitDemo/ViewController.h
@@ -2,7 +2,7 @@
 //  ViewController.h
 //  AudioKitDemo
 //
-//  Created by Aurelius Prochazka on 2/13/15.
+//  Created by Aurelius Prochazka on 2/23/15.
 //  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
 //
 
