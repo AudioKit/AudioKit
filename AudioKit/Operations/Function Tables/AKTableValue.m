48ddba7ce50126f73d862f9d86bf7f0f00519e1d
Aurelius Prochazka <aure@aure.com>
2015-02-27 21:06:28 -0800

    Modernized AKTableValue with rate agnosticism, recursive connections, etc.

--- AudioKit/Operations/Function Tables/AKTableValue.m
+++ AudioKit/Operations/Function Tables/AKTableValue.m
@@ -2,19 +2,39 @@
 //  AKTableValue.m
 //  AudioKit
 //
-//  Created by Aurelius Prochazka on 7/2/12.
-//  Copyright (c) 2012 Aurelius Prochazka. All rights reserved.
+//  Auto-generated on 2/27/15.
+//  Customized by Aurelius Prochazka to have many more intializer options.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+//  Implementation of Csound's table3:
+//  http://www.csounds.com/manual/html/table3.html
 //
 
 #import "AKTableValue.h"
+#import "AKManager.h"
 
 @implementation AKTableValue
 {
-    AKFunctionTable *ifn;
-    AKParameter *andx;
-    AKParameter *ixoff;
-    BOOL normalizeResult;
-    BOOL wrapData;
+    AKFunctionTable * _functionTable;
+    AKParameter * _index;
+    AKConstant *_useFractionalWidth;
+}
+
+- (instancetype)initWithFunctionTable:(AKFunctionTable *)functionTable
+                              atIndex:(AKParameter *)index
+                           withOffset:(AKConstant *)offset
+                   usingWrappingIndex:(BOOL)useWrappingIndex
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        _functionTable = functionTable;
+        _index = index;
+        _offset = offset;
+        _useWrappingIndex = useWrappingIndex;
+        _useFractionalWidth = akp(NO);
+        [self setUpConnections];
+    }
+    return self;
 }
 
 - (instancetype)initWithFunctionTable:(AKFunctionTable *)functionTable
@@ -22,33 +42,122 @@
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        ifn  = functionTable;
-        normalizeResult = NO;
-        ixoff = akpi(0);
-        wrapData = NO;
-        andx = index;
-        self.state = @"connectable";
-        self.dependencies = @[ifn, index];
+        _functionTable = functionTable;
+        _index = index;
+        // Default Values
+        _offset = akp(0);
+        _useWrappingIndex = NO;
+        _useFractionalWidth = akp(NO);
+        [self setUpConnections];
     }
     return self;
+}
+
++ (instancetype)valueOfFunctionTable:(AKFunctionTable *)functionTable
+                             atIndex:(AKParameter *)index
+{
+    return [[AKTableValue alloc] initWithFunctionTable:functionTable
+                                               atIndex:index];
+}
+
+
+- (instancetype)initWithFunctionTable:(AKFunctionTable *)functionTable
+               atFractionOfTotalWidth:(AKParameter *)fractionalIndex
+                           withOffset:(AKConstant *)offset
+                   usingWrappingIndex:(BOOL)useWrappingIndex
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        _functionTable = functionTable;
+        _index = fractionalIndex;
+        _offset = offset;
+        _useWrappingIndex = useWrappingIndex;
+        _useFractionalWidth = akp(YES);
+        [self setUpConnections];
+    }
+    return self;
+}
+
+- (instancetype)initWithFunctionTable:(AKFunctionTable *)functionTable
+               atFractionOfTotalWidth:(AKParameter *)fractionalIndex
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        _functionTable = functionTable;
+        _index = fractionalIndex;
+        // Default Values
+        _offset = akp(0);
+        _useWrappingIndex = NO;
+        _useFractionalWidth = akp(YES);
+        [self setUpConnections];
+    }
+    return self;
+}
+
++ (instancetype)valueOfFunctionTable:(AKFunctionTable *)functionTable
+              atFractionOfTotalWidth:(AKParameter *)fractionalIndex;
+{
+    return [[AKTableValue alloc] initWithFunctionTable:functionTable
+                                atFractionOfTotalWidth:fractionalIndex];
+}
+
+- (void)setOffset:(AKConstant *)offset {
+    _offset = offset;
+    [self setUpConnections];
+}
+
+- (void)setOptionalOffset:(AKConstant *)offset {
+    [self setOffset:offset];
+}
+
+
+- (void)setUpConnections
+{
+    self.state = @"connectable";
+    self.dependencies = @[_functionTable, _index, _offset];
+}
+
+- (NSString *)inlineStringForCSD
+{
+    NSMutableString *inlineCSDString = [[NSMutableString alloc] init];
     
+    [inlineCSDString appendString:@"table3("];
+    [inlineCSDString appendString:[self inputsString]];
+    [inlineCSDString appendString:@")"];
+    
+    return inlineCSDString;
 }
 
-- (void)wrap {
-    wrapData = YES;
-}
-
-- (void)offsetBy:(AKConstant *)offsetAmount {
-    ixoff = offsetAmount;
-}
 
 - (NSString *)stringForCSD
 {
-    int ixmode = normalizeResult ? 0:1;
-    int iwrap = wrapData ? 0:1;
-    return [NSString stringWithFormat:
-            @"%@ tablei AKAudio(%@), %@, %i, %@, %i",
-            self, andx, ifn, ixmode, ixoff, iwrap];
+    NSMutableString *csdString = [[NSMutableString alloc] init];
+    
+    [csdString appendFormat:@"%@ table3 ", self];
+    [csdString appendString:[self inputsString]];
+    return csdString;
+}
+
+- (NSString *)inputsString {
+    NSMutableString *inputsString = [[NSMutableString alloc] init];
+    
+    // Constant Values
+    
+    
+    if ([_index class] == [AKAudio class]) {
+        [inputsString appendFormat:@"%@, ", _index];
+    } else {
+        [inputsString appendFormat:@"AKAudio(%@), ", _index];
+    }
+    
+    [inputsString appendFormat:@"%@, ", _functionTable];
+    
+    [inputsString appendFormat:@"%@, ", _useFractionalWidth];
+    
+    [inputsString appendFormat:@"%@, ", _offset];
+    
+    [inputsString appendFormat:@"%d", _useWrappingIndex];
+    return inputsString;
 }
 
 @end
