4964363d8086188303af79064b5d06513a5ef3f7
Nicholas Arner <nicholasarner@gmail.com>
2015-03-01 18:06:40 -0500

    Started FMSynthesizer.swift

--- Examples/iOS/Swift/AudioKitDemo/AudioKitDemo/FMSynthesizer.swift
+++ Examples/iOS/Swift/AudioKitDemo/AudioKitDemo/FMSynthesizer.swift
@@ -6,4 +6,58 @@
 //  Copyright (c) 2015 AudioKit. All rights reserved.
 //
 
-import Foundation
+class FMSynthesizer: AKInstrument{
+    
+    override init() {
+        super.init()
+        
+        // Note Properties
+        var note = FMSynthesizerNote()
+        addNoteProperty(note.frequency)
+        addNoteProperty(note.color)
+     
+        let envelope = AKADSREnvelope(
+            attackDuration: 0.1.ak,
+            decayDuration: 0.1.ak,
+            sustainLevel: 0.5.ak,
+            releaseDuration: 0.3.ak,
+            delay: 0.ak
+        )
+        connect(envelope)
+        
+        var oscillator = AKFMOscillator()
+        oscillator.baseFrequency     = note.frequency
+        oscillator.carrierMultiplier = note.color.scaledBy(2.ak)
+        oscillator.modulationIndex   = note.color.scaledBy(3.ak)
+        oscillator.amplitude         = note.color.scaledBy(10.ak)
+        connect(oscillator)
+        
+        let out = AKAudioOutput(input: oscillator)
+        connect(out)
+    }
+}
+
+
+
+class FMSynthesizerNote: AKNote {
+    
+    // Note Properties
+    var frequency = AKNoteProperty(value: 440, minimum: 100, maximum: 20000)
+    var color = AKNoteProperty(value: 0, minimum: 0, maximum: 1)
+    
+    override init() {
+        super.init()
+        addProperty(frequency)
+        addProperty(color)
+        
+        // Optionally set a default note duration
+        duration.value = (1.0)
+    }
+    
+    convenience init(frequency:Float, color:Float){
+        self.init()
+        
+        self.frequency.value = frequency
+        self.color.value = color
+    }
+}
