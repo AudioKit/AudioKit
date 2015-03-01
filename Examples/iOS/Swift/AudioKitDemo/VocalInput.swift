49d13bf557c81607e95ec34debab25039400934f
Nicholas Arner <nicholasarner@gmail.com>
2015-03-01 16:11:23 -0500

    Started VocalInput.swift

--- Examples/iOS/Swift/AudioKitDemo/VocalInput.swift
+++ Examples/iOS/Swift/AudioKitDemo/VocalInput.swift
@@ -6,4 +6,15 @@
 //  Copyright (c) 2015 AudioKit. All rights reserved.
 //
 
-import Foundation
+class VocalInput: AKInstrument{
+    
+    override init() {
+        super.init()
+        
+        let microphone = AKAudioInput()
+        connect(microphone)
+        let auxilliaryOutput = AKAudio.globalParameter()
+        assignOutput(auxilliaryOutput, to: microphone)
+    }
+}
+
