f2b0193c3724a31671515815f5e9e0695e193a9b
Nicholas Arner <nicholasarner@gmail.com>
2015-02-28 19:57:04 -0500

    Created OS X HelloWorld (Swift) project

---
+++ Examples/OSX/Swift/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Nicholas Arner on 2/28/15.
+//  Copyright (c) 2015 AudioKit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
