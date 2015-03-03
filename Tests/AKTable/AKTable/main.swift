c6bff6e648a0624dbb0494a1a9d88c3299d37884
Aurelius Prochazka <aure@aure.com>
2015-03-02 20:50:53 -0800

    Moved exponential table tests

--- Tests/AKTable/AKTable/main.swift
+++ Tests/AKTable/AKTable/main.swift
@@ -15,7 +15,6 @@ class Instrument : AKInstrument {
     
     func presentFunctionTable(functionTable:AKFunctionTable, label:String) {
         NSLog("\n\n%@", label)
-        addFunctionTable(functionTable)
     }
     
     override init() {
@@ -36,24 +35,24 @@ class Instrument : AKInstrument {
         var point: AKParameter
         
         // Test Table values at specific points
-        point = AKTableValue(table: sine, atFractionOfTotalWidth: akp(0.25))
-        enableParameterLog("Sine wave value at 0.25 expect 1 = ", parameter: point, timeInterval: 100)
-        point = AKTableValue(table: sine, atFractionOfTotalWidth: akp(0.5))
-        enableParameterLog("Sine wave value at 0.5 expect 0 = ", parameter: point, timeInterval: 100)
-        point = AKTableValue(table: sine, atFractionOfTotalWidth: akp(0.75))
-        enableParameterLog("Sine wave value at 0.75 expect -1 = ", parameter: point, timeInterval: 100)
-        
-        point = AKTableValue(table: square, atFractionOfTotalWidth: akp(0.25))
-        enableParameterLog("Square wave value at 0.25 expect 1 = ", parameter: point, timeInterval: 100)
-        point = AKTableValue(table: square, atFractionOfTotalWidth: akp(0.75))
-        enableParameterLog("Square wave value at 0.75 expect -1 = ", parameter: point, timeInterval: 100)
-        
-        point = AKTableValue(functionTable: triangle, atFractionOfTotalWidth: akp(0.25))
-        enableParameterLog("Triangle wave value at 0.25 expect 1 = ", parameter: point, timeInterval: 100)
-        point = AKTableValue(functionTable: triangle, atFractionOfTotalWidth: akp(0.5))
-        enableParameterLog("Triangle wave value at 0.5 expect 0 = ", parameter: point, timeInterval: 100)
-        point = AKTableValue(functionTable: triangle, atFractionOfTotalWidth: akp(0.75))
-        enableParameterLog("Triangle wave value at 0.75 expect -1 = ", parameter: point, timeInterval: 100)
+//        point = AKTableValue(table: sine, atFractionOfTotalWidth: akp(0.25))
+//        enableParameterLog("Sine wave value at 0.25 expect 1 = ", parameter: point, timeInterval: 100)
+//        point = AKTableValue(table: sine, atFractionOfTotalWidth: akp(0.5))
+//        enableParameterLog("Sine wave value at 0.5 expect 0 = ", parameter: point, timeInterval: 100)
+//        point = AKTableValue(table: sine, atFractionOfTotalWidth: akp(0.75))
+//        enableParameterLog("Sine wave value at 0.75 expect -1 = ", parameter: point, timeInterval: 100)
+//        
+//        point = AKTableValue(table: square, atFractionOfTotalWidth: akp(0.25))
+//        enableParameterLog("Square wave value at 0.25 expect 1 = ", parameter: point, timeInterval: 100)
+//        point = AKTableValue(table: square, atFractionOfTotalWidth: akp(0.75))
+//        enableParameterLog("Square wave value at 0.75 expect -1 = ", parameter: point, timeInterval: 100)
+//        
+//        point = AKTableValue(functionTable: triangle, atFractionOfTotalWidth: akp(0.25))
+//        enableParameterLog("Triangle wave value at 0.25 expect 1 = ", parameter: point, timeInterval: 100)
+//        point = AKTableValue(functionTable: triangle, atFractionOfTotalWidth: akp(0.5))
+//        enableParameterLog("Triangle wave value at 0.5 expect 0 = ", parameter: point, timeInterval: 100)
+//        point = AKTableValue(functionTable: triangle, atFractionOfTotalWidth: akp(0.75))
+//        enableParameterLog("Triangle wave value at 0.75 expect -1 = ", parameter: point, timeInterval: 100)
         
         let lineSegments = AKLineTable(value: 0.0)
         lineSegments.addValue(1, atIndex: 1)
@@ -61,6 +60,15 @@ class Instrument : AKInstrument {
         lineSegments.addValue(0, atIndex: 4)
         lineSegments.size = 8192
         connect(lineSegments)
+        
+        let generator = AKExponentialTableGenerator(value: 0.1)
+        generator.addValue(1, atIndex: 1  )
+        generator.appendValue(0.1, afterNumberOfElements: 1)
+        generator.appendValue(1, afterNumberOfElements: 1)
+        generator.addValue(0.1, atIndex: 4)
+        
+        let newExponentialTable = AKTable(size: 16384)
+        newExponentialTable.populateTableWithGenerator(generator)
     }
 }
 
