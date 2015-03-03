8314ed57b3d2547d753b8fc0c6ceb3ab9bec4969
Aurelius Prochazka <aure@aure.com>
2015-03-02 20:50:04 -0800

    Converted AKExponentialTable into a generator for AKTable

--- AudioKit/Tables/Generators/AKExponentialTableGenerator.h
+++ AudioKit/Tables/Generators/AKExponentialTableGenerator.h
@@ -1,21 +1,15 @@
 //
-//  AKExponentialTable.h
+//  AKExponentialTableGenerator.h
 //  AudioKit
 //
-//  Created by Aurelius Prochazka on 12/26/14.
-//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
+//  Created by Aurelius Prochazka on 3/2/15.
+//  Copyright (c) 2015 AudioKit. All rights reserved.
 //
 
-#import "AKFunctionTable.h"
+#import <Foundation/Foundation.h>
+#import "AKTableGenerator.h"
 
-/** Constructs functions from segments of exponential curves.
- 
- Ordinate values, in odd-numbered parts of the array, must be nonzero and must be alike in sign.
- */
-@interface AKExponentialTable : AKFunctionTable
-
-/// Size of the table (default 4096)
-@property int size;
+@interface AKExponentialTableGenerator : AKTableGenerator
 
 /// Start the exponential curves at a specific value
 /// @param value Initial value of the the first line segment
@@ -30,4 +24,5 @@
 /// @param value The value at the given index
 /// @param numberOfElements The index at which the value will be set
 - (void)appendValue:(float)value afterNumberOfElements:(int)numberOfElements;
+
 @end
