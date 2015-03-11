f4717c546f4d4406df76e0d7d55f9dc57d994c38
Nicholas Arner <nicholasarner@gmail.com>
2015-03-11 18:42:17 -0400

    Simplified AnalysisViewController.swift

--- Examples/iOS/Swift/AudioKitDemo/AnalysisViewController.swift
+++ Examples/iOS/Swift/AudioKitDemo/AnalysisViewController.swift
@@ -6,24 +6,6 @@
 //  Copyright (c) 2015 AudioKit. All rights reserved.
 //
 
-class AnalysisConductor {
-    var analyzer: AKAudioAnalyzer
-    var microphone: VocalInput
-    
-    init() {
-        microphone = VocalInput()
-        analyzer = AKAudioAnalyzer(audioSource: microphone.auxilliaryOutput)
-    }
-    
-    func reallyStart() {
-        AKOrchestra.addInstrument(microphone)
-        AKOrchestra.addInstrument(analyzer)
-        AKOrchestra.start()
-        analyzer.play()
-        microphone.play()
-    }
-}
-
 class AnalysisViewController: UIViewController {
     
     @IBOutlet var frequencyLabel: UILabel!
@@ -31,7 +13,9 @@ class AnalysisViewController: UIViewController {
     @IBOutlet var noteNameWithSharpsLabel: UILabel!
     @IBOutlet var noteNameWithFlatsLabel: UILabel!
     
-    let conductor = AnalysisConductor()
+    let analyzer: AKAudioAnalyzer
+    let microphone: VocalInput
+
     let noteFrequencies = [16.35,17.32,18.35,19.45,20.6,21.83,23.12,24.5,25.96,27.5,29.14,30.87]
     let noteNamesWithSharps = ["C", "C♯","D","D♯","E","F","F♯","G","G♯","A","A♯","B"]
     let noteNamesWithFlats = ["C", "D♭","D","E♭","E","F","G♭","G","A♭","A","B♭","B"]
@@ -39,10 +23,27 @@ class AnalysisViewController: UIViewController {
     let analysisSequence = AKSequence()
     let updateAnalysis = AKEvent()
     
+    override init() {
+        microphone = VocalInput()
+        analyzer = AKAudioAnalyzer(audioSource: microphone.auxilliaryOutput)
+        super.init()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        microphone = VocalInput()
+        analyzer = AKAudioAnalyzer(audioSource: microphone.auxilliaryOutput)
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
-        conductor.reallyStart()
+        AKOrchestra.addInstrument(microphone)
+        AKOrchestra.addInstrument(analyzer)
+        AKOrchestra.start()
+        analyzer.play()
+        microphone.play()
+        
         let analysisSequence = AKSequence()
         var updateAnalysis = AKEvent()
         updateAnalysis = AKEvent(block: {
@@ -61,10 +62,10 @@ class AnalysisViewController: UIViewController {
     }
     
     func updateUI() {
-        if conductor.analyzer.trackedAmplitude.value > 0.1 {
-            frequencyLabel.text = String(format: "%0.1f", conductor.analyzer.trackedFrequency.value)
+        if analyzer.trackedAmplitude.value > 0.1 {
+            frequencyLabel.text = String(format: "%0.1f", analyzer.trackedFrequency.value)
             
-            var frequency = conductor.analyzer.trackedFrequency.value
+            var frequency = analyzer.trackedFrequency.value
             while (frequency > Float(noteFrequencies[noteFrequencies.count-1])) {
                 frequency = frequency / 2.0
             }
@@ -83,12 +84,12 @@ class AnalysisViewController: UIViewController {
                 }
             }
 
-            var octave = Int(log2f(conductor.analyzer.trackedFrequency.value / frequency))
+            var octave = Int(log2f(analyzer.trackedFrequency.value / frequency))
             var noteName = String(format: "%@%d", noteNamesWithSharps[index], octave)
             noteNameWithSharpsLabel.text = noteName
             noteName = String(format: "%@%d", noteNamesWithFlats[index], octave)
             noteNameWithFlatsLabel.text = noteName
         }
-        amplitudeLabel.text = String(format: "%0.2f", conductor.analyzer.trackedAmplitude.value)
+        amplitudeLabel.text = String(format: "%0.2f", analyzer.trackedAmplitude.value)
     }
 }
