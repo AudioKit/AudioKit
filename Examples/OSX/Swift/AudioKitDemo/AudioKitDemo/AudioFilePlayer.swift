92c11addbfc8d4d0eec3b0297958416dd768aa16
Nicholas Arner <nicholasarner@gmail.com>
2015-03-12 18:20:58 -0400

    Added Processing instruments (ConvolutionInstrument.swift and AudioFilePlayer.swift)

---
+++ Examples/OSX/Swift/AudioKitDemo/AudioKitDemo/AudioFilePlayer.swift
@@ -0,0 +1,86 @@
+//
+//  AudioFilePlayer.swift
+//  AudioKitDemo
+//
+//  Created by Nicholas Arner on 3/1/15.
+//  Copyright (c) 2015 AudioKit. All rights reserved.
+//
+
+
+class AudioFilePlayer: AKInstrument {
+    
+    var auxilliaryOutput = AKAudio()
+
+    // INSTRUMENT BASED CONTROL ============================================
+    var speed       = AKInstrumentProperty(value: 1, minimum: -2, maximum: -2)
+    var scaling     = AKInstrumentProperty(value: 1, minimum: 0.0, maximum: 3.0)
+    var sampleMix   = AKInstrumentProperty(value: 0, minimum: 0, maximum: 1)
+    
+    
+    // INSTRUMENT DEFINITION ===============================================
+    override init() {
+        super.init()
+        
+        addProperty(speed)
+        addProperty(scaling)
+        addProperty(sampleMix)
+        
+        let file1 = String(NSBundle.mainBundle().pathForResource("PianoBassDrumLoop", ofType: "wav")!)
+        let file2 = String(NSBundle.mainBundle().pathForResource("808loop", ofType: "wav")!)
+        
+        let fileIn1 = AKFileInput(filename: file1)
+        fileIn1.speed = speed;
+        connect(fileIn1)
+        
+        let fileIn2 = AKFileInput(filename: file2)
+        fileIn2.speed = speed;
+        connect(fileIn2)
+
+        var fileInLeft  = AKMix(input1: fileIn1.leftOutput,  input2: fileIn2.leftOutput, balance: sampleMix)
+        connect(fileInLeft)
+        var fileInRight = AKMix(input1: fileIn1.rightOutput, input2: fileIn2.rightOutput, balance: sampleMix)
+        connect(fileInRight)
+
+        var leftF = AKFFT(
+            input: fileInLeft.scaledBy(0.25.ak),
+            fftSize: (1024.ak),
+            overlap: (256.ak),
+            windowType: AKFFTWindowType.Hamming,
+            windowFilterSize: (1024.ak)
+        )
+        connect(leftF)
+        
+        var leftR = AKFFT(
+            input: fileInRight.scaledBy(0.25.ak),
+            fftSize: (1024.ak),
+            overlap: (256.ak),
+            windowType: AKFFTWindowType.Hamming,
+            windowFilterSize: (1024.ak)
+        )
+        connect(leftR)
+        
+        var scaledLeftF = AKScaledFFT(
+            signal: leftF,
+            frequencyRatio: scaling
+        )
+        connect(scaledLeftF)
+        
+        var scaledLeftR = AKScaledFFT(
+            signal: leftR,
+            frequencyRatio: scaling
+        )
+        connect(scaledLeftR)
+
+        var scaledLeft  = AKResynthesizedAudio(signal: scaledLeftF)
+        connect(scaledLeft)
+        var scaledRight = AKResynthesizedAudio(signal: scaledLeftR)
+        connect(scaledRight)
+        
+        var mono = AKMix(input1: scaledLeft, input2: scaledRight, balance: 0.5.ak)
+        connect(mono)
+        
+        // Output to global effects processing
+        auxilliaryOutput = AKAudio.globalParameter()
+        assignOutput(auxilliaryOutput, to: mono)
+    }
+}
