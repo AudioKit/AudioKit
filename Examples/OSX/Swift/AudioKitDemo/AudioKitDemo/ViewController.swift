ea64048836eb1928c42820fdd539e0e41632f496
Nicholas Arner <nicholasarner@gmail.com>
2015-03-12 18:09:17 -0400

    Created AudioKitDemo (OSX-Swift project)

---
+++ Examples/OSX/Swift/AudioKitDemo/AudioKitDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AudioKitDemo
+//
+//  Created by Nicholas Arner on 3/12/15.
+//  Copyright (c) 2015 AudioKit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
