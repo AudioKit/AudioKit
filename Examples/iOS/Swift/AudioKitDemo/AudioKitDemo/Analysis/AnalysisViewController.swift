2939e174ec84936a13724d9065e78a7a10164ace
Nicholas Arner <nicholasarner@gmail.com>
2015-03-13 19:43:05 -0400

    Reorganized folder structure of AudioKitDemo (iOS-Swift)

---
+++ Examples/iOS/Swift/AudioKitDemo/AudioKitDemo/Analysis/AnalysisViewController.swift
@@ -0,0 +1,95 @@
+//
+//  AnalysisViewController.swift
+//  AudioKitDemo
+//
+//  Created by Nicholas Arner on 3/1/15.
+//  Copyright (c) 2015 AudioKit. All rights reserved.
+//
+
+class AnalysisViewController: UIViewController {
+    
+    @IBOutlet var frequencyLabel: UILabel!
+    @IBOutlet var amplitudeLabel: UILabel!
+    @IBOutlet var noteNameWithSharpsLabel: UILabel!
+    @IBOutlet var noteNameWithFlatsLabel: UILabel!
+    
+    let analyzer: AKAudioAnalyzer
+    let microphone: VocalInput
+
+    let noteFrequencies = [16.35,17.32,18.35,19.45,20.6,21.83,23.12,24.5,25.96,27.5,29.14,30.87]
+    let noteNamesWithSharps = ["C", "C♯","D","D♯","E","F","F♯","G","G♯","A","A♯","B"]
+    let noteNamesWithFlats = ["C", "D♭","D","E♭","E","F","G♭","G","A♭","A","B♭","B"]
+    
+    let analysisSequence = AKSequence()
+    let updateAnalysis = AKEvent()
+    
+    override init() {
+        microphone = VocalInput()
+        analyzer = AKAudioAnalyzer(audioSource: microphone.auxilliaryOutput)
+        super.init()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        microphone = VocalInput()
+        analyzer = AKAudioAnalyzer(audioSource: microphone.auxilliaryOutput)
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        AKOrchestra.addInstrument(microphone)
+        AKOrchestra.addInstrument(analyzer)
+        AKOrchestra.start()
+        analyzer.play()
+        microphone.play()
+        
+        let analysisSequence = AKSequence()
+        var updateAnalysis = AKEvent()
+        updateAnalysis = AKEvent(block: {
+            self.updateUI()
+            analysisSequence.addEvent(updateAnalysis, afterDuration: 0.1)
+        })
+        analysisSequence.addEvent(updateAnalysis)
+        analysisSequence.play()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        AKOrchestra.reset()
+        AKManager.sharedManager().stop()
+    }
+    
+    func updateUI() {
+        if analyzer.trackedAmplitude.value > 0.1 {
+            frequencyLabel.text = String(format: "%0.1f", analyzer.trackedFrequency.value)
+            
+            var frequency = analyzer.trackedFrequency.value
+            while (frequency > Float(noteFrequencies[noteFrequencies.count-1])) {
+                frequency = frequency / 2.0
+            }
+            while (frequency < Float(noteFrequencies[0])) {
+                frequency = frequency * 2.0
+            }
+            var minDistance: Float = 10000.0
+            var index = 0
+            
+            for (var i = 0; i < noteFrequencies.count; i++){
+                
+                var distance = fabsf(Float(noteFrequencies[i]) - frequency)
+                if (distance < minDistance){
+                    index = i
+                    minDistance = distance
+                }
+            }
+
+            var octave = Int(log2f(analyzer.trackedFrequency.value / frequency))
+            var noteName = String(format: "%@%d", noteNamesWithSharps[index], octave)
+            noteNameWithSharpsLabel.text = noteName
+            noteName = String(format: "%@%d", noteNamesWithFlats[index], octave)
+            noteNameWithFlatsLabel.text = noteName
+        }
+        amplitudeLabel.text = String(format: "%0.2f", analyzer.trackedAmplitude.value)
+    }
+}
---
+++ Examples/iOS/Swift/AudioKitDemo/AudioKitDemo/Processing/ProcessingViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ProcessingViewController.swift
+//  AudioKitDemo
+//
+//  Created by Nicholas Arner on 3/1/15.
+//  Copyright (c) 2015 AudioKit. All rights reserved.
+//
+
+
+class ProcessingViewController: UIViewController {
+    
+    @IBOutlet var sourceSegmentedControl: UISegmentedControl!
+    @IBOutlet var maintainPitchSwitch: UISwitch!
+    @IBOutlet var pitchSlider: UISlider!
+    
+    var pitchToMaintain:Float
+    
+    let conv: ConvolutionInstrument
+    let audioFilePlayer = AudioFilePlayer()
+    
+    
+    override init() {
+        conv = ConvolutionInstrument(input: audioFilePlayer.auxilliaryOutput)
+        pitchToMaintain = 1.0
+        super.init()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        conv = ConvolutionInstrument(input: audioFilePlayer.auxilliaryOutput)
+        pitchToMaintain = 1.0
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        AKOrchestra.addInstrument(audioFilePlayer)
+        AKOrchestra.addInstrument(conv)
+        AKOrchestra.start()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        AKOrchestra.reset()
+        AKManager.sharedManager().stop()
+    }
+    
+    @IBAction func start(sender:UIButton) {
+        conv.play()
+        audioFilePlayer.play()
+    }
+    
+    @IBAction func stop(sender:UIButton) {
+        conv.stop()
+        audioFilePlayer.stop()
+    }
+    
+    @IBAction func wetnessChanged(sender:UISlider) {
+        AKTools.setProperty(conv.dryWetBalance, withSlider: sender)
+    }
+    
+    @IBAction func impulseResponseChanged(sender:UISlider) {
+        AKTools.setProperty(conv.dishWellBalance, withSlider: sender)
+    }
+    
+    @IBAction func speedChanged(sender:UISlider) {
+        AKTools.setProperty(audioFilePlayer.speed, withSlider: sender)
+        if (maintainPitchSwitch.on && fabs(audioFilePlayer.speed.value) > 0.1) {
+            audioFilePlayer.scaling.value = pitchToMaintain / fabs(audioFilePlayer.speed.value)
+            AKTools.setSlider(pitchSlider, withProperty: audioFilePlayer.scaling)
+        }
+    }
+    
+    @IBAction func pitchChanged(sender:UISlider) {
+        AKTools.setProperty(audioFilePlayer.scaling, withSlider: sender)
+    }
+    
+    @IBAction func togglePitchMaintenance(sender:UISwitch) {
+        if sender.on {
+            pitchSlider.enabled = false
+            pitchToMaintain = fabs(audioFilePlayer.speed.value) * audioFilePlayer.scaling.value
+        } else {
+            pitchSlider.enabled = true
+        }
+    }
+    
+    @IBAction func fileChanged(sender:UISegmentedControl) {
+        audioFilePlayer.sampleMix.value = Float(sender.selectedSegmentIndex)
+    }
+}
