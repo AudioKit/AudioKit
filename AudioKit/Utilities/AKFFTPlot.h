e3e784330ae5608b455a219e44485d68674b6ef8
Aurelius Prochazka <aure@aure.com>
2015-03-14 02:32:43 -0700

    Introducing AKFFTPlot

---
+++ AudioKit/Utilities/AKFFTPlot.h
@@ -0,0 +1,15 @@
+//
+//  AKFFTPlot.h
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 2/8/15.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CsoundObj.h"
+#import <Accelerate/Accelerate.h>
+
+@interface AKFFTPlot : UIView <CsoundBinding>
+
+@end
