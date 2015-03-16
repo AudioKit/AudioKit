15a8b11364b996e4d1c9113593a94fb8596f4bc8
Aurelius Prochazka <aure@aure.com>
2015-03-15 23:43:04 -0700

    Introducing the AudioKit Playground

---
+++ Playground/Playground/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Playground
+//
+//  Created by Aurelius Prochazka on 3/15/15.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Playground/Playground/Playgrounds/Playground.h
@@ -0,0 +1,13 @@
+//
+//  Playground.h
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 3/15/15.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+
+#import "AKPlayground.h"
+
+@interface Playground : AKPlayground <KZPActivePlayground>
+
+@end
