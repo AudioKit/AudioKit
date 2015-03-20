6e9df4901e8d70f97ac6e0b5d0588592fd786270
Aurelius Prochazka <aure@aure.com>
2015-03-20 15:03:23 -0700

    More default instruments

--- AudioKit/Utilities/Instruments/Amplifier.m
+++ AudioKit/Utilities/Instruments/Amplifier.m
@@ -1,14 +1,14 @@
 //
-//  ReverbProcessor.m
+//  Amplifier.m
 //  AudioKit
 //
 //  Created by Aurelius Prochazka on 3/20/15.
 //  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
 //
 
-#import "ReverbProcessor.h"
+#import "Amplifier.h"
 
-@implementation ReverbProcessor
+@implementation Amplifier
 
 - (instancetype)initWithAudioSource:(AKAudio *)audioSource
 {
@@ -16,14 +16,10 @@
     if (self) {
 
         // Instrument Properties
-        _feedback = [self createPropertyWithValue:0.0 minimum:0.0 maximum:1.0];
-
-        // Instrument Definition
-        AKReverb *reverb = [AKReverb reverbWithInput:audioSource];
-        reverb.feedback = _feedback;
+        _amplitude = [self createPropertyWithValue:1.0 minimum:0.0 maximum:2.0];
 
         // Audio Output
-        [self setStereoAudioOutput:reverb];
+        [self setAudioOutput:[audioSource scaledBy:_amplitude]];
 
         // Reset Inputs
         [self resetParameter:audioSource];
