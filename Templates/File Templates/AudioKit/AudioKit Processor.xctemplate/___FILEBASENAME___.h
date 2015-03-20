28d9b25547e58574efb1d72c3dccdd40f5b56397
Aurelius Prochazka <aure@aure.com>
2015-03-20 14:27:42 -0700

    Updated templates for latest syntax

--- Templates/File Templates/AudioKit/AudioKit Processor.xctemplate/___FILEBASENAME___.h
+++ Templates/File Templates/AudioKit/AudioKit Processor.xctemplate/___FILEBASENAME___.h
@@ -6,9 +6,11 @@
 //___COPYRIGHT___
 //
 
+#import "AKFoundation.h"
+
 @interface ___FILEBASENAMEASIDENTIFIER___ : AKInstrument
 
-@property (nonatomic) AKInstrumentProperty *reverb;
+@property (nonatomic) AKInstrumentProperty *feedback;
 
 - (instancetype)initWithAudioSource:(AKAudio *)audioSource;
 
