2b3e66a9f80191db3f9fe0b6b740eecffe63b139
Nicholas Arner <nicholasarner@gmail.com>
2015-03-21 15:09:48 -0400

    Added Sleighbells Instrument

---
+++ AudioKit/Utilities/Instruments/Sleighbells.h
@@ -0,0 +1,33 @@
+//
+//  Sleighbells.h
+//  AudioKitPlayground
+//
+//  Created by Nicholas Arner on 3/21/15.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+
+#import "AKFoundation.h"
+
+@interface Sleighbells : AKInstrument
+
+// Instrument Properties
+@property AKInstrumentProperty *amplitude;
+
+// Audio outlet for global effects processing (choose mono or stereo accordingly)
+@property (readonly) AKAudio *auxilliaryOutput;
+//@property (readonly) AKStereoAudio *auxilliaryOutput;
+
+@end
+
+@interface SleighbellsNote : AKNote
+
+// Note properties
+@property AKNoteProperty *intensity;
+@property AKNoteProperty *dampingFactor;
+@property AKNoteProperty *mainResonantFrequency;
+@property AKNoteProperty *firstResonantFrequency;
+@property AKNoteProperty *secondResonantFrequency;
+@property AKNoteProperty *amplitude;
+
+
+@end
