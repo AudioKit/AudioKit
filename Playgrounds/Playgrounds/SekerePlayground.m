3bbe31d51c382cb32425ff95e65ed692b539d2e3
Nicholas Arner <nicholasarner@gmail.com>
2015-03-21 15:52:49 -0400

    Added Sekere Playground

---
+++ Playgrounds/Playgrounds/SekerePlayground.m
@@ -0,0 +1,51 @@
+//
+//  SekerePlayground.m
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 3/15/15.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+
+#import "Playground.h"
+#import "Sekere.h"
+
+@implementation Playground {
+    Sekere *sekere;
+    SekereNote *note;
+}
+
+- (void) setup
+{
+    [super setup];
+    sekere = [[Sekere alloc] init];
+    [AKOrchestra addInstrument:sekere];
+}
+
+- (void)run
+{
+    [super run];
+    [self addStereoAudioOutputPlot];
+    note = [[SekereNote alloc] init];
+    AKPlaygroundButton(@"Play Once", [sekere playNote:note];);
+    
+    AKPlaygroundPropertySlider(volume, sekere.amplitude);
+    
+    AKPhrase *phrase = [[AKPhrase alloc] init];
+    [phrase addNote:note];
+    
+    
+    [self makeSection:@"Repeat Frequency"];
+    [self addRepeatSliderForInstrument:sekere
+                                phrase:phrase
+                      minimumFrequency:1.0f
+                      maximumFrequency:25.0f];
+    
+    AKPlaygroundButton(@"Stop Loop",  [sekere stopPhrase];);
+    
+    [self makeSection:@"Parameters"];
+    AKPlaygroundPropertySlider(count, note.count);
+    AKPlaygroundPropertySlider(dampingFactor, note.dampingFactor);
+    AKPlaygroundPropertySlider(amplitude, note.amplitude);
+}
+
+@end
