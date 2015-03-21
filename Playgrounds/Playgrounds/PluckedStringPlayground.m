d4b08e6460e6052cc422f2b335b6f19d9c0ce874
Nicholas Arner <nicholasarner@gmail.com>
2015-03-21 16:06:00 -0400

    Corrected attribution/time-stamps

--- Playgrounds/Playgrounds/PluckedStringPlayground.m
+++ Playgrounds/Playgrounds/PluckedStringPlayground.m
@@ -2,7 +2,7 @@
 //  PluckedString.m
 //  AudioKit
 //
-//  Created by Aurelius Prochazka on 3/15/15.
+//  Created by Nick Arner on 3/21/15.
 //  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
 //
 
