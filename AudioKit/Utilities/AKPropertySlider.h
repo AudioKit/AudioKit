58c0b487b186964fe91d8d621a280e8633fefd7e
Aurelius Prochazka <aure@aure.com>
2015-03-22 18:17:43 -0700

    Added AKPropertySlider to make quick work of creating sliders tied to properties

---
+++ AudioKit/Utilities/AKPropertySlider.h
@@ -0,0 +1,16 @@
+//
+//  AKNotePropertySLider.h
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 3/22/15.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AKFoundation.h"
+
+@interface AKPropertySlider : UISlider
+
+@property (nonatomic) id property;
+
+@end
