ff747fcf13a735e7bb4ee8ab754935b960809971
Aurelius Prochazka <aure@aure.com>
2015-03-22 22:41:50 -0700

    Moved UI utilities into their own folder

--- AudioKit/Utilities/User Interface Elements/AKPropertySlider.h
+++ AudioKit/Utilities/User Interface Elements/AKPropertySlider.h
@@ -6,10 +6,16 @@
 //  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import "AKFoundation.h"
+#import <Foundation/Foundation.h>
 
+#if TARGET_OS_IPHONE
+#import <UIKit/UIKit.h>
 @interface AKPropertySlider : UISlider
+#elif TARGET_OS_MAC
+#import <Cocoa/Cocoa.h>
+@interface AKPropertySlider : NSSlider
+#endif
+
 
 @property (nonatomic) id property;
 
