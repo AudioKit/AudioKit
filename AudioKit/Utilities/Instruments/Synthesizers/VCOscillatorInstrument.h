bfa76fce1495e051c4b044b54e9775a3030136a6
Aurelius Prochazka <aure@aure.com>
2015-03-22 22:42:52 -0700

    Create a VCOscillatorInstrument

---
+++ AudioKit/Utilities/Instruments/Synthesizers/VCOscillatorInstrument.h
@@ -0,0 +1,20 @@
+//
+//  VCOscillatorInstrument.h
+//  AudioKitPlayground
+//
+//  Created by Aurelius Prochazka on 3/22/15.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+
+#import "AKFoundation.h"
+
+@interface VCOscillatorInstrument : AKInstrument
+@property AKInstrumentProperty *amplitude;
+@end
+
+
+
+@interface VCOscillatorNote : AKNote
+@property AKNoteProperty *frequency;
+@property AKNoteProperty *waveformType;
+@end
--- Playgrounds/Playgrounds/VCOscillatorPlayground.m
+++ Playgrounds/Playgrounds/VCOscillatorPlayground.m
@@ -7,96 +7,43 @@
 //
 
 #import "Playground.h"
+#import "VCOscillatorInstrument.h"
 
-@interface Instrument : AKInstrument
-@property AKInstrumentProperty *frequency;
-@end
-
-@interface Note : AKNote
-@property AKNoteProperty *waveformType;
-@end
-
-@implementation Instrument
-
-- (instancetype)init
-{
-    self = [super init];
-    if (self) {
-        _frequency = [[AKInstrumentProperty alloc] initWithValue:323.0*2 minimum:0.0 maximum:2000.0];
-        Note *note = [[Note alloc] init];
-        AKVCOscillator *vco = [AKVCOscillator oscillator];
-        vco.frequency = _frequency;
-        vco.amplitude = akp(0.5);
-        vco.frequency = _frequency;
-        vco.waveformType = note.waveformType;
-        [self setAudioOutput:vco];
-    }
-    return self;
+@implementation Playground {
+    VCOscillatorInstrument *vcoInstrument;
 }
-@end
-
-@implementation Note
-
-- (instancetype)init
-{
-    self = [super init];
-    if (self) {
-        _waveformType = [self createPropertyWithValue:0 minimum:0 maximum:100];
-    }
-    return self;
-}
-- (instancetype)initWithWaveformType:(AKConstant *)waveformType
-{
-    self = [self init];
-    if (self) {
-        _waveformType.value = waveformType.value;
-    }
-    return self;
-}
-
-@end
-
-@implementation Playground
 
 - (void) setup
 {
     [super setup];
+    vcoInstrument = [[VCOscillatorInstrument alloc] init];
+    [AKOrchestra addInstrument:vcoInstrument];
 }
 
 - (void)run
 {
     [super run];
     [self addAudioOutputPlot];
+    VCOscillatorNote *note = [[VCOscillatorNote alloc] init];
 
-    Instrument *instrument = [[Instrument alloc] init];
-    [AKOrchestra addInstrument:instrument];
+    [self addSliderForProperty:vcoInstrument.amplitude title:@"Amplitude"];
+    [self addSliderForProperty:note.frequency title:@"Frequency"];
 
-    [self addSliderForProperty:instrument.frequency title:@"Frequency"];
-
-    Note *note = [[Note alloc] init];
 
     [self addButtonWithTitle:@"Play Triangle Wave" block:^{
         note.waveformType.value = [[AKVCOscillator waveformTypeForTriangle] value];
-        [instrument playNote:note];
-    }];
-    [self addButtonWithTitle:@"Play Triangle WithRamp" block:^{
-        note.waveformType.value = [[AKVCOscillator waveformTypeForTriangleWithRamp] value];
-        [instrument playNote:note];
+        [vcoInstrument playNote:note];
     }];
     [self addButtonWithTitle:@"Play Sawtooth Wave" block:^{
         note.waveformType.value = [[AKVCOscillator waveformTypeForSawtooth] value];
-        [instrument playNote:note];
-    }];
-    [self addButtonWithTitle:@"Play Square with PWM" block:^{
-        note.waveformType.value = [[AKVCOscillator waveformTypeForSquareWithPWM] value];
-        [instrument playNote:note];
+        [vcoInstrument playNote:note];
     }];
     [self addButtonWithTitle:@"Play Integrated" block:^{
         note.waveformType.value = [[AKVCOscillator waveformTypeForIntegratedSawtooth] value];
-        [instrument playNote:note];
+        [vcoInstrument playNote:note];
     }];
     [self addButtonWithTitle:@"Stop" block:^{
-        [instrument stop];
+        [vcoInstrument stop];
     }];
 }
 
