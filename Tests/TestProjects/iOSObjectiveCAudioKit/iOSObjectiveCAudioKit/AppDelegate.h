652ba8a90ced2066a776893a0c31e58f652e296d
Aurelius Prochazka <aure@aure.com>
2015-04-18 22:58:32 -0700

    Initial project with unit testing of AudioKit with XCTest

---
+++ Tests/TestProjects/iOSObjectiveCAudioKit/iOSObjectiveCAudioKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSObjectiveCAudioKit
+//
+//  Created by Aurelius Prochazka on 4/18/15.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tests/TestProjects/iOSObjectiveCAudioKit/iOSObjectiveCAudioKit/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  iOSObjectiveCAudioKit
+//
+//  Created by Aurelius Prochazka on 4/18/15.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    _mathTestInstrument = [[MathTestInstrument alloc] init];
+    [AKOrchestra addInstrument:_mathTestInstrument];
+    [_mathTestInstrument start];
+    
+    _tableTestInstrument= [[TableTestInstrument alloc] init];
+    [AKOrchestra addInstrument:_tableTestInstrument];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
