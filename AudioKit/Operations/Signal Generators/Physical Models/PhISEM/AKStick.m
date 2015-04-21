49afba6af743b0e4510f2d8a286e58a4e6b03f46
Nicholas Arner <nicholasarner@gmail.com>
2015-04-21 07:25:27 -0400

    Added presetThickStick to AKStick

--- AudioKit/Operations/Signal Generators/Physical Models/PhISEM/AKStick.m
+++ AudioKit/Operations/Signal Generators/Physical Models/PhISEM/AKStick.m
@@ -64,6 +64,24 @@
     return [[AKStick alloc] initWithPresetBundleOfSticks];
 }
 
+- (instancetype)initWithPresetThickStick
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        // Default Values
+        _intensity = akp(1000);
+        _dampingFactor = akp(1);
+        _amplitude = akp(1);
+        [self setUpConnections];
+    }
+    return self;
+}
+
++ (instancetype)presetThickStick
+{
+    return [[AKStick alloc] initWithPresetThickStick];
+}
+
 - (void)setIntensity:(AKConstant *)intensity {
     _intensity = intensity;
     [self setUpConnections];
