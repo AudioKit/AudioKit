0f6e77e0b3d18311d957f072e1f0d0ba73ad7a11
Stephane Peter <megastep@megastep.org>
2015-04-27 15:03:44 -0700

    Consolidated AKAudio input and output plot code into new parent AKAudioPlot class.

--- AudioKit/Utilities/Plots/AKAudioOutputPlot.h
+++ AudioKit/Utilities/Plots/AKAudioOutputPlot.h
@@ -6,17 +6,10 @@
 //  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
 //
 
-#import "AKPlotView.h"
+#import "AKAudioPlot.h"
 
 /// Plot the raw samples of the audio output to the DAC
 IB_DESIGNABLE
-@interface AKAudioOutputPlot : AKPlotView
-
-#if TARGET_OS_IPHONE
-@property IBInspectable UIColor *lineColor;
-#else
-@property IBInspectable NSColor *lineColor;
-#endif
-@property IBInspectable CGFloat lineWidth;
+@interface AKAudioOutputPlot : AKAudioPlot
 
 @end
