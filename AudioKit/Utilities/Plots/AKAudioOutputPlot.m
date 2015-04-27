d65aac7b10dc995eb4607490a51712f061975e6e
Stephane Peter <megastep@megastep.org>
2015-04-27 15:03:44 -0700

    Consolidated AKAudio input and output plot code into new parent AKAudioPlot class.

--- AudioKit/Utilities/Plots/AKAudioOutputPlot.m
+++ AudioKit/Utilities/Plots/AKAudioOutputPlot.m
@@ -11,85 +11,18 @@
 #import "AKSettings.h"
 #import "CsoundObj.h"
 
-@interface AKAudioOutputPlot() <CsoundBinding>
-{
-    NSData *outSamples;
-    UInt32 sampleSize;
-    int index;
-    CsoundObj *cs;
-}
-@end
 
 @implementation AKAudioOutputPlot
 
 - (void)defaultValues
 {
-    _lineWidth = 4.0f;
-    _lineColor = [AKColor greenColor];
+    [super defaultValues];
+    self.lineColor = [AKColor greenColor];
 }
 
-- (void)drawRect:(CGRect)rect
+- (NSData *)bufferWithCsound:(CsoundObj *)cs
 {
-    int plotPoints = sampleSize / 2;
-    // Draw waveform
-    AKBezierPath *wavePath = [AKBezierPath bezierPath];
-    
-    CGFloat yScale  = self.bounds.size.height / 2;
-    
-    CGFloat deltaX = (self.frame.size.width / plotPoints);
-    
-    CGFloat x = 0.0f;
-    CGFloat y = 0.0f;
-    
-    @synchronized(self) {
-        const MYFLT *samples = (const MYFLT *)outSamples.bytes;
-        for (int i = 0; i < plotPoints; i++) {
-//            y = AK_CLAMP(y, -1.0f, 1.0f);
-            y = (samples[(i * 2)]+1.0) * yScale;
-            if (i == 0) {
-                [wavePath moveToPoint:CGPointMake(x, y)];
-            } else {
-#if TARGET_OS_IPHONE
-                [wavePath addLineToPoint:CGPointMake(x, y)];
-#elif TARGET_OS_MAC
-                [wavePath lineToPoint:CGPointMake(x, y)];
-#endif
-            }
-            x += deltaX;
-        };
-    }
-    
-    [wavePath setLineWidth:self.lineWidth];
-    [self.lineColor setStroke];
-    [wavePath stroke];
+    return [cs getOutSamples];
 }
-
-//        y = AK_CLAMP(samples[i*2], -1.0f, 1.0f);
-//        y = self.bounds.size.height * (y + 1.0) / 2.0;
-
-// -----------------------------------------------------------------------------
-# pragma mark - CsoundBinding
-// -----------------------------------------------------------------------------
-
-- (void)setup:(CsoundObj *)csoundObj
-{
-    cs = csoundObj;
-    
-    sampleSize = AKSettings.settings.numberOfChannels * AKSettings.settings.samplesPerControlPeriod;
-
-    void *samples = malloc(sampleSize * sizeof(MYFLT));
-    bzero(samples, sampleSize * sizeof(MYFLT));
-    outSamples = [NSData dataWithBytesNoCopy:samples length:sampleSize * sizeof(MYFLT)];
-}
-
-- (void)updateValuesFromCsound
-{
-    @synchronized(self) {
-        outSamples = [cs getOutSamples];
-    }
-
-    [self performSelectorOnMainThread:@selector(updateUI) withObject:nil waitUntilDone:NO];
-}
-
 
 @end
