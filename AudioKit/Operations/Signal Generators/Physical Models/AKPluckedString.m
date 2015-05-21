129126166e1609569689949d1b416af2735f22b1
Nicholas Arner <nicholasarner@gmail.com>
2015-05-20 21:09:22 -0400

    Added presetSnappyPluckedString

--- AudioKit/Operations/Signal Generators/Physical Models/AKPluckedString.m
+++ AudioKit/Operations/Signal Generators/Physical Models/AKPluckedString.m
@@ -129,6 +129,33 @@
     return [[AKPluckedString alloc] initWithPresetRoundedPluckedString];
 }
 
+- (instancetype)initWithPresetSnappyPluckedString
+{
+    self = [super initWithString:[self operationName]];
+    if (self) {
+        // Default Values
+        _frequency = akp(440);
+        _pluckPosition = akp(0.5);
+        _samplePosition = akp(0.05);
+        _reflectionCoefficient = akp(0.5);
+        _amplitude = akp(0.5);
+        
+        // Constant Values
+        NSString *file = [AKManager pathToSoundFile:@"marmstk1" ofType:@"wav"];
+        AKSoundFileTable *strikeImpulseTable;
+        strikeImpulseTable = [[AKSoundFileTable alloc] initWithFilename:file];
+        _excitationSignal = [[AKMonoSoundFileLooper alloc] initWithSoundFile:strikeImpulseTable];
+        _excitationSignal.loopMode = [AKMonoSoundFileLooper loopPlaysOnce];
+        [self setUpConnections];
+    }
+    return self;
+}
+
++ (instancetype)presetSnappyPluckedString
+{
+    return [[AKPluckedString alloc] initWithPresetSnappyPluckedString];
+}
+
 - (void)setFrequency:(AKConstant *)frequency {
     _frequency = frequency;
     [self setUpConnections];
