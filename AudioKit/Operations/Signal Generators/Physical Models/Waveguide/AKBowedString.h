0e335652a2f1e862670c1df362cd98fbd603230c
Nicholas Arner <nicholasarner@gmail.com>
2015-05-21 21:06:03 -0400

    Added bowed cello preset

--- AudioKit/Operations/Signal Generators/Physical Models/Waveguide/AKBowedString.h
+++ AudioKit/Operations/Signal Generators/Physical Models/Waveguide/AKBowedString.h
@@ -55,6 +55,12 @@ NS_ASSUME_NONNULL_BEGIN
 /// Instantiates the bowed string with a sound like a train whistle
 + (instancetype)presetTrainWhislteBowedString;
 
+/// Instantiates the bowed string with a cello sound
+- (instancetype)initWithPresetCelloBowedString;
+
+/// Instantiates the bowed string with a cello sound
++ (instancetype)presetCelloBowedString;
+
 
 /// Frequency of the note played, note that this will be lowest allowable frequency unless the optional minimum frequency is set. [Default Value: 110]
 @property (nonatomic) AKParameter *frequency;
