b032a6bda1cf9d3ab602fde4ec1c49cca9f9ecc0
Nicholas Arner <nicholasarner@gmail.com>
2015-05-24 17:57:25 -0400

    Added 'short attack delay' preset

--- AudioKit/Operations/Signal Modifiers/Delays/AKDelay.h
+++ AudioKit/Operations/Signal Modifiers/Delays/AKDelay.h
@@ -48,18 +48,26 @@ NS_ASSUME_NONNULL_BEGIN
 + (instancetype)defaultDelayWithInput:(AKParameter *)input
                      delayTime:(AKConstant *)delayTime;
 
-/// Instantiates the delay with default values
+/// Instantiates the delay with 'chopped' values
 /// @param input Input signal, usually audio.
 /// @param delayTime Requested delay time in seconds.
 - (instancetype)initChoppedDelayWithInput:(AKParameter *)input
                                 delayTime:(AKConstant *)delayTime;
 
-/// Instantiates the delay with default values
+/// Instantiates the delay with 'chopped' values
 /// @param input Input signal, usually audio.
 /// @param delayTime Requested delay time in seconds.
-/// @param feedback How much of the signal is sent back into the delay line.  Usually values range from 0-1. Updated at Control-rate. [Default Value: 0.5]
 + (instancetype)choppedDelayWithInput:(AKParameter *)input
                             delayTime:(AKConstant *)delayTime;
+
+/// Instantiates the delay with 'short attack' values
+/// @param input Input signal, usually audio.
+- (instancetype)initShortAttackDelayWithInput:(AKParameter *)input;
+
+/// Instantiates the delay with 'chopped' values
+/// @param input Input signal, usually audio.
++ (instancetype)shortAttackDelayWithInput:(AKParameter *)input;
+
 
 /// How much of the signal is sent back into the delay line.  Usually values range from 0-1. [Default Value: 0.0]
 @property (nonatomic) AKParameter *feedback;
