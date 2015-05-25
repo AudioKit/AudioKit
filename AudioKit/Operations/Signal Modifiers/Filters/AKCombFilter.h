eefc2d1c5794439cc3010c30684b27b84527cc28
Nicholas Arner <nicholasarner@gmail.com>
2015-05-24 21:19:59 -0400

    Inserted 'springy' comb filter preset

--- AudioKit/Operations/Signal Modifiers/Filters/AKCombFilter.h
+++ AudioKit/Operations/Signal Modifiers/Filters/AKCombFilter.h
@@ -32,6 +32,23 @@ NS_ASSUME_NONNULL_BEGIN
 /// @param input Input signal, usually audio.
 + (instancetype)filterWithInput:(AKParameter *)input;
 
+/// Instantiates the comb filter with default values
+/// @param input Input signal, usually audio.
+- (instancetype)initDefaultFilterWithInput:(AKParameter *)input;
+
+/// Instantiates the comb filter with default values
+/// @param input Input signal, usually audio.
++ (instancetype)defaultFilterWithInput:(AKParameter *)input;
+
+/// Instantiates the comb filter with a 'springy' sound
+/// @param input Input signal, usually audio.
+- (instancetype)initSpringyFilterWithInput:(AKParameter *)input;
+
+/// Instantiates the comb filter with a 'springy' sound
+/// @param input Input signal, usually audio.
++ (instancetype)defaultSpringFilterWithInput:(AKParameter *)input;
+
+
 /// The time in seconds for a signal to decay to 1/1000, or 60dB from its original amplitude. [Default Value: 1]
 @property (nonatomic) AKParameter *reverbDuration;
 
