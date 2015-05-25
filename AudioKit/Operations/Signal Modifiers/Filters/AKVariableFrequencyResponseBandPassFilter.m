f90e2b08f0ec2367e377dfc9fa91403e8c3d8a01
Aurelius Prochazka <aure@aure.com>
2015-05-25 00:01:06 -0700

    Standardization of "input" over audioSource

--- AudioKit/Operations/Signal Modifiers/Filters/AKVariableFrequencyResponseBandPassFilter.m
+++ AudioKit/Operations/Signal Modifiers/Filters/AKVariableFrequencyResponseBandPassFilter.m
@@ -16,21 +16,21 @@
 
 @implementation AKVariableFrequencyResponseBandPassFilter
 {
-    AKParameter * _audioSource;
+    AKParameter * _input;
 }
 
 + (AKConstant *)scalingFactorNone { return akp(0); }
 + (AKConstant *)scalingFactorPeak { return akp(1); }
 + (AKConstant *)scalingFactorRMS  { return akp(2); }
 
-- (instancetype)initWithAudioSource:(AKParameter *)audioSource
+- (instancetype)initWithInput:(AKParameter *)input
                     cutoffFrequency:(AKParameter *)cutoffFrequency
                           bandwidth:(AKParameter *)bandwidth
                       scalingFactor:(AKConstant *)scalingFactor
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        _audioSource = audioSource;
+        _input = input;
         _cutoffFrequency = cutoffFrequency;
         _bandwidth = bandwidth;
         _scalingFactor = scalingFactor;
@@ -39,11 +39,11 @@
     return self;
 }
 
-- (instancetype)initWithAudioSource:(AKParameter *)audioSource
+- (instancetype)initWithInput:(AKParameter *)input
 {
     self = [super initWithString:[self operationName]];
     if (self) {
-        _audioSource = audioSource;
+        _input = input;
         // Default Values
         _cutoffFrequency = akp(1000);
         _bandwidth = akp(10);
@@ -53,9 +53,9 @@
     return self;
 }
 
-+ (instancetype)filterWithAudioSource:(AKParameter *)audioSource
++ (instancetype)filterWithInput:(AKParameter *)input
 {
-    return [[AKVariableFrequencyResponseBandPassFilter alloc] initWithAudioSource:audioSource];
+    return [[AKVariableFrequencyResponseBandPassFilter alloc] initWithInput:input];
 }
 
 - (void)setCutoffFrequency:(AKParameter *)cutoffFrequency {
@@ -89,7 +89,7 @@
 - (void)setUpConnections
 {
     self.state = @"connectable";
-    self.dependencies = @[_audioSource, _cutoffFrequency, _bandwidth, _scalingFactor];
+    self.dependencies = @[_input, _cutoffFrequency, _bandwidth, _scalingFactor];
 }
 
 - (NSString *)inlineStringForCSD
@@ -117,10 +117,10 @@
     NSMutableString *inputsString = [[NSMutableString alloc] init];
 
     
-    if ([_audioSource class] == [AKAudio class]) {
-        [inputsString appendFormat:@"%@, ", _audioSource];
+    if ([_input class] == [AKAudio class]) {
+        [inputsString appendFormat:@"%@, ", _input];
     } else {
-        [inputsString appendFormat:@"AKAudio(%@), ", _audioSource];
+        [inputsString appendFormat:@"AKAudio(%@), ", _input];
     }
 
     if ([_cutoffFrequency class] == [AKControl class]) {
