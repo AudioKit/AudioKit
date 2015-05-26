f2fcab5db1bd7ead7619b4fe87e68be2e94c5336
Aurelius Prochazka <aure@aure.com>
2015-05-25 22:31:55 -0700

    Removed local reference

--- Tests/TestProjects/Shared/AKSekereTests.m
+++ Tests/TestProjects/Shared/AKSekereTests.m
@@ -83,7 +83,6 @@
     
     // Render audio output
     NSString *outputFile = [NSString stringWithFormat:@"%@/AKTest-Sekere.aiff", NSTemporaryDirectory()];
-    outputFile = @"/Users/aure/test.aiff";
     [[AKManager sharedManager] renderToFile:outputFile forDuration:testDuration];
     
     // Check output
