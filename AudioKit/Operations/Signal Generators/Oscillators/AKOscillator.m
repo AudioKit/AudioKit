902cb85fb0b3188bf4d30f3620d3df9f3c3ff6bc
Aurelius Prochazka <aure@aure.com>
2015-05-26 14:10:34 -0700

    Since oscillators are used for more than just sound output, making the default amplitude 1 again.

--- AudioKit/Operations/Signal Generators/Oscillators/AKOscillator.m
+++ AudioKit/Operations/Signal Generators/Oscillators/AKOscillator.m
@@ -36,7 +36,7 @@
         _waveform = [AKTable standardSineWave];
     
         _frequency = akp(440);
-        _amplitude = akp(0.5);
+        _amplitude = akp(1);
         [self setUpConnections];
     }
     return self;
