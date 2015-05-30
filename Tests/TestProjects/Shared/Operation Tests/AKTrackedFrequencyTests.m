00bdb11a8ea4beaf80927965c2582fa9af150852
Aurelius Prochazka <aure@aure.com>
2015-05-29 22:19:13 -0700

    More tests

---
+++ Tests/TestProjects/Shared/Operation Tests/AKTrackedFrequencyTests.m
@@ -0,0 +1,73 @@
+//
+//  AKTrackedFrequencyTests.m
+//  iOSObjectiveCAudioKit
+//
+//  Created by Aurelius Prochazka on 5/22/15.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+
+#import "AKTestCase.h"
+
+#define testDuration 10.0
+
+@interface TestTrackedFrequencyInstrument : AKInstrument
+@end
+
+@implementation TestTrackedFrequencyInstrument
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+        AKLine *frequencyLine = [[AKLine alloc] initWithFirstPoint:akp(110)
+                                                       secondPoint:akp(880)
+                                             durationBetweenPoints:akp(testDuration)];
+        
+        
+        AKOscillator *frequencyDeviation = [AKOscillator oscillator];
+        frequencyDeviation.frequency = akp(1);
+        frequencyDeviation.amplitude = akp(30);
+        
+        AKOscillator *sine = [AKOscillator oscillator];
+        sine.frequency = [frequencyLine plus:frequencyDeviation];
+        
+        AKTrackedFrequency *tracker = [[AKTrackedFrequency alloc] initWithAudioSource:sine
+                                                                           sampleSize:akp(512)];
+        
+        AKOscillator *trackedSine = [AKOscillator oscillator];
+        trackedSine.frequency = tracker;
+        
+        AKStereoAudio *output = [[AKStereoAudio alloc] initWithLeftAudio:sine rightAudio:trackedSine];
+        
+        [self setStereoAudioOutput:output];
+    }
+    return self;
+}
+
+@end
+
+@interface AKTrackedFrequencyTests : AKTestCase
+@end
+
+@implementation AKTrackedFrequencyTests
+
+- (void)testTrackedFrequency
+{
+    // Set up performance
+    TestTrackedFrequencyInstrument *testInstrument = [[TestTrackedFrequencyInstrument alloc] init];
+    [AKOrchestra addInstrument:testInstrument];
+    [testInstrument playForDuration:testDuration];
+    
+    // Render audio output
+    NSString *outputFile = [NSString stringWithFormat:@"%@/AKTest-TrackedFrequency.aiff", NSTemporaryDirectory()];
+    [[AKManager sharedManager] renderToFile:outputFile forDuration:testDuration];
+    
+    // Check output
+    NSData *nsData = [NSData dataWithContentsOfFile:outputFile];
+    NSArray *validMD5s = @[@"3bb1ff85a24195d89ce2df34e0c78709",
+                           @"180d9fbeae818be4660174e7cd53d4e5"];
+    XCTAssertTrue([validMD5s containsObject:[nsData MD5]]);
+}
+
+@end
