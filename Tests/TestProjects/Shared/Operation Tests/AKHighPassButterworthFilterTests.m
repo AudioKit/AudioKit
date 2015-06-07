9d62eb33bfa694b3c7da8dbfad54aacd98e53fc9
Nicholas Arner <nicholasarner@gmail.com>
2015-06-07 10:37:53 -0400

    Added AKHighPassButterworthFilter preset tests

--- Tests/TestProjects/Shared/Operation Tests/AKHighPassButterworthFilterTests.m
+++ Tests/TestProjects/Shared/Operation Tests/AKHighPassButterworthFilterTests.m
@@ -55,4 +55,38 @@
     XCTAssertEqualObjects([self md5ForOutputWithDuration:testDuration], @"43b7b629cb436b983a2f3803bbcd918a");
 }
 
+- (void)testPresetExtremeFilterWithInput
+{
+    NSString *filename = [AKManager pathToSoundFile:@"PianoBassDrumLoop" ofType:@"wav"];
+    AKFileInput *audio = [[AKFileInput alloc] initWithFilename:filename];
+    audio.loop = YES;
+    AKMix *mono = [[AKMix alloc] initMonoAudioFromStereoInput:audio];
+    
+    AKInstrument *testInstrument = [AKInstrument instrument];
+    AKHighPassButterworthFilter *presetFilter = [AKHighPassButterworthFilter presetExtremeFilterWithInput:mono];
+    [testInstrument setAudioOutput:presetFilter];
+    [AKOrchestra addInstrument:testInstrument];
+    [testInstrument play];
+    
+    // Check output
+    XCTAssertEqualObjects([self md5ForOutputWithDuration:2.0], @"80af5ea17b1ab8ba6d7ecc3d348cf24b");
+}
+
+- (void)testPresetModerateFilterWithInput
+{
+    NSString *filename = [AKManager pathToSoundFile:@"PianoBassDrumLoop" ofType:@"wav"];
+    AKFileInput *audio = [[AKFileInput alloc] initWithFilename:filename];
+    audio.loop = YES;
+    AKMix *mono = [[AKMix alloc] initMonoAudioFromStereoInput:audio];
+    
+    AKInstrument *testInstrument = [AKInstrument instrument];
+    AKHighPassButterworthFilter *presetFilter = [AKHighPassButterworthFilter presetModerateFilterWithInput:mono];
+    [testInstrument setAudioOutput:presetFilter];
+    [AKOrchestra addInstrument:testInstrument];
+    [testInstrument play];
+    
+    // Check output
+    XCTAssertEqualObjects([self md5ForOutputWithDuration:2.0], @"40bcc339b8addb9e655d7f8b2b960e62");
+}
+
 @end
