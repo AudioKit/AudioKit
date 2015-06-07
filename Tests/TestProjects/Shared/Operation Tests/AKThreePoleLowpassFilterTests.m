ca8d0b92c869b1a738db21f5a2a34cc96dfdb158
Nicholas Arner <nicholasarner@gmail.com>
2015-06-07 10:26:54 -0400

    Added AKThreePoleLowpassFitler preset tests

--- Tests/TestProjects/Shared/Operation Tests/AKThreePoleLowpassFilterTests.m
+++ Tests/TestProjects/Shared/Operation Tests/AKThreePoleLowpassFilterTests.m
@@ -60,4 +60,55 @@
     XCTAssertEqualObjects([self md5ForOutputWithDuration:testDuration], @"2bb808ae078e6adf296662cdb354654a");
 }
 
+- (void)testPresetBrightFilterWithInput
+{
+    NSString *filename = [AKManager pathToSoundFile:@"PianoBassDrumLoop" ofType:@"wav"];
+    AKFileInput *audio = [[AKFileInput alloc] initWithFilename:filename];
+    audio.loop = YES;
+    AKMix *mono = [[AKMix alloc] initMonoAudioFromStereoInput:audio];
+    
+    AKInstrument *testInstrument = [AKInstrument instrument];
+    AKThreePoleLowpassFilter *presetFilter = [AKThreePoleLowpassFilter presetBrightFilterWithInput:mono];
+    [testInstrument setAudioOutput:presetFilter];
+    [AKOrchestra addInstrument:testInstrument];
+    [testInstrument play];
+    
+    // Check output
+    XCTAssertEqualObjects([self md5ForOutputWithDuration:2.0], @"c9a1561852fc6c67353ed3f162655307");
+}
+
+- (void)testPresetDullBassWithInput
+{
+    NSString *filename = [AKManager pathToSoundFile:@"PianoBassDrumLoop" ofType:@"wav"];
+    AKFileInput *audio = [[AKFileInput alloc] initWithFilename:filename];
+    audio.loop = YES;
+    AKMix *mono = [[AKMix alloc] initMonoAudioFromStereoInput:audio];
+    
+    AKInstrument *testInstrument = [AKInstrument instrument];
+    AKThreePoleLowpassFilter *presetFilter = [AKThreePoleLowpassFilter presetDullBassWithInput:mono];
+    [testInstrument setAudioOutput:presetFilter];
+    [AKOrchestra addInstrument:testInstrument];
+    [testInstrument play];
+    
+    // Check output
+    XCTAssertEqualObjects([self md5ForOutputWithDuration:2.0], @"572fe6f544068c93f8cc4757498f77a0");
+}
+
+- (void)testPresetScreamWithInput
+{
+    NSString *filename = [AKManager pathToSoundFile:@"PianoBassDrumLoop" ofType:@"wav"];
+    AKFileInput *audio = [[AKFileInput alloc] initWithFilename:filename];
+    audio.loop = YES;
+    AKMix *mono = [[AKMix alloc] initMonoAudioFromStereoInput:audio];
+    
+    AKInstrument *testInstrument = [AKInstrument instrument];
+    AKThreePoleLowpassFilter *presetFilter = [AKThreePoleLowpassFilter presetScreamWithInput:mono];
+    [testInstrument setAudioOutput:presetFilter];
+    [AKOrchestra addInstrument:testInstrument];
+    [testInstrument play];
+    
+    // Check output
+    XCTAssertEqualObjects([self md5ForOutputWithDuration:2.0], @"7f351499d3512d97308e758cf92a08ba");
+}
+
 @end
