3ee43375ffbcec3eb0ba1f7566e6a451c9b40cc3
Aurelius Prochazka <aure@aure.com>
2015-07-01 00:16:34 -0700

    Sound Font Instrument Player added, playground updated

--- Playgrounds/Playgrounds/SoundFontPlayground.m
+++ Playgrounds/Playgrounds/SoundFontPlayground.m
@@ -10,9 +10,12 @@
 
 @implementation Playground {
     AKSoundFont *soundFont;
-    AKInstrument *instrument;
+    AKInstrument *presetPlayer;
+    AKInstrument *instrumentPlayer;
     int presetNumber;
-    UILabel *label;
+    int instrumentNumber;
+    UILabel *presetLabel;
+    UILabel *instrumentLabel;
 }
 
 - (void) setup
@@ -25,46 +28,85 @@
     [super run];
 
     soundFont = [[AKSoundFont alloc] initWithFilename:[AKManager pathToSoundFile:@"GeneralMidi" ofType:@"sf2"]];
-    instrument = [AKInstrument instrumentWithNumber:1];
+
+    presetPlayer = [AKInstrument instrumentWithNumber:1];
+    instrumentPlayer = [AKInstrument instrumentWithNumber:2];
+
     presetNumber = 0;
+    instrumentNumber = 0;
 
-    label = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 300, 30)];
-    label.text = [[[soundFont presets] firstObject] name];
-    [timelineViewController addView:label];
+    [self updatePresetPlayer];
+    [self updateInstrumentPlayer];
 
-    [self updateInstrument];
+    presetLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 300, 30)];
+    presetLabel.text = [[[soundFont presets] firstObject] name];
+    [timelineViewController addView:presetLabel];
 
     [self addButtonWithTitle:@"Up Preset" block:^{
         if (presetNumber < soundFont.presets.count) {
             presetNumber = presetNumber + 1;
-            [self updateInstrument];
+            [self updatePresetPlayer];
         }
     }];
 
     [self addButtonWithTitle:@"Down Preset" block:^{
         if (presetNumber > 0) {
             presetNumber = presetNumber - 1;
-            [self updateInstrument];
+            [self updatePresetPlayer];
+        }
+    }];
+    [self addButtonWithTitle:@"Play Preset" block:^{
+        [presetPlayer playForDuration:1.0];
+    }];
+
+    instrumentLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 300, 30)];
+    instrumentLabel.text = [[[soundFont instruments] firstObject] name];
+    [timelineViewController addView:instrumentLabel];
+
+    [self addButtonWithTitle:@"Up Instrument" block:^{
+        if (instrumentNumber < soundFont.instruments.count) {
+            instrumentNumber = instrumentNumber + 1;
+            [self updateInstrumentPlayer];
         }
     }];
 
-    [self addButtonWithTitle:@"Play" block:^{
-        [instrument playForDuration:1.0];
+    [self addButtonWithTitle:@"Down Instrument" block:^{
+        if (instrumentNumber > 0) {
+            instrumentNumber = instrumentNumber - 1;
+            [self updateInstrumentPlayer];
+        }
+    }];
+
+
+    [self addButtonWithTitle:@"Play Instrument" block:^{
+        [instrumentPlayer playForDuration:1.0];
     }];
 }
 
-- (void)updateInstrument
+- (void)updatePresetPlayer
 {
-    instrument = [AKInstrument instrumentWithNumber:1];
+    presetPlayer = [AKInstrument instrumentWithNumber:1];
     AKSoundFontPreset *soundFontPreset = [[soundFont presets] objectAtIndex:presetNumber];
-    label.text = soundFontPreset.name;
+    presetLabel.text = soundFontPreset.name;
     AKSoundFontPresetPlayer *player = [[AKSoundFontPresetPlayer alloc] initWithSoundFontPreset:soundFontPreset];
     player.frequencyMultiplier = akp(1.5);
     player.amplitude = akp(0.1);
 
-    [instrument setStereoAudioOutput:player];
-    [AKOrchestra addInstrument:instrument];
+    [presetPlayer setStereoAudioOutput:player];
+    [AKOrchestra addInstrument:presetPlayer];
+}
 
+- (void)updateInstrumentPlayer
+{
+    instrumentPlayer = [AKInstrument instrumentWithNumber:2];
+    AKSoundFontInstrument *soundFontInstrument = [[soundFont instruments] objectAtIndex:instrumentNumber];
+    instrumentLabel.text = soundFontInstrument.name;
+    AKSoundFontInstrumentPlayer *player = [[AKSoundFontInstrumentPlayer alloc] initWithSoundFontInstrument:soundFontInstrument];
+    player.frequencyMultiplier = akp(1.5);
+    player.amplitude = akp(0.1);
+
+    [instrumentPlayer setStereoAudioOutput:player];
+    [AKOrchestra addInstrument:instrumentPlayer];
 }
 
 @end
