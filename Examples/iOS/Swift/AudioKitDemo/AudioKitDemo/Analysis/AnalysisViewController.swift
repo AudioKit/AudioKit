f567aad6f565ad12518d90c2ec2f0b23224b3728
Aurelius Prochazka <aure@aure.com>
2015-07-08 16:45:48 -0700

    Using initWithInput and "output" instead of "auxilliaryOutput" in Examples

--- Examples/iOS/Swift/AudioKitDemo/AudioKitDemo/Analysis/AnalysisViewController.swift
+++ Examples/iOS/Swift/AudioKitDemo/AudioKitDemo/Analysis/AnalysisViewController.swift
@@ -7,14 +7,14 @@
 //
 
 class AnalysisViewController: UIViewController {
-    
+
     @IBOutlet var frequencyLabel: UILabel!
     @IBOutlet var amplitudeLabel: UILabel!
     @IBOutlet var noteNameLabel: UILabel!
     @IBOutlet var amplitudePlot: AKInstrumentPropertyPlot!
     @IBOutlet var frequencyPlot: AKInstrumentPropertyPlot!
     @IBOutlet var normalizedFrequencyPlot: AKFloatPlot!
-    
+
     let microphone = AKMicrophone()
     var analyzer: AKAudioAnalyzer!
 
@@ -26,37 +26,37 @@ class AnalysisViewController: UIViewController {
 
     let analysisSequence = AKSequence()
     let updateAnalysis = AKEvent()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         AKSettings.shared().audioInputEnabled = true
-        
-        analyzer = AKAudioAnalyzer(audioSource: microphone.auxilliaryOutput)
+
+        analyzer = AKAudioAnalyzer(input: microphone.output)
 
         AKOrchestra.addInstrument(microphone)
         AKOrchestra.addInstrument(analyzer)
-        
+
         amplitudePlot.property = analyzer.trackedAmplitude
-        
+
         frequencyPlot.property = analyzer.trackedFrequency
         normalizedFrequencyPlot.minimum = 15
         normalizedFrequencyPlot.maximum = 32
     }
-    
+
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        
+
         analyzer.start()
         microphone.start()
-        
+
         let timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("updateUI"), userInfo: nil, repeats: true)
     }
-    
+
     func updateUI() {
         if analyzer.trackedAmplitude.value > 0.1 {
             frequencyLabel.text = String(format: "%0.1f", analyzer.trackedFrequency.value)
-            
+
             var frequency = analyzer.trackedFrequency.value
             while (frequency > Float(noteFrequencies[noteFrequencies.count-1])) {
                 frequency = frequency / 2.0
@@ -64,15 +64,15 @@ class AnalysisViewController: UIViewController {
             while (frequency < Float(noteFrequencies[0])) {
                 frequency = frequency * 2.0
             }
-            
+
             normalizedFrequency.value = frequency
             normalizedFrequencyPlot.updateWithValue(frequency)
-            
+
             var minDistance: Float = 10000.0
             var index = 0
-            
+
             for (var i = 0; i < noteFrequencies.count; i++){
-                
+
                 var distance = fabsf(Float(noteFrequencies[i]) - frequency)
                 if (distance < minDistance){
                     index = i
