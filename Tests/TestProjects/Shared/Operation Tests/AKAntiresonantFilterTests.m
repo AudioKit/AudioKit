77480e40090a39e338cdec731990f532f1e7563b
Aurelius Prochazka <aure@aure.com>
2015-08-19 22:27:45 -0700

    AntiresonantFilter Test

---
+++ Tests/TestProjects/Shared/Operation Tests/AKAntiresonantFilterTests.m
@@ -0,0 +1,63 @@
+//
+//  AKAntiresonantFilterTests.m
+//  iOSObjectiveCAudioKit
+//
+//  Created by Aurelius Prochazka on 5/22/15.
+//  Copyright (c) 2015 Aurelius Prochazka. All rights reserved.
+//
+
+#import "AKTestCase.h"
+
+#define testDuration 10.0
+
+@interface TestAntiresonantFilterInstrument : AKInstrument
+@end
+
+@implementation TestAntiresonantFilterInstrument
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+        
+        NSString *filename = [AKManager pathToSoundFile:@"PianoBassDrumLoop" ofType:@"wav"];
+        AKFileInput *audio = [[AKFileInput alloc] initWithFilename:filename];
+        audio.loop = YES;
+        AKMix *mono = [[AKMix alloc] initMonoAudioFromStereoInput:audio];
+        
+        AKLine *centerFrequency = [[AKLine alloc] initWithFirstPoint:akp(1)
+                                                         secondPoint:akp(5000)
+                                               durationBetweenPoints:akp(testDuration)];
+        AKLine *bandwidth = [[AKLine alloc] initWithFirstPoint:akp(500)
+                                                   secondPoint:akp(0)
+                                         durationBetweenPoints:akp(testDuration)];
+        AKAntiresonantFilter *filter = [[AKAntiresonantFilter alloc] initWithInput:mono];
+        filter.centerFrequency = centerFrequency;
+        filter.bandwidth       = bandwidth;
+        
+        [self setAudioOutput:filter];
+    }
+    return self;
+}
+
+@end
+
+@interface AKAntiresonantFilterTests : AKTestCase
+@end
+
+@implementation AKAntiresonantFilterTests
+
+- (void)testAntiresonantFilter
+{
+    // Set up performance
+    TestAntiresonantFilterInstrument *testInstrument = [[TestAntiresonantFilterInstrument alloc] init];
+    [AKOrchestra addInstrument:testInstrument];
+    [testInstrument playForDuration:testDuration];
+    
+    // Check output
+    XCTAssertEqualObjects([self md5ForOutputWithDuration:testDuration], @"6732f0f4e4b428dfcd2a043aa49b3c07");
+}
+
+
+@end
