43206aca81bba17825737d693bc5f02625257a6d
Aurelius Prochazka <aure@aure.com>
2015-08-23 16:10:31 -0700

    More documentation inside new utility instruments

--- AudioKit/Utilities/Instruments/Processors/AKDelayPedal.h
+++ AudioKit/Utilities/Instruments/Processors/AKDelayPedal.h
@@ -8,21 +8,34 @@
 
 #import "AKFoundation.h"
 
+/// AudioKit Delay Pedal instrument
 @interface AKDelayPedal : AKInstrument
 
+/// Amount of feedback (0-1)
 @property AKInstrumentProperty *feedback;
+
+/// Delay time in seconds
 @property AKInstrumentProperty *time;
+
+/// Source and delay mix (0-1)
 @property AKInstrumentProperty *mix;
 
-// Audio outlet for global effects processing
+/// Audio outlet for global effects processing
 @property (readonly) AKAudio *output;
 
+/// Initialize the delay line with an input
+/// @param input The input source audio signal
 - (instancetype)initWithInput:(AKAudio *)input;
 
 # pragma mark Presets
 
+/// A small chamber preset
 - (void)setPresetSmallChamber;
+
+/// A robotic voice preset
 - (void)setPresetRobotVoice;
+
+/// A menacing robotic voice preset
 - (void)setPresetDaleks;
 
 @end
