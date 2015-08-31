4d5662d0134beb69e252d35536afbb98b753f6c4
Aurelius Prochazka <aure@aure.com>
2015-08-30 22:36:23 -0700

    Making AKReverbPedal work with either mono or stereo inputs

--- AudioKit/Utilities/Instruments/Processors/AKReverbPedal.m
+++ AudioKit/Utilities/Instruments/Processors/AKReverbPedal.m
@@ -21,7 +21,7 @@
         _cutoffFrequency = [self createPropertyWithValue:4000 minimum:0 maximum:20000];
         _feedback = [self createPropertyWithValue:0.5 minimum:0.0 maximum:1.0];
         _mix      = [self createPropertyWithValue:0.5 minimum:0.0 maximum:1.0];
-
+        
         // Instrument Definition
         AKReverb *reverb = [AKReverb reverbWithInput:input];
         reverb.feedback = _feedback;
@@ -33,7 +33,42 @@
         _output = [AKStereoAudio globalParameter];
         [self assignOutput:_output.leftOutput to:leftMix];
         [self assignOutput:_output.rightOutput to:rightMix];
+        
+        // Reset Inputs
+        [self resetParameter:input];
+    }
+    return self;
+}
 
+- (instancetype)initWithStereoInput:(AKStereoAudio *)input
+{
+    self = [super init];
+    if (self) {
+        // Instrument Based Control
+        _feedback = [self createPropertyWithValue:0.5 minimum:0.0 maximum:1.0];
+        _mix      = [self createPropertyWithValue:0.5 minimum:0.0 maximum:1.0];
+        
+        AKReverb *reverb;
+        reverb = [[AKReverb alloc] initWithStereoInput:input];
+        reverb.feedback = _feedback;
+        
+        AKMix *leftMix;
+        leftMix = [[AKMix alloc] initWithInput1:input.leftOutput
+                                         input2:reverb.leftOutput
+                                        balance:_mix];
+        
+        AKMix *rightMix;
+        rightMix = [[AKMix alloc] initWithInput1:input.rightOutput
+                                          input2:reverb.rightOutput
+                                         balance:_mix];
+        
+        // AUDIO OUTPUT ========================================================
+        
+        AKAudioOutput *audio;
+        audio = [[AKAudioOutput alloc] initWithLeftAudio:leftMix
+                                              rightAudio:rightMix];
+        [self connect:audio];
+        
         // Reset Inputs
         [self resetParameter:input];
     }
