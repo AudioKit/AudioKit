056be55ddf4104f6ebcc734e32623531a1ca4ede
Aurelius Prochazka <aure@aure.com>
2015-09-05 21:00:35 -0700

    The oscillators need not be instance variables

--- InitialProofOfConcept/InitialProofOfConcept/AKInstrument.m
+++ InitialProofOfConcept/InitialProofOfConcept/AKInstrument.m
@@ -9,10 +9,7 @@
 #import "AKInstrument.h"
 #import "AKFMOscillator.h"
 
-@implementation AKInstrument {
-    AKFMOscillator *fmOscillator;
-    AKFMOscillator *fmOscillatorDefault;
-}
+@implementation AKInstrument
 
 - (instancetype)init
 {
@@ -20,17 +17,17 @@
     if (self) {
         _operations = [NSMutableArray array];
         
-        fmOscillatorDefault = [[AKFMOscillator alloc] initWithBaseFrequency:akp(0.1)
-                                                          carrierMultiplier:akp(10)
-                                                       modulatingMultiplier:akp(3)
-                                                            modulationIndex:akp(20)
-                                                                  amplitude:akp(440)];
+        AKFMOscillator *fmOscillatorDefault = [[AKFMOscillator alloc] initWithBaseFrequency:akp(0.1)
+                                                                          carrierMultiplier:akp(10)
+                                                                       modulatingMultiplier:akp(3)
+                                                                            modulationIndex:akp(20)
+                                                                                  amplitude:akp(440)];
         
-        fmOscillator = [[AKFMOscillator alloc] initWithBaseFrequency:fmOscillatorDefault
-                                                   carrierMultiplier:akp(1)
-                                                modulatingMultiplier:akp(0.1)
-                                                     modulationIndex:akp(1)
-                                                           amplitude:akp(0.6)];
+        AKFMOscillator *fmOscillator = [[AKFMOscillator alloc] initWithBaseFrequency:fmOscillatorDefault
+                                                                   carrierMultiplier:akp(1)
+                                                                modulatingMultiplier:akp(0.1)
+                                                                     modulationIndex:akp(1)
+                                                                           amplitude:akp(0.6)];
         
         // Kind of like our old connect, will have to be cleaned up
         [_operations addObject:fmOscillatorDefault];
