3b1739982fca71dcbcb515e3a8fa390a76b6f4d7
Aurelius Prochazka <aure@aure.com>
2015-09-05 20:31:27 -0700

    A little cleanup of redundant variables

--- InitialProofOfConcept/InitialProofOfConcept/AKManager.m
+++ InitialProofOfConcept/InitialProofOfConcept/AKManager.m
@@ -9,28 +9,20 @@
 #import "AKManager.h"
 
 @implementation AKManager
-{
-    sp_data *sp_data;
-}
 
 - (instancetype)init
 {
     NSLog(@"Initializing Manager");
     if ([super init]) {
-        sp_create(&sp_data);
+        sp_create(&_data);
         _instrument = [[AKInstrument alloc] init];
     }
     return self;
 }
 
-- (sp_data *)data
-{
-    return sp_data;
-}
-
 - (void)destroy
 {
-    sp_destroy(&sp_data);
+    sp_destroy(&_data);
 }
 
 // -----------------------------------------------------------------------------
