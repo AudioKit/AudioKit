0382f559c55625e674bff383bb01dc0c4ee6ccf2
Aurelius Prochazka <aure@aure.com>
2015-09-07 16:04:12 -0700

    Adding comments early, to make sure not to let it slide!

--- AudioKit/Core Classes/AKParameter.swift
+++ AudioKit/Core Classes/AKParameter.swift
@@ -26,31 +26,38 @@ extension Double {
     var midiratio: Double {return pow(2, self * 0.083333333333)}
 }
 
+/** A parent class for all variables in AudioKit */
 class AKParameter {
 
+    /** A pointer to the SoundPipe float */
     var pointer: UnsafeMutablePointer<Float> = UnsafeMutablePointer.alloc(1)
     
-    var value: Float = 0.0 {
-        didSet {
-            pointer.memory = value
-        }
-    }
+    /** The actual value of the parameter */
+    var value: Float = 0.0 { didSet { pointer.memory = value } }
     
+    /** 
+    An initializer for a constant parameter 
+    
+    :param: float The value of the constant parameter
+    */
     convenience init(float: Float) {
         self.init()
         value = float
     }
     
+    /** Bind the memory of the SoundPipe value to this parameter */
     func bind(binding:UnsafeMutablePointer<Float>) {
         pointer = binding
         pointer.memory = value
     }
     
+    /** The compute function to override in subclasses */
     func compute() -> Float {
         // override in subclass
         return 0.0
     }
     
+    /** A placeholder for a function to release the memory */
     func teardown() {
         // override in subclass
     }
