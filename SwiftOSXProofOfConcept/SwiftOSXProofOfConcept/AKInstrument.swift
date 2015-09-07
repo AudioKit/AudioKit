1a157579a7010e56e700d3a79a7460371ceff11f
Aurelius Prochazka <aure@aure.com>
2015-09-07 12:46:34 -0700

    Setting up full initialization and parameter setting methods of operation creation

--- SwiftOSXProofOfConcept/SwiftOSXProofOfConcept/AKInstrument.swift
+++ SwiftOSXProofOfConcept/SwiftOSXProofOfConcept/AKInstrument.swift
@@ -8,25 +8,43 @@
 
 import Foundation
 
-class AKInstrument {
-    var operations = Array<AKParameter>([])
+@objc class AKInstrument {
+    var operations: [AKParameter] = []
     
     var oscillatingFrequency: AKOscillator
     var fmOscillator: AKFMOscillator
     
     init() {
+        
+        // Method 1: Full Initializer
         oscillatingFrequency = AKOscillator(
-            frequency: akp(2),
+            frequency: akp(1),
             amplitude: akp(440),
             phase: 0.0
         )
         
+        // Method 2: Default Initializer plus property setting
+        oscillatingFrequency = AKOscillator()
+        oscillatingFrequency.frequency = akp(1)
+        oscillatingFrequency.amplitude = akp(440)
+        
+        // Method 1: Full Initializer
         fmOscillator = AKFMOscillator(
             baseFrequency: oscillatingFrequency,
             carrierMultiplier: akp(3),
             modulatingMultiplier: akp(5),
             modulationIndex: akp(11),
-            amplitude: akp(0.1))
+            amplitude: akp(0.1)
+        )
+        
+        // Method 2: Default Initializer plus property setting
+        fmOscillator = AKFMOscillator()
+        fmOscillator.baseFrequency        = oscillatingFrequency
+        fmOscillator.carrierMultiplier    = akp(3)
+        fmOscillator.modulatingMultiplier = akp(5)
+        fmOscillator.modulationIndex      = akp(11)
+        fmOscillator.amplitude            = akp(0.1)
+        
         operations.append(oscillatingFrequency)
         operations.append(fmOscillator)
     }
