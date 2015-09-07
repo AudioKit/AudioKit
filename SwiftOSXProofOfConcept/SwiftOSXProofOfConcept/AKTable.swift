20ed42069bf7e92a0b844728c18381a7adaf7d3c
Aurelius Prochazka <aure@aure.com>
2015-09-06 19:46:26 -0700

    Moved ftbl stuff into AKTable

--- SwiftOSXProofOfConcept/SwiftOSXProofOfConcept/AKTable.swift
+++ SwiftOSXProofOfConcept/SwiftOSXProofOfConcept/AKTable.swift
@@ -9,4 +9,20 @@
 import Foundation
 
 class AKTable : AKParameter {
+    var ftbl: UnsafeMutablePointer<sp_ftbl> = nil  //not just nil
+    
+    override init() {
+        super.init()
+        create()
+    }
+    
+    func create() {
+        sp_ftbl_create(AKManager.sharedManager.data, &ftbl, 4096)
+        sp_gen_sine(AKManager.sharedManager.data, ftbl);
+    }
+    
+    func destroy() {
+        sp_ftbl_destroy(&ftbl)
+    }
+
 }
