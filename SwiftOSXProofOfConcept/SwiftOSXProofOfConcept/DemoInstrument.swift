2c100940e550f3ea5432ac843d7ab37935fa5045
Aurelius Prochazka <aure@aure.com>
2015-09-07 13:02:02 -0700

    Moving AudioKit files to their own folder, removing initial proof of concept project

---
+++ SwiftOSXProofOfConcept/SwiftOSXProofOfConcept/DemoInstrument.swift
@@ -0,0 +1,9 @@
+//
+//  DemoInstrument.swift
+//  SwiftOSXProofOfConcept
+//
+//  Created by Aurelius Prochazka on 9/7/15.
+//  Copyright (c) 2015 AudioKit. All rights reserved.
+//
+
+import Foundation
