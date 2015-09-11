812cf8242ddf416a057238338f86edb879e962d1
Aurelius Prochazka <aure@aure.com>
2015-09-10 23:03:48 -0700

    Fixed the description of AKReverb's cutoff Frequency

--- AudioKit/Operations/Signal Modifiers/Reverbs/AKReverb.h
+++ AudioKit/Operations/Signal Modifiers/Reverbs/AKReverb.h
@@ -19,7 +19,7 @@ NS_ASSUME_NONNULL_BEGIN
 /// Instantiates the reverb with all values
 /// @param input Audio input
 /// @param feedback Feedback level in the range 0 to 1. 0.6 gives a good small 'live' room sound, 0.8 a small hall, and 0.9 a large hall. A setting of exactly 1 means infinite length, while higher values will make the opcode unstable. Updated at Control-rate. [Default Value: 0.6]
-/// @param cutoffFrequency comment Updated at Control-rate. [Default Value: 4000]
+/// @param cutoffFrequency Low-pass cutoff frequency Updated at Control-rate. [Default Value: 4000]
 - (instancetype)initWithInput:(AKParameter *)input
                      feedback:(AKParameter *)feedback
               cutoffFrequency:(AKParameter *)cutoffFrequency;
@@ -67,7 +67,7 @@ NS_ASSUME_NONNULL_BEGIN
 /// Instantiates the reverb with all values
 /// @param input Stereo input to the reverberator.
 /// @param feedback Feedback level in the range 0 to 1. 0.6 gives a good small 'live' room sound, 0.8 a small hall, and 0.9 a large hall. A setting of exactly 1 means infinite length, while higher values will make the opcode unstable. Updated at Control-rate. [Default Value: 0.6]
-/// @param cutoffFrequency comment Updated at Control-rate. [Default Value: 4000]
+/// @param cutoffFrequency Low-pass cutoff frequency Updated at Control-rate. [Default Value: 4000]
 - (instancetype)initWithStereoInput:(AKStereoAudio *)input
                            feedback:(AKParameter *)feedback
                     cutoffFrequency:(AKParameter *)cutoffFrequency;
@@ -120,11 +120,11 @@ NS_ASSUME_NONNULL_BEGIN
 /// @param feedback Feedback level in the range 0 to 1. 0.6 gives a good small 'live' room sound, 0.8 a small hall, and 0.9 a large hall. A setting of exactly 1 means infinite length, while higher values will make the opcode unstable. Updated at Control-rate. [Default Value: 0.6]
 - (void)setOptionalFeedback:(AKParameter *)feedback;
 
-/// comment [Default Value: 4000]
+/// Low-pass cutoff frequency [Default Value: 4000]
 @property (nonatomic) AKParameter *cutoffFrequency;
 
 /// Set an optional cutoff frequency
-/// @param cutoffFrequency comment Updated at Control-rate. [Default Value: 4000]
+/// @param cutoffFrequency Low-pass cutoff frequency Updated at Control-rate. [Default Value: 4000]
 - (void)setOptionalCutoffFrequency:(AKParameter *)cutoffFrequency;
 
 
