27e715bf0eb3f5b55ceaa51089c966b465a01444
Aurelius Prochazka <aure@aure.com>
2015-09-12 00:18:31 -0700

    AKTable with some constructors

--- AudioKit/Core Classes/AKTable.swift
+++ AudioKit/Core Classes/AKTable.swift
@@ -13,17 +13,44 @@ import Foundation
     
     /** Pointer to the SoundPipe table */
     var ftbl: UnsafeMutablePointer<sp_ftbl> = nil  //not just nil
+    var table: UnsafeMutablePointer<Float> = nil
+    private var size: Int
     
     /** Initialize and set up the default table */
-    override init() {
+    init(size tableSize: Int = 4096) {
+        size = tableSize
         super.init()
         setup()
+        standardSineWave()
     }
     
-    /** Set up the table as the default sine wave */
-     func setup() {
-        sp_ftbl_create(AKManager.sharedManager.data, &ftbl, 4096)
+    func standardTriangleWave() {
+        let slope = Float(4.0) / Float(size)
+        for i in 0..<size {
+            if i < size / 2 {
+                ftbl.memory.tbl[i] = slope * Float(i) - 1.0
+            } else {
+                ftbl.memory.tbl[i] = slope * Float(-i) + 3.0
+            }
+        }
+    }
+    
+    func standardSquareWave() {
+        for i in 0..<size {
+            if i < size / 2 {
+                ftbl.memory.tbl[i] = -1.0
+            } else {
+                ftbl.memory.tbl[i] = 1.0
+            }            
+        }
+    }
+    
+    func standardSineWave() {
         sp_gen_sine(AKManager.sharedManager.data, ftbl);
+    }
+    
+    func setup() {
+        sp_ftbl_create(AKManager.sharedManager.data, &ftbl, size)
     }
     
     /** Release the table's memory */
