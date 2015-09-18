23ac91cb04bc60392608624cded82c92349a02ca
Aurelius Prochazka <aure@aure.com>
2015-09-18 15:50:37 -0700

    Added AKMix Test

---
+++ Tests/OSXAudioKit/TestMix.swift
@@ -0,0 +1,29 @@
+//
+//  TestMix.swift
+//  OSXAudioKit
+//
+//  Created by Aurelius Prochazka on 9/18/15.
+//  Copyright © 2015 AudioKit. All rights reserved.
+//
+
+import Foundation
+
+@objc class TestMix : AKInstrument {
+    
+    override init() {
+        super.init()
+        
+        let osc1 = AKOscillator()
+        osc1.frequency = 300.ak
+        
+        let osc2 = AKOscillator()
+        osc2.frequency = 500.ak
+        
+        let lfo = AKOscillator()
+        lfo.frequency = 3.ak
+
+        let mix = AKMix(input1: osc1, input2: osc2, balancePoint: (lfo + 1 ) / 2 )
+        
+        output = AKAudioOutput(input: mix)
+    }
+}
