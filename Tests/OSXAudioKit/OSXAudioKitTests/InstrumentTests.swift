bbfa5b7c262ba600470672b1708d2afed2ecdfb7
Aurelius Prochazka <aure@aure.com>
2015-09-19 16:42:18 -0700

    Added AKTrackedAmplitude Tester

--- Tests/OSXAudioKit/OSXAudioKitTests/InstrumentTests.swift
+++ Tests/OSXAudioKit/OSXAudioKitTests/InstrumentTests.swift
@@ -11,26 +11,32 @@ import XCTest
 class InstrumentTests: AKTestCase {
     
     func testFMOscillator() {
-        testInstrument = TestFMOscillator()
+        testInstrument = AKFMOscillatorTester()
         process()
         XCTAssertEqual(calculatedMD5(), "2f03be95c90706a303370c38907f069f")
     }
     
     func testMix() {
-        testInstrument = TestMix()
+        testInstrument = AKMixTester()
         process()
         XCTAssertEqual(calculatedMD5(), "5220de22421c1ceb97d91f1772654974")
     }
 
     func testOscillator() {
-        testInstrument = TestOscillator()
+        testInstrument = AKOscillatorTester()
         process()
         XCTAssertEqual(calculatedMD5(), "b08efaa81dbf543493074d2464d7dcda")
     }
     
     func testSilence() {
-        testInstrument = TestSilence()
+        testInstrument = SilenceTester()
         process()
         XCTAssertEqual(calculatedMD5(), "6160230370c1f7ca3dc2ced3cd39f3dd")
+    }
+    
+    func testTrackedAmplitude() {
+        testInstrument = AKTrackedAmplitudeTester()
+        process()
+        XCTAssertEqual(calculatedMD5(), "3f0f17b839b354522f8a2f66bfe45d35")
     }
 }
---
+++ Tests/Test Instruments/Operation Testers/Analysis Testers/AKTrackedAmplitudeTester.swift
@@ -0,0 +1,27 @@
+//
+//  AKTrackedAmplitudeTester.swift
+//  OSXAudioKit
+//
+//  Created by Aurelius Prochazka on 9/19/15.
+//  Copyright © 2015 AudioKit. All rights reserved.
+//
+
+import Foundation
+
+class AKTrackedAmplitudeTester : AKInstrument {
+    
+    override init() {
+        super.init()
+        
+        let amplitude = AKPhasor(frequency: 0.5.ak, phase: 0)
+        
+        let growingLouderOscillator = AKOscillator(waveform: AKTable.standardSineWave(), frequency: 880.ak, amplitude: amplitude, phase: 0)
+        
+        let trackedAmplitude = AKTrackedAmplitude(input: growingLouderOscillator)
+        
+        let growingHigherOscillator = AKOscillator()
+        growingHigherOscillator.frequency = trackedAmplitude * 880.ak
+        
+        output = AKAudioOutput(input: growingHigherOscillator)
+    }
+}
