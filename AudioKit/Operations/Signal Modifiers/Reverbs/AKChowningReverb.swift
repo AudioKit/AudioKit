896fc6d69e19bf3dc369b76a37325b0555b24aa5
Aurelius Prochazka <aure@aure.com>
2015-09-18 23:44:09 -0700

    Updated AKChowningReverb comments

--- AudioKit/Operations/Signal Modifiers/Reverbs/AKChowningReverb.swift
+++ AudioKit/Operations/Signal Modifiers/Reverbs/AKChowningReverb.swift
@@ -11,6 +11,7 @@ import Foundation
 /** John Chowning reverberator
 
 This is was built using the JC reverb implentation found in FAUST. According to the source code, the specifications for this implementation were found on an old SAIL DART backup tape.
+This class is derived from the CLM JCRev function, which is based on the use of networks of simple allpass and comb delay filters.  This class implements three series allpass units, followed by four parallel comb filters, and two decorrelation delay lines in parallel at the output.
 */
 @objc class AKChowningReverb : AKParameter {
 
