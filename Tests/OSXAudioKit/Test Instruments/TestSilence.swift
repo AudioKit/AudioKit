b9751d5d4e8852c34b7d0c3e02403bd5992d9ea7
Aurelius Prochazka <aure@aure.com>
2015-09-18 23:43:07 -0700

    Added a silence test for performance tests

---
+++ Tests/OSXAudioKit/Test Instruments/TestSilence.swift
@@ -0,0 +1,17 @@
+//
+//  TestSilence.swift
+//  OSXAudioKit
+//
+//  Created by Aurelius Prochazka on 9/18/15.
+//  Copyright © 2015 AudioKit. All rights reserved.
+//
+
+import Foundation
+
+@objc class TestSilence : AKInstrument {
+    
+    override init() {
+        super.init()
+        output = 0.ak * AKOscillator()
+    }
+}
