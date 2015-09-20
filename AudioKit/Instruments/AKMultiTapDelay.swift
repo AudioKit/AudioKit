accd6d8d9db2c6690e1050dbab01e25a9571d8dc
Aurelius Prochazka <aure@aure.com>
2015-09-19 18:46:01 -0700

    Added AKMultiTapDelay

---
+++ AudioKit/Instruments/AKMultiTapDelay.swift
@@ -0,0 +1,23 @@
+//
+//  AKMultiTapDelay.swift
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 9/19/15.
+//  Copyright © 2015 AudioKit. All rights reserved.
+//
+
+class AKMultiTapDelay: AKInstrument {
+
+    /** initalize */
+    convenience init(input: AKParameter, timesAndGainsDictionary: [Float : Float]) {
+        self.init()
+
+        var sum = input
+        for (time, gain) in timesAndGainsDictionary {
+            sum = sum + akp(gain) * AKDelay(input: input, delayTime: time)
+        }
+        
+        output = AKAudioOutput(input: sum)
+    }
+
+}
