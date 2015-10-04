585e7cb2a788bd0eb2d604ec901c59d4bec19256
Aurelius Prochazka <aure@aure.com>
2015-10-04 14:22:04 -0700

    Pushing some complexities into AKMoogLadder class

--- Examples/TestApp/TestApp/ViewController.swift
+++ Examples/TestApp/TestApp/ViewController.swift
@@ -14,39 +14,24 @@ class ViewController: UIViewController {
     
     var engine = AVAudioEngine()
     var delay = AVAudioUnitDelay()
-    var distortion = AVAudioUnitDistortion()
-    var reverb = AVAudioUnitReverb()
-    var effect = AVAudioUnit()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        var componentDescription = AudioComponentDescription()
-        componentDescription.componentType = kAudioUnitType_Effect
-        
-        AUAudioUnit.registerSubclass(
-            AKMoogLadderAudioUnit.self,
-            asComponentDescription: componentDescription,
-            name: "Local AKMoogLadder",
-            version: UInt32.max)
-        
-        AVAudioUnit.instantiateWithComponentDescription(componentDescription, options: []) { avAudioUnit, error in
-            guard let avAudioUnitEffect = avAudioUnit else { return }
-            
-            self.effect = avAudioUnitEffect
-            self.engine.attachNode(avAudioUnitEffect)
-        }
         
         // Setup engine and node instances
         let input = engine.inputNode
         let output = engine.outputNode
         
+        let moog = AKMoogLadder()
+        moog.setup()
+        engine.attachNode(moog.effect!)
+        
         engine.attachNode(delay)
     
         // Connect nodes
         engine.connect(input!, to: delay,  format: nil)
-        engine.connect(delay,  to: effect, format: nil)
-        engine.connect(effect, to: output, format: nil)
+        engine.connect(delay,  to: moog.effect!, format: nil)
+        engine.connect(moog.effect!, to: output, format: nil)
         
         // Start the engine.
         do {
