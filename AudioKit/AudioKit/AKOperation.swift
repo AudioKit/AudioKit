28d98b2a51be780f8d75dce78f67b0beff01be51
Aurelius Prochazka <aure@aure.com>
2015-10-04 15:09:38 -0700

    Reintroducing AKOperation

---
+++ AudioKit/AudioKit/AKOperation.swift
@@ -0,0 +1,17 @@
+//
+//  AKOperation.swift
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 10/4/15.
+//  Copyright © 2015 AudioKit. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+public class AKOperation {
+    public var output: AVAudioNode?
+    init() {
+        // Override in subclass
+    }
+}
