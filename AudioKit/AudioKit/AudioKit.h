03d86da4b4d41730522b91a376a947847dc64119
Aurelius Prochazka <aure@aure.com>
2015-10-04 12:52:18 -0700

    Skeleton of a cross-platform framework

---
+++ AudioKit/AudioKit/AudioKit.h
@@ -0,0 +1,17 @@
+//
+//  AudioKit.h
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 10/4/15.
+//  Copyright © 2015 AudioKit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AudioKit.
+FOUNDATION_EXPORT double AudioKitVersionNumber;
+
+//! Project version string for AudioKit.
+FOUNDATION_EXPORT const unsigned char AudioKitVersionString[];
+
+#import <AudioKit/AKMoogLadderAudioUnit.h>
