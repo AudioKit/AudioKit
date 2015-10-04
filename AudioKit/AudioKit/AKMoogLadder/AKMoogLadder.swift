2206c88002852853adfa58bbfa8fec76c22b8cec
Aurelius Prochazka <aure@aure.com>
2015-10-04 15:51:34 -0700

    Updated AKMoogLadder a subclass of AKOperation

--- AudioKit/AudioKit/AKMoogLadder/AKMoogLadder.swift
+++ AudioKit/AudioKit/AKMoogLadder/AKMoogLadder.swift
@@ -8,26 +8,25 @@
 
 import AVFoundation
 
-public class AKMoogLadder {
+public class AKMoogLadder: AKOperation {
     
-    public var output: AVAudioUnit?
     var componentDescription = AudioComponentDescription()
     
-    public init() {
-
+    public init(_ input: AKOperation) {
+        super.init()
         componentDescription.componentType = kAudioUnitType_Effect
         AUAudioUnit.registerSubclass(
             AKMoogLadderAudioUnit.self,
             asComponentDescription: componentDescription,
             name: "Local AKMoogLadder",
             version: UInt32.max)
-    }
-    
-    public func setup() {
+
         AVAudioUnit.instantiateWithComponentDescription(componentDescription, options: []) { avAudioUnit, error in
             guard let avAudioUnitEffect = avAudioUnit else { return }
             
             self.output = avAudioUnitEffect
+            AKManager.sharedManager.engine.attachNode(self.output!)
+            AKManager.sharedManager.engine.connect(input.output!, to: self.output!, format: nil)
         }
     }
 }
