4be8d4ff8c405b83609fe82dd719bee509f5a729
Aurelius Prochazka <aure@aure.com>
2015-10-06 15:32:39 -0700

    Added sliders for property testing

--- Examples/TestApp/TestApp/ViewController.swift
+++ Examples/TestApp/TestApp/ViewController.swift
@@ -12,15 +12,37 @@ import AudioKit
 class ViewController: UIViewController {
     
     let audiokit = AKManager.sharedManager
-
+    let input = AKMicrophone()
+    var delay:  AKAUDelay?
+    var moog:   AKMoogLadder?
+    var reverb: AKAUReverb?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let input  = AKMicrophone()
-        let delay  = AKAUDelay(input)
-        let moog   = AKMoogLadder(delay)
-        let reverb = AKAUReverb(moog)
+        
+        delay  = AKAUDelay(input)
+        moog   = AKMoogLadder(delay!)
+        reverb = AKAUReverb(moog!)
+        if let reverb = reverb { reverb.loadFactoryPreset(.Plate) }
         audiokit.audioOutput = reverb
         audiokit.start()
+    }
+    
+    @IBAction func changeReverb(sender: UISlider) {
+        guard let reverb = reverb else { return }
+        reverb.wetDryMix = 100.0 * sender.value
+    }
+    @IBAction func changeDelayTime(sender: UISlider) {
+        if let delay = delay { delay.delayTime = NSTimeInterval(sender.value) }
+    }
+    @IBAction func changeCutoff(sender: UISlider) {
+        guard let moog = moog else { return }
+        moog.cutoffFrequency = sender.value * 10000.0
+    }
+    @IBAction func changeResonance(sender: UISlider) {
+        guard let moog = moog else { return }
+        moog.resonance = sender.value * 100.0
+
     }
 }
 
