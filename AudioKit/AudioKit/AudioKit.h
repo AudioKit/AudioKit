584b3c956bce711597a6f96ef97afa4e0b0dc89d
Aurelius Prochazka <aure@aure.com>
2015-10-09 16:25:15 -0700

    Added AKDCBlock

--- AudioKit/AudioKit/AudioKit.h
+++ AudioKit/AudioKit/AudioKit.h
@@ -20,6 +20,7 @@ FOUNDATION_EXPORT const unsigned char AudioKitVersionString[];
 #import <AudioKit/AKLowPassButterworthFilterAudioUnit.h>
 #import <AudioKit/AKCombFilterAudioUnit.h>
 #import <AudioKit/AKDecimatorAudioUnit.h>
+#import <AudioKit/AKDCBlockAudioUnit.h>
 #import <AudioKit/AKEqualizerFilterAudioUnit.h>
 #import <AudioKit/AKFlatFrequencyResponseReverbAudioUnit.h>
 #import <AudioKit/AKHighPassFilterAudioUnit.h>
