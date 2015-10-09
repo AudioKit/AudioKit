7fb843c46c5d4902536938d77c3192b27442a8c2
Aurelius Prochazka <aure@aure.com>
2015-10-09 00:54:47 -0700

    Added AKDecimator

--- AudioKit/AudioKit/AudioKit.h
+++ AudioKit/AudioKit/AudioKit.h
@@ -18,6 +18,7 @@ FOUNDATION_EXPORT const unsigned char AudioKitVersionString[];
 #import <AudioKit/AKBandRejectButterworthFilterAudioUnit.h>
 #import <AudioKit/AKHighPassButterworthFilterAudioUnit.h>
 #import <AudioKit/AKLowPassButterworthFilterAudioUnit.h>
+#import <AudioKit/AKDecimatorAudioUnit.h>
 #import <AudioKit/AKEqualizerFilterAudioUnit.h>
 #import <AudioKit/AKFlatFrequencyResponseReverbAudioUnit.h>
 #import <AudioKit/AKHighPassFilterAudioUnit.h>
