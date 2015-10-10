730a3f18c86ed40978f20052b22dfd0854127cec
Aurelius Prochazka <aure@aure.com>
2015-10-09 23:58:54 -0700

    Added AKClipper

--- AudioKit/AudioKit/AudioKit.h
+++ AudioKit/AudioKit/AudioKit.h
@@ -18,6 +18,7 @@ FOUNDATION_EXPORT const unsigned char AudioKitVersionString[];
 #import <AudioKit/AKBandRejectButterworthFilterAudioUnit.h>
 #import <AudioKit/AKHighPassButterworthFilterAudioUnit.h>
 #import <AudioKit/AKLowPassButterworthFilterAudioUnit.h>
+#import <AudioKit/AKClipperAudioUnit.h>
 #import <AudioKit/AKCombFilterAudioUnit.h>
 #import <AudioKit/AKDecimatorAudioUnit.h>
 #import <AudioKit/AKDelayAudioUnit.h>
