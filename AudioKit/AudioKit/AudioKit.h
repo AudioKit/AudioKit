c5f7eef6f2303337735e502fe77fe1da25d01bfb
Aurelius Prochazka <aure@aure.com>
2015-10-11 10:41:04 -0700

    Added AKPeakingParametricEqualizerFilter

--- AudioKit/AudioKit/AudioKit.h
+++ AudioKit/AudioKit/AudioKit.h
@@ -30,6 +30,7 @@ FOUNDATION_EXPORT const unsigned char AudioKitVersionString[];
 #import <AudioKit/AKFormantFilterAudioUnit.h>
 #import <AudioKit/AKHighPassFilterAudioUnit.h>
 #import <AudioKit/AKLowPassFilterAudioUnit.h>
+#import <AudioKit/AKPeakingParametricEqualizerFilterAudioUnit.h>
 #import <AudioKit/AKStringResonatorAudioUnit.h>
 #import <AudioKit/AKThreePoleLowpassFilterAudioUnit.h>
 #import <AudioKit/AKModalResonanceFilterAudioUnit.h>
