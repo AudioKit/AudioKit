03bb3093ba91cdf2b9dff9afa5bbca4ae7101482
Jeff Cooper <jeff@southernworkstx.com>
2015-11-02 16:12:22 -0600

    cleaning up comments and wetDry->dryWet standardization

--- AudioKit/AudioKit/Operations/Signal Modifiers/Dynamics/AKAUPeakLimiter.swift
+++ AudioKit/AudioKit/Operations/Signal Modifiers/Dynamics/AKAUPeakLimiter.swift
@@ -15,28 +15,33 @@ public class AKAUPeakLimiter: AKOperation {
     private let cd:AudioComponentDescription = AudioComponentDescription(componentType: OSType(kAudioUnitType_Effect),componentSubType: OSType(kAudioUnitSubType_PeakLimiter),componentManufacturer: OSType(kAudioUnitManufacturer_Apple),componentFlags: 0,componentFlagsMask: 0)
     
     private var limiterAU = AVAudioUnitEffect()
+    public var internalAU = AudioUnit()
     
     /** Attack Time - Range is from 0.001 through 0.03 seconds. Default value is 0.012 s. */
     public var attackTime:Float = 0.012 {
         didSet {
-            let internalUnit:AudioUnit = limiterAU.audioUnit
-            AudioUnitSetParameter(internalUnit, kLimiterParam_AttackTime, kAudioUnitScope_Global, 0, attackTime, 0)
+            AudioUnitSetParameter(internalAU, kLimiterParam_AttackTime, kAudioUnitScope_Global, 0, attackTime, 0)
         }
     }//attackTime
     
     /** Decay Time - Range is from 0.001 through 0.06 seconds. Default value is 0.024 s. */
     public var decayTime:Float = 0.024 {
         didSet {
-            let internalUnit:AudioUnit = limiterAU.audioUnit
-            AudioUnitSetParameter(internalUnit, kLimiterParam_DecayTime, kAudioUnitScope_Global, 0, decayTime, 0)
+            AudioUnitSetParameter(internalAU, kLimiterParam_DecayTime, kAudioUnitScope_Global, 0, decayTime, 0)
         }
     }//decayTime
     
     /** PreGain - Range is from –40 through +40 dB. Default value is 0 dB. */
     public var pregain:Float = 0 {
         didSet {
-            let internalUnit:AudioUnit = limiterAU.audioUnit
-            AudioUnitSetParameter(internalUnit, kLimiterParam_PreGain, kAudioUnitScope_Global, 0, pregain, 0)
+            AudioUnitSetParameter(internalAU, kLimiterParam_PreGain, kAudioUnitScope_Global, 0, pregain, 0)
+        }
+    }//pregain
+    
+    /** LimitingAmount - Range is from 0 through 20. Default value is 0. */
+    public var limitingAmount:Float = 0 {
+        didSet {
+            AudioUnitSetParameter(internalAU, 1000, kAudioUnitScope_Global, 0, pregain, 0)
         }
     }//pregain
     
@@ -44,6 +49,7 @@ public class AKAUPeakLimiter: AKOperation {
     public init(_ input: AKOperation) {
         super.init()
         limiterAU = AVAudioUnitEffect(audioComponentDescription: cd)
+        internalAU = limiterAU.audioUnit
         output = limiterAU
         AKManager.sharedInstance.engine.attachNode(output!)
         AKManager.sharedInstance.engine.connect(input.output!, to: output!, format: nil)
