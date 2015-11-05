8868ac0ce113c59895515a6b9096222b3771b0e8
Aurelius Prochazka <aure@aure.com>
2015-11-04 18:04:28 -0800

    AU effect template and generator

---
+++ DeveloperTools/bin/generate_au_swift.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+
+require 'erb'
+require 'active_support/all'
+
+class String
+  def uncapitalize
+    self[0, 1].downcase + self[1..-1]
+  end
+end
+
+swift_file = ARGV[0]
+
+au_name = /AU([A-z0-9]+).swift/.match(swift_file).captures[0]
+
+parameters = []
+k_prefix = ""
+name = ""
+scope = ""
+type = ""
+min = ""
+max = ""
+default = ""
+
+File.open( swift_file ).each do |line|
+
+    regex = /^\/\/ ([A-z0-9]+), ([A-z0-9]+), ([\-0-9\.]+)\s*->\s*([\-0-9\.]+), ([\-0-9\.]+)/
+    if line =~ regex
+        scope   = regex.match(line).captures[0]
+        type    = regex.match(line).captures[1]
+        min     = regex.match(line).captures[2]
+        max     = regex.match(line).captures[3]
+        default = regex.match(line).captures[4]
+    end
+
+    regex = /^\/\/ ([A-z0-9]+), ([A-z0-9]+), ([\-0-9\.]+)\s*->\s*\(SampleRate\/2\), ([\-0-9\.]+)/
+    if line =~ regex
+        scope   = regex.match(line).captures[0]
+        type    = regex.match(line).captures[1]
+        min     = regex.match(line).captures[2]
+        max     = "22050"
+        default = regex.match(line).captures[3]
+    end
+
+    regex = /public var ([A-z0-9]+)_([A-z0-9]+)/
+    if line =~ regex
+        k_prefix = regex.match(line).captures[0]
+        name     = regex.match(line).captures[1]
+
+        puts parameters
+        parameters.push({
+            :k_prefix => k_prefix,
+            :name => name,
+            :variable => name.uncapitalize,
+            :scope => scope,
+            :type => type,
+            :min => min,
+            :max => max,
+            :default => default
+        })
+    end
+
+end
+
+puts parameters
+
+File.open("templates/AKAUOperation.swift.erb") { |template|
+    erb = ERB.new( template.read, nil, '-' )
+    File.open("output/AKAU#{au_name}.swift", 'w+') {|f| f.write(erb.result) }
+    puts erb.result
+}
