62889dc7c3290a15e89b00cf78598a78a14a1fc1
Aurelius Prochazka <aure@aure.com>
2015-11-05 02:26:00 -0800

    Added an AKAudioPlayer

---
+++ AudioKit/AudioKit/Operations/Signal Generators/AKAudioPlayer.swift
@@ -0,0 +1,47 @@
+//
+//  AKAudioPlayer.swift
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 11/5/15.
+//  Copyright © 2015 AudioKit. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+public class AKAudioPlayer: AKOperation {
+    
+    var audioFile: AVAudioFile
+    var internalPlayer: AVAudioPlayerNode
+    
+    public init(_ file: String) {
+        let url = NSURL.fileURLWithPath(file, isDirectory: false)
+        audioFile = try! AVAudioFile(forReading: url)
+        print(audioFile.url)
+        internalPlayer = AVAudioPlayerNode()
+        AKManager.sharedInstance.engine.attachNode(internalPlayer)
+        internalPlayer.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        internalPlayer.volume = 0.3
+        super.init()
+        output = internalPlayer
+        
+    }
+    
+    
+    public func play() {
+        if !internalPlayer.playing {
+            internalPlayer.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        }
+        internalPlayer.play()
+    }
+    
+    public func pause() {
+        internalPlayer.pause()
+    }
+
+    
+    public func stop() {
+        internalPlayer.stop()
+    }
+
+}
