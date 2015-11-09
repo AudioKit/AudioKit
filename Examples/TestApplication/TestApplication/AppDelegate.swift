4d8a940f40ffc36a29ffff70bf9def605dae7632
Aurelius Prochazka <aure@aure.com>
2015-11-08 19:33:21 -0800

    Bringing TestApplication for OSX up to speed

--- Examples/TestApplication/TestApplication/AppDelegate.swift
+++ Examples/TestApplication/TestApplication/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import Cocoa
-import AVFoundation
 import AudioKit
 
 @NSApplicationMain
@@ -15,55 +14,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
     
-    var engine = AVAudioEngine()
-    var delay = AVAudioUnitDelay()
-    var effect = AVAudioUnit()
-
+    let audiokit = AKManager.sharedInstance
+    
+    let input = AKMicrophone()
+    var delay:  AKAUDelay?
+    var distortion: AKAUDistortion?
+    var jcReverb: AKChowningReverb?
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
-        
-        var componentDescription = AudioComponentDescription()
-        componentDescription.componentType = kAudioUnitType_Effect
-        
-        AUAudioUnit.registerSubclass(
-            AKMoogLadderAudioUnit.self,
-            asComponentDescription: componentDescription,
-            name: "Local AKMoogLadder",
-            version: UInt32.max)
-        
-        AVAudioUnit.instantiateWithComponentDescription(componentDescription, options: []) { avAudioUnit, error in
-            guard let avAudioUnitEffect = avAudioUnit else { return }
-            
-            self.effect = avAudioUnitEffect
-            self.engine.attachNode(avAudioUnitEffect)
-        }
-        
-        // Setup engine and node instances
-        let input  = engine.inputNode
-        let output = engine.outputNode
-
-        engine.attachNode(delay)
-
-        // Connect nodes
-        engine.connect(input!, to: delay,  format: nil)
-        engine.connect(delay,  to: effect, format: nil)
-        engine.connect(effect, to: output, format: nil)
-        
-        // Start the engine.
-        do {
-            try self.engine.start()
-        }
-        catch {
-            fatalError("Could not start engine. error: \(error).")
-        }
+        delay = AKAUDelay(input)
+        distortion = AKAUDistortion(delay!)
+        jcReverb = AKChowningReverb(distortion!)
+        audiokit.audioOutput = jcReverb
+        audiokit.start()
 
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
-
 
 }
 
