b1261e0db5568d5a7cd71961ec9d58baf6f45bb2
Aurelius Prochazka <aure@aure.com>
2015-11-09 11:14:31 -0800

    Making Csound wait time a parameter for some slower simulators

--- AudioKit/Core Classes/AKSettings.h
+++ AudioKit/Core Classes/AKSettings.h
@@ -23,6 +23,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic,readonly) UInt16 numberOfChannels;
 @property (nonatomic,readonly) float  zeroDBFullScaleValue;
 @property (nonatomic,readonly) BOOL MIDIEnabled;
+@property (nonatomic,readonly) float maximumWaitTime;
 
 // The following properties can be changed dynamically after AudioKit has been initalized
 @property (nonatomic) BOOL loggingEnabled, messagesEnabled;
