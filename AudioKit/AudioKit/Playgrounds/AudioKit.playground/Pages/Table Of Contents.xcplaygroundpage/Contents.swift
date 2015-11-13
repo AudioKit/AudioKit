0962874d8bf9792e837aaf301685e3588d79a92d
Aurelius Prochazka <aure@aure.com>
2015-11-12 16:13:31 -0800

    Table of Contents for playgrounds

---
+++ AudioKit/AudioKit/Playgrounds/AudioKit.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: # AudioKitAU Playgrounds
+//: ## Tutorials
+//: * [Introduction](Intro)
+//: * [Connecting Operations](Connecting%20Operations)
+//: ---
+//: ## Operation Examples
+//: * [AKAUBandPassFilter](AKAUBandPassFilter)
+//: * [AKAUDelay](AKAUDelay)
+//: * [AKAUDistortion](AKAUDistortion)
+//: * [AKAUDynamicsProcessor](AKAUDynamicsProcessor)
+//: * [AKAUHighPassFilter](AKAUHighPassFilter)
+//: * [AKAUHighShelfFilter](AKAUHighShelfFilter)
+//: * [AKAULowPassFilter](AKAULowPassFilter)
+//: * [AKAULowShelfFilter](AKAULowShelfFilter)
+//: * [AKAUParametricEQ](AKAUParametricEQ)
+//: * [AKAUPeakLimiter](AKAUPeakLimiter)
+//: * [AKAUReverb2](AKAUReverb2)
+//: * [AKAUTimePitch](AKAUTimePitch)
+//: * [AKAUVarispeed](AKAUVarispeed)
