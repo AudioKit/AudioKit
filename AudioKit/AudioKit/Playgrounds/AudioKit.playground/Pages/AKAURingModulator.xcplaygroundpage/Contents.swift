334b3f79af7b6c410cce878d8cb4f3603352ed63
Aurelius Prochazka <aure@aure.com>
2015-11-13 16:07:22 -0800

    Added AKAURingModulator window and playground

---
+++ AudioKit/AudioKit/Playgrounds/AudioKit.playground/Pages/AKAURingModulator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
+//:
+//: ---
+//:
+//: ## AKAURingModulator
+//: ### Add description
+import XCPlayground
+import AudioKit
+
+//: Change the source to "mic" to process your voice
+let source = "player"
+
+//: This is set-up, the next thing to change is in the next section:
+let audiokit = AKManager.sharedInstance
+let mic = AKMicrophone()
+let file = NSBundle.mainBundle().pathForResource("PianoBassDrumLoop", ofType: "wav")
+let player = AKAudioPlayer(file!)
+player.looping = true
+let playerWindow: AKAudioPlayerWindow
+let ringModulator: AKAURingModulator
+
+switch (source) {
+case "mic":
+    ringModulator = AKAURingModulator(mic)
+default:
+    ringModulator = AKAURingModulator(player)
+    playerWindow = AKAudioPlayerWindow(player)
+}
+//: Set the parameters of the Peak Limiter here
+ringModulator.frequency1 = 100 // Hertz
+ringModulator.frequency2 = 100 // Hertz
+ringModulator.balance = 50 // Percent
+ringModulator.mix = 50 // Percent
+
+var ringModulatorWindow = AKAURingModulatorWindow(ringModulator)
+
+audiokit.audioOutput = ringModulator
+audiokit.start()
+
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
