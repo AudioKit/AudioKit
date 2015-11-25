17a8f9e67a50c2889bee1179fd10c0b8472ff06c
Aurelius Prochazka <aure@aure.com>
2015-11-25 00:20:45 -0800

    Table of Contents for iOS Playgrounds

--- AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
@@ -1,25 +1,9 @@
-//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
-//:
+//: # AudioKit for iOS Playgrounds
+//: ## Tutorials
+//: * [Introduction](Intro)
+//: * [Connecting Operations](Connecting%20Operations)
 //: ---
-//:
-//: ## Oscillator
-//: ### Generating audio
-import XCPlayground
-import AudioKit
-
-let audiokit = AKManager.sharedInstance
-let oscillator = AKOscillator()
-oscillator.frequency = 0
-print(oscillator.output)
-audiokit.audioOutput = oscillator
-audiokit.start()
-
-while true {
-    let nowDouble = NSDate().timeIntervalSince1970
-    oscillator.frequency = abs(Float(sin(nowDouble*10))) * 400  + 400
-    usleep(80000)
-}
-
-XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
-
-//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
+//: ## Operation Examples
+//: * [AKAUBandPassFilter](AKAUBandPassFilter)
+//: * [AKMoogLadder](AKMoogLadder)
+//: * [AKOscillator](AKOscillator)
