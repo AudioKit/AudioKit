27f30a3c0ebc0f37834e279fe449d96807981988
Aurelius Prochazka <aure@aure.com>
2015-11-26 02:41:01 -0800

    Added AKWhiteNoise Playground

--- AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
@@ -3,23 +3,29 @@
 //: * [Introduction](Intro)
 //: * [Connecting Operations](Connecting%20Operations)
 //: ---
-//: ## Operation Examples
-//: * [Band Pass Filter](AKAUBandPassFilter)
-//: * [Decimator](AKAUDecimator)
-//: * [Delay](AKAUDelay)
-//: * [Distortion](AKAUDistortion)
-//: * [Dynamics Processor](AKAUDynamicsProcessor)
-//: * [High Pass Filter](AKAUHighPassFilter)
-//: * [High Shelf Filter](AKAUHighShelfFilter)
-//: * [Low Pass Filter](AKAULowPassFilter)
-//: * [Low  Shelf Filter](AKAULowShelfFilter)
-//: * [Parametric Equalizer](AKAUParametricEQ)
-//: * [Peak Limiter](AKAUPeakLimiter)
-//: * [Ring Modulator](AKAURingModulatr)
-//: * [Time Pitch](AKAUTimePitch)
-//: * [Moog Ladder Filter](AKMoogLadder)
+//: # Operations
+//: ## Signal Generators
+//: * [White Noise](AKWhiteNoise)
 //: ### Oscillators
 //: * [FM Oscillator](AKFMOscillator)
 //: * [Oscillator](AKOscillator)
 //: * [Phasor](AKPhasor)
 //: * [Sawtooth Oscillator](AKSawtoothOscillator)
+//: ## Signal Modifiers
+//: * [Delay](AKAUDelay)
+//: ### Dynamics
+//: * [Dynamics Processor](AKAUDynamicsProcessor)
+//: * [Peak Limiter](AKAUPeakLimiter)
+//: ### Effects
+//: * [Decimator](AKAUDecimator)
+//: * [Distortion](AKAUDistortion)
+//: * [Ring Modulator](AKAURingModulatr)
+//: * [Time Pitch](AKAUTimePitch)
+//: ### Filters
+//: * [Band Pass Filter](AKAUBandPassFilter)
+//: * [High Pass Filter](AKAUHighPassFilter)
+//: * [High Shelf Filter](AKAUHighShelfFilter)
+//: * [Low Pass Filter](AKAULowPassFilter)
+//: * [Low  Shelf Filter](AKAULowShelfFilter)
+//: * [Parametric Equalizer](AKAUParametricEQ)
+//: * [Moog Ladder Filter](AKMoogLadder)
