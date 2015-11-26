667fb2592a1a80a6bfe10315b5fff7d7caa7e38a
Aurelius Prochazka <aure@aure.com>
2015-11-26 13:41:39 -0800

    Added AKBitCrusher playground

--- AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
@@ -3,29 +3,18 @@
 //: * [Introduction](Intro)
 //: * [Connecting Operations](Connecting%20Operations)
 //: ---
-//: # Operations
-//: ## Signal Generators
+//: ## Operations
+//: ### Signal Generators
 //: * [White Noise](AKWhiteNoise)
-//: ### Oscillators
 //: * [FM Oscillator](AKFMOscillator)
 //: * [Oscillator](AKOscillator)
 //: * [Phasor](AKPhasor)
 //: * [Sawtooth Oscillator](AKSawtoothOscillator)
-//: ## Signal Modifiers
+//: ### Signal Modifiers
 //: * [Delay](AKAUDelay)
+//: * [Effects](Effects)
+//: * [Filters](Filters)
 //: ### Dynamics
 //: * [Dynamics Processor](AKAUDynamicsProcessor)
 //: * [Peak Limiter](AKAUPeakLimiter)
-//: ### Effects
-//: * [Decimator](AKAUDecimator)
-//: * [Distortion](AKAUDistortion)
-//: * [Ring Modulator](AKAURingModulatr)
-//: * [Time Pitch](AKAUTimePitch)
-//: ### Filters
-//: * [Band Pass Filter](AKAUBandPassFilter)
-//: * [High Pass Filter](AKAUHighPassFilter)
-//: * [High Shelf Filter](AKAUHighShelfFilter)
-//: * [Low Pass Filter](AKAULowPassFilter)
-//: * [Low  Shelf Filter](AKAULowShelfFilter)
-//: * [Parametric Equalizer](AKAUParametricEQ)
-//: * [Moog Ladder Filter](AKMoogLadder)
+
