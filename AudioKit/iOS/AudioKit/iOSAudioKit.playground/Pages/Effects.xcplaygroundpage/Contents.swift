8eab997511d30dba8edfda9f01ed608ebe2a365b
Aurelius Prochazka <aure@aure.com>
2015-11-26 13:42:19 -0800

    Added playground pages for effects and filters

---
+++ AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Effects.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
+//: # Effects
+//: ---------
+//: ## Apple AU Based Filters
+//: * [Decimator](AKAUDecimator)
+//: * [Distortion](AKAUDistortion)
+//: * [Ring Modulator](AKAURingModulator)
+//: * [Time Pitch](AKAUTimePitch)
+//:
+//: ---------
+//:
+//: ## AudioKit Effects
+//: * [Bitcrusher](AKBitCrusher)
+//:
+//: ---------
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
