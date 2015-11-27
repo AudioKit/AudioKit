d441b8e39c95469f27f4cc242ac34eeaf3408752
Aurelius Prochazka <aure@aure.com>
2015-11-27 01:12:41 -0800

    Add table stuff to AKOscillator playground

--- AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/AKOscillator.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/AKOscillator.xcplaygroundpage/Contents.swift
@@ -8,13 +8,26 @@ import XCPlayground
 import AudioKit
 
 let audiokit = AKManager.sharedInstance
-let oscillator = AKOscillator()
-oscillator.amplitude = 0.3
+
+let triangle = AKTable.standardTriangleWave()
+for value in triangle.values { value } // Click the eye icon ->
+
+let square = AKTable.standardSquareWave()
+for value in square.values { value } // Click the eye icon ->
+
+let sine = AKTable.standardSineWave()
+for value in sine.values { value } // Click the eye icon ->
+
+let sawtooth = AKTable.standardSawtoothWave()
+for value in sawtooth.values { value } // Click the eye icon ->
+
+//: Try changing the table to triangle, square, sine, or sawtooth
+let oscillator = AKOscillator(table: sine)
 audiokit.audioOutput = oscillator
 audiokit.start()
 
 while true {
-    oscillator.frequency.randomize(0,400)
+    oscillator.frequency.randomize(220,440)
     oscillator.amplitude.randomize(0, 0.5)
     usleep(200000)
 }
