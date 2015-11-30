e41dbab04c0f72694120334bed6f9922fbdd3a96
Aurelius Prochazka <aure@aure.com>
2015-11-30 12:01:23 -0800

    Tidying up playgrounds

---
+++ AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Oscillators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
+//: # Oscillators
+//: ---------
+//: * [FM Oscillator](AKFMOscillator)
+//: * [Oscillator](AKOscillator)
+//: * [Phasor](AKPhasor)
+//: * [Sawtooth Oscillator](AKSawtoothOscillator)
+//: * [Square Wave Oscillator](AKSquareWaveOscillator)
+//: * [Triangle Oscillator](AKTriangleOscillator)
+//: ---------
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
