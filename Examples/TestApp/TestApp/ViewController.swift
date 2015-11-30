9aa7e36d056cc839e45ac8ad8e37d1ba33feaf45
Aurelius Prochazka <aure@aure.com>
2015-11-30 01:39:17 -0800

    Updated templates for new class names

--- Examples/TestApp/TestApp/ViewController.swift
+++ Examples/TestApp/TestApp/ViewController.swift
@@ -11,7 +11,7 @@ import Foundation
 import AudioKit
 
 class ViewController: UIViewController {
-    
+
     let audiokit = AKManager.sharedInstance
     let input = AKMicrophone()
     var delay:  AKDelay?
@@ -28,53 +28,53 @@ class ViewController: UIViewController {
     var exs2 = AKSampler()
     var seq = AKSequencer(filename:"4tracks")
     var mixer = AKMixer()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         /*
         moog    = AKMoogLadder(input)
-        delay   = AKAUDelay(moog!)
+        delay   = AKDelay(moog!)
 //        allpass = AKFlatFrequencyResponseReverb(moog!)//, loopDuration: 0.1)
-        verb2  = AKAUReverb2(delay!)
-        limiter = AKAUPeakLimiter(verb2!)
+        verb2  = AKReverb2(delay!)
+        limiter = AKPeakLimiter(verb2!)
         //if let reverb = reverb { reverb.loadFactoryPreset(.Cathedral) }
         audiokit.audioOutput = limiter
         //print(verb2?.internalAudioUnit.debugDescription)
         //getAUParams((limiter?.internalAU)!)
-        
+
         */
 //        exs.loadEXS24("Sounds/sawPiano1")
 //        exs2.loadWav("Sounds/kylebell1-shrt")
-        
+
         mixer.connect(exs)
         mixer.connect(exs2)
         moog    = AKMoogLadder(mixer)
         verb2  = AKReverb2(moog!)
         audiokit.audioOutput = verb2
-        
+
         audiokit.start()
-        
+
         midi.openMidiOut("Session 1")
         midi.openMidiIn("Session 1")
         let defaultCenter = NSNotificationCenter.defaultCenter()
         let mainQueue = NSOperationQueue.mainQueue()
         //        defaultCenter.addObserverForName(AKMidiStatus.ControllerChange.name(), object: nil, queue: mainQueue, usingBlock: midiNotif)
-        
+
         //        defaultCenter.addObserverForName(AKMidiStatus.NoteOn.name(), object: nil, queue: mainQueue, usingBlock: midiNoteNotif)
-        
+
         seq.setGlobalMidiOutput(midi)
         seq.setLength(4)
         CAShow(seq.sequencePointer)
         print(seq.length)
-        
+
     }
-    
+
     func midiNotif(notif:NSNotification){
         print(notif.userInfo!)
         exs.playNote(Int(arc4random_uniform(127)))
         exs2.playNote(Int(arc4random_uniform(127)))
     }
-    
+
     func midiNoteNotif(notif:NSNotification){
         exs.playNote(Int((notif.userInfo?["note"])! as! NSNumber))
         exs2.playNote(Int((notif.userInfo?["note"])! as! NSNumber))
@@ -130,7 +130,7 @@ class ViewController: UIViewController {
         //  Get number of parameters in this unit (size in bytes really):
         var size: UInt32 = 0
         var propertyBool = DarwinBoolean(true)
-        
+
         AudioUnitGetPropertyInfo(inputAU, kAudioUnitProperty_ParameterList, kAudioUnitScope_Global, 0, &size, &propertyBool)
         let numParams = Int(size)/sizeof(AudioUnitParameterID)
         var parameterIDs = [AudioUnitParameterID](count: Int(numParams), repeatedValue: 0)
