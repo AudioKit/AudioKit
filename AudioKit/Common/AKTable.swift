6e71ac4b13314f82d7c2b1a4b4e51ef43aad254b
Aurelius Prochazka <aure@aure.com>
2015-11-29 22:11:06 -0800

    Turned AKTable into a struct

--- AudioKit/Common/AKTable.swift
+++ AudioKit/Common/AKTable.swift
@@ -8,83 +8,86 @@
 
 import Foundation
 
+public enum AKTableType: String {
+    case Sine, Triangle, Square, Sawtooth, ReverseSawtooth
+}
+
 /** A table of values accessible as a waveform or lookup mechanism */
-public class AKTable {
+public struct AKTable {
     
     // MARK: - Properties
     
     public var values = [Float]()
-    public var size = 4096
+    var size = 4096
+    var type: AKTableType
     
     // MARK: - Initializers with Generators
-    /** Initialize and set up the default table */
-    public init(size tableSize: Int = 4096) {
+    /** Initialize and set up the default table 
+    - parameter size: Size of the table (multiple of 2)
+    */
+    public init(_ tableType: AKTableType = .Sine, size tableSize: Int = 4096) {
+        type = tableType
         size = tableSize
+        switch type {
+        case .Sine:
+            self.standardSineWave()
+        case .Sawtooth:
+            self.standardSawtoothWave()
+        case .Triangle:
+            self.standardTriangleWave()
+        case .ReverseSawtooth:
+            self.standardReverseSawtoothWave()
+        case .Square:
+            self.standardSquareWave()
+        }
     }
     
-    /** Instantiate the table as a triangle wave
-    - parameter size: Size of the table (multiple of 2)
-    */
-    public class func standardTriangleWave(size tableSize: Int = 4096) -> AKTable {
-        let triangle = AKTable(size: tableSize)
-        let slope = Float(4.0) / Float(triangle.size)
-        for i in 0..<triangle.size {
-            if i < triangle.size / 2 {
-                triangle.values.append(slope * Float(i) - 1.0)
+    /** Instantiate the table as a triangle wave */
+    mutating func standardTriangleWave() {
+        values = [Float]()
+        let slope = Float(4.0) / Float(size)
+        for i in 0..<size {
+            if i < size / 2 {
+                values.append(slope * Float(i) - 1.0)
             } else {
-                triangle.values.append(slope * Float(-i) + 3.0)
+                values.append(slope * Float(-i) + 3.0)
             }
         }
-        return triangle
     }
 
-    /** Instantiate the table as a square wave
-    - parameter size: Size of the table (multiple of 2)
-    */
-    public class func standardSquareWave(size tableSize: Int = 4096) -> AKTable {
-        let square = AKTable(size: tableSize)
-        for i in 0..<square.size {
-            if i < square.size / 2 {
-                square.values.append(-1.0)
+    /** Instantiate the table as a square wave */
+    mutating func standardSquareWave() {
+        values = [Float]()
+        for i in 0..<size {
+            if i < size / 2 {
+                values.append(-1.0)
             } else {
-                square.values.append(1.0)
+                values.append(1.0)
             }
         }
-        return square
     }
     
-    /** Instantiate the table as a sawtooth wave
-    - parameter size: Size of the table (multiple of 2)
-    */
-    public class func standardSawtoothWave(size tableSize: Int = 4096) -> AKTable {
-        let sawtooth = AKTable(size: tableSize)
-        for i in 0..<sawtooth.size {
-            sawtooth.values.append(-1.0 + 2.0*Float(i)/Float(sawtooth.size))
+    /** Instantiate the table as a sawtooth wave */
+    mutating func standardSawtoothWave() {
+        values = [Float]()
+        for i in 0..<size {
+            values.append(-1.0 + 2.0*Float(i)/Float(size))
         }
-        return sawtooth
     }
 
-    /** Instantiate the table as a reverse sawtooth wave
-    - parameter size: Size of the table (multiple of 2)
-    */
-    public class func standardReverseSawtoothWave(size tableSize: Int = 4096) -> AKTable {
-        let sawtooth = AKTable(size: tableSize)
-        for i in 0..<sawtooth.size {
-            sawtooth.values.append(1.0 - 2.0*Float(i)/Float(sawtooth.size))
+    /** Instantiate the table as a reverse sawtooth wave */
+    mutating func standardReverseSawtoothWave() {
+        values = [Float]()
+        for i in 0..<size {
+            values.append(1.0 - 2.0*Float(i)/Float(size))
         }
-        return sawtooth
     }
 
-    /** Instantiate the table as a sine wave
-    - parameter size: Size of the table (multiple of 2)
-    */
-    public class func standardSineWave(size tableSize: Int = 4096) -> AKTable {
-        let sine = AKTable(size: tableSize)
-        for i in 0..<sine.size {
-            sine.values.append(sin(2 * 3.14159265 * Float(i)/Float(sine.size)))
+    /** Instantiate the table as a sine wave */
+    mutating func standardSineWave() {
+        values = [Float]()
+        for i in 0..<size {
+            values.append(sin(2 * 3.14159265 * Float(i)/Float(size)))
         }
-        return sine
     }
-
-
 }
