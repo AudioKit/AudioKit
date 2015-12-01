1086e093e631de4ce035a99f8a509323f2ab11e2
Aurelius Prochazka <aure@aure.com>
2015-12-01 00:31:46 -0800

    Latest comb filter

--- AudioKit/Common/Internals/Soundpipe/modules/comb.c
+++ AudioKit/Common/Internals/Soundpipe/modules/comb.c
@@ -1,18 +1,20 @@
 /*
- * Comb
- * 
+ * comb
+ *
  * This code has been extracted from the Csound opcode "comb".
  * It has been modified to work as a Soundpipe module.
- * 
+ *
  * Original Author(s): Barry Vercoe, John ffitch
  * Year: 1991
- * Location: Opcodes/ugens6.c
+ * Location: OOps/ugens6.c
  *
  */
 
 #include <math.h>
 #include <stdlib.h>
 #include "soundpipe.h"
+
+#define log001 (-(SPFLOAT)6.9078)    /* log(.001) */
 
 int sp_comb_create(sp_comb **p)
 {
@@ -32,7 +34,7 @@ int sp_comb_init(sp_data *sp, sp_comb *p, SPFLOAT looptime)
 {
     p->revtime = 3.5;
     p->looptime = looptime;
-    p->bufsize = 0.5 + looptime * sp->sr;
+    p->bufsize = (uint32_t) (0.5 + looptime * sp->sr);
     sp_auxdata_alloc(&p->aux, p->bufsize * sizeof(SPFLOAT));
     p->prvt = 0.0;
     p->coef = 0.0;
@@ -42,20 +44,28 @@ int sp_comb_init(sp_data *sp, sp_comb *p, SPFLOAT looptime)
 
 int sp_comb_compute(sp_data *sp, sp_comb *p, SPFLOAT *in, SPFLOAT *out)
 {
-    SPFLOAT samp = 0;
+    SPFLOAT tmp = 0;
     SPFLOAT coef = p->coef;
-
+    SPFLOAT outsamp = 0;
+    
     if(p->prvt != p->revtime) {
         p->prvt = p->revtime;
-        coef = p->coef = exp(-6.9078 * p->looptime / p->prvt);
+        SPFLOAT exp_arg = (SPFLOAT) (log001 * p->looptime / p->prvt);
+        if(exp_arg < -36.8413615) {
+            coef = p->coef = 0;
+        } else {
+            coef = p->coef = exp(exp_arg);
+        }
     }
-    sp_auxdata_getbuf(&p->aux, p->bufpos, &samp);
-    samp *= coef;
-    samp += *in;
-    sp_auxdata_setbuf(&p->aux, p->bufpos, &samp);
-    *out = samp;
-
+    sp_auxdata_getbuf(&p->aux, p->bufpos, &outsamp);
+//    *out = outsamp;
+    tmp = outsamp;
+    tmp *= coef;
+    tmp += *in;
+    sp_auxdata_setbuf(&p->aux, p->bufpos, &tmp);
+    *out = outsamp;
+    
     p->bufpos++;
-    p->bufpos %= p->bufsize; 
+    p->bufpos %= p->bufsize;
     return SP_OK;
 }
