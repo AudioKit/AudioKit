8892015d6e17110b7cb1c4dc664179d669534994
Nicholas Arner <nicholasarner@gmail.com>
2015-12-03 06:20:58 -0500

    Added a few notes/questions for Aure.

--- AudioKit/OSX/AudioKit/Playgrounds/AudioKit for OSX.playground/Pages/AKDistortion.xcplaygroundpage/Contents.swift
+++ AudioKit/OSX/AudioKit/Playgrounds/AudioKit for OSX.playground/Pages/AKDistortion.xcplaygroundpage/Contents.swift
@@ -2,8 +2,7 @@
 //:
 //: ---
 //:
-//: ## AK
-Distortion
+//: ## AKDistortion
 //: ### Add description
 import XCPlayground
 import AudioKit
@@ -49,6 +48,7 @@ distortion.ringModMix = 0 // Percent
 distortion.softClipGain = -6 // dB
 distortion.finalMix = 50 // Percent
 
+//Aure --- I'm currently getting an error of
 var distortionWindow = AKDistortionWindow(distortion)
 
 audiokit.audioOutput = distortion
