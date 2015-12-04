d828ec509d7e7daa25dbf9a8a11f2c9c52e46178
Aurelius Prochazka <aure@aure.com>
2015-12-04 13:28:16 -0800

    Playground pages for low and highPassFilter

---
+++ AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/AKP.lowPassFilter.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
+//:
+//: ---
+//:
+//: ## lowPassFilter
+//: ### Add description
+import XCPlayground
+import AudioKit
+
+//: This is set-up, the next thing to change is in the next section:
+let audiokit = AKManager.sharedInstance
+
+//: Filter setup
+let cutoff = AKP.scale(AKP.sine(frequency: 0.2.ak), minimumOutput: 100.ak, maximumOutput: 1000.ak)
+let filter = AKP.lowPassFilter(AKP.input, cutoffFrequency: cutoff)
+
+//: Noise Example
+let whiteNoise = AKWhiteNoise()
+whiteNoise.amplitude = 0.1 // Bring down the amplitude so that when it is mixed it is not so loud
+let noise = AKP.effect(whiteNoise, operation: filter)
+let noiseExample = AKMixer(noise)
+
+//: Music Example
+let bundle = NSBundle.mainBundle()
+let file = bundle.pathForResource("PianoBassDrumLoop", ofType: "wav")
+let player = AKAudioPlayer(file!)
+player.looping = true
+let filteredPlayer = AKP.effect(player, operation: filter)
+let musicExample = AKMixer(filteredPlayer)
+
+//: Change the volumes below to hear the examples
+noiseExample.volume = 1
+musicExample.volume = 0
+
+//: Mixdown and playback
+let mixer = AKMixer(noiseExample, musicExample)
+audiokit.audioOutput = mixer
+audiokit.start()
+player.play()
+
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
