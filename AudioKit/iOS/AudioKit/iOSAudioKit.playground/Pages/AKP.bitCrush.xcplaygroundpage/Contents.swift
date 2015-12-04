c85a658fa5c7069a6d75073173a7b6e3ac76081f
Aurelius Prochazka <aure@aure.com>
2015-12-04 01:29:52 -0800

    Added first crop of auto-generated operations

--- AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/AKP.bitCrush.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/AKP.bitCrush.xcplaygroundpage/Contents.swift
@@ -11,10 +11,10 @@ let audiokit = AKManager.sharedInstance
 
 //: ```AKP``` is basically shorthand for AKParameter, with type methods that return AKParameters that you can use in other operations
 let sine = AKP.sine(frequency: 1.ak)
-let sampleRate = AKP.scale(sine, minimum: 1000.ak, maximum: 5000.ak)
-let bitDepth   = AKP.scale(sine, minimum:    2.ak, maximum:   16.ak)
-let frequency  = AKP.scale(sine, minimum:  220.ak, maximum:  440.ak)
-let oscillator = AKP.sine(frequency: frequency)
+let sampleRate = AKP.scale(sine, minimumOutput: 1000.ak, maximumOutput: 5000.ak)
+let bitDepth   = AKP.scale(sine, minimumOutput:   16.ak, maximumOutput:    4.ak)
+let frequency  = AKP.scale(sine, minimumOutput:  220.ak, maximumOutput:  440.ak)
+let oscillator = AKP.sine(frequency: frequency, amplitude:   0.1.ak)
 
 let bitCrush = AKP.bitCrush(oscillator, bitDepth: bitDepth, sampleRate: sampleRate)
 
