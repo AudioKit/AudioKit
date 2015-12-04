d77c0df312a31a094c9e70538b22ec6f3d9ac6e3
Aurelius Prochazka <aure@aure.com>
2015-12-04 01:29:52 -0800

    Added first crop of auto-generated operations

--- AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Custom Generator.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Custom Generator.xcplaygroundpage/Contents.swift
@@ -11,7 +11,7 @@ let audiokit = AKManager.sharedInstance
 
 //: ```AKP``` is basically shorthand for AKParameter, with type methods that return AKParameters that you can use in other operations
 let slowSine = AKP.sine(preset: .Slow)
-let vibrato  = AKP.scale(slowSine, minimum: 220.ak, maximum: 440.ak)
+let vibrato  = AKP.scale(slowSine, minimumOutput: 220.ak, maximumOutput: 440.ak)
 
 let fastSine = AKP.sine(preset: .Fast)
 let volume   = AKP.scale(fastSine)
@@ -19,7 +19,7 @@ let volume   = AKP.scale(fastSine)
 let leftOutput  = AKP.sine(frequency: vibrato, amplitude: volume)
 let rightOutput = AKP.sine(frequency: vibrato, amplitude: leftOutput)
 
-let generator = AKNewGenerator(leftOutput, rightOutput)
+let generator = AKP.generator(leftOutput, rightOutput)
 
 audiokit.audioOutput = generator
 audiokit.start()
