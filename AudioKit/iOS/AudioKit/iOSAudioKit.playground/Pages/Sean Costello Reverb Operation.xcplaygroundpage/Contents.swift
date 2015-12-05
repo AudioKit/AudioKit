9734c73e1d11e83fdf4c7c876e3bab5579d081d3
Aurelius Prochazka <aure@aure.com>
2015-12-05 00:24:41 -0800

    Added costelloReverb operation

---
+++ AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Sean Costello Reverb Operation.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
+//:
+//: ---
+//:
+//: ## costelloReverb
+//: ### This is an example of building a sound generator from scratch
+import XCPlayground
+import AudioKit
+
+let audiokit = AKManager.sharedInstance
+let bundle = NSBundle.mainBundle()
+let file = bundle.pathForResource("808loop", ofType: "wav")
+let player = AKAudioPlayer(file!)
+player.looping = true
+
+let reverb = AKP.costelloReverb(AKP.input, feedback: 0.9.ak, cutoffFrequency: 10000.ak)
+let effect = AKP.effect(player, operation: reverb)
+
+audiokit.audioOutput = effect
+audiokit.start()
+player.play()
+
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
