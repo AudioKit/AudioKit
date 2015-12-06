3615c2bf8511bda1ef58a704e728804d2c5bf5eb
Nicholas Arner <nicholasarner@gmail.com>
2015-12-06 14:01:54 -0500

    Some minor copy edits, and a historical background to the Moog Ladder filter.

--- AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Moog Ladder Filter.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Moog Ladder Filter.xcplaygroundpage/Contents.swift
@@ -3,7 +3,8 @@
 //: ---
 //:
 //: ## AKMoogLadder
-//: ### Add description
+//: ### One of the coolest filters available in AudioKitAU is the Moog Ladder. It's based off of Robert Moog's iconic ladder filter, which was the first implementation of a voltage - controlled filter used in an analog synthesizer. As such, it was the first filter that gave the ability to use voltage control to determine the cutoff frequency of the filter. As we're dealing with a software implementation, and not an analog synthesizer, we don't have to worry about dealing with voltage control directly. However, by using this node, you can emulate some of the sounds of classic analog synthesizers in your app. 
+
 import XCPlayground
 import AudioKit
 
@@ -14,7 +15,7 @@ let file = bundle.pathForResource("PianoBassDrumLoop", ofType: "wav")
 let player = AKAudioPlayer(file!)
 let moogLadder = AKMoogLadder(player)
 
-//: Set the parameters of the low pass filter here
+//: Set the parameters of the low pass filter here. Hertz is a common unit of measurement for a frequency parameter. TODO: find a non-terrible explanation of what "Cents" are.
 
 moogLadder.cutoffFrequency = 300 // Hz
 moogLadder.resonance = 0.6  // Cents
