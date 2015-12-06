fbb1c54af66df346c727cddc260b33086a94b302
Aurelius Prochazka <aure@aure.com>
2015-12-06 00:57:14 -0800

    Fixing playground links

--- AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
@@ -3,19 +3,42 @@
 //: * [Non-Audio Tutorial](Non-Audio%20Tutorial)
 //: * [Introduction and Hello World](Introduction%20and%20Hello%20World)
 //: * [Connecting Nodes](Connecting%20Nodes)
+//: * [Mixing Nodes](Mixing%20Nodes)
 //: ---
-//: ## Nodes
-//: ### Generators
-//: * [White Noise](AKWhiteNoise)
-//: * [FM Oscillator](AKFMOscillator)
-//: * [Oscillator](AKOscillator)
-//: * [Phasor](AKPhasor)
-//: * [Sawtooth Oscillator](AKSawtoothOscillator)
-//: ### Effects
-//: * [Delay](AKDelay)
-//: * [Effects](Effects)
-//: * [Filters](Filters)
+//: ## Effect Processor Nodes
+//: ### Delays
+//: * [Delay](Delay)
+//: * [Variable Delay](Variable%20Delay)
+//: ### Distortion
+//: * [Bit Crush Effect](Bit%20Crush%20Effect)
+//: * [Decimator](Decimator)
+//: * [Apple Multi-Function Distortion](Apple%20Multi-Function%20Distortion)
+//: * [Ring Modulation](Ring%20Modulation)
+//: * [Time Stretching and Pitch Shifting](Time%20Stretching%20and%20Pitch%20Shifting)
 //: ### Dynamics
-//: * [Dynamics Processor](AKDynamicsProcessor)
-//: * [Peak Limiter](AKPeakLimiter)
-
+//: * [Dynamics Processor](Dynamics%20Processor)
+//: * [Peak Limiter](Peak%20Limiter)
+//: ### Filters
+//: * [Band Pass Filter](Band%20Pass%20Filter)
+//: * [Comb Filter](Comb%20Filter)
+//: * [High Pass Filter](High%20Pass%20Filter)
+//: * [High Shelf Filter](High%20Shelf%20Filter)
+//: * [Low Pass Filter](Low%20Pass%20Filter)
+//: * [Low  Shelf Filter](Low%20Shelf%20Filter)
+//: * [Moog Ladder Filter](Moog%20Ladder)
+//: * [Parametric Equalizer](Parametric%20Equalizer)
+//: ### Reverbs
+//: * [Sean Costello Reverb](Sean%20Costello%20Reverb)
+//: * [Flat Frequency Response Reverb](Flat%20Frequency%20Response%20Reverb)
+//: ---
+//: ## Generator Nodes
+//: ### Oscillators
+//: * [FM Oscillator](FM%20Oscillator)
+//: * [General Purpose Oscillator](General%20Purpose%20Oscillator)
+//: * [Phasor](Phasor)
+//: * [Sawtooth Oscillator](Sawtooth%20Oscillator)
+//: * [Square Wave Oscillator](Square%20Wave%20Oscillator)
+//: * [Triangle Oscillator](Triangle%20Oscillator)
+//: ### Noise Generators
+//: * [Pink Noise](Pink%20Noise)
+//: * [White Noise](White%20Noise)
