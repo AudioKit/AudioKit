eae4ee77692452fc4ea30c729829f0bf60332566
Aurelius Prochazka <aure@aure.com>
2015-12-06 00:57:14 -0800

    Fixing playground links

---
+++ AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Distortions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
+//: # Distortions
+//: ---------
+//: * [Bit Crush Effect](Bit%20Crush%20Effect)
+//: * [Decimator](Decimator)
+//: * [Apple Multi-Function Distortion](Apple%20Multi-Function%20Distortion)
+//: * [Ring Modulation](Ring%20Modulation)
+//: * [Time Stretching and Pitch Shifting](Time%20Stretching%20and%20Pitch%20Shifting)
+//: ---------
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
--- AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Oscillators.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/iOSAudioKit.playground/Pages/Oscillators.xcplaygroundpage/Contents.swift
@@ -1,11 +1,11 @@
 //: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
 //: # Oscillators
 //: ---------
-//: * [FM Oscillator](AKFMOscillator)
-//: * [Oscillator](AKOscillator)
-//: * [Phasor](AKPhasor)
-//: * [Sawtooth Oscillator](AKSawtoothOscillator)
-//: * [Square Wave Oscillator](AKSquareWaveOscillator)
-//: * [Triangle Oscillator](AKTriangleOscillator)
+//: * [FM Oscillator](FM%20Oscillator)
+//: * [General Purpose Oscillator](General%20Purpose%20Oscillator)
+//: * [Phasor](Phasor)
+//: * [Sawtooth Oscillator](Sawtooth%20Oscillator)
+//: * [Square Wave Oscillator](Square%20Wave%20Oscillator)
+//: * [Triangle Oscillator](Triangle%20Oscillator)
 //: ---------
 //: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
