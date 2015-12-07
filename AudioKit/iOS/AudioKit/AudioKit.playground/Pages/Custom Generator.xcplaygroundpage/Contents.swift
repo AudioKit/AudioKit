e9751eda68dca6a2d119a9b16d9209df6d10d692
Aurelius Prochazka <aure@aure.com>
2015-12-06 21:10:55 -0800

    Added basic math function to operations and used them in playgrounds

--- AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Custom Generator.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Custom Generator.xcplaygroundpage/Contents.swift
@@ -11,13 +11,13 @@ let audiokit = AKManager.sharedInstance
 
 //: ```AKP``` is basically shorthand for AKParameter, with type methods that return AKParameters that you can use in other operations
 let slowSine = AKP.sine(preset: .Slow)
-let vibrato  = AKP.scale(slowSine, minimumOutput: 220.ak, maximumOutput: 440.ak)
+let vibrato  = AKP.scale(slowSine, minimumOutput: (-10).ak, maximumOutput: 10.ak)
 
 let fastSine = AKP.sine(preset: .Fast)
-let volume   = AKP.scale(fastSine)
+let volume   = AKP.scale(fastSine) * 0.5
 
-let leftOutput  = AKP.sine(frequency: vibrato, amplitude: volume)
-let rightOutput = AKP.sine(frequency: vibrato, amplitude: leftOutput)
+let leftOutput  = AKP.sine(frequency: 440 + vibrato, amplitude: volume)
+let rightOutput = AKP.sine(frequency: 220 + vibrato, amplitude: volume)
 
 let generator = AKP.generator(leftOutput, rightOutput)
 
