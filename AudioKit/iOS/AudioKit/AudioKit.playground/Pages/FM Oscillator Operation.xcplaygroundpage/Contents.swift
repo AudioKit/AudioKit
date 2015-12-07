a0041ba73900cea9bb643d37fa2cc6a85ce169db
Aurelius Prochazka <aure@aure.com>
2015-12-07 00:55:44 -0800

    Playground cleanup

--- AudioKit/iOS/AudioKit/AudioKit.playground/Pages/FM Oscillator Operation.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/AudioKit.playground/Pages/FM Oscillator Operation.xcplaygroundpage/Contents.swift
@@ -18,12 +18,19 @@ let freq  = AKP.scale(sine3, minimumOutput: 900.ak, maximumOutput: 0.ak)
 let car   = AKP.scale(sine2, minimumOutput: 1.ak,   maximumOutput: 1.4.ak)
 let mod   = AKP.scale(sine,  minimumOutput: 1.ak,   maximumOutput: 3.ak)
 let index = sine3 * 3 + 5
-let oscillator = AKP.fmOscillator(baseFrequency: freq, carrierMultiplier: car, modulatingMultiplier: mod, modulationIndex: index, amplitude: 0.1.ak)
+let oscillator = AKP.fmOscillator(
+    baseFrequency: freq,
+    carrierMultiplier: car,
+    modulatingMultiplier: mod,
+    modulationIndex: index,
+    amplitude: 0.1.ak)
 
 //: Set up the nodes
 let generator = AKNode.generator(oscillator)
-let delay1 = AKDelay(generator, time: 0.01, feedback: 99, lowPassCutoff: 0, dryWetMix: 50)
-let delay2 = AKDelay(delay1, time: 0.1, feedback: 10, lowPassCutoff: 0, dryWetMix: 50)
+let delay1 = AKDelay(generator,
+    time: 0.01, feedback: 99, lowPassCutoff: 0, dryWetMix: 50)
+let delay2 = AKDelay(delay1,
+    time: 0.1, feedback: 10, lowPassCutoff: 0, dryWetMix: 50)
 let reverb = AKReverb(delay2, dryWetMix: 50)
 
 audiokit.audioOutput = reverb
