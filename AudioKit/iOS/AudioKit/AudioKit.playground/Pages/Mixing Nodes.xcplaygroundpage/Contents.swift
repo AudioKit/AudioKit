b41de07c5e0e0d71a11dbe6ff6b84216f23653d7
Jeff Cooper <jeff@southernworkstx.com>
2015-12-08 15:32:03 -0600

    adding different loops for mixer playground - these are synced and very obviously separate

--- AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Mixing Nodes.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Mixing Nodes.xcplaygroundpage/Contents.swift
@@ -10,8 +10,8 @@ import AudioKit
 //: This section prepares the players
 let audiokit = AKManager.sharedInstance
 let bundle = NSBundle.mainBundle()
-let file1 = bundle.pathForResource("808loop", ofType: "wav")
-let file2 = bundle.pathForResource("PianoBassDrumLoop", ofType: "wav")
+let file1 = bundle.pathForResource("drumloop", ofType: "wav")
+let file2 = bundle.pathForResource("guitarloop", ofType: "wav")
 let player1 = AKAudioPlayer(file1!)
 player1.looping = true
 
