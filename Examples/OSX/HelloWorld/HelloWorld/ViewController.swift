d6b09c00cd35491f046d23a63578f55a233fbb3d
Syed Haris Ali <syedhali07@gmail.com>
2015-12-07 21:07:11 -0800

    Added microphone feedback working

--- Examples/OSX/HelloWorld/HelloWorld/ViewController.swift
+++ Examples/OSX/HelloWorld/HelloWorld/ViewController.swift
@@ -13,21 +13,34 @@ class ViewController: NSViewController {
 
     let audiokit = AKManager.sharedInstance
     let oscillator = AKOscillator()
+    let mic = AKMicrophone();
+    @IBOutlet weak var plot: EZAudioPlot?;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        audiokit.audioOutput = oscillator
+        let mixer = AKMixer(mic);
+        mixer.volume = 0.25;
+        audiokit.audioOutput = mixer;
         audiokit.start()
+        
+        plot?.plotType = EZPlotType.Buffer;
+        
+        mixer.output?.installTapOnBus(0, bufferSize: 1024, format: nil) { [weak self] (buffer, time) -> Void in
+            if let strongSelf = self {
+                buffer.frameLength = 512;
+                strongSelf.plot?.updateBuffer(buffer.floatChannelData[0], withBufferSize: 1024);
+            }
+        };
     }
     
     
     @IBAction func toggleSound(sender: NSButton) {
-        if oscillator.amplitude >  0.5 {
-            oscillator.amplitude = 0
+        if oscillator.amplitude >  0.0 {
+            oscillator.amplitude = 0.0
             sender.title = "Play Sine Wave at 440Hz"
         } else {
-            oscillator.amplitude = 1
+            oscillator.amplitude = 0.25
             sender.title = "Stop Sine Wave at 440Hz"
         }
         sender.setNeedsDisplay()
