95261019eb72098cdb0ef60a98b481dcd779fd8f
Aurelius Prochazka <aure@aure.com>
2015-12-11 02:33:28 -0800

    Phasor need not be a node, just an operation

--- AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Oscillators.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Oscillators.xcplaygroundpage/Contents.swift
@@ -3,7 +3,6 @@
 //: ---------
 //: * [FM Oscillator](FM%20Oscillator)
 //: * [General Purpose Oscillator](General%20Purpose%20Oscillator)
-//: * [Phasor](Phasor)
 //: * [Sawtooth Oscillator](Sawtooth%20Oscillator)
 //: * [Square Wave Oscillator](Square%20Wave%20Oscillator)
 //: * [Triangle Oscillator](Triangle%20Oscillator)
