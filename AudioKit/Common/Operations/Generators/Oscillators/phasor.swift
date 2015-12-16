2531738edd75bd59e1649eace33c6e3fe9fd1aaf
Aurelius Prochazka <aure@aure.com>
2015-12-16 00:38:38 -0800

    Added global funcs for oscillator operations

--- AudioKit/Common/Operations/Generators/Oscillators/phasor.swift
+++ AudioKit/Common/Operations/Generators/Oscillators/phasor.swift
@@ -24,6 +24,12 @@ extension AKParameter {
     }
 }
 
+/** phasor: A normalized moving phase value. - Produces a normalized sawtooth wave between the values of 0 and 1. Phasors are
+ often used when building table-lookup oscillators.
+- returns: AKParameter
+- Parameter frequency: Frequency in cycles per second, or Hz. (Default: 1.0, Minimum: 0.0, Maximum: 1000.0)
+- Parameter phase: Initial phase (Default: 0, Minimum: , Maximum: )
+*/
 public func phasor(
     frequency frequency: AKParameter = 1.0.ak,
     phase: Double = 0
