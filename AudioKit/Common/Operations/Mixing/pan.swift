7141a004a68b44a135b4b8d404ae44704c7b375b
Aurelius Prochazka <aure@aure.com>
2015-12-18 22:04:41 -0800

    A bit of documentation correction

--- AudioKit/Common/Operations/Mixing/pan.swift
+++ AudioKit/Common/Operations/Mixing/pan.swift
@@ -10,8 +10,8 @@ import Foundation
 
 extension AKOperation {
 
-    /** paned: Panner - Panner
-     - returns: AKOperation
+    /** panned: Panner
+     - returns: AKStereoOperation
      - Parameter pan: Panning. A value of -1 is hard left, and a value of 1 is hard right, and 0 is center. (Default: 0, Minimum: -1, Maximum: 1)
      */
     public func panned(pan: AKOperation = 0.ak) -> AKStereoOperation {
