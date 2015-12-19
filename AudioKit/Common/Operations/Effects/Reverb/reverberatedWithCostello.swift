419b0d08d275ac93628bb1d1dd1713e066d8318c
Aurelius Prochazka <aure@aure.com>
2015-12-19 01:56:52 -0800

    Stereo reverb acts on stereo input differently than mono

--- AudioKit/Common/Operations/Effects/Reverb/reverberatedWithCostello.swift
+++ AudioKit/Common/Operations/Effects/Reverb/reverberatedWithCostello.swift
@@ -22,6 +22,25 @@ extension AKOperation {
         feedback feedback: AKOperation = 0.6.ak,
         cutoffFrequency: AKOperation = 4000.ak
         ) -> AKStereoOperation {
+            return AKStereoOperation("\(self)dup \(feedback)\(cutoffFrequency)revsc")
+    }
+}
+
+extension AKStereoOperation {
+    
+    /** reverberatedWithCostello: 8 delay line stereo FDN reverb - 8 delay line stereo FDN reverb, with feedback matrix based upon physical
+     modeling scattering junction of 8 lossless waveguides of equal characteristic
+     impedance.
+     
+     - returns: AKOperation
+     - parameter feedback: Feedback level in the range 0 to 1. 0.6 gives a good small 'live' room sound, 0.8 a small hall, and 0.9 a large hall. A setting of exactly 1 means infinite length, while higher values will make the opcode unstable. (Default: 0.6, Minimum: 0.0, Maximum: 1.0)
+     - parameter cutoffFrequency: Low-pass cutoff frequency. (Default: 4000, Minimum: 12.0, Maximum: 20000.0)
+     */
+    public func reverberatedWithCostello(
+        feedback feedback: AKOperation = 0.6.ak,
+        cutoffFrequency: AKOperation = 4000.ak
+        ) -> AKStereoOperation {
             return AKStereoOperation("\(self)\(feedback)\(cutoffFrequency)revsc")
     }
 }
+
