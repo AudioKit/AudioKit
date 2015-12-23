580019f7d868d21074a5b023e36e5623a62e3633
Aurelius Prochazka <aure@aure.com>
2015-12-22 19:54:22 -0800

    Proof of concept for AU type effects

--- AudioKit/Common/Nodes/Effects/Filters/AKHighShelfFilter/AKHighShelfFilter.swift
+++ AudioKit/Common/Nodes/Effects/Filters/AKHighShelfFilter/AKHighShelfFilter.swift
@@ -9,7 +9,7 @@
 import AVFoundation
 
 /** AudioKit version of Apple's HighShelfFilter Audio Unit */
-public class AKHighShelfFilter: AKNode {
+public struct AKHighShelfFilter: AKProtocolNode {
     
     private let cd = AudioComponentDescription(
         componentType: kAudioUnitType_Effect,
@@ -20,7 +20,7 @@ public class AKHighShelfFilter: AKNode {
     
     private var internalEffect = AVAudioUnitEffect()
     private var internalAU = AudioUnit()
-    public var internalAudioUnit:AudioUnit?
+    public var avAudioNode: AVAudioNode
     
     /** Cut Off Frequency (Hz) ranges from 10000 to 22050 (Default: 10000) */
     public var cutOffFrequency: Float = 10000 {
@@ -64,14 +64,12 @@ public class AKHighShelfFilter: AKNode {
             
             self.cutOffFrequency = cutOffFrequency
             self.gain = gain
-            super.init()
             
             internalEffect = AVAudioUnitEffect(audioComponentDescription: cd)
-            output = internalEffect
-            AKManager.sharedInstance.engine.attachNode(internalEffect)
-            AKManager.sharedInstance.engine.connect(input.output!, to: internalEffect, format: AKManager.format)
+            self.avAudioNode = internalEffect
+            AKManager.sharedInstance.engine.attachNode(self.avAudioNode)
+            AKManager.sharedInstance.engine.connect(input.output!, to: self.avAudioNode, format: AKManager.format)
             internalAU = internalEffect.audioUnit
-            internalAudioUnit = internalEffect.audioUnit
             
             AudioUnitSetParameter(internalAU, kHighShelfParam_CutOffFrequency, kAudioUnitScope_Global, 0, cutOffFrequency, 0)
             AudioUnitSetParameter(internalAU, kHighShelfParam_Gain,            kAudioUnitScope_Global, 0, gain, 0)
