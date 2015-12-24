29c9ed5bea229c4cc60466e6030e79ac05a9804c
Aurelius Prochazka <aure@aure.com>
2015-12-24 13:06:39 -0800

    Cleaning Out sub index playgrounds

--- AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Operations Index.xcplaygroundpage/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: # Operations
-//: ---
-//: Operations are used to make the internals of a single node.
-//:
-//: * [Bit Crush](Bit%20Crush%20Operation)
-//: * [Clip](Clip%20Operation)
-//: * [Sean Costello Reverb](Sean%20Costello%20Reverb%20Operation)
-//: * [FM Oscillator](AKFMOscillator)
-//: * [Oscillator](AKOscillator)
