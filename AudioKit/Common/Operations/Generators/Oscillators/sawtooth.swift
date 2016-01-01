54b974680babd9f4990cc1ee7304bfb238089ecc
Aurelius Prochazka <aure@aure.com>
2016-01-01 10:57:42 -0800

    Added generic waveform oscillators

---
+++ AudioKit/Common/Operations/Generators/Oscillators/sawtooth.swift
@@ -0,0 +1,41 @@
+//
+//  sawtooth.swift
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 1/1/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+import Foundation
+
+extension AKOperation {
+    
+    /// Simple sawtooth oscillator, not-band limited, can be used for LFO or wave,
+    /// but sawtoothWave is probably better for audio.
+    ///
+    /// - returns: AKOperation
+    /// - parameter frequency: In cycles per second, or Hz. (Default: 440, Minimum: 0.0, Maximum: 20000.0)
+    /// - parameter amplitude: Output Amplitude. (Default: 0.5, Minimum: 0.0, Maximum: 1.0)
+    ///
+    public static func sawtooth(
+        frequency frequency: AKParameter = 440,
+        amplitude: AKParameter = 0.5,
+        phase: AKParameter = 0
+        ) -> AKOperation {
+            return AKOperation("\"sawtooth\" 4096 \"0 -1 4095 1\" gen_line (\(frequency) \(amplitude) \(phase) \"sawtooth\" osc)")
+    }
+    
+    /// Simple reverse sawtooth oscillator, not-band limited, can be used for LFO or wave.
+    ///
+    /// - returns: AKOperation
+    /// - parameter frequency: In cycles per second, or Hz. (Default: 440, Minimum: 0.0, Maximum: 20000.0)
+    /// - parameter amplitude: Output Amplitude. (Default: 0.5, Minimum: 0.0, Maximum: 1.0)
+    ///
+    public static func reverseSawtooth(
+        frequency frequency: AKParameter = 440,
+        amplitude: AKParameter = 0.5,
+        phase: AKParameter = 0
+        ) -> AKOperation {
+            return AKOperation("\"sawtooth\" 4096 \"0 1 4095 -1\" gen_line (\(frequency) \(amplitude) \(phase) \"sawtooth\" osc)")
+    }
+}
---
+++ AudioKit/Common/Operations/Generators/Oscillators/square.swift
@@ -0,0 +1,27 @@
+//
+//  square.swift
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 1/1/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+import Foundation
+
+extension AKOperation {
+    
+    /// Simple square oscillator, not-band limited, can be used for LFO or wave,
+    /// but squareWave is probably better for audio.
+    ///
+    /// - returns: AKOperation
+    /// - parameter frequency: In cycles per second, or Hz. (Default: 440, Minimum: 0.0, Maximum: 20000.0)
+    /// - parameter amplitude: Output Amplitude. (Default: 0.5, Minimum: 0.0, Maximum: 1.0)
+    ///
+    public static func square(
+        frequency frequency: AKParameter = 440,
+        amplitude: AKParameter = 0.5,
+        phase: AKParameter = 0
+        ) -> AKOperation {
+            return AKOperation("\"square\" 4096 \"0 -1 2047 -1 2048 1 4095 1\" gen_line (\(frequency) \(amplitude) \(phase) \"square\" osc)")
+    }
+}
