f232073db40395e33d210832358aee6b2f086eaf
Aurelius Prochazka <aure@aure.com>
2016-01-03 00:23:05 -0800

    Commenting out these requirements on AKNode temporarily until all Apple-based nodes conform to the new AKNode.

--- AudioKit/Common/Nodes/AKNode.swift
+++ AudioKit/Common/Nodes/AKNode.swift
@@ -18,29 +18,29 @@ public protocol AKNode {
     /// Array of all connection points (effectively split output signal)
     var connectionPoints: [AVAudioConnectionPoint] { get set }
 
-    /// Tells whether the node is processing (ie. started, playing, or active)
-    var isStarted: Bool { get }
-
-    /// Tells whether the node is processing (ie. started, playing, or active)
-    var isPlaying: Bool { get }
-    
-    /// Tells whether the node is not processing (ie. stopped or bypassed)
-    var isStopped: Bool { get }
-
-    /// Tells whether the node is not processing (ie. stopped or bypassed)
-    var isBypassed: Bool { get }
-    
-    /// Function to start, play, or activate the node, all do the same thing
-    func start()
-    
-    /// Function to start, play, or activate the node, all do the same thing
-    func play()
-    
-    /// Function to stop or bypass the node, both are equivalent
-    func stop()
-
-    /// Function to stop or bypass the node, both are equivalent
-    func bypass()
+//    /// Tells whether the node is processing (ie. started, playing, or active)
+//    var isStarted: Bool { get }
+//
+//    /// Tells whether the node is processing (ie. started, playing, or active)
+//    var isPlaying: Bool { get }
+//    
+//    /// Tells whether the node is not processing (ie. stopped or bypassed)
+//    var isStopped: Bool { get }
+//
+//    /// Tells whether the node is not processing (ie. stopped or bypassed)
+//    var isBypassed: Bool { get }
+//    
+//    /// Function to start, play, or activate the node, all do the same thing
+//    func start()
+//    
+//    /// Function to start, play, or activate the node, all do the same thing
+//    func play()
+//    
+//    /// Function to stop or bypass the node, both are equivalent
+//    func stop()
+//
+//    /// Function to stop or bypass the node, both are equivalent
+//    func bypass()
     
 }
 
