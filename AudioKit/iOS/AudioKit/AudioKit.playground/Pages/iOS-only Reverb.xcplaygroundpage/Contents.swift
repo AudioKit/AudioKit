aeaf518acf06104af225e031c2ddeb7184354b99
Aurelius Prochazka <aure@aure.com>
2016-01-03 23:10:23 -0800

    Added reverb playgrounds

---
+++ AudioKit/iOS/AudioKit/AudioKit.playground/Pages/iOS-only Reverb.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
+//:
+//: ---
+//:
+//: ## iOS-only Reverb
+//: ### For some reason, this reverb is only supplied on iOS devices. It is super-powerful.
+
+import XCPlayground
+import AudioKit
+
+let audiokit = AKManager.sharedInstance
+
+let bundle = NSBundle.mainBundle()
+let file = bundle.pathForResource("drumloop", ofType: "wav")
+var player = AKAudioPlayer(file!)
+player.looping = true
+var reverb2 = AKReverb2(player)
+
+//: Set the parameters here
+reverb2.dryWetMix = 100 // CrossFade
+reverb2.gain = 0 // Decibels
+reverb2.minDelayTime = 0.008 // Secs
+reverb2.maxDelayTime = 0.050 // Secs
+reverb2.decayTimeAt0Hz = 3.0 // Secs
+reverb2.decayTimeAtNyquist = 0.5 // Secs
+reverb2.randomizeReflections = 0 // Integer
+
+audiokit.audioOutput = reverb2
+audiokit.start()
+
+player.play()
+
+//: Toggle processing on every loop
+AKPlaygroundLoop(every: 3.428) { () -> () in
+    if reverb2.isBypassed {
+        reverb2.start()
+    } else {
+        reverb2.bypass()
+    }
+    reverb2.isBypassed ? "Bypassed" : "Processing" // Open Quicklook for this
+}
+
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
