8b664d751f66ae634b1980bbc13ba2c347e5eddc
Aurelius Prochazka <aure@aure.com>
2016-01-06 21:34:08 -0800

    Swift Synth responding to midi

--- Examples/iOS/SwiftSynth/SwiftSynth/ViewController.swift
+++ Examples/iOS/SwiftSynth/SwiftSynth/ViewController.swift
@@ -7,17 +7,39 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
 
+    let audiokit = AKManager.sharedInstance
+    var midi = AKMidi()
+   
+    var midiInst:AKFMOscillatorInstrument?
+    var mixer = AKMixer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        midiInst = AKFMOscillatorInstrument(numVoicesInit: 12)
+//        midiInst = AKOscillatorInstrument(table: AKTable(.Sine), numVoicesInit: 12)
+        //AKMidiInstrument(inst: AKOscillator(), numVoicesInit: 12)
+        
+        midi.openMidiIn("Session 1")
+        midiInst!.enableMidi(midi.midiClient, name: "PolyOsc")
+        audiokit.audioOutput = midiInst
+        audiokit.start()
+        
+        let defaultCenter = NSNotificationCenter.defaultCenter()
+        let mainQueue = NSOperationQueue.mainQueue()
+        
+        defaultCenter.addObserverForName(AKMidiStatus.NoteOn.name(), object: nil, queue: mainQueue, usingBlock: midiNoteNotif)
+        defaultCenter.addObserverForName(AKMidiStatus.NoteOff.name(), object: nil, queue: mainQueue, usingBlock: midiNoteNotif)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    func midiNoteNotif(notif:NSNotification){
+        dump(notif)
+        midiInst?.handleMidiNotif(notif)
     }
 
 
