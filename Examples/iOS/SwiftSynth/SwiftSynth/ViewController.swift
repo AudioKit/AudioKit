e641e1a2319b799383045b953cb69a59c42a21be
Aurelius Prochazka <aure@aure.com>
2016-01-06 17:06:09 -0800

    Started the SwiftSynth example project

---
+++ Examples/iOS/SwiftSynth/SwiftSynth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSynth
+//
+//  Created by Aurelius Prochazka on 1/6/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
