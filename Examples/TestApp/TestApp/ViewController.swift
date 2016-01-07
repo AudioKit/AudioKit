6c3417567ea6cd1b9f997f30466f78e97b19f47f
Jeff Cooper <jeff@southernworkstx.com>
2016-01-06 21:45:48 -0600

    Added some demo examples creating basic instruments from basic oscillators

--- Examples/TestApp/TestApp/ViewController.swift
+++ Examples/TestApp/TestApp/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
     var exs = AKSampler()
     var exs2 = AKSampler()
     var osc = AKOscillator()
-    var midiInst:AKOscIsntrument?
+    var midiInst:AKNoiseIsntrument?
     var seq = AKSequencer(filename:"4tracks")
     var mixer = AKMixer()
 //    var spatMix = AKSpatialMixer()
@@ -51,7 +51,7 @@ class ViewController: UIViewController {
         
         let sawtooth = AKTable(.Sawtooth, size: 16)
         for value in sawtooth.values { value }
-        midiInst = AKOscIsntrument(table: sawtooth, numVoicesInit: 4)
+        midiInst = AKNoiseIsntrument(white: false, numVoicesInit: 4)
         mixer.connect(midiInst!)
 //        mixer.connect(exs2)
         moog    = AKMoogLadder(mixer)
