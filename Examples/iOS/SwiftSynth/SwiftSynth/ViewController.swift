85fb5f9376b68bade48014b606237b55b749e0e0
Aurelius Prochazka <aure@aure.com>
2016-01-07 23:06:57 -0800

    Added more effects nad sliders to swiftsynth

--- Examples/iOS/SwiftSynth/SwiftSynth/ViewController.swift
+++ Examples/iOS/SwiftSynth/SwiftSynth/ViewController.swift
@@ -17,25 +17,44 @@ class ViewController: UIViewController {
     var fm: AKFMOscillatorInstrument?
     var osc: AKOscillatorInstrument?
     var noise: AKNoiseInstrument?
+    var sourceMixer: AKMixer?
+    var bitCrusher: AKBitCrusher?
+    var bitCrushMixer: AKDryWetMixer?
+    var masterVolume: AKMixer?
+    var reverb: AKReverb2?
 
     @IBOutlet var statusLabel: UILabel!
     @IBOutlet var slider1: UISlider!
     @IBOutlet var slider2: UISlider!
     @IBOutlet var slider3: UISlider!
+    @IBOutlet var slider4: UISlider!
+    
+    
+    @IBOutlet var bottomSlider2: UISlider!
+    @IBOutlet var bottomSlider1: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         fm = AKFMOscillatorInstrument(voiceCount: 12)
+        fm!.output.volume = 0.4
         osc = AKOscillatorInstrument(table: AKTable(.Sine), voiceCount: 12)
         noise = AKNoiseInstrument(whitePinkMix: 0.5, voiceCount: 12)
+        noise!.output.volume = 0.2
         
         midi.openMidiIn("Session 1")
         fm!.enableMidi(midi.midiClient, name: "fm")
         osc!.enableMidi(midi.midiClient, name: "osc")
         noise!.enableMidi(midi.midiClient, name: "osc")
         
+        sourceMixer = AKMixer(osc!, fm!, noise!)
         
-        audiokit.audioOutput = AKMixer(osc!, fm!, noise!)
+        bitCrusher = AKBitCrusher(sourceMixer!)
+        bitCrushMixer = AKDryWetMixer(sourceMixer!, bitCrusher!, t: 0.5)
+        
+        masterVolume = AKMixer(bitCrusher!)
+        reverb = AKReverb2(masterVolume!)
+        reverb!.decayTimeAt0Hz = 2.0
+        audiokit.audioOutput = reverb
         audiokit.start()
         
         let defaultCenter = NSNotificationCenter.defaultCenter()
@@ -50,6 +69,17 @@ class ViewController: UIViewController {
         slider2.addTarget(self, action: "updateFMOscillatorVolume:", forControlEvents: .ValueChanged)
         slider3.value = Float(noise!.output.volume)
         slider3.addTarget(self, action: "updateNoiseVolume:", forControlEvents: .ValueChanged)
+        slider4.value = Float(bitCrusher!.bitDepth)
+        slider4.minimumValue = 0
+        slider4.maximumValue = 24
+        slider4.addTarget(self, action: "updateBitCrusherBitDepth:", forControlEvents: .ValueChanged)
+        
+        bottomSlider2.value = Float(masterVolume!.volume)
+        bottomSlider2.maximumValue = 2
+        bottomSlider2.addTarget(self, action: "updateMasterVolume:", forControlEvents: .ValueChanged)
+        
+        bottomSlider1.value = Float(reverb!.dryWetMix)
+        bottomSlider1.addTarget(self, action: "updateReverb:", forControlEvents: .ValueChanged)
         
     }
     
@@ -67,6 +97,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func updateFMOscillatorVolume(sender: UISlider) {
+        // You can also access all the FM oscillator parameters
+        // like fm!.carrierMultiplier, etc. but for this demo just doing volume
         fm!.output.volume = Double(sender.value)
         let status = String(format: "%0.2f", fm!.output.volume)
         statusLabel.text = "FM Oscillator: Volume: \(status)"
@@ -76,6 +108,28 @@ class ViewController: UIViewController {
         noise!.output.volume = Double(sender.value)
         let status = String(format: "%0.2f", noise!.output.volume)
         statusLabel.text = "Noise: Volume: \(status)"
+    }
+    
+    @IBAction func updateBitCrusherBitDepth(sender: UISlider) {
+        // Bitcrusher also has sample rate which you can control
+        // Plus there is a bitCrushMixer for dry/wet
+        // And the bitCrusher can be bypassed
+        bitCrusher!.bitDepth = Double(sender.value)
+        let status = String(format: "%0f", bitCrusher!.bitDepth)
+        statusLabel.text = "BitCrusher: Bit Depth Rate: \(status)"
+    }
+    
+    @IBAction func updateMasterVolume(sender: UISlider) {
+        masterVolume!.volume = Double(sender.value)
+        let status = String(format: "%0.2f", masterVolume!.volume)
+        statusLabel.text = "Master: Volume: \(status)"
+    }
+    
+    @IBAction func updateReverb(sender: UISlider) {
+        // Reverb has oodles of parameters to tweak, just doing dry/wet now
+        reverb!.dryWetMix = Double(sender.value)
+        let status = String(format: "%0.2f", reverb!.dryWetMix)
+        statusLabel.text = "Reverb: Mix: \(status)"
     }
 
 }
