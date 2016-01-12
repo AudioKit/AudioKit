2e94c014d64f56fd4b4eeaa4afe88304e0fe3f13
Aurelius Prochazka <aure@aure.com>
2016-01-11 19:33:02 -0800

    Removed out of date developer tools

--- DeveloperTools/interfaces/Filters/AUHighPassFilter.swift
+++
@@ -1,7 +0,0 @@
-// Parameters for the AUHipass unit
-
-// Global, Hz, 10->(SampleRate/2), 6900
-public var kHipassParam_CutoffFrequency: AudioUnitParameterID { get }
-
-// Global, dB, -20->40, 0
-public var kHipassParam_Resonance: AudioUnitParameterID { get }
--- DeveloperTools/interfaces/Filters/AULowPassFilter.swift
+++
@@ -1,7 +0,0 @@
-// Parameters for the AULowpass unit
-
-// Global, Hz, 10->(SampleRate/2), 6900
-public var kLowPassParam_CutoffFrequency: AudioUnitParameterID { get }
-
-// Global, dB, -20->40, 0
-public var kLowPassParam_Resonance: AudioUnitParameterID { get }
