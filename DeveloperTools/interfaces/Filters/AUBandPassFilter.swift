b7e0c39472ddeddcac2fcb95bdf58534012f3066
Aurelius Prochazka <aure@aure.com>
2016-01-11 19:33:02 -0800

    Removed out of date developer tools

--- DeveloperTools/interfaces/Filters/AUBandPassFilter.swift
+++
@@ -1,7 +0,0 @@
-// Parameters for the AUBandpass unit
-
-// Global, Hz, 20->(SampleRate/2), 5000
-public var kBandpassParam_CenterFrequency: AudioUnitParameterID { get }
-
-// Global, Cents, 100->12000, 600
-public var kBandpassParam_Bandwidth: AudioUnitParameterID { get }
--- DeveloperTools/interfaces/Filters/AULowShelfFilter.swift
+++
@@ -1,7 +0,0 @@
-// Parameters for the AULowShelfFilter unit
-
-// Global, Hz, 10->200, 80
-public var kAULowShelfParam_CutoffFrequency: AudioUnitParameterID { get }
-
-// Global, dB, -40->40, 0
-public var kAULowShelfParam_Gain: AudioUnitParameterID { get }
