fb85cde58e3ea75cdf5388febfbea1025f4c78bc
Aurelius Prochazka <aure@aure.com>
2016-01-13 01:53:09 -0800

    Knobs react to either horizontal or vertical changes, pretty naturally

--- Examples/iOS/SwiftSynth/SwiftSynth/CustomControls/Knob.swift
+++ Examples/iOS/SwiftSynth/SwiftSynth/CustomControls/Knob.swift
@@ -11,48 +11,41 @@ import UIKit
 @IBDesignable
 class Knob: UIView {
     
-    // Allow knobs to be controlled left-to-right or up-and-down
-    let directionHorizontal = false
-    
     // Knob properties
     var knobValue: CGFloat = 0.5 {
         didSet {
-            horizontalPercentage = knobValue
-            verticalPercentage = knobValue
+            if knobValue > 1 {
+                knobValue = 1
+            }
+            if knobValue < 0 {
+                knobValue = 0
+            }
         }
     }
-    let knobSensitivity: CGFloat = 0.005
-    var horizontalPercentage: CGFloat = 0.0
-    var verticalPercentage: CGFloat = 0.0
+    let knobSensitivity: CGFloat = 0.005 // Value change per pt I think
     var lastX: CGFloat = 0
     var lastY: CGFloat = 0
     
     func setPercentagesWithTouchPoint(touchPoint: CGPoint) {
+        
+        // Knobs assume up or right is increasing, and down or lef is decreasing
+        
         let horizontalChange = (touchPoint.x - lastX) * knobSensitivity
-        horizontalPercentage += horizontalChange
-        horizontalPercentage = max(0, min(horizontalPercentage, 1))
+        knobValue += horizontalChange
         
         let verticalChange = (touchPoint.y - lastY) * knobSensitivity
-        verticalPercentage -= verticalChange
-        verticalPercentage = max(0, min(verticalPercentage, 1))
-        
-        if directionHorizontal {
-            knobValue = horizontalPercentage
-        } else {
-            knobValue = verticalPercentage
-        }
+        knobValue -= verticalChange
+
         lastX = touchPoint.x
         lastY = touchPoint.y
     }
     
     func setMaximumValue() {
         knobValue = 1.0
-        verticalPercentage = 0
     }
     
     func setMinimumValue() {
         knobValue = 0.0
-        verticalPercentage = 1.0
     }
     
     // Scale any range to 0.0-1.0 for Knob position
