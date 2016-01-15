dfba21d2b890ed35183d6bbfc2e66510bbf9e730
Aurelius Prochazka <aure@aure.com>
2016-01-14 17:50:39 -0800

    Better PWM sounds for square wave playground

--- AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Square Wave Oscillator.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Square Wave Oscillator.xcplaygroundpage/Contents.swift
@@ -17,10 +17,9 @@ square.start()
 var t: Double = 0
 
 AKPlaygroundLoop(every: 0.12) {
-
-    square.pulseWidth = 0.99 -  abs(0.9 * cos(t))
+    square.pulseWidth = 0.75 -  0.24 * cos(10*t)
+    square.pulseWidth
     t = t + 0.01
-    square.amplitude = random(0, 0.2)
     
     let scale = [0,2,4,5,7,9,11,12]
     var note = scale.randomElement()
