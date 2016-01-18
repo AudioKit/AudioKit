12b9612b236e9dce614fa6678c8cf267b19a79b8
Matthew Michael <matthew.fecher@gmail.com>
2016-01-17 21:10:32 -0700

    refactor 'pwmKnob' to 'morphKnob'

--- Examples/iOS/SwiftSynth/SwiftSynth/Extensions/WaveformSegmentedView+Extensions.swift
+++ Examples/iOS/SwiftSynth/SwiftSynth/Extensions/WaveformSegmentedView+Extensions.swift
@@ -12,16 +12,16 @@ extension WaveformSegmentedView {
     
     func addOscWaveforms() {
         // Add segments
-        self.addSegmentWithTitle("", onSelectionImage: UIImage(named: "wave_saw_selected"), offSelectionImage: UIImage(named: "wave_saw"))
-        self.addSegmentWithTitle("", onSelectionImage: UIImage(named: "wave_square_selected"), offSelectionImage: UIImage(named: "wave_square"))
-        self.addSegmentWithTitle("", onSelectionImage: UIImage(named: "wave_sine_selected"), offSelectionImage: UIImage(named: "wave_sine"))
         self.addSegmentWithTitle("", onSelectionImage: UIImage(named: "wave_triangle_selected"), offSelectionImage: UIImage(named: "wave_triangle"))
+        self.addSegmentWithTitle("", onSelectionImage: UIImage(named: "wave_square_selected"), offSelectionImage: UIImage(named: "wave_square"))
+        self.addSegmentWithTitle("", onSelectionImage: UIImage(named: "wave_square_selected"), offSelectionImage: UIImage(named: "wave_square"))
+        self.addSegmentWithTitle("", onSelectionImage: UIImage(named: "wave_saw_selected"), offSelectionImage: UIImage(named: "wave_saw"))
     }
     
     func addLfoWaveforms() {
+        self.addSegmentWithTitle("", onSelectionImage: UIImage(named: "wave_triangle_selected"), offSelectionImage: UIImage(named: "wave_triangle"))
         self.addSegmentWithTitle("", onSelectionImage: UIImage(named: "wave_sine_selected"), offSelectionImage: UIImage(named: "wave_sine"))
         self.addSegmentWithTitle("", onSelectionImage: UIImage(named: "wave_square_selected"), offSelectionImage: UIImage(named: "wave_square"))
-        self.addSegmentWithTitle("", onSelectionImage: UIImage(named: "wave_triangle_selected"), offSelectionImage: UIImage(named: "wave_triangle"))
         self.addSegmentWithTitle("", onSelectionImage: UIImage(named: "wave_upSaw_selected"), offSelectionImage: UIImage(named: "wave_upSaw"))
         self.addSegmentWithTitle("", onSelectionImage: UIImage(named: "wave_downSaw_selected"), offSelectionImage: UIImage(named: "wave_downSaw"))
     }
