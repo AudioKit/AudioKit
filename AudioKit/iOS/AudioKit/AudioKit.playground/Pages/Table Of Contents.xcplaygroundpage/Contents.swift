7cb27c6f2fdfc694d5f765b09115784f58ec079e
Aurelius Prochazka <aure@aure.com>
2016-01-18 19:29:17 -0800

    Typo

--- AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
@@ -68,7 +68,7 @@
 //: Over the course of viewing the playgrounds so far, you've come across AKAudioPlayer repeatedly.  It is a simple neough class that it doesn't require a playground of its own, but there are some playback-oriented nodes that are very useful and cool.
 //:
 //: * [Sampler Instrument - EXS24](Sampler%20Instrument%20-%20EXS24)
-//: * [Sampler Instrument - Wav file](Sampler%20Instrument%20-%20Wav file)
+//: * [Sampler Instrument - Wav file](Sampler%20Instrument%20-%20Wav%20file)
 //: * [Sequencer - Single output](Sequencer%20-%20Single%20output)
 //: * [Sequencer - Multiple output](Sequencer%20-%20Multiple%20output)
 //: * [Time Stretching and Pitch Shifting](Time%20Stretching%20and%20Pitch%20Shifting)
