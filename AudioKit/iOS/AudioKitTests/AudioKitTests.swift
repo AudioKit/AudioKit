8cae1607bad0122dad49ce003588629c6bd5a069
Stephane Peter <megastep@megastep.org>
2016-01-28 15:45:38 -0800

    Fixed examples and AKTests for new AKManager API

--- AudioKit/iOS/AudioKitTests/AudioKitTests.swift
+++ AudioKit/iOS/AudioKitTests/AudioKitTests.swift
@@ -26,18 +26,16 @@ class AKTestCase: XCTestCase {
     func testFMOscillator() {
         let samples = duration * AKSettings.sampleRate
         
-        let audiokit = AKManager.sharedInstance
-        
         //: Try changing the table type to triangle or another AKTableType
         //: or changing the number of points to a smaller number (has to be a power of 2)
         let fm = AKFMOscillator(waveform: AKTable(.Sine, size: 4096))
-        audiokit.testOutput(fm, samples: samples)
-        audiokit.start()
-        print("IS testing %@", AKManager.sharedInstance.tester!.isTesting())
-        while AKManager.sharedInstance.tester!.isTesting() {
+        AKManager.testOutput(fm, samples: samples)
+        AKManager.start()
+        print("IS testing %@", AKManager.tester!.isTesting())
+        while AKManager.tester!.isTesting() {
             usleep(10)
         }
-        print("Got this MD5: \(AKManager.sharedInstance.tester!.getMD5())")
+        print("Got this MD5: \(AKManager.tester!.getMD5())")
     }
 
     
