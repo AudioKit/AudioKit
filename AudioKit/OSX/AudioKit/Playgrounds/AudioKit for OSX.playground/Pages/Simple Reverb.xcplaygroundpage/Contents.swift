eaf6425d4e9a482469fe895024f8582b905d6d80
Aurelius Prochazka <aure@aure.com>
2016-01-29 13:56:56 -0800

    Moving current OSX playgrounds into "Interactive" versions

--- AudioKit/OSX/AudioKit/Playgrounds/AudioKit for OSX.playground/Pages/Simple Reverb.xcplaygroundpage/Contents.swift
+++ AudioKit/OSX/AudioKit/Playgrounds/AudioKit for OSX.playground/Pages/Simple Reverb.xcplaygroundpage/Contents.swift
@@ -23,6 +23,16 @@ AudioKit.start()
 
 player.play()
 
+//: Toggle processing on every loop
+AKPlaygroundLoop(every: 3.428) { () -> () in
+    if reverb.isBypassed {
+        reverb.start()
+    } else {
+        reverb.bypass()
+    }
+    reverb.isBypassed ? "Bypassed" : "Processing" // Open Quicklook for this
+}
+
 XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
 
 //: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
