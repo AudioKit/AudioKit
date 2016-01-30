f5e7d867827e839d688e786f6ebf1217daa4accf
Jeff Cooper <jeff@southernworkstx.com>
2016-01-30 17:06:28 -0600

    midi monitor handles all messages

--- Examples/iOS/MidiMonitor/MidiMonitor/ViewController.swift
+++ Examples/iOS/MidiMonitor/MidiMonitor/ViewController.swift
@@ -20,12 +20,53 @@ class ViewController: UIViewController, AKMIDIListener {
         midi.openMIDIIn("Session 1")
         midi.addListener(self)
     }
+    func midiNoteOn(note: Int, velocity: Int, channel: Int) {
+        var newString = "Channel: \(channel+1) "
+        newString.appendContentsOf("noteOn: \(note) velocity: \(velocity) ")
+        updateText(newString)
+    }
+    func midiNoteOff(note: Int, velocity: Int, channel: Int) {
+        var newString = "Channel: \(channel+1) "
+        newString.appendContentsOf("noteOff: \(note) velocity: \(velocity) ")
+        updateText(newString)
+    }
     func midiController(controller: Int, value: Int, channel: Int) {
         var newString = "Channel: \(channel+1) "
         newString.appendContentsOf("controller: \(controller) value: \(value) ")
         updateText(newString)
     }
+    func midiAftertouchOnNote(note:Int, pressure:Int, channel:Int){
+        var newString = "Channel: \(channel+1) "
+        newString.appendContentsOf("midiAftertouchOnNote: \(note) pressure: \(pressure) ")
+        updateText(newString)
+    }
     
+    func midiAfterTouch(pressure:Int, channel:Int){
+        var newString = "Channel: \(channel+1) "
+        newString.appendContentsOf("midiAfterTouch pressure: \(pressure) ")
+        updateText(newString)
+    }
+    
+    func midiPitchWheel(pitchWheelValue:Int, channel:Int){
+        var newString = "Channel: \(channel+1) "
+        newString.appendContentsOf("midiPitchWheel: \(pitchWheelValue) ")
+        updateText(newString)
+    }
+    
+    func midiProgramChange(program:Int, channel:Int){
+        var newString = "Channel: \(channel+1) "
+        newString.appendContentsOf("programChange: \(program) ")
+        updateText(newString)
+    }
+    
+    func midiSystemCommand(data:[UInt8]){
+        print("MIDI System Command: \(AKMIDISystemCommand(rawValue: data[0])!)")
+        var newString = "MIDI System Command: \(AKMIDISystemCommand(rawValue: data[0])!) \n"
+        for (var i = 0; i < data.count; i++){
+            newString.appendContentsOf("\(data[i]) ")
+        }
+        updateText(newString)
+    }
     func updateText(input:String){
         dispatch_async(dispatch_get_main_queue(), {
            self.outputTextView.text = "\(input)\n\(self.outputTextView.text)"
