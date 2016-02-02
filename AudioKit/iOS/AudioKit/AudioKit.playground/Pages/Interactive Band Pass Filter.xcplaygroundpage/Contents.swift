3d56aaecc45ad52b8ca8990199daacafbdb31edc
Aurelius Prochazka <aure@aure.com>
2016-02-02 12:35:37 -0800

    Added interactive Band Pass Filter playground to iOS

---
+++ AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Interactive Band Pass Filter.xcplaygroundpage/Contents.swift
@@ -0,0 +1,77 @@
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
+//:
+//: ---
+//:
+//: ## Band Pass Filter
+//: ### Band-pass filters allow audio above a specified frequency range and bandwidth to pass through to an output. The center frequency is the starting point from where the frequency limit is set. Adjusting the bandwidth sets how far out above and below the center frequency the frequency band should be. Anything above that band should pass through.
+import XCPlayground
+import AudioKit
+
+let bundle = NSBundle.mainBundle()
+let file = bundle.pathForResource("mixloop", ofType: "wav")
+var player = AKAudioPlayer(file!)
+player.looping = true
+
+//: Next, we'll connect the audio sources to a band pass filter
+var bandPassFilter = AKBandPassFilter(player)
+
+//: Set the parameters of the band pass filter here
+bandPassFilter.centerFrequency = 5000 // Hz
+bandPassFilter.bandwidth = 600  // Cents
+
+AudioKit.output = bandPassFilter
+AudioKit.start()
+
+//: User Interface Set up
+
+class PlaygroundView: AKPlaygroundView {
+    
+    //: UI Elements we'll need to be able to access
+    var centerFrequencyLabel: Label?
+    var bandwidthLabel: Label?
+    
+    override func setup() {
+        addTitle("Band Pass Filter")
+        
+        addLabel("Audio Player")
+        addButton("Start", action: "start")
+        addButton("Stop", action: "stop")
+        
+        addLabel("Band Pass Filter Parameters")
+        centerFrequencyLabel = addLabel("Center Frequency: 5000 Hz")
+        addSlider("setCenterFrequency:", value: 5000, minimum: 20, maximum: 22050)
+        
+        bandwidthLabel = addLabel("Bandwidth 600 Cents")
+        addSlider("setBandwidth:", value: 600, minimum: 100, maximum: 12000)
+        self.backgroundColor = UIColor.whiteColor()
+    }
+    
+    //: Handle UI Events
+    
+    func start() {
+        player.play()
+    }
+    
+    func stop() {
+        player.stop()
+    }
+    
+    func setCenterFrequency(slider: Slider) {
+        bandPassFilter.centerFrequency = Double(slider.value)
+        let frequency = String(format: "%0.1f", bandPassFilter.centerFrequency)
+        centerFrequencyLabel!.text = "Center Frequency: \(frequency) Hz"
+    }
+    
+    func setBandwidth(slider: Slider) {
+        bandPassFilter.bandwidth = Double(slider.value)
+        let bandwidth = String(format: "%0.1f", bandPassFilter.bandwidth)
+        bandwidthLabel!.text = "Bandwidth: \(bandwidth) Cents"
+    }
+}
+
+
+let view = PlaygroundView(frame: CGRectMake(0, 0, 500, 350));
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+XCPlaygroundPage.currentPage.liveView = view
+
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
