eb546ba4da86166187d72345512a56090733de39
Aurelius Prochazka <aure@aure.com>
2016-02-02 22:32:51 -0800

    New interactive playgrouds for high/low pass/shelf filters on both systems

--- AudioKit/OSX/AudioKit/Playgrounds/AudioKit for OSX.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
+++ AudioKit/OSX/AudioKit/Playgrounds/AudioKit for OSX.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
@@ -23,15 +23,15 @@
 //:
 //: In these playgrounds you can update the parameters of a synthesized sound or effect using sliders.  While this may not teach you much about programming, it is educational to hear what parameter sound like under user control.
 //:
-//: * [Interactive Band Pass Filter](Interactive%20Band%20Pass%20Filter)
-//: * [Interactive Decimator](Interactive%20Decimator)
-//: * [Interactive Delay](Interactive%20Delay)
+//: * [Band Pass Filter](Band%20Pass%20Filter)
+//: * [Decimator](Decimator)
+//: * [Delay](Delay)
 //: * [Interactive Distortion](Interactive%20Distortion)
 //: * [Interactive Dynamics Processor](Interactive%20Dynamics%20Processor)
-//: * [Interactive High Pass Filter](Interactive%20High%20Pass%20Filter)
-//: * [Interactive High Shelf Filter](Interactive%20High%20Shelf%20Filter)
-//: * [Interactive Low Pass Filter](Interactive%20Low%20Pass%20Filter)
-//: * [Interactive Low  Shelf Filter](Interactive%20Low%20Shelf%20Filter)
+//: * [High Pass Filter](High%20Pass%20Filter)
+//: * [High Shelf Filter](High%20Shelf%20Filter)
+//: * [Low Pass Filter](Low%20Pass%20Filter)
+//: * [Low  Shelf Filter](Low%20Shelf%20Filter)
 //: * [Interactive Parametric Equalizer](Interactive%20Parametric%20Equalizer)
 //: * [Interactive Peak Limiter](Interactive%20Peak%20Limiter)
 //: * [Interactive Ring Modulator](Interactive%20Ring%20Modulator)
--- AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Low Pass Filter.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Low Pass Filter.xcplaygroundpage/Contents.swift
@@ -3,7 +3,8 @@
 //: ---
 //:
 //: ## Low Pass Filter
-//: ### A low-pass filter takes an audio signal as an input, and cuts out the high-frequency components of the audio signal, allowing for the lower     frequency components to "pass through" the filter.
+//: ### A low-pass filter takes an audio signal as an input, and cuts out the low-frequency components of the audio signal, allowing for the lower frequency components to "pass through" the filter.
+//:
 import XCPlayground
 import AudioKit
 
@@ -11,28 +12,77 @@ let bundle = NSBundle.mainBundle()
 let file = bundle.pathForResource("mixloop", ofType: "wav")
 var player = AKAudioPlayer(file!)
 player.looping = true
+
 var lowPassFilter = AKLowPassFilter(player)
 
-//: Set the parameters of the Low-Pass Filter here
-lowPassFilter.cutoffFrequency = 1000 // Hz
+//: Set the parameters here
+lowPassFilter.cutoffFrequency = 6900 // Hz
 lowPassFilter.resonance = 0 // dB
 
 AudioKit.output = lowPassFilter
 AudioKit.start()
 
-player.play()
+//: User Interface Set up
 
-//: Toggle processing on every loop
-AKPlaygroundLoop(every: 3.428) { () -> () in
-    if lowPassFilter.isBypassed {
+class PlaygroundView: AKPlaygroundView {
+    
+    //: UI Elements we'll need to be able to access
+    var cutoffFrequencyLabel: Label?
+    var resonanceLabel: Label?
+    
+    override func setup() {
+        addTitle("Low Pass Filter")
+        
+        addLabel("Audio Player")
+        addButton("Start", action: "start")
+        addButton("Stop", action: "stop")
+        
+        addLabel("Low Pass Filter Parameters")
+        
+        addButton("Process", action: "process")
+        addButton("Bypass", action: "bypass")
+        
+        cutoffFrequencyLabel = addLabel("Cut-off Frequency: 6900 Hz")
+        addSlider("setCutoffFrequency:", value: 6900, minimum: 10, maximum: 22050)
+        
+        resonanceLabel = addLabel("Resonance: 0 dB")
+        addSlider("setResonance:", value: 0, minimum: -20, maximum: 40)
+        
+    }
+    
+    //: Handle UI Events
+    
+    func start() {
+        player.play()
+    }
+    
+    func stop() {
+        player.stop()
+    }
+    
+    func process() {
         lowPassFilter.start()
-    } else {
+    }
+    
+    func bypass() {
         lowPassFilter.bypass()
     }
-    lowPassFilter.isBypassed ? "Bypassed" : "Processing" // Open Quicklook for this
+    func setCutoffFrequency(slider: Slider) {
+        lowPassFilter.cutoffFrequency = Double(slider.value)
+        let cutoffFrequency = String(format: "%0.1f", lowPassFilter.cutoffFrequency)
+        cutoffFrequencyLabel!.text = "Cut-off Frequency: \(cutoffFrequency) Hz"
+    }
+    
+    func setResonance(slider: Slider) {
+        lowPassFilter.resonance = Double(slider.value)
+        let resonance = String(format: "%0.1f", lowPassFilter.resonance)
+        resonanceLabel!.text = "Resonance: \(resonance) dB"
+    }
+    
 }
 
-
+let view = PlaygroundView(frame: CGRectMake(0, 0, 500, 550));
 XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+XCPlaygroundPage.currentPage.liveView = view
 
 //: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
