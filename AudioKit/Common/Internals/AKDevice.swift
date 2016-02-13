2a0f97244d7a5819db7d7433d845601d0e4d52f6
Aurelius Prochazka <aure@aure.com>
2016-02-12 18:14:33 -0800

    Code cleanup

--- AudioKit/Common/Internals/AKDevice.swift
+++ AudioKit/Common/Internals/AKDevice.swift
@@ -15,40 +15,47 @@ import AVFoundation
     public var name: String
     
     #if os(OSX)
+    
     /// The device identifier.
     public private(set) var deviceID: AudioDeviceID
+    
     #else
+    
     /// The device identifier.
     public private(set) var deviceID: String
+    
     #endif
 
     #if os(OSX)
+    
     /// Initialize the device
     ///
     /// - parameter name: The human-readable name for the device.
     /// - paramter deviceID: The device identifier.
-    public init(name: String, deviceID: AudioDeviceID)
-    {
+    ///
+    public init(name: String, deviceID: AudioDeviceID) {
         self.name = name
         self.deviceID = deviceID
         super.init()
     }
+    
     #else
+    
     /// Initialize the device
     ///
     /// - parameter name: The human-readable name for the device.
     /// - paramter deviceID: The device identifier.
-    public init(name: String, deviceID: String)
-    {
+    ///
+    public init(name: String, deviceID: String) {
         self.name = name
         self.deviceID = deviceID
         super.init()
     }
+    
     #endif
     
     /// Printable device description
-    public override var description: String
-    {
+    public override var description: String {
         return "<Device: \(name) (\(deviceID))>"
     }
 }
