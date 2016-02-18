792c299b9be3e1904ee2b6b6bf9085a62524ed19
Aurelius Prochazka <aure@aure.com>
2016-02-18 13:24:53 -0800

    Audio player clean up

--- AudioKit/Common/Nodes/Playback/Player/AKAudioPlayer.swift
+++ AudioKit/Common/Nodes/Playback/Player/AKAudioPlayer.swift
@@ -16,9 +16,9 @@ public class AKAudioPlayer: AKNode, AKToggleable {
     private var internalPlayer: AVAudioPlayerNode
     
     private var internalFile: String
-    private var sampleRate : Double = 1.0
-    private var totalFrameCount : Int64
-    private var initialFrameCount : Int64 = -1
+    private var sampleRate: Double = 1.0
+    private var totalFrameCount: Int64
+    private var initialFrameCount: Int64 = -1
     
     /// Boolean indicating whether or not to loop the playback
     public var looping = false
@@ -63,6 +63,7 @@ public class AKAudioPlayer: AKNode, AKToggleable {
         let audioFrameCount = UInt32(audioFile.length)
         audioFileBuffer = AVAudioPCMBuffer(PCMFormat: audioFormat, frameCapacity: audioFrameCount)
         try! audioFile.readIntoBuffer(audioFileBuffer)
+        
         // added for currentTime calculation later on
         sampleRate = audioFile.fileFormat.sampleRate
         totalFrameCount = Int64( audioFrameCount )
@@ -104,7 +105,7 @@ public class AKAudioPlayer: AKNode, AKToggleable {
         }
         internalPlayer.play()
         // get the initialFrameCount for currentTime as it's relative to the audio engine's time.
-        if (initialFrameCount == -1){
+        if initialFrameCount == -1 {
             if let t = internalPlayer.lastRenderTime {
                 initialFrameCount = t.sampleTime
             }
@@ -127,7 +128,7 @@ public class AKAudioPlayer: AKNode, AKToggleable {
         if internalPlayer.playing {
             if let time = internalPlayer.lastRenderTime {
                 // wrap the sampleTime by the totalFrameCount as sampleTime does not reset when audio loops.
-                return Double( ( Int64(time.sampleTime - initialFrameCount) % totalFrameCount) ) / sampleRate
+                return Double((Int64(time.sampleTime - initialFrameCount) % totalFrameCount)) / sampleRate
             }
         }
         return 0.0
