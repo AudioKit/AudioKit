c354207c052aadbc960463aec7923a075a28adac
Aurelius Prochazka <aure@aure.com>
2016-03-15 13:39:56 -0700

    Added inertia to 3-pole lowpass filter

--- AudioKit/Common/Nodes/Effects/Filters/Three Pole Low Pass Filter/AKThreePoleLowpassFilterAudioUnit.mm
+++ AudioKit/Common/Nodes/Effects/Filters/Three Pole Low Pass Filter/AKThreePoleLowpassFilterAudioUnit.mm
@@ -101,6 +101,8 @@
     distortionAUParameter.value = 0.5;
     cutoffFrequencyAUParameter.value = 1500;
     resonanceAUParameter.value = 0.5;
+    
+    _inertia = 0.0002;
 
     _kernel.setParameter(distortionAddress,      distortionAUParameter.value);
     _kernel.setParameter(cutoffFrequencyAddress, cutoffFrequencyAUParameter.value);
@@ -172,20 +174,24 @@
     _kernel.init(self.outputBus.format.channelCount, self.outputBus.format.sampleRate);
     _kernel.reset();
 
+    [self setUpParameterRamp];
+    
+    return YES;
+}
+
+- (void)setUpParameterRamp{
     /*
      While rendering, we want to schedule all parameter changes. Setting them
      off the render thread is not thread safe.
      */
     __block AUScheduleParameterBlock scheduleParameter = self.scheduleParameterBlock;
-
-    // Ramp over 20 milliseconds.
-    __block AUAudioFrameCount rampTime = AUAudioFrameCount(0.02 * self.outputBus.format.sampleRate);
-
+    
+    // Ramp over inertia time in seconds.
+    __block AUAudioFrameCount rampTime = AUAudioFrameCount(_inertia * self.outputBus.format.sampleRate);
+    
     self.parameterTree.implementorValueObserver = ^(AUParameter *param, AUValue value) {
         scheduleParameter(AUEventSampleTimeImmediate, rampTime, param.address, value);
     };
-
-    return YES;
 }
 
 - (void)deallocateRenderResources {
