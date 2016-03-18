4b23570198289affc8c435139945cbab6d4d86b8
Aurelius Prochazka <aure@aure.com>
2016-03-17 21:49:22 -0700

    Adding inertia to AKOscillator

--- AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Oscillator.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Oscillator.xcplaygroundpage/Contents.swift
@@ -17,6 +17,7 @@ class PlaygroundView: AKPlaygroundView {
 
     var frequencyLabel: Label?
     var amplitudeLabel: Label?
+    var inertiaLabel: Label?
 
     override func setup() {
         addTitle("Oscillator")
@@ -24,11 +25,14 @@ class PlaygroundView: AKPlaygroundView {
         addButton("Start", action: #selector(self.start))
         addButton("Stop", action: #selector(self.stop))
 
-        frequencyLabel = addLabel("Frequency: 440")
-        addSlider(#selector(self.setFrequency(_:)), value: 440, minimum: 200, maximum: 800)
+        frequencyLabel = addLabel("Frequency: \(oscillator.frequency)")
+        addSlider(#selector(self.setFrequency(_:)), value: oscillator.frequency, minimum: 200, maximum: 800)
 
-        amplitudeLabel = addLabel("Amplitude: 0.1")
-        addSlider(#selector(self.setAmplitude(_:)), value: 0.1)
+        amplitudeLabel = addLabel("Amplitude: \(oscillator.amplitude)")
+        addSlider(#selector(self.setAmplitude(_:)), value: oscillator.amplitude)
+
+        inertiaLabel = addLabel("Inertia: \(oscillator.inertia)")
+        addSlider(#selector(self.setInertia(_:)), value: oscillator.inertia)
     }
 
     func start() {
@@ -39,15 +43,21 @@ class PlaygroundView: AKPlaygroundView {
     }
 
     func setFrequency(slider: Slider) {
-        oscillator.ramp(frequency: Double(slider.value))
+        oscillator.frequency = Double(slider.value)
         let frequency = String(format: "%0.1f", oscillator.frequency)
         frequencyLabel!.text = "Frequency: \(frequency)"
     }
 
     func setAmplitude(slider: Slider) {
-        oscillator.ramp(amplitude: Double(slider.value))
+        oscillator.amplitude = Double(slider.value)
         let amp = String(format: "%0.3f", oscillator.amplitude)
         amplitudeLabel!.text = "Amplitude: \(amp)"
+    }
+    
+    func setInertia(slider: Slider) {
+        oscillator.inertia = Double(slider.value)
+        let inertia = String(format: "%0.3f", oscillator.inertia)
+        inertiaLabel!.text = "Inertia: \(inertia)"
     }
 }
 
