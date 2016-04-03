43347c66d84d92b3b973dc32d606861d8eefeb90
Aurelius Prochazka <aure@aure.com>
2016-04-03 00:15:58 -0700

    A little UI for the already cool Linear and Exp Segment Playground

--- AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Linear and Exponential Segment Operations.xcplaygroundpage/Contents.swift
+++ AudioKit/iOS/AudioKit/AudioKit.playground/Pages/Linear and Exponential Segment Operations.xcplaygroundpage/Contents.swift
@@ -8,17 +8,15 @@
 import XCPlayground
 import AudioKit
 
-
-//: Generate a new pew sound twice per second
-let updateRate = 2.0
+let updateRate = AKOperation.parameters(0)
 
 //: Vary the starting frequency and duration randomly
 let start = AKOperation.randomNumberPulse() * 2000 + 300
 let duration = AKOperation.randomNumberPulse()
-let frequency = AKOperation.lineSegment(AKOperation.trigger, start: start, end: 0, duration: duration)
+let frequency = AKOperation.lineSegment(AKOperation.metronome(updateRate), start: start, end: 0, duration: duration)
 
 //: Decrease the amplitude exponentially
-let amplitude = AKOperation.exponentialSegment(AKOperation.trigger, start: 0.3, end: 0.01, duration: 1.0 / updateRate)
+let amplitude = AKOperation.exponentialSegment(AKOperation.metronome(updateRate), start: 0.3, end: 0.01, duration: 1.0 / updateRate)
 let sine = AKOperation.sineWave(frequency: frequency, amplitude:  amplitude)
 
 let generator = AKOperationGenerator(operation:  sine)
@@ -26,7 +24,7 @@ let generator = AKOperationGenerator(operation:  sine)
 var delay = AKDelay(generator)
 
 //: Add some effects for good fun
-delay.time = 0.25 / updateRate
+delay.time = 0.125
 delay.feedback = 0.8
 var reverb = AKReverb(delay)
 reverb.loadFactoryPreset(.LargeHall)
@@ -34,12 +32,28 @@ reverb.loadFactoryPreset(.LargeHall)
 AudioKit.output = reverb
 AudioKit.start()
 
+generator.parameters = [2.0]
 generator.start()
 
-AKPlaygroundLoop(frequency: updateRate) {
-    generator.trigger()
+class PlaygroundView: AKPlaygroundView {
+    var speedLabel: Label?
+    
+    override func setup() {
+        addTitle("Segment Operations")
+        
+        speedLabel = addLabel("Update Rate: \(generator.parameters[0])")
+        addSlider(#selector(self.setSpeed(_:)), value: generator.parameters[0], minimum: 0.1, maximum: 25)
+        
+    }
+    
+    func setSpeed(slider: Slider) {
+        generator.parameters[0] = Double(slider.value)
+        speedLabel!.text = "Update Rate: \(String(format: "%0.3f", generator.parameters[0]))"
+    }
 }
 
+let view = PlaygroundView(frame: CGRect(x: 0, y: 0, width: 500, height: 650))
 XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+XCPlaygroundPage.currentPage.liveView = view
 
 //: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
