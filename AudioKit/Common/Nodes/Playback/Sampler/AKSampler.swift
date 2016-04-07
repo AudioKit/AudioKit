e780c0f72feb86055a0cc9ee246a7eaabbc5d273
Jeff Cooper <jeff@southernworkstx.com>
2016-04-07 16:44:15 -0500

    aupresettemplate initial setup

--- AudioKit/Common/Nodes/Playback/Sampler/AKSampler.swift
+++ AudioKit/Common/Nodes/Playback/Sampler/AKSampler.swift
@@ -579,4 +579,80 @@ public class AKSampler: AKNode {
         templateStr.appendContentsOf("</plist>\n")
         return templateStr
     }
+    
+    static public func generateAUPresetOpenInstrument()->String{
+        var templateStr:String = ""
+        templateStr = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+        templateStr.appendContentsOf("<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n")
+        templateStr.appendContentsOf("<plist version=\"1.0\">\n")
+        templateStr.appendContentsOf("    <dict>\n")
+        templateStr.appendContentsOf("        <key>AU version</key>\n")
+        templateStr.appendContentsOf("        <real>1</real>\n")
+        templateStr.appendContentsOf("        <key>Instrument</key>\n")
+        templateStr.appendContentsOf("        <dict>\n")
+        return templateStr
+    }
+    static public func generateAUPresetOpenLayers()->String{
+        var templateStr:String = ""
+        templateStr.appendContentsOf("            <key>Layers</key>\n")
+        templateStr.appendContentsOf("            <array>\n")
+        return templateStr
+    }
+    static public func generateAUPresetOpenLayer()->String{
+            var templateStr = ""
+            templateStr.appendContentsOf("                <dict>\n")
+            templateStr.appendContentsOf("                    <key>Amplifier</key>\n")
+            templateStr.appendContentsOf("                    <dict>\n")
+            templateStr.appendContentsOf("                        <key>ID</key>\n")
+            templateStr.appendContentsOf("                        <integer>0</integer>\n")
+            templateStr.appendContentsOf("                        <key>enabled</key>\n")
+            templateStr.appendContentsOf("                        <true/>\n")
+            templateStr.appendContentsOf("                    </dict>\n")
+            return templateStr
+    }
+    static public func generateAUPresetOpenConnections()->String{
+        var templateStr = ""
+        templateStr.appendContentsOf("                    <key>Connections</key>\n")
+        templateStr.appendContentsOf("                    <array>\n")
+        return templateStr
+    }
+    static public func generateAUPresetConnectionDict
+        (id:Int, source:Int, destination:Int, scale:Int, transform:Int = 1, invert:Bool = false)
+        ->String{
+        var templateStr = ""
+        templateStr.appendContentsOf("                        <dict>\n")
+        templateStr.appendContentsOf("                            <key>ID</key>\n")
+        templateStr.appendContentsOf("                            <integer>\(id)</integer>\n")
+        templateStr.appendContentsOf("                            <key>control</key>\n")
+        templateStr.appendContentsOf("                            <integer>0</integer>\n")
+        templateStr.appendContentsOf("                            <key>destination</key>\n")
+        templateStr.appendContentsOf("                            <integer>\(destination)</integer>\n")
+        templateStr.appendContentsOf("                            <key>enabled</key>\n")
+        templateStr.appendContentsOf("                            <true/>\n")
+        templateStr.appendContentsOf("                            <key>inverse</key>\n")
+        templateStr.appendContentsOf("                            <\((invert ? "true" : "false"))/>\n")
+        templateStr.appendContentsOf("                            <key>scale</key>\n")
+        templateStr.appendContentsOf("                            <real>\(scale)</real>\n")
+        templateStr.appendContentsOf("                            <key>source</key>\n")
+        templateStr.appendContentsOf("                            <integer>\(source)</integer>\n")
+        templateStr.appendContentsOf("                            <key>transform</key>\n")
+        templateStr.appendContentsOf("                            <integer>1</integer>\n")
+        templateStr.appendContentsOf("                        </dict>\n")
+        return templateStr
+    }
+    static public func generateAUPresetCloseConnections()->String{
+        var templateStr = ""
+        templateStr.appendContentsOf("                    </array>\n")
+        return templateStr
+    }
+    static public func generateAUPresetCloseLayer()->String{
+        var templateStr = ""
+        templateStr.appendContentsOf("                </dict>\n")
+        return templateStr
+    }
+    static public func generateAUPresetCloseLayers()->String{
+        var templateStr:String = ""
+        templateStr.appendContentsOf("            </array>\n")
+        return templateStr
+    }
 }
