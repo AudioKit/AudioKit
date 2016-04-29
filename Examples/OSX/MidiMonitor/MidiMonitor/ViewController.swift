10b84387004248aae79da2ebe1105c0d6f3a5f0c
Aurelius Prochazka <aure@aure.com>
2016-04-29 15:49:40 -0700

    Updated MIDI capabilities including OSX version of the MIDI Monitor

--- Examples/OSX/MidiMonitor/MidiMonitor/ViewController.swift
+++ Examples/OSX/MidiMonitor/MidiMonitor/ViewController.swift
@@ -2,23 +2,34 @@
 //  ViewController.swift
 //  MidiMonitor
 //
-//  Created by Aurelius Prochazka on 1/29/16.
+//  Created by Aurelius Prochazka on 4/29/16.
 //  Copyright © 2016 AudioKit. All rights reserved.
 //
 
-import UIKit
+import Cocoa
 import AudioKit
 
-class ViewController: UIViewController, AKMIDIListener {
-    @IBOutlet var outputTextView: UITextView!
+class ViewController: NSViewController, AKMIDIListener {
+    @IBOutlet var outputTextView: NSTextView!
+    @IBOutlet var sourcePopUpButton: NSPopUpButton!
     var midi = AKMIDI()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         midi.openMIDIIn("Session 1")
         midi.addListener(self)
+        
+        midi.printMIDIInputs()
+        midi.printMIDIDestinations()
+        
+        sourcePopUpButton.removeAllItems()
+        sourcePopUpButton.addItemsWithTitles(midi.inputNames)
+    }
+    
+    @IBAction func sourceChanged(sender: NSPopUpButton) {
+        midi.openMIDIIn(midi.inputNames[sender.indexOfSelectedItem])
     }
     
     func midiNoteOn(note: Int, velocity: Int, channel: Int) {
@@ -44,25 +55,25 @@ class ViewController: UIViewController, AKMIDIListener {
         newString.appendContentsOf("midiAftertouchOnNote: \(note) pressure: \(pressure) ")
         updateText(newString)
     }
-
+    
     func midiAfterTouch(pressure: Int, channel: Int) {
         var newString = "Channel: \(channel+1) "
         newString.appendContentsOf("midiAfterTouch pressure: \(pressure) ")
         updateText(newString)
     }
-
+    
     func midiPitchWheel(pitchWheelValue: Int, channel: Int) {
         var newString = "Channel: \(channel+1) "
         newString.appendContentsOf("midiPitchWheel: \(pitchWheelValue) ")
         updateText(newString)
     }
-
+    
     func midiProgramChange(program: Int, channel: Int) {
         var newString = "Channel: \(channel+1) "
         newString.appendContentsOf("programChange: \(program) ")
         updateText(newString)
     }
-
+    
     func midiSystemCommand(data: [UInt8]) {
         print("MIDI System Command: \(AKMIDISystemCommand(rawValue: data[0])!)")
         var newString = "MIDI System Command: \(AKMIDISystemCommand(rawValue: data[0])!) \n"
@@ -73,13 +84,19 @@ class ViewController: UIViewController, AKMIDIListener {
     }
     func updateText(input: String) {
         dispatch_async(dispatch_get_main_queue(), {
-           self.outputTextView.text = "\(input)\n\(self.outputTextView.text)"
+            self.outputTextView.string = "\(input)\n\(self.outputTextView.string!)"
         })
     }
-
+    
     @IBAction func clearText(sender: AnyObject) {
         dispatch_async(dispatch_get_main_queue(), {
-            self.outputTextView.text = ""
+            self.outputTextView.string = ""
         })
+    }
+    
+    override var representedObject: AnyObject? {
+        didSet {
+            // Update the view, if already loaded.
+        }
     }
 }
