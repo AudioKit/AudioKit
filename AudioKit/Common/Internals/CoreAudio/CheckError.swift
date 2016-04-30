56cae3df3cd808ff96b661bc4afd02fe18d6f007
Aurelius Prochazka <aure@aure.com>
2016-04-30 11:58:10 -0700

    Moved CheckError out of AKMIDI

---
+++ AudioKit/Common/Internals/CoreAudio/CheckError.swift
@@ -0,0 +1,158 @@
+//
+//  CheckError.swift
+//  AudioKit For OSX
+//
+//  Created by Aurelius Prochazka on 4/30/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+// Print out a more human readable error message
+///
+/// - parameter error: OSStatus flag
+///
+public func CheckError(error: OSStatus) {
+    if error == 0 {return}
+    switch error {
+    // AudioToolbox
+    case kAudio_ParamError:
+        print("Error: kAudio_ParamError \n")
+        
+    case kAUGraphErr_NodeNotFound:
+        print("Error: kAUGraphErr_NodeNotFound \n")
+        
+    case kAUGraphErr_OutputNodeErr:
+        print( "Error: kAUGraphErr_OutputNodeErr \n")
+        
+    case kAUGraphErr_InvalidConnection:
+        print("Error: kAUGraphErr_InvalidConnection \n")
+        
+    case kAUGraphErr_CannotDoInCurrentContext:
+        print( "Error: kAUGraphErr_CannotDoInCurrentContext \n")
+        
+    case kAUGraphErr_InvalidAudioUnit:
+        print( "Error: kAUGraphErr_InvalidAudioUnit \n")
+        
+    case kMIDIInvalidClient :
+        print( "kMIDIInvalidClient ")
+        
+    case kMIDIInvalidPort :
+        print( "Error: kMIDIInvalidPort ")
+        
+    case kMIDIWrongEndpointType :
+        print( "Error: kMIDIWrongEndpointType")
+        
+    case kMIDINoConnection :
+        print( "Error: kMIDINoConnection ")
+        
+    case kMIDIUnknownEndpoint :
+        print( "Error: kMIDIUnknownEndpoint ")
+        
+    case kMIDIUnknownProperty :
+        print( "Error: kMIDIUnknownProperty ")
+        
+    case kMIDIWrongPropertyType :
+        print( "Error: kMIDIWrongPropertyType ")
+        
+    case kMIDINoCurrentSetup :
+        print( "Error: kMIDINoCurrentSetup ")
+        
+    case kMIDIMessageSendErr :
+        print( "kError: MIDIMessageSendErr ")
+        
+    case kMIDIServerStartErr :
+        print( "kError: MIDIServerStartErr ")
+        
+    case kMIDISetupFormatErr :
+        print( "Error: kMIDISetupFormatErr ")
+        
+    case kMIDIWrongThread :
+        print( "Error: kMIDIWrongThread ")
+        
+    case kMIDIObjectNotFound :
+        print( "Error: kMIDIObjectNotFound ")
+        
+    case kMIDIIDNotUnique :
+        print( "Error: kMIDIIDNotUnique ")
+        
+    case kMIDINotPermitted:
+        print( "Error: kMIDINotPermitted: Have you enabled the audio background mode in your ios app?")
+        
+    case kAudioToolboxErr_InvalidSequenceType :
+        print( "Error: kAudioToolboxErr_InvalidSequenceType ")
+        
+    case kAudioToolboxErr_TrackIndexError :
+        print( "Error: kAudioToolboxErr_TrackIndexError ")
+        
+    case kAudioToolboxErr_TrackNotFound :
+        print( "Error: kAudioToolboxErr_TrackNotFound ")
+        
+    case kAudioToolboxErr_EndOfTrack :
+        print( "Error: kAudioToolboxErr_EndOfTrack ")
+        
+    case kAudioToolboxErr_StartOfTrack :
+        print( "Error: kAudioToolboxErr_StartOfTrack ")
+        
+    case kAudioToolboxErr_IllegalTrackDestination :
+        print( "Error: kAudioToolboxErr_IllegalTrackDestination")
+        
+    case kAudioToolboxErr_NoSequence :
+        print( "Error: kAudioToolboxErr_NoSequence ")
+        
+    case kAudioToolboxErr_InvalidEventType :
+        print( "Error: kAudioToolboxErr_InvalidEventType")
+        
+    case kAudioToolboxErr_InvalidPlayerState :
+        print( "Error: kAudioToolboxErr_InvalidPlayerState")
+        
+    case kAudioUnitErr_InvalidProperty :
+        print( "Error: kAudioUnitErr_InvalidProperty")
+        
+    case kAudioUnitErr_InvalidParameter :
+        print( "Error: kAudioUnitErr_InvalidParameter")
+        
+    case kAudioUnitErr_InvalidElement :
+        print( "Error: kAudioUnitErr_InvalidElement")
+        
+    case kAudioUnitErr_NoConnection :
+        print( "Error: kAudioUnitErr_NoConnection")
+        
+    case kAudioUnitErr_FailedInitialization :
+        print( "Error: kAudioUnitErr_FailedInitialization")
+        
+    case kAudioUnitErr_TooManyFramesToProcess :
+        print( "Error: kAudioUnitErr_TooManyFramesToProcess")
+        
+    case kAudioUnitErr_InvalidFile :
+        print( "Error: kAudioUnitErr_InvalidFile")
+        
+    case kAudioUnitErr_FormatNotSupported :
+        print( "Error: kAudioUnitErr_FormatNotSupported")
+        
+    case kAudioUnitErr_Uninitialized :
+        print( "Error: kAudioUnitErr_Uninitialized")
+        
+    case kAudioUnitErr_InvalidScope :
+        print( "Error: kAudioUnitErr_InvalidScope")
+        
+    case kAudioUnitErr_PropertyNotWritable :
+        print( "Error: kAudioUnitErr_PropertyNotWritable")
+        
+    case kAudioUnitErr_InvalidPropertyValue :
+        print( "Error: kAudioUnitErr_InvalidPropertyValue")
+        
+    case kAudioUnitErr_PropertyNotInUse :
+        print( "Error: kAudioUnitErr_PropertyNotInUse")
+        
+    case kAudioUnitErr_Initialized :
+        print( "Error: kAudioUnitErr_Initialized")
+        
+    case kAudioUnitErr_InvalidOfflineRender :
+        print( "Error: kAudioUnitErr_InvalidOfflineRender")
+        
+    case kAudioUnitErr_Unauthorized :
+        print( "Error: kAudioUnitErr_Unauthorized")
+        
+    default:
+        print("Error: \(error)")
+    }
+}
--- AudioKit/Common/MIDI/AKMIDI.swift
+++ AudioKit/Common/MIDI/AKMIDI.swift
@@ -232,17 +232,18 @@ public class AKMIDI {
     
     /// Array of input names
     public var inputNames: [String] {
+        var nameArray = [String]()
         let sourceCount = MIDIGetNumberOfSources()
-        var returnedArray = [String]()
+        
         for i in 0 ..< sourceCount {
-            let src = MIDIGetSource(i)
+            let source = MIDIGetSource(i)
             var inputName: Unmanaged<CFString>?
             inputName = nil
-            MIDIObjectGetStringProperty(src, kMIDIPropertyName, &inputName)
+            MIDIObjectGetStringProperty(source, kMIDIPropertyName, &inputName)
             let inputNameStr = (inputName?.takeRetainedValue())! as String
-            returnedArray.append(inputNameStr)
+            nameArray.append(inputNameStr)
         }
-        return returnedArray
+        return nameArray
     }
     
     /// Open a MIDI Output Port
@@ -280,18 +281,27 @@ public class AKMIDI {
         }
     }
     
-    /// Prints a list of all MIDI Destinations
+    /// Prints a list of all MIDI Inputs
     public func printMIDIDestinations() {
+        for destinationName in destinationNames {
+            print("Destination at \(destinationName)")
+        }
+    }
+    
+    /// Array of destination names
+    public var destinationNames: [String] {
+        var nameArray = [String]()
         let outputCount = MIDIGetNumberOfDestinations()
         print("MIDI Destinations:")
         for i in 0 ..< outputCount {
-            let src = MIDIGetDestination(i)
+            let destination = MIDIGetDestination(i)
             var endpointName: Unmanaged<CFString>?
             endpointName = nil
-            MIDIObjectGetStringProperty(src, kMIDIPropertyName, &endpointName)
+            MIDIObjectGetStringProperty(destination, kMIDIPropertyName, &endpointName)
             let endpointNameStr = (endpointName?.takeRetainedValue())! as String
-            print("Destination at \(endpointNameStr)")
-        }//end foreach midi destination
+            nameArray.append(endpointNameStr)
+        }
+        return nameArray
     }
     
     // MARK: - Sending MIDI
@@ -336,156 +346,5 @@ public class AKMIDI {
         let controlCommand: UInt8 = UInt8(0xB0) + UInt8(channel)
         let message: [UInt8] = [controlCommand, UInt8(control), UInt8(value)]
         self.sendMessage(message)
-    }
-}
-
-/// Print out a more human readable error message
-///
-/// - parameter error: OSStatus flag
-///
-public func CheckError(error: OSStatus) {
-    if error == 0 {return}
-    switch error {
-        // AudioToolbox
-    case kAudio_ParamError:
-        print("Error: kAudio_ParamError \n")
-        
-    case kAUGraphErr_NodeNotFound:
-        print("Error: kAUGraphErr_NodeNotFound \n")
-        
-    case kAUGraphErr_OutputNodeErr:
-        print( "Error: kAUGraphErr_OutputNodeErr \n")
-        
-    case kAUGraphErr_InvalidConnection:
-        print("Error: kAUGraphErr_InvalidConnection \n")
-        
-    case kAUGraphErr_CannotDoInCurrentContext:
-        print( "Error: kAUGraphErr_CannotDoInCurrentContext \n")
-        
-    case kAUGraphErr_InvalidAudioUnit:
-        print( "Error: kAUGraphErr_InvalidAudioUnit \n")
-        
-    case kMIDIInvalidClient :
-        print( "kMIDIInvalidClient ")
-        
-    case kMIDIInvalidPort :
-        print( "Error: kMIDIInvalidPort ")
-        
-    case kMIDIWrongEndpointType :
-        print( "Error: kMIDIWrongEndpointType")
-        
-    case kMIDINoConnection :
-        print( "Error: kMIDINoConnection ")
-        
-    case kMIDIUnknownEndpoint :
-        print( "Error: kMIDIUnknownEndpoint ")
-        
-    case kMIDIUnknownProperty :
-        print( "Error: kMIDIUnknownProperty ")
-        
-    case kMIDIWrongPropertyType :
-        print( "Error: kMIDIWrongPropertyType ")
-        
-    case kMIDINoCurrentSetup :
-        print( "Error: kMIDINoCurrentSetup ")
-        
-    case kMIDIMessageSendErr :
-        print( "kError: MIDIMessageSendErr ")
-        
-    case kMIDIServerStartErr :
-        print( "kError: MIDIServerStartErr ")
-        
-    case kMIDISetupFormatErr :
-        print( "Error: kMIDISetupFormatErr ")
-        
-    case kMIDIWrongThread :
-        print( "Error: kMIDIWrongThread ")
-        
-    case kMIDIObjectNotFound :
-        print( "Error: kMIDIObjectNotFound ")
-        
-    case kMIDIIDNotUnique :
-        print( "Error: kMIDIIDNotUnique ")
-        
-    case kMIDINotPermitted:
-        print( "Error: kMIDINotPermitted: Have you enabled the audio background mode in your ios app?")
-        
-    case kAudioToolboxErr_InvalidSequenceType :
-        print( "Error: kAudioToolboxErr_InvalidSequenceType ")
-        
-    case kAudioToolboxErr_TrackIndexError :
-        print( "Error: kAudioToolboxErr_TrackIndexError ")
-        
-    case kAudioToolboxErr_TrackNotFound :
-        print( "Error: kAudioToolboxErr_TrackNotFound ")
-        
-    case kAudioToolboxErr_EndOfTrack :
-        print( "Error: kAudioToolboxErr_EndOfTrack ")
-        
-    case kAudioToolboxErr_StartOfTrack :
-        print( "Error: kAudioToolboxErr_StartOfTrack ")
-        
-    case kAudioToolboxErr_IllegalTrackDestination :
-        print( "Error: kAudioToolboxErr_IllegalTrackDestination")
-        
-    case kAudioToolboxErr_NoSequence :
-        print( "Error: kAudioToolboxErr_NoSequence ")
-        
-    case kAudioToolboxErr_InvalidEventType :
-        print( "Error: kAudioToolboxErr_InvalidEventType")
-        
-    case kAudioToolboxErr_InvalidPlayerState :
-        print( "Error: kAudioToolboxErr_InvalidPlayerState")
-        
-    case kAudioUnitErr_InvalidProperty :
-        print( "Error: kAudioUnitErr_InvalidProperty")
-        
-    case kAudioUnitErr_InvalidParameter :
-        print( "Error: kAudioUnitErr_InvalidParameter")
-        
-    case kAudioUnitErr_InvalidElement :
-        print( "Error: kAudioUnitErr_InvalidElement")
-        
-    case kAudioUnitErr_NoConnection :
-        print( "Error: kAudioUnitErr_NoConnection")
-        
-    case kAudioUnitErr_FailedInitialization :
-        print( "Error: kAudioUnitErr_FailedInitialization")
-        
-    case kAudioUnitErr_TooManyFramesToProcess :
-        print( "Error: kAudioUnitErr_TooManyFramesToProcess")
-        
-    case kAudioUnitErr_InvalidFile :
-        print( "Error: kAudioUnitErr_InvalidFile")
-        
-    case kAudioUnitErr_FormatNotSupported :
-        print( "Error: kAudioUnitErr_FormatNotSupported")
-        
-    case kAudioUnitErr_Uninitialized :
-        print( "Error: kAudioUnitErr_Uninitialized")
-        
-    case kAudioUnitErr_InvalidScope :
-        print( "Error: kAudioUnitErr_InvalidScope")
-        
-    case kAudioUnitErr_PropertyNotWritable :
-        print( "Error: kAudioUnitErr_PropertyNotWritable")
-        
-    case kAudioUnitErr_InvalidPropertyValue :
-        print( "Error: kAudioUnitErr_InvalidPropertyValue")
-        
-    case kAudioUnitErr_PropertyNotInUse :
-        print( "Error: kAudioUnitErr_PropertyNotInUse")
-        
-    case kAudioUnitErr_Initialized :
-        print( "Error: kAudioUnitErr_Initialized")
-        
-    case kAudioUnitErr_InvalidOfflineRender :
-        print( "Error: kAudioUnitErr_InvalidOfflineRender")
-        
-    case kAudioUnitErr_Unauthorized :
-        print( "Error: kAudioUnitErr_Unauthorized")
-        
-    default:
-        print("Error: \(error)")
     }
 }
