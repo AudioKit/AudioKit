6ac06d3564cb106687219848eb7e675789e426aa
Aurelius Prochazka <aure@aure.com>
2016-04-30 17:31:04 -0700

    More AKMIDI linting

--- AudioKit/Common/MIDI/AKMIDIInstrument.swift
+++ AudioKit/Common/MIDI/AKMIDIInstrument.swift
@@ -90,13 +90,13 @@ public class AKMIDIInstrument: AKNode, AKMIDIListener {
         srcConnRefCon: UnsafeMutablePointer<Void>) -> Void {
             let packetCount = Int(packetList.memory.numPackets)
             let packet = packetList.memory.packet as MIDIPacket
-            var packetPtr: UnsafeMutablePointer<MIDIPacket> = UnsafeMutablePointer.alloc(1)
-            packetPtr.initialize(packet)
+            var packetPointer: UnsafeMutablePointer<MIDIPacket> = UnsafeMutablePointer.alloc(1)
+            packetPointer.initialize(packet)
             for _ in 0 ..< packetCount {
-                let event = AKMIDIEvent(packet: packetPtr.memory)
+                let event = AKMIDIEvent(packet: packetPointer.memory)
                 //the next line is unique for midiInstruments - otherwise this function is the same as AKMIDI
                 handleMIDI(UInt32(event.internalData[0]), data2: UInt32(event.internalData[1]), data3: UInt32(event.internalData[2]))
-                packetPtr = MIDIPacketNext(packetPtr)
+                packetPointer = MIDIPacketNext(packetPointer)
             }
     }
 }
