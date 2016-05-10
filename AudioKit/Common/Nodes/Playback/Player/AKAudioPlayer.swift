73a6abf2a7a58af86a90e96667828804146426e6
Aurelius Prochazka <aure@aure.com>
2016-05-09 17:49:37 -0700

    Added duration and playhead to AKAudioPlayer

--- AudioKit/Common/Nodes/Playback/Player/AKAudioPlayer.swift
+++ AudioKit/Common/Nodes/Playback/Player/AKAudioPlayer.swift
@@ -26,6 +26,11 @@ public class AKAudioPlayer: AKNode, AKToggleable {
     public var looping = false
     private var paused = false
     
+    /// Total duration of one loop through of the file
+    public var duration: Double {
+        return Double(totalFrameCount) / Double(sampleRate)
+    }
+    
     /// Output Volume (Default 1)
     public var volume: Double = 1.0 {
         didSet {
@@ -33,6 +38,19 @@ public class AKAudioPlayer: AKNode, AKToggleable {
                 volume = 0
             }
             internalPlayer.volume = Float(volume)
+        }
+    }
+    
+    /// Time within the audio file at the current time
+    public var playhead: Double {
+        if looping {
+            return currentTime % duration
+        } else {
+            if currentTime > duration {
+                return duration
+            } else {
+                return currentTime
+            }
         }
     }
     
@@ -103,7 +121,7 @@ public class AKAudioPlayer: AKNode, AKToggleable {
     
     /// Start playback
     public func start() {
-        if !internalPlayer.playing && !paused {
+        if (!internalPlayer.playing && !paused) || playhead == duration {
             var options = AVAudioPlayerNodeBufferOptions.Interrupts
             if looping {
                 options = .Loops
@@ -114,6 +132,7 @@ public class AKAudioPlayer: AKNode, AKToggleable {
                 options: options,
                 completionHandler: nil)
         }
+        
         internalPlayer.play()
         // get the initialFrameCount for currentTime as it's relative to the audio engine's time.
         if initialFrameCount == -1 {
@@ -140,7 +159,7 @@ public class AKAudioPlayer: AKNode, AKToggleable {
     }
     
     /// Current playback time (in seconds)
-    public var currentTime : Double {
+    public var currentTime: Double {
         
         if internalPlayer.playing {
             if let nodeTime = internalPlayer.lastRenderTime,
@@ -148,6 +167,7 @@ public class AKAudioPlayer: AKNode, AKToggleable {
                 return Double( Double( playerTime.sampleTime ) / playerTime.sampleRate ) + skippedToTime
             }
         }
+        
         return skippedToTime
     }
     
@@ -156,6 +176,7 @@ public class AKAudioPlayer: AKNode, AKToggleable {
     /// - parameter time: Time into the file at which to start playing back
     ///
     public func playFrom(time: Double) {
+        internalPlayer.stop()
         skippedToTime = time
         let startingFrame = Int64(sampleRate * time)
         let frameCount = UInt32(totalFrameCount - startingFrame)
@@ -170,8 +191,7 @@ public class AKAudioPlayer: AKNode, AKToggleable {
     }
     
     /// Replace the current audio file with a new audio file
-    public func replaceFile( newFile : String )
-    {
+    public func replaceFile(newFile: String) {
         internalFile = newFile
         reloadFile()
     }
