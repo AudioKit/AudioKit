e71de42823aa95868a07b78b205154d7729db1c1
Aurelius Prochazka <aure@aure.com>
2016-05-18 19:36:43 -0700

    Latest AKNodeRecorder.  Kinda works in playgrounds, may work better in an app.

--- AudioKit/Common/Internals/AKNodeRecorder.swift
+++ AudioKit/Common/Internals/AKNodeRecorder.swift
@@ -15,6 +15,7 @@ public class AKNodeRecorder {
     private var avAudioFile: AVAudioFile?
     private var settings: [String: AnyObject]
     private var format: AVAudioFormat
+    private var url: NSURL
     private var node: AKNode?
     public var isRecording = false
     
@@ -23,25 +24,39 @@ public class AKNodeRecorder {
     /// - parameter file: Path to the audio file
     /// - parameter node: Node to record (Defaults to the overall output)
     ///
-    public init(_ file: String, node: AKNode = AudioKit.output!) {
+    public init(
+        _ file: String,
+          node: AKNode = AudioKit.output!) {
         self.node = node
-        let url = NSURL.fileURLWithPath(file, isDirectory: false)
+        url = NSURL.fileURLWithPath(file, isDirectory: false)
+        
+        format = AudioKit.format
+        settings = AudioKit.format.settings
         do {
-            let audioFile = try! AVAudioFile(forReading: url)
+            let audioFile = try AVAudioFile(forReading: url)
             format = audioFile.processingFormat
             settings = audioFile.processingFormat.settings
             settings[AVLinearPCMIsNonInterleaved] = false
+        } catch {
+            print("Could not open file for reading the format from.")
+        }
+    }
+    
+    private func prepareToRecord() {
+        do {
             avAudioFile = try AVAudioFile(forWriting: url, settings: settings)
         } catch {
-            print("Could not open file.")
+            print("Could not open the audio file for writing")
         }
-
     }
     
     /// Record audio
     public func record() {
         if isRecording { return }
+        
+        prepareToRecord()
         isRecording = true
+        
         if let recordingNode = node {
             recordingNode.avAudioNode.installTapOnBus(0, bufferSize: 1024, format: format) {
                 (buffer, time) in
@@ -52,6 +67,7 @@ public class AKNodeRecorder {
                 }
             }
         }
+        print("Started recording.")
     }
     
     /// Stop recording
@@ -59,7 +75,7 @@ public class AKNodeRecorder {
         isRecording = false
         if let recordingNode = node {
             recordingNode.avAudioNode.removeTapOnBus(0)
-            print("stopping")
+            print("Stopped recording.")
         }
     }
 }
