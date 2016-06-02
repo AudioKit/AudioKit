ba24d0ba0db7c6d238d5878651b22518192be8e0
Aurelius Prochazka <aure@aure.com>
2016-06-02 16:24:38 -0700

    Clean up AKFader

--- AudioKit/Common/Nodes/Playback/Sequencer/AKFader.swift
+++ AudioKit/Common/Nodes/Playback/Sequencer/AKFader.swift
@@ -12,7 +12,7 @@ public enum CurveType {
     case Linear, EqualPower, Exponential
     
     public static func fromString(curve: String) -> CurveType {
-
+        
         switch curve {
         case "linear":
             return .Linear
@@ -31,14 +31,14 @@ public class AKFader {
     let π = M_PI
     
     var output: AKBooster?
-
-    var initialVolume: Double = 1.0 //make these only 0 or 1 right now
-    var finalVolume: Double = 0.0 //make these only 0 or 1 right now
+    
+    var initialVolume: Double = 1.0 // make these only 0 or 1 right now
+    var finalVolume: Double = 0.0   // make these only 0 or 1 right now
     
     var controlRate: Double = 1 / 30 // 30 times per second
     var curveType: CurveType = .Linear
-    var curvature: Double = 0.5 //doesn't apply to linear
-    var duration: Double = 1.0 //seconds
+    var curvature: Double = 0.5 // doesn't apply to linear
+    var duration: Double = 1.0 // seconds
     var offset: Double = 0.0
     var stepCounter = 0
     var stepCount = 0
@@ -46,14 +46,14 @@ public class AKFader {
     var fadeTimer = NSTimer()
     var fadeScheduleTimer = NSTimer()
     
-    /// Init with a single control output
+    /// Initialize the fader with a single control output
     public init(initialVolume: Double,
-         finalVolume: Double,
-         duration: Double = 1.0,
-         type: CurveType = .Exponential,
-         curvature: Double = 1.0,
-         offset: Double = 0.0,
-         output: AKBooster) {
+                finalVolume: Double,
+                duration: Double = 1.0,
+                type: CurveType = .Exponential,
+                curvature: Double = 1.0,
+                offset: Double = 0.0,
+                output: AKBooster) {
         
         self.initialVolume = initialVolume
         self.finalVolume = finalVolume
@@ -65,27 +65,33 @@ public class AKFader {
         self.output = output
     }
     
+    /// Schedule a fade to start after fireTime
+    ///
+    /// - parameter fireTime: Time to start the fade at
+    ///
     func scheduleFade(fireTime: Double) {
-        //this schedules a fade to start after fireTime
+        
         fadeScheduleTimer = NSTimer.scheduledTimerWithTimeInterval(
             fireTime + offset,
             target: self,
             selector: #selector(scheduleFadeTimer),
             userInfo: nil,
             repeats: false)
-        //print("scheduled for \(fireTime) @ \(duration) seconds")
+        
         output!.gain = initialVolume
     }
+    
     @objc func scheduleFadeTimer(timer: NSTimer) {
         startImmediately()
     }
     
-    public func start() { //starts the fade WITH the offset
+    /// Start the fade WITH the offset
+    public func start() {
         scheduleFade(0.0)
     }
-    public func startImmediately() { //skips the offset
-        //this starts the recurring timer
-        //print("starting fade \((finalVolume > initialVolume ? "up" : "down"))")
+    
+    /// Start the fade WITHOUT the offset
+    public func startImmediately() {
         stepCounter = 0
         
         if fadeTimer.valid {
@@ -101,10 +107,12 @@ public class AKFader {
     @objc func updateFade(timer: NSTimer) {
         let direction: Double = (initialVolume > finalVolume ? 1.0 : -1.0)
         
-        if stepCount == 0{
+        if stepCount == 0 {
             endFade()
-        }else if stepCounter <= stepCount {
-            let controlAmount: Double = Double(stepCounter) / Double(stepCount) //normalized 0-1 value
+        } else if stepCounter <= stepCount {
+            
+            // normalized 0-1 value
+            let controlAmount: Double = Double(stepCounter) / Double(stepCount)
             var scaledControlAmount: Double = 0.0
             
             switch curveType {
@@ -119,12 +127,12 @@ public class AKFader {
                                                           maximum: finalVolume,
                                                           taper: curvature)
             case .EqualPower:
-                scaledControlAmount = pow((0.5 + 0.5 * direction * cos(π * controlAmount)), 0.5) //direction will be negative if going up
+                //direction will be negative if going up
+                scaledControlAmount = pow((0.5 + 0.5 * direction * cos(π * controlAmount)), 0.5)
             }
             
             output!.gain = scaledControlAmount
             stepCounter += 1
-            //print(scaledControlAmount)
             
         } else {
             endFade()
@@ -132,13 +140,16 @@ public class AKFader {
         
     }//end updateFade
     
-    func endFade(){
+    func endFade() {
         fadeTimer.invalidate()
         output!.gain = finalVolume
         stepCounter = 0
     }
     
-    static func denormalize(input: Double, minimum: Double, maximum: Double, taper: Double) -> Double {
+    static func denormalize(input: Double,
+                            minimum: Double,
+                            maximum: Double,
+                            taper: Double) -> Double {
         if taper > 0 {
             // algebraic taper
             return minimum + (maximum - minimum) * pow(input, taper)
@@ -148,7 +159,7 @@ public class AKFader {
             var adjustedMaximum: Double = 0.0
             if minimum == 0 { adjustedMinimum = 0.00000000001 }
             if maximum == 0 { adjustedMaximum = 0.00000000001 }
-            return log(input / adjustedMinimum) / log(adjustedMaximum / adjustedMinimum);//not working right for 0 values
+            return log(input / adjustedMinimum) / log(adjustedMaximum / adjustedMinimum) // not working right for 0 values
         }
     }
     
@@ -157,22 +168,31 @@ public class AKFader {
                                     duration: Double = 1.0,
                                     type: CurveType = .Exponential,
                                     curvature: Double = 1.0,
-                                    controlRate: Double = 1/30) -> [Double] {
+                                    controlRate: Double = 1 / 30) -> [Double] {
         var curvePoints = [Double]()
         let stepCount = Int(floor(duration / controlRate))
         var counter = 0
         let direction: Double = source > target ? 1.0 : -1.0
         if counter <= stepCount {
-            let controlAmount: Double = Double(counter) / Double(stepCount) //normalised 0-1 value
+            
+            // normalized 0-1 value
+            let controlAmount = Double(counter) / Double(stepCount)
             var scaledControlAmount: Double = 0.0
             
             switch type {
             case .Linear:
-                scaledControlAmount = denormalize(controlAmount, minimum: source, maximum: target, taper: 1)
+                scaledControlAmount = denormalize(controlAmount,
+                                                  minimum: source,
+                                                  maximum: target,
+                                                  taper: 1)
             case .Exponential:
-                scaledControlAmount = denormalize(controlAmount, minimum: source, maximum: target, taper: curvature)
+                scaledControlAmount = denormalize(controlAmount,
+                                                  minimum: source,
+                                                  maximum: target,
+                                                  taper: curvature)
             case .EqualPower:
-                scaledControlAmount = pow((0.5 + 0.5 * direction * cos(M_PI * controlAmount)), 0.5) //direction will be negative if going up
+                //direction will be negative if going up
+                scaledControlAmount = pow((0.5 + 0.5 * direction * cos(M_PI * controlAmount)), 0.5)
             }
             curvePoints.append(scaledControlAmount)
             counter += 1
