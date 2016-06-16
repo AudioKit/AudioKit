1cb2cdcfc0065de4c91c5abe77c2ec21ec8fcb56
Aurelius Prochazka <aure@aure.com>
2016-06-16 14:59:10 -0700

    Added Mandolin from STK and created playgrounds

--- AudioKit/OSX/AudioKit/AudioKit for OSX.playground/Pages/Mandolin.xcplaygroundpage/Contents.swift
+++ AudioKit/OSX/AudioKit/AudioKit for OSX.playground/Pages/Mandolin.xcplaygroundpage/Contents.swift
@@ -1,7 +1,38 @@
-//: [Previous](@previous)
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
+//:
+//: ---
+//:
+//: ## Mandolin
+//: ### Physical model of a mandolin
+import AudioKit
+import XCPlayground
 
-import Foundation
+let playRate = 2.0
 
-var str = "Hello, playground"
+let mandolin = AKMandolin()
 
-//: [Next](@next)
+var delay  = AKDelay(mandolin)
+delay.time = 1.5 / playRate
+delay.dryWetMix = 0.3
+delay.feedback = 0.2
+
+let reverb = AKReverb(delay)
+
+AudioKit.output = reverb
+AudioKit.start()
+let scale = [0, 2, 4, 5, 7, 9, 11, 12]
+
+AKPlaygroundLoop(frequency: playRate) {
+    var note = scale.randomElement()
+    let octave = randomInt(2...5)  * 12
+    if random(0, 10) < 1.0 { note += 1 }
+    if !scale.contains(note % 12) { print("ACCIDENT!") }
+    
+    let frequency = (note+octave).midiNoteToFrequency()
+    if random(0, 6) > 1.0 {
+        mandolin.trigger(frequency: frequency)
+    }
+}
+
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
