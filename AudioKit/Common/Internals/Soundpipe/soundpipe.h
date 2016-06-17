17a44615539f4e85570816f3f829a57e77aa330b
Aurelius Prochazka <aure@aure.com>
2016-06-17 01:14:11 -0700

    Sporth updates

--- AudioKit/Common/Internals/Soundpipe/soundpipe.h
+++ AudioKit/Common/Internals/Soundpipe/soundpipe.h
@@ -1148,7 +1148,7 @@ typedef struct {
 
 int sp_tabread_create(sp_tabread **p);
 int sp_tabread_destroy(sp_tabread **p);
-int sp_tabread_init(sp_data *sp, sp_tabread *p, sp_ftbl *ft);
+int sp_tabread_init(sp_data *sp, sp_tabread *p, sp_ftbl *ft, int mode);
 int sp_tabread_compute(sp_data *sp, sp_tabread *p, SPFLOAT *in, SPFLOAT *out);
 typedef struct {
     SPFLOAT value;
