09ff6da343ae75945299dfe2968b7bf819a8b924
nuts23 <linouk23@gmail.com>
2016-06-19 23:35:20 +0300

    Added SongProcessor example for iOS

---
+++ Examples/iOS/SongProcessor/SongProcessor/PlayerState.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerState.swift
+//  SongProcessor
+//
+//  Created by Kanstantsin Linou on 6/19/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+import Foundation
+
+enum PlayerState {
+    case Playing, Stopped
+}
