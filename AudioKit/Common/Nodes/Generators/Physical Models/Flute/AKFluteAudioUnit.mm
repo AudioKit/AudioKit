85172eed410a8ab85b304bd0bc8733a344a181b9
Aurelius Prochazka <aure@aure.com>
2016-06-18 18:05:52 -0700

    Start to the AKFlute, needs a bit more work

---
+++ AudioKit/Common/Nodes/Generators/Physical Models/Flute/AKFluteAudioUnit.mm
@@ -0,0 +1,251 @@
+//
+//  AKFluteAudioUnit.mm
+//  AudioKit
+//
+//  Created by Aurelius Prochazka, revision history on Github.
+//  Copyright (c) 2016 Aurelius Prochazka. All rights reserved.
+//
+
+#import "AKFluteAudioUnit.h"
+#import "AKFluteDSPKernel.hpp"
+
+#import <AVFoundation/AVFoundation.h>
+#import "BufferedAudioBus.hpp"
+
+#import <AudioKit/AudioKit-Swift.h>
+
+@interface AKFluteAudioUnit()
+
+@property AUAudioUnitBus *outputBus;
+
+@property AUAudioUnitBusArray *outputBusArray;
+
+@property (nonatomic, readwrite) AUParameterTree *parameterTree;
+
+@end
+
+@implementation AKFluteAudioUnit {
+    // C++ members need to be ivars; they would be copied on access if they were properties.
+    AKFluteDSPKernel _kernel;
+
+    BufferedInputBus _inputBus;
+}
+@synthesize parameterTree = _parameterTree;
+
+- (void)setFrequency:(float)frequency {
+    _kernel.setFrequency(frequency);
+}
+- (void)setAmplitude:(float)amplitude {
+    _kernel.setAmplitude(amplitude);
+}
+
+- (void)triggerFrequency:(float)frequency amplitude:(float)amplitude {
+    _kernel.setFrequency(frequency);
+    _kernel.setAmplitude(amplitude);
+    _kernel.trigger();
+}
+
+- (void)start {
+    _kernel.start();
+}
+
+- (void)stop {
+    _kernel.stop();
+}
+
+- (BOOL)isPlaying {
+    return _kernel.started;
+}
+
+- (BOOL)isSetUp {
+    return _kernel.resetted;
+}
+
+- (instancetype)initWithComponentDescription:(AudioComponentDescription)componentDescription
+                                     options:(AudioComponentInstantiationOptions)options
+                                       error:(NSError **)outError {
+    self = [super initWithComponentDescription:componentDescription options:options error:outError];
+
+    if (self == nil) {
+        return nil;
+    }
+
+    // Initialize a default format for the busses.
+    AVAudioFormat *defaultFormat = [[AVAudioFormat alloc] initStandardFormatWithSampleRate:AKSettings.sampleRate
+                                                                                  channels:AKSettings.numberOfChannels];
+
+    // Create a DSP kernel to handle the signal processing.
+    _kernel.init(defaultFormat.channelCount, defaultFormat.sampleRate);
+
+        // Create a parameter object for the frequency.
+    AUParameter *frequencyAUParameter =
+    [AUParameterTree createParameterWithIdentifier:@"frequency"
+                                              name:@"Variable frequency. Values less than the initial frequency  will be doubled until it is greater than that."
+                                           address:frequencyAddress
+                                               min:0
+                                               max:22000
+                                              unit:kAudioUnitParameterUnit_Hertz
+                                          unitName:nil
+                                             flags:0
+                                      valueStrings:nil
+                               dependentParameters:nil];
+    // Create a parameter object for the amplitude.
+    AUParameter *amplitudeAUParameter =
+    [AUParameterTree createParameterWithIdentifier:@"amplitude"
+                                              name:@"Amplitude"
+                                           address:amplitudeAddress
+                                               min:0
+                                               max:1
+                                              unit:kAudioUnitParameterUnit_Generic
+                                          unitName:nil
+                                             flags:0
+                                      valueStrings:nil
+                               dependentParameters:nil];
+
+
+    // Initialize the parameter values.
+    frequencyAUParameter.value = 110;
+    amplitudeAUParameter.value = 0.5;
+
+    _rampTime = AKSettings.rampTime;
+
+    _kernel.setParameter(frequencyAddress,       frequencyAUParameter.value);
+    _kernel.setParameter(amplitudeAddress,       amplitudeAUParameter.value);
+
+    // Create the parameter tree.
+    _parameterTree = [AUParameterTree createTreeWithChildren:@[
+        frequencyAUParameter,
+        amplitudeAUParameter
+    ]];
+
+    // Create the input and output busses.
+    _inputBus.init(defaultFormat, 8);
+    _outputBus = [[AUAudioUnitBus alloc] initWithFormat:defaultFormat error:nil];
+
+    // Create the output bus arrays.
+    _outputBusArray = [[AUAudioUnitBusArray alloc] initWithAudioUnit:self
+                                                             busType:AUAudioUnitBusTypeOutput
+                                                              busses: @[_outputBus]];
+
+    // Make a local pointer to the kernel to avoid capturing self.
+    __block AKFluteDSPKernel *blockKernel = &_kernel;
+
+    // implementorValueObserver is called when a parameter changes value.
+    _parameterTree.implementorValueObserver = ^(AUParameter *param, AUValue value) {
+        blockKernel->setParameter(param.address, value);
+    };
+
+    // implementorValueProvider is called when the value needs to be refreshed.
+    _parameterTree.implementorValueProvider = ^(AUParameter *param) {
+        return blockKernel->getParameter(param.address);
+    };
+
+    self.maximumFramesToRender = 512;
+
+    return self;
+}
+
+#pragma mark - AUAudioUnit Overrides
+
+- (AUAudioUnitBusArray *)outputBusses {
+    return _outputBusArray;
+}
+
+- (BOOL)allocateRenderResourcesAndReturnError:(NSError **)outError {
+    if (![super allocateRenderResourcesAndReturnError:outError]) {
+        return NO;
+    }
+    if (self.outputBus.format.channelCount != _inputBus.bus.format.channelCount) {
+        if (outError) {
+            *outError = [NSError errorWithDomain:NSOSStatusErrorDomain
+                                            code:kAudioUnitErr_FailedInitialization
+                                        userInfo:nil];
+        }
+        // Notify superclass that initialization was not successful
+        self.renderResourcesAllocated = NO;
+
+        return NO;
+    }
+    _inputBus.allocateRenderResources(self.maximumFramesToRender);
+
+    _kernel.init(self.outputBus.format.channelCount, self.outputBus.format.sampleRate);
+    _kernel.reset();
+
+    [self setUpParameterRamp];
+
+    return YES;
+}
+
+- (void)setUpParameterRamp {
+    /*
+     While rendering, we want to schedule all parameter changes. Setting them
+     off the render thread is not thread safe.
+     */
+    __block AUScheduleParameterBlock scheduleParameter = self.scheduleParameterBlock;
+
+    // Ramp over rampTime in seconds.
+    __block AUAudioFrameCount rampTime = AUAudioFrameCount(_rampTime * self.outputBus.format.sampleRate);
+
+    self.parameterTree.implementorValueObserver = ^(AUParameter *param, AUValue value) {
+        scheduleParameter(AUEventSampleTimeImmediate, rampTime, param.address, value);
+    };
+}
+
+- (void)deallocateRenderResources {
+    [super deallocateRenderResources];
+    _kernel.destroy();
+
+    _inputBus.deallocateRenderResources();
+
+    // Make a local pointer to the kernel to avoid capturing self.
+    __block AKFluteDSPKernel *blockKernel = &_kernel;
+
+    // Go back to setting parameters instead of scheduling them.
+    self.parameterTree.implementorValueObserver = ^(AUParameter *param, AUValue value) {
+        blockKernel->setParameter(param.address, value);
+    };
+}
+
+- (AUInternalRenderBlock)internalRenderBlock {
+    /*
+     Capture in locals to avoid ObjC member lookups. If "self" is captured in
+     render, we're doing it wrong.
+     */
+    __block AKFluteDSPKernel *state = &_kernel;
+    __block BufferedInputBus *input = &_inputBus;
+
+    return ^AUAudioUnitStatus(
+                              AudioUnitRenderActionFlags *actionFlags,
+                              const AudioTimeStamp       *timestamp,
+                              AVAudioFrameCount           frameCount,
+                              NSInteger                   outputBusNumber,
+                              AudioBufferList            *outputData,
+                              const AURenderEvent        *realtimeEventListHead,
+                              AURenderPullInputBlock      pullInputBlock) {
+
+        AudioBufferList *inAudioBufferList = input->mutableAudioBufferList;
+
+        /*
+         If the caller passed non-nil output pointers, use those. Otherwise,
+         process in-place in the input buffer. If your algorithm cannot process
+         in-place, then you will need to preallocate an output buffer and use
+         it here.
+         */
+        AudioBufferList *outAudioBufferList = outputData;
+        if (outAudioBufferList->mBuffers[0].mData == nullptr) {
+            for (UInt32 i = 0; i < outAudioBufferList->mNumberBuffers; ++i) {
+                outAudioBufferList->mBuffers[i].mData = inAudioBufferList->mBuffers[i].mData;
+            }
+        }
+
+        state->setBuffers(outAudioBufferList);
+        state->processWithEvents(timestamp, frameCount, realtimeEventListHead);
+
+        return noErr;
+    };
+}
+
+
+@end
+
+
