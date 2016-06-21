a71a9976a6b6646c9bfc9cde9b1821ee5a82d03e
Aurelius Prochazka <aure@aure.com>
2016-06-20 20:11:10 -0700

    Updated templates

--- AudioKit/Common/Nodes/Effects/Delay/Variable Delay/AKVariableDelayAudioUnit.mm
+++ AudioKit/Common/Nodes/Effects/Delay/Variable Delay/AKVariableDelayAudioUnit.mm
@@ -126,16 +126,32 @@
                                                               busses: @[_outputBus]];
 
     // Make a local pointer to the kernel to avoid capturing self.
-    __block AKVariableDelayDSPKernel *blockKernel = &_kernel;
+    __block AKVariableDelayDSPKernel *delayKernel = &_kernel;
 
     // implementorValueObserver is called when a parameter changes value.
     _parameterTree.implementorValueObserver = ^(AUParameter *param, AUValue value) {
-        blockKernel->setParameter(param.address, value);
+        delayKernel->setParameter(param.address, value);
     };
 
     // implementorValueProvider is called when the value needs to be refreshed.
     _parameterTree.implementorValueProvider = ^(AUParameter *param) {
-        return blockKernel->getParameter(param.address);
+        return delayKernel->getParameter(param.address);
+    };
+
+    // A function to provide string representations of parameter values.
+    _parameterTree.implementorStringFromValueCallback = ^(AUParameter *param, const AUValue *__nullable valuePtr) {
+        AUValue value = valuePtr == nil ? param.value : *valuePtr;
+
+        switch (param.address) {
+            case timeAddress:
+                return [NSString stringWithFormat:@"%.3f", value];
+
+            case feedbackAddress:
+                return [NSString stringWithFormat:@"%.3f", value];
+
+            default:
+                return @"?";
+        }
     };
 
     self.maximumFramesToRender = 512;
