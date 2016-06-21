a799f18e55072efd459e6364fa796c80e1b62c82
Aurelius Prochazka <aure@aure.com>
2016-06-20 22:52:35 -0700

    updated template

--- AudioKit/Common/Nodes/Mixing/Panner/AKPannerAudioUnit.mm
+++ AudioKit/Common/Nodes/Mixing/Panner/AKPannerAudioUnit.mm
@@ -108,16 +108,29 @@
                                                               busses: @[_outputBus]];
 
     // Make a local pointer to the kernel to avoid capturing self.
-    __block AKPannerDSPKernel *blockKernel = &_kernel;
+    __block AKPannerDSPKernel *pannerKernel = &_kernel;
 
     // implementorValueObserver is called when a parameter changes value.
     _parameterTree.implementorValueObserver = ^(AUParameter *param, AUValue value) {
-        blockKernel->setParameter(param.address, value);
+        pannerKernel->setParameter(param.address, value);
     };
 
     // implementorValueProvider is called when the value needs to be refreshed.
     _parameterTree.implementorValueProvider = ^(AUParameter *param) {
-        return blockKernel->getParameter(param.address);
+        return pannerKernel->getParameter(param.address);
+    };
+
+    // A function to provide string representations of parameter values.
+    _parameterTree.implementorStringFromValueCallback = ^(AUParameter *param, const AUValue *__nullable valuePtr) {
+        AUValue value = valuePtr == nil ? param.value : *valuePtr;
+
+        switch (param.address) {
+            case panAddress:
+                return [NSString stringWithFormat:@"%.3f", value];
+
+            default:
+                return @"?";
+        }
     };
 
     self.maximumFramesToRender = 512;
