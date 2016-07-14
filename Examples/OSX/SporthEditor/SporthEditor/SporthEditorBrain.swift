454d2b2ea4eabd11b5d9d01bfd55e2f8a3c01a3c
nuts23 <linouk23@gmail.com>
2016-07-15 00:18:12 +0300

    Added SporthEditor example for OSX

---
+++ Examples/OSX/SporthEditor/SporthEditor/SporthEditorBrain.swift
@@ -0,0 +1,54 @@
+//
+//  SporthEditorBrain.swift
+//  SporthEditor
+//
+//  Created by Kanstantsin Linou on 7/12/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+import Foundation
+import AudioKit
+
+class SporthEditorBrain {
+    private var generator: AKOperationGenerator?
+    private var knownCodes = [String:String]()
+    var lastSavedName: String?
+    var rows: Int {
+        return knownCodes.count
+    }
+    var names: [String] {
+        return Array(knownCodes.keys)
+    }
+    
+    func getCode(name: String) -> String {
+        return knownCodes[name]!
+    }
+    
+    func run(code: String) {
+        generator?.stop()
+        AudioKit.stop()
+        generator = AKOperationGenerator(operation: AKOperation(code))
+        AudioKit.output = generator
+        AudioKit.start()
+        generator?.start()
+    }
+    
+    func stop() {
+        generator?.stop()
+    }
+    
+    func save(name: String, code: String) {
+        let fileName = name + FileUtilities.fileExtension
+        let filePath = FileUtilities.filePath(fileName)
+        
+        do {
+            try code.writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding)
+            knownCodes[name] = code
+            NSLog("Saving was completed successfully")
+            
+        } catch {
+            NSLog("Error during saving the file")
+        }
+        lastSavedName = name
+    }
+}
