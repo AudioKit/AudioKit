8fbcb5a745a0243508b1c25e908236490c81b84a
Aurelius Prochazka <aure@aure.com>
2016-07-19 00:10:54 -0700

    Minor linting

--- Examples/iOS/SporthEditor/SporthEditor/SporthEditorBrain.swift
+++ Examples/iOS/SporthEditor/SporthEditor/SporthEditorBrain.swift
@@ -10,15 +10,18 @@ import Foundation
 import AudioKit
 
 class SporthEditorBrain {
+
     var generator: AKOperationGenerator?
-    var knownCodes = [String:String]()
+    var knownCodes = [String : String]()
+
     var rows: Int {
         return knownCodes.count
     }
+
     var names: [String] {
         return Array(knownCodes.keys)
     }
-    
+
     func run(code: String) {
         generator?.stop()
         AudioKit.stop()
@@ -27,20 +30,20 @@ class SporthEditorBrain {
         AudioKit.start()
         generator?.start()
     }
-    
+
     func stop() {
         generator?.stop()
     }
-    
+
     func save(name: String, code: String) {
         let fileName = name + FileUtilities.fileExtension
         let filePath = FileUtilities.filePath(fileName)
-        
+
         do {
             try code.writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding)
             knownCodes[name] = code
             NSLog("Saving was completed successfully")
-            
+
         } catch {
             NSLog("Error during saving the file")
         }
