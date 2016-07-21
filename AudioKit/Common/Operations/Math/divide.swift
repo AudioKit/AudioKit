dbed82d743e0b5e24f255f78b3dd012559aa752a
Aurelius Prochazka <aure@aure.com>
2016-07-21 10:48:00 -0700

    Stereo operation scaling as multiplication and division with parameters

--- AudioKit/Common/Operations/Math/divide.swift
+++ AudioKit/Common/Operations/Math/divide.swift
@@ -28,3 +28,12 @@ public func /(left: AKParameter, right: AKParameter) -> AKOperation {
     return left.toMono().dividedBy(right)
 }
 
+/// Helper function for Division
+///
+/// - Parameters:
+///   - left: stereo operation
+///   - right: parameter
+///
+public func /(left: AKStereoOperation, right: AKParameter) -> AKStereoOperation {
+    return AKStereoOperation(module: "dup rot swap / rot rot / swap", inputs: left, right)
+}
