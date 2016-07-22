ea8e7313e4090ef20c0bce480ef85598e3f07cb9
Aurelius Prochazka <aure@aure.com>
2016-07-22 14:42:30 -0700

    Comment repairing

--- AudioKit/Common/Internals/Audio File/AKAudioFile+Utilities.swift
+++ AudioKit/Common/Internals/Audio File/AKAudioFile+Utilities.swift
@@ -12,17 +12,17 @@ import Foundation
 import AVFoundation
 
 extension AKAudioFile {
-
+    
     /// returns file Mime Type if exists
     /// Otherwise, returns nil
     /// (useful when sendind an AKAudioFile by email)
-    public var mimeType:String? {
+    public var mimeType: String? {
         switch self.fileExt.uppercaseString {
         case "WAV":
             return  "audio/wav"
         case "CAF":
             return  "audio/x-caf"
-        case "AIF","AIFF", "AIFC":
+        case "AIF", "AIFF", "AIFC":
             return "audio/aiff"
         case "M4R":
             return  "audio/x-m4r"
@@ -30,7 +30,7 @@ extension AKAudioFile {
             return  "audio/x-m4a"
         case "MP4":
             return  "audio/mp4"
-        case "M2A","MP2":
+        case "M2A", "MP2":
             return  "audio/mpeg"
         case "AAC":
             return  "audio/aac"
@@ -39,24 +39,22 @@ extension AKAudioFile {
         default: return nil
         }
     }
-
+    
     /// Static function to delete all audiofiles from Temp directory
     ///
     /// AKAudioFile.cleanTempDirectory()
     ///
-    public static func cleanTempDirectory()
-    {
+    public static func cleanTempDirectory() {
         var deletedFilesCount = 0
-
+        
         let fileManager = NSFileManager.defaultManager()
         let tempPath =  NSTemporaryDirectory()
-
+        
         do {
             let fileNames = try fileManager.contentsOfDirectoryAtPath("\(tempPath)")
-
+            
             // function for deleting files
-            func deleteFileWithFileName (fileName:String)
-            {
+            func deleteFileWithFileName (fileName: String) {
                 let filePathName = "\(tempPath)/\(fileName)"
                 do {
                     try fileManager.removeItemAtPath(filePathName)
@@ -67,63 +65,57 @@ extension AKAudioFile {
                     print ("Error: \(error)")
                 }
             }
-
+            
             // Checks file type (only Audio Files)
             for fileName in fileNames {
                 let fileNameLowerCase = fileName.lowercaseString
-                if (fileNameLowerCase.hasSuffix(".wav"))
-                {
+                if (fileNameLowerCase.hasSuffix(".wav")) {
                     deleteFileWithFileName (fileName)
                 }
-                if (fileNameLowerCase.hasSuffix(".caf"))
-                {
+                if (fileNameLowerCase.hasSuffix(".caf")) {
                     deleteFileWithFileName (fileName)
                 }
-                if (fileNameLowerCase.hasSuffix(".aif"))
-                {
+                if (fileNameLowerCase.hasSuffix(".aif")) {
                     deleteFileWithFileName (fileName)
                 }
-                if (fileNameLowerCase.hasSuffix(".mp4"))
-                {
+                if (fileNameLowerCase.hasSuffix(".mp4")) {
                     deleteFileWithFileName (fileName)
                 }
-                if (fileNameLowerCase.hasSuffix(".m4a"))
-                {
+                if (fileNameLowerCase.hasSuffix(".m4a")) {
                     deleteFileWithFileName (fileName)
                 }
             }
-
+            
             // print report
             switch deletedFilesCount {
             case 0: print ("AKAudioFile.cleanTempDirectory: No file deleted.")
             case 1: print ("AKAudioFile.cleanTempDirectory: \(deletedFilesCount) File deleted.")
             default: print ("AKAudioFile.cleanTempDirectory: \(deletedFilesCount) Files deleted.")
-
+                
             }
-
-
+            
+            
         } catch let error as NSError {
             print ("Couldn't access Temp Directory")
             print ("Error: \(error)")
         }
     }
-
-    /**
-     Returns a silent AKAudioFile with a length set in samples.
-     For a silent file of one second, set samples value to 44100...
-
-     - Parameters:
-     - samples: the number of samples to generate ( equals length in seconds multiplied by sample rate)
-     - name: the name of the file without its extension (String).
-     - baseDir: where the file will be located, can be set to .Resources,  .Documents or .Temp
-
-     - Throws: NSError if failed .
-
-     - Returns: An AKAudioFile, or nil if init failed.*/
+    
+    /// Returns a silent AKAudioFile with a length set in samples.
+    ///
+    /// For a silent file of one second, set samples value to 44100...
+    ///
+    /// - Parameters:
+    ///   - samples: the number of samples to generate ( equals length in seconds multiplied by sample rate)
+    ///   - baseDir: where the file will be located, can be set to .Resources,  .Documents or .Temp
+    ///   - name: the name of the file without its extension (String).
+    ///
+    /// - Returns: An AKAudioFile, or nil if init failed.
+    ///
     static public func silent(samples samples: Int64,
                                       baseDir: BaseDirectory = .Temp,
                                       name: String = "") throws -> AKAudioFile {
-
+        
         if samples < 0 {
             print( "ERROR AKAudioFile: cannot create silent AKAUdioFile with negative samples count !")
             throw NSError(domain: NSURLErrorDomain, code: NSURLErrorCannotCreateFile, userInfo:nil)
@@ -132,13 +124,13 @@ extension AKAudioFile {
             // we return it as a file for reading
             return try AKAudioFile(forReading: emptyFile.url)
         }
-
+        
         let array = [Float](count:Int(samples), repeatedValue: 0.0)
         let silentFile = try AKAudioFile(createFileFromFloats: [array, array], baseDir: baseDir, name: name)
-
+        
         return try AKAudioFile(forReading: silentFile.url)
     }
-
-
-
+    
+    
+    
 }
