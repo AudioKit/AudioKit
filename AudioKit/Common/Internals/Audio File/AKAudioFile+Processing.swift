fc03f174947f6e00986d6d85153c82dc30fce0d2
laurentVeliscek <laurent.veliscek@gmail.com>
2016-07-22 11:32:02 +0200

    Added AKAudioFiles utilities
    - Added computed var AKAudioFile.mimeType
    - Added static Func AKAudioFile.cleanTempDirectory()
    - Moved AKAudioFile.silent(…) to utilities, as is more a static utility
    than a file process.

--- AudioKit/Common/Internals/Audio File/AKAudioFile+Processing.swift
+++ AudioKit/Common/Internals/Audio File/AKAudioFile+Processing.swift
@@ -177,35 +177,5 @@ extension AKAudioFile {
         return try AKAudioFile(forReading: newFile.url)
     }
    
-    /**
-     Returns a silent AKAudioFile with a length set in samples.
-     For a silent file of one second, set samples value to 44100...
-     
-     - Parameters:
-        - samples: the number of samples to generate ( equals length in seconds multiplied by sample rate)
-        - name: the name of the file without its extension (String).
-        - baseDir: where the file will be located, can be set to .Resources,  .Documents or .Temp
-     
-     - Throws: NSError if failed .
-     
-     - Returns: An AKAudioFile, or nil if init failed.*/
-    static public func silent(samples samples: Int64,
-                                      baseDir: BaseDirectory = .Temp,
-                                      name: String = "") throws -> AKAudioFile {
-        
-        if samples < 0 {
-            print( "ERROR AKAudioFile: cannot create silent AKAUdioFile with negative samples count !")
-            throw NSError(domain: NSURLErrorDomain, code: NSURLErrorCannotCreateFile, userInfo:nil)
-        } else if samples == 0 {
-            let emptyFile = try AKAudioFile(writeIn: baseDir, name: name)
-            // we return it as a file for reading
-            return try AKAudioFile(forReading: emptyFile.url)
-        }
-        
-        let array = [Float](count:Int(samples), repeatedValue: 0.0)
-        let silentFile = try AKAudioFile(createFileFromFloats: [array, array], baseDir: baseDir, name: name)
-        
-        return try AKAudioFile(forReading: silentFile.url)
-    }
 
 }
