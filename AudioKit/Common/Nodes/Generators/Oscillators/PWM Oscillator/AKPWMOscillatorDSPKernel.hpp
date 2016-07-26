595594bdbfe2ee2eeef2cfc1a4dbadc9bde2fbd8
Aurelius Prochazka <aure@aure.com>
2016-07-25 21:29:09 -0700

    Widened the detuning multiplier range to up/down an octave

--- AudioKit/Common/Nodes/Generators/Oscillators/PWM Oscillator/AKPWMOscillatorDSPKernel.hpp
+++ AudioKit/Common/Nodes/Generators/Oscillators/PWM Oscillator/AKPWMOscillatorDSPKernel.hpp
@@ -91,7 +91,7 @@ public:
     }
 
     void setDetuningMultiplier(float value) {
-        detuningMultiplier = clamp(value, 0.9f, 1.11f);
+        detuningMultiplier = clamp(value, 0.5f, 2.0f);
         detuningMultiplierRamper.setImmediate(detuningMultiplier);
     }
     
@@ -120,7 +120,7 @@ public:
                 break;
 
             case detuningMultiplierAddress:
-                detuningMultiplierRamper.setUIValue(clamp(value, 0.9f, 1.11f));
+                detuningMultiplierRamper.setUIValue(clamp(value, 0.5f, 2.0f));
                 break;
 
         }
@@ -166,7 +166,7 @@ public:
                 break;
 
             case detuningMultiplierAddress:
-                detuningMultiplierRamper.startRamp(clamp(value, 0.9f, 1.11f), duration);
+                detuningMultiplierRamper.startRamp(clamp(value, 0.5f, 2.0f), duration);
                 break;
 
         }
