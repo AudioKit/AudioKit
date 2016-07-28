c4070fc4b95d8a814cf3c6baa4e1c6db176702ce
Aurelius Prochazka <aure@aure.com>
2016-07-28 00:49:47 -0700

    Audio file playground updates for trailing closure versions

--- AudioKit/macOS/AudioKit for macOS/Playgrounds/FileHandling.playground/Pages/Reading and Writing Audio Files.xcplaygroundpage/Contents.swift
+++ AudioKit/macOS/AudioKit for macOS/Playgrounds/FileHandling.playground/Pages/Reading and Writing Audio Files.xcplaygroundpage/Contents.swift
@@ -9,14 +9,14 @@ import XCPlayground
 import AudioKit
 
 // Let's create an AKaudioFile :
-let akAudioFile = try? AKAudioFile(readFileName: "click.wav",
+let akAudioFile = try AKAudioFile(readFileName: "click.wav",
                                    baseDir: .Resources)
 
 // converted in an AVAudioFile
-let avAudioFile = akAudioFile! as AVAudioFile
+let avAudioFile = akAudioFile as AVAudioFile
 
 // converted back into an AKAudioFile
-let akAudioFile2 = try? AKAudioFile(forReading: avAudioFile.url)
+let akAudioFile2 = try AKAudioFile(forReading: avAudioFile.url)
 
 
 //: The baseDirectory parameter if an enum value from AKAudioFile.BaseDirectory :
@@ -26,11 +26,7 @@ let tempDir = AKAudioFile.BaseDirectory.Temp
 
 //: baseDir is defaulted to be .Resources, so loading an AKAudiofile
 //: from this playground Resources folder can be done like this :
-let drumloop = try? AKAudioFile(readFileName: "drumloop.wav")
-
-//: You can load a file from a sub directory like this:
-let fmpia = try? AKAudioFile(readFileName: "Sounds/fmpia1.wav",
-                             baseDir: .Resources)
+let drumloop = try AKAudioFile(readFileName: "drumloop.wav")
 
 //: As AKAudioFile is an optional, it will be set to nil if a problem occurs.
 //: Notice that an error message is printed in the debug area, and an error is thrown...
@@ -43,42 +39,40 @@ do {
 
 //: So it's a good idea to check that the AKAudioFile is valid before using it.
 //: Let's display some info about drumloop :
-if drumloop != nil {
-    print("drumloop!.sampleRate:  \(drumloop!.sampleRate)")
-    print("drumloop!.duration:  \(drumloop!.duration)")
-    // and so on...
-}
+print("drumloop!.sampleRate:  \(drumloop.sampleRate)")
+print("drumloop!.duration:  \(drumloop.duration)")
+// and so on...
 
-//: AKAudioFile can easily be trimmed and exported. First, you must set a
+
+//: AKAudioFile can easily be trimmed and exported and you can set a
 //: callback function that will be triggered upon export has been completed.
-func myExportCallBack() {
-    print ("myExportCallBack has been triggered. It means that export ended")
-    if myExport!!.succeeded {
-        print ("Export succeeded")
-        // we get the resulting AKAudioFile
-        let exportedfile = myExport?!.exportedAudioFile!
-        
-        
-        // If it is valid, we can play it :
-        if exportedfile != nil {
-            
-            print (exportedfile?.fileNamePlusExtension)
-            let player = try? AKAudioPlayer(file: exportedfile!)
-            AudioKit.output = player
-            AudioKit.start()
-            player!.play()
-        }
-        
-    } else {
-        print ("Export failed")
-    }
-}
 
 //: Then, we can extract from 1 to 2 seconds of drumloop, as an mp4 file that will be
 //: written in documents directory. If the destination file exists, it will be overwritten.
-let myExport = try? drumloop?.export(name: "exported", ext: .m4a, baseDir: .Documents,
-                                     callBack: myExportCallBack,
-                                     fromTime: 1, toTime: 2)
+try drumloop.export(
+    name: "exported", ext: .m4a, baseDir: .Documents,
+    fromTime: 1, toTime: 2) { export in
+        
+        print ("myExportCallBack has been triggered. It means that export ended")
+        if export.succeeded {
+            print ("Export succeeded")
+            // we get the resulting AKAudioFile
+            let exportedfile = export.audioFile
+            
+            // If it is valid, we can play it :
+            if exportedfile != nil {
+                
+                print (exportedfile?.fileNamePlusExtension)
+                let player = try? AKAudioPlayer(file: exportedfile!)
+                AudioKit.output = player
+                AudioKit.start()
+                player!.play()
+            }
+            
+        } else {
+            print ("Export failed")
+        }
+}
 
 
 
