5674a4052d08069490972d869fa9e3e559504642
Aurelius Prochazka <aure@aure.com>
2016-07-29 13:34:16 -0700

    Playground organization on iOS side

---
+++ AudioKit/iOS/AudioKit/Playgrounds/Synthesis.playground/Pages/Phasor Operation.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
+//:
+//: ---
+//:
+//: ## Phasor Operation
+//: ### Here we use the phasor to sweep amplitude and frequencies
+import XCPlayground
+import AudioKit
+
+let interval: Double = 2
+let noteCount: Double = 24
+let startingNote: Double = 48 // C
+
+let generator = AKOperationGenerator() { _ in
+    
+    let frequency = (floor(AKOperation.phasor(frequency: 0.5) * noteCount) * interval  + startingNote)
+        .midiNoteToFrequency()
+    
+    var amplitude = (AKOperation.phasor(frequency: 0.5) - 1).portamento() // prevents the click sound
+    
+    var oscillator = AKOperation.sineWave(frequency: frequency, amplitude: amplitude)
+    let reverb = oscillator.reverberateWithChowning()
+    return mixer(oscillator, reverb, balance: 0.6)
+}
+
+AudioKit.output = generator
+AudioKit.start()
+generator.start()
+
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+
+//: [TOC](Table%20Of%20Contents) | [Previous](@previous) | [Next](@next)
