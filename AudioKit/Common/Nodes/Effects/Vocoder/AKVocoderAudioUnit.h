83072496d4f230e6341248f34fa9949a3043f639
Aurelius Prochazka <aure@aure.com>
2016-08-04 15:02:55 -0700

    Added AKVocoder and playground

--- AudioKit/Common/Nodes/Effects/Vocoder/AKVocoderAudioUnit.h
+++ AudioKit/Common/Nodes/Effects/Vocoder/AKVocoderAudioUnit.h
@@ -13,12 +13,17 @@
 
 @interface AKVocoderAudioUnit : AUAudioUnit
 @property (nonatomic) float attackTime;
-@property (nonatomic) float rel;
+@property (nonatomic) float releaseTime;
 @property (nonatomic) float bandwidthRatio;
 
 - (void)start;
 - (void)stop;
 - (BOOL)isPlaying;
+- (void)setUpParameterRamp;
+- (BOOL)isSetUp;
+
+@property double rampTime;
+
 @end
 
 #endif /* AKVocoderAudioUnit_h */
