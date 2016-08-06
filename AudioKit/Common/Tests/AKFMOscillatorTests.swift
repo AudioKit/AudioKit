1a691ff6104a133bfc6ac2dd1645030a1c251c4f
Aurelius Prochazka <aure@aure.com>
2016-08-06 12:48:51 -0700

    FM Oscillator tests

--- AudioKit/Common/Tests/AKFMOscillatorTests.swift
+++ AudioKit/Common/Tests/AKFMOscillatorTests.swift
@@ -20,4 +20,25 @@ class AKFMOscillatorTests: AKTestCase {
         output = AKFMOscillator(waveform: AKTable(.Square, size: 4096))
         AKTestMD5("c6b194d7bf925ade38c3a1d5333326f8")
     }
+    
+    func testParametersSetOnInit() {
+        output = AKFMOscillator(waveform: AKTable(.Square),
+                                baseFrequency: 1234,
+                                carrierMultiplier: 1.234,
+                                modulatingMultiplier: 1.234,
+                                modulationIndex: 1.234,
+                                amplitude: 0.5)
+        AKTestMD5("9d4ba935daab84b999de48fd9314d3ae")
+    }
+    
+    func testParametersSetAfterInit() {
+        let oscillator = AKFMOscillator(waveform: AKTable(.Square))
+        oscillator.baseFrequency        = 1234
+        oscillator.carrierMultiplier    = 1.234
+        oscillator.modulatingMultiplier = 1.234
+        oscillator.modulationIndex      = 1.234
+        oscillator.amplitude            = 0.5
+        output = oscillator
+        AKTestMD5("9d4ba935daab84b999de48fd9314d3ae")
+    }
 }
