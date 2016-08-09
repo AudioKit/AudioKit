5bafba5f73ae3de9f657827d1a58d4eb0e766838
Aurelius Prochazka <aure@aure.com>
2016-08-09 06:41:14 -0700

    More tests

---
+++ AudioKit/Common/Tests/AKChowningReverbTests.swift
@@ -0,0 +1,20 @@
+//
+//  AKChowningReverbTests.swift
+//  AudioKitTestSuite
+//
+//  Created by Aurelius Prochazka on 8/9/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+import XCTest
+@testable import AudioKit
+
+class AKChowningReverbTests: AKTestCase {
+    
+    func testDefault() {
+        let input = AKOscillator()
+        output = AKChowningReverb(input)
+        input.start()
+        AKTestMD5("1189b85f351b279ff2988e249686a26b")
+    }
+}
