76353f61cf696add135a08de6269e9ab5fdfdb47
Aurelius Prochazka <aure@aure.com>
2016-08-09 14:45:49 -0700

    Some more test templates to fill in

---
+++ AudioKit/Common/Tests/sawtoothWaveTests.swift
@@ -0,0 +1,27 @@
+//
+//  sawtoothWaveTests.swift
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 8/9/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+import XCTest
+
+@testable import AudioKit
+
+class sawtoothWaveTests: AKTestCase {
+
+    override func setUp() {
+        super.setUp()
+        duration = 1.0
+    }
+
+    func testDefault() {
+        output = AKOperationGenerator() { _ in
+            return AKOperation.sawtoothWave()
+        }
+        AKTestMD5("")
+    }
+
+}
---
+++ AudioKit/Common/Tests/whiteNoiseTests.swift
@@ -0,0 +1,27 @@
+//
+//  whiteNoiseTests.swift
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 8/9/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+import XCTest
+
+@testable import AudioKit
+
+class whiteNoiseTests: AKTestCase {
+
+    override func setUp() {
+        super.setUp()
+        duration = 1.0
+    }
+
+    func testDefault() {
+        output = AKOperationGenerator() { _ in
+            return AKOperation.whiteNoise()
+        }
+        AKTestMD5("")
+    }
+
+}
