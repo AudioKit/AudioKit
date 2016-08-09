e70e1724d10b62d7837a2639e6013a847a1476b9
Nicholas Arner <nicholasarner@gmail.com>
2016-08-09 14:16:45 -0400

    Added tests for White and Pink noise generators.

---
+++ AudioKit/iOS/AudioKitTestSuite/AudioKitTestSuiteTests/AKPinkNoiseTest.swift
@@ -0,0 +1,18 @@
+//
+//  AKPinkNoiseTest.swift
+//  AudioKitTestSuite
+//
+//  Created by Nicholas Arner on 8/9/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+import XCTest
+@testable import AudioKit
+
+class AKPinkNoiseTests: AKTestCase {
+
+    func testDefault() {
+        output = AKPinkNoise()
+        AKTestMD5("b56ddd343583e6e58b559d10b8b4c147")
+    }
+}
---
+++ AudioKit/iOS/AudioKitTestSuite/AudioKitTestSuiteTests/AKWhiteNoiseTests.swift
@@ -0,0 +1,18 @@
+//
+//  AKWhiteNoiseTests.swift
+//  AudioKitTestSuite
+//
+//  Created by Nicholas Arner on 8/9/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+import XCTest
+@testable import AudioKit
+
+class AKWhiteNoiseTests: AKTestCase {
+    
+    func testDefault() {
+        output = AKWhiteNoise()
+        AKTestMD5("d6b3484278d57bc40ce66df5decb88be")
+    }
+}
