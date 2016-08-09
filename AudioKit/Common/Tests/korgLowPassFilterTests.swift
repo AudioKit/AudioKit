67b091b947e921ddde0685105123a8ac9a778fff
Aurelius Prochazka <aure@aure.com>
2016-08-09 15:12:07 -0700

    Almost all effect and generators have default tests

---
+++ AudioKit/Common/Tests/korgLowPassFilterTests.swift
@@ -0,0 +1,29 @@
+//
+//  korgLowPassFilterTests.swift
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 8/9/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+import XCTest
+
+@testable import AudioKit
+
+class korgLowPassFilterTests: AKTestCase {
+
+    override func setUp() {
+        super.setUp()
+        duration = 1.0
+    }
+
+    func testDefault() {
+        let input = AKOscillator()
+        input.start()
+        output = AKOperationEffect(input) { input, _ in
+            return input.korgLowPassFilter()
+        }
+        AKTestMD5("")
+    }
+
+}
---
+++ AudioKit/Common/Tests/reverberateWithCombFilterTests.swift
@@ -0,0 +1,29 @@
+//
+//  reverberateWithCombFilterTests.swift
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 8/9/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+import XCTest
+
+@testable import AudioKit
+
+class reverberateWithCombFilterTests: AKTestCase {
+
+    override func setUp() {
+        super.setUp()
+        duration = 1.0
+    }
+
+    func testDefault() {
+        let input = AKOscillator()
+        input.start()
+        output = AKOperationEffect(input) { input, _ in
+            return input.reverberateWithCombFilter()
+        }
+        AKTestMD5("")
+    }
+
+}
