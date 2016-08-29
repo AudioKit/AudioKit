70d5a6446fe3058a695c95d84e5b05cd93a43173
Matthew Michael <matthew.fecher@gmail.com>
2016-08-29 09:01:43 -0600

    modify large knob to appear in IB

--- Examples/iOS/AnalogSynthX/AnalogSynthX/CustomControls/KnobLarge.swift
+++ Examples/iOS/AnalogSynthX/AnalogSynthX/CustomControls/KnobLarge.swift
@@ -16,6 +16,10 @@ protocol KnobLargeDelegate {
 class KnobLarge: Knob {
 
     var delegate: KnobLargeDelegate?
+    
+    // Image Declarations
+    var knob212_base = UIImage(named: "knob212_base")
+    var knob212_indicator = UIImage(named: "knob212_indicator")
 
     override func drawRect(rect: CGRect) {
         SynthStyleKit.drawKnobLarge(knobValue: knobValue)
@@ -26,6 +30,44 @@ class KnobLarge: Knob {
         super.setPercentagesWithTouchPoint(touchPoint)
         delegate?.updateKnobValue(value, tag: self.tag)
         setNeedsDisplay()
+    }
+    
+    // MARK: - PaintCode generated code
+    func drawKnobLarge(knobValue knobValue: CGFloat = 0.332) {
+        //// General Declarations
+        let context = UIGraphicsGetCurrentContext()
+        
+        //// Variable Declarations
+        let knobAngle: CGFloat = -240 * knobValue
+        
+        //// Picture Drawing
+        let picturePath = UIBezierPath(rect: CGRectMake(10, 10, 106, 106))
+        CGContextSaveGState(context)
+        picturePath.addClip()
+        knob212_base!.drawInRect(CGRectMake(10, 10, knob212_base!.size.width, knob212_base!.size.height))
+        CGContextRestoreGState(context)
+        
+        //// Picture 2 Drawing
+        CGContextSaveGState(context)
+        CGContextTranslateCTM(context, 63, 63)
+        CGContextRotateCTM(context, -(knobAngle + 120) * CGFloat(M_PI) / 180)
+        
+        let picture2Path = UIBezierPath(rect: CGRectMake(-53, -53, 106, 106))
+        CGContextSaveGState(context)
+        picture2Path.addClip()
+        knob212_indicator!.drawInRect(CGRectMake(-53, -53, knob212_indicator!.size.width, knob212_indicator!.size.height))
+        CGContextRestoreGState(context)
+        
+        CGContextRestoreGState(context)
+    }
+    
+    // MARK: - Allow knobs to appear in IB
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        
+        let bundle = NSBundle(forClass: self.dynamicType)
+        knob212_base = UIImage(named: "knob212_base", inBundle: bundle, compatibleWithTraitCollection: self.traitCollection)!
+        knob212_indicator = UIImage(named: "knob212_indicator", inBundle: bundle, compatibleWithTraitCollection: self.traitCollection)!
     }
 
 }
