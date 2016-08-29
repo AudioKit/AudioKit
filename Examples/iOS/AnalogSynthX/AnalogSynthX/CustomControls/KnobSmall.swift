22162bcf65acab24b60c33d75def90033ff9bf2c
Matthew Michael <matthew.fecher@gmail.com>
2016-08-29 09:10:50 -0600

    modify small knob to appear on IB

--- Examples/iOS/AnalogSynthX/AnalogSynthX/CustomControls/KnobSmall.swift
+++ Examples/iOS/AnalogSynthX/AnalogSynthX/CustomControls/KnobSmall.swift
@@ -16,9 +16,14 @@ protocol KnobSmallDelegate {
 class KnobSmall: Knob {
 
     var delegate: KnobSmallDelegate?
+    
+    //// Image Declarations
+    var knob120_base = UIImage(named: "knob120_base")
+    var knob120_indicator = UIImage(named: "knob120_indicator")
+
 
     override func drawRect(rect: CGRect) {
-        SynthStyleKit.drawKnobSmall(knobValue: knobValue)
+        drawKnobSmall(knobValue: knobValue)
     }
 
     // MARK: - Set Percentages
@@ -27,5 +32,42 @@ class KnobSmall: Knob {
         delegate?.updateKnobValue(value, tag: self.tag)
         setNeedsDisplay()
     }
+    
+    func drawKnobSmall(knobValue knobValue: CGFloat = 0.332) {
+        //// General Declarations
+        let context = UIGraphicsGetCurrentContext()
+        
+        //// Variable Declarations
+        let knobAngle: CGFloat = -240 * knobValue
+        
+        //// Picture Drawing
+        let picturePath = UIBezierPath(rect: CGRectMake(5, 5, 60, 60))
+        CGContextSaveGState(context)
+        picturePath.addClip()
+        knob120_base!.drawInRect(CGRectMake(5, 5, knob120_base!.size.width, knob120_base!.size.height))
+        CGContextRestoreGState(context)
+        
+        //// Indicator Drawing
+        CGContextSaveGState(context)
+        CGContextTranslateCTM(context, 35, 35)
+        CGContextRotateCTM(context, -(knobAngle + 120) * CGFloat(M_PI) / 180)
+        
+        let indicatorPath = UIBezierPath(rect: CGRectMake(-30, -30, 60, 60))
+        CGContextSaveGState(context)
+        indicatorPath.addClip()
+        knob120_indicator!.drawInRect(CGRectMake(-30, -30, knob120_indicator!.size.width, knob120_indicator!.size.height))
+        CGContextRestoreGState(context)
+        
+        CGContextRestoreGState(context)
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        
+        let bundle = NSBundle(forClass: self.dynamicType)
+        knob120_base = UIImage(named: "knob120_base", inBundle: bundle, compatibleWithTraitCollection: self.traitCollection)!
+        knob120_indicator = UIImage(named: "knob120_indicator", inBundle: bundle, compatibleWithTraitCollection: self.traitCollection)!
+    }
+
 
 }
