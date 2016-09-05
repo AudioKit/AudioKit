dae491b09b5ce48556f829a54ce47306dff26ef0
Aurelius Prochazka <aure@aure.com>
2016-09-05 14:45:16 -0700

    Removed debug printouts

--- AudioKit/Common/Playgrounds/Synthesis.playground/Pages/FM Oscillator Bank.xcplaygroundpage/Contents.swift
+++ AudioKit/Common/Playgrounds/Synthesis.playground/Pages/FM Oscillator Bank.xcplaygroundpage/Contents.swift
@@ -69,7 +69,6 @@ class PlaygroundView: AKPlaygroundView, AKKeyboardDelegate {
         
         addSubview(AKButton(title: "Go Polyphonic") {
             self.keyboard?.polyphonicMode = !self.keyboard!.polyphonicMode
-            dump(self.keyboard?.polyphonicMode)
             if self.keyboard!.polyphonicMode {
                 return "Go Monophonic"
             } else {
