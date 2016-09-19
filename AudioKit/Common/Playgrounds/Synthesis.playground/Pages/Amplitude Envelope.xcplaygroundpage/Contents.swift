a08019b01ef3ada3b6e97e7ff4cee7a736e28a1c
Aurelius Prochazka <aure@aure.com>
2016-09-19 02:05:00 -0700

    Fixes noticed by esimonian pull request

--- AudioKit/Common/Playgrounds/Synthesis.playground/Pages/Amplitude Envelope.xcplaygroundpage/Contents.swift
+++ AudioKit/Common/Playgrounds/Synthesis.playground/Pages/Amplitude Envelope.xcplaygroundpage/Contents.swift
@@ -44,12 +44,12 @@ class PlaygroundView: AKPlaygroundView, AKKeyboardDelegate {
 
     }
 
-    func noteOn(noteNumber: MIDINoteNumber) {
-        fmWithADSR.play(noteNumber: noteNumber, velocity: 80)
+    func noteOn(note: MIDINoteNumber) {
+        fmWithADSR.play(noteNumber: note, velocity: 80)
     }
 
-    func noteOff(noteNumber: MIDINoteNumber) {
-        fmWithADSR.stop(noteNumber: noteNumber)
+    func noteOff(note: MIDINoteNumber) {
+        fmWithADSR.stop(noteNumber: note)
     }
 
 }
