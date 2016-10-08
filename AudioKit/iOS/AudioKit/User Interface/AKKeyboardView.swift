c8c6ba0e75ea2285405d86d5ebd70b02e08d084d
Elizabeth Simonian <elizabethsimonian@elizabethsimac2>
2016-10-08 18:40:46 +0700

    Add in icons fix frames

--- AudioKit/iOS/AudioKit/User Interface/AKKeyboardView.swift
+++ AudioKit/iOS/AudioKit/User Interface/AKKeyboardView.swift
@@ -18,6 +18,7 @@ public protocol AKKeyboardDelegate {
     @IBInspectable open var octaveCount: Int = 2
     @IBInspectable open var firstOctave: Int = 4
     @IBInspectable open var topKeyHeightRatio: CGFloat = 0.55
+    @IBInspectable open var polyphonicButton: UIColor = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1.000)
 
     @IBInspectable open var  whiteKeyOff: UIColor = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1.000)
     @IBInspectable open var  blackKeyOff: UIColor = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1.000)
