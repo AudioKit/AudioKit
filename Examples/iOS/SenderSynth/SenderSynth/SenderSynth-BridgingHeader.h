6f3593fa12b0d24199493f8436ecad2c39c217fd
Aurelius Prochazka <aure@aure.com>
2016-10-15 11:22:03 -0700

    Audiobus examples, initial versions

---
+++ Examples/iOS/SenderSynth/SenderSynth/SenderSynth-BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  SenderSynth-BridgingHeader.h
+//  SenderSynth
+//
+//  Created by Aurelius Prochazka on 10/7/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+#ifndef SenderSynth_BridgingHeader_h
+#define SenderSynth_BridgingHeader_h
+
+#import "Audiobus.h"
+
+#endif /* SenderSynth_BridgingHeader_h */
