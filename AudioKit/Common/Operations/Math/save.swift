2b9e9b2aa4f2d6db62cc4c6c740fae57688c4aef
Aurelius Prochazka <aure@aure.com>
2016-11-04 22:22:57 -0700

    New operations for common math things: count, increment, save

---
+++ AudioKit/Common/Operations/Math/save.swift
@@ -0,0 +1,20 @@
+//
+//  save.swift
+//  AudioKit
+//
+//  Created by Aurelius Prochazka on 11/4/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+import Foundation
+
+extension AKOperation {
+    
+    /// Save a value into the parameters array for using outside of the operation
+    ///
+    /// - parameter parameterIndex: Location in the parameters array to save this value
+    //
+    public func save(parameterIndex: Int) -> AKOperation {
+        return AKOperation(module: "dup \(parameterIndex) pset", inputs: self.toMono())
+    }
+}
