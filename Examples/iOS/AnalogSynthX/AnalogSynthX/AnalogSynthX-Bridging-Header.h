c89fd27e531eee0f41946d4662d3f309cb67ed62
Aurelius Prochazka <aure@aure.com>
2016-11-05 00:40:21 -0700

    Made the Analog Synth X example use Audiobus

---
+++ Examples/iOS/AnalogSynthX/AnalogSynthX/AnalogSynthX-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  AnalogSynthX-BridgingHeader.h
+//  AnalogSynthX
+//
+//  Created by Aurelius Prochazka on 10/7/16.
+//  Copyright © 2016 AudioKit. All rights reserved.
+//
+
+#ifndef AnalogSynthX_BridgingHeader_h
+#define AnalogSynthX_BridgingHeader_h
+
+#import "Audiobus.h"
+
+#endif /* AnalogSynthX_BridgingHeader_h */
