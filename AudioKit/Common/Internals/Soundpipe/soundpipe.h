82e8c8b597a3379cbb002f1ded592ac0b901a506
Aurelius Prochazka <aure@aure.com>
2016-11-12 01:21:12 -0800

    Updated Soundpipe portamento

--- AudioKit/Common/Internals/Soundpipe/soundpipe.h
+++ AudioKit/Common/Internals/Soundpipe/soundpipe.h
@@ -1000,6 +1000,7 @@ int sp_port_create(sp_port **p);
 int sp_port_destroy(sp_port **p);
 int sp_port_init(sp_data *sp, sp_port *p, SPFLOAT htime);
 int sp_port_compute(sp_data *sp, sp_port *p, SPFLOAT *in, SPFLOAT *out);
+int sp_port_reset(sp_data *sp, sp_port *p, SPFLOAT *in);    
 typedef struct {
     SPFLOAT freq, amp, iphs;
     sp_ftbl *tbl;
