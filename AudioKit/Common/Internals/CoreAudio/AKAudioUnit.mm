6f11f9f213bab43e8ece48ca9c536fced3cfcbf9
Aurelius Prochazka <aure@aure.com>
2016-11-28 11:13:02 -0800

    Slightly scaled back, but at least working version of AKAudioUnit

--- AudioKit/Common/Internals/CoreAudio/AKAudioUnit.mm
+++ AudioKit/Common/Internals/CoreAudio/AKAudioUnit.mm
@@ -8,15 +8,11 @@
 
 #import "AKAudioUnit.h"
 #import <AVFoundation/AVFoundation.h>
-#import "BufferedAudioBus.hpp"
 
-#import <AudioKit/AudioKit-Swift.h>
+@implementation AKAudioUnit
 
-@implementation AKAudioUnit {
-    BufferedInputBus _inputBus;
-}
 @synthesize parameterTree = _parameterTree;
-
+@synthesize rampTime = _rampTime;
 - (void)start {}
 - (void)stop {}
 - (BOOL)isPlaying {
@@ -35,25 +31,7 @@
     if (self == nil) {
         return nil;
     }
-    
-    // Initialize a default format for the busses.
-    AVAudioFormat *defaultFormat = [[AVAudioFormat alloc] initStandardFormatWithSampleRate:AKSettings.sampleRate
-                                                                                  channels:AKSettings.numberOfChannels];
-    _rampTime = AKSettings.rampTime;
-    
-    // Create the input and output busses.
-    _inputBus.init(defaultFormat, 8);
-    _outputBus = [[AUAudioUnitBus alloc] initWithFormat:defaultFormat error:nil];
-    
-    // Create the input and output bus arrays.
-    _inputBusArray  = [[AUAudioUnitBusArray alloc] initWithAudioUnit:self
-                                                             busType:AUAudioUnitBusTypeInput
-                                                              busses: @[_inputBus.bus]];
-    _outputBusArray = [[AUAudioUnitBusArray alloc] initWithAudioUnit:self
-                                                             busType:AUAudioUnitBusTypeOutput
-                                                              busses: @[_outputBus]];
-    
-    
+        
     self.maximumFramesToRender = 512;
     
     return self;
@@ -75,18 +53,6 @@
     if (![super allocateRenderResourcesAndReturnError:outError]) {
         return NO;
     }
-    if (self.outputBus.format.channelCount != _inputBus.bus.format.channelCount) {
-        if (outError) {
-            *outError = [NSError errorWithDomain:NSOSStatusErrorDomain
-                                            code:kAudioUnitErr_FailedInitialization
-                                        userInfo:nil];
-        }
-        // Notify superclass that initialization was not successful
-        self.renderResourcesAllocated = NO;
-        
-        return NO;
-    }
-    _inputBus.allocateRenderResources(self.maximumFramesToRender);
     
     [self setUpParameterRamp];
     
@@ -107,12 +73,6 @@
     self.parameterTree.implementorValueObserver = ^(AUParameter *param, AUValue value) {
         scheduleParameter(AUEventSampleTimeImmediate, rampTime, param.address, value);
     };
-}
-
-- (void)deallocateRenderResources {
-    [super deallocateRenderResources];
-    
-    _inputBus.deallocateRenderResources();
 }
 
 
