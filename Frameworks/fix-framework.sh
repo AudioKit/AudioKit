d0bd34604f22c7b75ee39c26d32992faf0854a38
Carlos García <carlosypunto@gmail.com>
2016-12-14 13:37:54 +0100

    Pevent the script emit errors when all is ok
    ![Actual script error](http://imgur.com/ZlEtixj)

--- Frameworks/fix-framework.sh
+++ Frameworks/fix-framework.sh
@@ -10,8 +10,19 @@ binary="$framework/AudioKit"
 
 if [ "$ACTION" == "install" ]; then
 	find "$framework/BCSymbolMaps" -name \*.bcsymbolmap -type f -exec mv {} "$CONFIGURATION_BUILD_DIR" \;
-	lipo -remove i386 -output "$binary" "$binary"
-	lipo -remove x86_64 -output "$binary" "$binary"
+
+	BYNARY_INFO=`lipo -info AudioKit`;
+	
+	if (`echo $BYNARY_INFO | grep i386`); then
+	    # Binary has i386
+	    lipo -remove i386 -output "$binary" "$binary"
+	fi
+
+	if (`echo $BYNARY_INFO | grep x86_64`); then
+	    # Binary has x86_64
+	    lipo -remove x86_64 -output "$binary" "$binary"
+	fi
+	
 	rm -f "$framework/fix-framework.sh"
 fi
 
