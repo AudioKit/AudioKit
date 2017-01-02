092993778b27cd155aac753573734cb84cd65de5
Adam Nemecek <adamnemecek@gmail.com>
2017-01-02 12:36:20 -0800

    * refactored AKLowShelfFilter

--- AudioKit/Common/Nodes/Effects/Filters/Low Shelf Filter/AKLowShelfFilter.swift
+++ AudioKit/Common/Nodes/Effects/Filters/Low Shelf Filter/AKLowShelfFilter.swift
@@ -14,20 +14,15 @@ open class AKLowShelfFilter: AKNode, AKToggleable, AUComponent {
 
     public static let ComponentDescription = AudioComponentDescription(appleEffect: kAudioUnitSubType_LowShelfFilter)
 
-    internal var internalEffect = AVAudioUnitEffect()
-    internal var internalAU: AudioUnit? = nil
-
-    fileprivate var mixer: AKMixer
+    private var internalEffect = AVAudioUnitEffect()
+    private var au: AUWrapper
+    private var mixer: AKMixer
 
     /// Cutoff Frequency (Hz) ranges from 10 to 200 (Default: 80)
     open var cutoffFrequency: Double = 80 {
         didSet {
             cutoffFrequency = (10...200).clamp(cutoffFrequency)
-            AudioUnitSetParameter(
-                internalAU!,
-                kAULowShelfParam_CutoffFrequency,
-                kAudioUnitScope_Global, 0,
-                Float(cutoffFrequency), 0)
+            au[kAULowShelfParam_CutoffFrequency] = cutoffFrequency
         }
     }
 
@@ -35,11 +30,7 @@ open class AKLowShelfFilter: AKNode, AKToggleable, AUComponent {
     open var gain: Double = 0 {
         didSet {
             gain = (-40...40).clamp(gain)
-            AudioUnitSetParameter(
-                internalAU!,
-                kAULowShelfParam_Gain,
-                kAudioUnitScope_Global, 0,
-                Float(gain), 0)
+            au[kAULowShelfParam_Gain] = gain
         }
     }
 
@@ -52,9 +43,9 @@ open class AKLowShelfFilter: AKNode, AKToggleable, AUComponent {
         }
     }
 
-    fileprivate var lastKnownMix: Double = 100
-    fileprivate var inputGain: AKMixer?
-    fileprivate var effectGain: AKMixer?
+    private var lastKnownMix: Double = 100
+    private var inputGain: AKMixer?
+    private var effectGain: AKMixer?
 
     // MARK: - Initialization
 
@@ -73,27 +64,28 @@ open class AKLowShelfFilter: AKNode, AKToggleable, AUComponent {
         cutoffFrequency: Double = 80,
         gain: Double = 0) {
 
-            self.cutoffFrequency = cutoffFrequency
-            self.gain = gain
+        self.cutoffFrequency = cutoffFrequency
+        self.gain = gain
 
-            inputGain = AKMixer(input)
-            inputGain!.volume = 0
-            mixer = AKMixer(inputGain!)
+        inputGain = AKMixer(input)
+        inputGain!.volume = 0
+        mixer = AKMixer(inputGain!)
 
-            effectGain = AKMixer(input)
-            effectGain!.volume = 1
+        effectGain = AKMixer(input)
+        effectGain!.volume = 1
 
-            internalEffect = AVAudioUnitEffect(audioComponentDescription: _Self.ComponentDescription)
-            super.init()
+        internalEffect = AVAudioUnitEffect(audioComponentDescription: _Self.ComponentDescription)
+        au = AUWrapper(au: internalEffect.audioUnit)
 
-            AudioKit.engine.attach(internalEffect)
-            internalAU = internalEffect.audioUnit
-            AudioKit.engine.connect((effectGain?.avAudioNode)!, to: internalEffect)
-            AudioKit.engine.connect(internalEffect, to: mixer.avAudioNode)
-            avAudioNode = mixer.avAudioNode
+        super.init()
 
-            AudioUnitSetParameter(internalAU!, kAULowShelfParam_CutoffFrequency, kAudioUnitScope_Global, 0, Float(cutoffFrequency), 0)
-            AudioUnitSetParameter(internalAU!, kAULowShelfParam_Gain, kAudioUnitScope_Global, 0, Float(gain), 0)
+        AudioKit.engine.attach(internalEffect)
+        AudioKit.engine.connect((effectGain?.avAudioNode)!, to: internalEffect)
+        AudioKit.engine.connect(internalEffect, to: mixer.avAudioNode)
+        avAudioNode = mixer.avAudioNode
+
+        au[kAULowShelfParam_CutoffFrequency] = cutoffFrequency
+        au[kAULowShelfParam_Gain] = gain
     }
 
     // MARK: - Control
