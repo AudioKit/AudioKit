57e3f18317f6010f146a611a626d2d97e7551fb7
Ryan Francesconi <rf@rf-mbp.local>
2017-01-23 12:32:59 -0800

    just cleanup

--- AudioKit/Common/Playgrounds/Playback.playground/Pages/Audio Player.xcplaygroundpage/Contents.swift
+++ AudioKit/Common/Playgrounds/Playback.playground/Pages/Audio Player.xcplaygroundpage/Contents.swift
@@ -86,11 +86,6 @@ class PlaygroundView: AKPlaygroundView {
         }
         addSubview(playingPositionSlider!)
     }
-    
-    func delay(_ delay:Double, closure:@escaping ()->()) {
-        DispatchQueue.main.asyncAfter(
-            deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: closure)
-    }
 }
 
 PlaygroundPage.current.needsIndefiniteExecution = true
