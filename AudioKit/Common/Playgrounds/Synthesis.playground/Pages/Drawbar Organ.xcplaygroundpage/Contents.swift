ff49e9413ac6c08a464fa2c06f1b3e61da561103
Aurelius Prochazka <aure@aure.com>
2017-01-29 01:34:23 -0800

    MIDINoteNumber related fix

--- AudioKit/Common/Playgrounds/Synthesis.playground/Pages/Drawbar Organ.xcplaygroundpage/Contents.swift
+++ AudioKit/Common/Playgrounds/Synthesis.playground/Pages/Drawbar Organ.xcplaygroundpage/Contents.swift
@@ -12,7 +12,7 @@ let noteCount = 9
 var amplitudes = Array<Double>(repeating: 0.1, count: noteCount)
 var offsets = [-12, 7, 0, 12, 19, 24, 28, 31, 36]
 var names = ["16", "5 1/3", "8", "4", "2 2/3", "2", "1 3/5", "1 1/3", "1"]
-var baseNote = 0
+var baseNote: MIDINoteNumber = 0
 
 class PlaygroundView: AKPlaygroundView, AKKeyboardDelegate {
 
@@ -50,13 +50,13 @@ class PlaygroundView: AKPlaygroundView, AKKeyboardDelegate {
 
     func stopAll() {
         for i in 0 ..< noteCount {
-            oscillator.stop(noteNumber: baseNote + offsets[i])
+            oscillator.stop(noteNumber: MIDINoteNumber(Int(baseNote) + offsets[i]))
         }
     }
 
     func startAll() {
         for i in 0 ..< noteCount {
-            oscillator.play(noteNumber: baseNote + offsets[i], velocity: Int(amplitudes[i] * 127))
+            oscillator.play(noteNumber: MIDINoteNumber(Int(baseNote) + offsets[i]), velocity: MIDIVelocity(amplitudes[i] * 127))
         }
     }
 }
