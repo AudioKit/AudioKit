dcc3e6eff630063686ddd973071e9b252455412a
Aurelius Prochazka <aure@aure.com>
2017-03-09 23:37:10 -0800

    Corrected a comment

--- AudioKit/Common/Operations/Effects/Distortion/clip.swift
+++ AudioKit/Common/Operations/Effects/Distortion/clip.swift
@@ -8,8 +8,7 @@
 
 extension AKComputedParameter {
 
-    /// Clips a signal to a predefined limit, in a "soft" manner, using one of three
-    /// methods.
+    /// Clips a signal to a predefined limit, in a "soft" manner.
     ///
     /// - parameter limit: Threshold / limiting value. (Default: 1.0, Minimum: 0.0, Maximum: 1.0)
     ///
