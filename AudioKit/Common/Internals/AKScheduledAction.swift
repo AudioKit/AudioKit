71515093dc99621d8a9285e508a52603f23789d3
David Sweetman <david@davidsweetman.com>
2017-04-01 18:36:32 -0400

    When we toggle an audio player to 'looping = false', stop the track at the end of the next loop.

---
+++ AudioKit/Common/Internals/AKScheduledAction.swift
@@ -0,0 +1,40 @@
+//
+//  AKScheduledAction.swift
+//  AudioKit For iOS
+//
+//  Created by David Sweetman on 4/1/17.
+//  Copyright © 2017 AudioKit. All rights reserved.
+//
+
+import Foundation
+
+class AKScheduledAction
+{
+    private var interval: TimeInterval
+    private var block: (() -> Void)
+    private var timer: Timer?
+    
+    init(interval: TimeInterval, block: @escaping () -> Void) {
+        self.interval = interval
+        self.block = block
+        start()
+    }
+    
+    dynamic func start() {
+        timer?.invalidate()
+        timer = nil
+        timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: false) { [weak self] (timer) in
+            guard timer.isValid else { return }
+            self?.block()
+        }
+    }
+    
+    dynamic func stop() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    deinit {
+        stop()
+    }
+}
