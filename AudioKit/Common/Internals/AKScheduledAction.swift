3eb7655c833c00cbeeac9119e3310a68b49012b9
David Sweetman <david@davidsweetman.com>
2017-04-01 20:15:20 -0400

    Fix some linter & spelling errors

--- AudioKit/Common/Internals/AKScheduledAction.swift
+++ AudioKit/Common/Internals/AKScheduledAction.swift
@@ -8,32 +8,34 @@
 
 import Foundation
 
-class AKScheduledAction
-{
+class AKScheduledAction {
+
     private var interval: TimeInterval
     private var block: (() -> Void)
     private var timer: Timer?
-    
+
     init(interval: TimeInterval, block: @escaping () -> Void) {
         self.interval = interval
         self.block = block
         start()
     }
-    
+
     dynamic func start() {
         timer?.invalidate()
         timer = nil
-        timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: false) { [weak self] (timer) in
-            guard timer.isValid else { return }
-            self?.block()
-        }
+        timer = Timer.scheduledTimer(withTimeInterval: interval,
+                                     repeats: false,
+                                     block: { [weak self] (timer) in
+                                        guard timer.isValid else { return }
+                                        self?.block()
+        })
     }
-    
+
     dynamic func stop() {
         timer?.invalidate()
         timer = nil
     }
-    
+
     deinit {
         stop()
     }
