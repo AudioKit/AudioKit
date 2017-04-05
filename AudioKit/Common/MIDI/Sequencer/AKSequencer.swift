a78dfdedffdcc67174a8f461abb3299945cb4a35
james <jsmilton05@gmail.com>
2017-04-05 19:21:11 +0100

    Fixed the other whitespace violation

--- AudioKit/Common/MIDI/Sequencer/AKSequencer.swift
+++ AudioKit/Common/MIDI/Sequencer/AKSequencer.swift
@@ -247,7 +247,7 @@ open class AKSequencer {
         }
         return  AKDuration(beats: length, tempo: tempo)
     }
-    
+
     /// Set the rate of the sequencer
     /// 
     /// - parameter rate: Set the rate relative to the tempo of the track
