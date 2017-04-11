4afa5cc715d88830acab94eaa2c4c6976d78df62
Aurelius Prochazka <aure@aure.com>
2017-04-11 01:24:11 -0700

    Initial version of AKPeriodicFunction

--- Playgrounds/AudioKitPlaygrounds/Playgrounds/Synthesis.playground/Pages/Plucked String.xcplaygroundpage/Contents.swift
+++ Playgrounds/AudioKitPlaygrounds/Playgrounds/Synthesis.playground/Pages/Plucked String.xcplaygroundpage/Contents.swift
@@ -13,22 +13,24 @@ delay.dryWetMix = 0.3
 delay.feedback = 0.2
 
 let reverb = AKReverb(delay)
-
-AudioKit.output = reverb
-AudioKit.start()
-let scale = [0, 2, 4, 5, 7, 9, 11, 12]
-
-AKPlaygroundLoop(frequency: playRate) {
+let performance = AKPeriodicFunction(frequency: playRate) {
     var note = scale.randomElement()
     let octave = [2, 3, 4, 5].randomElement() * 12
     if random(0, 10) < 1.0 { note += 1 }
     if !scale.contains(note % 12) { print("ACCIDENT!") }
-
+    
     let frequency = (note + octave).midiNoteToFrequency()
     if random(0, 6) > 1.0 {
         pluckedString.trigger(frequency: frequency)
     }
 }
+
+AudioKit.output = reverb
+AudioKit.periodicFunctions = [performance]
+AudioKit.start()
+
+let scale = [0, 2, 4, 5, 7, 9, 11, 12]
+performance.start()
 
 import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
