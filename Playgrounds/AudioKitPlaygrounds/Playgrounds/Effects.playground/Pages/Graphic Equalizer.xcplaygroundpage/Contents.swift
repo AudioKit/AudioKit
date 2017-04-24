20629dee3f06ec95ee898a437464f49562bc66fc
Aurelius Prochazka <aure@aure.com>
2017-04-23 18:55:47 -0700

    Using iTunes bands

--- Playgrounds/AudioKitPlaygrounds/Playgrounds/Effects.playground/Pages/Graphic Equalizer.xcplaygroundpage/Contents.swift
+++ Playgrounds/AudioKitPlaygrounds/Playgrounds/Effects.playground/Pages/Graphic Equalizer.xcplaygroundpage/Contents.swift
@@ -9,11 +9,14 @@ let file = try AKAudioFile(readFileName: processingPlaygroundFiles[0],
 let player = try AKAudioPlayer(file: file)
 player.looping = true
 
-var lowFilter = AKEqualizerFilter(player, centerFrequency: 50, bandwidth: 100, gain: 1.0)
-var midFilter = AKEqualizerFilter(lowFilter, centerFrequency: 350, bandwidth: 300, gain: 1.0)
-var highFilter = AKEqualizerFilter(midFilter, centerFrequency: 5_000, bandwidth: 1_000, gain: 1.0)
+let filterBand2 = AKEqualizerFilter(player, centerFrequency: 32, bandwidth: 44.7, gain: 1.0)
+let filterBand3 = AKEqualizerFilter(filterBand2, centerFrequency: 64, bandwidth: 70.8, gain: 1.0)
+let filterBand4 = AKEqualizerFilter(filterBand3, centerFrequency: 125, bandwidth: 141, gain: 1.0)
+let filterBand5 = AKEqualizerFilter(filterBand4, centerFrequency: 250, bandwidth: 282, gain: 1.0)
+let filterBand6 = AKEqualizerFilter(filterBand5, centerFrequency: 500, bandwidth: 562, gain: 1.0)
+let filterBand7 = AKEqualizerFilter(filterBand6, centerFrequency: 1_000, bandwidth: 1_112, gain: 1.0)
 
-AudioKit.output = highFilter
+AudioKit.output = filterBand7
 AudioKit.start()
 player.play()
 
@@ -31,28 +34,53 @@ class PlaygroundView: AKPlaygroundView {
         addLabel("Equalizer Gains")
 
         addSubview(AKPropertySlider(
-            property: "Low",
-            value: lowFilter.gain, maximum: 10,
+            property: "32Hz",
+            value: filterBand2.gain, maximum: 2,
             color: AKColor.red
         ) { sliderValue in
-            lowFilter.gain = sliderValue
+            filterBand2.gain = sliderValue
+        })
+        
+        addSubview(AKPropertySlider(
+            property: "64Hz",
+            value: filterBand3.gain, maximum: 2,
+            color: AKColor.red
+        ) { sliderValue in
+            filterBand3.gain = sliderValue
+        })
+        
+        addSubview(AKPropertySlider(
+            property: "125Hz",
+            value: filterBand4.gain, maximum: 2,
+            color: AKColor.red
+        ) { sliderValue in
+            filterBand4.gain = sliderValue
+        })
+        
+        addSubview(AKPropertySlider(
+            property: "250Hz",
+            value: filterBand5.gain, maximum: 2,
+            color: AKColor.red
+        ) { sliderValue in
+            filterBand5.gain = sliderValue
+        })
+        
+        addSubview(AKPropertySlider(
+            property: "500Hz",
+            value: filterBand6.gain, maximum: 2,
+            color: AKColor.red
+        ) { sliderValue in
+            filterBand6.gain = sliderValue
+        })
+        
+        addSubview(AKPropertySlider(
+            property: "1000Hz",
+            value: filterBand7.gain, maximum: 2,
+            color: AKColor.red
+        ) { sliderValue in
+            filterBand7.gain = sliderValue
         })
 
-        addSubview(AKPropertySlider(
-            property: "Mid",
-            value: midFilter.gain, maximum: 10,
-            color: AKColor.green
-        ) { sliderValue in
-            midFilter.gain = sliderValue
-        })
-
-        addSubview(AKPropertySlider(
-            property: "High",
-            value: highFilter.gain, maximum: 10,
-            color: AKColor.cyan
-        ) { sliderValue in
-            highFilter.gain = sliderValue
-        })
     }
 
 }
