53d64a6e09ff1fc4cdb23d86c27fdf7f14122942
Marcus Hobbs <mh160c@att.com>
2017-05-02 13:25:33 -0700

    added documentation

--- AudioKit/Common/Internals/Microtonality/AKTuningTable+EqualTemperament.swift
+++ AudioKit/Common/Internals/Microtonality/AKTuningTable+EqualTemperament.swift
@@ -7,25 +7,31 @@
 //
 
 extension AKTuningTable {
-
-    // Default tuning table is 12ET.
-    public func defaultTuning() {
-        twelveToneEqualTemperament()
+  
+  // Default tuning table is 12ET.
+  public func defaultTuning() {
+    twelveToneEqualTemperament()
+  }
+  
+  /// Create 12-tone equal temperament
+  public func twelveToneEqualTemperament() {
+    equalTemperament(notesPerOctave: 12)
+  }
+  
+  /// Create 31-tone equal temperament
+  public func thirtyOneEqualTemperament() {
+    equalTemperament(notesPerOctave: 31)
+  }
+  
+  /// Create an equal temperament with notesPerOctave
+  ///
+  /// - parameter notesPerOctave divides the octave equally by this many steps
+  ///
+  public func equalTemperament(notesPerOctave npo: Int) {
+    var nf = [Frequency](repeatElement(1.0, count: npo))
+    for i in 0 ..< npo {
+      nf[i] = Frequency(pow(2.0, Frequency(Frequency(i) / npo)))
     }
-
-    public func twelveToneEqualTemperament() {
-        equalTemperament(notesPerOctave: 12)
-    }
-
-    public func thirtyOneEqualTemperament() {
-        equalTemperament(notesPerOctave: 31)
-    }
-
-    public func equalTemperament(notesPerOctave npo: Int) {
-        var nf = [Frequency](repeatElement(1.0, count: npo))
-        for i in 0 ..< npo {
-            nf[i] = Frequency(pow(2.0, Frequency(Frequency(i) / npo)))
-        }
-        tuningTable(fromFrequencies: nf)
-    }
+    tuningTable(fromFrequencies: nf)
+  }
 }
