95ad945193e5031c2b0ff68c78b7dff9548905f8
Marcus Hobbs <marcushobbs@mac.com>
2017-05-02 11:50:55 -0700

    corrected octave reduction.  adding more comments.  some methods now return # notes per octave.

--- AudioKit/Common/Internals/Microtonality/AKTuningTable+Wilson.swift
+++ AudioKit/Common/Internals/Microtonality/AKTuningTable+Wilson.swift
@@ -9,12 +9,15 @@
 extension AKTuningTable {
 
     // From Erv Wilson
-    public func presetHighlandBagPipes() {
+    public func presetHighlandBagPipes() -> Int {
         tuningTable(fromFrequencies: [32, 36, 39, 171, 48, 52, 57])
+        return 7
     }
 
-    public func presetDiaphonicTetrachord() {
+    // From Erv Wilson
+    public func presetDiaphonicTetrachord() -> Int {
         tuningTable(fromFrequencies: [1, 27 / 26.0, 9 / 8.0, 4 / 3.0, 18 / 13.0, 3 / 2.0, 27 / 16.0])
+        return 7
     }
 
 }
