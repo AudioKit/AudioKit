61ad3432496c03013dcd4b2ef6bdef8727c73475
Aurelius Prochazka <aure@aure.com>
2017-05-11 01:04:36 -0700

    Swiftlinting

--- AudioKit/Common/MIDI/AKMIDIEndpointInfo.swift
+++ AudioKit/Common/MIDI/AKMIDIEndpointInfo.swift
@@ -1,6 +1,6 @@
 //
 //  AKMIDIEndpointInfo.swift
-//  AudioKit For iOS
+//  AudioKit
 //
 //  Created by dejaWorks on 06/05/2017.
 //  Copyright © 2017 AudioKit. All rights reserved.
@@ -13,19 +13,17 @@ public struct EndpointInfo {
     public var manufacturer = ""
     public var image = ""
     public var driverOwner = ""
-
 }
+
 extension Collection where Iterator.Element == MIDIEndpointRef {
     var endpointInfos: [EndpointInfo] {
         
-        let name            = map { GetMIDIObjectStringProperty(ref: $0, property: kMIDIPropertyName) }
-        let displayName     = map { GetMIDIObjectStringProperty(ref: $0, property: kMIDIPropertyDisplayName) }
-        let manufacturer    = map { GetMIDIObjectStringProperty(ref: $0, property: kMIDIPropertyModel) }
-        let model           = map { GetMIDIObjectStringProperty(ref: $0, property: kMIDIPropertyManufacturer) }
-        let image           = map { GetMIDIObjectStringProperty(ref: $0, property: kMIDIPropertyImage) }
-        let driverOwner     = map { GetMIDIObjectStringProperty(ref: $0, property: kMIDIPropertyDriverOwner) }
-        
-        
+        let name         = map { GetMIDIObjectStringProperty(ref: $0, property: kMIDIPropertyName) }
+        let displayName  = map { GetMIDIObjectStringProperty(ref: $0, property: kMIDIPropertyDisplayName) }
+        let manufacturer = map { GetMIDIObjectStringProperty(ref: $0, property: kMIDIPropertyModel) }
+        let model        = map { GetMIDIObjectStringProperty(ref: $0, property: kMIDIPropertyManufacturer) }
+        let image        = map { GetMIDIObjectStringProperty(ref: $0, property: kMIDIPropertyImage) }
+        let driverOwner  = map { GetMIDIObjectStringProperty(ref: $0, property: kMIDIPropertyDriverOwner) }
         
         var ei = [EndpointInfo]()
         for i in 0 ..< displayName.count{
@@ -34,8 +32,7 @@ extension Collection where Iterator.Element == MIDIEndpointRef {
                                    model: model[i],
                                    manufacturer: manufacturer[i],
                                    image: image[i],
-                                   driverOwner: driverOwner[i]
-                                   ))
+                                   driverOwner: driverOwner[i]))
         }
         return ei
     }
@@ -46,6 +43,7 @@ extension AKMIDI {
     public var destinationInfos: [EndpointInfo] {
         return MIDIDestinations().endpointInfos
     }
+    
     // Array
     public var inputInfos: [EndpointInfo] {
         return MIDISources().endpointInfos
