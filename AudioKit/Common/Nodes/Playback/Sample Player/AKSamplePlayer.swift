a79213b26173e10c36de59f4bd35502c2ef0b0e1
Aurelius Prochazka <aure@aure.com>
2017-05-21 00:33:05 -0700

    Swiftlint autocorrect new AKSamplePlayer

--- AudioKit/Common/Nodes/Playback/Sample Player/AKSamplePlayer.swift
+++ AudioKit/Common/Nodes/Playback/Sample Player/AKSamplePlayer.swift
@@ -11,23 +11,23 @@ import Foundation
 open class AKSamplePlayer: AKNode, AKComponent {
     public typealias AKAudioUnitType = AKSamplePlayerAudioUnit
     public static let ComponentDescription = AudioComponentDescription(generator: "smpl")
-    
+
     // MARK: - Properties
-    
+
     private var internalAU: AKAudioUnitType?
     private var token: AUParameterObserverToken?
-    
+
     fileprivate var startPointParameter: AUParameter?
     fileprivate var endPointParameter: AUParameter?
     fileprivate var rateParameter: AUParameter?
-    
+
     /// Ramp Time represents the speed at which parameters are allowed to change
     open dynamic var rampTime: Double = AKSettings.rampTime {
         willSet {
             internalAU?.rampTime = newValue
         }
     }
-    
+
     /// startPoint in time. When non-changing it will do a spectral freeze of a the current point in time.
     open dynamic var startPoint: Int = 0 {
         willSet {
@@ -42,7 +42,7 @@ open class AKSamplePlayer: AKNode, AKComponent {
             }
         }
     }
-    
+
     /// endPoint.
     open dynamic var endPoint: Int = 1 {
         willSet {
@@ -57,7 +57,7 @@ open class AKSamplePlayer: AKNode, AKComponent {
             }
         }
     }
-    
+
     /// Pitch ratio. A value of 1 is normal, 2 is double speed, 0.5 is halfspeed, etc.
     open dynamic var rate: Double = 1 {
         willSet {
@@ -72,23 +72,23 @@ open class AKSamplePlayer: AKNode, AKComponent {
             }
         }
     }
-    
+
     /// Loop Enabled
     open dynamic var loop: Bool = false {
         willSet {
             internalAU?.loop = loop
         }
     }
-    
+
     /// Tells whether the node is processing (ie. started, playing, or active)
     open dynamic var isStarted: Bool {
         return internalAU?.isPlaying() ?? false
     }
-    
+
     fileprivate var avAudiofile: AVAudioFile
-    
+
     // MARK: - Initialization
-    
+
     /// Initialize this SamplePlayer node
     ///
     public init(
@@ -96,32 +96,32 @@ open class AKSamplePlayer: AKNode, AKComponent {
         startPoint: Int = 0,
         endPoint: Int = 1,
         rate: Double = 1) {
-        
+
         self.startPoint = startPoint
         self.endPoint = endPoint
         self.rate = rate
         self.avAudiofile = file
-        
+
         _Self.register()
-        
+
         super.init()
-        
+
         AVAudioUnit._instantiate(with: _Self.ComponentDescription) { [weak self] avAudioUnit in
-            
+
             self?.avAudioNode = avAudioUnit
             self?.internalAU = avAudioUnit.auAudioUnit as? AKAudioUnitType
         }
-        
+
         guard let tree = internalAU?.parameterTree else {
             return
         }
-        
+
         startPointParameter = tree["startPoint"]
         endPointParameter = tree["endPoint"]
         rateParameter = tree["rate"]
-        
+
         token = tree.token(byAddingParameterObserver: { [weak self] address, value in
-            
+
             DispatchQueue.main.async {
                 if address == self?.startPointParameter?.address {
                     self?.startPoint = Int(value)
@@ -136,9 +136,9 @@ open class AKSamplePlayer: AKNode, AKComponent {
         internalAU?.endPoint = Float(endPoint)
         internalAU?.rate = Float(rate)
     }
-    
+
     // MARK: - Control
-    
+
     /// Function to start, play, or activate the node, all do the same thing
     open func start() {
         Exit: do {
@@ -149,7 +149,7 @@ open class AKSamplePlayer: AKNode, AKComponent {
             var extRef: ExtAudioFileRef?
             var theData: UnsafeMutablePointer<CChar>?
             var theOutputFormat: AudioStreamBasicDescription = AudioStreamBasicDescription()
-            
+
             err = ExtAudioFileOpenURL(self.avAudiofile.url as CFURL, &extRef)
             if err != 0 { AKLog("ExtAudioFileOpenURL FAILED, Error = \(err)"); break Exit }
             // Get the audio data format
@@ -168,7 +168,7 @@ open class AKSamplePlayer: AKNode, AKComponent {
                 AKLog("Unsupported Format, channel count is greater than stereo")
                 break Exit
             }
-            
+
             theOutputFormat.mSampleRate = AKSettings.sampleRate
             theOutputFormat.mFormatID = kAudioFormatLinearPCM
             theOutputFormat.mFormatFlags = kLinearPCMFormatFlagIsFloat
@@ -177,7 +177,7 @@ open class AKSamplePlayer: AKNode, AKComponent {
             theOutputFormat.mBytesPerFrame = theOutputFormat.mChannelsPerFrame * UInt32(MemoryLayout<Float>.stride)
             theOutputFormat.mFramesPerPacket = 1
             theOutputFormat.mBytesPerPacket = theOutputFormat.mFramesPerPacket * theOutputFormat.mBytesPerFrame
-            
+
             // Set the desired client (output) data format
             err = ExtAudioFileSetProperty(externalAudioFileRef,
                                           kExtAudioFileProperty_ClientDataFormat,
@@ -187,7 +187,7 @@ open class AKSamplePlayer: AKNode, AKComponent {
                 AKLog("ExtAudioFileSetProperty(kExtAudioFileProperty_ClientDataFormat) FAILED, Error = \(err)")
                 break Exit
             }
-            
+
             // Get the total frame count
             thePropertySize = UInt32(MemoryLayout.stride(ofValue: theFileLengthInFrames))
             err = ExtAudioFileGetProperty(externalAudioFileRef,
@@ -198,7 +198,7 @@ open class AKSamplePlayer: AKNode, AKComponent {
                 AKLog("ExtAudioFileGetProperty(kExtAudioFileProperty_FileLengthFrames) FAILED, Error = \(err)")
                 break Exit
             }
-            
+
             // Read all the data into memory
             let dataSize = UInt32(theFileLengthInFrames) * theOutputFormat.mBytesPerFrame
             theData = UnsafeMutablePointer.allocate(capacity: Int(dataSize))
@@ -208,7 +208,7 @@ open class AKSamplePlayer: AKNode, AKComponent {
                 bufferList.mBuffers.mDataByteSize = dataSize
                 bufferList.mBuffers.mNumberChannels = theOutputFormat.mChannelsPerFrame
                 bufferList.mBuffers.mData = UnsafeMutableRawPointer(theData)
-                
+
                 // Read the data into an AudioBufferList
                 var ioNumberFrames: UInt32 = UInt32(theFileLengthInFrames)
                 err = ExtAudioFileRead(externalAudioFileRef, &ioNumberFrames, &bufferList)
@@ -228,22 +228,22 @@ open class AKSamplePlayer: AKNode, AKComponent {
             }
         }
     }
-    
+
     /// Function to stop or bypass the node, both are equivalent
     open func stop() {
         internalAU?.stop()
     }
-    
-    open func play(from:Int = 0){
+
+    open func play(from: Int = 0) {
         startPoint = from
         start()
     }
-    open func play(from:Int = 0, length:Int = 0){
+    open func play(from: Int = 0, length: Int = 0) {
         startPoint = from
         endPoint = startPoint + length
         start()
     }
-    open func play(from:Int = 0, to:Int = 0){
+    open func play(from: Int = 0, to: Int = 0) {
         startPoint = from
         endPoint = to
         start()
