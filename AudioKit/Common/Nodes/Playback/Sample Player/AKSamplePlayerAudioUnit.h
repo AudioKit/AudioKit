52f5f9d1c1074488567dcf5613698d7be4074167
Jeff Cooper <jeff@southernworkstx.com>
2017-05-20 21:35:12 -0500

    aksampleplayer added

---
+++ AudioKit/Common/Nodes/Playback/Sample Player/AKSamplePlayerAudioUnit.h
@@ -0,0 +1,22 @@
+//
+//  AKSamplePlayerAudioUnit.h
+//  AudioKit
+//
+//  Created by Aurelius Prochazka, revision history on Github.
+//  Copyright © 2017 Aurelius Prochazka. All rights reserved.
+//
+
+#pragma once
+#import "AKAudioUnit.h"
+
+@interface AKSamplePlayerAudioUnit : AKAudioUnit
+@property (nonatomic) float startPoint;
+@property (nonatomic) float endPoint;
+@property (nonatomic) float rate;
+@property (nonatomic) BOOL loop;
+
+- (void)setupAudioFileTable:(float *)data size:(UInt32)size;
+
+@end
+
+
