f2fbd31cb360d4198c9c1e3f8f2779a5a529245c
Eric George <eegee.dev@gmail.com>
2017-07-07 18:31:55 -0700

    swiftlint on playgrounds

--- AudioKit/macOS/AudioKit/Dev.playground/Pages/MIDIChords.xcplaygroundpage/Contents.swift
+++ AudioKit/macOS/AudioKit/Dev.playground/Pages/MIDIChords.xcplaygroundpage/Contents.swift
@@ -1,6 +1,5 @@
 //: MIDIChords
 
-
 import AudioKit
 
 let sampler = AKSampler()
@@ -15,18 +14,18 @@ mixer.volume = 5.0
 AudioKit.output = mixer
 AudioKit.start()
 
-let keys = ["C" : 0,
+let keys = ["C": 0,
             "Db": 1,
-            "D" : 2,
+            "D": 2,
             "Eb": 3,
-            "E" : 4,
-            "F" : 5,
+            "E": 4,
+            "F": 5,
             "Gb": 6,
-            "G" : 7,
+            "G": 7,
             "Ab": 8,
-            "A" : 9,
+            "A": 9,
             "Bb": 10,
-            "B" : 11]
+            "B": 11]
 
 let modes = ["major": [0, 2, 4, 5, 7, 9, 11],
              "minor": [0, 2, 3, 5, 7, 8, 10]]
@@ -35,12 +34,12 @@ let majorTriad      = [0, 4, 7]
 let minorTriad      = [0, 3, 7]
 let diminishedTriad = [0, 3, 6]
 
-let chords = ["major":[majorTriad, minorTriad, minorTriad, majorTriad, majorTriad, minorTriad, diminishedTriad],
-              "minor":[minorTriad, diminishedTriad, majorTriad, minorTriad, minorTriad, majorTriad, majorTriad]]
+let chords = ["major": [majorTriad, minorTriad, minorTriad, majorTriad, majorTriad, minorTriad, diminishedTriad],
+              "minor": [minorTriad, diminishedTriad, majorTriad, minorTriad, minorTriad, majorTriad, majorTriad]]
 
-var key:Int!
-var mode:[Int]!
-var chordSet:[[Int]]!
+var key: Int!
+var mode: [Int]!
+var chordSet: [[Int]]!
 
 let midi = AKMIDI()
 
@@ -48,9 +47,9 @@ midi.inputNames
 midi.openInput()
 
 class MIDIScaleQuantizer: AKMIDITransformer {
-    func transform(eventList:[AKMIDIEvent]) -> [AKMIDIEvent] {
+    func transform(eventList: [AKMIDIEvent]) -> [AKMIDIEvent] {
         var transformedList = [AKMIDIEvent]()
-        
+
         for event in eventList {
             guard let type = event.status else {
                 transformedList.append(event)
@@ -61,49 +60,48 @@ class MIDIScaleQuantizer: AKMIDITransformer {
                 if event.noteNumber != nil, mode != nil, key != nil {
                     let normalizedNote = (Int(event.noteNumber!) - key) % 12
                     let octave = (Int(event.noteNumber!) - key) / 12
-                    var inScaleNote:Int?
-                    
-                    for number in mode {
-                        if number <= normalizedNote {
+                    var inScaleNote: Int?
+
+                    for number in mode where number <= normalizedNote {
                             inScaleNote = number
-                        }
                     }
-                    
+
                     if inScaleNote != nil {
                         let newNote = octave * 12 + inScaleNote! + key!
-                        transformedList.append(AKMIDIEvent(noteOn: MIDINoteNumber(newNote), velocity: event.data2, channel: event.channel!))
+                        transformedList.append(AKMIDIEvent(noteOn: MIDINoteNumber(newNote),
+                                                           velocity: event.data2,
+                                                           channel: event.channel!))
                     }
                 }
             case .noteOff:
                 if event.noteNumber != nil, mode != nil, key != nil {
                     let normalizedNote = (Int(event.noteNumber!) - key) % 12
                     let octave = (Int(event.noteNumber!) - key) / 12
-                    var inScaleNote:Int?
-                    
-                    for number in mode {
-                        if number <= normalizedNote {
-                            inScaleNote = number
-                        }
+                    var inScaleNote: Int?
+
+                    for number in mode where number <= normalizedNote {
+                        inScaleNote = number
                     }
-                    
+
                     if inScaleNote != nil {
                         let newNote = octave * 12 + inScaleNote! + key!
-                        transformedList.append(AKMIDIEvent(noteOff: MIDINoteNumber(newNote), velocity: 0, channel: event.channel!))
+                        transformedList.append(AKMIDIEvent(noteOff: MIDINoteNumber(newNote),
+                                                           velocity: 0,
+                                                           channel: event.channel!))
                     }
                 }
             default:
                 transformedList.append(event)
             }
         }
-        
         return transformedList
     }
 }
 
 class MIDIChordGenerator: AKMIDITransformer {
-    func transform(eventList:[AKMIDIEvent]) -> [AKMIDIEvent] {
+    func transform(eventList: [AKMIDIEvent]) -> [AKMIDIEvent] {
         var transformedList = [AKMIDIEvent]()
-        
+
         for event in eventList {
             guard let type = event.status else {
                 transformedList.append(event)
@@ -113,26 +111,30 @@ class MIDIChordGenerator: AKMIDITransformer {
             case .noteOn:
                 if event.noteNumber != nil, mode != nil, key != nil {
                     let normalizedNote = (Int(event.noteNumber!) - key) % 12
-                    let scaleDegree:Int? = mode.index(of: normalizedNote)
+                    let scaleDegree: Int? = mode.index(of: normalizedNote)
                     if scaleDegree != nil {
                         let chordTemplate = chordSet[scaleDegree!]
 
                         for note in chordTemplate {
                             AKLog("noteOn: chord note is: \(note + Int(event.noteNumber!))")
-                            transformedList.append(AKMIDIEvent(noteOn: MIDINoteNumber(note + Int(event.noteNumber!)), velocity: event.data2, channel: event.channel!))
+                            transformedList.append(AKMIDIEvent(noteOn: MIDINoteNumber(note + Int(event.noteNumber!)),
+                                                               velocity: event.data2,
+                                                               channel: event.channel!))
                         }
                     }
                 }
             case .noteOff:
                 if event.noteNumber != nil, mode != nil, key != nil {
                     let normalizedNote = (Int(event.noteNumber!) - key) % 12
-                    let scaleDegree:Int? = mode.index(of: normalizedNote)
+                    let scaleDegree: Int? = mode.index(of: normalizedNote)
                     if scaleDegree != nil {
                         let chordTemplate = chordSet[scaleDegree!]
-                        
+
                         for note in chordTemplate {
                             AKLog("noteOff: chord note is: \(note + Int(event.noteNumber!))")
-                            transformedList.append(AKMIDIEvent(noteOff: MIDINoteNumber(note + Int(event.noteNumber!)), velocity: event.data2, channel: event.channel!))
+                            transformedList.append(AKMIDIEvent(noteOff: MIDINoteNumber(note + Int(event.noteNumber!)),
+                                                               velocity: event.data2,
+                                                               channel: event.channel!))
                         }
                     }
                 }
@@ -140,10 +142,10 @@ class MIDIChordGenerator: AKMIDITransformer {
                 transformedList.append(event)
             }
         }
-        
         return transformedList
     }
 }
+
 let scaleQuantizer = MIDIScaleQuantizer()
 let chordGenerator = MIDIChordGenerator()
 
@@ -164,10 +166,9 @@ let listener = PlaygroundMIDIListener()
 midi.addListener(listener)
 
 class PlaygroundView: AKPlaygroundView {
-    
     override func setup() {
         addTitle("Scale Quantizer")
-        
+
         let keyPresets = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
         addSubview(AKPresetLoaderView(presets: keyPresets) { preset in
             switch preset {
@@ -218,5 +219,3 @@ class PlaygroundView: AKPlaygroundView {
 import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 PlaygroundPage.current.liveView = PlaygroundView()
-
-
