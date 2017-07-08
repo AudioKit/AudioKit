28f5b07ed10a87fa44a8122fc18ec33f8f0b086f
Eric George <eegee.dev@gmail.com>
2017-07-07 18:31:55 -0700

    swiftlint on playgrounds

--- AudioKit/macOS/AudioKit/Dev.playground/Pages/MIDIScaleQuantize.xcplaygroundpage/Contents.swift
+++ AudioKit/macOS/AudioKit/Dev.playground/Pages/MIDIScaleQuantize.xcplaygroundpage/Contents.swift
@@ -1,6 +1,5 @@
 //: MIDIScaleQuantize
 
-
 import AudioKit
 
 let sampler = AKSampler()
@@ -15,23 +14,23 @@ mixer.volume = 5.0
 AudioKit.output = mixer
 AudioKit.start()
 
-let keys = ["C" : 0,
+let keys = ["C": 0,
             "Db": 1,
-            "D" : 2,
+            "D": 2,
             "Eb": 3,
-            "E" : 4,
-            "F" : 5,
+            "E": 4,
+            "F": 5,
             "Gb": 6,
-            "G" : 7,
+            "G": 7,
             "Ab": 8,
-            "A" : 9,
+            "A": 9,
             "Bb": 10,
-            "B" : 11]
+            "B": 11]
 
 let modes = ["major": [0, 2, 4, 5, 7, 9, 11],
              "minor": [0, 2, 3, 5, 7, 8, 10]]
-var key:Int!
-var mode:[Int]!
+var key: Int!
+var mode: [Int]!
 
 let midi = AKMIDI()
 
@@ -39,9 +38,9 @@ midi.inputNames
 midi.openInput()
 
 class MIDIScaleQuantizer: AKMIDITransformer {
-    func transform(eventList:[AKMIDIEvent]) -> [AKMIDIEvent] {
+    func transform(eventList: [AKMIDIEvent]) -> [AKMIDIEvent] {
         var transformedList = [AKMIDIEvent]()
-        
+
         for event in eventList {
             guard let type = event.status else {
                 transformedList.append(event)
@@ -52,41 +51,41 @@ class MIDIScaleQuantizer: AKMIDITransformer {
                 if event.noteNumber != nil, mode != nil, key != nil {
                     let normalizedNote = (Int(event.noteNumber!) - key) % 12
                     let octave = (Int(event.noteNumber!) - key) / 12
-                    var inScaleNote:Int?
-                    
-                    for number in mode {
-                        if number <= normalizedNote {
+                    var inScaleNote: Int?
+
+                    for number in mode where number <= normalizedNote {
                             inScaleNote = number
-                        }
                     }
-                    
+
                     if inScaleNote != nil {
                         let newNote = octave * 12 + inScaleNote! + key
-                        transformedList.append(AKMIDIEvent(noteOn: MIDINoteNumber(newNote), velocity: event.data2, channel: event.channel!))
+                        transformedList.append(AKMIDIEvent(noteOn: MIDINoteNumber(newNote),
+                                                           velocity: event.data2,
+                                                           channel: event.channel!))
                     }
                 }
              case .noteOff:
                 if event.noteNumber != nil, mode != nil, key != nil {
                     let normalizedNote = (Int(event.noteNumber!) - key) % 12
                     let octave = (Int(event.noteNumber!) - key) / 12
-                    var inScaleNote:Int?
-                    
-                    for number in mode {
-                        if number <= normalizedNote {
-                            inScaleNote = number
-                        }
+                    var inScaleNote: Int?
+
+                    for number in mode where number <= normalizedNote {
+                        inScaleNote = number
                     }
-                    
+
                     if inScaleNote != nil {
                         let newNote = octave * 12 + inScaleNote! + key
-                        transformedList.append(AKMIDIEvent(noteOff: MIDINoteNumber(newNote), velocity: 0, channel: event.channel!))
+                        transformedList.append(AKMIDIEvent(noteOff: MIDINoteNumber(newNote),
+                                                           velocity: 0,
+                                                           channel: event.channel!))
                     }
                 }
             default:
                 transformedList.append(event)
             }
         }
-        
+
         return transformedList
     }
 }
@@ -108,10 +107,9 @@ let listener = PlaygroundMIDIListener()
 midi.addListener(listener)
 
 class PlaygroundView: AKPlaygroundView {
-    
     override func setup() {
         addTitle("Scale Quantizer")
-        
+
         let keyPresets = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
         addSubview(AKPresetLoaderView(presets: keyPresets) { preset in
             switch preset {
@@ -160,4 +158,3 @@ class PlaygroundView: AKPlaygroundView {
 import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 PlaygroundPage.current.liveView = PlaygroundView()
-
