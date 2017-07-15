734d845cac8c1ec3f502c96aa3e4b4c1f010fd0d
Aurelius Prochazka <aure@aure.com>
2017-07-15 13:13:18 -0700

    Improving tests

--- AudioKit/Common/Tests/AKCompressorTests.swift
+++ AudioKit/Common/Tests/AKCompressorTests.swift
@@ -17,4 +17,29 @@ class AKCompressorTests: AKTestCase {
         input.start()
         AKTestMD5("cf12ea8dbbe0a1bee2c4c83956c61a49")
     }
+
+    func testParameters1() {
+        let input = AKOscillator()
+        output = AKCompressor(input,
+                              threshold: -25,
+                              headRoom: 10,
+                              attackTime: 0.1,
+                              releaseTime: 0.1,
+                              masterGain: 1)
+        input.start()
+        AKTestMD5("e83f89c8249e4044639328b7fad55628")
+    }
+
+    func testParameters2() {
+        let input = AKOscillator()
+        output = AKCompressor(input,
+                              threshold: -30,
+                              headRoom: 20,
+                              attackTime: 0.01,
+                              releaseTime: 0.01,
+                              masterGain: -1)
+        input.start()
+        AKTestMD5("7ca40d7ff9964ceca52edc9835379a56")
+    }
+
 }
--- AudioKit/Common/Tests/AKDecimatorTests.swift
+++ AudioKit/Common/Tests/AKDecimatorTests.swift
@@ -17,4 +17,11 @@ class AKDecimatorTests: AKTestCase {
         input.start()
         AKTestMD5("06415139391aca488441c521aea09726")
     }
+
+    func testParameters() {
+        let input = AKOscillator()
+        output = AKDecimator(input, decimation: 0.75, rounding: 0.5, mix: 0.5)
+        input.start()
+        AKTestMD5("b1f53c4d7e4040362baf457352d3e6f4")
+    }
 }
