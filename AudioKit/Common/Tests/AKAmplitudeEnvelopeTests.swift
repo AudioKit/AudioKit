aa8697c0a186a36d49534864089630d5db2b36cd
Aurelius Prochazka <aure@aure.com>
2017-07-17 00:48:26 -0700

    Beefing up test coverage

--- AudioKit/Common/Tests/AKAmplitudeEnvelopeTests.swift
+++ AudioKit/Common/Tests/AKAmplitudeEnvelopeTests.swift
@@ -11,17 +11,47 @@ import XCTest
 
 class AKAmplitudeEnvelopeTests: AKTestCase {
 
+    override func setUp() {
+        super.setUp()
+        // Need to have a longer test duration to allow for envelope to progress
+        duration = 1.0
+    }
+
     func testDefault() {
         let input = AKOscillator()
         output = AKAmplitudeEnvelope(input)
         input.start()
-        AKTestMD5("793ce26c8e18b1e224460cf3a7b45931")
+        AKTestMD5("bfaf1f674bd86c1e45fdac3b96e96fe8")
     }
 
     func testParameters() {
         let input = AKOscillator()
-        output = AKAmplitudeEnvelope(input, attackDuration: 0.05) // Only attack is being tested properly
+        output = AKAmplitudeEnvelope(input, attackDuration: 0.1234, decayDuration: 0.234, sustainLevel: 0.345)
         input.start()
-        AKTestMD5("f42e67b02ea316f01db2a145321ce450")
+        AKTestMD5("9a788f314cdfd0cb8834837246b7b2d9")
     }
+
+    func testAttack() {
+        let input = AKOscillator()
+        output = AKAmplitudeEnvelope(input, attackDuration: 0.1234)
+        input.start()
+        AKTestMD5("6d1bd9d118a9a51accb1a8d077ba3b8f")
+    }
+
+    func testDecay() {
+        let input = AKOscillator()
+        output = AKAmplitudeEnvelope(input, decayDuration: 0.234, sustainLevel: 0.345)
+        input.start()
+        AKTestMD5("1723f29dc04272525bdfe6cce82a7179")
+    }
+
+    func testSustain() {
+        let input = AKOscillator()
+        output = AKAmplitudeEnvelope(input, sustainLevel: 0.345)
+        input.start()
+        AKTestMD5("47820ec698e568481eff7e744f21657f")
+    }
+
+    // Release is not tested at this time since there is no sample accurate way to define release point
+
 }
--- AudioKit/Common/Tests/AKBandPassButterworthFilterTests.swift
+++ AudioKit/Common/Tests/AKBandPassButterworthFilterTests.swift
@@ -17,4 +17,26 @@ class AKBandPassButterworthFilterTests: AKTestCase {
         input.start()
         AKTestMD5("574b9097eff0c33a2a04ae112fe19164")
     }
+
+    func testParameters() {
+        let input = AKOscillator()
+        output = AKBandPassButterworthFilter(input, centerFrequency: 1500, bandwidth: 200)
+        input.start()
+        AKTestMD5("e1185c8d2a1772f989e5439320997ef3")
+    }
+
+    func testCenterFrequency() {
+        let input = AKOscillator()
+        output = AKBandPassButterworthFilter(input, centerFrequency: 1500)
+        input.start()
+        AKTestMD5("754a44acd72dc5c9c8cf35bb53e6e753")
+    }
+
+    func testBandwidth() {
+        let input = AKOscillator()
+        output = AKBandPassButterworthFilter(input, bandwidth: 200)
+        input.start()
+        AKTestMD5("48032ecc3ff8bc7dbd47cdfa77f561fd")
+    }
+
 }
