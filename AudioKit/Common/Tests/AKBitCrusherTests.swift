aac498fab9ad7f3c5f60866a49e50bc2e6885ddb
Aurelius Prochazka <aure@aure.com>
2017-07-17 00:48:26 -0700

    Beefing up test coverage

--- AudioKit/Common/Tests/AKBitCrusherTests.swift
+++ AudioKit/Common/Tests/AKBitCrusherTests.swift
@@ -17,4 +17,27 @@ class AKBitCrusherTests: AKTestCase {
         input.start()
         AKTestMD5("801dc339290c62b79f18ae22828ee665")
     }
+
+    func testParameters() {
+        let input = AKOscillator()
+        output = AKBitCrusher(input, bitDepth: 12, sampleRate: 2400)
+        input.start()
+        AKTestMD5("016a569c401187b65a8f9e6e5680c27a")
+    }
+
+    func testBitDepth() {
+        let input = AKOscillator()
+        output = AKBitCrusher(input, bitDepth: 12)
+        input.start()
+        AKTestMD5("88eb95a6c826ddafd6e3e33f660ec99d")
+    }
+
+
+    func testSampleRate() {
+        let input = AKOscillator()
+        output = AKBitCrusher(input, sampleRate: 2400)
+        input.start()
+        AKTestMD5("84fcd2b2368aa607c644c9131f568285")
+    }
+
 }
