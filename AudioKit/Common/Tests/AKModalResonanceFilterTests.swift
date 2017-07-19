8ef97e96f2a0f2bb44b2cc7bc908cd85fb39596d
Aurelius Prochazka <aure@aure.com>
2017-07-19 00:31:43 -0700

    More test coverage

--- AudioKit/Common/Tests/AKModalResonanceFilterTests.swift
+++ AudioKit/Common/Tests/AKModalResonanceFilterTests.swift
@@ -17,4 +17,25 @@ class AKModalResonanceFilterTests: AKTestCase {
         input.start()
         AKTestMD5("9cd15a61ff271c2717bfb42a66e45c00")
     }
+
+    func testParameters() {
+        let input = AKOscillator()
+        output = AKModalResonanceFilter(input, frequency: 400, qualityFactor: 66)
+        input.start()
+        AKTestMD5("fbbc4d5d65034860afccece4d69e8e9f")
+    }
+
+    func testFrequency() {
+        let input = AKOscillator()
+        output = AKModalResonanceFilter(input, frequency: 400)
+        input.start()
+        AKTestMD5("c82b8176ce2048e3f409140a41ab5bea")
+    }
+
+    func testQualityFactor() {
+        let input = AKOscillator()
+        output = AKModalResonanceFilter(input, qualityFactor: 66)
+        input.start()
+        AKTestMD5("ccf890b181da61f58b545c80d57d8002")
+    }
 }
--- AudioKit/Common/Tests/AKRolandTB303FilterTests.swift
+++ AudioKit/Common/Tests/AKRolandTB303FilterTests.swift
@@ -17,4 +17,43 @@ class AKRolandTB303FilterTests: AKTestCase {
         input.start()
         AKTestMD5("0f8345a5be46169f37d5650dc01f8ffa")
     }
+
+    func testParameters() {
+        let input = AKOscillator()
+        output = AKRolandTB303Filter(input,
+                                     cutoffFrequency: 400,
+                                     resonance: 1,
+                                     distortion: 1,
+                                     resonanceAsymmetry: 0.66)
+        input.start()
+        AKTestMD5("06cf058d7132f973361ec9c22bab11ce")
+    }
+
+    func testCutoffFrequency() {
+        let input = AKOscillator()
+        output = AKRolandTB303Filter(input, cutoffFrequency: 400)
+        input.start()
+        AKTestMD5("a67a0ae2773ed3126f59c911342860be")
+    }
+
+    func testResonance() {
+        let input = AKOscillator()
+        output = AKRolandTB303Filter(input, resonance: 1)
+        input.start()
+        AKTestMD5("c456837d4c82e7f0d43aeb0ada75a86d")
+    }
+
+    func testDistortion() {
+        let input = AKOscillator()
+        output = AKRolandTB303Filter(input, distortion: 1)
+        input.start()
+        AKTestMD5("efb5123eaa7b760c5e220fefd0ae45a2")
+    }
+
+    func testResonanceAsymmetry() {
+        let input = AKOscillator()
+        output = AKRolandTB303Filter(input, resonanceAsymmetry: 0.66)
+        input.start()
+        AKTestMD5("5fd76c6ec136770c7402119a3eef8ebf")
+    }
 }
