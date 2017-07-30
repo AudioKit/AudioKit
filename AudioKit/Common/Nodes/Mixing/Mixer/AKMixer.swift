4d5d41ec95dce4848b31db6cae9319919bf0d1fc
Ryan Francesconi <rf@rf-mbp.local>
2017-07-30 11:31:05 -0700

    There was a bug in AKMixer which was incorrectly specifying which bus to use for incoming inputs. I think the comments in the updated code are clear, but essentially before it was always connecting on mixerAU.numberOfInputs. The problem I found is that after around 40ish connections the audio going through this mixer would start to break up in a very unsettling way. Since the AVAudioMixerNode starts its life with 8 inputs, the starting point should be mixerAU.nextAvailableInputBus, which starts with 0. I've also changed the AKMixer to allow specifying which bus exactly to use. Kind of a power user feature, but it's something that I need.

--- AudioKit/Common/Nodes/Mixing/Mixer/AKMixer.swift
+++ AudioKit/Common/Nodes/Mixing/Mixer/AKMixer.swift
@@ -8,13 +8,17 @@
 
 /// AudioKit version of Apple's Mixer Node
 open class AKMixer: AKNode, AKToggleable {
-    fileprivate let mixerAU = AVAudioMixerNode()
+    /// The internal mixer node
+    fileprivate var mixerAU: AVAudioMixerNode?
 
+    /// How many inputs have been connected to this mixer in its lifespan
+    private var connectionCounter: Int = 0
+    
     /// Output Volume (Default 1)
     open dynamic var volume: Double = 1.0 {
         didSet {
             volume = max(volume, 0)
-            mixerAU.outputVolume = Float(volume)
+            mixerAU?.outputVolume = Float(volume)
         }
     }
 
@@ -26,23 +30,18 @@ open class AKMixer: AKNode, AKToggleable {
     }
 
     /// Initialize the mixer node with no inputs, to be connected later
-    ///
-    /// - parameter inputs: A varaiadic list of AKNodes
-    ///
     public override init() {
         super.init()
-        self.avAudioNode = mixerAU
-        AudioKit.engine.attach(self.avAudioNode)
+        initialize()
     }
 
     /// Initialize the mixer node with multiple inputs
     ///
-    /// - parameter inputs: A varaiadic list of AKNodes
+    /// - parameter inputs: A variadic list of AKNodes
     ///
     public init(_ inputs: AKNode?...) {
         super.init()
-        self.avAudioNode = mixerAU
-        AudioKit.engine.attach(self.avAudioNode)
+        initialize()
         for input in inputs {
             connect(input)
         }
@@ -54,33 +53,63 @@ open class AKMixer: AKNode, AKToggleable {
     ///
     public init(_ inputs: [AKNode]) {
         super.init()
-        self.avAudioNode = mixerAU
-        AudioKit.engine.attach(self.avAudioNode)
+        initialize()
         for input in inputs {
             connect(input)
         }
+    }
+    
+    private func initialize() {
+        mixerAU = AVAudioMixerNode()
+        self.avAudioNode = mixerAU!
+        AudioKit.engine.attach(self.avAudioNode)
     }
 
     /// Connnect another input after initialization
     ///
     /// - parameter input: AKNode to connect
-    ///
     open func connect(_ input: AKNode?) {
+        connect(input, bus: -1)
+    }
+
+    /// Connnect another input after initialization
+    ///
+    /// - parameter input: AKNode to connect
+    /// - parameter bus: what channel of the mixer to connect on. 
+    /// If you use this it is up to your application to keep track of what inputs are in use to make sure you
+    /// don't overwrite an existing channel with an active node you're still using.
+    open func connect(_ input: AKNode?, bus: Int = -1) {
+        guard mixerAU != nil else { return }
+        
         var wasRunning = false
         if AudioKit.engine.isRunning {
             wasRunning = true
             AudioKit.stop()
         }
+        
+        var chan = bus
+
         if let existingInput = input {
-            existingInput.connectionPoints.append(AVAudioConnectionPoint(node: mixerAU, bus: mixerAU.numberOfInputs))
+            // If the channel index isn't passed in, so let the mixer choose the next input bus. This seems to be an ever-increasing counter.
+            if chan == -1 {
+                chan = mixerAU!.nextAvailableInputBus
+            }
+            
+            existingInput.connectionPoints.append(AVAudioConnectionPoint(node: mixerAU!, bus: chan))
             AudioKit.engine.connect(existingInput.avAudioNode,
                                     to: existingInput.connectionPoints,
                                     fromBus: 0,
                                     format: AudioKit.format)
+            
+            connectionCounter += 1
+            
+            AKLog("AKMixer.connect() input: \(existingInput) on bus \(chan), Mixer now has \(mixerAU!.numberOfInputs) total inputs and \(connectionCounter) recent connections.")
+
         }
         if wasRunning {
             AudioKit.start()
         }
+        
     }
 
     /// Function to start, play, or activate the node, all do the same thing
@@ -97,4 +126,5 @@ open class AKMixer: AKNode, AKToggleable {
             volume = 0
         }
     }
+
 }
