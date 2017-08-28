c03ad66346d5607b1c4f7c5c463b7ec62a0ee402
Jeff Cooper <jeff@southernworkstx.com>
2017-08-28 17:02:12 -0500

    Log on failure to create strong reference to self in audiounits

--- AudioKit/Common/Nodes/Generators/Physical Models/Vocal Tract/AKVocalTract.swift
+++ AudioKit/Common/Nodes/Generators/Physical Models/Vocal Tract/AKVocalTract.swift
@@ -158,7 +158,10 @@ public class AKVocalTract: AKNode, AKToggleable, AKComponent {
 
         token = tree.token(byAddingParameterObserver: { [weak self] _, _ in
 
-            guard let _ = self else { return } // Replace _ with strongSelf if needed
+            guard let _ = self else {
+                AKLog("Unable to create strong reference to self")
+                return
+            } // Replace _ with strongSelf if needed
             DispatchQueue.main.async {
                 // This node does not change its own values so we won't add any
                 // value observing, but if you need to, this is where that goes.
