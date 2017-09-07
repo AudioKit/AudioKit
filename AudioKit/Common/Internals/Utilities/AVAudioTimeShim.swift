2c2e0d3fbe42ba32625ca1e4c4af73f52512d8e1
dave234 <daveoneill234@gmail.com>
2017-09-07 15:47:56 -0700

    Swiftlint fixes

--- AudioKit/Common/Internals/Utilities/AVAudioTimeShim.swift
+++ AudioKit/Common/Internals/Utilities/AVAudioTimeShim.swift
@@ -13,21 +13,22 @@ import AVFoundation
 private let ticksToSeconds: Double = {
     var tinfo = mach_timebase_info()
     let err = mach_timebase_info(&tinfo)
-    let timecon = Double(tinfo.numer) / Double(tinfo.denom);
+    let timecon = Double(tinfo.numer) / Double(tinfo.denom)
     return timecon * 0.000_000_001
 }()
 /// Utility to convert between seconds to host time.
 private let secondsToTicks: Double = {
     var tinfo = mach_timebase_info()
     let err = mach_timebase_info(&tinfo)
-    let timecon = Double(tinfo.denom) / Double(tinfo.numer);
+    let timecon = Double(tinfo.denom) / Double(tinfo.numer)
     return timecon * 1_000_000_000
 }()
 
-extension AVAudioTime{
+extension AVAudioTime {
 
-    /// AVAudioTime.extrapolateTime fails for host time valid times, use this instead. https://bugreport.apple.com/web/?problemID=34249528
-    func extrapolateTimeShimmed(fromAnchor anchorTime: AVAudioTime) -> AVAudioTime{
+    /// AVAudioTime.extrapolateTime fails for host time valid times, use
+    /// extrapolateTimeShimmed instead. https://bugreport.apple.com/web/?problemID=34249528
+    func extrapolateTimeShimmed(fromAnchor anchorTime: AVAudioTime) -> AVAudioTime {
         guard ((isSampleTimeValid && sampleRate == anchorTime.sampleRate) || isHostTimeValid) &&
             !(isSampleTimeValid && isHostTimeValid) &&
             anchorTime.isSampleTimeValid && anchorTime.isHostTimeValid else {
@@ -35,17 +36,16 @@ extension AVAudioTime{
         }
         if isSampleTimeValid && sampleRate == anchorTime.sampleRate {
             let secondsDiff = Double(sampleTime - anchorTime.sampleTime) / anchorTime.sampleRate
-            let hostTime = secondsDiff >= 0 ? anchorTime.hostTime + UInt64(secondsDiff / ticksToSeconds) : anchorTime.hostTime - UInt64(-secondsDiff / ticksToSeconds)
-            return AVAudioTime.init(hostTime: hostTime, sampleTime: sampleTime, atRate: anchorTime.sampleRate)
-        }
-        else{
+            let hostTime = anchorTime.hostTime + secondsDiff / ticksToSeconds
+            return AVAudioTime(hostTime: hostTime, sampleTime: sampleTime, atRate: anchorTime.sampleRate)
+        } else {
             let secondsDiff = Double(hostTime.safeSubtract(anchorTime.hostTime)) * ticksToSeconds
             let sampleTime = anchorTime.sampleTime + AVAudioFramePosition(round(secondsDiff * anchorTime.sampleRate))
-            let audioTime = AVAudioTime.init(hostTime: hostTime, sampleTime: sampleTime, atRate: anchorTime.sampleRate)
+            let audioTime = AVAudioTime(hostTime: hostTime, sampleTime: sampleTime, atRate: anchorTime.sampleRate)
             return audioTime
         }
     }
-    
+
     /// An AVAudioTime with a valid hostTime representing now.
     static func now() -> AVAudioTime {
         return AVAudioTime(hostTime: mach_absolute_time())
@@ -58,12 +58,10 @@ extension AVAudioTime{
             return AVAudioTime(hostTime: hostTime + seconds / ticksToSeconds,
                                sampleTime: sampleTime + AVAudioFramePosition(seconds * sampleRate),
                                atRate: sampleRate)
-        }
-        else if isSampleTimeValid {
+        } else if isSampleTimeValid {
             return AVAudioTime(sampleTime: sampleTime + AVAudioFramePosition(seconds * sampleRate),
                                atRate: sampleRate)
-        }
-        else if isHostTimeValid {
+        } else if isHostTimeValid {
             return AVAudioTime(hostTime: hostTime + seconds / ticksToSeconds)
         }
         return nil
@@ -90,14 +88,11 @@ extension AVAudioTime{
 
 }
 
-fileprivate extension UInt64{
+fileprivate extension UInt64 {
     func safeSubtract(_ other: UInt64) -> Int64 {
         return self > other ? Int64(self - other) : -Int64(other - self)
     }
-    static func +(left: UInt64, right: Double) -> UInt64 {
+    static func + (left: UInt64, right: Double) -> UInt64 {
         return right >= 0 ? left + UInt64(right) : left - UInt64(-right)
     }
 }
-
-
-
