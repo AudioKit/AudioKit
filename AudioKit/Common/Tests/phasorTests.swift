b588d7f5e1388bc2a067dc600652fd4556ec06cc
Aurelius Prochazka <aure@aure.com>
2017-09-11 12:29:13 -0700

    Restoring tests that work again

--- AudioKit/Common/Tests/phasorTests.swift
+++ AudioKit/Common/Tests/phasorTests.swift
@@ -1,27 +1,27 @@
-////
-////  phasorTests.swift
-////  AudioKit
-////
-////  Created by Aurelius Prochazka on 8/9/16.
-////  Copyright © 2017 Aurelius Prochazka. All rights reserved.
-////
 //
-//import AudioKit
-//import XCTest
+//  phasorTests.swift
+//  AudioKit
 //
-//class PhasorTests: AKTestCase {
+//  Created by Aurelius Prochazka on 8/9/16.
+//  Copyright © 2017 Aurelius Prochazka. All rights reserved.
 //
-//    override func setUp() {
-//        super.setUp()
-//        duration = 1.0
-//    }
-//
-//    func testDefault() {
-//        output = AKOperationGenerator { _ in
-//            return AKOperation.phasor()
-//        }
-//        AKTestMD5("3158517a6a14167e736cf7038a828dc8")
-//    }
-//
-//}
+
+import AudioKit
+import XCTest
+
+class PhasorTests: AKTestCase {
+
+    override func setUp() {
+        super.setUp()
+        duration = 1.0
+    }
+
+    func testDefault() {
+        output = AKOperationGenerator { _ in
+            return AKOperation.phasor()
+        }
+        AKTestMD5("3158517a6a14167e736cf7038a828dc8")
+    }
+
+}
 
