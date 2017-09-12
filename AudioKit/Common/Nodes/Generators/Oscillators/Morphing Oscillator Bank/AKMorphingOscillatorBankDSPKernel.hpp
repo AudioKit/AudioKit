587ae8ddd10513cfc743531392611d385ab9cefe
Aurelius Prochazka <aure@aure.com>
2017-09-11 20:33:29 -0700

    Fixed oscillator banks and now we should be back to all tests passing

--- AudioKit/Common/Nodes/Generators/Oscillators/Morphing Oscillator Bank/AKMorphingOscillatorBankDSPKernel.hpp
+++ AudioKit/Common/Nodes/Generators/Oscillators/Morphing Oscillator Bank/AKMorphingOscillatorBankDSPKernel.hpp
@@ -109,7 +109,10 @@ public:
             
             for (int frameIndex = 0; frameIndex < frameCount; ++frameIndex) {
                 float x = 0;
-                osc->freq = bentFrequency * powf(2, kernel->vibratoValues[frameIndex]);
+                float depth = kernel->vibratoDepth / 12.0;
+                float variation = sinf((kernel->currentRunningIndex + frameIndex) * 2 * 2 * M_PI * kernel->vibratoRate / kernel->sampleRate);
+                osc->freq = bentFrequency * powf(2, depth * variation);
+
                 sp_adsr_compute(kernel->sp, adsr, &internalGate, &amp);
                 sp_oscmorph_compute(kernel->sp, osc, nil, &x);
                 *outL++ += amp * x;
@@ -193,16 +196,6 @@ public:
         
         index = double(indexRamper.getAndStep());
         standardBankGetAndSteps()
-        
-        for (AUAudioFrameCount i = 0; i < frameCount; ++i) {
-            outL[i] = 0.0f;
-            outR[i] = 0.0f;
-            if (vibratoDepth != 0 && vibratoRate != 0 && &(vibrato->tbl) != nullptr) {
-                sp_osc_compute(sp, vibrato, nil, &vibratoValues[i]);
-            } else {
-                vibratoValues[i] = 0;
-            }
-        }
         
         NoteState* noteState = playingNotes;
         while (noteState) {
--- AudioKit/Common/Tests/AKMorphingOscillatorBankTests.swift
+++ AudioKit/Common/Tests/AKMorphingOscillatorBankTests.swift
@@ -1,91 +1,91 @@
-////
-////  AKMorphingOscillatorBankTests.swift
-////  AudioKitTestSuite
-////
-////  Created by Aurelius Prochazka on 7/21/17.
-////  Copyright © 2017 AudioKit. All rights reserved.
-////
 //
-//import AudioKit
-//import XCTest
+//  AKMorphingOscillatorBankTests.swift
+//  AudioKitTestSuite
 //
-//class AKMorphingOscillatorBankTests: AKTestCase {
+//  Created by Aurelius Prochazka on 7/21/17.
+//  Copyright © 2017 AudioKit. All rights reserved.
 //
-//    var inputBank: AKMorphingOscillatorBank!
-//    let waveforms = [AKTable(.sawtooth), AKTable(.sine), AKTable(.square), AKTable(.triangle)]
-//
-//    override func setUp() {
-//        super.setUp()
-//        // Need to have a longer test duration to allow for envelope to progress
-//        duration = 1.0
-//
-//        afterStart = {
-//            self.inputBank.play(noteNumber: 60, velocity: 120)
-//            self.inputBank.play(noteNumber: 64, velocity: 110)
-//            self.inputBank.play(noteNumber: 67, velocity: 100)
-//        }
-//    }
-//
-//    func testAttackDuration() {
-//        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms, attackDuration: 0.123)
-//        output = inputBank
-//        AKTestMD5("c3dda6103bb693aef41ec27806e3d4c3")
-//    }
-//
-//    func testDecayDuration() {
-//        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms, decayDuration: 0.234)
-//        output = inputBank
-//        AKTestMD5("05af83eb0e1204a9778bdb77d90dc537")
-//    }
-//
-//    func testDefault() {
-//        inputBank = AKMorphingOscillatorBank()
-//        output = inputBank
-//        AKTestMD5("17345f26560ce643990c5a269ab74a43")
-//    }
-//
-//    func testIndex() {
-//        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms, index: 1.7)
-//        output = inputBank
-//        AKTestMD5("460123ae0622996dd8f04441dff25e8b")
-//    }
-//
-//    func testParameters() {
-//        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms,
-//                                             index: 1.7,
-//                                             attackDuration: 0.123,
-//                                             decayDuration: 0.234,
-//                                             sustainLevel: 0.345,
-//                                             pitchBend: 1,
-//                                             vibratoDepth: 1.1,
-//                                             vibratoRate: 1.2)
-//        output = inputBank
-//        AKTestMD5("cf78944a8431b5ee0440fa7b1b124493")
-//    }
-//
-//    func testPitchBend() {
-//        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms, pitchBend: 1.1)
-//        output = inputBank
-//        AKTestMD5("effb427b8635ec021ca792d7ff2a0832")
-//    }
-//
-//    func testSustainLevel() {
-//        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms, sustainLevel: 0.345)
-//        output = inputBank
-//        AKTestMD5("37a2084b030c5b29a3553a00c30aa61a")
-//    }
-//
-//    func testVibrato() {
-//        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms, vibratoDepth: 1, vibratoRate: 10)
-//        output = inputBank
-//        AKTestMD5("4be5ef808007fcdcc73f74dfb7603216")
-//    }
-//
-//    func testWaveformArray() {
-//        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms)
-//        output = inputBank
-//        AKTestMD5("460123ae0622996dd8f04441dff25e8b")
-//    }
-//
-//}
+
+import AudioKit
+import XCTest
+
+class AKMorphingOscillatorBankTests: AKTestCase {
+
+    var inputBank: AKMorphingOscillatorBank!
+    let waveforms = [AKTable(.sawtooth), AKTable(.sine), AKTable(.square), AKTable(.triangle)]
+
+    override func setUp() {
+        super.setUp()
+        // Need to have a longer test duration to allow for envelope to progress
+        duration = 1.0
+
+        afterStart = {
+            self.inputBank.play(noteNumber: 60, velocity: 120)
+            self.inputBank.play(noteNumber: 64, velocity: 110)
+            self.inputBank.play(noteNumber: 67, velocity: 100)
+        }
+    }
+
+    func testAttackDuration() {
+        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms, attackDuration: 0.123)
+        output = inputBank
+        AKTestMD5("c3dda6103bb693aef41ec27806e3d4c3")
+    }
+
+    func testDecayDuration() {
+        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms, decayDuration: 0.234)
+        output = inputBank
+        AKTestMD5("05af83eb0e1204a9778bdb77d90dc537")
+    }
+
+    func testDefault() {
+        inputBank = AKMorphingOscillatorBank()
+        output = inputBank
+        AKTestMD5("17345f26560ce643990c5a269ab74a43")
+    }
+
+    func testIndex() {
+        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms, index: 1.7)
+        output = inputBank
+        AKTestMD5("460123ae0622996dd8f04441dff25e8b")
+    }
+
+    func testParameters() {
+        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms,
+                                             index: 1.7,
+                                             attackDuration: 0.123,
+                                             decayDuration: 0.234,
+                                             sustainLevel: 0.345,
+                                             pitchBend: 1,
+                                             vibratoDepth: 1.1,
+                                             vibratoRate: 1.2)
+        output = inputBank
+        AKTestMD5("f75cbbe4dbb83c3b224fc79432725f5b")
+    }
+
+    func testPitchBend() {
+        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms, pitchBend: 1.1)
+        output = inputBank
+        AKTestMD5("90a063716a4e32707059b2fda6526010")
+    }
+
+    func testSustainLevel() {
+        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms, sustainLevel: 0.345)
+        output = inputBank
+        AKTestMD5("37a2084b030c5b29a3553a00c30aa61a")
+    }
+
+    func testVibrato() {
+        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms, vibratoDepth: 1, vibratoRate: 10)
+        output = inputBank
+        AKTestMD5("3e9e1f0202f1cc441cea911b472a18b3")
+    }
+
+    func testWaveformArray() {
+        inputBank = AKMorphingOscillatorBank(waveformArray: waveforms)
+        output = inputBank
+        AKTestMD5("460123ae0622996dd8f04441dff25e8b")
+    }
+
+}
 
