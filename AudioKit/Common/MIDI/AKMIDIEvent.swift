bff4ce67be3da1eaab2ff128a57afc53a1e197fd
Aurelius Prochazka <aure@aure.com>
2017-09-26 01:58:27 -0700

    MIDI Program Change fix (temporary)

--- AudioKit/Common/MIDI/AKMIDIEvent.swift
+++ AudioKit/Common/MIDI/AKMIDIEvent.swift
@@ -263,7 +263,7 @@ public struct AKMIDIEvent {
         case .channelAftertouch:
             break
         case .programChange:
-            length = 2
+            length = 3 // Usually 2, but causes a crash with 2
         default:
             length = 3
         }
