855ef930f00a53d53399dd2f221e7ef7fa59550a
“Ryan <hi@spongefork.com>
2017-10-06 09:07:25 -0700

    🖖

--- AudioKit/Common/Internals/Audio File/AKAudioFile+Utilities.swift
+++ AudioKit/Common/Internals/Audio File/AKAudioFile+Utilities.swift
@@ -85,7 +85,7 @@ extension AKAudioFile {
         
         for i in 0 ..< buffer.count {
             // store the absolute value of the sample
-            let absSample = abs (buffer[i])
+            let absSample = abs(buffer[i])
             
             if absSample > peak {
                 peak = absSample
