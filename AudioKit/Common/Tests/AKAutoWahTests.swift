35e78408fd971f30a9f50de53b9e4ba1b8a7cea2
Aurelius Prochazka <aure@aure.com>
2017-11-01 00:19:19 -0700

    Fixed tests

--- AudioKit/Common/Tests/AKAutoWahTests.swift
+++ AudioKit/Common/Tests/AKAutoWahTests.swift
@@ -13,7 +13,7 @@ class AKAutoWahTests: AKTestCase {
 
     func testAmplitude() {
         output = AKAutoWah(input, wah: 0.123, amplitude: 0.789)
-        AKTestMD5("82afa0cb12a180631ace15f11c9c94ef")
+        AKTestMD5("786f65133d587399be35aa789e287815")
     }
 
     func testDefault() {
@@ -23,17 +23,17 @@ class AKAutoWahTests: AKTestCase {
 
     func testMix() {
         output = AKAutoWah(input, wah: 0.123, mix: 0.456)
-        AKTestMD5("5f7efbc76cae8c392fe0b4a7b70ef855")
+        AKTestMD5("564135c992b22da1afc7115d5efd2831")
     }
 
     func testParamters() {
         output = AKAutoWah(input, wah: 0.123, mix: 0.456, amplitude: 0.789)
-        AKTestMD5("489197d654ce3fb79c802e05e51e4558")
+        AKTestMD5("7c9b38d3ff498b7f707644f85b18e60c")
     }
 
     func testWah() {
         output = AKAutoWah(input, wah: 0.123)
-        AKTestMD5("5d2f580b18e37f24b497328dddeb5721")
+        AKTestMD5("221729db45a1088330ea7ffffd466780")
     }
 
 }
