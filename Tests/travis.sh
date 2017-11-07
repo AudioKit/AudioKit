5277919b331c7369e1f521c6e050ece9a4cbfc37
Aurelius Prochazka <aure@aure.com>
2017-11-07 00:33:44 -0800

    Removed XCPretty to see if there is more output

--- Tests/travis.sh
+++ Tests/travis.sh
@@ -92,4 +92,4 @@ echo "Running iOS Unit Tests"
 xcodebuild -scheme AudioKitTestSuite -project AudioKit/iOS/AudioKitTestSuite/AudioKitTestSuite.xcodeproj test -sdk iphonesimulator  -destination 'platform=iOS Simulator,name=iPhone 7,OS=11.1' | xcpretty -c || exit 100
 
 echo "Running macOS Unit Tests"
-xcodebuild -project AudioKit/macOS/AudioKitTestSuite/AudioKitTestSuite.xcodeproj -scheme AudioKitTestSuite test ONLY_ACTIVE_ARCH=YES CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY="" | xcpretty -c || exit 101
+xcodebuild -project AudioKit/macOS/AudioKitTestSuite/AudioKitTestSuite.xcodeproj -scheme AudioKitTestSuite test ONLY_ACTIVE_ARCH=YES CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY="" || exit 101
