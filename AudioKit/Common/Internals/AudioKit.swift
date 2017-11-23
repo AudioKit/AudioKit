ecd31de9754314b3565a47ab4a56381f808ff111
Aurelius Prochazka <aure@aure.com>
2017-11-23 01:49:52 -0800

    Booster2 testing

--- AudioKit/Common/Internals/AudioKit.swift
+++ AudioKit/Common/Internals/AudioKit.swift
@@ -28,7 +28,7 @@ public typealias AKCallback = () -> Void
     // MARK: - Internal audio engine mechanics
 
     /// Reference to the AV Audio Engine
-    @objc open static let engine = AVAudioEngine()
+    @objc open static var engine = AVAudioEngine()
 
     @objc static var shouldBeRunning = false
 
@@ -377,6 +377,7 @@ public typealias AKCallback = () -> Void
                     #endif
 
                     try engine.start()
+                    dump(engine.outputNode.audioUnit)
 
                     // Sends notification after restarting the engine, so it is safe to resume AudioKit functions.
                     if AKSettings.notificationsEnabled {
@@ -561,7 +562,6 @@ extension AudioKit {
 
 }
 
-
 extension AVAudioEngine {
 
     /// Adding connection between nodes with default format
@@ -578,11 +578,11 @@ extension AVAudioEngine {
     @available(iOS 11.0, macOS 10.13, tvOS 11.0, *)
     public func renderToFile(_ audioFile: AVAudioFile, seconds: Double, prerender: (() -> Void)? = nil) throws {
         guard seconds >= 0 else {
-            throw NSError.init(domain: "AVAudioEngine ext", code: 1, userInfo: [NSLocalizedDescriptionKey:"Seconds needs to be a positive value"])
+            throw NSError(domain: "AVAudioEngine ext", code: 1, userInfo: [NSLocalizedDescriptionKey: "Seconds needs to be a positive value"])
         }
         // Engine can't be running when switching to offline render mode.
         if isRunning { stop() }
-        try enableManualRenderingMode(.offline, format: audioFile.processingFormat, maximumFrameCount: 4096)
+        try enableManualRenderingMode(.offline, format: audioFile.processingFormat, maximumFrameCount: 4_096)
 
         // This resets the sampleTime of offline rendering to 0.
         reset()
@@ -590,7 +590,7 @@ extension AVAudioEngine {
         try start()
 
         guard let buffer = AVAudioPCMBuffer(pcmFormat: manualRenderingFormat, frameCapacity: manualRenderingMaximumFrameCount) else {
-            throw NSError.init(domain: "AVAudioEngine ext", code: 1, userInfo: [NSLocalizedDescriptionKey:"Couldn't creat buffer in renderToFile"])
+            throw NSError(domain: "AVAudioEngine ext", code: 1, userInfo: [NSLocalizedDescriptionKey: "Couldn't creat buffer in renderToFile"])
         }
 
         // This is for users to prepare the nodes for playing, i.e player.play()
@@ -608,7 +608,7 @@ extension AVAudioEngine {
                 print("renderToFile cannotDoInCurrentContext")
                 continue
             case .error, .insufficientDataFromInputNode:
-                throw NSError.init(domain: "AVAudioEngine ext", code: 1, userInfo: [NSLocalizedDescriptionKey:"renderToFile render error"])
+                throw NSError(domain: "AVAudioEngine ext", code: 1, userInfo: [NSLocalizedDescriptionKey: "renderToFile render error"])
             }
         }
 
--- AudioKit/Common/Tests/AKBoosterTests.swift
+++ AudioKit/Common/Tests/AKBoosterTests.swift
@@ -16,11 +16,6 @@ class AKBoosterTests: AKTestCase {
         AKTestNoEffect()
     }
 
-    func testDefault2() {
-        output = AKBooster2(input)
-        AKTestNoEffect()
-    }
-
     func testParameters() {
         output = AKBooster(input, gain: 2.0)
         AKTestMD5("09fdb24adb3181f6985eba4b408d8c6d")
@@ -30,15 +25,4 @@ class AKBoosterTests: AKTestCase {
         output = AKBooster(input, gain: 0.5)
         AKTestMD5("79972090508032a146d806185f9bc871")
     }
-
-//    func testParameters3() {
-//        output = AKBooster2(input, gain: 2.0)
-//        AKTestMD5("845f682ff41d7017cd37942f3ccb21c2")
-//    }
-//
-//    func testParameters4() {
-//        output = AKBooster2(input, gain: 0.5)
-//        AKTestMD5("5b0bf534309552ac6bc55ae7e831f7cc")
-//    }
-
 }
