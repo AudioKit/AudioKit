8fe9b83e20b64623063f5ce6ca02c8d2406189b3
Aurelius Prochazka <aure@aure.com>
2017-11-29 12:37:08 -0800

    macOS Development Test project added

---
+++ Tests/macOSDevelopment/macOSDevelopment/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  macOSDevelopment
+//
+//  Created by Aurelius Prochazka on 11/29/17.
+//  Copyright © 2017 AudioKit. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
