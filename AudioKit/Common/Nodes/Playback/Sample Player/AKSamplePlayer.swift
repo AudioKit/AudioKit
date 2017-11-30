4aba3e7e99db7122fae8964991398f0bac49b191
Jeff Cooper <jeff@southernworkstx.com>
2017-11-30 16:47:00 -0600

    converts mono to stereo for aksampleplayer loading

--- AudioKit/Common/Nodes/Playback/Sample Player/AKSamplePlayer.swift
+++ AudioKit/Common/Nodes/Playback/Sample Player/AKSamplePlayer.swift
@@ -240,7 +240,7 @@ open class AKSamplePlayer: AKNode, AKComponent {
         if maximumSamples != 0 {
             internalAU?.setupAudioFileTable(UInt32(maximumSamples) * 2)
         }
-        loadSound(file: file)
+        load(file: file)
     }
 
     // MARK: - Control
@@ -286,19 +286,21 @@ open class AKSamplePlayer: AKNode, AKComponent {
     }
 
     /// Load a new audio file into memory
-    open func loadSound(file: AKAudioFile) {
-        //load(file: file)
-        print("loading akaudiofile")
+    func load(file: AKAudioFile) {
+        if file.channelCount > 2 {
+            AKLog("AKSamplePlayer currently only supports mono or stereo samples")
+        }
         let sizeToUse = UInt32(file.samplesCount * 2)
         if maximumSamples == 0 {
-            maximumSamples = Int(sizeToUse)
+            maximumSamples = Int(file.samplesCount)
             internalAU?.setupAudioFileTable(sizeToUse)
         }
-        let flatted = Array(file.floatChannelData!.joined())
-        
-        let ptr = UnsafeMutablePointer<Float>.allocate(capacity: Int(sizeToUse))
-        let data = UnsafeMutablePointer<Float>(mutating: flatted)
-        internalAU?.loadAudioData(data, size: UInt32(flatted.count))
+        var flattened = Array(file.floatChannelData!.joined())
+        if file.channelCount == 1 { //if mono
+            flattened.append(contentsOf: file.floatChannelData![0])
+        }
+        let data = UnsafeMutablePointer<Float>(mutating: flattened)
+        internalAU?.loadAudioData(data, size: UInt32(flattened.count))
         
         self.avAudiofile = file
         self.startPoint = 0
@@ -306,103 +308,6 @@ open class AKSamplePlayer: AKNode, AKComponent {
         self.loopStartPoint = 0
         self.loopEndPoint = Sample(file.samplesCount)
     }
+    //todo open func loadSound()
     
-    func load(file: AVAudioFile) {
-        Exit: do {
-            var err: OSStatus = noErr
-            var theFileLengthInFrames: Int64 = 0
-            var theFileFormat: AudioStreamBasicDescription = AudioStreamBasicDescription()
-            var thePropertySize: UInt32 = UInt32(MemoryLayout.stride(ofValue: theFileFormat))
-            var extRef: ExtAudioFileRef?
-            var theData: UnsafeMutablePointer<CChar>?
-            var theOutputFormat: AudioStreamBasicDescription = AudioStreamBasicDescription()
-            err = ExtAudioFileOpenURL(file.url as CFURL, &extRef)
-            if err != 0 { AKLog("ExtAudioFileOpenURL FAILED, Error = \(err)"); break Exit }
-            // Get the audio data format
-            guard let externalAudioFileRef = extRef else {
-                break Exit
-            }
-            err = ExtAudioFileGetProperty(externalAudioFileRef,
-                                          kExtAudioFileProperty_FileDataFormat,
-                                          &thePropertySize,
-                                          &theFileFormat)
-            if err != 0 {
-                AKLog("ExtAudioFileGetProperty(kExtAudioFileProperty_FileDataFormat) FAILED, Error = \(err)")
-                break Exit
-            }
-            if theFileFormat.mChannelsPerFrame > 2 {
-                AKLog("Unsupported Format, channel count is greater than stereo")
-                break Exit
-            }
-
-            theOutputFormat.mFormatID = kAudioFormatLinearPCM
-            theOutputFormat.mFormatFlags = kLinearPCMFormatFlagIsFloat
-            theOutputFormat.mBitsPerChannel = UInt32(MemoryLayout<Float>.stride) * 8
-            theOutputFormat.mChannelsPerFrame = 2
-            theOutputFormat.mBytesPerFrame = theOutputFormat.mChannelsPerFrame * UInt32(MemoryLayout<Float>.stride)
-            theOutputFormat.mFramesPerPacket = 1
-            theOutputFormat.mBytesPerPacket = theOutputFormat.mFramesPerPacket * theOutputFormat.mBytesPerFrame
-
-            // Set the desired client (output) data format
-            err = ExtAudioFileSetProperty(externalAudioFileRef,
-                                          kExtAudioFileProperty_ClientDataFormat,
-                                          UInt32(MemoryLayout.stride(ofValue: theOutputFormat)),
-                                          &theOutputFormat)
-            if err != 0 {
-                AKLog("ExtAudioFileSetProperty(kExtAudioFileProperty_ClientDataFormat) FAILED, Error = \(err)")
-                break Exit
-            }
-
-            // Get the total frame count
-            thePropertySize = UInt32(MemoryLayout.stride(ofValue: theFileLengthInFrames))
-            err = ExtAudioFileGetProperty(externalAudioFileRef,
-                                          kExtAudioFileProperty_FileLengthFrames,
-                                          &thePropertySize,
-                                          &theFileLengthInFrames)
-            if err != 0 {
-                AKLog("ExtAudioFileGetProperty(kExtAudioFileProperty_FileLengthFrames) FAILED, Error = \(err)")
-                break Exit
-            }
-            
-            // Read all the data into memory
-            let dataSize = UInt32(theFileLengthInFrames) * theOutputFormat.mBytesPerFrame
-            theData = UnsafeMutablePointer.allocate(capacity: Int(dataSize))
-            if theData != nil {
-                var bufferList: AudioBufferList = AudioBufferList()
-                bufferList.mNumberBuffers = 1
-                bufferList.mBuffers.mDataByteSize = dataSize
-                bufferList.mBuffers.mNumberChannels = theOutputFormat.mChannelsPerFrame
-                bufferList.mBuffers.mData = UnsafeMutableRawPointer(theData)
-
-                // Read the data into an AudioBufferList
-                var ioNumberFrames: UInt32 = UInt32(theFileLengthInFrames)
-                err = ExtAudioFileRead(externalAudioFileRef, &ioNumberFrames, &bufferList)
-                if err == noErr {
-                    // success
-                    let data = UnsafeMutablePointer<Float>(
-                        bufferList.mBuffers.mData?.assumingMemoryBound(to: Float.self)
-                    )
-
-                    if maximumSamples == 0 {
-                        maximumSamples = Int(ioNumberFrames * 2)
-                        internalAU?.setupAudioFileTable(UInt32(maximumSamples) * 2)
-                    }
-                    internalAU?.loadAudioData(data, size: ioNumberFrames * 2)
-
-                    self.avAudiofile = file
-                    self.startPoint = 0
-                    self.endPoint = Sample(file.samplesCount)
-                    self.loopStartPoint = 0
-                    self.loopEndPoint = Sample(file.samplesCount)
-                } else {
-                    // failure
-                    AKLog("Error = \(err)"); break Exit
-                }
-            }
-            theData?.deallocate(capacity: Int(dataSize))
-            theData = nil // make sure to return NULL
-            ExtAudioFileDispose(externalAudioFileRef)
-            ExtAudioFileDispose(extRef!)
-        }
-    }
 }
