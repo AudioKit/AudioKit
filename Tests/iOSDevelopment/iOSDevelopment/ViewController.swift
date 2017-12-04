61e2a46b98cc31984046017f7123c1740afe03e1
Aurelius Prochazka <aure@aure.com>
2017-12-03 17:48:31 -0800

    Started iOSDevelopment sandbox project

---
+++ Tests/iOSDevelopment/iOSDevelopment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSDevelopment
+//
+//  Created by Aurelius Prochazka on 12/3/17.
+//  Copyright © 2017 Aurelius Prochazka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
