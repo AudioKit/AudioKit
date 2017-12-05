022cde159d63374aa66b564a861d46374a41e642
Aurelius Prochazka <aure@aure.com>
2017-12-05 02:14:31 -0800

    Swiftlinting and ramp times in seconds

--- Tests/iOSDevelopment/iOSDevelopment/AppDelegate.swift
+++ Tests/iOSDevelopment/iOSDevelopment/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
     }
@@ -33,6 +32,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
     }
 
-
 }
-
