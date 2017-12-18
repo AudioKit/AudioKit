15aa00305d5bd7c0c421dae1b945693aa3d9a70b
Jeff Cooper <jeff@southernworkstx.com>
2017-12-18 17:29:21 -0600

    first round of aksampleplayer tests

--- Tests/TestCases/AKSamplePlayerTests.swift
+++ Tests/TestCases/AKSamplePlayerTests.swift
@@ -1,5 +1,5 @@
 //
-//  AKTanhDistortionTests.swift
+//  AKSamplePlayerTests.swift
 //  AudioKitTestSuite
 //
 //  Created by Jeff Cooper and Aurelius Prochazka on 18 Dec 2017.
@@ -14,11 +14,72 @@ class AKSamplePlayerTests: AKTestCase {
     var sampler: AKSamplePlayer?
     
     func testDefault() {
-        setupSampler()
         afterStart = {
-            
+            self.sampler?.play()
         }
-        AKTestMD5("882c7029a5097769b85bd176f5752684")
+        AKTestMD5("72ff03c8f6b529625877f89f4c7325bf")
+    }
+    
+    func testReversePlayback(){
+        sampler?.startPoint = sampler!.size
+        sampler?.endPoint = 0
+        afterStart = {
+            self.sampler?.play()
+        }
+        AKTestMD5("4ff8bf4506289ce8b4f5f26f6d3c77ac")
+    }
+    
+    func testStartOffset(){
+        afterStart = {
+            self.sampler?.play(from: 2000)
+        }
+        AKTestMD5("d06ca74db9d9e5ca6892a5c6b32b978c")
+    }
+    
+    func testStartOffsetUsingPoints(){
+        sampler?.startPoint = 2000
+        afterStart = {
+            self.sampler?.play()
+        }
+        AKTestMD5("d06ca74db9d9e5ca6892a5c6b32b978c")
+    }
+    
+    func testEndOffset(){
+        afterStart = {
+            self.sampler?.play(from: 0, to: 300)
+        }
+        AKTestMD5("4fead31c7eb9c03698e8b94e286aa7ac")
+    }
+    
+    func testEndOffsetUsingPoints(){
+        sampler?.startPoint = 0
+        sampler?.endPoint = 300
+        afterStart = {
+            self.sampler?.play()
+        }
+        AKTestMD5("4fead31c7eb9c03698e8b94e286aa7ac")
+    }
+    
+    func testForwardLoop(){
+        sampler?.loopStartPoint = 100
+        sampler?.loopEndPoint = 300
+        sampler?.endPoint = 300
+        sampler?.loopEnabled = true
+        afterStart = {
+            self.sampler?.play()
+        }
+        AKTestMD5("664e78be6d2d3a3daef538f981e20ecd")
+    }
+    
+    func testReverseLoop(){
+        sampler?.loopStartPoint = 300
+        sampler?.loopEndPoint = 100
+        sampler?.endPoint = 300
+        sampler?.loopEnabled = true
+        afterStart = {
+            self.sampler?.play()
+        }
+        AKTestMD5("d8f001556a3efed4577101c2249b986c")
     }
     
     func setupSampler(){
@@ -27,6 +88,11 @@ class AKSamplePlayerTests: AKTestCase {
         let file = try! AKAudioFile(forReading: url)
         sampler = AKSamplePlayer(file: file)
         output = sampler
+    }
+    
+    override func setUp() {
+        super.setUp()
+        setupSampler()
     }
     
 }
