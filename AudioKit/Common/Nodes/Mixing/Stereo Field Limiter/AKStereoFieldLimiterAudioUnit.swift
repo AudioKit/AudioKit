21990e2d800a832c9786b72807c736b1301db390
Aurelius Prochazka <aure@aure.com>
2018-01-12 18:29:51 -0800

    Updated stereo field limiter

---
+++ AudioKit/Common/Nodes/Mixing/Stereo Field Limiter/AKStereoFieldLimiterAudioUnit.swift
@@ -0,0 +1,54 @@
+//
+//  AKStereoFieldLimiterAudioUnit.swift
+//  AudioKit
+//
+//  Created by Andrew Voelkel on 9/23/17.
+//  Copyright © 2017 AudioKit. All rights reserved.
+//
+
+import AVFoundation
+
+public class AKStereoFieldLimiterAudioUnit: AKAudioUnitBase {
+
+    func setParameter(_ address: AKStereoFieldLimiterParameter, value: Double) {
+        setParameterWithAddress(AUParameterAddress(address.rawValue), value: Float(value))
+    }
+
+    func setParameterImmediately(_ address: AKStereoFieldLimiterParameter, value: Double) {
+        setParameterImmediatelyWithAddress(AUParameterAddress(address.rawValue), value: Float(value))
+    }
+
+    var amount: Double = 1.0 {
+        didSet { setParameter(.amount, value: amount) }
+    }
+
+    var rampTime: Double = 0.0 {
+        didSet { setParameter(.rampTime, value: rampTime) }
+    }
+
+    public override func initDSP(withSampleRate sampleRate: Double,
+                                 channelCount count: AVAudioChannelCount) -> UnsafeMutableRawPointer! {
+        return createStereoFieldLimiterDSP(Int32(count), sampleRate)
+    }
+
+    override init(componentDescription: AudioComponentDescription,
+                  options: AudioComponentInstantiationOptions = []) throws {
+        try super.init(componentDescription: componentDescription, options: options)
+
+        let flags: AudioUnitParameterOptions = [.flag_IsReadable, .flag_IsWritable, .flag_CanRamp]
+        let amount = AUParameterTree.createParameter(withIdentifier: "amount",
+                                                     name: "Limiting amount",
+                                                     address: AUParameterAddress(0),
+                                                     min: 0.0, max: 1.0,
+                                                     unit: .generic,
+                                                     unitName: nil,
+                                                     flags: flags,
+                                                     valueStrings: nil,
+                                                     dependentParameters: nil)
+        setParameterTree(AUParameterTree.createTree(withChildren: [amount]))
+        amount.value = 1.0
+    }
+
+    public override var canProcessInPlace: Bool { get { return true; }}
+
+}
