d7fb111696976b59fdf1a4ab6d8ec01a3dce40dd
Aurelius Prochazka <aure@aure.com>
2018-01-17 20:29:59 -0800

    Updated white noise generator

---
+++ AudioKit/Common/Nodes/Generators/Noise/White Noise/AKWhiteNoiseAudioUnit.swift
@@ -0,0 +1,59 @@
+//
+//  AKWhiteNoiseAudioUnit.swift
+//  AudioKit
+//
+//  Created by Aurelius Prochazka, revision history on Github.
+//  Copyright © 2018 AudioKit. All rights reserved.
+//
+
+import AVFoundation
+
+public class AKWhiteNoiseAudioUnit: AKGeneratorAudioUnitBase {
+
+    func setParameter(_ address: AKWhiteNoiseParameter, value: Double) {
+        setParameterWithAddress(AUParameterAddress(address.rawValue), value: Float(value))
+    }
+
+    func setParameterImmediately(_ address: AKWhiteNoiseParameter, value: Double) {
+        setParameterImmediatelyWithAddress(AUParameterAddress(address.rawValue), value: Float(value))
+    }
+
+    var amplitude: Double = 1 {
+        didSet { setParameter(.amplitude, value: amplitude) }
+    }
+
+    var rampTime: Double = 0.0 {
+        didSet { setParameter(.rampTime, value: rampTime) }
+    }
+
+    public override func initDSP(withSampleRate sampleRate: Double,
+                                 channelCount count: AVAudioChannelCount) -> UnsafeMutableRawPointer! {
+        return createWhiteNoiseDSP(Int32(count), sampleRate)
+    }
+
+    override init(componentDescription: AudioComponentDescription,
+                  options: AudioComponentInstantiationOptions = []) throws {
+        try super.init(componentDescription: componentDescription, options: options)
+
+        let flags: AudioUnitParameterOptions = [.flag_IsReadable, .flag_IsWritable, .flag_CanRamp]
+
+        let amplitude = AUParameterTree.createParameter(
+            withIdentifier: "amplitude",
+            name: "Amplitude",
+            address: AUParameterAddress(0),
+            min: 0.0,
+            max: 1.0,
+            unit: .generic,
+            unitName: nil,
+            flags: flags,
+            valueStrings: nil,
+            dependentParameters: nil
+        )
+        
+        setParameterTree(AUParameterTree.createTree(withChildren: [amplitude]))
+        amplitude.value = 1
+    }
+
+    public override var canProcessInPlace: Bool { get { return true; }}
+
+}
