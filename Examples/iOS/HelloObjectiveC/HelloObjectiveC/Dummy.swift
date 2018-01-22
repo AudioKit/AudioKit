8d4aaa65355135d497cf9b27b14f55a186cb781f
Stephane Peter <megastep@megastep.org>
2018-01-22 02:01:24 -0800

    Fixed Obj-C example somehow

---
+++ Examples/iOS/HelloObjectiveC/HelloObjectiveC/Dummy.swift
@@ -0,0 +1,12 @@
+//
+//  Dummy.swift
+//  HelloObjectiveC
+//
+//  Created by Stéphane Peter on 1/22/18.
+//  Copyright © 2018 AudioKit. All rights reserved.
+//
+
+import Foundation
+
+// Apparently having an empty Swift file allows this project to compile!
+
