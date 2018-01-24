8b6b11d0ddff2390092f9f7396aba3e670409c0f
Aurelius Prochazka <aure@aure.com>
2018-01-23 23:25:17 -0800

    Added playgrounds for modulation effects
    Chorus, Flanger, and Phaser

--- Playgrounds/AudioKitPlaygrounds/Playgrounds/Effects.playground/Pages/Phaser.xcplaygroundpage/Contents.swift
+++ Playgrounds/AudioKitPlaygrounds/Playgrounds/Effects.playground/Pages/Phaser.xcplaygroundpage/Contents.swift
@@ -1,7 +1,81 @@
-//: [Previous](@previous)
+//: ## Phaser
+import AudioKitPlaygrounds
+import AudioKit
+import AudioKitUI
 
-import Foundation
+let file = try AKAudioFile(readFileName: playgroundAudioFiles[0])
 
-var str = "Hello, playground"
+let player = AKPlayer(audioFile: file)
+player.isLooping = true
 
-//: [Next](@next)
+var phaser = AKPhaser(player)
+
+AudioKit.output = phaser
+try AudioKit.start()
+
+player.play()
+
+class LiveView: AKLiveViewController {
+
+    override func viewDidLoad() {
+        addTitle("Phaser")
+
+        addView(AKResourcesAudioFileLoaderView(player: player, filenames: playgroundAudioFiles))
+
+        addView(AKSlider(property: "Feedback", value: phaser.feedback) { sliderValue in
+                            phaser.feedback
+        })
+
+        addView(AKSlider(property: "Depth", value: phaser.depth) { sliderValue in
+            phaser.depth
+        })
+
+        addView(AKSlider(property: "Notch Minimum",
+                         value: phaser.notchMinimumFrequency,
+                         range: 20 ... 5_000,
+                         format: "%0.1f Hz"
+        ) { sliderValue in
+            phaser.notchMinimumFrequency = sliderValue
+        })
+
+        addView(AKSlider(property: "Notch Maximum",
+                         value: phaser.notchMaximumFrequency,
+                         range: 20 ... 10_000,
+                         format: "%0.1f Hz"
+        ) { sliderValue in
+            phaser.notchMaximumFrequency = sliderValue
+        })
+
+        addView(AKSlider(property: "Notch Width",
+                         value: phaser.notchWidth,
+                         range: 10 ... 5_000,
+                         format: "%0.1f Hz"
+        ) { sliderValue in
+            phaser.notchWidth = sliderValue
+        })
+
+        addView(AKSlider(property: "Notch Frequency",
+                         value: phaser.notchFrequency,
+                         range: 1.1 ... 4,
+                         format: "%0.2f Hz"
+        ) { sliderValue in
+            phaser.notchFrequency = sliderValue
+        })
+
+        addView(AKSlider(property: "LFO BPM",
+                         value: phaser.lfoBPM,
+                         range: 24 ... 360,
+                         format: "%0.2f Hz"
+        ) { sliderValue in
+            phaser.lfoBPM = sliderValue
+        })
+
+
+    }
+}
+
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
+PlaygroundPage.current.liveView = LiveView()
+
+
