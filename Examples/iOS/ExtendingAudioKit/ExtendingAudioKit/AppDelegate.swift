5b52fa5c1b1f95e83c248737a790f6e1afce7450
getdunne <shane@getdunne.com>
2018-01-25 16:58:06 -0500

    Appeasing the Hound

--- Examples/iOS/ExtendingAudioKit/ExtendingAudioKit/AppDelegate.swift
+++ Examples/iOS/ExtendingAudioKit/ExtendingAudioKit/AppDelegate.swift
@@ -10,9 +10,8 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    var window: UIWindow?
 
+    var window: UIWindow?
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
