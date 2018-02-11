378d7cb0dc05d446b339859d0f61f992479b6294
Aurelius Prochazka <aure@aure.com>
2018-02-10 17:38:48 -0800

    Updated AKOscillator's architecture and added new tests

--- AudioKit/Common/Nodes/Generators/Oscillators/Oscillator/AKOscillatorDSP.hpp
+++ AudioKit/Common/Nodes/Generators/Oscillators/Oscillator/AKOscillatorDSP.hpp
@@ -18,8 +18,6 @@ typedef NS_ENUM(AUParameterAddress, AKOscillatorParameter) {
     AKOscillatorParameterRampTime
 };
 
-#import "AKLinearParameterRamp.hpp"  // have to put this here to get it included in umbrella header
-
 #ifndef __cplusplus
 
 void* createOscillatorDSP(int nChannels, double sampleRate);
@@ -29,127 +27,45 @@ void* createOscillatorDSP(int nChannels, double sampleRate);
 #import "AKSoundpipeDSPBase.hpp"
 
 class AKOscillatorDSP : public AKSoundpipeDSPBase {
-
-    sp_osc *_osc;
-    sp_ftbl *_ftbl;
-    UInt32 _ftbl_size = 4096;
-
 private:
-    AKLinearParameterRamp frequencyRamp;
-    AKLinearParameterRamp amplitudeRamp;
-    AKLinearParameterRamp detuningOffsetRamp;
-    AKLinearParameterRamp detuningMultiplierRamp;
-
+    struct _Internal;
+    std::unique_ptr<_Internal> _private;
+ 
 public:
-    AKOscillatorDSP() {
-        frequencyRamp.setTarget(440, true);
-        frequencyRamp.setDurationInSamples(10000);
-        amplitudeRamp.setTarget(1, true);
-        amplitudeRamp.setDurationInSamples(10000);
-        detuningOffsetRamp.setTarget(0, true);
-        detuningOffsetRamp.setDurationInSamples(10000);
-        detuningMultiplierRamp.setTarget(1, true);
-        detuningMultiplierRamp.setDurationInSamples(10000);
-    }
+    AKOscillatorDSP();
+    ~AKOscillatorDSP();
 
-    /** Uses the ParameterAddress as a key */
-    void setParameter(AUParameterAddress address, float value, bool immediate) override {
-        switch (address) {
-            case AKOscillatorParameterFrequency:
-                frequencyRamp.setTarget(value, immediate);
-                break;
-            case AKOscillatorParameterAmplitude:
-                amplitudeRamp.setTarget(value, immediate);
-                break;
-            case AKOscillatorParameterDetuningOffset:
-                detuningOffsetRamp.setTarget(value, immediate);
-                break;
-            case AKOscillatorParameterDetuningMultiplier:
-                detuningMultiplierRamp.setTarget(value, immediate);
-                break;
-            case AKOscillatorParameterRampTime:
-                frequencyRamp.setRampTime(value, _sampleRate);
-                amplitudeRamp.setRampTime(value, _sampleRate);
-                detuningOffsetRamp.setRampTime(value, _sampleRate);
-                detuningMultiplierRamp.setRampTime(value, _sampleRate);
-                break;
-        }
-    }
+    float frequencyLowerBound = 0;
+    float frequencyUpperBound = 20000;
+    float amplitudeLowerBound = 0;
+    float amplitudeUpperBound = 10;
+    float detuningOffsetLowerBound = -1000;
+    float detuningOffsetUpperBound = 1000;
+    float detuningMultiplierLowerBound = 0.9;
+    float detuningMultiplierUpperBound = 1.11;
 
-    /** Uses the ParameterAddress as a key */
-    float getParameter(AUParameterAddress address) override {
-        switch (address) {
-            case AKOscillatorParameterFrequency:
-                return frequencyRamp.getTarget();
-            case AKOscillatorParameterAmplitude:
-                return amplitudeRamp.getTarget();
-            case AKOscillatorParameterDetuningOffset:
-                return detuningOffsetRamp.getTarget();
-            case AKOscillatorParameterDetuningMultiplier:
-                return detuningMultiplierRamp.getTarget();
-            case AKOscillatorParameterRampTime:
-                return frequencyRamp.getRampTime(_sampleRate);
-        }
-        return 0;
-    }
+    float defaultFrequency = 440;
+    float defaultAmplitude = 1;
+    float defaultDetuningOffset = 0;
+    float defaultDetuningMultiplier = 1;
 
-    void init(int _channels, double _sampleRate) override {
-        AKSoundpipeDSPBase::init(_channels, _sampleRate);
-        
-        sp_osc_create(&_osc);
-        sp_osc_init(_sp, _osc, _ftbl, 0);
-        _osc->freq = 440;
-        _osc->amp = 1;
-    }
+    int defaultRampTimeSamples = 10000;
 
-    void destroy() {
-        sp_osc_destroy(&_osc);
-        AKSoundpipeDSPBase::destroy();
-    }
+    // Uses the ParameterAddress as a key
+    void setParameter(AUParameterAddress address, float value, bool immediate) override;
 
-    void setupWaveform(uint32_t size) override {
-        _ftbl_size = size;
-        sp_ftbl_create(_sp, &_ftbl, _ftbl_size);
-    }
+    // Uses the ParameterAddress as a key
+    float getParameter(AUParameterAddress address) override;
+    
+    void init(int _channels, double _sampleRate) override;
 
-    void setWaveformValue(uint32_t index, float value) override {
-        _ftbl->tbl[index] = value;
-    }
+    void destroy();
 
-    void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) override {
+    void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) override;
 
-        for (int frameIndex = 0; frameIndex < frameCount; ++frameIndex) {
-            int frameOffset = int(frameIndex + bufferOffset);
-
-            // do gain ramping every 8 samples
-            if ((frameOffset & 0x7) == 0) {
-                frequencyRamp.advanceTo(_now + frameOffset);
-                amplitudeRamp.advanceTo(_now + frameOffset);
-                detuningOffsetRamp.advanceTo(_now + frameOffset);
-                detuningMultiplierRamp.advanceTo(_now + frameOffset);
-            }
-            float frequency = frequencyRamp.getValue();
-            float amplitude = amplitudeRamp.getValue();
-            float detuningOffset = detuningOffsetRamp.getValue();
-            float detuningMultiplier = detuningMultiplierRamp.getValue();
-            _osc->freq = frequency * detuningMultiplier + detuningOffset;
-            _osc->amp = amplitude;
-
-            float temp = 0;
-            for (int channel = 0; channel < _nChannels; ++channel) {
-                float* out = (float *)_outBufferListPtr->mBuffers[channel].mData + frameOffset;
-
-                if (_playing) {
-                    if (channel == 0) {
-                        sp_osc_compute(_sp, _osc, nil, &temp);
-                    }
-                    *out = temp;
-                } else {
-                    *out = 0.0;
-                }
-            }
-        }
-    }
+    // Generator Stuff
+    void setupWaveform(uint32_t size) override;
+    void setWaveformValue(uint32_t index, float value) override;
 };
 
 #endif
