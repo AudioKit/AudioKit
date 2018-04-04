d3f41d0be457cbc5e99d8d2e2630c39ba4442f12
Aurelius Prochazka <aurephd@gmail.com>
2018-04-03 23:02:48 -0700

    Addressing https://github.com/AudioKit/AudioKit/issues/1222

--- AudioKit/Common/Internals/Microphone Tracker/AKMicrophoneTrackerEngine.h
+++ AudioKit/Common/Internals/Microphone Tracker/AKMicrophoneTrackerEngine.h
@@ -11,6 +11,9 @@
 #import "EZAudio.h"
 
 @interface AKMicrophoneTrackerEngine : NSObject<EZMicrophoneDelegate>
+
+- (instancetype)initWithHopSize:(UInt32)hopSize peakCount:(UInt32)peakCount;
+
 @property float amplitude;
 @property float frequency;
 
