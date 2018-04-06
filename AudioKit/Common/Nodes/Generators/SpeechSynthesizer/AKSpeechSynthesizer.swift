207b20b3b601c8dbcfe8cb993d3b515e5b8228fc
Wangchou Lu <kindalu@gmail.com>
2018-04-06 12:06:40 +0800

    remove whitespaces and change captial cases

--- AudioKit/Common/Nodes/Generators/SpeechSynthesizer/AKSpeechSynthesizer.swift
+++ AudioKit/Common/Nodes/Generators/SpeechSynthesizer/AKSpeechSynthesizer.swift
@@ -10,33 +10,35 @@
 ///
 
 open class AKSpeechSynthesizer: AKNode {
-    public static let ComponentDescription = AudioComponentDescription(componentType: kAudioUnitType_Generator,
-                                                                       componentSubType: kAudioUnitSubType_SpeechSynthesis,
-                                                                       componentManufacturer: kAudioUnitManufacturer_Apple,
-                                                                       componentFlags: 0,
-                                                                       componentFlagsMask: 0)
-    
+    public static let ComponentDescription = AudioComponentDescription(
+      componentType: kAudioUnitType_Generator,
+      componentSubType: kAudioUnitSubType_SpeechSynthesis,
+      componentManufacturer: kAudioUnitManufacturer_Apple,
+      componentFlags: 0,
+      componentFlagsMask: 0
+    )
+
     fileprivate let speechAU = AVAudioUnitGenerator(audioComponentDescription: ComponentDescription)
-    
+
     public func sayHello() {
         var channel: SpeechChannel?
         var propsize: UInt32 = UInt32(MemoryLayout<SpeechChannel>.size)
-        
+
         CheckError(AudioUnitGetProperty(speechAU.audioUnit,
                                         kAudioUnitProperty_SpeechChannel,
                                         kAudioUnitScope_Global,
                                         0,
                                         &channel,
                                         &propsize))
-        
+
         guard let speechChannel = channel else {
             debugPrint("Cannot get Speech Channel")
             exit(1)
         }
-        
+
         SpeakCFString(speechChannel, "Hello World. AK Speech Synthesizer works!" as CFString, nil)
     }
-    
+
     @objc public override init() {
         super.init(avAudioNode: speechAU, attach: true)
     }
