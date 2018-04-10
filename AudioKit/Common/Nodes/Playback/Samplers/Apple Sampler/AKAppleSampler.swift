999dddf3a3dfa22ec88d250e4913084d5f02c533
mahal <mahal@raskinformac.com>
2018-04-10 18:29:31 +0200

    Comment formatted to show the numbered list properly formatted.

--- AudioKit/Common/Nodes/Playback/Samplers/Apple Sampler/AKAppleSampler.swift
+++ AudioKit/Common/Nodes/Playback/Samplers/Apple Sampler/AKAppleSampler.swift
@@ -11,10 +11,10 @@ import CoreAudio
 
 /// Sampler audio generation.
 ///
-/// 1) init the audio unit like this: var sampler = AKAppleSampler()
-/// 2) load a sound a file: sampler.loadWav("path/to/your/sound/file/in/app/bundle") (without wav extension)
-/// 3) connect to the engine: AudioKit.output = sampler
-/// 4) start the engine AudioKit.start()
+/// 1. init the audio unit like this: var sampler = AKAppleSampler()
+/// 2. load a sound a file: sampler.loadWav("path/to/your/sound/file/in/app/bundle") (without wav extension)
+/// 3. connect to the engine: AudioKit.output = sampler
+/// 4. start the engine AudioKit.start()
 ///
 open class AKAppleSampler: AKNode {
 
