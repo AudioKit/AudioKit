d11c331bd64333c8eecfb9ea437eade5a6e1f827
Jeff Cooper <jeff@southernworkstx.com>
2018-04-27 12:13:28 -0500

    initial akstepper rewrite

--- AudioKit/iOS/AudioKit/User Interface/AKStepper.swift
+++ AudioKit/iOS/AudioKit/User Interface/AKStepper.swift
@@ -12,142 +12,87 @@ import UIKit
 /// Incrementor view, normally used for MIDI presets, but could be useful elsehwere
 open class AKStepper: UIView {
 
-    var plusPath = UIBezierPath()
-    var minusPath = UIBezierPath()
-
-    /// Text / label to display
-    open var text = "Value"
-
-    /// Current value
-    open var value: MIDIByte
-
-    /// Function to call on change
-    open var callback: (MIDIByte) -> Void
-
-    /// Handle new touches
-    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            let touchLocation = touch.location(in: self)
-            if minusPath.contains(touchLocation) {
-                if value > 1 {
-                    value -= 1
-                }
-            }
-            if plusPath.contains(touchLocation) {
-                if value < 127 {
-                    value += 1
-                }
-            }
-            self.callback(value)
-            setNeedsDisplay()
-        }
+    @IBInspectable var text: String = "Stepper"
+    var label: UILabel! //fixme
+    var plusButton: AKButton!
+    var minusButton: AKButton!
+    @IBInspectable var value: Double = 1.0
+    @IBInspectable var increment: Double = 1.0
+    @IBInspectable var minimum: Double = 0
+    @IBInspectable var maximum: Double = 0
+    open var callback: (Double)->Void = {val in
+        print("callback: \(val)")
     }
 
+    private var mainStack: UIStackView!
+    
+    private func doPlusAction(){
+        print("up")
+        value += increment
+        callback(value)
+    }
+    private func doMinusAction(){
+        print("dn")
+        value -= increment
+        callback(value)
+    }
     /// Initialize the stepper view
-    @objc public init(text: String,
-                      value: MIDIByte,
-                      frame: CGRect = CGRect(x: 0, y: 0, width: 440, height: 60),
-                      callback: @escaping (MIDIByte) -> Void) {
+    public init(text: String,
+                      value: Double?,
+                      frame: CGRect = CGRect(x: 0, y: 0, width: 100, height: 100),
+                      callback: @escaping (Double) -> Void) {
         self.callback = callback
-        self.value = value
+        self.value = value!
         self.text = text
         super.init(frame: frame)
     }
 
     /// Initialize within Interface Builder
     required public init?(coder aDecoder: NSCoder) {
-        self.callback = { filename in return }
-        self.value = 0
-        self.text = "Value"
         super.init(coder: aDecoder)
     }
 
     /// Draw the stepper
     override open func draw(_ rect: CGRect) {
-        //// General Declarations
-        let context = UIGraphicsGetCurrentContext()!
-
-        //// Color Declarations
-        let red = UIColor(red: 1.000, green: 0.150, blue: 0.061, alpha: 1.000)
-        let gray = UIColor(red: 0.866, green: 0.872, blue: 0.867, alpha: 0.925)
-        let green = UIColor(red: 0.000, green: 0.977, blue: 0.000, alpha: 1.000)
-
-        //// background Drawing
-        let backgroundPath = UIBezierPath(rect: CGRect(x: 0, y: 0, width: 440, height: 60))
-        gray.setFill()
-        backgroundPath.fill()
-
-        //// textLabel Drawing
-        let textLabelRect = CGRect(x: 68, y: 0, width: 304, height: 60)
-        let textLabelStyle = NSMutableParagraphStyle()
-        textLabelStyle.alignment = .left
-
-        let textLabelInset: CGRect = textLabelRect.insetBy(dx: 10, dy: 0)
-        let textLabelTextHeight: CGFloat = text.boundingRect(with: CGSize(width: textLabelInset.width,
-                                                                          height: CGFloat.infinity),
-                                                             options: .usesLineFragmentOrigin,
-                                                             attributes: nil,
-                                                             context: nil).height
-        context.saveGState()
-        context.clip(to: textLabelInset)
-        let newText = "\(text): \(value)"
-        newText.draw(in: CGRect(x: textLabelInset.minX,
-                                y: textLabelInset.minY + (textLabelInset.height - textLabelTextHeight) / 2,
-                                width: textLabelInset.width,
-                                height: textLabelTextHeight),
-                     withAttributes: nil)
-        context.restoreGState()
-
-        //// minusGroup
-        //// minusRectangle Drawing
-        let minusRectanglePath = UIBezierPath(roundedRect: CGRect(x: 4, y: 5, width: 60, height: 50), cornerRadius: 16)
-        red.setFill()
-        minusRectanglePath.fill()
-        UIColor.black.setStroke()
-        minusRectanglePath.lineWidth = 2
-        minusRectanglePath.stroke()
-
-        //// minus Drawing
-        minusPath = UIBezierPath(rect: CGRect(x: 19, y: 25, width: 31, height: 10))
-        UIColor.black.setFill()
-        minusPath.fill()
-
-        //// plusGroup
-        //// plusRectangle Drawing
-        let plusRectanglePath = UIBezierPath(roundedRect: CGRect(x: 376, y: 5, width: 60, height: 50), cornerRadius: 16)
-        green.setFill()
-        plusRectanglePath.fill()
-        UIColor.black.setStroke()
-        plusRectanglePath.lineWidth = 2
-        plusRectanglePath.stroke()
-
-        //// plus Drawing
-        plusPath = UIBezierPath()
-        plusPath.move(to: CGPoint(x: 411, y: 15))
-        plusPath.addCurve(to: CGPoint(x: 411, y: 25),
-                          controlPoint1: CGPoint(x: 411, y: 15),
-                          controlPoint2: CGPoint(x: 411, y: 19.49))
-        plusPath.addLine(to: CGPoint(x: 421, y: 25))
-        plusPath.addLine(to: CGPoint(x: 421, y: 35))
-        plusPath.addLine(to: CGPoint(x: 411, y: 35))
-        plusPath.addCurve(to: CGPoint(x: 411, y: 45),
-                          controlPoint1: CGPoint(x: 411, y: 40.51),
-                          controlPoint2: CGPoint(x: 411, y: 45))
-        plusPath.addLine(to: CGPoint(x: 401, y: 45))
-        plusPath.addCurve(to: CGPoint(x: 401, y: 35),
-                          controlPoint1: CGPoint(x: 401, y: 45),
-                          controlPoint2: CGPoint(x: 401, y: 40.51))
-        plusPath.addLine(to: CGPoint(x: 391, y: 35))
-        plusPath.addLine(to: CGPoint(x: 391, y: 25))
-        plusPath.addLine(to: CGPoint(x: 401, y: 25))
-        plusPath.addCurve(to: CGPoint(x: 401, y: 15),
-                          controlPoint1: CGPoint(x: 401, y: 19.49),
-                          controlPoint2: CGPoint(x: 401, y: 15))
-        plusPath.addLine(to: CGPoint(x: 411, y: 15))
-        plusPath.addLine(to: CGPoint(x: 411, y: 15))
-        plusPath.close()
-        UIColor.black.setFill()
-        plusPath.fill()
+        super.draw(rect)
+        genStackViews(rect: rect)
     }
-
+    
+    private func genStackViews(rect: CGRect){
+        mainStack = UIStackView(frame: rect)
+        mainStack.axis = .vertical
+        mainStack.distribution = .fillEqually
+        mainStack.spacing = 10
+        label = UILabel(frame: rect)
+        label.text = text
+        label.backgroundColor = .lightGray
+        label.textAlignment = .center
+        
+        let buttons = UIStackView(frame: rect)
+        buttons.axis = .horizontal
+        buttons.distribution = .fillEqually
+        buttons.spacing = 5
+        plusButton = AKButton(title: "+", callback: {_ in
+            self.doPlusAction()
+        })
+        minusButton = AKButton(title: "-", callback: {_ in
+            self.doMinusAction()
+        })
+        buttons.addArrangedSubview(minusButton)
+        buttons.addArrangedSubview(plusButton)
+        
+        mainStack.addArrangedSubview(label)
+        mainStack.addArrangedSubview(buttons)
+        self.addSubview(mainStack)
+        buttons.layoutIfNeeded()
+    }
+    
+    /// Require constraint-based layout
+    open class override var requiresConstraintBasedLayout: Bool {
+        return true
+    }
+    override open func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        clipsToBounds = true
+    }
 }
