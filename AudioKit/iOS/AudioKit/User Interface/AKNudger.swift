d7cb7a42a224149582c588ba92b98beae97004db
Jeff Cooper <jeff@southernworkstx.com>
2018-04-27 20:24:08 -0500

    AKNudger works with slew rate

--- AudioKit/iOS/AudioKit/User Interface/AKNudger.swift
+++ AudioKit/iOS/AudioKit/User Interface/AKNudger.swift
@@ -24,23 +24,28 @@ open class AKNugder : AKStepper {
         }
     }
     private func doPlusActionHit() {
-//        value += increment
-        startPlusTimer()
-        callback(value)
+        if slewRate == 0 {
+            value += increment
+        }else{
+            startPlusTimer()
+        }
     }
     private func doPlusActionRelease() {
-        //        value -= increment
-        callback(value)
+        if slewRate == 0 {
+            value -= increment
+        }
     }
     private func doMinusActionHit() {
-        value -= increment
-        valueLabel.text = "\(value)"
-        callback(value)
+        if slewRate == 0 {
+            value -= increment
+        }else{
+            startMinusTimer()
+        }
     }
     private func doMinusActionRelease() {
-        value += increment
-        valueLabel.text = "\(value)"
-        callback(value)
+        if slewRate == 0 {
+            value += increment
+        }
     }
     override internal func checkValues() {
         //nothing to assert
@@ -48,26 +53,30 @@ open class AKNugder : AKStepper {
         maximum = value + increment
         minimum = value - increment
     }
-    private var slewRate = 0.01
+    var slewRate = 0.01
     private var frameRate = TimeInterval(1.0 / 60.0)
     private var plusButtonTimer: Timer?
     private var minusButtonTimer: Timer?
     private func startPlusTimer(){
         DispatchQueue.main.async {
-            if #available(iOS 10.0, *) {
-                self.plusButtonTimer = Timer.scheduledTimer(withTimeInterval: self.frameRate, repeats: true, block: {_ in
-                        self.animatePlusValue()
-                })
-                self.plusButtonTimer?.fire()
-            } else {
-                // Fallback on earlier versions
-                
+            if let timer = self.startTimerIfNeeded(timer: self.plusButtonTimer,
+                                                   callback: {_ in self.animatePlusValue() }){
+                self.plusButtonTimer = timer
+            }
+        }
+    }
+    private func startMinusTimer(){
+        DispatchQueue.main.async {
+            if let timer = self.startTimerIfNeeded(timer: self.minusButtonTimer,
+                                                   callback: {_ in self.animateMinusValue() }){
+                self.minusButtonTimer = timer
             }
         }
     }
     private var rate: Double{
         return increment * slewRate
     }
+    private var lastValue: Double = 0
     private func animatePlusValue(){
         if plusButton.isPressed{
             if value + rate < maximum {
@@ -78,16 +87,36 @@ open class AKNugder : AKStepper {
                 value -= rate
             }
         }
+        callbackOnChange()
+        lastValue = value
     }
     private func animateMinusValue(){
         if minusButton.isPressed{
-            if value >= originalValue - increment{
-                value -= slewRate
+            if value - rate > minimum{
+                value -= rate
             }
         }else{
-            if value <= originalValue{
-                value += slewRate
+            if value + rate <= originalValue {
+                value += rate
             }
+        }
+        callbackOnChange()
+        lastValue = value
+    }
+    private func callbackOnChange(){
+        if lastValue != value{
+            callback(value)
+        }
+    }
+    private func startTimerIfNeeded(timer: Timer?, callback: @escaping (Timer) -> Void ) -> Timer?{
+        if timer != nil, timer!.isValid{
+            return nil
+        }
+        if #available(iOS 10.0, *) {
+            return Timer.scheduledTimer(withTimeInterval: self.frameRate, repeats: true,
+                                        block: callback)
+        } else {
+            return nil
         }
     }
 }
