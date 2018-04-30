82b3ef0d756d0a44ba0539390754c6b3ee3d471f
Jeff Cooper <jeff@southernworkstx.com>
2018-04-30 18:31:33 -0500

    renamed approximation operators

--- AudioKit/Common/Internals/AudioKitHelpers+Approximation.swift
+++ AudioKit/Common/Internals/AudioKitHelpers+Approximation.swift
@@ -9,33 +9,33 @@
 import Foundation
 
 /// Approximation Operators - for when Swift gets double / float arithmetic wrong
-infix operator ==~ : ComparisonPrecedence
-public func ==~ (left: Double, right: Double) -> Bool
+infix operator ~== : ComparisonPrecedence
+public func ~== (left: Double, right: Double) -> Bool
 {
     return fabs(left.distance(to: right)) <= 1e-15
 }
-infix operator !=~ : ComparisonPrecedence
-public func !=~ (left: Double, right: Double) -> Bool
+infix operator ~!= : ComparisonPrecedence
+public func ~!= (left: Double, right: Double) -> Bool
 {
-    return !(left ==~ right)
+    return !(left ~== right)
 }
-infix operator <=~ : ComparisonPrecedence
-public func <=~ (left: Double, right: Double) -> Bool
+infix operator ~<= : ComparisonPrecedence
+public func ~<= (left: Double, right: Double) -> Bool
 {
-    return (left ==~ right) || (left <~ right)
+    return (left ~== right) || (left ~< right)
 }
-infix operator >=~ : ComparisonPrecedence
-public func >=~ (left: Double, right: Double) -> Bool
+infix operator ~>= : ComparisonPrecedence
+public func ~>= (left: Double, right: Double) -> Bool
 {
-    return (left ==~ right) || (left >~ right)
+    return (left ~== right) || (left ~> right)
 }
-infix operator <~ : ComparisonPrecedence
-public func <~ (left: Double, right: Double) -> Bool
+infix operator ~< : ComparisonPrecedence
+public func ~< (left: Double, right: Double) -> Bool
 {
     return left.distance(to: right) > 1e-15
 }
-infix operator >~ : ComparisonPrecedence
-public func >~ (left: Double, right: Double) -> Bool
+infix operator ~> : ComparisonPrecedence
+public func ~> (left: Double, right: Double) -> Bool
 {
     return left.distance(to: right) < -1e-15
 }
