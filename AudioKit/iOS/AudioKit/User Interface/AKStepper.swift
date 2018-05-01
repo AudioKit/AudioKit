ae7cfc2aeff703980c8a17ba791cc073146a0098
Jeff Cooper <jeff@southernworkstx.com>
2018-05-01 00:59:17 -0500

    trying to build another ui component from akstepper and aknudger.... renamed 'value' -> 'currentValue' to prevent confusion, opened up some values re: akstepper and aknudger

--- AudioKit/iOS/AudioKit/User Interface/AKStepper.swift
+++ AudioKit/iOS/AudioKit/User Interface/AKStepper.swift
@@ -15,48 +15,55 @@ open class AKStepper: UIView {
     @IBInspectable open var text: String = "Stepper"
     var label: UILabel! //fixme
     var valueLabel: UILabel! //fixme}
-    var showsValue: Bool = true
-    var plusButton: AKButton!
-    var minusButton: AKButton!
-    @IBInspectable var value: Double = 0.5 {
+    open var showsValue: Bool = true
+    public var plusButton: AKButton!
+    public var minusButton: AKButton!
+    @IBInspectable public var currentValue: Double = 0.5 {
         didSet{
             DispatchQueue.main.async {
-                self.valueLabel.text = String(format: "%.3f", self.value)
+                self.valueLabel.text = String(format: "%.3f", self.currentValue)
             }
             
         }
     }
-    @IBInspectable var increment: Double = 0.1
-    @IBInspectable var minimum: Double = 0
-    @IBInspectable var maximum: Double = 1
+    @IBInspectable public var increment: Double = 0.1
+    @IBInspectable public var minimum: Double = 0
+    @IBInspectable public var maximum: Double = 1
     internal var originalValue:Double = 0.5
     open var callback: (Double)->Void = {val in
         print("callback: \(val)")
     }
 
     internal func doPlusAction(){
-        value += min(increment, maximum - value)
-        callback(value)
+        currentValue += min(increment, maximum - currentValue)
+        callback(currentValue)
     }
     internal func doMinusAction(){
-        value -= min(increment, value - minimum)
-        callback(value)
+        currentValue -= min(increment, currentValue - minimum)
+        callback(currentValue)
     }
     /// Initialize the stepper view
-    public init(text: String, value: Double,
+    public init(text: String, value: Double, minimum: Double, maximum: Double, increment: Double,
                 frame: CGRect = CGRect(x: 0, y: 0, width: 100, height: 100),
                 callback: @escaping (Double) -> Void) {
         self.callback = callback
-        self.value = value
+        self.minimum = minimum
+        self.maximum = maximum
+        self.increment = increment
+        self.currentValue = value
         self.originalValue = value
         self.text = text
         super.init(frame: frame)
+        checkValues()
+        setupButtons()
     }
 
     /// Initialize within Interface Builder
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.originalValue = value
+        checkValues()
+        self.originalValue = currentValue
+        setupButtons()
     }
     override open func awakeFromNib() {
         checkValues()
@@ -69,13 +76,12 @@ open class AKStepper: UIView {
     }
     
     private func genStackViews(rect: CGRect){
-        setupButtons()
         let borderWidth = minusButton!.borderWidth
         label = UILabel(frame: CGRect(x: rect.origin.x + borderWidth, y: rect.origin.y, width: rect.width, height: rect.height * 0.3))
         label.text = text
         label.textAlignment = .left
         valueLabel = UILabel(frame: CGRect(x: rect.origin.x - borderWidth, y: rect.origin.y, width: rect.width, height: rect.height * 0.3))
-        valueLabel.text = "\(value)"
+        valueLabel.text = "\(currentValue)"
         valueLabel.textAlignment = .right
         
         let buttons = UIStackView(frame: CGRect(x: rect.origin.x, y: rect.origin.y + label.frame.height, width: rect.width, height: rect.height * 0.7))
@@ -112,10 +118,10 @@ open class AKStepper: UIView {
     }
     internal func checkValues(){
         assert(minimum < maximum)
-        assert(value >= minimum)
-        assert(value <= maximum)
+        assert(currentValue >= minimum)
+        assert(currentValue <= maximum)
         assert(increment < maximum - minimum)
-        originalValue = value
+        originalValue = currentValue
     }
     internal func setupButtons(){
         plusButton = AKButton(title: "+", callback: {_ in
