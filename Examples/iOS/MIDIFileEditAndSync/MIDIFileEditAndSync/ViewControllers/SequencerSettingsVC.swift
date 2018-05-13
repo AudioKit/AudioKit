411861d4ccf411856c61a39eefed7b0a9363c455
crashing booth <jholtzkener@hotmail.com>
2018-05-13 11:51:38 +0900

    updated to include new time signature setter

--- Examples/iOS/MIDIFileEditAndSync/MIDIFileEditAndSync/ViewControllers/SequencerSettingsVC.swift
+++ Examples/iOS/MIDIFileEditAndSync/MIDIFileEditAndSync/ViewControllers/SequencerSettingsVC.swift
@@ -69,10 +69,13 @@ extension SequencerSettingsVC: UITextFieldDelegate {
             }
         case timeSigTextField:
             if 1 ..< 60 ~= value {
-                sequencerDelegate?.addTimeSignatureEvent(timeSignatureTop: UInt8(value),
-                                                         timeSignatureBottom: .four,
+                let timeSig = AKTimeSignature(topValue: UInt8(value),
+                                              bottomValue: AKTimeSignature.TimeSignatureBottomValue = .four)
+                sequencerDelegate?.addTimeSignatureEvent(at: 0.0,
+                                                         timeSignature: timeSig,
                                                          ticksPerMetronomeClick: 24,
-                                                         thirtySecondNotesPerQuarter: 8)
+                                                         thirtySecondNotesPerQuarter: 8,
+                                                         clearExistingEvents: true)
                 moveTextField(textField, moveDistance: -200, up: false)
                 return true
             }
@@ -112,10 +115,11 @@ protocol SequencerDelegate: class {
     var tempo: Double { get }
     var length: AKDuration { get }
 
-    func addTimeSignatureEvent(timeSignatureTop: UInt8,
-                               timeSignatureBottom: AKSequencer.TimeSignatureBottomValue,
+    func addTimeSignatureEvent(at timeStamp: MusicTimeStamp,
+                               timeSignature: AKTimeSignature,
                                ticksPerMetronomeClick: UInt8,
-                               thirtySecondNotesPerQuarter: UInt8)
+                               thirtySecondNotesPerQuarter: UInt8,
+                               clearExistingEvents: Bool)
     func setTempo(_ bpm: Double)
     func enableLooping()
     func disableLooping()
