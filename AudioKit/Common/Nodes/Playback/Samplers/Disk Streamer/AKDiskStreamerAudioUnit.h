d9a8bb5441f7cbe4f69af5af17b88903a1b7fcf7
Jeff Cooper <jeff@southernworkstx.com>
2018-06-20 19:08:32 -0500

    lots of cleanup on diskstreamer

--- AudioKit/Common/Nodes/Playback/Samplers/Disk Streamer/AKDiskStreamerAudioUnit.h
+++ AudioKit/Common/Nodes/Playback/Samplers/Disk Streamer/AKDiskStreamerAudioUnit.h
@@ -16,7 +16,7 @@ typedef void (^AKCCallback)(void);
 @property (nonatomic) float endPoint;
 @property (nonatomic) float tempStartPoint;
 @property (nonatomic) float tempEndPoint;
-@property (nonatomic) float rate;
+//@property (nonatomic) float rate;
 @property (nonatomic) float volume;
 @property (nonatomic) BOOL loop;
 @property (nonatomic) float loopStartPoint;
@@ -25,8 +25,6 @@ typedef void (^AKCCallback)(void);
 @property (nonatomic) AKCCallback completionHandler;
 @property (nonatomic) AKCCallback loopCallback;
 
-- (void)setupAudioFileTable:(UInt32)size;
-- (void)loadAudioData:(float *)data size:(UInt32)size sampleRate:(float)sampleRate numChannels:(UInt32)numChannels;
 - (void)loadFile:(const char*)filename;
 - (int)size;
 - (double)position;
