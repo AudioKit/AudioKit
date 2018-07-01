a96651f545523468c275c093cd3081dda7386adf
Aurelius Prochazka <aurephd@gmail.com>
2018-07-01 02:09:57 -0700

    Revert another print

--- Examples/macOS/AudioUnitManager/AudioUnitManager/Extensions.swift
+++ Examples/macOS/AudioUnitManager/AudioUnitManager/Extensions.swift
@@ -164,7 +164,7 @@ extension NSLayoutConstraint {
 
     public static func activateConstraintsEqualToSuperview(child: NSView) {
         guard let superview = child.superview else {
-            AKLog("NSLayoutConstraint.fillSuperview() superview of child is nil")
+            print("NSLayoutConstraint.fillSuperview() superview of child is nil")
             return
         }
 
