4a4c574ce921d7c2b409a33444681965f79a9d22
Aurelius Prochazka <aurephd@gmail.com>
2018-07-05 19:28:42 -0700

    Swiftlinting

--- AudioKit/Common/MIDI/AKMIDI.swift
+++ AudioKit/Common/MIDI/AKMIDI.swift
@@ -20,52 +20,52 @@ import CoreMIDI
 ///
 /// You then implement the methods you need from AKMIDIListener and use the data how you need.
 open class AKMIDI {
-    
+
     // MARK: - Properties
-    
+
     /// MIDI Client Reference
     open var client = MIDIClientRef()
-    
+
     /// Array of MIDI In ports
     internal var inputPorts: [String: MIDIPortRef] = [:]
-    
+
     /// Virtual MIDI Input destination
     open var virtualInput = MIDIPortRef()
-    
+
     /// MIDI Client Name
     private let clientName: CFString = "MIDI Client" as CFString
-    
+
     /// MIDI In Port Name
     internal let inputPortName: CFString = "MIDI In Port" as CFString
-    
+
     /// MIDI Out Port Reference
     internal var outputPort = MIDIPortRef()
-    
+
     /// Virtual MIDI output
     open var virtualOutput = MIDIPortRef()
-    
+
     /// Array of MIDI Endpoints
     open var endpoints = [String: MIDIEndpointRef]()
-    
+
     /// MIDI Out Port Name
     internal var outputPortName: CFString = "MIDI Out Port" as CFString
-    
+
     /// Array of all listeners
     internal var listeners = [AKMIDIListener]()
-    
+
     internal var transformers = [AKMIDITransformer]()
-    
+
     // MARK: - Initialization
-    
+
     /// Initialize the AKMIDI system
     @objc public init() {
-        
+
         #if os(iOS)
         MIDINetworkSession.default().isEnabled = true
         MIDINetworkSession.default().connectionPolicy =
             MIDINetworkConnectionPolicy.anyone
         #endif
-        
+
         if client == 0 {
             let result = MIDIClientCreateWithBlock(clientName, &client) {
                 guard $0.pointee.messageID == .msgSetupChanged else {
@@ -80,21 +80,21 @@ open class AKMIDI {
             }
         }
     }
-    
+
     // MARK: - Virtual MIDI
-    
+
     /// Create set of virtual MIDI ports
     open func createVirtualPorts(_ uniqueID: Int32 = 2_000_000, name: String? = nil) {
         destroyVirtualPorts()
         createVirtualInputPort(uniqueID, name: name)
         createVirtualOutputPort(uniqueID, name: name)
     }
-    
+
     /// Create a virtual MIDI input port
     open func createVirtualInputPort(_ uniqueID: Int32 = 2_000_000, name: String? = nil) {
         destroyVirtualInputPort()
         let virtualPortname = name ?? String(clientName)
-        
+
         let result = MIDIDestinationCreateWithBlock(client,
                                                     virtualPortname as CFString,
                                                     &virtualInput) { packetList, _ in
@@ -105,19 +105,19 @@ open class AKMIDI {
                                                             }
                                                         }
         }
-        
+
         if result == noErr {
             MIDIObjectSetIntegerProperty(virtualInput, kMIDIPropertyUniqueID, uniqueID)
         } else {
             AKLog("Error creatervirt dest: \(virtualPortname) -- \(virtualInput)")
         }
     }
-    
+
     /// Create a virtual MIDI output port
     open func createVirtualOutputPort(_ uniqueID: Int32 = 2_000_000, name: String? = nil) {
         destroyVirtualOutputPort()
         let virtualPortname = name ?? String(clientName)
-        
+
         let result = MIDISourceCreate(client, virtualPortname as CFString, &virtualOutput)
         if result == noErr {
             MIDIObjectSetIntegerProperty(virtualInput, kMIDIPropertyUniqueID, uniqueID + 1)
@@ -125,13 +125,13 @@ open class AKMIDI {
             AKLog("Error creating virtual source: \(virtualPortname) -- \(virtualOutput)")
         }
     }
-    
+
     /// Discard all virtual ports
     open func destroyVirtualPorts() {
         destroyVirtualInputPort()
         destroyVirtualOutputPort()
     }
-    
+
     /// Closes the virtual input port, if created one already.
     ///
     /// - Returns: Returns true if virtual input closed.
@@ -144,7 +144,7 @@ open class AKMIDI {
         }
         return false
     }
-    
+
     /// Closes the virtual output port, if created one already.
     ///
     /// - Returns: Returns true if virtual output closed.
