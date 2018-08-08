fd84b5ca59afece479902a0ef88ac9bbbe1b0cf4
Aurelius Prochazka <aurephd@gmail.com>
2018-08-08 16:54:40 -0700

    Added default rectangle size

--- AudioKit/Common/User Interface/AKNodeOutputPlot.swift
+++ AudioKit/Common/User Interface/AKNodeOutputPlot.swift
@@ -99,7 +99,7 @@ open class AKNodeOutputPlot: EZAudioPlot {
     ///   - width: Width of the view
     ///   - height: Height of the view
     ///
-    @objc public init(_ input: AKNode? = AudioKit.output, frame: CGRect, bufferSize: Int = 1_024) {
+    @objc public init(_ input: AKNode? = AudioKit.output, frame: CGRect = CGRect.zero, bufferSize: Int = 1_024) {
         super.init(frame: frame)
         self.plotType = .buffer
         self.backgroundColor = AKColor.white
