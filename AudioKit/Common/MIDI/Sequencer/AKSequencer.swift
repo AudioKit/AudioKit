65babcd10c47c91cab29d3713288c55c030ffa90
crashing booth <jholtzkener@hotmail.com>
2018-08-25 18:40:57 -0400

    updated docs for clearRange, set Tempo

--- AudioKit/Common/MIDI/Sequencer/AKSequencer.swift
+++ AudioKit/Common/MIDI/Sequencer/AKSequencer.swift
@@ -176,7 +176,8 @@ open class AKSequencer {
         return rate
     }
 
-    /// Set the tempo of the sequencer
+    /// Clears all existing tempo events and adds single tempo event at start
+    /// Will also adjust the tempo immediately if sequence is playing when called
     open func setTempo(_ bpm: Double) {
         let constrainedTempo = (10...280).clamp(bpm)
 
@@ -723,7 +724,7 @@ open class AKSequencer {
         tracks.remove(at: trackIndex)
     }
 
-    /// Clear all events from all tracks within the specified range
+    /// Clear all non-tempo events from all tracks within the specified range
     //
     /// - Parameters:
     ///   - start: Start of the range to clear, in beats (inclusive)
