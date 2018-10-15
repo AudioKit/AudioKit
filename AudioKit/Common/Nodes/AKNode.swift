bcd6a14433dbe7f17a9cdd8966cf5e1ad2c69e99
Jeff Cooper <jeff@southernworkstx.com>
2018-10-14 21:34:26 -0500

    AKPolyphonicNode split into its own file

--- AudioKit/Common/Nodes/AKNode.swift
+++ AudioKit/Common/Nodes/AKNode.swift
@@ -105,48 +105,6 @@ public protocol AKPolyphonic {
     func stop(noteNumber: MIDINoteNumber)
 }
 
-/// Bare bones implementation of AKPolyphonic protocol
-@objc open class AKPolyphonicNode: AKNode, AKPolyphonic, AKSequenceable {
-
-    /// Global tuning table used by AKPolyphonicNode (AKNode classes adopting AKPolyphonic protocol)
-    @objc public static var tuningTable = AKTuningTable()
-    open var midiInstrument: AVAudioUnitMIDIInstrument?
-
-    /// Play a sound corresponding to a MIDI note with frequency
-    ///
-    /// - Parameters:
-    ///   - noteNumber: MIDI Note Number
-    ///   - velocity:   MIDI Velocity
-    ///   - frequency:  Play this frequency
-    ///
-    @objc open func play(noteNumber: MIDINoteNumber, velocity: MIDIVelocity, frequency: Double) {
-        AKLog("Playing note: \(noteNumber), velocity: \(velocity), frequency: \(frequency), override in subclass")
-    }
-
-    /// Play a sound corresponding to a MIDI note
-    ///
-    /// - Parameters:
-    ///   - noteNumber: MIDI Note Number
-    ///   - velocity:   MIDI Velocity
-    ///
-    @objc open func play(noteNumber: MIDINoteNumber, velocity: MIDIVelocity) {
-
-        // MARK: Microtonal pitch lookup
-        // default implementation is 12 ET
-        let frequency = AKPolyphonicNode.tuningTable.frequency(forNoteNumber: noteNumber)
-        //        AKLog("Playing note: \(noteNumber), velocity: \(velocity), using tuning table frequency: \(frequency)")
-        self.play(noteNumber: noteNumber, velocity: velocity, frequency: frequency)
-    }
-
-    /// Stop a sound corresponding to a MIDI note
-    ///
-    /// - parameter noteNumber: MIDI Note Number
-    ///
-    @objc open func stop(noteNumber: MIDINoteNumber) {
-        AKLog("Stopping note \(noteNumber), override in subclass")
-    }
-}
-
 /// Protocol for dictating that a node can be in a started or stopped state
 @objc public protocol AKToggleable {
     /// Tells whether the node is processing (ie. started, playing, or active)
