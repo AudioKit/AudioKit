ae87022e46b3961692693c131ab05f5ea69cb357
Jeff Cooper <jeff@southernworkstx.com>
2018-10-20 12:48:48 -0500

    clean up akseq swift file

--- AudioKit/Common/MIDI/Sequencer/Timeline Sequencer/AKSequencerTrack.swift
+++ AudioKit/Common/MIDI/Sequencer/Timeline Sequencer/AKSequencerTrack.swift
@@ -11,44 +11,16 @@ import Foundation
 public class AKSequencerTrack {
 
     var engine: AKSequencerTrackEngine!
-    var lengthInBeats: Double = 4 {
-        didSet {
-            engine.lengthInBeats = lengthInBeats
-        }
-    }
-    var tempo: Double = 120 {
-        didSet {
-            engine.tempo = tempo
-        }
-    }
-    var loopEnabled: Bool = true {
-        didSet {
-            engine.maximumPlayCount = loopEnabled ? 0 : 1
-        }
-    }
-    public var trackID: Int = 0
-    public var events: [AKMIDIEvent] {
-        return _events
-    }
     var _events = [AKMIDIEvent]()
-    public var noteOffset: Int = 0 {
-        didSet {
-            engine.noteOffset = Int32(noteOffset)
-        }
-    }
-    public var channelOffset: Int = 0 {
-        didSet { engine.channelOffset = Int32(channelOffset) }
-    }
-    public var timeMultiplier: Double = 1 {
-        didSet {
-            engine.timeMultiplier = timeMultiplier
-        }
-    }
-    public var gateMultiplier: Double = 1 {
-        didSet {
-            engine.gateMultiplier = gateMultiplier
-        }
-    }
+    public var events: [AKMIDIEvent] {  return _events }
+    public var trackID: Int = 0 {  didSet { engine.trackIndex = Int32(trackID) } }
+    public var lengthInBeats: Double = 4 {  didSet { engine.lengthInBeats = lengthInBeats } }
+    public var tempo: Double = 120 {  didSet { engine.tempo = tempo } }
+    public var loopEnabled: Bool = true {  didSet { engine.maximumPlayCount = loopEnabled ? 0 : 1 } }
+    public var noteOffset: Int = 0 {  didSet { engine.noteOffset = Int32(noteOffset) } }
+    public var channelOffset: Int = 0 { didSet { engine.channelOffset = Int32(channelOffset) } }
+    public var timeMultiplier: Double = 1 {  didSet { engine.timeMultiplier = timeMultiplier } }
+    public var gateMultiplier: Double = 1 {  didSet { engine.gateMultiplier = gateMultiplier } }
 
     init(_ node: AKNode, index: Int = 0) {
         engine = AKSequencerTrackEngine(node, index: Int32(index))
@@ -59,7 +31,7 @@ public class AKSequencerTrack {
             engine.addMIDIEvent(UInt8(status.rawValue), data1: event.data1, data2: event.data2, at: position)
         }
     }
-    public func addNote(noteNumber: MIDINoteNumber, velocity: MIDIVelocity, at: Double, duration: Double){
+    public func addNote(noteNumber: MIDINoteNumber, velocity: MIDIVelocity, at: Double, duration: Double) {
         engine.addNote(noteNumber, velocity: velocity, at: at, duration: duration)
     }
 
