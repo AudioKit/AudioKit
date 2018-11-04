d74efe58b87bfaf9f204e4352c8eaf0ecabd7d86
Jeff Cooper <jeff@southernworkstx.com>
2018-11-04 14:51:53 -0600

    clean up akmidiinstrument handle midi function

--- AudioKit/Common/MIDI/AKMIDIInstrument.swift
+++ AudioKit/Common/MIDI/AKMIDIInstrument.swift
@@ -113,14 +113,16 @@ open class AKMIDIInstrument: AKPolyphonicNode, AKMIDIListener {
 
     // Send MIDI data to the audio unit
     func handleMIDI(data1: MIDIByte, data2: MIDIByte, data3: MIDIByte) {
-        let status = data1 >> 4
-        let channel = data1 & 0xF
-        if Int(status) == AKMIDIStatusType.noteOn.rawValue && data3 > 0 {
-            start(noteNumber: MIDINoteNumber(data2),
-                  velocity: MIDIVelocity(data3),
-                  channel: MIDIChannel(channel))
-        } else if Int(status) == AKMIDIStatusType.noteOn.rawValue && data3 == 0 {
-            stop(noteNumber: MIDINoteNumber(data2), channel: MIDIChannel(channel))
+        let status = AKMIDIStatus(byte: data1)
+        let statusType = status.type
+        if let channel = status.channel {
+            if statusType == AKMIDIStatusType.noteOn && data3 > 0  {
+                start(noteNumber: data2,
+                      velocity: data3,
+                      channel: channel)
+            } else if statusType == AKMIDIStatusType.noteOn && data3 == 0 || statusType == AKMIDIStatusType.noteOff {
+                stop(noteNumber: data2, channel: channel)
+            }
         }
     }
 
