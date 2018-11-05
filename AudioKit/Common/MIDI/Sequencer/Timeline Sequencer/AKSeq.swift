0d7c27f9278ea472cfa9e31deaf918729b66803e
Jeff Cooper <jeff@southernworkstx.com>
2018-11-05 12:35:53 -0600

    update akseq loadMIDIFile function

--- AudioKit/Common/MIDI/Sequencer/Timeline Sequencer/AKSeq.swift
+++ AudioKit/Common/MIDI/Sequencer/Timeline Sequencer/AKSeq.swift
@@ -77,85 +77,13 @@ open class AKSeq {
         tracks.append(AKSequencerTrack(midiPort: midiPort, midiEndpoint: midiEndpoint, node: node, index: tracks.count))
         return id
     }
-
-    public func loadMIDIFile(path: String) -> [AKMIDIEvent]? {
+    
+    public func loadMIDIFile(path: String) {
         print("loadind file at \(path)")
-        let url = URL(fileURLWithPath: path)
-        let headerSize = 6 //don't know why
-        let offset = 0
-        if let midiData = try? Data(contentsOf: url) {
-            print("got data \(midiData.count)")
-            let dataSize = midiData.count
-            if dataSize < headerSize + offset {
-                AKLog("size of MIDI file is too small - is your file valid?")
-                return nil
-            }
-            let typeLength = 4
-            var typeIndex = 0
-            let sizeLength = 4
-            var sizeIndex = 0
-            var dataLength = 0
-            var dataIndex = 0
-            var chunks = [AKMIDIFileChunk]()
-            var currentTypeChunk: [UInt8] = Array(repeating: 0, count: 4)
-            var currentLengthChunk: [UInt8] = Array(repeating: 0, count: 4)
-            var currentDataChunk: [UInt8] = []
-            var newChunk = true
-            var isParsingType = false
-            var isParsingLength = false
-            var isParsingHeader = true
-            for i in 0..<dataSize {
-                if newChunk {
-                    isParsingType = true
-                    isParsingLength = false
-                    newChunk = false
-                    currentTypeChunk = Array(repeating: 0, count: 4)
-                    currentLengthChunk = Array(repeating: 0, count: 4)
-                    currentDataChunk = []
-                }
-                if isParsingType { //get chunk type
-                    currentTypeChunk[typeIndex] = midiData[i]
-                    typeIndex += 1
-                    if typeIndex == typeLength {
-                        isParsingType = false
-                        isParsingLength = true
-                        typeIndex = 0
-                    }
-                } else if isParsingLength { //get chunk length
-                    currentLengthChunk[sizeIndex] = midiData[i]
-                    sizeIndex += 1
-                    if sizeIndex == sizeLength {
-                        isParsingLength = false
-                        sizeIndex = 0
-                        dataLength = Int(currentLengthChunk.map(String.init).joined()) ?? 0
-                    }
-                } else { //get chunk data
-                    var tempChunk: AKMIDIFileChunk
-                    currentDataChunk.append(midiData[i])
-                    if UInt8(currentDataChunk.count) == dataLength {
-                        if isParsingHeader {
-                            tempChunk = MIDIFileHeaderChunk(typeData: currentTypeChunk,
-                                                            lengthData: currentLengthChunk, data: currentDataChunk)
-                        } else {
-                            tempChunk = MIDIFileTrackChunk(typeData: currentTypeChunk,
-                                                           lengthData: currentLengthChunk, data: currentDataChunk)
-                        }
-                        newChunk = true
-                        isParsingHeader = false
-                        chunks.append(tempChunk)
-                    }
-                }
-            }
-            for chunk in chunks {
-                if let trackChunk = chunk as? MIDIFileTrackChunk {
-                    let events = trackChunk.events
-                    print("track chunk w \(events.count) events")
-                } else {
-                    print("header chunk")
-                }
-            }
+        let file = AKMIDIFile(path: path)
+        for track in file.trackChunks {
+            let events = track.events
         }
-        return nil
     }
 }
 
