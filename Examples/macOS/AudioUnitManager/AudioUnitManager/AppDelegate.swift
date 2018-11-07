46ea13dcf5169cbdae5e8037a0222e8987a8e879
“Ryan <hi@spongefork.com>
2018-11-07 13:10:03 -0800

    whitespace

--- Examples/macOS/AudioUnitManager/AudioUnitManager/AppDelegate.swift
+++ Examples/macOS/AudioUnitManager/AudioUnitManager/AppDelegate.swift
@@ -10,7 +10,6 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
 
@@ -20,5 +19,4 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
 }
