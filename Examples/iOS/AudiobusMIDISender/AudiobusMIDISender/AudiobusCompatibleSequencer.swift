e0b85fad8d2192f93ce13e9aeac2874b950b5f42
Jeff Cooper <jeff@southernworkstx.com>
2018-11-07 09:32:14 -0600

    update ABSender project to use new midistatus

--- Examples/iOS/AudiobusMIDISender/AudiobusMIDISender/AudiobusCompatibleSequencer.swift
+++ Examples/iOS/AudiobusMIDISender/AudiobusMIDISender/AudiobusCompatibleSequencer.swift
@@ -74,7 +74,7 @@ class AudiobusCompatibleSequencer {
     // MARK: - Building Tracks, Callback Instruments, and ABMIDISendPorts
     fileprivate func createTracksAndCallBackInst() {
         tracks = [AKMusicTrack]()
-        callbackInsts = [AKCallbackInstrument]()
+        callbackInsts = [AKMIDICallbackInstrument]()
         ports = [ABMIDISenderPort]()
         Audiobus.start()
         for i in 0 ..< numTracks {
@@ -92,11 +92,11 @@ class AudiobusCompatibleSequencer {
     fileprivate func setUpCallBackFunctions(channel: Int) -> AKMIDICallbackInstrument {
         return  AKMIDICallbackInstrument { [weak self] status, note, velocity in
             guard let this = self else { return }
-            switch status {
-            case .noteOn:
+            switch Int(status >> 4) {
+            case AKMIDIStatus.noteOn.rawValue:
                 this.noteOn(midiSendPort: this.ports[channel], status: status, note: note, velocity: velocity, channel: MIDIChannel(channel))
                 this.displayDelegate?.flashNoteOnDisplay(index: channel, noteOn: true)
-            case .noteOff:
+            case AKMIDIStatus.noteOff.rawValue:
                 this.noteOff(midiSendPort: this.ports[channel], status: status, note: note, velocity: velocity, channel: MIDIChannel(channel))
                 this.displayDelegate?.flashNoteOnDisplay(index: channel, noteOn: false)
             default:
