1430d9c731c0013dba1104362546dcc372d214ff
Jeff Cooper <jeff@southernworkstx.com>
2018-11-26 12:54:17 +0000

    AKSeqPlus works - being used in test project

--- AudioKit/Common/MIDI/Sequencer/Timeline Sequencer/AKSequencerTrackPlus.swift
+++ AudioKit/Common/MIDI/Sequencer/Timeline Sequencer/AKSequencerTrackPlus.swift
@@ -9,16 +9,45 @@
 import Foundation
 
 public class AKSequencerTrackPlus: AKSequencerTrack {
-    public var sequenceEvents: [SequenceEvent] = [] {
+
+    public var notes: [NoteOnOffEvent] = [] {
         didSet {
             updateSequence()
         }
     }
 
+    public override func add(noteNumber: MIDINoteNumber, velocity: MIDIVelocity, at: Double, duration: Double) {
+        let noteEvent = NoteOnOffEvent(noteNumber: noteNumber, velocity: velocity, channel: 0,
+                                       position: at, duration: duration)
+        notes.append(noteEvent)
+    }
+
+    public override func clear() {
+        notes.removeAll()
+        updateSequence()
+    }
+    
     public func updateSequence() {
-        clear()
-        for event in sequenceEvents {
-            add(event: event.event, at: event.position.beats)
+        engine.clear()
+        for notePair in notes {
+            add(event: notePair.noteOnEvent.event, at: notePair.noteOnEvent.position)
+            add(event: notePair.noteOffEvent.event, at: notePair.noteOffEvent.position)
         }
+    }
+}
+
+public struct NoteOnOffEvent {
+    var duration = 0.1
+    var noteOnEvent: SequenceNoteEvent
+    var noteOffEvent: SequenceNoteEvent {
+        let event = AKMIDIEvent(noteOff: noteOnEvent.event.bytes[1], velocity: 0,
+                                channel: noteOnEvent.event.channel ?? 0)
+        return SequenceNoteEvent(event: event, position: noteOnEvent.position + duration)
+    }
+
+    public init(noteNumber: MIDINoteNumber, velocity: MIDIVelocity, channel: MIDIChannel, position: Double,
+                duration: Double){
+        noteOnEvent = SequenceNoteEvent(event: AKMIDIEvent.init(noteOn: noteNumber, velocity: velocity, channel: channel), position: position)
+        self.duration = duration
     }
 }
