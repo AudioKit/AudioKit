58bd08511bc102088740b90b26543bdc3b80b129
Nevo Segal <nevosegal@gmail.com>
2018-12-03 12:15:39 +0000

    divide by file duration if endTime - startTime is zero

--- AudioKit/Common/Nodes/Playback/Players/Player/AKPlayer.swift
+++ AudioKit/Common/Nodes/Playback/Players/Player/AKPlayer.swift
@@ -306,7 +306,9 @@ import AVFoundation
 
     /// - Returns: Current time of the player in seconds while playing.
     @objc public var currentTime: Double {
-        let current = startTime + playerTime.truncatingRemainder(dividingBy: (endTime - startTime))
+        let currentDuration = (endTime - startTime == 0) ? duration : (endTime - startTime)
+        let current = startTime + playerTime.truncatingRemainder(dividingBy: currentDuration)
+        
         return current
     }
 
