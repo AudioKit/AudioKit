969f788b87204af36f67f6c42073169408dfa15c
Shane Dunne <shane@getdunne.com>
2018-12-18 22:38:58 -0500

    Partial refactoring of Bank nodes

--- AudioKit/Common/Nodes/Generators/Oscillators/Phase Distortion Oscillator Bank/AKPhaseDistortionOscillatorBankDSPKernel.hpp
+++ AudioKit/Common/Nodes/Generators/Oscillators/Phase Distortion Oscillator Bank/AKPhaseDistortionOscillatorBankDSPKernel.hpp
@@ -18,97 +18,57 @@ enum {
 class AKPhaseDistortionOscillatorBankDSPKernel : public AKBankDSPKernel, public AKOutputBuffered {
 public:
     // MARK: Types
-    struct NoteState {
-        NoteState *next;
-        NoteState *prev;
-        AKPhaseDistortionOscillatorBankDSPKernel *kernel;
-
-        enum { stageOff, stageOn, stageRelease };
-        int stage = stageOff;
-
-        float internalGate = 0;
-        float amp = 0;
+    struct NoteState  : public AKBankDSPKernel::NoteState {
+        
         float velocityAmp = 0;
 
-        sp_adsr *adsr;
         sp_tabread *tab;
         sp_phasor *phs;
         sp_pdhalf *pdhalf;
 
-        void init() {
-            sp_adsr_create(&adsr);
-            sp_adsr_init(kernel->sp, adsr);
-
-            sp_pdhalf_create(&pdhalf);
+        NoteState() {
             sp_tabread_create(&tab);
-            sp_tabread_init(kernel->sp, tab, kernel->ftbl, 1);
             sp_phasor_create(&phs);
+            sp_pdhalf_create(&pdhalf);
+        }
+        
+        virtual ~NoteState() {
+            sp_tabread_destroy(&tab);
+            sp_phasor_destroy(&phs);
+            sp_pdhalf_destroy(&pdhalf);
+        }
+        
+        void init() override {
+            auto bankKernel = (AKPhaseDistortionOscillatorBankDSPKernel*)kernel;
+            
+            sp_adsr_init(kernel->getSpData(), adsr);
+            sp_tabread_init(kernel->getSpData(), tab, bankKernel->ftbl, 1);
 
-            sp_pdhalf_init(kernel->sp, pdhalf);
-            sp_phasor_init(kernel->sp, phs, 0);
+            sp_pdhalf_init(kernel->getSpData(), pdhalf);
+            sp_phasor_init(kernel->getSpData(), phs, 0);
 
             phs->freq = 0;
         }
 
-
-        void clear() {
-            stage = stageOff;
-            amp = 0;
-        }
-
-        // linked list management
-        void remove() {
-            if (prev) prev->next = next;
-            else kernel->playingNotes = next;
-
-            if (next) next->prev = prev;
-
-            //prev = next = nullptr; Had to remove due to a click, potentially bad
-
-            --kernel->playingNotesCount;
-
-            sp_pdhalf_destroy(&pdhalf);
-            sp_tabread_destroy(&tab);
-            sp_phasor_destroy(&phs);
-        }
-
-        void add() {
-            init();
-            prev = nullptr;
-            next = kernel->playingNotes;
-            if (next) next->prev = this;
-            kernel->playingNotes = this;
-            ++kernel->playingNotesCount;
-        }
-
-        void noteOn(int noteNumber, int velocity) {
-            noteOn(noteNumber, velocity, (float)noteToHz(noteNumber));
-        }
-
-        void noteOn(int noteNumber, int velocity, float frequency) {
-            if (velocity == 0) {
-                if (stage == stageOn) {
-                    stage = stageRelease;
-                    internalGate = 0;
-                }
-            } else {
-                if (stage == stageOff) { add(); }
+        void noteOn(int noteNumber, int velocity, float frequency) override {
+            AKBankDSPKernel::NoteState::noteOn(noteNumber, velocity, frequency);
+            
+            if (velocity != 0) {
                 phs->freq = frequency;
                 velocityAmp = (float)pow2(velocity / 127.);
-                stage = stageOn;
-                internalGate = 1;
             }
         }
 
-
-        void run(int frameCount, float *outL, float *outR)
+        void run(int frameCount, float *outL, float *outR) override
         {
+            auto bankKernel = (AKPhaseDistortionOscillatorBankDSPKernel*)kernel;
+            
             float originalFrequency = phs->freq;
             phs->freq *= powf(2, kernel->pitchBend / 12.0);
             phs->freq = clamp(phs->freq, 0.0f, 22050.0f);
             float bentFrequency = phs->freq;
 
-            pdhalf->amount = kernel->phaseDistortion;
+            pdhalf->amount = bankKernel->phaseDistortion;
 
             adsr->atk = (float)kernel->attackDuration;
             adsr->dec = (float)kernel->decayDuration;
@@ -120,15 +80,15 @@ public:
                 float pd = 0;
                 float ph = 0;
                 float depth = kernel->vibratoDepth / 12.0;
-                float variation = sinf((kernel->currentRunningIndex + frameIndex) * 2 * 2 * M_PI * kernel->vibratoRate / kernel->sampleRate);
+                float variation = sinf((kernel->currentRunningIndex + frameIndex) * 2 * 2 * M_PI * kernel->vibratoRate / kernel->getSampleRate());
                 phs->freq = bentFrequency * powf(2, depth * variation);
 
-                sp_adsr_compute(kernel->sp, adsr, &internalGate, &amp);
+                sp_adsr_compute(kernel->getSpData(), adsr, &internalGate, &amp);
 
-                sp_phasor_compute(kernel->sp, phs, NULL, &ph);
-                sp_pdhalf_compute(kernel->sp, pdhalf, &ph, &pd);
+                sp_phasor_compute(kernel->getSpData(), phs, NULL, &ph);
+                sp_pdhalf_compute(kernel->getSpData(), pdhalf, &ph, &pd);
                 tab->index = pd;
-                sp_tabread_compute(kernel->sp, tab, NULL, &temp);
+                sp_tabread_compute(kernel->getSpData(), tab, NULL, &temp);
 
                 *outL++ += velocityAmp * amp * temp;
                 *outR++ += velocityAmp * amp * temp;
@@ -147,8 +107,10 @@ public:
 
     AKPhaseDistortionOscillatorBankDSPKernel() {
         noteStates.resize(128);
-        for (NoteState& state : noteStates) {
-            state.kernel = this;
+        for (auto& ns : noteStates)
+        {
+            ns.reset(new NoteState);
+            ns->kernel = this;
         }
     }
 
@@ -161,17 +123,11 @@ public:
         ftbl->tbl[index] = value;
     }
 
-    void reset() {
-        for (NoteState& state : noteStates) {
-            state.clear();
-        }
-        playingNotes = nullptr;
-        phaseDistortionRamper.reset();
+    void reset() override {
         AKBankDSPKernel::reset();
+        phaseDistortionRamper.reset();
     }
-
-    standardBankKernelFunctions()
-
+    
     void setPhaseDistortion(float value) {
         phaseDistortion = clamp(value, -1.0f, 1.0f);
         phaseDistortionRamper.setImmediate(phaseDistortion);
@@ -206,17 +162,15 @@ public:
         }
     }
 
-    standardHandleMIDI()
-
     void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) override {
 
         float *outL = (float *)outBufferListPtr->mBuffers[0].mData + bufferOffset;
         float *outR = (float *)outBufferListPtr->mBuffers[1].mData + bufferOffset;
 
         phaseDistortion = double(phaseDistortionRamper.getAndStep());
-        standardBankGetAndSteps()
+        standardBankGetAndSteps();
 
-        NoteState *noteState = playingNotes;
+        AKBankDSPKernel::NoteState *noteState = playingNotes;
         while (noteState) {
             noteState->run(frameCount, outL, outR);
             noteState = noteState->next;
@@ -232,16 +186,11 @@ public:
     // MARK: Member Variables
 
 private:
-    std::vector<NoteState> noteStates;
-
     sp_ftbl *ftbl;
     UInt32 ftbl_size = 4096;
-
     float phaseDistortion = 0.0;
 
 public:
-    NoteState *playingNotes = nullptr;
-
     ParameterRamper phaseDistortionRamper = 0.0;
 };
 
