3b273555f6db01825fa5cf7b69f6529a101080ee
Aurelius Prochazka <aurephd@gmail.com>
2018-12-22 13:02:53 -0800

    Fixed file naming

--- AudioKit/Common/Nodes/Mixing/Auto Panner/AKAutoPannerDSP.mm
+++ AudioKit/Common/Nodes/Mixing/Auto Panner/AKAutoPannerDSP.mm
@@ -1,17 +1,17 @@
 //
-//  AKPanner.mm
+//  AKAutoPannerDSP.mm
 //  AudioKit
 //
 //  Created by Aurelius Prochazka, revision history on Github.
 //  Copyright © 2018 AudioKit. All rights reserved.
 //
 
-#import "AKPannerDSP.hpp"
+#import "AKAutoPannerDSP.hpp"
 
 // "Constructor" function for interop with Swift
 
-extern "C" AKDSPRef createPannerDSP(int nChannels, double sampleRate) {
-    AKPannerDSP *dsp = new AKPannerDSP();
+extern "C" AKDSPRef createAutoPannerDSP(int nChannels, double sampleRate) {
+    AKAutoPannerDSP *dsp = new AKAutoPannerDSP();
     dsp->init(nChannels, sampleRate);
     return dsp;
 }
