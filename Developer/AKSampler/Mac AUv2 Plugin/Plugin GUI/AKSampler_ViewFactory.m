c17f029005213cca5661ce70ea2c06f17725f804
Adam Nemecek <adamnemecek@gmail.com>
2019-01-05 13:38:05 -0800

    m whitespace

--- Developer/AKSampler/Mac AUv2 Plugin/Plugin GUI/AKSampler_ViewFactory.m
+++ Developer/AKSampler/Mac AUv2 Plugin/Plugin GUI/AKSampler_ViewFactory.m
@@ -26,15 +26,15 @@
         NSLog (@"Unable to load nib for view.");
         return nil;
     }
-    
+
     // This particular nib has a fixed size, so we don't do anything with the inPreferredSize argument.
     // It's up to the host application to handle.
     [uiFreshlyLoadedView setAU:inAU];
-    
+
     NSView *returnView = (NSView*)uiFreshlyLoadedView;
     uiFreshlyLoadedView = nil;    // zero out pointer.  This is a view factory.  Once a view's been created
     // and handed off, the factory keeps no record of it.
-    
+
     return [returnView autorelease];
 }
 
