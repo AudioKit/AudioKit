c417d8e57b0db35357db23142f180198e8c51dd1
Shane Dunne <shane@getdunne.com>
2019-01-10 17:09:29 -0500

    Rename AKSynth as AKCoreSynth, add Swift AU and Node classes

--- AudioKit/Common/Internals/CoreAudio/AK4/AKAudioUnitBase.h
+++ AudioKit/Common/Internals/CoreAudio/AK4/AKAudioUnitBase.h
@@ -18,7 +18,7 @@
 @interface AKAudioUnitBase : BufferedAudioUnit
 
 /** Pointer to AKDSPBase subclass. */
-@property (readonly) AKDSPRef dsp;
+@property (readonly) AKDSPRef _Nonnull dsp;
 
 /**
  This method should be overridden by the specific AU code, because it knows how to set up
@@ -27,7 +27,7 @@
  is. I'm not sure the standard way to deal with this.
  */
 
-- (AKDSPRef)initDSPWithSampleRate:(double) sampleRate channelCount:(AVAudioChannelCount) count;
+- (AKDSPRef _Nonnull )initDSPWithSampleRate:(double) sampleRate channelCount:(AVAudioChannelCount) count;
 
 /**
  Sets the parameter tree. The important piece here is that setting the parameter tree
@@ -53,7 +53,7 @@
 - (void)setWaveformValue:(float)value atIndex:(UInt32)index;
 
 // Convolution and Phase-Locked Vocoder
-- (void)setupAudioFileTable:(float *)data size:(UInt32)size;
+- (void)setupAudioFileTable:(float *_Nonnull)data size:(UInt32)size;
 - (void)setPartitionLength:(int)partitionLength;
 - (void)initConvolutionEngine;
 
