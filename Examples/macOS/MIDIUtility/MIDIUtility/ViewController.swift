55912ea883f6137b885ea5e8a6506c03d0d4f989
Jeff Cooper <jeff@southernworkstx.com>
2019-01-11 14:45:00 -0600

    midi utility prints sysex  padded correclty

--- Examples/macOS/MIDIUtility/MIDIUtility/ViewController.swift
+++ Examples/macOS/MIDIUtility/MIDIUtility/ViewController.swift
@@ -72,7 +72,7 @@ class ViewController: NSViewController, AKMIDIListener {
             updateText("")
             var newString = "MIDI System Command: \(command) \n"
             for i in 0 ..< data.count {
-                let hexValue = String(format: "%2X", data[i])
+                let hexValue = String(format: "%02X", data[i])
                 newString.append("\(hexValue) ")
             }
             updateText(newString)
