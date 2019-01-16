8f50eb56f520881a8ff6439106297031c763359c
Kurt Arnlund <kurtarnlund@Kurts-Thync-MacBook-Pro.local>
2019-01-16 10:57:36 -0800

    CI Houd Cleanups

--- Developer/macOS/macOSDevelopment/MacOSCommandLineDev/GeneralSysexCommunicationsManger.swift
+++ Developer/macOS/macOSDevelopment/MacOSCommandLineDev/GeneralSysexCommunicationsManger.swift
@@ -14,13 +14,13 @@ import AudioKit
 /// Since the external caller is responsible for what constitues succes,
 /// they are expected to call succeed() which will prevent timeout from
 /// happening.
-open class SuccessOrTimeoutMgr : NSObject {
-    private var onSuccess : (() -> Void)?
-    private var onTimeout : (() -> Void)?
-    let timeoutInterval : TimeInterval
+open class SuccessOrTimeoutMgr: NSObject {
+    private var onSuccess: (() -> Void)?
+    private var onTimeout: (() -> Void)?
+    let timeoutInterval: TimeInterval
 
-    init(timeoutInterval t: TimeInterval, success: @escaping () -> Void, timeout: @escaping () -> Void) {
-        timeoutInterval = t
+    init(timeoutInterval time: TimeInterval, success: @escaping () -> Void, timeout: @escaping () -> Void) {
+        timeoutInterval = time
         onSuccess = success
         onTimeout = timeout
         super.init()
@@ -45,12 +45,12 @@ open class SuccessOrTimeoutMgr : NSObject {
 
 /// A class responsible for sending sysex messages and informing a caller of
 /// reception of a sysex response, or a timeout error condition.
-open class GeneralSysexCommunicationsManger : AKMIDIListener {
+open class GeneralSysexCommunicationsManger: AKMIDIListener {
     static let ReceivedSysex = Notification.Name(rawValue: "ReceivedSysexNotification")
     static let SysexTimedOut = Notification.Name(rawValue: "SysexTimedOutNotification")
 
     let midi = AudioKit.midi
-    let K5000 = K5000messages()
+    let synthK5000 = K5000messages()
 
     /// Defaults to 44 seconds, which is just a bit longer than it takes
     /// the largest K5000 sysex messages to be received.
@@ -60,7 +60,7 @@ open class GeneralSysexCommunicationsManger : AKMIDIListener {
     init() {
         messageTimeout = SuccessOrTimeoutMgr(timeoutInterval: timeoutInterval, success: {
         NotificationCenter.default.post(name: GeneralSysexCommunicationsManger.ReceivedSysex, object: nil)
-        }) {
+        }) timeout: {
             NotificationCenter.default.post(name: GeneralSysexCommunicationsManger.SysexTimedOut, object: nil)
         }
         midi.addListener(self)
@@ -71,18 +71,18 @@ open class GeneralSysexCommunicationsManger : AKMIDIListener {
     public func requestAndWaitForResponse() {
         messageTimeout.performWithTimeout( {
             // Very fast requests
-            let sysexMessage = K5000.oneSingleAreaA(channel: .channel0, patch: 0)
-//            let sysexMessage = K5000.oneCombinationAreaC(channel: .channel0, combi: 0)
-//            let sysexMessage = K5000.oneSingleAreaD(channel: .channel0, patch: 0)
-//            let sysexMessage = K5000.oneSingleAreaE(channel: .channel0, patch: 0)
-//            let sysexMessage = K5000.oneSingleAreaF(channel: .channel0, patch: 0)
+            let sysexMessage = synthK5000.oneSingleAreaA(channel: .channel0, patch: 0)
+//            let sysexMessage = synthK5000.oneCombinationAreaC(channel: .channel0, combi: 0)
+//            let sysexMessage = synthK5000.oneSingleAreaD(channel: .channel0, patch: 0)
+//            let sysexMessage = synthK5000.oneSingleAreaE(channel: .channel0, patch: 0)
+//            let sysexMessage = synthK5000.oneSingleAreaF(channel: .channel0, patch: 0)
 
             // Very slow requests
-//            let sysexMessage = K5000.blockSingleAreaA(channel: .channel0)
-//            let sysexMessage = K5000.blockCombinationAreaC(channel: .channel0)
-//            let sysexMessage = K5000.blockSingleAreaD(channel: .channel0)
-//            let sysexMessage = K5000.blockSingleAreaE(channel: .channel0)
-//            let sysexMessage = K5000.blockSingleAreaF(channel: .channel0)
+//            let sysexMessage = synthK5000.blockSingleAreaA(channel: .channel0)
+//            let sysexMessage = synthK5000.blockCombinationAreaC(channel: .channel0)
+//            let sysexMessage = synthK5000.blockSingleAreaD(channel: .channel0)
+//            let sysexMessage = synthK5000.blockSingleAreaE(channel: .channel0)
+//            let sysexMessage = synthK5000.blockSingleAreaF(channel: .channel0)
 
             midi.sendMessage(sysexMessage)
         })
