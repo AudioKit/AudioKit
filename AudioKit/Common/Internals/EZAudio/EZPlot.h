833a6a197c8242778f8558ac3f75d026f9c723e4
Tomasz Dubik <tdubik@gmail.com>
2019-01-29 07:42:08 +0100

    Revert "Removing EZAudioUtilities header from EZPlot UI file. Fixing #1454"
    This reverts commit 6b1837af9502d32b31f3b60ceb4cff87696b00c8.

--- AudioKit/Common/Internals/EZAudio/EZPlot.h
+++ AudioKit/Common/Internals/EZAudio/EZPlot.h
@@ -24,6 +24,7 @@
 //  THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
+#import "AudioKit/EZAudioUtilities.h"
 
 //------------------------------------------------------------------------------
 #pragma mark - Enumerations
