dcfbbb98058425e43af23b9df69fd9794ecc34d5
Matteo Caldari <mail@matteocaldari.com>
2019-02-14 16:42:34 +0100

    Fixed MIDI callbacks not reaching the AKAudioUnitIntrument

--- AudioKit/Common/Nodes/Generators/Polysynths/FM Oscillator Bank/AKFMOscillatorBank.swift
+++ AudioKit/Common/Nodes/Generators/Polysynths/FM Oscillator Bank/AKFMOscillatorBank.swift
@@ -297,7 +297,10 @@ open class AKFMOscillatorBank: AKPolyphonicNode, AKComponent {
     // MARK: - AKPolyphonic
 
     // Function to start, play, or activate the node at frequency
-    open override func play(noteNumber: MIDINoteNumber, velocity: MIDIVelocity, frequency: Double) {
+    open override func play(noteNumber: MIDINoteNumber,
+                            velocity: MIDIVelocity,
+                            frequency: Double,
+                            channel: MIDIChannel = 0) {
         internalAU?.startNote(noteNumber, velocity: velocity, frequency: Float(frequency))
     }
 
