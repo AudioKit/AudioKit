45be9e013512a75ae8a7ff395b166a67ac50e202
Matteo Caldari <mail@matteocaldari.com>
2019-02-14 16:42:34 +0100

    Fixed MIDI callbacks not reaching the AKAudioUnitIntrument

--- AudioKit/Common/Nodes/Generators/Polysynths/Phase Distortion Oscillator Bank/AKPhaseDistortionOscillatorBank.swift
+++ AudioKit/Common/Nodes/Generators/Polysynths/Phase Distortion Oscillator Bank/AKPhaseDistortionOscillatorBank.swift
@@ -248,7 +248,10 @@ open class AKPhaseDistortionOscillatorBank: AKPolyphonicNode, AKComponent {
     // MARK: - AKPolyphonic
 
     // Function to start, play, or activate the node at frequency
-    open override func play(noteNumber: MIDINoteNumber, velocity: MIDIVelocity, frequency: Double) {
+    open override func play(noteNumber: MIDINoteNumber,
+                            velocity: MIDIVelocity,
+                            frequency: Double,
+                            channel: MIDIChannel = 0) {
         internalAU?.startNote(noteNumber, velocity: velocity, frequency: Float(frequency))
     }
     /// Function to stop or bypass the node, both are equivalent
