af2b18350bb95c3da646d8783faf63af81ead252
Jeff Cooper <jeff@southernworkstx.com>
2019-03-25 17:51:46 -0500

    temporarily make chunk events public

--- AudioKit/Common/MIDI/MIDIFile/AKMIDITrack.swift
+++ AudioKit/Common/MIDI/MIDIFile/AKMIDITrack.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public struct AKMIDITrack {
 
-    var chunk: MIDIFileTrackChunk
+    public var chunk: MIDIFileTrackChunk
     
     public var events: [AKMIDIEvent] {
         return chunk.chunkEvents.compactMap({ AKMIDIEvent(fileEvent: $0) })
