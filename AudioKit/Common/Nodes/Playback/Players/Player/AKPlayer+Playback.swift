4ade6c98cb73feb30d8198a35779bf26d0e1a12a
Jacob Parker <jacobparker1992@gmail.com>
2019-04-15 12:10:28 +0100

    Update AKPlayer+Playback.swift

--- AudioKit/Common/Nodes/Playback/Players/Player/AKPlayer+Playback.swift
+++ AudioKit/Common/Nodes/Playback/Players/Player/AKPlayer+Playback.swift
@@ -263,6 +263,7 @@ extension AKPlayer {
             startTime = loop.start
             endTime = loop.end
             play()
+            loopCompletionHandler?()
             return
         }
         if pauseTime != nil {
