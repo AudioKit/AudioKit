e859467c99bd5e66ecbe30dce98cc7ebc18af737
Jeff Cooper <jeff@southernworkstx.com>
2019-05-08 16:12:21 -0500

    AKWavetable responds (poorly) to MIDI

--- AudioKit/Common/Nodes/Playback/Samplers/Wave Table/AKWaveTableDSPKernel.hpp
+++ AudioKit/Common/Nodes/Playback/Samplers/Wave Table/AKWaveTableDSPKernel.hpp
@@ -8,6 +8,8 @@
 
 #pragma once
 #import "AKSoundpipeKernel.hpp"
+#import "AKWaveTableDSPKernel.hpp"
+#import "AKDSPKernel.hpp"
 
 enum {
     startPointAddress = 0,
@@ -313,6 +315,28 @@ public:
             loopCallback();
         }
     }
+
+
+    virtual void handleMIDIEvent(AUMIDIEvent const& midiEvent) override {
+        if (midiEvent.length != 3) return;
+        uint8_t status = midiEvent.data[0] & 0xF0;
+        switch (status) {
+            case 0x80 : { //note off
+                uint8_t note = midiEvent.data[1];
+                if (note > 127) break;
+                start();
+                break;
+            }
+            case 0x90 : { //note on
+                uint8_t note = midiEvent.data[1];
+                uint8_t veloc = midiEvent.data[2];
+                if (note > 127 || veloc > 127) break;
+                stop();
+                break;
+            }
+        }
+    }
+    
 private:
 
     sp_tabread *tabread1;
