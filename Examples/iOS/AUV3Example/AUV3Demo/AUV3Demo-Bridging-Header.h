4e0ab3275c53251298238c4664e687161dd9d5b8
Jeff Cooper <jeff@southernworkstx.com>
2019-06-26 13:00:11 -0500

    removes auv3 example files - this will be on a different branch

--- Examples/iOS/AUV3Example/AUV3Demo/AUV3Demo-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  AUV3Demo-Bridging-Header.h
-//  AUV3Demo
-//
-//  Created by Jeff Cooper on 5/16/19.
-//  Copyright © 2019 AudioKit. All rights reserved.
-//
-
-//#import "AUV3DemoAudioUnit.h" //Comment this out because we're going to do this in swift
