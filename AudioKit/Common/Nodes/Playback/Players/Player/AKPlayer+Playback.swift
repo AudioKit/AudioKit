dab59118f6649f9db41fa7e9e6f562725f3a4ba7
“Ryan <hi@spongefork.com>
2019-09-15 14:32:30 -0700

    changed the isPlaying flag to be set in AKPlayer via play / stop rather than rely on playerNode.isPlaying. I had that returning false when in fact it should of been true.

--- AudioKit/Common/Nodes/Playback/Players/Player/AKPlayer+Playback.swift
+++ AudioKit/Common/Nodes/Playback/Players/Player/AKPlayer+Playback.swift
@@ -89,10 +89,6 @@ extension AKPlayer {
 
     /// Stop playback and cancel any pending scheduled playback or completion events
     @objc public func stop() {
-        guard isPlaying else {
-            // AKLog("Player isn't playing")
-            return
-        }
         stopCompletion()
     }
 
@@ -128,6 +124,7 @@ extension AKPlayer {
     @objc private func autoFadeOutCompletion() {
         playerNode.stop()
         super.faderNode?.stopAutomation()
+        isPlaying = false
     }
 
     @objc private func stopCompletion() {
@@ -136,6 +133,8 @@ extension AKPlayer {
         if isFaded {
             super.faderNode?.stopAutomation()
         }
+
+        isPlaying = false
     }
 
     // MARK: - Scheduling
