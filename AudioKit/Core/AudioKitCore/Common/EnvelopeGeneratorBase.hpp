fb356434d7a28212f0e38ae07307e98af9e472ab
Shane Dunne <shane@getdunne.com>
2019-11-06 16:40:34 -0500

    Revert "AKSampler: ADSR generator always does A, D, R phases"
    This reverts commit a0c339adf8fdad6ff7ca09a9db0e7c6718a31ca1.

--- AudioKit/Core/AudioKitCore/Common/EnvelopeGeneratorBase.hpp
+++ AudioKit/Core/AudioKitCore/Common/EnvelopeGeneratorBase.hpp
@@ -24,7 +24,6 @@ namespace AudioKitCore
     {
     public:
         void reset(double initialValue, double targetValue, double tco, int segmentLengthSamples);
-        void setReleased(bool rel) { released = rel; }
 
         inline float getValue()
         {
@@ -36,7 +35,7 @@ namespace AudioKitCore
             if (isHorizontal)
             {
                 out = float(target);
-                if (segLength < 0) return released;     // non-timed "sustain"segment
+                if (segLength < 0) return false;        // non-timed "sustain"segment
                 else return (++tcount >= segLength);    // timed "hold" segment
             }
             else
@@ -58,7 +57,6 @@ namespace AudioKitCore
         bool isHorizontal;
         int tcount, segLength;
         bool isLinear;
-        bool released;
     };
 
     class MultiSegmentEnvelopeGenerator : public ExponentialSegmentGenerator
