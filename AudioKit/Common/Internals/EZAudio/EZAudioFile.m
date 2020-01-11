71e9c1721a9647e1856c61055ef0ac72d6b279a5
IlyaBlinov <cyberrenessance@gmail.com>
2020-01-11 15:31:46 +0300

    Update EZAudioFile.m
    add access to iPod library

--- AudioKit/Common/Internals/EZAudio/EZAudioFile.m
+++ AudioKit/Common/Internals/EZAudio/EZAudioFile.m
@@ -248,7 +248,7 @@ typedef struct {
     //
     // Create an ExtAudioFileRef for the file handle
     //
-    if (fileExists) {
+    if ((fileExists) || ([fileURL.absoluteString hasPrefix:@"ipod-library://"])) {
         [EZAudioUtilities checkResult:ExtAudioFileOpenURL(url, &self.info->extAudioFileRef)
                             operation:"Failed to create ExtAudioFileRef"];
     } else {
