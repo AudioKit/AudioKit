34848a4cc46c275d419b4727efcaf6a2f77a09c2
Jan Ratschko <jratschko@me.com>
2020-03-03 10:27:56 +0100

    some changes

--- AudioKit/Common/Internals/AKComponent.swift
+++ AudioKit/Common/Internals/AKComponent.swift
@@ -52,7 +52,7 @@ extension AudioComponentDescription {
         self.init(componentType: type,
                   componentSubType: subType,
                   componentManufacturer: fourCC("AuKt"),
-                  componentFlags: 0,
+                  componentFlags: AudioComponentFlags.sandboxSafe.rawValue,
                   componentFlagsMask: 0)
     }
 
