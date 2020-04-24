99d9b8590dd135c135300bd6b73861eb354e6f6f
Aurelius Prochazka <aurephd@gmail.com>
2020-04-23 17:36:06 -0700

    Updated copyright and other small changes

--- AudioKit/Common/Internals/Utilities/Circular Buffer/TPCircularBuffer+Unit.h
+++ AudioKit/Common/Internals/Utilities/Circular Buffer/TPCircularBuffer+Unit.h
@@ -1,9 +1,4 @@
-//
-//  TPCircularBuffer+Unit.h
-//
-//  Created by David O'Neill, revision history on Github.
-//  Copyright © 2017 O'Neill. All rights reserved.
-//
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
 
 #pragma once
 
