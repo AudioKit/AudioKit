4124b0b5e063418fc1ae7149acbfde162a20965b
Jon Latane <jonlatane@gmail.com>
2020-04-28 11:13:29 -0400

    use recommended fix

--- AudioKit/Core/AudioKitCore/Sampler/AKCoreSampler.cpp
+++ AudioKit/Core/AudioKitCore/Sampler/AKCoreSampler.cpp
@@ -331,8 +331,10 @@ void AKCoreSampler::play(unsigned noteNumber, unsigned velocity, bool anotherKey
         AudioKitCore::SamplerVoice *pVoice = voicePlayingNote(noteNumber);
         if (pVoice)
         {
+            AudioKitCore::KeyMappedSampleBuffer *pBuf = lookupSample(noteNumber, velocity);
+            if (pBuf == 0) return; // don't crash if someone forgets to build map
             // re-start the note
-            pVoice->restartSameNote(velocity / 127.0f, lookupSample(noteNumber, velocity));
+            pVoice->restartSameNote(velocity / 127.0f, pBuf);
             //printf("Restart note %d as %d\n", noteNumber, pVoice->noteNumber);
             return;
         }
