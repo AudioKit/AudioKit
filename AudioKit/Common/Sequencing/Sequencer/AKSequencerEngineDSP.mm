40f3b9b0b14c79af5c3d20ba77b64994adbf8cee
Aurelius Prochazka <aurephd@gmail.com>
2020-05-14 15:21:07 -0700

    added extern "C" to fix the linking issues

--- AudioKit/Common/Sequencing/Sequencer/AKSequencerEngineDSP.mm
+++ AudioKit/Common/Sequencing/Sequencer/AKSequencerEngineDSP.mm
@@ -6,30 +6,30 @@ extern "C" AKDSPRef createAKSequencerEngineDSP() {
     return new AKSequencerEngineDSP();
 }
 
-void sequencerEngineAddMIDIEvent(AKDSPRef dsp, uint8_t status, uint8_t data1, uint8_t data2, double beat) {
+extern "C" void sequencerEngineAddMIDIEvent(AKDSPRef dsp, uint8_t status, uint8_t data1, uint8_t data2, double beat) {
     ((AKSequencerEngineDSP*)dsp)->addMIDIEvent(status, data1, data2, beat);
 }
 
-void sequencerEngineAddMIDINote(AKDSPRef dsp, uint8_t noteNumber, uint8_t velocity, double beat, double duration) {
+extern "C" void sequencerEngineAddMIDINote(AKDSPRef dsp, uint8_t noteNumber, uint8_t velocity, double beat, double duration) {
     ((AKSequencerEngineDSP*)dsp)->addMIDINote(noteNumber, velocity, beat, duration);
 }
 
-void sequencerEngineRemoveMIDIEvent(AKDSPRef dsp, double beat) {
+extern "C" void sequencerEngineRemoveMIDIEvent(AKDSPRef dsp, double beat) {
     ((AKSequencerEngineDSP*)dsp)->removeEventAt(beat);
 }
 
-void sequencerEngineRemoveMIDINote(AKDSPRef dsp, double beat) {
+extern "C" void sequencerEngineRemoveMIDINote(AKDSPRef dsp, double beat) {
     ((AKSequencerEngineDSP*)dsp)->removeNoteAt(beat);
 }
 
-void sequencerEngineStopPlayingNotes(AKDSPRef dsp) {
+extern "C" void sequencerEngineStopPlayingNotes(AKDSPRef dsp) {
     ((AKSequencerEngineDSP*)dsp)->stopPlayingNotes();
 }
 
-void sequencerEngineClear(AKDSPRef dsp) {
+extern "C" void sequencerEngineClear(AKDSPRef dsp) {
     ((AKSequencerEngineDSP*)dsp)->clear();
 }
 
-void sequencerEngineSetAUTarget(AKDSPRef dsp, AudioUnit audioUnit) {
+extern "C" void sequencerEngineSetAUTarget(AKDSPRef dsp, AudioUnit audioUnit) {
     ((AKSequencerEngineDSP*)dsp)->setTargetAU(audioUnit);
 }
