fbb922cc7e404331f2d7b0b83ede06ca22edecb0
Aurelius Prochazka <aurephd@gmail.com>
2020-05-15 13:34:21 -0700

    REmoved HelloObjectiveC example project

--- Examples/iOS/HelloObjectiveC/HelloObjectiveC/Dummy.swift
+++
@@ -1,7 +0,0 @@
-// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
-
-import Foundation
-
-// Since Xcode 10.2 and iOS 12.2, you need to have at least one empty Swift file in your project, otherwise your project will not run on devices and simulators running iOS 12.2 and later.
-// This is a change in the way that Swift libraries are linked now that Swift 5 includes system-wide dylibs on the OS image itself.
-// There doesn't appear to be a way to fix Objective-C projects depending on a Swift 5 library (like AudioKit) besides including at least one Swift file in your project, even devoid of code.
--- Examples/iOS/HelloObjectiveC/HelloObjectiveC/ViewController.h
+++
@@ -1,7 +0,0 @@
-// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
-
-#import <UIKit/UIKit.h>
-
-@interface ViewController : UIViewController
-
-@end
