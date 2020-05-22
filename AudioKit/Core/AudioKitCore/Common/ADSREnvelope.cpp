cd3e718b6ff0536da50643d236ba6e5852cff403
Jeff Cooper <jeff@southernworkstx.com>
2020-05-22 14:17:04 -0500

    adapt adsrenvelope to new envelope mechanism

--- AudioKit/Core/AudioKitCore/Common/ADSREnvelope.cpp
+++ AudioKit/Core/AudioKitCore/Common/ADSREnvelope.cpp
@@ -94,7 +94,7 @@ namespace AudioKitCore
 
     void ADSREnvelope::start()
     {
-        env.advanceToSegment(kAttack);
+        env.startAtSegment(kAttack);
     }
 
     void ADSREnvelope::restart()
@@ -104,6 +104,7 @@ namespace AudioKitCore
 
     void ADSREnvelope::release()
     {
+        envDesc[kRelease].initialValue = env.getValue();
         env.advanceToSegment(kRelease);
     }
 
