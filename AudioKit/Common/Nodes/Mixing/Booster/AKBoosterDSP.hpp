df3b327a3704d7547abeb270bd5e2d2167a31498
Aurelius Prochazka <aurephd@gmail.com>
2020-05-29 20:24:56 -0700

    Updating Developer directory stuff for v5 (not completely done)

--- AudioKit/Common/Nodes/Mixing/Booster/AKBoosterDSP.hpp
+++ AudioKit/Common/Nodes/Mixing/Booster/AKBoosterDSP.hpp
@@ -17,13 +17,6 @@ AKDSPRef createBoosterDSP(void);
 
 #import "AKDSPBase.hpp"
 
-/**
- A simple DSP kernel. Most of the plumbing is in the base class. All the code at this
- level has to do is supply the core of the rendering code. A less trivial example would probably
- need to coordinate the updating of DSP parameters, which would probably involve thread locks,
- etc.
- */
-
 struct AKBoosterDSP : AKDSPBase {
 private:
     struct InternalData;
--- Developer/Common/SDBooster/SDBoosterAudioUnit.swift
+++ Developer/Common/SDBooster/SDBoosterAudioUnit.swift
@@ -5,55 +5,30 @@ import AudioKit
 
 public class SDBoosterAudioUnit: AKAudioUnitBase {
 
-    func setParameter(_ address: SDBoosterParameter, value: Double) {
-        setParameterWithAddress(AUParameterAddress(address.rawValue), value: Float(value))
+    let leftGain = AUParameter(
+        identifier: "leftGain",
+        name: "Left Boosting Amount",
+        address: 0,
+        range: 0.0...2.0,
+        unit: .linearGain,
+        flags: .default)
+
+    let rightGain = AUParameter(
+        identifier: "rightGain",
+        name: "Right Boosting Amount",
+        address: 1,
+        range: 0.0...2.0,
+        unit: .linearGain,
+        flags: .default)
+
+    public override func createDSP() -> AKDSPRef {
+        return createSDBoosterDSP()
     }
 
-    func setParameterImmediately(_ address: SDBoosterParameter, value: Double) {
-        setParameterImmediatelyWithAddress(AUParameterAddress(address.rawValue), value: Float(value))
-    }
-
-    var leftGain: Double = 1.0 {
-        didSet { setParameter(.leftGain, value: leftGain) }
-    }
-
-    var rightGain: Double = 1.0 {
-        didSet { setParameter(.rightGain, value: rightGain) }
-    }
-
-    var rampDuration: Double = 0.0 {
-        didSet { setParameter(.rampDuration, value: rampDuration) }
-    }
-
-    public override func initDSP(withSampleRate sampleRate: Double,
-                                 channelCount count: AVAudioChannelCount) -> AKDSPRef {
-        return createSDBoosterDSP(Int32(count), sampleRate)
-    }
-
-    override public init(componentDescription: AudioComponentDescription,
+    public override init(componentDescription: AudioComponentDescription,
                          options: AudioComponentInstantiationOptions = []) throws {
         try super.init(componentDescription: componentDescription, options: options)
 
-        let flags: AudioUnitParameterOptions = [.flag_IsReadable, .flag_IsWritable, .flag_CanRamp]
-        let leftGain = AUParameterTree.createParameter(withIdentifier: "leftGain",
-                                                       name: "Left Boosting Amount",
-                                                       address: AUParameterAddress(0),
-                                                       min: 0.0, max: 2.0,
-                                                       unit: .linearGain, unitName: nil,
-                                                       flags: flags,
-                                                       valueStrings: nil, dependentParameters: nil)
-        let rightGain = AUParameterTree.createParameter(withIdentifier: "rightGain",
-                                                        name: "Right Boosting Amount",
-                                                        address: AUParameterAddress(1),
-                                                        min: 0.0, max: 2.0,
-                                                        unit: .linearGain, unitName: nil,
-                                                        flags: flags,
-                                                        valueStrings: nil, dependentParameters: nil)
-        setParameterTree(AUParameterTree.createTree(withChildren: [leftGain, rightGain]))
-        leftGain.value = 1.0
-        rightGain.value = 1.0
+        parameterTree = AUParameterTree.createTree(withChildren: [leftGain, rightGain])
     }
-
-    public override var canProcessInPlace: Bool { return true; }
-
 }
