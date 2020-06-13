676f472c4cd433ad1f0640b83ee61ec193356960
Trevor <trevor.d@beydag.com>
2020-06-13 18:21:08 +0100

    midiUniqueID property added into EndpointInfo

--- AudioKit/Common/MIDI/AKMIDIEndpointInfo.swift
+++ AudioKit/Common/MIDI/AKMIDIEndpointInfo.swift
@@ -26,20 +26,29 @@ public struct EndpointInfo {
     /// Driver Owner
     public var driverOwner = ""
     
+    /// MIDIUniqueID
+    public var midiUniqueID:MIDIUniqueID
+    
+    /// MIDIEndpointRef
     public var midiEndpointRef:MIDIEndpointRef
+    
+    
 }
 
 extension Collection where Iterator.Element == MIDIEndpointRef {
     var endpointInfos: [EndpointInfo] {
         
-        return self.map { element -> EndpointInfo in
-            EndpointInfo(name:          getMIDIObjectStringProperty(ref: element, property: kMIDIPropertyName),
-                         displayName:   getMIDIObjectStringProperty(ref: element, property: kMIDIPropertyDisplayName),
-                         model:         getMIDIObjectStringProperty(ref: element, property: kMIDIPropertyModel),
-                         manufacturer:  getMIDIObjectStringProperty(ref: element, property: kMIDIPropertyManufacturer),
-                         image:         getMIDIObjectStringProperty(ref: element, property: kMIDIPropertyImage),
-                         driverOwner:   getMIDIObjectStringProperty(ref: element, property: kMIDIPropertyDriverOwner),
-                         midiEndpointRef: element as MIDIEndpointRef)
+        return self.map { (element:MIDIEndpointRef) -> EndpointInfo in
+            EndpointInfo(
+                name:          getMIDIObjectStringProperty(ref: element, property: kMIDIPropertyName),
+                displayName:   getMIDIObjectStringProperty(ref: element, property: kMIDIPropertyDisplayName),
+                model:         getMIDIObjectStringProperty(ref: element, property: kMIDIPropertyModel),
+                manufacturer:  getMIDIObjectStringProperty(ref: element, property: kMIDIPropertyManufacturer),
+                image:         getMIDIObjectStringProperty(ref: element, property: kMIDIPropertyImage),
+                driverOwner:   getMIDIObjectStringProperty(ref: element, property: kMIDIPropertyDriverOwner),
+                midiUniqueID:  getMIDIObjectIntegerProperty(ref: element, property: kMIDIPropertyUniqueID),
+                midiEndpointRef: element
+            )
         }
     }
 }
