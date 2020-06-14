5c0a2d358b18d8098e9c87fca60fb75701d76cf5
Ryan Francesconi <ryan@add.app>
2020-06-14 10:30:39 -0700

    Just moving a few things around and added a few extensions. No new functionality.

--- AudioKit/Common/Nodes/Playback/Players/Player/AKPlayer+Playback.swift
+++ AudioKit/Common/Nodes/Playback/Players/Player/AKPlayer+Playback.swift
@@ -275,3 +275,39 @@ extension AKPlayer {
         completionHandler?()
     }
 }
+
+@objc extension AKPlayer: AKTiming {
+    public func start(at audioTime: AVAudioTime?) {
+        play(at: audioTime)
+    }
+
+    public var isStarted: Bool {
+        return isPlaying
+    }
+
+    public func setPosition(_ position: Double) {
+        startTime = position
+        if isPlaying {
+            stop()
+            play()
+        }
+    }
+
+    public func position(at audioTime: AVAudioTime?) -> Double {
+        guard let playerTime = playerNode.playerTime(forNodeTime: audioTime ?? AVAudioTime.now()) else {
+            return startTime
+        }
+        return startTime + Double(playerTime.sampleTime) / playerTime.sampleRate
+    }
+
+    public func audioTime(at position: Double) -> AVAudioTime? {
+        let sampleRate = playerNode.outputFormat(forBus: 0).sampleRate
+        let sampleTime = (position - startTime) * sampleRate
+        let playerTime = AVAudioTime(sampleTime: AVAudioFramePosition(sampleTime), atRate: sampleRate)
+        return playerNode.nodeTime(forPlayerTime: playerTime)
+    }
+
+    open func prepare() {
+        preroll(from: startTime, to: endTime)
+    }
+}
