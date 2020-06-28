db427bca56b8f8381a6ca334675960c2705b37b9
Evan Murray <evanmurray22@gmail.com>
2020-06-27 20:59:43 -0500

    Fixed Trailing Whitespaces

--- AudioKit/iOS/AKMIDITrackView.swift
+++ AudioKit/iOS/AKMIDITrackView.swift
@@ -11,12 +11,17 @@ import Foundation
 import AudioKit
 
 /*
- This file contains the code for a horizontal MIDI sequencer view, similar to what you see in most Digital Audio Workstations.
- To add this view to your app, it's as simple as specifying the size/position of the view you would like, giving it a MIDI File URL and track number, populating the view with notes, adding the track to a parent view and playing the track!
+ This file contains the code for a horizontal MIDI sequencer view, similar to what you see 
+in most Digital Audio Workstations.
+ To add this view to your app, it's as simple as specifying the size/position of the view you 
+would like, giving it a MIDI File URL and track number, populating the view with notes, 
+adding the track to a parent view and playing the track!
  
  For example:
  
- var trackView1: AKMIDITrackView = AKMIDITrackView(frame: CGRect(x: , y: , width: , height: ), midiFile: AKMIDIFile(url: urltoyourmidifile), trackNumber: The MIDI track number you want to display)
+ var trackView1: AKMIDITrackView = AKMIDITrackView(frame: CGRect(x: , y: , width: , height: ), 
+midiFile: AKMIDIFile(url: urltoyourmidifile), 
+trackNumber: The MIDI track number you want to display)
  
  //Inside View Controller
  
@@ -26,17 +31,19 @@ import AudioKit
  
  self.trackView1.play()
  
- If you are using an AKAppleSampler or AKSampler, sequencer, etc, you will want to play the track directly before you play through the sequencer so the sound is synced with the playback.
+ If you are using an AKAppleSampler or AKSampler, sequencer, etc, 
+you will want to play the track directly before you play through the sequencer 
+so the sound is synced with the playback.
  
- This file is still in development. I have tried loading some forms of MIDI files, and they do not yet work. I also have not set up the playback to be synced with automated tempos which change over time. I am hoping to achieve this soon by using AudioKit's AKMIDITempoListener.
+ This file is still in development. I have tried loading some forms of MIDI files, and they do not yet work. 
+I also have not set up the playback to be synced with automated tempos which change over time. 
+I am hoping to achieve this soon by using AudioKit's AKMIDITempoListener.
  */
 
 //Display a MIDI Sequence in a track
 
 public class AKMIDITrackView: AKButton {
-    
     //Quarter note at 120 bpm is 20.833333333333333333... pixels - standard
-    
     var length: Double!
     var playbackCursorRect: CGRect!
     var playbackCursorView: UIView!
@@ -46,14 +53,14 @@ public class AKMIDITrackView: AKButton {
     var playbackCursorPosition: Double = 0.0
     var noteGroupPosition: Double = 0.0
     public var midiTrackNoteMap: AKMIDIFileTrackNoteMap!
-    
+ 
     var trackLength: Double {
         return midiTrackNoteMap.endOfTrack
     }
-    
+ 
     //How far the view is zoomed in
     public var noteZoomConstant: Double = 10000.0
-    
+ 
     /// Initialize the Track View
     public convenience init(frame: CGRect, midiFile: AKMIDIFile, trackNumber: Int) {
         self.init(frame: frame)
@@ -61,21 +68,21 @@ public class AKMIDITrackView: AKButton {
         clipsToBounds = true
         self.midiTrackNoteMap = AKMIDIFileTrackNoteMap(midiFile: midiFile, trackNum: trackNumber)
     }
-    
+ 
     /// Default init from superclass
     public override init(frame: CGRect) {
         super.init(frame: frame)
         self.borderWidth = 0.0
         clipsToBounds = true
     }
-    
+ 
     /// Initialization within Interface Builder
     public required init?(coder: NSCoder) {
         super.init(coder: coder)
         self.borderWidth = 0.0
         clipsToBounds = true
     }
-    
+ 
     //Cursor which displays for the first few seconds of the midi clip until it goes out of bounds
     public func play() {
         playbackCursorRect = CGRect(x: 0, y: 0, width: 3, height: Double(self.frame.height))
@@ -124,6 +131,7 @@ public class AKMIDITrackView: AKButton {
             collectiveNoteView.addSubview(singleNoteView)
         }
     }
+ 
     //Move the playback cursor across the screen
     @objc func updateCursor() {
         let width = Double(self.frame.size.width)
@@ -141,14 +149,10 @@ public class AKMIDITrackView: AKButton {
             cursorTimer.invalidate()
         }
     }
-    
+ 
     //Move the note view across the screen
     @objc func scrollNotes() {
         noteGroupPosition -= 1
         collectiveNoteView.frame.origin.x = CGFloat(noteGroupPosition)
-        if noteGroupPosition == (0 - Double(collectiveNoteView.frame.size.width)) {
-            //TODO: Fix this code so the timer actually gets invalidated
-            scrollTimer.invalidate()
-        }
-    }   
+    }
 }
