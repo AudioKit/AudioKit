6873e8f4251e11c2a8e5fa12ebdbb63808075819
Taylor Holliday <taylor@audulus.com>
2020-07-09 11:11:19 -0700

    Cleanup.

--- Developer/Common/SDBooster/SDBoosterDSP.mm
+++ Developer/Common/SDBooster/SDBoosterDSP.mm
@@ -8,9 +8,35 @@ private:
     ParameterRamper rightGainRamp;
 
 public:
-    SDBoosterDSP();
+    SDBoosterDSP() {
+        parameters[SDBoosterParameterLeftGain] = &leftGainRamp;
+        parameters[SDBoosterParameterRightGain] = &rightGainRamp;
 
-    void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) override;
+        bCanProcessInPlace = true;
+    }
+
+    void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) override {
+        for (int frameIndex = 0; frameIndex < frameCount; ++frameIndex) {
+            int frameOffset = int(frameIndex + bufferOffset);
+
+            float lgain = leftGainRamp.getAndStep();
+            float rgain = rightGainRamp.getAndStep();
+
+            for (int channel = 0; channel < channelCount; ++channel) {
+                float *in = (float *)inputBufferLists[0]->mBuffers[channel].mData  + frameOffset;
+                float *out = (float *)outputBufferLists[0]->mBuffers[channel].mData + frameOffset;
+                if (isStarted) {
+                    if (channel == 0) {
+                        *out = *in * lgain;
+                    } else {
+                        *out = *in * rgain;
+                    }
+                } else {
+                    *out = *in;
+                }
+            }
+        }
+    }
 };
 
 extern "C" AKDSPRef createSDBoosterDSP()
@@ -18,34 +44,3 @@ extern "C" AKDSPRef createSDBoosterDSP()
     return new SDBoosterDSP();
 }
 
-SDBoosterDSP::SDBoosterDSP()
-{
-    parameters[SDBoosterParameterLeftGain] = &leftGainRamp;
-    parameters[SDBoosterParameterRightGain] = &rightGainRamp;
-
-    bCanProcessInPlace = true;
-}
-
-void SDBoosterDSP::process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset)
-{
-    for (int frameIndex = 0; frameIndex < frameCount; ++frameIndex) {
-        int frameOffset = int(frameIndex + bufferOffset);
-
-        float lgain = leftGainRamp.getAndStep();
-        float rgain = rightGainRamp.getAndStep();
-
-        for (int channel = 0; channel < channelCount; ++channel) {
-            float *in = (float *)inputBufferLists[0]->mBuffers[channel].mData  + frameOffset;
-            float *out = (float *)outputBufferLists[0]->mBuffers[channel].mData + frameOffset;
-            if (isStarted) {
-                if (channel == 0) {
-                    *out = *in * lgain;
-                } else {
-                    *out = *in * rgain;
-                }
-            } else {
-                *out = *in;
-            }
-        }
-    }
-}
