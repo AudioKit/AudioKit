b85fb2fbb1b1088d1c66dd7c7ea8162571133dde
Jeff Cooper <jeff@southernworkstx.com>
2020-07-09 20:28:14 -0500

    add midiSysexMessage struct - this could be used in other places

---
+++ AudioKit/Common/MIDI/Utilities/MIDISysexMessage.swift
@@ -0,0 +1,30 @@
+//
+//  MIDISysexMessage.swift
+//  AudioKit
+//
+//  Created by Jeff Cooper on 7/9/20.
+//  Copyright © 2020 AudioKit. All rights reserved.
+//
+
+import Foundation
+
+public struct MIDISysexMessage: AKMIDIMessage {
+    public let data: [UInt8]
+    public let length: Int
+    public var description: String {
+        return "MIDI Sysex message \(length) bytes long"
+    }
+
+    init?(bytes: [UInt8]) {
+        guard
+            bytes.count > 2,
+            bytes[0] == 0xFF,
+            let vlqLength = MIDIVariableLengthQuantity(fromBytes: bytes.suffix(from: 1))
+        else {
+            return nil
+        }
+        self.data = Array(bytes.prefix(2 + Int(vlqLength.quantity))) //2 is for F0 and F7
+        self.length = Int(vlqLength.quantity)
+    }
+
+}
