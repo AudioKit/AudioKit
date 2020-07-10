8c422d0ed6ca7cb12f2f7a93c6ce868f3041da65
Aurelius Prochazka <aurephd@gmail.com>
2020-07-10 01:12:41 -0700

    Converted all oscillators to Taylor's syntax

--- AudioKit/Common/Nodes/Generators/Oscillators/PWM Oscillator/AKPWMOscillatorDSP.hpp
+++ AudioKit/Common/Nodes/Generators/Oscillators/PWM Oscillator/AKPWMOscillatorDSP.hpp
@@ -16,25 +16,4 @@ typedef NS_ENUM(AUParameterAddress, AKPWMOscillatorParameter) {
 
 AKDSPRef createPWMOscillatorDSP(void);
 
-#else
-
-#import "AKSoundpipeDSPBase.hpp"
-
-class AKPWMOscillatorDSP : public AKSoundpipeDSPBase {
-private:
-    struct InternalData;
-    std::unique_ptr<InternalData> data;
-
-public:
-    AKPWMOscillatorDSP();
-
-    void init(int channelCount, double sampleRate) override;
-
-    void deinit() override;
-
-    void reset() override;
-
-    void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) override;
-};
-
 #endif
