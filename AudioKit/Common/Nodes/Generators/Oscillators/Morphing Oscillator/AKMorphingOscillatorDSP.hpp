309d6fbd47d87b140ca081ee853b232af265a874
Aurelius Prochazka <aurephd@gmail.com>
2020-07-10 00:39:16 -0700

    DSP Simplification Taylor's way

--- AudioKit/Common/Nodes/Generators/Oscillators/Morphing Oscillator/AKMorphingOscillatorDSP.hpp
+++ AudioKit/Common/Nodes/Generators/Oscillators/Morphing Oscillator/AKMorphingOscillatorDSP.hpp
@@ -16,27 +16,4 @@ typedef NS_ENUM(AUParameterAddress, AKMorphingOscillatorParameter) {
 
 AKDSPRef createMorphingOscillatorDSP(void);
 
-#else
-
-#import "AKSoundpipeDSPBase.hpp"
-
-class AKMorphingOscillatorDSP : public AKSoundpipeDSPBase {
-private:
-    struct InternalData;
-    std::unique_ptr<InternalData> data;
- 
-public:
-    AKMorphingOscillatorDSP();
-
-    void setWavetable(const float* table, size_t length, int index) override;
-
-    void init(int channelCount, double sampleRate) override;
-
-    void deinit() override;
-
-    void reset() override;
-
-    void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) override;
-};
-
 #endif
