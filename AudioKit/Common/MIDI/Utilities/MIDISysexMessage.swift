12c832638b9e9409427304450ac1cc04e09bc262
Aurelius Prochazka <aurephd@gmail.com>
2020-07-11 13:12:29 -0700

    Fixed tvOS example

--- AudioKit/Common/MIDI/Utilities/MIDISysexMessage.swift
+++ AudioKit/Common/MIDI/Utilities/MIDISysexMessage.swift
@@ -1,10 +1,4 @@
-//
-//  MIDISysexMessage.swift
-//  AudioKit
-//
-//  Created by Jeff Cooper on 7/9/20.
-//  Copyright © 2020 AudioKit. All rights reserved.
-//
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
 
 import Foundation
 
