78f130318bf2d7cb0e77ce64749f0b2b14b4643b
Taylor Holliday <taylor@audulus.com>
2020-07-10 20:46:16 -0700

    Don't use interface header.

--- AudioKit/Common/Nodes/Generators/Polysynths/Filter Polysynths/PWM Filter Synth/AKPWMOscillatorFilterSynthAudioUnit.mm
+++ AudioKit/Common/Nodes/Generators/Polysynths/Filter Polysynths/PWM Filter Synth/AKPWMOscillatorFilterSynthAudioUnit.mm
@@ -1,7 +1,5 @@
 // Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
 
-#import <AudioKit/AudioKit-Swift.h>
-
 #import "AKPWMOscillatorFilterSynthAudioUnit.h"
 #import "AKPWMOscillatorFilterSynthDSPKernel.hpp"
 
