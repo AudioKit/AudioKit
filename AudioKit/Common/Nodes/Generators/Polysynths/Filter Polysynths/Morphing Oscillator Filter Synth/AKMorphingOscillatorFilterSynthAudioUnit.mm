a9637f06c3013ace0d14716f107b4d26f78e0ee7
Taylor Holliday <taylor@audulus.com>
2020-07-10 20:44:06 -0700

    Don't use interface header.

--- AudioKit/Common/Nodes/Generators/Polysynths/Filter Polysynths/Morphing Oscillator Filter Synth/AKMorphingOscillatorFilterSynthAudioUnit.mm
+++ AudioKit/Common/Nodes/Generators/Polysynths/Filter Polysynths/Morphing Oscillator Filter Synth/AKMorphingOscillatorFilterSynthAudioUnit.mm
@@ -1,7 +1,5 @@
 // Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
 
-#import <AudioKit/AudioKit-Swift.h>
-
 #import "AKMorphingOscillatorFilterSynthAudioUnit.h"
 #import "AKMorphingOscillatorFilterSynthDSPKernel.hpp"
 
