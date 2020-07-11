dbc8f96c04aa2319d1895b4f45839006e3db1d55
Aurelius Prochazka <aurephd@gmail.com>
2020-07-10 19:30:02 -0700

    More cleanup

--- AudioKit/Common/Nodes/Mixing/Stereo Field Limiter/AKStereoFieldLimiterDSP.mm
+++ AudioKit/Common/Nodes/Mixing/Stereo Field Limiter/AKStereoFieldLimiterDSP.mm
@@ -2,10 +2,6 @@
 
 #import "AKStereoFieldLimiterDSP.hpp"
 
-// "Constructor" function for interop with Swift
-// In this case a destructor is not needed, since the DSP object doesn't do any of
-// its own heap based allocation.
-
 extern "C" AKDSPRef createStereoFieldLimiterDSP() {
     return new AKStereoFieldLimiterDSP();
 }
