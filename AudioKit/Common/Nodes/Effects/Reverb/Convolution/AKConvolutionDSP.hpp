8a1ea9c51a68647ffe475d5d036039a1dbb512ac
Aurelius Prochazka <aurephd@gmail.com>
2020-07-10 19:52:21 -0700

    More Tayloresque cleanup

--- AudioKit/Common/Nodes/Effects/Reverb/Convolution/AKConvolutionDSP.hpp
+++ AudioKit/Common/Nodes/Effects/Reverb/Convolution/AKConvolutionDSP.hpp
@@ -10,29 +10,4 @@ AKDSPRef createConvolutionDSP(void);
 
 void setPartitionLengthConvolutionDSP(AKDSPRef dsp, int length);
 
-#else
-
-#import "AKSoundpipeDSPBase.hpp"
-
-class AKConvolutionDSP : public AKSoundpipeDSPBase {
-private:
-    struct InternalData;
-    std::unique_ptr<InternalData> data;
- 
-public:
-    AKConvolutionDSP();
-
-    void setPartitionLength(int partLength);
-
-    void setWavetable(const float* table, size_t length, int index) override;
-
-    void init(int channelCount, double sampleRate) override;
-
-    void deinit() override;
-
-    void reset() override;
-
-    void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) override;
-};
-
 #endif
