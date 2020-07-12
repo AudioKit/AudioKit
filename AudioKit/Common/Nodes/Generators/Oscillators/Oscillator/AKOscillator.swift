b9e12ca626be5b4aa318091a62cb0426f31e4e67
Aurelius Prochazka <aurephd@gmail.com>
2020-07-11 17:27:02 -0700

    Further simplification of AKOscillator, for purpose of template regen

--- AudioKit/Common/Nodes/Generators/Oscillators/Oscillator/AKOscillator.swift
+++ AudioKit/Common/Nodes/Generators/Oscillators/Oscillator/AKOscillator.swift
@@ -5,46 +5,17 @@
 ///
 open class AKOscillator: AKNode, AKToggleable, AKComponent, AKAutomatable {
 
-    // MARK: - AKComponent
-
-    /// Four letter unique description of the node
     public static let ComponentDescription = AudioComponentDescription(generator: "oscl")
 
     public typealias AKAudioUnitType = AKOscillatorAudioUnit
 
     public private(set) var internalAU: AKAudioUnitType?
 
-    // MARK: - AKAutomatable
-
     public private(set) var parameterAutomation: AKParameterAutomation?
 
     // MARK: - Parameters
 
     fileprivate var waveform: AKTable?
-
-    /// Lower and upper bounds for Frequency
-    public static let frequencyRange: ClosedRange<AUValue> = 0.0 ... 20_000.0
-
-    /// Lower and upper bounds for Amplitude
-    public static let amplitudeRange: ClosedRange<AUValue> = 0.0 ... 10.0
-
-    /// Lower and upper bounds for Detuning Offset
-    public static let detuningOffsetRange: ClosedRange<AUValue> = -1_000.0 ... 1_000.0
-
-    /// Lower and upper bounds for Detuning Multiplier
-    public static let detuningMultiplierRange: ClosedRange<AUValue> = 0.9 ... 1.11
-
-    /// Initial value for Frequency
-    public static let defaultFrequency: AUValue = 440.0
-
-    /// Initial value for Amplitude
-    public static let defaultAmplitude: AUValue = 1.0
-
-    /// Initial value for Detuning Offset
-    public static let defaultDetuningOffset: AUValue = 0.0
-
-    /// Initial value for Detuning Multiplier
-    public static let defaultDetuningMultiplier: AUValue = 1.0
 
     /// Frequency in cycles per second
     @Parameter public var frequency: AUValue
@@ -71,10 +42,10 @@ open class AKOscillator: AKNode, AKToggleable, AKComponent, AKAutomatable {
     ///
     public init(
         waveform: AKTable = AKTable(.sine),
-        frequency: AUValue = defaultFrequency,
-        amplitude: AUValue = defaultAmplitude,
-        detuningOffset: AUValue = defaultDetuningOffset,
-        detuningMultiplier: AUValue = defaultDetuningMultiplier
+        frequency: AUValue = 440,
+        amplitude: AUValue = 1,
+        detuningOffset: AUValue = 0,
+        detuningMultiplier: AUValue = 1
     ) {
         super.init(avAudioNode: AVAudioNode())
 
