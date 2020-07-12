6a649086246e9940a7593139931a8710013887d2
Aurelius Prochazka <aurephd@gmail.com>
2020-07-12 00:41:32 -0700

    Updating for @Parameters

--- AudioKit/Common/Nodes/Effects/Envelopes/Amplitude Envelope/AKAmplitudeEnvelope.swift
+++ AudioKit/Common/Nodes/Effects/Envelopes/Amplitude Envelope/AKAmplitudeEnvelope.swift
@@ -4,57 +4,29 @@
 ///
 open class AKAmplitudeEnvelope: AKNode, AKToggleable, AKComponent, AKInput, AKAutomatable {
 
-    // MARK: - AKComponent
-
-    /// Four letter unique description of the node
     public static let ComponentDescription = AudioComponentDescription(effect: "adsr")
 
     public typealias AKAudioUnitType = AKAmplitudeEnvelopeAudioUnit
 
     public private(set) var internalAU: AKAudioUnitType?
 
-    // MARK: - AKAutomatable
-
     public private(set) var parameterAutomation: AKParameterAutomation?
 
     // MARK: - Parameters
 
-    /// Lower and upper bounds for Attack Duration
-    public static let attackDurationRange: ClosedRange<AUValue> = 0 ... 99
-
-    /// Lower and upper bounds for Decay Duration
-    public static let decayDurationRange: ClosedRange<AUValue> = 0 ... 99
-
-    /// Lower and upper bounds for Sustain Level
-    public static let sustainLevelRange: ClosedRange<AUValue> = 0 ... 99
-
-    /// Lower and upper bounds for Release Duration
-    public static let releaseDurationRange: ClosedRange<AUValue> = 0 ... 99
-
-    /// Initial value for Attack Duration
-    public static let defaultAttackDuration: AUValue = 0.1
-
-    /// Initial value for Decay Duration
-    public static let defaultDecayDuration: AUValue = 0.1
-
-    /// Initial value for Sustain Level
-    public static let defaultSustainLevel: AUValue = 1.0
-
-    /// Initial value for Release Duration
-    public static let defaultReleaseDuration: AUValue = 0.1
-
     /// Attack time
-    public var attackDuration = AKNodeParameter(identifier: "attackDuration")
+    @Parameter public var attackDuration: AUValue
 
     /// Decay time
-    public var decayDuration = AKNodeParameter(identifier: "decayDuration")
+    @Parameter public var decayDuration: AUValue
 
     /// Sustain Level
-    public var sustainLevel = AKNodeParameter(identifier: "sustainLevel")
+    @Parameter public var sustainLevel: AUValue
 
     /// Release time
-    public var releaseDuration = AKNodeParameter(identifier: "releaseDuration")
+    @Parameter public var releaseDuration: AUValue
 
+    
     // MARK: - Initialization
 
     /// Initialize this envelope node
@@ -68,24 +40,22 @@ open class AKAmplitudeEnvelope: AKNode, AKToggleable, AKComponent, AKInput, AKAu
     ///
     public init(
         _ input: AKNode? = nil,
-        attackDuration: AUValue = defaultAttackDuration,
-        decayDuration: AUValue = defaultDecayDuration,
-        sustainLevel: AUValue = defaultSustainLevel,
-        releaseDuration: AUValue = defaultReleaseDuration
+        attackDuration: AUValue = 0.1,
+        decayDuration: AUValue = 0.1,
+        sustainLevel: AUValue = 1.0,
+        releaseDuration: AUValue = 0.1
         ) {
         super.init(avAudioNode: AVAudioNode())
-
+        self.attackDuration = attackDuration
+        self.decayDuration = decayDuration
+        self.sustainLevel = sustainLevel
+        self.releaseDuration = releaseDuration
         instantiateAudioUnit { avAudioUnit in
             self.avAudioUnit = avAudioUnit
             self.avAudioNode = avAudioUnit
 
             self.internalAU = avAudioUnit.auAudioUnit as? AKAudioUnitType
             self.parameterAutomation = AKParameterAutomation(avAudioUnit)
-
-            self.attackDuration.associate(with: self.internalAU, value: attackDuration)
-            self.decayDuration.associate(with: self.internalAU, value: decayDuration)
-            self.sustainLevel.associate(with: self.internalAU, value: sustainLevel)
-            self.releaseDuration.associate(with: self.internalAU, value: releaseDuration)
 
             input?.connect(to: self)
         }
