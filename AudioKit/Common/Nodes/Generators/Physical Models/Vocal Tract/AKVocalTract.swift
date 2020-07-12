690427e2cc4148c1c483245d4a522b6cee474449
Aurelius Prochazka <aurephd@gmail.com>
2020-07-11 18:03:47 -0700

    Updated AKVocalTract with @Parameters

--- AudioKit/Common/Nodes/Generators/Physical Models/Vocal Tract/AKVocalTract.swift
+++ AudioKit/Common/Nodes/Generators/Physical Models/Vocal Tract/AKVocalTract.swift
@@ -7,65 +7,30 @@
 ///
 open class AKVocalTract: AKNode, AKToggleable, AKComponent, AKAutomatable {
 
-    // MARK: - AKComponent
-
-    /// Four letter unique description of the node
     public static let ComponentDescription = AudioComponentDescription(generator: "vocw")
 
     public typealias AKAudioUnitType = AKVocalTractAudioUnit
 
     public private(set) var internalAU: AKAudioUnitType?
 
-    // MARK: - AKAutomatable
-
     public private(set) var parameterAutomation: AKParameterAutomation?
 
     // MARK: - Parameters
 
-    /// Lower and upper bounds for Frequency
-    public static let frequencyRange: ClosedRange<AUValue> = 0.0 ... 22_050.0
-
-    /// Lower and upper bounds for Tongue Position
-    public static let tonguePositionRange: ClosedRange<AUValue> = 0.0 ... 1.0
-
-    /// Lower and upper bounds for Tongue Diameter
-    public static let tongueDiameterRange: ClosedRange<AUValue> = 0.0 ... 1.0
-
-    /// Lower and upper bounds for Tenseness
-    public static let tensenessRange: ClosedRange<AUValue> = 0.0 ... 1.0
-
-    /// Lower and upper bounds for Nasality
-    public static let nasalityRange: ClosedRange<AUValue> = 0.0 ... 1.0
-
-    /// Initial value for Frequency
-    public static let defaultFrequency: AUValue = 160.0
-
-    /// Initial value for Tongue Position
-    public static let defaultTonguePosition: AUValue = 0.5
-
-    /// Initial value for Tongue Diameter
-    public static let defaultTongueDiameter: AUValue = 1.0
-
-    /// Initial value for Tenseness
-    public static let defaultTenseness: AUValue = 0.6
-
-    /// Initial value for Nasality
-    public static let defaultNasality: AUValue = 0.0
-
     /// Glottal frequency.
-    public var frequency = AKNodeParameter(identifier: "frequency")
+    @Parameter public var frequency: AUValue
 
     /// Tongue position (0-1)
-    public var tonguePosition = AKNodeParameter(identifier: "tonguePosition")
+    @Parameter public var tonguePosition: AUValue
 
     /// Tongue diameter (0-1)
-    public var tongueDiameter = AKNodeParameter(identifier: "tongueDiameter")
+    @Parameter public var tongueDiameter: AUValue
 
     /// Vocal tenseness. 0 = all breath. 1=fully saturated.
-    public var tenseness = AKNodeParameter(identifier: "tenseness")
+    @Parameter public var tenseness: AUValue
 
     /// Sets the velum size. Larger values of this creates more nasally sounds.
-    public var nasality = AKNodeParameter(identifier: "nasality")
+    @Parameter public var nasality: AUValue
 
     // MARK: - Initialization
 
@@ -79,13 +44,19 @@ open class AKVocalTract: AKNode, AKToggleable, AKComponent, AKAutomatable {
     ///   - nasality: Sets the velum size. Larger values of this creates more nasally sounds.
     ///
     public init(
-        frequency: AUValue = defaultFrequency,
-        tonguePosition: AUValue = defaultTonguePosition,
-        tongueDiameter: AUValue = defaultTongueDiameter,
-        tenseness: AUValue = defaultTenseness,
-        nasality: AUValue = defaultNasality
+        frequency: AUValue = 160.0,
+        tonguePosition: AUValue = 0.5,
+        tongueDiameter: AUValue = 1.0,
+        tenseness: AUValue = 0.6,
+        nasality: AUValue = 0.0
     ) {
         super.init(avAudioNode: AVAudioNode())
+
+        self.frequency = frequency
+        self.tonguePosition = tonguePosition
+        self.tongueDiameter = tongueDiameter
+        self.tenseness = tenseness
+        self.nasality = nasality
 
         instantiateAudioUnit { avAudioUnit in
             self.avAudioUnit = avAudioUnit
@@ -93,12 +64,6 @@ open class AKVocalTract: AKNode, AKToggleable, AKComponent, AKAutomatable {
 
             self.internalAU = avAudioUnit.auAudioUnit as? AKAudioUnitType
             self.parameterAutomation = AKParameterAutomation(avAudioUnit)
-
-            self.frequency.associate(with: self.internalAU, value: frequency)
-            self.tonguePosition.associate(with: self.internalAU, value: tonguePosition)
-            self.tongueDiameter.associate(with: self.internalAU, value: tongueDiameter)
-            self.tenseness.associate(with: self.internalAU, value: tenseness)
-            self.nasality.associate(with: self.internalAU, value: nasality)
         }
 
     }
