30136286f83439869457ac5614257c5ad2a74309
Ryan Francesconi <ryan@add.app>
2020-07-13 13:40:36 -0700

    updating recording for macOS

--- AudioKit/Common/Internals/Utilities/AKNodeRecorder.swift
+++ AudioKit/Common/Internals/Utilities/AKNodeRecorder.swift
@@ -15,7 +15,7 @@ open class AKNodeRecorder: NSObject {
 
     /// Duration of recording
     open var recordedDuration: Double {
-        return internalAudioFile.duration
+        return internalAudioFile?.duration ?? 0
     }
 
     /// If non-nil, attempts to apply this as the format of the specified output bus. This should
@@ -28,7 +28,7 @@ open class AKNodeRecorder: NSObject {
     open var recordFormat: AVAudioFormat?
 
     // The file to record to
-    private var internalAudioFile: AVAudioFile
+    private var internalAudioFile: AVAudioFile?
 
     /// The bus to install the recording tap on. Default is 0.
     private var bus: Int = 0
@@ -39,7 +39,8 @@ open class AKNodeRecorder: NSObject {
     /// return the AVAudioFile for reading
     open var audioFile: AVAudioFile? {
         do {
-            return try AVAudioFile(forReading: internalAudioFile.url)
+            guard let url = internalAudioFile?.url else { return nil }
+            return try AVAudioFile(forReading: url)
 
         } catch let error as NSError {
             AKLog("Error, Cannot create internal audio file for reading: \(error.localizedDescription)")
@@ -60,20 +61,21 @@ open class AKNodeRecorder: NSObject {
     ///   - bus: Integer index of the bus to use
     ///
     @objc public init(node: AKNode? = AKManager.output,
-                      file: AVAudioFile? = nil,
+                      file: AVAudioFile? = AKNodeRecorder.createTempFile(),
                       bus: Int = 0) throws {
-        guard let existingFile = file else {
-            internalAudioFile = AVAudioFile()
-            self.node = node
+        super.init()
+
+        guard let file = file else {
+            AKLog("Error, no file to write to")
             return
         }
 
         do {
             // We initialize AVAudioFile for writing (and check that we can write to)
-            internalAudioFile = try AVAudioFile(forWriting: existingFile.url,
-                                                settings: existingFile.fileFormat.settings)
+            internalAudioFile = try AVAudioFile(forWriting: file.url,
+                                                settings: file.fileFormat.settings)
         } catch let error as NSError {
-            AKLog("Error: cannot write to \(existingFile.url.lastPathComponent)")
+            AKLog("Error: cannot write to", file.url)
             throw error
         }
 
@@ -82,6 +84,20 @@ open class AKNodeRecorder: NSObject {
     }
 
     // MARK: - Methods
+
+    // Returns a CAF file in the NSTemporaryDirectory suitable for writing to via AKSettings.audioFormat
+    public static func createTempFile() -> AVAudioFile? {
+        let filename = UUID().uuidString + ".caf"
+        let url = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(filename)
+        var settings = AKSettings.audioFormat.settings
+        settings[AVLinearPCMIsNonInterleaved] = NSNumber(value: false)
+
+        AKLog("Creating temp file at", url)
+        return try? AVAudioFile(forWriting: url,
+                                settings: settings,
+                                commonFormat: AKSettings.audioFormat.commonFormat,
+                                interleaved: true)
+    }
 
     /// Start recording
     open func record() throws {
@@ -99,7 +115,7 @@ open class AKNodeRecorder: NSObject {
         isRecording = true
 
         // Note: if you install a tap on a bus that already has a tap it will crash your application.
-        AKLog("Recording using format \(internalAudioFile.processingFormat.debugDescription)")
+        AKLog("⏺ Recording using format", internalAudioFile?.processingFormat.debugDescription)
 
         // note, format should be nil as per the documentation for installTap:
         // "If non-nil, attempts to apply this as the format of the specified output bus. This should
@@ -112,6 +128,8 @@ open class AKNodeRecorder: NSObject {
     }
 
     private func process(buffer: AVAudioPCMBuffer, time: AVAudioTime) {
+        guard let internalAudioFile = internalAudioFile else { return }
+
         do {
             recordBufferDuration = Double(buffer.frameLength) / AKSettings.sampleRate
             try internalAudioFile.write(from: buffer)
@@ -150,6 +168,8 @@ open class AKNodeRecorder: NSObject {
             stop()
         }
 
+        guard let internalAudioFile = internalAudioFile else { return }
+
         // Delete the physical recording file
         let fileManager = FileManager.default
         let settings = internalAudioFile.processingFormat.settings
@@ -165,7 +185,7 @@ open class AKNodeRecorder: NSObject {
 
         // Creates a blank new file
         do {
-            internalAudioFile = try AVAudioFile(forWriting: url, settings: settings)
+            self.internalAudioFile = try AVAudioFile(forWriting: url, settings: settings)
             AKLog("File has been cleared")
         } catch let error as NSError {
             AKLog("Error: Can't record to" + url.lastPathComponent)
