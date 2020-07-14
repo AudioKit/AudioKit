cbd1145e4761c570d2cedd8141e996fcc93d3dc0
Aurelius Prochazka <aurephd@gmail.com>
2020-07-13 20:21:34 -0700

    Remove AKAudioFile for AKBooster test

--- Tests/TestCases/AKBoosterTests.swift
+++ Tests/TestCases/AKBoosterTests.swift
@@ -31,7 +31,7 @@ class AKBoosterTests: AKTestCase {
         let desktop = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first!
         let url = desktop.appendingPathComponent("TestOutput.aif")
         let settings: [String: Any] = [AVSampleRateKey: 44_100.0, AVNumberOfChannelsKey: 2]
-        let audioFile = try! AKAudioFile(forWriting: url, settings: settings)
+        let audioFile = try! AVAudioFile(forWriting: url, settings: settings)
         let osc = AKOscillator()
         let booster = AKBooster(osc, gain: 1.0)
         booster.rampDuration = 1
