abbc99ee38d8ac73455211df483a47db2e1c44a0
Taylor Holliday <taylor@audulus.com>
2020-07-16 11:37:14 -0700

    Add more tests.

--- Tests/TestCases/AKParameterAutomationTests.swift
+++ Tests/TestCases/AKParameterAutomationTests.swift
@@ -11,37 +11,68 @@ import AudioKit
 
 class AKParameterAutomationTests: XCTestCase {
 
-    func testSimpleAutomation() throws {
+    func observerTest(automation: [AKParameterAutomationPoint], sampleTime: Float64) -> ([AUParameterAddress], [AUValue]) {
 
         let address = AUParameterAddress(42)
-        let automationPoints = [ AKParameterAutomationPoint(targetValue: 880, startTime: 0, rampDuration: 1.0) ]
 
-        var addresses:[AUParameterAddress] = []
-        var values:[AUValue] = []
+        var addresses: [AUParameterAddress] = []
+        var values: [AUValue] = []
 
         let scheduleParameterBlock: AUScheduleParameterBlock = { (sampleTime, frameCount, address, value) in
             addresses.append(address)
             values.append(value)
         }
 
-        let observer: AURenderObserver = automationPoints.withUnsafeBufferPointer { automationPtr in
+        let observer: AURenderObserver = automation.withUnsafeBufferPointer { automationPtr in
             AKParameterAutomationGetRenderObserver(address,
                                                    scheduleParameterBlock,
                                                    44100,
                                                    0, // start time
                                                    1, // playback speed
                                                    automationPtr.baseAddress!,
-                                                   automationPoints.count)
+                                                   automation.count)
         }
 
         var timeStamp = AudioTimeStamp()
-        timeStamp.mSampleTime = 0
+        timeStamp.mSampleTime = sampleTime
 
         observer(.unitRenderAction_PreRender, &timeStamp, 256, 0)
+
+        return (addresses, values)
+    }
+
+    func testSimpleAutomation() throws {
+
+        let automationPoints = [ AKParameterAutomationPoint(targetValue: 880, startTime: 0, rampDuration: 1.0) ]
+
+        let (addresses, values) = observerTest(automation: automationPoints, sampleTime: 0)
 
         // order is: taper, skew, offset, value
         XCTAssertEqual(addresses, [9223372036854775850, 4611686018427387946, 2305843009213693994, 42])
         XCTAssertEqual(values, [1.0, 0.0, 0.0, 880.0])
     }
 
+    func testPastAutomation() {
+
+        let automationPoints = [ AKParameterAutomationPoint(targetValue: 880, startTime: 0, rampDuration: 0.1) ]
+
+        let (addresses, values) = observerTest(automation: automationPoints, sampleTime: 44100)
+
+        // If the automation is in the past, the value should be set to the final value.
+        XCTAssertEqual(addresses, [42])
+        XCTAssertEqual(values, [880.0])
+    }
+
+    func testPastAutomationTwo() {
+
+        let automationPoints = [ AKParameterAutomationPoint(targetValue: 880, startTime: 0, rampDuration: 0.1),
+                                 AKParameterAutomationPoint(targetValue: 440, startTime: 0.1, rampDuration: 0.1) ]
+
+        let (addresses, values) = observerTest(automation: automationPoints, sampleTime: 44100)
+
+        // If the automation is in the past, the value should be set to the final value.
+        XCTAssertEqual(addresses, [42, 42])
+        XCTAssertEqual(values, [880.0, 440.0])
+
+    }
 }
