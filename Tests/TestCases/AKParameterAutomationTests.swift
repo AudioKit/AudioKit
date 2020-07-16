ac6b3f4fd205a227310507cceb59114cb409ac43
Taylor Holliday <taylor@audulus.com>
2020-07-16 11:12:32 -0700

    Add test for AKParameterAutomationGetRenderObserver.

---
+++ Tests/TestCases/AKParameterAutomationTests.swift
@@ -0,0 +1,43 @@
+//
+//  AKParameterAutomationTests.swift
+//  iOSTestSuiteTests
+//
+//  Created by Taylor Holliday on 7/16/20.
+//  Copyright © 2020 AudioKit. All rights reserved.
+//
+
+import XCTest
+import AudioKit
+
+class AKParameterAutomationTests: XCTestCase {
+
+    func testSimpleAutomation() throws {
+
+        let address = AUParameterAddress(42)
+        let automationPoints = [ AKParameterAutomationPoint(targetValue: 880, startTime: 0, rampDuration: 1.0) ]
+
+        var addresses:[AUParameterAddress] = []
+
+        let scheduleParameterBlock: AUScheduleParameterBlock = { (sampleTime, frameCount, address, value) in
+            addresses.append(address)
+        }
+
+        let observer: AURenderObserver = automationPoints.withUnsafeBufferPointer { automationPtr in
+            AKParameterAutomationGetRenderObserver(address,
+                                                   scheduleParameterBlock,
+                                                   44100,
+                                                   0, // start time
+                                                   1, // playback speed
+                                                   automationPtr.baseAddress!,
+                                                   automationPoints.count)
+        }
+
+        var timeStamp = AudioTimeStamp()
+        timeStamp.mSampleTime = 0
+
+        observer(.unitRenderAction_PreRender, &timeStamp, 256, 0)
+
+        XCTAssertEqual(addresses, [9223372036854775850, 4611686018427387946, 2305843009213693994, 42])
+    }
+
+}
