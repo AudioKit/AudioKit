a6a82867081ae93ddc6c5b8810610340bec740a5
Stephane Peter <megastep@megastep.org>
2020-07-17 19:09:11 -0700

    Build fixes for XCframework without device builds

--- Frameworks/build_xcframework.sh
+++ Frameworks/build_xcframework.sh
@@ -12,13 +12,20 @@ create_xcframework()
 {
 	echo "Assembling xcframework for $1 ..."
 	rm -rf ${DESTINATION}/$1.xcframework # Start fresh
-	xcodebuild -create-xcframework -output ${DESTINATION}/$1.xcframework \
-		-framework "${BUILD_DIR}/${CONFIGURATION}-iphoneos/$1.framework" \
-		-framework "${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/$1.framework" \
-		-framework "${BUILD_DIR}/${CONFIGURATION}-appletvos/$1.framework" \
-		-framework "${BUILD_DIR}/${CONFIGURATION}-appletvsimulator/$1.framework" \
-		-framework "${BUILD_DIR}/${CONFIGURATION}/$1.framework" \
-        -framework "${BUILD_DIR}/Catalyst.xcarchive/Products/Library/Frameworks/$1.framework"
+	BASIC_OPTS="-framework ${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/$1.framework \
+		-framework ${BUILD_DIR}/${CONFIGURATION}-appletvsimulator/$1.framework \
+		-framework ${BUILD_DIR}/${CONFIGURATION}/$1.framework \
+        -framework ${BUILD_DIR}/Catalyst.xcarchive/Products/Library/Frameworks/$1.framework"
+
+	DEVICE_OPTS="-framework ${BUILD_DIR}/${CONFIGURATION}-iphoneos/$1.framework \
+		-framework ${BUILD_DIR}/${CONFIGURATION}-appletvos/$1.framework"
+
+	# Only include device frameworks for staging or release builds - or manual calls
+	if [[ "$GITHUB_REF" == "refs/heads/staging" ]] || [[ "$GITHUB_REF" == refs/tag/* ]] || [[ "$GITHUB_REF" == "" ]]; then
+		xcodebuild -create-xcframework -output ${DESTINATION}/$1.xcframework $BASIC_OPTS $DEVICE_OPTS
+	else
+		xcodebuild -create-xcframework -output ${DESTINATION}/$1.xcframework $BASIC_OPTS
+	fi
 	# OMFG, we need to manually unfuck the generated swift interface files. WTF!
 	find ${DESTINATION}/$1.xcframework -name "*.swiftinterface" -exec sed -i -e "s/$1\.//g" {} \;
 }
