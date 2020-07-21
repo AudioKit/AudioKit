0f5fbccf7d745ed88ba6811d11fe3a33c8771115
Taylor Holliday <taylor@audulus.com>
2020-07-21 13:57:50 -0700

    Add another test.

--- Tests/TestCases/AKPlayerTests.swift
+++ Tests/TestCases/AKPlayerTests.swift
@@ -11,14 +11,11 @@ import AudioKit
 
 class AKPlayerTests: AKTestCase {
 
+    let audioFileURL = Bundle.main.url(forResource: "sinechirp", withExtension: "wav")!
+
     func testBasic() {
 
-        guard let url = Bundle.main.url(forResource: "sinechirp", withExtension: "wav") else {
-            XCTFail()
-            return
-        }
-
-        guard let player = AKPlayer(url: url) else {
+        guard let player = AKPlayer(url: audioFileURL) else {
             XCTFail()
             return
         }
@@ -30,6 +27,27 @@ class AKPlayerTests: AKTestCase {
         output = player
 
         AKTestMD5("72ff03c8f6b529625877f89f4c7325bf")
+    }
+
+    func testAutomation() {
+
+        guard let player = AKPlayer(url: audioFileURL) else {
+            XCTFail()
+            return
+        }
+
+        afterStart = {
+            player.play()
+        }
+
+        output = player
+
+        player.fade.inTime = self.duration
+        player.faderNode?.leftGain = 0
+        player.faderNode?.rightGain = 0
+        player.faderNode?.addAutomationPoint(value: 1.0, at: 0.0, rampDuration: self.duration)
+
+        AKTestMD5("4ada6b1b67edc990324c041a22857acc")
     }
 
 }
