fde3357f3a12e2318c4f2b039a13bc389f7a529e
Taylor Holliday <taylor@audulus.com>
2020-07-25 16:57:15 -0700

    Fix delayed start with AKPlayer.

--- Tests/TestCases/AKPlayerTests.swift
+++ Tests/TestCases/AKPlayerTests.swift
@@ -52,7 +52,7 @@ class AKPlayerTests: AKTestCase {
         player.fade.inTime = 0.2
 
         //auditionTest()
-        AKTestMD5("70ace673dd8972652d490bacae67631e")
+        AKTestMD5("72ff03c8f6b529625877f89f4c7325bf")
     }
 
     func testFadeInOut() {
@@ -80,8 +80,8 @@ class AKPlayerTests: AKTestCase {
         player.endTime = 5
         duration = 5
 
-        // auditionTest()
-        AKTestMD5("aaf7406741627df074bf050839c269bb")
+        //auditionTest()
+        AKTestMD5("90ef5318b34510ef7c81f957046c06d6")
     }
 
     func testFadeOut() {
@@ -110,6 +110,35 @@ class AKPlayerTests: AKTestCase {
 
         // auditionTest()
         AKTestMD5("bbb8d6a862cf8974e0b3ec399a94bba0")
+    }
+
+    func testDelay() {
+
+        let bundle = Bundle(for: AKPlayerTests.self)
+
+        guard let audioFileURL = bundle.url(forResource: "PinkNoise", withExtension: "wav") else {
+            XCTFail("Couldn't find audio file.")
+            return
+        }
+
+        guard let player = AKPlayer(url: audioFileURL) else {
+            XCTFail("Couldn't load audio file.")
+            return
+        }
+
+        afterStart = {
+            player.play(at: AVAudioTime(sampleTime: 2 * 44100, atRate: 44100))
+        }
+
+        output = player
+        player.fade.inTime = 1.0
+        player.fade.outTime = 1.0
+        player.gain = 0.2
+        player.endTime = 3
+        duration = 5
+
+        // auditionTest()
+        AKTestMD5("3316cbd51ca69099e018280912bd02f1")
     }
 
 }
