9ea6465305836337616e3b9f888686eb8a4e1c6b
Aurelius Prochazka <aurephd@gmail.com>
2020-07-26 18:14:58 -0700

    Examples project progress

--- Examples/iOS+Catalyst/Examples/Examples/Views/PluckedStringOperation.swift
+++ Examples/iOS+Catalyst/Examples/Examples/Views/PluckedStringOperation.swift
@@ -1,5 +1,5 @@
 import AudioKit
-import Combine
+import SwiftUI
 
 class PluckedStringOperationConductor: Conductor, ObservableObject {
 
@@ -54,3 +54,26 @@ class PluckedStringOperationConductor: Conductor, ObservableObject {
 
 
 
+
+struct PluckedStringOperationView: View {
+    @ObservedObject var conductor = PluckedStringOperationConductor()
+
+    var body: some View {
+        VStack {
+            Text(self.conductor.isPlaying ? "STOP" : "START").onTapGesture {
+                self.conductor.isPlaying.toggle()
+            }
+//            Slider(value: self.$conductor.frequency, in: 220...880)
+
+        }.navigationBarTitle(Text("Plucked String Operation"))
+        .onAppear {
+            self.conductor.start()
+        }
+    }
+}
+
+struct PluckedStringOperationView_Previews: PreviewProvider {
+    static var previews: some View {
+        PluckedStringOperationView()
+    }
+}
