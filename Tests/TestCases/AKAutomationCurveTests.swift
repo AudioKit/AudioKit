381f9c58cc7dbafe6b33aaf56a776728877a0375
Taylor Holliday <taylor@audulus.com>
2020-07-29 07:04:25 -0700

    Cleanup.

--- Tests/TestCases/AKAutomationCurveTests.swift
+++ Tests/TestCases/AKAutomationCurveTests.swift
@@ -11,12 +11,14 @@ import AudioKit
 
 class AKAutomationCurveTests: AKTestCase {
 
+    typealias Point = AKParameterAutomationPoint
+
     func testReplaceAutomation() {
 
         {
-            let points = [ AKParameterAutomationPoint(targetValue: 440, startTime: 0, rampDuration: 0.1),
-                           AKParameterAutomationPoint(targetValue: 880, startTime: 1, rampDuration: 0.1),
-                           AKParameterAutomationPoint(targetValue: 440, startTime: 2, rampDuration: 0.1)]
+            let points = [ Point(targetValue: 440, startTime: 0, rampDuration: 0.1),
+                           Point(targetValue: 880, startTime: 1, rampDuration: 0.1),
+                           Point(targetValue: 440, startTime: 2, rampDuration: 0.1)]
 
             let events: [(Double, AUValue)] = [ (0.5, 100), (1.5, 200) ]
 
@@ -25,10 +27,10 @@ class AKAutomationCurveTests: AKTestCase {
                                                 startTime: 0.25,
                                                 stopTime: 1.75)
 
-            let expected = [ AKParameterAutomationPoint(targetValue: 440, startTime: 0, rampDuration: 0.1),
-                             AKParameterAutomationPoint(targetValue: 100, startTime: 0.5, rampDuration: 0.01),
-                             AKParameterAutomationPoint(targetValue: 200, startTime: 1.5, rampDuration: 0.01),
-                             AKParameterAutomationPoint(targetValue: 440, startTime: 2, rampDuration: 0.1)]
+            let expected = [ Point(targetValue: 440, startTime: 0, rampDuration: 0.1),
+                             Point(targetValue: 100, startTime: 0.5, rampDuration: 0.01),
+                             Point(targetValue: 200, startTime: 1.5, rampDuration: 0.01),
+                             Point(targetValue: 440, startTime: 2, rampDuration: 0.1)]
 
             XCTAssertEqual(newPoints.count, 4)
             XCTAssertEqual(newPoints, expected)
@@ -36,9 +38,9 @@ class AKAutomationCurveTests: AKTestCase {
 
 
         {
-            let points = [ AKParameterAutomationPoint(targetValue: 440, startTime: 0, rampDuration: 0.1),
-                           AKParameterAutomationPoint(targetValue: 880, startTime: 1, rampDuration: 0.1),
-                           AKParameterAutomationPoint(targetValue: 440, startTime: 2, rampDuration: 0.1)]
+            let points = [ Point(targetValue: 440, startTime: 0, rampDuration: 0.1),
+                           Point(targetValue: 880, startTime: 1, rampDuration: 0.1),
+                           Point(targetValue: 440, startTime: 2, rampDuration: 0.1)]
 
             let events: [(Double, AUValue)] = [ ]
 
@@ -51,7 +53,7 @@ class AKAutomationCurveTests: AKTestCase {
         }();
 
         {
-            let points: [AKParameterAutomationPoint] = [ ]
+            let points: [Point] = [ ]
 
             let events: [(Double, AUValue)] = [ (0.5, 100), (1.5, 200) ]
 
@@ -60,15 +62,15 @@ class AKAutomationCurveTests: AKTestCase {
                                                 startTime: 0,
                                                 stopTime: 2)
 
-            let expected = [ AKParameterAutomationPoint(targetValue: 100, startTime: 0.5, rampDuration: 0.01),
-                             AKParameterAutomationPoint(targetValue: 200, startTime: 1.5, rampDuration: 0.01)]
+            let expected = [ Point(targetValue: 100, startTime: 0.5, rampDuration: 0.01),
+                             Point(targetValue: 200, startTime: 1.5, rampDuration: 0.01)]
 
             XCTAssertEqual(newPoints, expected)
         }()
     }
 
     func testEvaluateAutomationLinear() {
-        let points = [AKParameterAutomationPoint(targetValue: 1, startTime: 0, rampDuration: 1.0)]
+        let points = [Point(targetValue: 1, startTime: 0, rampDuration: 1.0)]
 
         let newPoints = AKEvaluateAutomation(initialValue: 0, points: points, resolution: 0.5)
 
@@ -80,7 +82,7 @@ class AKAutomationCurveTests: AKTestCase {
 
     func testEvaluateAutomationAlmostLinear() {
 
-        let points = [AKParameterAutomationPoint(targetValue: 1, startTime: 0, rampDuration: 1.0, rampTaper: 1.0, rampSkew: 0.000001)]
+        let points = [Point(targetValue: 1, startTime: 0, rampDuration: 1.0, rampTaper: 1.0, rampSkew: 0.000001)]
 
         let newPoints = AKEvaluateAutomation(initialValue: 0,
                                              points: points,
@@ -95,7 +97,7 @@ class AKAutomationCurveTests: AKTestCase {
 
     func testEvaluateAutomationSlightTaper() {
 
-        let points = [AKParameterAutomationPoint(targetValue: 1, startTime: 0, rampDuration: 1.0, rampTaper: 1.00001, rampSkew: 0.0)]
+        let points = [Point(targetValue: 1, startTime: 0, rampDuration: 1.0, rampTaper: 1.00001, rampSkew: 0.0)]
 
         let newPoints = AKEvaluateAutomation(initialValue: 0,
                                              points: points,
@@ -110,7 +112,7 @@ class AKAutomationCurveTests: AKTestCase {
 
     func testEvaluateAutomationCurved() {
 
-        let points = [AKParameterAutomationPoint(targetValue: 1, startTime: 0, rampDuration: 1.0, rampTaper: 0.5, rampSkew: 0.1)]
+        let points = [Point(targetValue: 1, startTime: 0, rampDuration: 1.0, rampTaper: 0.5, rampSkew: 0.1)]
 
         let newPoints = AKEvaluateAutomation(initialValue: 0,
                                              points: points,
@@ -123,8 +125,8 @@ class AKAutomationCurveTests: AKTestCase {
     func testEvaluateAutomationTwoSegment() {
 
         // One linear, one curved segment.
-        let points = [AKParameterAutomationPoint(targetValue: 1, startTime: 0, rampDuration: 1.0),
-                      AKParameterAutomationPoint(targetValue: 0, startTime: 1.0, rampDuration: 1.0, rampTaper: 1.0, rampSkew: 0.000001)]
+        let points = [Point(targetValue: 1, startTime: 0, rampDuration: 1.0),
+                      Point(targetValue: 0, startTime: 1.0, rampDuration: 1.0, rampTaper: 1.0, rampSkew: 0.000001)]
 
         let newPoints = AKEvaluateAutomation(initialValue: 0,
                                              points: points,
@@ -144,8 +146,8 @@ class AKAutomationCurveTests: AKTestCase {
     func testEvaluateAutomationTwoSegment2() {
 
         // Curved segment cut off by linear segment.
-        let points = [AKParameterAutomationPoint(targetValue: 1, startTime: 0, rampDuration: 2.0, rampTaper: 1.0, rampSkew: 0.000001),
-                      AKParameterAutomationPoint(targetValue: 1, startTime: 1, rampDuration: 0.0)]
+        let points = [Point(targetValue: 1, startTime: 0, rampDuration: 2.0, rampTaper: 1.0, rampSkew: 0.000001),
+                      Point(targetValue: 1, startTime: 1, rampDuration: 0.0)]
 
         let newPoints = AKEvaluateAutomation(initialValue: 0,
                                              points: points,
