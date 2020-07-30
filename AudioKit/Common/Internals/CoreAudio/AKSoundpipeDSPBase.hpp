3cc4fbc710a377c8f19789cf017a5d0a5a4fbcac
Aurelius Prochazka <aurephd@gmail.com>
2020-07-30 15:51:31 -0700

    My attempt to include SoundpipeDSP in AudioKit.h, not working

--- AudioKit/Common/Internals/CoreAudio/AKSoundpipeDSPBase.hpp
+++ AudioKit/Common/Internals/CoreAudio/AKSoundpipeDSPBase.hpp
@@ -3,60 +3,11 @@
 #pragma once
 
 #import "AKDSPBase.hpp"
-#import "DSPKernel.hpp" // for the clamp
-#ifndef __cplusplus
 
-#include "soundpipe.h"
-#include "soundpipeextension.h"
-#include "vocwrapper.h"
-
-#else
-
-extern "C" {
-#include "soundpipe.h"
-#include "soundpipeextension.h"
-#include "vocwrapper.h"
-}
-
+#ifdef __cplusplus
 class AKSoundpipeDSPBase: public AKDSPBase {
-protected:
-    sp_data *sp = nullptr;
 public:
-    AKSoundpipeDSPBase() {
-        bCanProcessInPlace = true;
-    }
-    
-    virtual void init(int channelCount, double sampleRate) override {
-        AKDSPBase::init(channelCount, sampleRate);
-        sp_create(&sp);
-        sp->sr = sampleRate;
-        sp->nchan = channelCount;
-    }
-
-    virtual void deinit() override {
-        AKDSPBase::deinit();
-        sp_destroy(&sp);
-    }
-
-    virtual void processSample(int channel, float *in, float *out) {
-        *out = *in;
-    }
-
-    void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) override {
-        for (int frameIndex = 0; frameIndex < frameCount; ++frameIndex) {
-            int frameOffset = int(frameIndex + bufferOffset);
-            for (int channel = 0; channel <  channelCount; ++channel) {
-                float *in  = (float *)inputBufferLists[0]->mBuffers[channel].mData  + frameOffset;
-                float *out = (float *)outputBufferLists[0]->mBuffers[channel].mData + frameOffset;
-
-                if (isStarted) {
-                    processSample(channel, in, out);
-                } else {
-                    *out = *in;
-                }
-            }
-        }
-    }
+    AKSoundpipeDSPBase();
 };
 
 #endif
---
+++ AudioKit/Common/Internals/CoreAudio/AKSoundpipeDSPBase.mm
@@ -0,0 +1,50 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
+
+#import "AKSoundpipeDSPBase.hpp"
+
+#include "soundpipe.h"
+#include "soundpipeextension.h"
+#include "vocwrapper.h"
+
+#import "DSPKernel.hpp" // for the clamp
+
+class AKSoundpipeDSPBase: public AKDSPBase {
+protected:
+    sp_data *sp = nullptr;
+public:
+    AKSoundpipeDSPBase() {
+        bCanProcessInPlace = true;
+    }
+
+    virtual void init(int channelCount, double sampleRate) override {
+        AKDSPBase::init(channelCount, sampleRate);
+        sp_create(&sp);
+        sp->sr = sampleRate;
+        sp->nchan = channelCount;
+    }
+
+    virtual void deinit() override {
+        AKDSPBase::deinit();
+        sp_destroy(&sp);
+    }
+
+    virtual void processSample(int channel, float *in, float *out) {
+        *out = *in;
+    }
+
+    void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) override {
+        for (int frameIndex = 0; frameIndex < frameCount; ++frameIndex) {
+            int frameOffset = int(frameIndex + bufferOffset);
+            for (int channel = 0; channel <  channelCount; ++channel) {
+                float *in  = (float *)inputBufferLists[0]->mBuffers[channel].mData  + frameOffset;
+                float *out = (float *)outputBufferLists[0]->mBuffers[channel].mData + frameOffset;
+
+                if (isStarted) {
+                    processSample(channel, in, out);
+                } else {
+                    *out = *in;
+                }
+            }
+        }
+    }
+};
