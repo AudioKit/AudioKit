9a6b70b33d75cdb0a509a614e43b44d3132f806e
Taylor Holliday <taylor@audulus.com>
2020-07-31 15:43:58 -0700

    Add basic test for AKSequencerTrack.

---
+++ Tests/TestCases/AKSequencerTrackTests.swift
@@ -0,0 +1,33 @@
+//
+//  AKSequencerTrackTests.swift
+//  macOSTestSuiteTests
+//
+//  Created by Taylor Holliday on 7/31/20.
+//  Copyright © 2020 AudioKit. All rights reserved.
+//
+
+import XCTest
+import AudioKit
+
+class AKSequencerTrackTests: AKTestCase {
+
+    func testBasicSequence() throws {
+
+        duration = 1
+
+        let synth = AKOscillatorFilterSynth()
+
+        let seq = AKSequencerTrack(targetNode: synth)
+
+        output = AKMixer(synth, seq)
+
+        seq.add(noteNumber: 60, position: 0, duration: 0.1)
+        seq.add(noteNumber: 62, position: 0.1, duration: 0.1)
+        seq.add(noteNumber: 63, position: 0.2, duration: 0.1)
+
+        seq.playFromStart()
+        // auditionTest()
+        AKTestMD5("9bea8068185763c2b1a9970a916688fa")
+    }
+
+}
