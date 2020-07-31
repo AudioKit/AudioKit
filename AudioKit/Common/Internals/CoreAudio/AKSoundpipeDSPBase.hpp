66221ae40b826022792bf34d6083c12c1b1f97b0
Taylor Holliday <taylor@audulus.com>
2020-07-30 18:05:28 -0700

    Fix redundant definition of AKSoundpipeDSPBase.
    Keep soundpipe.h out of umbrella header.

--- AudioKit/Common/Internals/CoreAudio/AKSoundpipeDSPBase.hpp
+++ AudioKit/Common/Internals/CoreAudio/AKSoundpipeDSPBase.hpp
@@ -5,9 +5,19 @@
 #import "AKDSPBase.hpp"
 
 #ifdef __cplusplus
+
 class AKSoundpipeDSPBase: public AKDSPBase {
+protected:
+    struct sp_data *sp = nullptr;
 public:
-    AKSoundpipeDSPBase();
+    AKSoundpipeDSPBase() {
+        bCanProcessInPlace = true;
+    }
+
+    virtual void init(int channelCount, double sampleRate) override;
+    virtual void deinit() override;
+    virtual void processSample(int channel, float *in, float *out);
+    void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) override;
 };
 
 #endif
