29beccbb8d42430a4e0c1b48cca8308ab0cab5dd
Taylor Holliday <taylor@audulus.com>
2020-07-31 19:16:50 -0700

    Add testRemoveInstances.

--- Tests/TestCases/AKSequencerTrackTests.swift
+++ Tests/TestCases/AKSequencerTrackTests.swift
@@ -60,4 +60,25 @@ class AKSequencerTrackTests: AKTestCase {
         AKTestMD5("ec7e33775d8c926b2676a7002c123360")
     }
 
+    func testRemoveInstances() throws {
+
+        duration = 1
+
+        let synth = AKOscillatorFilterSynth()
+
+        let seq = AKSequencerTrack(targetNode: synth)
+
+        output = AKMixer(synth, seq)
+
+        seq.add(noteNumber: 60, position: 0, duration: 0.1)
+        seq.add(noteNumber: 62, position: 0.1, duration: 0.1)
+        seq.add(noteNumber: 63, position: 0.2, duration: 0.1)
+
+        seq.removeAllInstancesOf(noteNumber: 63)
+
+        seq.playFromStart()
+        // auditionTest()
+        AKTestMD5("a1c11d9faec613e1676d5db7e0a0f434")
+    }
+
 }
