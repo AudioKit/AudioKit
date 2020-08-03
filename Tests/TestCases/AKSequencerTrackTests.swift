f294658e7a7918a1e3f1738ed2c8f7ef0baeea84
Taylor Holliday <taylor@audulus.com>
2020-08-02 17:11:30 -0700

    Add testOneShot.

--- Tests/TestCases/AKSequencerTrackTests.swift
+++ Tests/TestCases/AKSequencerTrackTests.swift
@@ -107,6 +107,16 @@ class AKSequencerTrackTests: AKTestCase {
 
     }
 
+    func getTestSequence() -> AKSequence {
+
+        var seq = AKSequence()
+        seq.add(noteNumber: 60, position: 0, duration: 0.5)
+        seq.add(noteNumber: 62, position: 1, duration: 0.5)
+        seq.add(noteNumber: 63, position: 2, duration: 0.5)
+
+        return seq
+    }
+
     func testLoop() {
 
         duration = 5
@@ -117,15 +127,27 @@ class AKSequencerTrackTests: AKTestCase {
 
         output = AKMixer(track, synth)
 
-        var seq = AKSequence()
-        seq.add(noteNumber: 60, position: 0, duration: 0.5)
-        seq.add(noteNumber: 62, position: 1, duration: 0.5)
-        seq.add(noteNumber: 63, position: 2, duration: 0.5)
-
-        track.sequence = seq
+        track.sequence = getTestSequence()
         track.playFromStart()
         // auditionTest()
         AKTestMD5("c01d447a0f869a73f9ef82a9f2fa4607")
+    }
+
+    func testOneShot() {
+
+        duration = 5
+
+        let synth = AKOscillatorFilterSynth()
+
+        let track = AKSequencerTrack(targetNode: synth)
+
+        output = AKMixer(track, synth)
+
+        track.sequence = getTestSequence()
+        track.loopEnabled = false
+        track.playFromStart()
+        // auditionTest()
+        AKTestMD5("3553248d0221171d23aab45b4772c7b0")
     }
 
 }
