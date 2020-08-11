1fa4c41277668ff61a5bda5fd7a416db8fe6c6e7
Taylor Holliday <taylor@audulus.com>
2020-08-11 09:20:13 -0700

    Cleanup/modernize.

--- AudioKit/DevoloopAudioKit/Rhino/AKRhinoGuitarProcessorDSP.mm
+++ AudioKit/DevoloopAudioKit/Rhino/AKRhinoGuitarProcessorDSP.mm
@@ -7,21 +7,22 @@
 #include "Equalisator.h"
 #include "ParameterRamper.hpp"
 #include <math.h>
+#include <memory>
 
 class AKRhinoGuitarProcessorDSP : public AKDSPBase {
 private:
-    RageProcessor *leftRageProcessor;
-    RageProcessor *rightRageProcessor;
-    Equalisator *leftEqLo;
-    Equalisator *rightEqLo;
-    Equalisator *leftEqGtr;
-    Equalisator *rightEqGtr;
-    Equalisator *leftEqMi;
-    Equalisator *rightEqMi;
-    Equalisator *leftEqHi;
-    Equalisator *rightEqHi;
-    MikeFilter *mikeFilterL;
-    MikeFilter *mikeFilterR;
+    std::unique_ptr<RageProcessor> leftRageProcessor;
+    std::unique_ptr<RageProcessor> rightRageProcessor;
+    Equalisator leftEqLo;
+    Equalisator rightEqLo;
+    Equalisator leftEqGtr;
+    Equalisator rightEqGtr;
+    Equalisator leftEqMi;
+    Equalisator rightEqMi;
+    Equalisator leftEqHi;
+    Equalisator rightEqHi;
+    MikeFilter mikeFilterL;
+    MikeFilter mikeFilterR;
 
     ParameterRamper preGainRamper;
     ParameterRamper postGainRamper;
@@ -43,40 +44,18 @@ public:
     void init(int channelCount, double sampleRate) {
         AKDSPBase::init(channelCount, sampleRate);
 
-        leftEqLo = new Equalisator();
-        rightEqLo = new Equalisator();
-        leftEqGtr = new Equalisator();
-        rightEqGtr = new Equalisator();
-        leftEqMi = new Equalisator();
-        rightEqMi = new Equalisator();
-        leftEqHi = new Equalisator();
-        rightEqHi = new Equalisator();
-        mikeFilterL = new MikeFilter();
-        mikeFilterR = new MikeFilter();
+        leftRageProcessor = std::make_unique<RageProcessor>((int)sampleRate);
+        rightRageProcessor = std::make_unique<RageProcessor>((int)sampleRate);
 
-        leftRageProcessor = new RageProcessor((int)sampleRate);
-        rightRageProcessor = new RageProcessor((int)sampleRate);
-
-        mikeFilterL->calc_filter_coeffs(2500.f, sampleRate);
-        mikeFilterR->calc_filter_coeffs(2500.f, sampleRate);
+        mikeFilterL.calc_filter_coeffs(2500.f, sampleRate);
+        mikeFilterR.calc_filter_coeffs(2500.f, sampleRate);
     }
 
     void deinit() {
         AKDSPBase::deinit();
 
-        delete leftEqLo;
-        delete rightEqLo;
-        delete leftEqGtr;
-        delete rightEqGtr;
-        delete leftEqMi;
-        delete rightEqMi;
-        delete leftEqHi;
-        delete rightEqHi;
-        delete mikeFilterL;
-        delete mikeFilterR;
-
-        delete leftRageProcessor;
-        delete rightRageProcessor;
+        leftRageProcessor = nullptr;
+        rightRageProcessor = nullptr;
     }
 
     void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) {
@@ -91,14 +70,14 @@ public:
             float highGain = highGainRamper.getAndStep();
             float distortion = distortionRamper.getAndStep();
 
-            leftEqLo->calc_filter_coeffs(7, 120, sampleRate, 0.75, -2 * -lowGain, false);
-            rightEqLo->calc_filter_coeffs(7, 120, sampleRate, 0.75, -2 * -lowGain, false);
+            leftEqLo.calc_filter_coeffs(7, 120, sampleRate, 0.75, -2 * -lowGain, false);
+            rightEqLo.calc_filter_coeffs(7, 120, sampleRate, 0.75, -2 * -lowGain, false);
 
-            leftEqMi->calc_filter_coeffs(6, 2450, sampleRate, 1.7, 2.5 * midGain, true);
-            rightEqMi->calc_filter_coeffs(6, 2450, sampleRate, 1.7, 2.5 * midGain, true);
+            leftEqMi.calc_filter_coeffs(6, 2450, sampleRate, 1.7, 2.5 * midGain, true);
+            rightEqMi.calc_filter_coeffs(6, 2450, sampleRate, 1.7, 2.5 * midGain, true);
 
-            leftEqHi->calc_filter_coeffs(8, 6100, sampleRate, 1.6, -15 * -highGain, false);
-            rightEqHi->calc_filter_coeffs(8, 6100, sampleRate, 1.6, -15 * -highGain, false);
+            leftEqHi.calc_filter_coeffs(8, 6100, sampleRate, 1.6, -15 * -highGain, false);
+            rightEqHi.calc_filter_coeffs(8, 6100, sampleRate, 1.6, -15 * -highGain, false);
 
             float *tmpin[2];
             float *tmpout[2];
@@ -116,7 +95,7 @@ public:
 
                 *in = *in * preGain;
                 const float r_Sig = leftRageProcessor->doRage(*in, distortion * 2, distortion * 2);
-                const float e_Sig = leftEqLo->filter(leftEqMi->filter(leftEqHi->filter(r_Sig))) *
+                const float e_Sig = leftEqLo.filter(leftEqMi.filter(leftEqHi.filter(r_Sig))) *
                 (1 / (distortion*0.8));
                 *out = e_Sig * postGain;
             }
