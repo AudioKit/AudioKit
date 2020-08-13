afb75a3d7c82d3b44ecd6a02d92b8a588914c377
Taylor Holliday <taylor@audulus.com>
2020-08-12 21:27:57 -0700

    Add testAmplitude.

--- Tests/TestCases/AKFluteTests.swift
+++ Tests/TestCases/AKFluteTests.swift
@@ -22,4 +22,15 @@ class AKFluteTests: AKTestCase {
 
     }
 
+    func testAmplitude() {
+
+        let flute = AKFlute()
+        flute.trigger(frequency: 440, amplitude: 0.5)
+        output = flute
+
+        // auditionTest()
+        AKTestMD5("0e499f74ca4a5f4d93b28daed2b40b06")
+
+    }
+
 }
