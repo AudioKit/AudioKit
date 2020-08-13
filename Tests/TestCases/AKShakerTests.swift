bfa9663125e7c9bdbf4591e9f8f660a62bbc9fa7
Aurelius Prochazka <aurephd@gmail.com>
2020-08-12 19:30:33 -0700

    Various stk things

--- Tests/TestCases/AKShakerTests.swift
+++ Tests/TestCases/AKShakerTests.swift
@@ -1,10 +1,4 @@
-//
-//  AKShakerTests.swift
-//  iOSTestSuiteTests
-//
-//  Created by Taylor Holliday on 8/12/20.
-//  Copyright © 2020 AudioKit. All rights reserved.
-//
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
 
 import XCTest
 import AudioKit
