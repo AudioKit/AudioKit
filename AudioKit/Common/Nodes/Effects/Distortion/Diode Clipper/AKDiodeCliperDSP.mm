5e5c911d4fb25da09eaa45d08b3688adfa4bb11a
Taylor Holliday <taylor@audulus.com>
2020-08-14 13:44:22 -0700

    Work on AKDiodeClipperDSP.

--- AudioKit/Common/Nodes/Effects/Distortion/Diode Clipper/AKDiodeCliperDSP.mm
+++ AudioKit/Common/Nodes/Effects/Distortion/Diode Clipper/AKDiodeCliperDSP.mm
@@ -5,6 +5,51 @@
 #include "DiodeClipper.hpp"
 #include "AKSoulDSPBase.hpp"
 
-using AKDiodeClipperDSP = AKSoulDSPBase<Diode>;
+class AKDiodeClipperDSP : public AKDSPBase {
+
+public:
+    Diode patch;
+    std::vector<Diode::MIDIMessage> midiMessages;
+    
+    AKDiodeClipperDSP() {
+        // Reserve space for MIDI messages so we don't have to allocate.
+        midiMessages.reserve(1024);
+    }
+    
+    // Need to override this since it's pure virtual.
+    void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) override {
+        // Do nothing.
+    }
+    
+    void processWithEvents(AudioTimeStamp const *timestamp,
+                           AUAudioFrameCount frameCount,
+                           AURenderEvent const *events) override {
+        
+        Diode::RenderContext<float> context;
+        
+        midiMessages.clear();
+        
+        AURenderEvent const* event = events;
+        while(event) {
+            if(event->head.eventType == AURenderEventMIDI) {
+                Diode::MIDIMessage message;
+                message.frameIndex = event->head.eventSampleTime - timestamp->mSampleTime;
+                message.byte0 = event->MIDI.data[0];
+                message.byte1 = event->MIDI.data[1];
+                message.byte2 = event->MIDI.data[2];
+            }
+            event = event->head.next;
+        }
+        
+        context.numFrames = frameCount;
+        context.inputChannels[0] = (const float *)inputBufferLists[0]->mBuffers[0].mData;
+        context.outputChannels[0] = (float *)outputBufferList->mBuffers[0].mData;
+        
+        patch.render(context);
+        
+    }
+};
+
+// using AKDiodeClipperDSP = AKSoulDSPBase<Diode>;
 AK_REGISTER_DSP(AKDiodeClipperDSP)
 
