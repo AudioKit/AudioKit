d7ce9c1881efd9a1af943fd7c9f4f19fa31bf096
Taylor Holliday <taylor@audulus.com>
2020-08-14 08:38:19 -0700

    Formatting.

--- AudioKit/Common/Nodes/Generators/Physical Models/STK/Rhodes Piano/AKRhodesPiano.swift
+++ AudioKit/Common/Nodes/Generators/Physical Models/STK/Rhodes Piano/AKRhodesPiano.swift
@@ -4,28 +4,28 @@
 ///
 public class AKRhodesPiano: AKNode, AKToggleable, AKComponent, AKAutomatable {
 
-public static let ComponentDescription = AudioComponentDescription(generator: "flut")
+    public static let ComponentDescription = AudioComponentDescription(generator: "flut")
 
-public typealias AKAudioUnitType = InternalAU
+    public typealias AKAudioUnitType = InternalAU
 
-public private(set) var internalAU: AKAudioUnitType?
+    public private(set) var internalAU: AKAudioUnitType?
 
-public private(set) var parameterAutomation: AKParameterAutomation?
+    public private(set) var parameterAutomation: AKParameterAutomation?
 
-// MARK: - Parameters
+    // MARK: - Parameters
 
-public static let frequencyDef = AKNodeParameterDef(
-    identifier: "frequency",
-    name: "Frequency (Hz)",
-    address: akGetParameterAddress("AKRhodesPianoParameterFrequency"),
-    range: 0.0 ... 20_000.0,
-    unit: .hertz,
-    flags: .default)
+    public static let frequencyDef = AKNodeParameterDef(
+        identifier: "frequency",
+        name: "Frequency (Hz)",
+        address: akGetParameterAddress("AKRhodesPianoParameterFrequency"),
+        range: 0.0 ... 20_000.0,
+        unit: .hertz,
+        flags: .default)
 
-/// Variable frequency. Values less than the initial frequency will be doubled until it is greater than that.
-@Parameter public var frequency: AUValue
+    /// Variable frequency. Values less than the initial frequency will be doubled until it is greater than that.
+    @Parameter public var frequency: AUValue
 
-public static let amplitudeDef = AKNodeParameterDef(
+    public static let amplitudeDef = AKNodeParameterDef(
         identifier: "amplitude",
         name: "Amplitude",
         address: akGetParameterAddress("AKRhodesPianoParameterAmplitude"),
@@ -33,43 +33,43 @@ public static let amplitudeDef = AKNodeParameterDef(
         unit: .generic,
         flags: .default)
 
-/// Amplitude
-@Parameter public var amplitude: AUValue
+    /// Amplitude
+    @Parameter public var amplitude: AUValue
 
-public class InternalAU: AKAudioUnitBase {
+    public class InternalAU: AKAudioUnitBase {
 
-    public override func getParameterDefs() -> [AKNodeParameterDef] {
-        [AKRhodesPiano.frequencyDef,
-         AKRhodesPiano.amplitudeDef]
+        public override func getParameterDefs() -> [AKNodeParameterDef] {
+            [AKRhodesPiano.frequencyDef,
+             AKRhodesPiano.amplitudeDef]
+        }
+
+        public override func createDSP() -> AKDSPRef {
+            return akCreateDSP("AKRhodesPianoDSP")
+        }
     }
 
-    public override func createDSP() -> AKDSPRef {
-        return akCreateDSP("AKRhodesPianoDSP")
+    // MARK: - Initialization
+
+    /// Initialize the STK RhodesPiano model
+    ///
+    /// - Parameters:
+    ///   - frequency: Variable frequency. Values less than the initial frequency will be doubled until it is
+    ///                greater than that.
+    ///   - amplitude: Amplitude
+    ///
+    public init(frequency: AUValue = 440, amplitude: AUValue = 0.5) {
+        super.init(avAudioNode: AVAudioNode())
+        self.frequency = frequency
+        self.amplitude = amplitude
+
+        instantiateAudioUnit { avAudioUnit in
+            self.avAudioUnit = avAudioUnit
+            self.avAudioNode = avAudioUnit
+            self.internalAU = avAudioUnit.auAudioUnit as? AKAudioUnitType
+
+            self.parameterAutomation = AKParameterAutomation(avAudioUnit)
+        }
     }
-}
-
-// MARK: - Initialization
-
-/// Initialize the STK RhodesPiano model
-///
-/// - Parameters:
-///   - frequency: Variable frequency. Values less than the initial frequency will be doubled until it is
-///                greater than that.
-///   - amplitude: Amplitude
-///
-public init(frequency: AUValue = 440, amplitude: AUValue = 0.5) {
-    super.init(avAudioNode: AVAudioNode())
-    self.frequency = frequency
-    self.amplitude = amplitude
-
-    instantiateAudioUnit { avAudioUnit in
-        self.avAudioUnit = avAudioUnit
-        self.avAudioNode = avAudioUnit
-        self.internalAU = avAudioUnit.auAudioUnit as? AKAudioUnitType
-
-        self.parameterAutomation = AKParameterAutomation(avAudioUnit)
-    }
-}
 
     /// Trigger the sound with an optional set of parameters
     /// - Parameters:
