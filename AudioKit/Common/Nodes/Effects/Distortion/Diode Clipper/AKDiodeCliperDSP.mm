33b55e921a333fe4f13b189f2aacc072e2514f73
Taylor Holliday <taylor@audulus.com>
2020-08-14 14:14:08 -0700

    Work on AKDiodeClipperDSP.

--- AudioKit/Common/Nodes/Effects/Distortion/Diode Clipper/AKDiodeCliperDSP.mm
+++ AudioKit/Common/Nodes/Effects/Distortion/Diode Clipper/AKDiodeCliperDSP.mm
@@ -38,49 +38,42 @@ public:
         patch.reset();
     }
     
-    // Need to override this since it's pure virtual.
-    void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) override {
-        // Do nothing.
+    void handleMIDIEvent(AUMIDIEvent const& midiEvent) override {
+        Diode::MIDIMessage message;
+        message.frameIndex = 0;
+        message.byte0 = midiEvent.data[0];
+        message.byte1 = midiEvent.data[1];
+        message.byte2 = midiEvent.data[2];
+        midiMessages.push_back(message);
     }
     
-    void processWithEvents(AudioTimeStamp const *timestamp,
-                           AUAudioFrameCount frameCount,
-                           AURenderEvent const *events) override {
+    // Need to override this since it's pure virtual.
+    void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) override {
         
         // Zero output channels.
         for(int channel=0; channel<channelCount; ++channel) {
-            float* outputChanel = (float *)outputBufferList->mBuffers[channel].mData;
+            float* outputChanel = ((float *)outputBufferList->mBuffers[channel].mData) + bufferOffset;
             std::fill(outputChanel, outputChanel+frameCount, 0.0f);
         }
         
         Diode::RenderContext<float> context;
         
-        midiMessages.clear();
-        
-        AURenderEvent const* event = events;
-        while(event) {
-            if(event->head.eventType == AURenderEventMIDI) {
-                Diode::MIDIMessage message;
-                message.frameIndex = event->head.eventSampleTime - timestamp->mSampleTime;
-                message.byte0 = event->MIDI.data[0];
-                message.byte1 = event->MIDI.data[1];
-                message.byte2 = event->MIDI.data[2];
-                midiMessages.push_back(message);
-            }
-            event = event->head.next;
-        }
-        
         context.numFrames = frameCount;
-        context.inputChannels[0] = (const float *)inputBufferLists[0]->mBuffers[0].mData;
-        context.outputChannels[0] = (float *)outputBufferList->mBuffers[0].mData;
+        context.inputChannels[0] = ((const float *)inputBufferLists[0]->mBuffers[0].mData) + bufferOffset;
+        context.outputChannels[0] = ((float *)outputBufferList->mBuffers[0].mData) + bufferOffset;
         context.incomingMIDI.messages = midiMessages.data();
         context.incomingMIDI.numMessages = (uint32_t) midiMessages.size();
         
         patch.render(context);
+        
+        midiMessages.clear();
         
     }
 };
 
 // using AKDiodeClipperDSP = AKSoulDSPBase<Diode>;
 AK_REGISTER_DSP(AKDiodeClipperDSP)
+AK_REGISTER_PARAMETER(AKDiodeClipperParameterCutoff)
+AK_REGISTER_PARAMETER(AKDiodeClipperParameterGaindB)
+
 
