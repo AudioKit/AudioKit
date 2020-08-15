32b8883d888457e5ae2e349d155c0c59454a6d1f
Aurelius Prochazka <aurephd@gmail.com>
2020-08-14 21:31:10 -0700

    Basic chorus test (and update to AudioKit.h which should have been in last commit)

---
+++ Tests/TestCases/AKChorusTests.swift
@@ -0,0 +1,20 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
+
+import AudioKit
+
+class AKChorusTests: AKTestCase {
+
+    func testParameters() {
+        output = AKChorus(input,
+                          frequency: 1.1,
+                          depth: 0.8,
+                          feedback: 0.7,
+                          dryWetMix: 0.9)
+        AKTestMD5("35e3290ff7046179cc267153153b0e7e")
+    }
+
+//    func testDefault() {
+//        output = AKChorus(input)
+//        AKTestMD5("6ee413495f949542432eb00d32ecd903")
+//    }
+}
