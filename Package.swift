965836e5fdfa96cfe947ed098ac6e67e3031ceb2
Taylor Holliday <taylor@audulus.com>
2020-08-16 08:09:34 -0700

    Add target.

--- Package.swift
+++ Package.swift
@@ -18,9 +18,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(name: "CAudioKit"),
         .target(
             name: "AudioKit",
-            dependencies: []),
+            dependencies: ["CAudioKit"]),
         .testTarget(
             name: "AudioKitTests",
             dependencies: ["AudioKit"]),
