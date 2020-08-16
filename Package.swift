8c59a3d40edee9d098c4bf59ca35c789554b7619
Taylor Holliday <taylor@audulus.com>
2020-08-16 08:39:35 -0700

    Add soundpipe.

--- Package.swift
+++ Package.swift
@@ -18,8 +18,16 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(name: "Soundpipe",
+                cSettings: [
+                    .headerSearchPath("lib/dr_wav"),
+                    .headerSearchPath("lib/faust"),
+                    .headerSearchPath("lib/inih"),
+                    .headerSearchPath("lib/kissfft"),
+                    .define("NO_LIBSNDFILE")
+        ]),
         .target(name: "STK"),
-        .target(name: "CAudioKit", dependencies: ["STK"]),
+        .target(name: "CAudioKit", dependencies: ["STK", "Soundpipe"]),
         .target(
             name: "AudioKit",
             dependencies: ["CAudioKit"]),
