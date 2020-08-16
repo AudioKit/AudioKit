f3a3ea9fdae598a652c05b6085b7ab65d26f25a1
Aurelius Prochazka <aurephd@gmail.com>
2020-08-16 14:47:22 -0700

    Updated mm files

--- Sources/CAudioKit/Nodes/Effects/Delay/AKStereoDelayDSP.mm
+++ Sources/CAudioKit/Nodes/Effects/Delay/AKStereoDelayDSP.mm
@@ -1,6 +1,7 @@
 // Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
 
-#include "AudioKit.h"
+#include "AKSoundpipeDSPBase.hpp"
+#include "ParameterRamper.hpp"
 #include "StereoDelay.hpp"
 
 enum AKStereoDelayParameter : AUParameterAddress {
