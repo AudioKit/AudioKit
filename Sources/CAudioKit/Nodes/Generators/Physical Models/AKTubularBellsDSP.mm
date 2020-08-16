a69eb088b4859c1789954b818b47141ec5070f4c
Aurelius Prochazka <aurephd@gmail.com>
2020-08-16 14:25:11 -0700

    Physical model mm changes and moving headers to "include" folder

--- Sources/CAudioKit/Nodes/Generators/Physical Models/AKTubularBellsDSP.mm
+++ Sources/CAudioKit/Nodes/Generators/Physical Models/AKTubularBellsDSP.mm
@@ -1,6 +1,5 @@
 // Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
 
-#import "AudioKit.h"
 #import "AKSTKInstrumentDSP.hpp"
 
 #include "TubeBell.h"
