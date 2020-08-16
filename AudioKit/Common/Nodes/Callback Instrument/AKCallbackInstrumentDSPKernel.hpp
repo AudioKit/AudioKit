025a3196ae371fc54c1ffb287436b5f7b677c37c
Taylor Holliday <taylor@audulus.com>
2020-08-15 18:57:06 -0700

    Address thread safety issues.
    Call callback on the main thread.

--- AudioKit/Common/Nodes/Callback Instrument/AKCallbackInstrumentDSPKernel.hpp
+++ AudioKit/Common/Nodes/Callback Instrument/AKCallbackInstrumentDSPKernel.hpp
@@ -2,12 +2,29 @@
 
 #pragma once
 #import "AKSoundpipeKernel.hpp"
+#import "TPCircularBuffer.h"
 
 class AKCallbackInstrumentDSPKernel : public AKSoundpipeKernel, public AKOutputBuffered {
 public:
     // MARK: Member Functions
 
-    AKCallbackInstrumentDSPKernel() {}
+    TPCircularBuffer midiBuffer;
+    NSTimer* timer;
+
+    AKCallbackInstrumentDSPKernel() {
+        TPCircularBufferInit(&midiBuffer, 4096);
+        // Hopefully this polling interval is ok.
+        timer = [NSTimer scheduledTimerWithTimeInterval:0.01
+                                                repeats:true
+                                                  block:^(NSTimer * _Nonnull timer) {
+                 consumer();
+                 }];
+    }
+
+    ~AKCallbackInstrumentDSPKernel() {
+        TPCircularBufferCleanup(&midiBuffer);
+        [timer invalidate];
+    }
 
     void init(int _channels, double _sampleRate) override {
         AKSoundpipeKernel::init(_channels, _sampleRate);
@@ -55,11 +72,11 @@ public:
     }
 
     void startNote(int note, int velocity) {
-        doCallback(0x90, note, velocity);
+        send(0x90, note, velocity);
     }
 
     void stopNote(int note) {
-        doCallback(0x80, note, 0);
+        send(0x80, note, 0);
     }
 
     virtual void handleMIDIEvent(AUMIDIEvent const& midiEvent) override {
@@ -67,12 +84,29 @@ public:
         uint8_t status = midiEvent.data[0];
         uint8_t data1 = midiEvent.data[1];
         uint8_t data2 = midiEvent.data[2];
-        doCallback(status, data1, data2);
+        send(status, data1, data2);
     }
 
-    void doCallback(int status, int data1, int data2) {
-        if (callback != NULL){
-            callback(status, data1, data2);
+    void send(uint8_t status, uint8_t data1, uint8_t data2) {
+        uint8_t midi[3] = {status, data1, data2};
+        TPCircularBufferProduceBytes(&midiBuffer, midi, 3);
+    }
+
+    void consumer() {
+        int32_t availableBytes;
+        uint8_t* data = (uint8_t*) TPCircularBufferTail(&midiBuffer, &availableBytes);
+        if(data) {
+            int32_t messageCount = availableBytes / 3;
+
+            if(callback) {
+                for(int messageIndex=0; messageIndex < messageCount; ++messageIndex) {
+                    uint8_t status = data[3*messageIndex];
+                    uint8_t data1 = data[3*messageIndex+1];
+                    uint8_t data2 = data[3*messageIndex+2];
+                    callback(status, data1, data2);
+                }
+            }
+            TPCircularBufferConsume(&midiBuffer, messageCount * 3);
         }
     }
     
