b862162c615b73581370890e92508079baea8789
Taylor Holliday <taylor@audulus.com>
2020-08-16 08:26:26 -0700

    Ensure we can call into C++.

--- Sources/CAudioKit/test.cpp
+++ Sources/CAudioKit/test.cpp
@@ -1,2 +1,6 @@
 
-void test_func() { }
+#include "Stk.h"
+extern "C" void test_func() {
+    printf("Setting STK sample rate\n");
+    stk::Stk::setSampleRate(44100);
+}
--- Tests/AudioKitTests/AudioKitTests.swift
+++ Tests/AudioKitTests/AudioKitTests.swift
@@ -6,6 +6,7 @@ final class AudioKitTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
+        setStkSampleRate()
         XCTAssertEqual(AudioKit().text, "Hello, World!")
     }
 
