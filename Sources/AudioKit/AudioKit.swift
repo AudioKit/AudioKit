90a1c34704fc71433f525bd3f02807316e42d265
Taylor Holliday <taylor@audulus.com>
2020-08-16 21:05:43 -0700

    Import lots of stuff globally.

--- Sources/AudioKit/AudioKit.swift
+++ Sources/AudioKit/AudioKit.swift
@@ -1,4 +1,13 @@
-import CAudioKit
+
+// XXX: this is really bad
+@_exported import Foundation
+@_exported import CAudioKit
+@_exported import AVFoundation
+@_exported import AudioToolbox
+@_exported import OSLog
+@_exported import Soundpipe
+@_exported import CoreAudioKit
+@_exported import EZAudio
 
 struct AudioKit {
     var text = "Hello, World!"
