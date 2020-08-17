fc4ae1e1b6ca1143466f86534bfb5b4e2238cbe1
Taylor Holliday <taylor@audulus.com>
2020-08-17 11:59:41 -0700

    Hound.

--- Package.swift
+++ Package.swift
@@ -12,15 +12,13 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "AudioKit",
-            targets: ["AudioKit"]),
+            targets: ["AudioKit"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CAudioKit",
             dependencies: [],
@@ -49,7 +47,7 @@ let package = Package(
             dependencies: ["CAudioKit"]),
         .testTarget(
             name: "AudioKitTests",
-            dependencies: ["AudioKit"]),
+            dependencies: ["AudioKit"])
     ],
     cxxLanguageStandard: .cxx14
 )
