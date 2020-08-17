e49e6f384b291e4feabc7db7b419f5074f072474
Aurelius Prochazka <aurephd@gmail.com>
2020-08-16 19:49:46 -0700

    Added target platform for macOS

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "AudioKit",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
