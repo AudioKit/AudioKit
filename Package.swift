f1b53c100f60dee89f10846a1e41a53780bf24a3
Taylor Holliday <taylor@audulus.com>
2020-08-17 18:19:52 -0700

    Add platform.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AudioKit",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v10_15), .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
