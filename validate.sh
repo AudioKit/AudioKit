698f084cbd19511c0b56d879b5148001ede4de90
Taylor Holliday <taylor@audulus.com>
2020-08-18 17:14:34 -0700

    Add iOS Simulator to validation script.

--- validate.sh
+++ validate.sh
@@ -2,3 +2,4 @@
 set -e
 swift package generate-xcodeproj --xcconfig-overrides AudioKit.xcconfig
 xcodebuild -project AudioKit.xcodeproj -scheme AudioKit-Package clean test | xcpretty -c
+xcodebuild -project AudioKit.xcodeproj -scheme AudioKit-Package -destination 'platform=iOS Simulator,name=iPhone 11,OS=13.6' clean test | xcpretty -c
