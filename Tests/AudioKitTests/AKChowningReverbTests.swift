cb2a094a80086480546c7855d33a1e509bb0aacb
Aurelius Prochazka <aurephd@gmail.com>
2020-08-19 23:39:39 -0700

    Moved MD5s to their own file and stopped using useless Not tests

--- Tests/AudioKitTests/AKChowningReverbTests.swift
+++ Tests/AudioKitTests/AKChowningReverbTests.swift
@@ -4,12 +4,6 @@ import AudioKit
 
 class AKChowningReverbTests: AKTestCase {
 
-    func testActuallyProcessing() {
-        input = AKOscillator(waveform: AKTable(.square))
-        output = input
-        AKTestMD5Not("038cb0338d1615e9a5d7c2750f24e6da")
-    }
-
     func testDefault() {
         output = AKChowningReverb(input)
         AKTest()
--- Tests/AudioKitTests/AKDCBlockTests.swift
+++ Tests/AudioKitTests/AKDCBlockTests.swift
@@ -4,12 +4,6 @@ import AudioKit
 
 class AKDCBlockTests: AKTestCase {
 
-    func testActuallyProcessing() {
-        let input = AKOscillator(waveform: AKTable(.square))
-        output = input
-        AKTestMD5Not("cdca0c19d803bbf2cce357df5fca3013")
-    }
-
     func testDefault() {
         output = AKDCBlock(input)
         AKTest()
