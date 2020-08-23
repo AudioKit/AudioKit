ec50425a64283c586d67e67fae77c393e73ef8ad
Aurelius Prochazka <aurephd@gmail.com>
2020-08-23 00:00:44 -0700

    Converted AKOperationGenerator

--- Sources/CAudioKit/Operations/AKOperationGeneratorDSP.mm
+++ Sources/CAudioKit/Operations/AKOperationGeneratorDSP.mm
@@ -5,14 +5,15 @@
 #include "soundpipe.h"
 #include "plumber.h"
 
-class AKOperationEffectDSP : public AKSoundpipeDSPBase {
+class AKOperationGeneratorDSP : public AKSoundpipeDSPBase {
 private:
     plumber_data pd;
     char *sporthCode = nil;
     float params[14] = {0};
+    int internalTriggers[14] = {0};
 
 public:
-    AKOperationEffectDSP() {
+    AKOperationGeneratorDSP() {
         bCanProcessInPlace = false;
     }
 
@@ -31,10 +32,14 @@ public:
         return params;
     }
 
-    void setParameters(float* newParams) {
+    void setParameters(float newParams[]) {
         for (int i = 0; i < 14; i++) {
             params[i] = newParams[i];
         }
+    }
+
+    void trigger(int trigger) {
+        internalTriggers[trigger] = 1;
     }
 
     void init(int channelCount, double sampleRate) override {
@@ -71,16 +76,14 @@ public:
 
             int frameOffset = int(frameIndex + bufferOffset);
 
-            for (int channel = 0; channel < channelCount; ++channel) {
-                float *in  = (float *)inputBufferLists[0]->mBuffers[channel].mData  + frameOffset;
-                if (channel < 2) {
-                    pd.p[channel+14] = *in;
+            for (int i = 0; i < 14; i++) {
+                if (internalTriggers[i] == 1) {
+                    pd.p[i] = 1.0;
+                } else {
+                    pd.p[i] = params[i];
                 }
             }
 
-            for (int i = 0; i < 14; i++) {
-                pd.p[i] = params[i];
-            }
             plumber_compute(&pd, PLUMBER_COMPUTE);
 
             for (int channel = 0; channel < channelCount; ++channel) {
@@ -89,28 +92,38 @@ public:
             }
 
             for (int i = 0; i < 14; i++) {
+                if (internalTriggers[i] == 1) {
+                    pd.p[i] = 0.0;
+                }
                 params[i] = pd.p[i];
+                internalTriggers[i] = 0;
             }
         }
     }
 };
 
-AK_API void akOperationEffectSetSporth(AKDSPRef dspRef, const char *sporth, int length) {
-    auto dsp = dynamic_cast<AKOperationEffectDSP *>(dspRef);
+AK_API void akOperationGeneratorSetSporth(AKDSPRef dspRef, const char *sporth, int length) {
+    auto dsp = dynamic_cast<AKOperationGeneratorDSP *>(dspRef);
     assert(dsp);
     dsp->setSporth(sporth, length);
 }
 
-AK_API float* akOperationEffectGetParameters(AKDSPRef dspRef) {
-    auto dsp = dynamic_cast<AKOperationEffectDSP *>(dspRef);
+AK_API float* akOperationGeneratorGetParameters(AKDSPRef dspRef) {
+    auto dsp = dynamic_cast<AKOperationGeneratorDSP *>(dspRef);
     assert(dsp);
     dsp->getParameters();
 }
 
-AK_API float* akOperationEffectSetParameters(AKDSPRef dspRef, float *params) {
-    auto dsp = dynamic_cast<AKOperationEffectDSP *>(dspRef);
+AK_API float* akOperationGeneratorSetParameters(AKDSPRef dspRef, float *params) {
+    auto dsp = dynamic_cast<AKOperationGeneratorDSP *>(dspRef);
     assert(dsp);
     dsp->setParameters(params);
 }
 
-AK_REGISTER_DSP(AKOperationEffectDSP)
+AK_API float* akOperationGeneratorTrigger(AKDSPRef dspRef, int trigger) {
+    auto dsp = dynamic_cast<AKOperationGeneratorDSP *>(dspRef);
+    assert(dsp);
+    dsp->trigger(trigger);
+}
+
+AK_REGISTER_DSP(AKOperationGeneratorDSP)
