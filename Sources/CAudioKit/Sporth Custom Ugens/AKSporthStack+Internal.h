f416758090aaaa7a7c496028fae96496defcc53e
Aurelius Prochazka <aurephd@gmail.com>
2020-08-22 22:02:31 -0700

    Moved operation stuff up into its own directory and removed custom ugen stuff too

--- Sources/CAudioKit/Sporth Custom Ugens/AKSporthStack+Internal.h
+++
@@ -1,9 +0,0 @@
-// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
-
-#pragma once
-
-#import "AKSporthStack.h"
-
-@interface AKSporthStack (Internal)
-- (void)setStack:(sporth_stack *)stack;
-@end
