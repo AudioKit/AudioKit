0a890456f8ff82736e5760d8f2f1c4e145431dd0
Aurelius Prochazka <aurephd@gmail.com>
2020-08-24 14:01:58 -0700

    Replaced 1_000_000 ranges with float maxes

--- Sources/AudioKit/Operations/AKOperationGenerator.swift
+++ Sources/AudioKit/Operations/AKOperationGenerator.swift
@@ -24,98 +24,98 @@ public class AKOperationGenerator: AKNode, AKToggleable, AKComponent {
         identifier: "parameter1",
         name: "Parameter 1",
         address: akGetParameterAddress("AKOperationGeneratorParameter1"),
-        range: -1_000_000 ... 1_000_000,
+        range: floatRange,
         unit: .generic,
         flags: .default)
     public static let parameter2Def = AKNodeParameterDef(
         identifier: "parameter2",
         name: "Parameter 2",
         address: akGetParameterAddress("AKOperationGeneratorParameter2"),
-        range: -1_000_000 ... 1_000_000,
+        range: floatRange,
         unit: .generic,
         flags: .default)
     public static let parameter3Def = AKNodeParameterDef(
         identifier: "parameter3",
         name: "Parameter 3",
         address: akGetParameterAddress("AKOperationGeneratorParameter3"),
-        range: -1_000_000 ... 1_000_000,
+        range: floatRange,
         unit: .generic,
         flags: .default)
     public static let parameter4Def = AKNodeParameterDef(
         identifier: "parameter4",
         name: "Parameter 4",
         address: akGetParameterAddress("AKOperationGeneratorParameter4"),
-        range: -1_000_000 ... 1_000_000,
+        range: floatRange,
         unit: .generic,
         flags: .default)
     public static let parameter5Def = AKNodeParameterDef(
         identifier: "parameter5",
         name: "Parameter 5",
         address: akGetParameterAddress("AKOperationGeneratorParameter5"),
-        range: -1_000_000 ... 1_000_000,
+        range: floatRange,
         unit: .generic,
         flags: .default)
     public static let parameter6Def = AKNodeParameterDef(
         identifier: "parameter6",
         name: "Parameter 6",
         address: akGetParameterAddress("AKOperationGeneratorParameter6"),
-        range: -1_000_000 ... 1_000_000,
+        range: floatRange,
         unit: .generic,
         flags: .default)
     public static let parameter7Def = AKNodeParameterDef(
         identifier: "parameter7",
         name: "Parameter 7",
         address: akGetParameterAddress("AKOperationGeneratorParameter7"),
-        range: -1_000_000 ... 1_000_000,
+        range: floatRange,
         unit: .generic,
         flags: .default)
     public static let parameter8Def = AKNodeParameterDef(
         identifier: "parameter8",
         name: "Parameter 8",
         address: akGetParameterAddress("AKOperationGeneratorParameter8"),
-        range: -1_000_000 ... 1_000_000,
+        range: floatRange,
         unit: .generic,
         flags: .default)
     public static let parameter9Def = AKNodeParameterDef(
         identifier: "parameter9",
         name: "Parameter 9",
         address: akGetParameterAddress("AKOperationGeneratorParameter9"),
-        range: -1_000_000 ... 1_000_000,
+        range: floatRange,
         unit: .generic,
         flags: .default)
     public static let parameter10Def = AKNodeParameterDef(
         identifier: "parameter10",
         name: "Parameter 10",
         address: akGetParameterAddress("AKOperationGeneratorParameter10"),
-        range: -1_000_000 ... 1_000_000,
+        range: floatRange,
         unit: .generic,
         flags: .default)
     public static let parameter11Def = AKNodeParameterDef(
         identifier: "parameter11",
         name: "Parameter 11",
         address: akGetParameterAddress("AKOperationGeneratorParameter11"),
-        range: -1_000_000 ... 1_000_000,
+        range: floatRange,
         unit: .generic,
         flags: .default)
     public static let parameter12Def = AKNodeParameterDef(
         identifier: "parameter12",
         name: "Parameter 12",
         address: akGetParameterAddress("AKOperationGeneratorParameter12"),
-        range: -1_000_000 ... 1_000_000,
+        range: floatRange,
         unit: .generic,
         flags: .default)
     public static let parameter13Def = AKNodeParameterDef(
         identifier: "parameter13",
         name: "Parameter 13",
         address: akGetParameterAddress("AKOperationGeneratorParameter13"),
-        range: -1_000_000 ... 1_000_000,
+        range: floatRange,
         unit: .generic,
         flags: .default)
     public static let parameter14Def = AKNodeParameterDef(
         identifier: "parameter14",
         name: "Parameter 14",
         address: akGetParameterAddress("AKOperationGeneratorParameter14"),
-        range: -1_000_000 ... 1_000_000,
+        range: floatRange,
         unit: .generic,
         flags: .default)
 
