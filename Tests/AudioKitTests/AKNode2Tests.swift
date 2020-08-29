aa0278de2c65544619a6a3b88688bb912839c9d0
Taylor Holliday <taylor@audulus.com>
2020-08-29 16:12:18 -0700

    Add AKOscillator2 and test.

---
+++ Tests/AudioKitTests/AKNode2Tests.swift
@@ -0,0 +1,29 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
+import AudioKit
+import XCTest
+import AVFoundation
+
+class AKNode2Tests: XCTestCase {
+
+    func testNode2Basic() {
+
+        let engine = AKEngine()
+
+        let osc = AKOscillator2()
+        XCTAssertNotNil(osc.avAudioUnit)
+        osc.start()
+
+        engine.output = osc
+
+        do {
+            try engine.start()
+        } catch let error {
+            XCTFail("Couldn't start engine: \(error)")
+        }
+
+        sleep(2)
+
+        engine.stop()
+
+    }
+}
