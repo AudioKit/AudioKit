2473684e29db5e698f3c667cc1555d6d52e783e4
Aurelius Prochazka <aurephd@gmail.com>
2020-08-30 03:20:45 -0700

    Committing to new style AKCostelloReverb

--- Tests/AudioKitTests/AKCostelloReverbTests.swift
+++ Tests/AudioKitTests/AKCostelloReverbTests.swift
@@ -5,22 +5,22 @@ import AudioKit
 class AKCostelloReverbTests: AKTestCase2 {
 
     func testCutoffFrequency() {
-        output = AKCostelloReverb2(input, cutoffFrequency: 1_234)
+        output = AKCostelloReverb(input, cutoffFrequency: 1_234)
         AKTest()
     }
 
     func testDefault() {
-        output = AKCostelloReverb2(input)
+        output = AKCostelloReverb(input)
         AKTest()
     }
 
     func testFeedback() {
-        output = AKCostelloReverb2(input, feedback: 0.95)
+        output = AKCostelloReverb(input, feedback: 0.95)
         AKTest()
     }
 
     func testParametersSetAfterInit() {
-        let effect = AKCostelloReverb2(input)
+        let effect = AKCostelloReverb(input)
         effect.rampDuration = 0.0
         effect.cutoffFrequency = 1_234
         effect.feedback = 0.95
@@ -29,7 +29,7 @@ class AKCostelloReverbTests: AKTestCase2 {
     }
 
     func testParametersSetOnInit() {
-        output = AKCostelloReverb2(input,
+        output = AKCostelloReverb(input,
                                   feedback: 0.95,
                                   cutoffFrequency: 1_234)
         AKTest()
