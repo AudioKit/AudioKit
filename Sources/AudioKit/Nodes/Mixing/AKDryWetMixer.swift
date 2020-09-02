cb7d7b3adf160c2671e1f3b5140643402cec0d9f
Aurelius Prochazka <aurephd@gmail.com>
2020-09-01 23:41:23 -0700

    Converting AKDryWetMixer from mixers to DSP

--- Sources/AudioKit/Nodes/Mixing/AKDryWetMixer.swift
+++ Sources/AudioKit/Nodes/Mixing/AKDryWetMixer.swift
@@ -1,25 +1,46 @@
 // Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
 
 import AVFoundation
+import CAudioKit
 
 /// Balanceable Mix between two signals, usually used for a dry signal and wet signal
 ///
-public class AKDryWetMixer: AKNode {
-    fileprivate let mixer = AKMixer()
+public class AKDryWetMixer: AKNode, AKToggleable, AKComponent, AKAutomatable {
 
-    /// Balance (Default 0.5)
-    public var balance: Double = 0.5 {
-        didSet {
-            balance = (0...1).clamp(balance)
-            setGainsViaBalance()
+   public static let ComponentDescription = AudioComponentDescription(effect: "dwmx")
+
+   public typealias AKAudioUnitType = InternalAU
+
+   public private(set) var internalAU: AKAudioUnitType?
+
+   public var parameterAutomation: AKParameterAutomation?
+
+   // MARK: - Parameters
+
+    public static let balanceDef = AKNodeParameterDef(
+        identifier: "balance",
+        name: "Balance",
+        address: akGetParameterAddress("AKDryWetMixerParameterBalance"),
+        range: 0.0...1.0,
+        unit: .generic,
+        flags: .default)
+
+    /// Left Channel Amplification Factor
+    @Parameter public var balance: AUValue
+
+
+    // MARK: - Audio Unit
+
+    public class InternalAU: AKAudioUnitBase {
+
+        public override func getParameterDefs() -> [AKNodeParameterDef] {
+            [AKDryWetMixer.balanceDef]
+        }
+
+        public override func createDSP() -> AKDSPRef {
+            akCreateDSP("AKDryWetMixerDSP")
         }
     }
-
-    fileprivate var input1Attenuator = AKBooster()
-    fileprivate var input2Attenuator = AKBooster()
-
-    /// Tells whether the node is processing (ie. started, playing, or active)
-    public var isStarted = true
 
     /// Initialize this dry wet mixer node
     ///
@@ -28,39 +49,24 @@ public class AKDryWetMixer: AKNode {
     ///   - input2: 2nd source
     ///   - balance: Balance Point (0 = all input1, 1 = all input2)
     ///
-    public init(_ input1: AKNode, _ input2: AKNode, balance: Double = 0.5) {
+    public init(_ input1: AKNode, _ input2: AKNode, balance: AUValue = 0.5) {
         super.init(avAudioNode: AVAudioNode())
         self.balance = balance
-        setGainsViaBalance()
 
-        avAudioNode = mixer.avAudioNode
-        input1 >>> input1Attenuator
-        input2 >>> input2Attenuator
+        instantiateAudioUnit { avAudioUnit in
+            self.avAudioUnit = avAudioUnit
+            self.avAudioNode = avAudioUnit
 
-        connections.append(input1Attenuator)
-        connections.append(input2Attenuator)
+            self.internalAU = avAudioUnit.auAudioUnit as? AKAudioUnitType
+            self.parameterAutomation = AKParameterAutomation(avAudioUnit)
+        }
 
-        self.balance = balance
-        setGainsViaBalance()
-
-
+        connections.append(input1)
+        connections.append(input2)
     }
 
-    public convenience init(dry: AKNode, wet: AKNode, balance: Double = 0.5) {
+    public convenience init(dry: AKNode, wet: AKNode, balance: AUValue = 0.5) {
         self.init(dry, wet, balance: balance)
-    }
-
-    private func setGainsViaBalance() {
-        input1Attenuator.gain = AUValue(1 - balance)
-        input2Attenuator.gain = AUValue(balance)
-    }
-
-    open var dryInput: AVAudioConnectionPoint {
-        return AVAudioConnectionPoint(node: input1Attenuator.avAudioUnitOrNode, bus: 0)
-    }
-
-    open var wetInput: AVAudioConnectionPoint {
-        return AVAudioConnectionPoint(node: input2Attenuator.avAudioUnitOrNode, bus: 0)
     }
 
 }
