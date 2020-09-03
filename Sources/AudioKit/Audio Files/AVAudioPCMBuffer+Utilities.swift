bc51e8f07cdc53f5014b5e7d23be6cd899f397cc
Aurelius Prochazka <aurephd@gmail.com>
2020-09-02 19:28:39 -0700

    Progress toward Taylor's ideal testing idea

--- Sources/AudioKit/Audio Files/AVAudioPCMBuffer+Utilities.swift
+++ Sources/AudioKit/Audio Files/AVAudioPCMBuffer+Utilities.swift
@@ -45,6 +45,26 @@ extension AVAudioPCMBuffer {
         return digestHex
 
     }
+
+    public func append(_ buffer: AVAudioPCMBuffer) {
+        append(buffer, startingFrame: 0, frameCount: buffer.frameLength)
+    }
+
+    public func append(_ buffer: AVAudioPCMBuffer, startingFrame: AVAudioFramePosition, frameCount: AVAudioFrameCount) {
+        precondition(format == buffer.format, "Format mismatch")
+        precondition(startingFrame + AVAudioFramePosition(frameCount) <= AVAudioFramePosition(buffer.frameLength), "Insufficient audio in buffer")
+        precondition(frameLength + frameCount <= frameCapacity, "Insufficient space in buffer")
+
+        let dst = floatChannelData!
+        let src = buffer.floatChannelData!
+
+        memcpy(dst.pointee.advanced(by: stride * Int(frameLength)),
+               src.pointee.advanced(by: stride * Int(startingFrame)),
+               Int(frameCount) * stride * MemoryLayout<Float>.size)
+
+        frameLength += frameCount
+    }
+
     /// Copies data from another PCM buffer.  Will copy to the end of the buffer (frameLength), and
     /// increment frameLength. Will not exceed frameCapacity.
     ///
