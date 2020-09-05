878ae4ffe0e477a9b1388016ea5ff33cb2e2b624
Aurelius Prochazka <aurephd@gmail.com>
2020-09-05 15:21:52 -0700

    Finished AKDelay conversion

--- Sources/AudioKit/Nodes/Effects/Delay/AKDelay.swift
+++ Sources/AudioKit/Nodes/Effects/Delay/AKDelay.swift
@@ -8,33 +8,10 @@ import CAudioKit
 public class AKDelay: AKNode, AKToggleable {
     let delayAU = AVAudioUnitDelay()
 
-    fileprivate var lastKnownMix: AUValue = 0.5
-
-    /// Delay time in seconds (Default: 1)
-    public var time: TimeInterval = 1 {
-        didSet {
-            time = max(time, 0)
-            delayAU.delayTime = time
-        }
-    }
-
-    /// Feedback (Normalized Value) ranges from 0 to 1 (Default: 0.5)
-    public var feedback: AUValue = 0.5 {
-        didSet {
-            feedback = (0...1).clamp(feedback)
-            delayAU.feedback = feedback * 100.0
-        }
-    }
-
-    /// Low pass cut-off frequency in Hertz (Default: 15000)
-    public var lowPassCutoff: AUValue = 15_000.00 {
-        didSet {
-            lowPassCutoff = max(lowPassCutoff, 0)
-            delayAU.lowPassCutoff = lowPassCutoff
-        }
-    }
-
-    @Parameter var dryWetMix: AUValue
+    @Parameter public var dryWetMix: AUValue
+    @Parameter public var time: AUValue
+    @Parameter public var feedback: AUValue
+    @Parameter public var lowPassCutoff: AUValue
 
     /// Tells whether the node is processing (ie. started, playing, or active)
     public var isStarted = true
@@ -44,46 +21,44 @@ public class AKDelay: AKNode, AKToggleable {
     /// - Parameters:
     ///   - input: Input audio AKNode to process
     ///   - time: Delay time in seconds (Default: 1)
-    ///   - feedback: Amount of feedback, ranges from 0 to 1 (Default: 0.5)
+    ///   - feedback: Amount of feedback, ranges from 0 to 100 (Default: 50)
     ///   - lowPassCutoff: Low-pass cutoff frequency in Hz (Default 15000)
     ///   - dryWetMix: Amount of unprocessed (dry) to delayed (wet) audio, ranges from 0 to 100 (Default: 50.0)
     ///
     public init(
         _ input: AKNode,
         time: AUValue = 1,
-        feedback: AUValue = 0.5,
+        feedback: AUValue = 50,
         lowPassCutoff: AUValue = 15_000,
-        dryWetMix: AUValue = 50.0) {
-
-        self.time = TimeInterval(AUValue(time))
-        self.feedback = feedback
-        self.lowPassCutoff = lowPassCutoff
+        dryWetMix: AUValue = 50) {
 
         super.init(avAudioUnit: delayAU)
         connections.append(input)
 
-        delayAU.delayTime = self.time
-        delayAU.feedback = feedback * 100.0
-        delayAU.lowPassCutoff = lowPassCutoff
-
         self.$dryWetMix.associate(with: delayAU, index: 0)
+        self.$time.associate(with: delayAU, index: 1)
+        self.$feedback.associate(with: delayAU, index: 2)
+        self.$lowPassCutoff.associate(with: delayAU, index: 3)
+
         self.dryWetMix = dryWetMix
+        self.time = time
+        self.feedback = feedback
+        self.lowPassCutoff = lowPassCutoff
     }
 
     /// Function to start, play, or activate the node, all do the same thing
     public func start() {
         if isStopped {
-            dryWetMix = lastKnownMix
             isStarted = true
+            delayAU.bypass = false
         }
     }
 
     /// Function to stop or bypass the node, both are equivalent
     public func stop() {
         if isPlaying {
-            lastKnownMix = dryWetMix
-            dryWetMix = 0
             isStarted = false
+            delayAU.bypass = true
         }
     }
 }
