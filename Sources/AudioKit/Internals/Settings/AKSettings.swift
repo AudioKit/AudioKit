0871a02c8f2ea90227540ecbac4b7e17a0e7d2dd
Aurelius Prochazka <aurephd@gmail.com>
2020-09-08 10:18:06 -0700

    Making a globally accessible AKMIDI (temporary)

--- Sources/AudioKit/Internals/Settings/AKSettings.swift
+++ Sources/AudioKit/Internals/Settings/AKSettings.swift
@@ -81,6 +81,9 @@ public class AKSettings: NSObject {
         }
     }
 
+    // temporary location of AKMIDI
+    public static var midi = AKMIDI()
+
     /// Whether we should be listening to audio input (microphone)
     public static var audioInputEnabled: Bool = false
 
