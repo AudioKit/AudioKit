b38d8f43b5f1266b64e99f6667abeb56f92f7fd6
Aurelius Prochazka <aurephd@gmail.com>
2020-09-08 16:04:10 -0700

    Moved example to https://github.com/AudioKit/MIDIFileEditAndSync

--- Examples/iOS/MIDIFileEditAndSync/MIDIFileEditAndSync/CustomTableViewCells/TrackCell.swift
+++
@@ -1,9 +0,0 @@
-// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
-
-import UIKit
-
-class TrackCell: UITableViewCell {
-    @IBOutlet weak var index: UILabel!
-    @IBOutlet weak var noteEvents: UILabel!
-    var trackNum: Int!
-}
