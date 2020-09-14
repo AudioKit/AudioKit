e4a412e9b92a3cb20e02d774a26933448433b21b
Aurelius Prochazka <aurephd@gmail.com>
2020-09-13 17:53:15 -0700

    Fixes https://github.com/AudioKit/AudioKit/issues/2257
    "AKSampler will not stop sounding and keep repeating the note indefinitely"

--- Sources/CAudioKit/Nodes/Generators/Synth/AKSynthDSP.mm
+++ Sources/CAudioKit/Nodes/Generators/Synth/AKSynthDSP.mm
@@ -144,6 +144,13 @@ float AKSynthDSP::getParameter(uint64_t address)
 
 void AKSynthDSP::process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset)
 {
+
+    float *pLeft = (float *)outputBufferList->mBuffers[0].mData + bufferOffset;
+    float *pRight = (float *)outputBufferList->mBuffers[1].mData + bufferOffset;
+
+    memset(pLeft, 0, frameCount * sizeof(float));
+    memset(pRight, 0, frameCount * sizeof(float));
+    
     // process in chunks of maximum length CHUNKSIZE
     for (int frameIndex = 0; frameIndex < frameCount; frameIndex += AKSYNTH_CHUNKSIZE) {
         int frameOffset = int(frameIndex + bufferOffset);
