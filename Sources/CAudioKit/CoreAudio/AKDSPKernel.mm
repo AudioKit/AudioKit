dd304597eb674ddf074f12e0f16d44ddddb693fd
Aurelius Prochazka <aurephd@gmail.com>
2020-09-14 23:12:08 -0700

    Removing unused DSP

--- Sources/CAudioKit/CoreAudio/AKDSPKernel.mm
+++
@@ -1,7 +0,0 @@
-// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
-
-#include "AKDSPKernel.hpp"
-#include "AKGlobals.h"
-
-AKDSPKernel::AKDSPKernel() : AKDSPKernel(__akDefaultChannelCount, __akDefaultSampleRate) { }
-
