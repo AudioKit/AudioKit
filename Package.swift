52bd2d4aa3291d9b35128b5548b99db207b27b77
Aurelius Prochazka <aurephd@gmail.com>
2020-09-25 23:56:17 -0700

    Moving officially targeted version to iOS13+

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AudioKit",
     platforms: [
-        .macOS(.v10_14), .iOS(.v11), .tvOS(.v11)
+        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
