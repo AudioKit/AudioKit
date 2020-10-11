93e78bc128720e2fa20c343f4ff0d074e077cd5a
Aurelius Prochazka <aurephd@gmail.com>
2020-10-10 23:19:51 -0700

    More playgrounds to cookbook

--- Playgrounds/Basics.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
+++ Playgrounds/Basics.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
@@ -1,20 +1,5 @@
 //: # Basics
 //:
-//: ## Getting Started
-//:
-//: Let's start off just making sure you're all set up and can make sound.
-//:
-//: * [Introduction and Hello World](Introduction%20and%20Hello%20World)
-//:
-//: ## Basic Tutorials
-//:
-//: These tutorials help you get started with the basic concepts of AudioKit,
-//: starting with what you need to know about playgrounds in general:
-//:
-//: * [Non-Audio Tutorial](Non-Audio%20Tutorial)
-//:
-//: Then, moving on to working with AudioKit nodes:
-//:
 //: * [Connecting Nodes](Connecting%20Nodes)
 //: * [Connecting Nodes Part 2](Connecting%20Nodes%20Part%202)
 //: * [Mixing Nodes](Mixing%20Nodes)
@@ -25,13 +10,7 @@
 //:
 //: A gentle introduction to the power of AudioKit operations:
 //:
-//: * [Low-Frequency Oscillating of Parameters](Low-Frequency%20Oscillating%20of%20Parameters)
 //: * [Using Functions](Using%20Functions)
 //: * [Using Functions Part 2](Using%20Functions%20Part%202)
 //: * [Stereo Operation](Stereo%20Operation)
-//:
-//: And finally some notes about the differences between playground
-//: code and production code
-//:
-//: * [Playgrounds to Production](Playgrounds%20to%20Production)
 //:
