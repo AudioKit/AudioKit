85b73c789cbda08cb63091aa7ad7963af2e721e9
Aurelius Prochazka <aurephd@gmail.com>
2020-10-11 03:52:35 -0700

    Moving playgrounds to cookbook

--- Playgrounds/Basics.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
+++ Playgrounds/Basics.playground/Pages/Table Of Contents.xcplaygroundpage/Contents.swift
@@ -1,16 +1,7 @@
 //: # Basics
 //:
-//: * [Connecting Nodes](Connecting%20Nodes)
-//: * [Connecting Nodes Part 2](Connecting%20Nodes%20Part%202)
 //: * [Mixing Nodes](Mixing%20Nodes)
 //: * [Splitting Nodes](Splitting%20Nodes)
-//: * [Dry Wet Mixer](Dry%20Wet%20Mixer)
 //: * [Balancing Nodes](Balancing%20Nodes)
 //: * [Tables](Tables)
-//:
-//: A gentle introduction to the power of AudioKit operations:
-//:
-//: * [Using Functions](Using%20Functions)
-//: * [Using Functions Part 2](Using%20Functions%20Part%202)
-//: * [Stereo Operation](Stereo%20Operation)
 //:
