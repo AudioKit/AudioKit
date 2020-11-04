0b8e6f2bf76d1f13a89b41bbd6477ae444b251b6
Ryan Francesconi <ryan@add.app>
2020-11-03 16:14:42 -0800

    spaces

--- Sources/AudioKit/Nodes/Mixing/Fader.swift
+++ Sources/AudioKit/Nodes/Mixing/Fader.swift
@@ -5,148 +5,148 @@ import CAudioKit
 
 /// Stereo Fader.
 public class Fader: Node, AudioUnitContainer, Tappable, Toggleable {
-	/// Unique four-letter identifier "fder"
-	public static let ComponentDescription = AudioComponentDescription(effect: "fder")
+    /// Unique four-letter identifier "fder"
+    public static let ComponentDescription = AudioComponentDescription(effect: "fder")
 
-	/// Internal type of audio unit for this node
-	public typealias AudioUnitType = InternalAU
+    /// Internal type of audio unit for this node
+    public typealias AudioUnitType = InternalAU
 
-	/// Internal audio unit
-	public private(set) var internalAU: AudioUnitType?
+    /// Internal audio unit
+    public private(set) var internalAU: AudioUnitType?
 
-	// MARK: - Parameters
+    // MARK: - Parameters
 
-	/// Amplification Factor, from 0 ... 4
-	open var gain: AUValue = 1 {
-		willSet {
-			leftGain = gain
-			rightGain = gain
-		}
-	}
+    /// Amplification Factor, from 0 ... 4
+    open var gain: AUValue = 1 {
+        willSet {
+            leftGain = gain
+            rightGain = gain
+        }
+    }
 
-	/// Allow gain to be any non-negative number
-	public static let gainRange: ClosedRange<AUValue> = 0.0 ... Float.greatestFiniteMagnitude
+    /// Allow gain to be any non-negative number
+    public static let gainRange: ClosedRange<AUValue> = 0.0 ... Float.greatestFiniteMagnitude
 
-	/// Specification details for left gain
-	public static let leftGainDef = NodeParameterDef(
-		identifier: "leftGain",
-		name: "Left Gain",
-		address: akGetParameterAddress("FaderParameterLeftGain"),
-		range: Fader.gainRange,
-		unit: .linearGain,
-		flags: .default)
+    /// Specification details for left gain
+    public static let leftGainDef = NodeParameterDef(
+        identifier: "leftGain",
+        name: "Left Gain",
+        address: akGetParameterAddress("FaderParameterLeftGain"),
+        range: Fader.gainRange,
+        unit: .linearGain,
+        flags: .default)
 
-	/// Left Channel Amplification Factor
-	@Parameter public var leftGain: AUValue
+    /// Left Channel Amplification Factor
+    @Parameter public var leftGain: AUValue
 
-	/// Specification details for right gain
-	public static let rightGainDef = NodeParameterDef(
-		identifier: "rightGain",
-		name: "Right Gain",
-		address: akGetParameterAddress("FaderParameterRightGain"),
-		range: Fader.gainRange,
-		unit: .linearGain,
-		flags: .default)
+    /// Specification details for right gain
+    public static let rightGainDef = NodeParameterDef(
+        identifier: "rightGain",
+        name: "Right Gain",
+        address: akGetParameterAddress("FaderParameterRightGain"),
+        range: Fader.gainRange,
+        unit: .linearGain,
+        flags: .default)
 
-	/// Right Channel Amplification Factor
-	@Parameter public var rightGain: AUValue
+    /// Right Channel Amplification Factor
+    @Parameter public var rightGain: AUValue
 
-	/// Amplification Factor in db
-	public var dB: AUValue {
-		set { gain = pow(10.0, newValue / 20.0) }
-		get { return 20.0 * log10(gain) }
-	}
+    /// Amplification Factor in db
+    public var dB: AUValue {
+        set { gain = pow(10.0, newValue / 20.0) }
+        get { return 20.0 * log10(gain) }
+    }
 
-	/// Whether or not to flip left and right channels
-	public static let flipStereoDef = NodeParameterDef(
-		identifier: "flipStereo",
-		name: "Flip Stereo",
-		address: akGetParameterAddress("FaderParameterFlipStereo"),
-		range: 0.0 ... 1.0,
-		unit: .boolean,
-		flags: .default)
+    /// Whether or not to flip left and right channels
+    public static let flipStereoDef = NodeParameterDef(
+        identifier: "flipStereo",
+        name: "Flip Stereo",
+        address: akGetParameterAddress("FaderParameterFlipStereo"),
+        range: 0.0 ... 1.0,
+        unit: .boolean,
+        flags: .default)
 
-	/// Flip left and right signal
-	@Parameter public var flipStereo: Bool
+    /// Flip left and right signal
+    @Parameter public var flipStereo: Bool
 
-	/// Specification for whether to mix the stereo signal down to mono
-	public static let mixToMonoDef = NodeParameterDef(
-		identifier: "mixToMono",
-		name: "Mix To Mono",
-		address: akGetParameterAddress("FaderParameterMixToMono"),
-		range: 0.0 ... 1.0,
-		unit: .boolean,
-		flags: .default)
+    /// Specification for whether to mix the stereo signal down to mono
+    public static let mixToMonoDef = NodeParameterDef(
+        identifier: "mixToMono",
+        name: "Mix To Mono",
+        address: akGetParameterAddress("FaderParameterMixToMono"),
+        range: 0.0 ... 1.0,
+        unit: .boolean,
+        flags: .default)
 
-	/// Make the output on left and right both be the same combination of incoming left and mixed equally
-	@Parameter public var mixToMono: Bool
+    /// Make the output on left and right both be the same combination of incoming left and mixed equally
+    @Parameter public var mixToMono: Bool
 
-	// MARK: - Audio Unit
+    // MARK: - Audio Unit
 
-	/// Internal audio unit for fader
-	public class InternalAU: AudioUnitBase {
-		/// Get an array of the parameter definitions
-		/// - Returns: Array of parameter definitions
-		override public func getParameterDefs() -> [NodeParameterDef] {
-			[Fader.leftGainDef,
-			 Fader.rightGainDef,
-			 Fader.flipStereoDef,
-			 Fader.mixToMonoDef]
-		}
+    /// Internal audio unit for fader
+    public class InternalAU: AudioUnitBase {
+        /// Get an array of the parameter definitions
+        /// - Returns: Array of parameter definitions
+        override public func getParameterDefs() -> [NodeParameterDef] {
+            [Fader.leftGainDef,
+             Fader.rightGainDef,
+             Fader.flipStereoDef,
+             Fader.mixToMonoDef]
+        }
 
-		/// Create the DSP Refence for this node
-		/// - Returns: DSP Reference
-		override public func createDSP() -> DSPRef {
-			akCreateDSP("FaderDSP")
-		}
-	}
+        /// Create the DSP Refence for this node
+        /// - Returns: DSP Reference
+        override public func createDSP() -> DSPRef {
+            akCreateDSP("FaderDSP")
+        }
+    }
 
-	// MARK: - Initialization
+    // MARK: - Initialization
 
-	/// Initialize this fader node
-	///
-	/// - Parameters:
-	///   - input: Node whose output will be amplified
-	///   - gain: Amplification factor (Default: 1, Minimum: 0)
-	///
-	public init(_ input: Node? = nil, gain: AUValue = 1) {
-		super.init(avAudioNode: AVAudioNode())
+    /// Initialize this fader node
+    ///
+    /// - Parameters:
+    ///   - input: Node whose output will be amplified
+    ///   - gain: Amplification factor (Default: 1, Minimum: 0)
+    ///
+    public init(_ input: Node? = nil, gain: AUValue = 1) {
+        super.init(avAudioNode: AVAudioNode())
 
-		instantiateAudioUnit { avAudioUnit in
-			self.avAudioUnit = avAudioUnit
-			self.avAudioNode = avAudioUnit
+        instantiateAudioUnit { avAudioUnit in
+            self.avAudioUnit = avAudioUnit
+            self.avAudioNode = avAudioUnit
 
-			self.internalAU = avAudioUnit.auAudioUnit as? AudioUnitType
+            self.internalAU = avAudioUnit.auAudioUnit as? AudioUnitType
 
-			self.leftGain = gain
-			self.rightGain = gain
-			self.flipStereo = false
-			self.mixToMono = false
-		}
+            self.leftGain = gain
+            self.rightGain = gain
+            self.flipStereo = false
+            self.mixToMono = false
+        }
 
-		if let input = input {
-			connections.append(input)
-		}
-	}
+        if let input = input {
+            connections.append(input)
+        }
+    }
 
-	deinit {
-		Log("* { Fader }")
-	}
+    deinit {
+        Log("* { Fader }")
+    }
 
-	// MARK: - Automation
+    // MARK: - Automation
 
-	/// Gain automation helper
-	/// - Parameters:
-	///   - events: List of events
-	///   - startTime: start time
-	public func automateGain(events: [AutomationEvent], startTime: AVAudioTime? = nil) {
-		$leftGain.automate(events: events, startTime: startTime)
-		$rightGain.automate(events: events, startTime: startTime)
-	}
+    /// Gain automation helper
+    /// - Parameters:
+    ///   - events: List of events
+    ///   - startTime: start time
+    public func automateGain(events: [AutomationEvent], startTime: AVAudioTime? = nil) {
+        $leftGain.automate(events: events, startTime: startTime)
+        $rightGain.automate(events: events, startTime: startTime)
+    }
 
-	/// Stop automation
-	public func stopAutomation() {
-		$leftGain.stopAutomation()
-		$rightGain.stopAutomation()
-	}
+    /// Stop automation
+    public func stopAutomation() {
+        $leftGain.stopAutomation()
+        $rightGain.stopAutomation()
+    }
 }
