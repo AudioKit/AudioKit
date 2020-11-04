21937c2899621757e96b12d14be92da8361a329c
Ryan Francesconi <ryan@add.app>
2020-11-03 18:15:36 -0800

    ____

--- Tests/AudioKitTests/Operation Tests/ErrorHandlingTests.swift
+++ Tests/AudioKitTests/Operation Tests/ErrorHandlingTests.swift
@@ -5,18 +5,18 @@ import CAudioKit
 import XCTest
 
 class ErrorHandlingTests: XCTestCase {
-	func testExceptionCatcher() {
-		do {
-			try ExceptionCatcher {
-				// engine is nil, so this will throw this error:
-				// required condition is false: _engine != nil
-				AudioPlayer().play()
-			}
-		} catch let error as NSError {
-			// and it should be caught here:
-			Log("👍", error)
-			return
-		}
-		XCTFail("ExceptionCatcher didn't catch the error")
-	}
+    func testExceptionCatcher() {
+        do {
+            try ExceptionCatcher {
+                // engine is nil, so this will throw this error:
+                // required condition is false: _engine != nil
+                AudioPlayer().play()
+            }
+        } catch let error as NSError {
+            // and it should be caught here:
+            Log("👍", error)
+            return
+        }
+        XCTFail("ExceptionCatcher didn't catch the error")
+    }
 }
