aedd2072013be4bc49854921144d5fbbaf5be869
Aurelius Prochazka <aurephd@gmail.com>
2020-12-14 20:22:13 -0800

    Cleanup

--- Sources/AudioKit/User Interface/DisplayLink.swift
+++ Sources/AudioKit/User Interface/DisplayLink.swift
@@ -7,17 +7,15 @@
 import QuartzCore
 
 public class DisplayLink {
-
     fileprivate let callback: () -> Void
 
     private var link: CADisplayLink!
 
-    public init (_ callback: @escaping () -> Void) {
+    public init(_ callback: @escaping () -> Void) {
         self.callback = callback
         link = CADisplayLink(target: DisplayTarget(self), selector: #selector(DisplayTarget.callback))
         link.add(to: RunLoop.main, forMode: RunLoop.Mode.common)
     }
-    
 
     deinit {
         link.invalidate()
@@ -26,21 +24,21 @@ public class DisplayLink {
     func pause() {
         link.isPaused = true
     }
+
     func resume() {
         link.isPaused = false
     }
-    
+
     public func invalidate() {
         link.invalidate()
     }
-    
 }
 
 /// Retained by CADisplayLink.
 fileprivate class DisplayTarget {
     weak var link: DisplayLink!
 
-    init (_ link: DisplayLink) {
+    init(_ link: DisplayLink) {
         self.link = link
     }
 
