5cffd3e20171af89494c39d1826a44557c058e0c
Aurelius Prochazka <aurephd@gmail.com>
2020-12-20 13:39:33 -0800

    Minor update

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
         .macOS(.v10_14), .iOS(.v11), .tvOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "AudioKit",
             type: .static,
