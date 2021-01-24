21c4ec18300018dc3768432bb75f477dbfaca14c
Evan Murray <evanmurray22@gmail.com>
2021-01-23 19:35:17 -0500

    Added MD5s for TransientShaper

--- Tests/AudioKitTests/Node Tests/Effects Tests/TransientShaperTests.swift
+++ Tests/AudioKitTests/Node Tests/Effects Tests/TransientShaperTests.swift
@@ -5,20 +5,50 @@ import XCTest
 
 class TransientShaperTests: XCTestCase {
 
-    func testAttackAmount() {
+    func testDefault() {
         let engine = AudioEngine()
         let input = Oscillator(amplitude: 2.0)
-        engine.output = TransientShaper(input, attackAmount: 3.0, releaseAmount: -40.0)
+        engine.output = TransientShaper(input)
         input.start()
         let audio = engine.startTest(totalDuration: 1.0)
         audio.append(engine.render(duration: 1.0))
         testMD5(audio)
     }
 
-    func testDefault() {
+    func testInputAmount() {
         let engine = AudioEngine()
         let input = Oscillator(amplitude: 2.0)
-        engine.output = TransientShaper(input)
+        engine.output = TransientShaper(input, inputAmount: -20.0)
+        input.start()
+        let audio = engine.startTest(totalDuration: 1.0)
+        audio.append(engine.render(duration: 1.0))
+        testMD5(audio)
+    }
+
+    func testOutputAmount() {
+        let engine = AudioEngine()
+        let input = Oscillator(amplitude: 2.0)
+        engine.output = TransientShaper(input, outputAmount: -20.0)
+        input.start()
+        let audio = engine.startTest(totalDuration: 1.0)
+        audio.append(engine.render(duration: 1.0))
+        testMD5(audio)
+    }
+
+    func testAttackAmount() {
+        let engine = AudioEngine()
+        let input = Oscillator(amplitude: 2.0)
+        engine.output = TransientShaper(input, attackAmount: 1.0)
+        input.start()
+        let audio = engine.startTest(totalDuration: 1.0)
+        audio.append(engine.render(duration: 1.0))
+        testMD5(audio)
+    }
+
+    func testReleaseAmount() {
+        let engine = AudioEngine()
+        let input = Oscillator(amplitude: 2.0)
+        engine.output = TransientShaper(input, releaseAmount: 1.0)
         input.start()
         let audio = engine.startTest(totalDuration: 1.0)
         audio.append(engine.render(duration: 1.0))
@@ -30,39 +60,9 @@ class TransientShaperTests: XCTestCase {
         let input = Oscillator(amplitude: 2.0)
         engine.output = TransientShaper(input,
                                         inputAmount: -1.0,
-                                        attackAmount: 1.5,
-                                        releaseAmount: -40.0,
-                                        outputAmount: -3.0)
-        input.start()
-        let audio = engine.startTest(totalDuration: 1.0)
-        audio.append(engine.render(duration: 1.0))
-        testMD5(audio)
-    }
-
-    func testInputAmount() {
-        let engine = AudioEngine()
-        let input = Oscillator(amplitude: 2.0)
-        engine.output = TransientShaper(input, inputAmount: -5.0)
-        input.start()
-        let audio = engine.startTest(totalDuration: 1.0)
-        audio.append(engine.render(duration: 1.0))
-        testMD5(audio)
-    }
-
-    func testReleaseAmount() {
-        let engine = AudioEngine()
-        let input = Oscillator(amplitude: 2.0)
-        engine.output = TransientShaper(input, releaseAmount: 5.0)
-        input.start()
-        let audio = engine.startTest(totalDuration: 1.0)
-        audio.append(engine.render(duration: 1.0))
-        testMD5(audio)
-    }
-
-    func testOutputAmount() {
-        let engine = AudioEngine()
-        let input = Oscillator(amplitude: 2.0)
-        engine.output = TransientShaper(input, outputAmount: -10.0)
+                                        attackAmount: -3.0,
+                                        releaseAmount: 1.0,
+                                        outputAmount: 0.0)
         input.start()
         let audio = engine.startTest(totalDuration: 1.0)
         audio.append(engine.render(duration: 1.0))
