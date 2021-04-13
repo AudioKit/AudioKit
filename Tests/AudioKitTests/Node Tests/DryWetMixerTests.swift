97bf880e732a4bb501865bd6d41b9e30cd666262
Aurelius Prochazka <aurephd@gmail.com>
2021-04-13 15:09:26 -0700

    Progress toward test showing bug with just AVAudioEngine re: #2377

--- Tests/AudioKitTests/Node Tests/DryWetMixerTests.swift
+++ Tests/AudioKitTests/Node Tests/DryWetMixerTests.swift
@@ -106,6 +106,82 @@ class DryWetMixerTests: XCTestCase {
     /*
     var dryWet: AVAudioUnit!
     
+    class TestingAU: AUAudioUnit {
+        private var inputBusArray: [AUAudioUnitBus] = []
+        private var outputBusArray: [AUAudioUnitBus] = []
+        private var internalBuffers: [AVAudioPCMBuffer] = []
+        
+        /// Allocate the render resources
+        override public func allocateRenderResources() throws {
+            try super.allocateRenderResources()
+
+            let format = Settings.audioFormat
+
+            try inputBusArray.forEach { if $0.format != format { try $0.setFormat(format) } }
+            try outputBusArray.forEach { if $0.format != format { try $0.setFormat(format) } }
+
+            // we don't need to allocate a buffer if we can process in place
+            if !canProcessInPlace || inputBusArray.count > 1 {
+                for i in inputBusArray.indices {
+                    if let buffer = AVAudioPCMBuffer(pcmFormat: format, frameCapacity: maximumFramesToRender) {
+                        internalBuffers.append(buffer)
+                    }
+                }
+            }
+        }
+
+        /// Delllocate Render Resources
+        override public func deallocateRenderResources() {
+            super.deallocateRenderResources()
+            internalBuffers = []
+        }
+        
+        private lazy var auInputBusArray: AUAudioUnitBusArray = {
+            AUAudioUnitBusArray(audioUnit: self, busType: .input, busses: inputBusArray)
+        }()
+
+        /// Input busses
+        override public var inputBusses: AUAudioUnitBusArray {
+            return auInputBusArray
+        }
+
+        private lazy var auOutputBusArray: AUAudioUnitBusArray = {
+            AUAudioUnitBusArray(audioUnit: self, busType: .output, busses: outputBusArray)
+        }()
+
+        /// Output bus array
+        override public var outputBusses: AUAudioUnitBusArray {
+            return auOutputBusArray
+        }
+        
+        override var internalRenderBlock: AUInternalRenderBlock {
+            return { ( _, _, _, _, _, _, _) in
+                return noErr
+            }
+        }
+        
+        /// Initialize with component description and options
+        /// - Parameters:
+        ///   - componentDescription: Audio Component Description
+        ///   - options: Audio Component Instantiation Options
+        /// - Throws: error
+        override public init(componentDescription: AudioComponentDescription,
+                             options: AudioComponentInstantiationOptions = []) throws {
+            try super.init(componentDescription: componentDescription, options: options)
+
+            // create audio bus connection points
+            let format = AVAudioFormat(standardFormatWithSampleRate: 44_100,
+                                       channels: 2) ?? AVAudioFormat()
+            for _ in 0..<2 {
+                inputBusArray.append(try AUAudioUnitBus(format: format))
+            }
+            for _ in 0..<2 {
+                outputBusArray.append(try AUAudioUnitBus(format: format))
+            }
+        }
+
+    }
+    
     func testReattachInputMixerAV() {
         
         let engine = AVAudioEngine()
@@ -113,11 +189,11 @@ class DryWetMixerTests: XCTestCase {
         let player = AVAudioPlayerNode()
         engine.attach(player)
         
-        AUAudioUnit.registerSubclass(DryWetMixer.InternalAU.self,
-                                     as: DryWetMixer.ComponentDescription,
+        AUAudioUnit.registerSubclass(TestingAU.self,
+                                     as: AudioComponentDescription(mixer: "dwm2"),
                                      name: "Local DryWetMixer",
                                      version: .max)
-        AVAudioUnit.instantiate(with: DryWetMixer.ComponentDescription) { avAudioUnit, _ in
+        AVAudioUnit.instantiate(with: AudioComponentDescription(mixer: "dwm2")) { avAudioUnit, _ in
             guard let au = avAudioUnit else {
                 fatalError("Unable to instantiate AVAudioUnit")
             }
@@ -146,4 +222,5 @@ class DryWetMixerTests: XCTestCase {
         
     }
     */
+    
 }
