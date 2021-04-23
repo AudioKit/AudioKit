6fc537f0fb549049bc002ea36742a05ad57cdd36
Gertjan Jansen <gertjan@miniem.nl>
2021-04-23 12:25:28 +0200

    Remove try!'s and whitespaces

--- Tests/AudioKitTests/Node Tests/Playback Tests/AppleSamplerTests.swift
+++ Tests/AudioKitTests/Node Tests/Playback Tests/AppleSamplerTests.swift
@@ -8,12 +8,12 @@ class AppleSamplerTests: XCTestCase {
     func testSamplePlayback() {
         let sampler = AppleSampler()
         let sampleURL = Bundle.module.url(forResource: "Resources/drumloop", withExtension: "wav")!
-        let audioFile = try! AVAudioFile(forReading: sampleURL)
-        try! sampler.loadAudioFile(audioFile)
-        
+        let audioFile = try? AVAudioFile(forReading: sampleURL)
+        try? sampler.loadAudioFile(audioFile)
+
         let engine = AudioEngine()
         engine.output = sampler
-        
+
         let audio = engine.startTest(totalDuration: 2.0)
         sampler.play(noteNumber: 50, velocity: 127, channel: 1)
         audio.append(engine.render(duration: 2.0))
@@ -23,22 +23,17 @@ class AppleSamplerTests: XCTestCase {
     func testStop() {
         let sampler = AppleSampler()
         let sampleURL = Bundle.module.url(forResource: "Resources/drumloop", withExtension: "wav")!
-        let audioFile = try! AVAudioFile(forReading: sampleURL)
-        try! sampler.loadAudioFile(audioFile)
-        
+        let audioFile = try? AVAudioFile(forReading: sampleURL)
+        try? sampler.loadAudioFile(audioFile)
+
         let engine = AudioEngine()
         engine.output = sampler
-        
+
         let audio = engine.startTest(totalDuration: 2.0)
         sampler.play(noteNumber: 50, velocity: 127, channel: 1)
         // this immediately stops the sample
         sampler.stop(noteNumber: 50, channel: 1)
         audio.append(engine.render(duration: 2.0))
         testMD5(audio)
-    }
-    
-    func testPolyphonicNodeInheritance() {
-        let polyphonic = AppleSampler() as? PolyphonicNode
-        XCTAssertNotNil(polyphonic)
     }
 }
