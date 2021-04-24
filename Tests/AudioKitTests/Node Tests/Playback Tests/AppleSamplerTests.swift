48b3a5fa9879e898df906398143d59a80acd720e
Gertjan Jansen <gertjan@miniem.nl>
2021-04-24 07:35:14 +0200

    Add tests for AppleSampler's volume, pan and amplitude

--- Tests/AudioKitTests/Node Tests/Playback Tests/AppleSamplerTests.swift
+++ Tests/AudioKitTests/Node Tests/Playback Tests/AppleSamplerTests.swift
@@ -5,18 +5,17 @@ import XCTest
 import AVFoundation
 
 class AppleSamplerTests: XCTestCase {
-    func testSamplePlayback() {
-        let sampler = AppleSampler()
+    let sampler = AppleSampler()
+    let engine = AudioEngine()
+
+    override func setUpWithError() throws {
         let sampleURL = Bundle.module.url(forResource: "Resources/drumloop", withExtension: "wav")!
-        guard let audioFile = try? AVAudioFile(forReading: sampleURL) else {
-            XCTFail("Failed to load drumloop sample")
-            return
-        }
-        try? sampler.loadAudioFile(audioFile)
-
-        let engine = AudioEngine()
+        let audioFile = try AVAudioFile(forReading: sampleURL)
+        try sampler.loadAudioFile(audioFile)
         engine.output = sampler
+    }
 
+    func testSamplePlayback() {
         let audio = engine.startTest(totalDuration: 2.0)
         sampler.play(noteNumber: 50, velocity: 127, channel: 1)
         audio.append(engine.render(duration: 2.0))
@@ -24,17 +23,6 @@ class AppleSamplerTests: XCTestCase {
     }
 
     func testStop() {
-        let sampler = AppleSampler()
-        let sampleURL = Bundle.module.url(forResource: "Resources/drumloop", withExtension: "wav")!
-        guard let audioFile = try? AVAudioFile(forReading: sampleURL) else {
-            XCTFail("Failed to load drumloop sample")
-            return
-        }
-        try? sampler.loadAudioFile(audioFile)
-
-        let engine = AudioEngine()
-        engine.output = sampler
-
         let audio = engine.startTest(totalDuration: 2.0)
         sampler.play(noteNumber: 50, velocity: 127, channel: 1)
         // this immediately stops the sample
@@ -44,25 +32,37 @@ class AppleSamplerTests: XCTestCase {
     }
 
     func testAppleSamplerPolyphonicInheritance() {
-        let sampler = AppleSampler()
-        let sampleURL = Bundle.module.url(forResource: "Resources/drumloop", withExtension: "wav")!
-        guard let audioFile = try? AVAudioFile(forReading: sampleURL) else {
-            XCTFail("Failed to load drumloop sample")
-            return
-        }
-        try? sampler.loadAudioFile(audioFile)
-
-        let engine = AudioEngine()
-        engine.output = sampler
-
         func playMultiple(polyphonicNode: PolyphonicNode) {
             polyphonicNode.play(noteNumber: 1, velocity: 127, channel: 1)
             polyphonicNode.play(noteNumber: 2, velocity: 127, channel: 2)
             polyphonicNode.play(noteNumber: 3, velocity: 127, channel: 3)
         }
-
         let audio = engine.startTest(totalDuration: 2.0)
         playMultiple(polyphonicNode: sampler)
+        audio.append(engine.render(duration: 2.0))
+        testMD5(audio)
+    }
+
+    func testVolume() {
+        sampler.volume = 0.8
+        let audio = engine.startTest(totalDuration: 2.0)
+        sampler.play(noteNumber: 50, velocity: 127, channel: 1)
+        audio.append(engine.render(duration: 2.0))
+        testMD5(audio)
+    }
+
+    func testPan() {
+        sampler.pan = 1.0
+        let audio = engine.startTest(totalDuration: 2.0)
+        sampler.play(noteNumber: 50, velocity: 127, channel: 1)
+        audio.append(engine.render(duration: 2.0))
+        testMD5(audio)
+    }
+
+    func testAmplitude() {
+        sampler.amplitude = 12
+        let audio = engine.startTest(totalDuration: 2.0)
+        sampler.play(noteNumber: 50, velocity: 127, channel: 1)
         audio.append(engine.render(duration: 2.0))
         testMD5(audio)
     }
