09322b5227acbdbd6bf7432ff68687cde84f0413
Taylor Holliday <taylor@audulus.com>
2021-05-05 09:32:41 -0700

    add StkBundleHelper for finding resources

---
+++ Sources/Stk/StkBundleHelper.m
@@ -0,0 +1,11 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
+
+#import "StkBundleHelper.h"
+
+@implementation StkBundleHelper
+
++ (NSBundle*) moduleBundle {
+    return SWIFTPM_MODULE_BUNDLE;
+}
+
+@end
