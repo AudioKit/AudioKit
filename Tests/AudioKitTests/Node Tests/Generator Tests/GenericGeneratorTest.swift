ef11047e2c5389db4156f03fee57c52ebe204fd3
Taylor Holliday <taylor@audulus.com>
2021-05-07 12:05:46 -0700

    Started work on generic generator regression test.

---
+++ Tests/AudioKitTests/Node Tests/Generator Tests/GenericGeneratorTest.swift
@@ -0,0 +1,58 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
+
+import Foundation
+@testable import AudioKit
+import GameplayKit
+import AVFoundation
+
+func setParams(node: Node, rng: GKRandomSource) {
+
+    let mirror = Mirror(reflecting: node)
+
+    for child in mirror.children {
+        if let param = child.value as? ParameterBase {
+            guard let def = param.projectedValue.def else {
+                fatalError("Node is missing parameter def. Please update.")
+            }
+            let size = def.range.upperBound - def.range.lowerBound
+            let value = rng.nextUniform() * size + def.range.lowerBound
+            print("setting parameter \(def.name) to \(value)")
+            param.projectedValue.value = value
+        }
+    }
+}
+
+func generatorNodeTest(factory: ()->Node) -> AVAudioPCMBuffer {
+
+    // We want determinism.
+    let rng = GKMersenneTwisterRandomSource(seed: 0)
+
+    let duration = 10
+    let engine = AudioEngine()
+    var bigBuffer: AVAudioPCMBuffer? = nil
+
+    for _ in 0 ..< duration {
+
+        let node = factory()
+        engine.output = node
+
+        if let node = node as? Toggleable {
+            node.start()
+        }
+
+        let audio = engine.startTest(totalDuration: 1.0)
+        setParams(node: node, rng: rng)
+        audio.append(engine.render(duration: 1.0))
+
+        if bigBuffer == nil {
+            bigBuffer = AVAudioPCMBuffer(pcmFormat: audio.format, frameCapacity: audio.frameLength*UInt32(duration))
+        }
+
+        bigBuffer?.append(audio)
+
+    }
+
+    return bigBuffer!
+
+}
+
