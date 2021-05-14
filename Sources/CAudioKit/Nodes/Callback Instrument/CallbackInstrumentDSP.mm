18b55960b7048fa9f8742061ad3c0842349150b2
Taylor Holliday <taylor@audulus.com>
2021-05-14 08:40:46 -0700

    cleanup: messageCount was always 1

--- Sources/CAudioKit/Nodes/Callback Instrument/CallbackInstrumentDSP.mm
+++ Sources/CAudioKit/Nodes/Callback Instrument/CallbackInstrumentDSP.mm
@@ -43,15 +43,11 @@ public:
 
     void consumer() {
         midiBuffer.popAll([this] (const AUMIDIEvent& event) {
-            if(event.length > 0 && callback) {
-                int32_t messageCount = sizeof(event.data) / 3;
-
-                for(int messageIndex=0; messageIndex < messageCount; ++messageIndex) {
-                    uint8_t status = event.data[3*messageIndex];
-                    uint8_t data1 = event.data[3*messageIndex+1];
-                    uint8_t data2 = event.data[3*messageIndex+2];
-                    callback(status, data1, data2);
-                }
+            if(event.length == 3 && callback) {
+                uint8_t status = event.data[0];
+                uint8_t data1 = event.data[1];
+                uint8_t data2 = event.data[2];
+                callback(status, data1, data2);
             }
         });
     }
