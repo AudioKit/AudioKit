fc31e643574356a343f48f99558b9d58a5ff941a
Taylor Holliday <taylor@audulus.com>
2021-05-13 21:21:46 -0700

    update from template

--- Sources/AudioKit/Nodes/Generators/Oscillators/PWMOscillator.swift
+++ Sources/AudioKit/Nodes/Generators/Oscillators/PWMOscillator.swift
@@ -12,10 +12,7 @@ import CAudioKit
 /// 
 public class PWMOscillator: Node {
 
-    /// Connected nodes
     public var connections: [Node] { [] }
-
-    /// Underlying AVAudioNode
     public var avAudioNode = instantiate(instrument: "pwmo")
 
     /// Specification details for frequency
