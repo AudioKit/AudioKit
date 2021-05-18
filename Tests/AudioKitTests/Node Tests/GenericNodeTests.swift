c14018dd8079a2e8fd74730b46e486edfbc99a89
Aurelius Prochazka <aurephd@gmail.com>
2021-05-17 20:11:15 -0700

    Adding more generic tests

--- Tests/AudioKitTests/Node Tests/GenericNodeTests.swift
+++ Tests/AudioKitTests/Node Tests/GenericNodeTests.swift
@@ -119,6 +119,36 @@ class GenericNodeTests: XCTestCase {
         input.start()
         nodeParameterTest(md5: "1075bfbdd871ae8fd4b9953b93a48438", factory: { AutoPanner(input, waveform: Table(.triangle)) })
         nodeParameterTest(md5: "4a63f96ea20794d24273a43b4d9f01ac", factory: { AutoWah(input) })
+        nodeParameterTest(md5: "dc0b9cda4374c77307305ac0e6274526", factory: { BitCrusher(input) })
+        nodeParameterTest(md5: "5dce42089c2cdfdc25af39026ea2fcf6", factory: { Clipper(input) })
+        nodeParameterTest(md5: "d998be5c79d0f96513a698a5dfdda9f3", factory: { Compressor(input) })
         nodeParameterTest(md5: "f893b3a2e1334c34d1fa6704b29cb35b", factory: { CostelloReverb(input) })
+    }
+    
+    func testFilters() {
+        let input = Oscillator(waveform: Table(.triangle))
+        input.start()
+        nodeParameterTest(md5: "25087938497d8c0bf5f38ea1f563612f", factory: { BandPassButterworthFilter(input) })
+        nodeParameterTest(md5: "8e64777705cd5f8e3e88780abcc8c3a6", factory: { BandRejectButterworthFilter(input) })
+        nodeParameterTest(md5: "ac25d953bf2dc1a16ab46ecc150b4158", factory: { EqualizerFilter(input) })
+        nodeParameterTest(md5: "cdb62e633e7a4ce6cc58aca1a2586bde", factory: { FormantFilter(input) })
+        nodeParameterTest(md5: "f83fcc5a97ed48a43b0547ccb3403b00", factory: { HighPassButterworthFilter(input) })
+        nodeParameterTest(md5: "2aa558acdf5d8f405f75a25f56ad57bf", factory: { HighPassFilter(input) })
+        nodeParameterTest(md5: "b2417fe1f8b5e50f8a5edb828295a56d", factory: { HighShelfFilter(input) })
+        nodeParameterTest(md5: "84c316c2c9a68b396b395715027e7655", factory: { HighShelfParametricEqualizerFilter(input) })
+        nodeParameterTest(md5: "7af996d0eac52734b0eb91fe52a5acd0", factory: { KorgLowPassFilter(input) })
+        nodeParameterTest(md5: "614d21e487bb7fae7085c34c15916449", factory: { LowPassButterworthFilter(input) })
+        nodeParameterTest(md5: "22571d6861e9b938c956b3b0349c8824", factory: { LowPassFilter(input) })
+        nodeParameterTest(md5: "294a562a44f5cf972543667de71a990e", factory: { LowShelfFilter(input) })
+        nodeParameterTest(md5: "02ae0a77f57ed63bc2bd7ca9662b15d6", factory: { LowShelfParametricEqualizerFilter(input) })
+        nodeParameterTest(md5: "1653597d53066533321351455644f07a", factory: { ModalResonanceFilter(input) })
+        nodeParameterTest(md5: "3f11726b6b3a8002e3216e30f9a27893", factory: { MoogLadder(input) })
+        nodeParameterTest(md5: "8777004f0ddcbcd025980fc56453ce00", factory: { PeakingParametricEqualizerFilter(input) })
+        nodeParameterTest(md5: "ea3c5cad1401b2e7a54a244ef11fe37d", factory: { ResonantFilter(input) })
+        nodeParameterTest(md5: "ff6a59faaca20d7f07f0f1d02989e471", factory: { RolandTB303Filter(input) })
+        nodeParameterTest(md5: "9511d8c2459250d4008d5e56ceabc743", factory: { ThreePoleLowpassFilter(input) })
+        nodeParameterTest(md5: "12b4066b7ea32dd9ab1cac98bb827d12", factory: { ToneComplementFilter(input) })
+        nodeParameterTest(md5: "e7265f68f6ce6480f915408b8e898b72", factory: { ToneFilter(input) })
+
     }
 }
