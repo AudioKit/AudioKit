e0a1b59fcd40757069d840ccb02a109f05dd0bfc
Aurelius Prochazka <aurephd@gmail.com>
2021-05-18 23:57:05 -0700

    cleanup

--- Tests/AudioKitTests/Operation Tests/GenericOperationTests.swift
+++ Tests/AudioKitTests/Operation Tests/GenericOperationTests.swift
@@ -5,7 +5,7 @@ import Foundation
 import XCTest
 
 class GenericOperationTests: XCTestCase {
-    func defaultTest(md5: String, operation: AudioKit.Operation, audition: Bool = false) {
+    func defaultTest(_ md5: String, _ operation: AudioKit.Operation, audition: Bool = false) {
         let engine = AudioEngine()
         let generator = OperationGenerator { operation }
         engine.output = generator
@@ -19,41 +19,39 @@ class GenericOperationTests: XCTestCase {
     }
 
     func testDefaultGenerators() {
-        defaultTest(md5: "e82a86ae4e7d47f24eeba9700e4745d4", operation: Operation.fmOscillator())
-        defaultTest(md5: "f4cc261bdf98ae17320f9561941c8664", operation: Operation.morphingOscillator())
-        defaultTest(md5: "3828cd394361df9739360d1b910516cf", operation: Operation.phasor())
-        defaultTest(md5: "79e0b102124e0b7521fb277d3f8d27f9", operation: Operation.pinkNoise())
-        defaultTest(md5: "379388bf41f4ece5cf274bf53f270c46", operation: Operation.sawtooth())
-        defaultTest(md5: "2a5f4c75768a09c068f2c27377142aa7", operation: Operation.sawtoothWave())
-        defaultTest(md5: "91ec96732b1d1d40a585b6415eef8b51", operation: Operation.sineWave())
-        defaultTest(md5: "9e7c2af4b9e70a73ca7d8453d59d6953", operation: Operation.square())
-        defaultTest(md5: "9d4465fdcff811f568807c43e41859e1", operation: Operation.squareWave())
-        defaultTest(md5: "fc235d00451be0893bb69d971ce2832f", operation: Operation.triangle())
-        defaultTest(md5: "c95b04e9e39e47f3f26a8b9c96c7fe0e", operation: Operation.triangleWave())
-        defaultTest(md5: "5b3296351674e4e4d3a5cca9a1bf355f", operation: Operation.vocalTract())
-        defaultTest(md5: "bbe4898f76a6bd42a573f618ba831372", operation: Operation.whiteNoise())
+        defaultTest("e82a86ae4e7d47f24eeba9700e4745d4", Operation.fmOscillator())
+        defaultTest("f4cc261bdf98ae17320f9561941c8664", Operation.morphingOscillator())
+        defaultTest("3828cd394361df9739360d1b910516cf", Operation.phasor())
+        defaultTest("79e0b102124e0b7521fb277d3f8d27f9", Operation.pinkNoise())
+        defaultTest("379388bf41f4ece5cf274bf53f270c46", Operation.sawtooth())
+        defaultTest("2a5f4c75768a09c068f2c27377142aa7", Operation.sawtoothWave())
+        defaultTest("91ec96732b1d1d40a585b6415eef8b51", Operation.sineWave())
+        defaultTest("9e7c2af4b9e70a73ca7d8453d59d6953", Operation.square())
+        defaultTest("9d4465fdcff811f568807c43e41859e1", Operation.squareWave())
+        defaultTest("fc235d00451be0893bb69d971ce2832f", Operation.triangle())
+        defaultTest("c95b04e9e39e47f3f26a8b9c96c7fe0e", Operation.triangleWave())
+        defaultTest("5b3296351674e4e4d3a5cca9a1bf355f", Operation.vocalTract())
+        defaultTest("bbe4898f76a6bd42a573f618ba831372", Operation.whiteNoise())
     }
     
     func testDefaultEffects() {
         let input = Operation.triangle()
-        defaultTest(md5: "0cf9c4cd7a70f48e31b323540f365709", operation: input.bitCrush() )
-        defaultTest(md5: "e2996e3be4916978068370badfb7e0e6", operation: input.clip() )
-        //defaultTest(md5: "fc235d00451be0893bb69d971ce2832f", operation: input.dcBlock())
-        //defaultTest(md5: "fc235d00451be0893bb69d971ce2832f", operation: input.delay())
-        defaultTest(md5: "fa9cc80070670c9197077f7c99a941a9", operation: input.distort())
-        defaultTest(md5: "9eddbd3f55e0d1502117867c3f123b4f", operation: input.highPassButterworthFilter())
-        defaultTest(md5: "eb229d6421f9d10f7f67c1ee7552645e", operation: input.highPassFilter())
-        defaultTest(md5: "403984fd06d667882c00d1095fd5a049", operation: input.korgLowPassFilter())
-        defaultTest(md5: "e98ecf679fb2719032547edbd4698922", operation: input.lowPassButterworthFilter() )
-        defaultTest(md5: "f8cf4d107fdf86d414d9443c1004f545", operation: input.lowPassFilter())
-        defaultTest(md5: "610d75428245fb0f61ab2a6c70e38a90", operation: input.modalResonanceFilter())
-        defaultTest(md5: "cd808ced1a67801fda02a115e16aea18", operation: input.moogLadderFilter() )
-        defaultTest(md5: "2492706f3ebaaf2ea56e2276bedab140", operation: input.resonantFilter() )
-        defaultTest(md5: "4fe7d3c8a545ef1977902c803bd8e780", operation: input.reverberateWithChowning() )
-        defaultTest(md5: "34a8432a564417a01f17ee2806fa62fa", operation: input.reverberateWithCombFilter() )
-        defaultTest(md5: "18d5019f893191ba5e51e6621b022383", operation: input.reverberateWithFlatFrequencyResponse() )
-        defaultTest(md5: "7c73ff68ff48d4e81d4443619a75e81d", operation: input.stringResonator() )
-        defaultTest(md5: "671a9fa25803283c9b7df893bf78c147", operation: input.threePoleLowPassFilter() )
+        defaultTest("0cf9c4cd7a70f48e31b323540f365709", input.bitCrush() )
+        defaultTest("e2996e3be4916978068370badfb7e0e6", input.clip() )
+        defaultTest("fa9cc80070670c9197077f7c99a941a9", input.distort())
+        defaultTest("9eddbd3f55e0d1502117867c3f123b4f", input.highPassButterworthFilter())
+        defaultTest("eb229d6421f9d10f7f67c1ee7552645e", input.highPassFilter())
+        defaultTest("403984fd06d667882c00d1095fd5a049", input.korgLowPassFilter())
+        defaultTest("e98ecf679fb2719032547edbd4698922", input.lowPassButterworthFilter() )
+        defaultTest("f8cf4d107fdf86d414d9443c1004f545", input.lowPassFilter())
+        defaultTest("610d75428245fb0f61ab2a6c70e38a90", input.modalResonanceFilter())
+        defaultTest("cd808ced1a67801fda02a115e16aea18", input.moogLadderFilter() )
+        defaultTest("2492706f3ebaaf2ea56e2276bedab140", input.resonantFilter() )
+        defaultTest("4fe7d3c8a545ef1977902c803bd8e780", input.reverberateWithChowning() )
+        defaultTest("34a8432a564417a01f17ee2806fa62fa", input.reverberateWithCombFilter() )
+        defaultTest("18d5019f893191ba5e51e6621b022383", input.reverberateWithFlatFrequencyResponse() )
+        defaultTest("7c73ff68ff48d4e81d4443619a75e81d", input.stringResonator() )
+        defaultTest("671a9fa25803283c9b7df893bf78c147", input.threePoleLowPassFilter() )
     }
 
 
