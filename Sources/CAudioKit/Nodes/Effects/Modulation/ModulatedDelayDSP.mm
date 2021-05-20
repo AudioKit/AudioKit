d66507c2a58e71021edd095c35eccb476d0aec10
Taylor Holliday <taylor@audulus.com>
2021-05-19 20:16:11 -0700

    use process2

--- Sources/CAudioKit/Nodes/Effects/Modulation/ModulatedDelayDSP.mm
+++ Sources/CAudioKit/Nodes/Effects/Modulation/ModulatedDelayDSP.mm
@@ -57,7 +57,7 @@ public:
 
     void deinit() override;
 
-    void process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset) override;
+    void process2(FrameRange range) override;
 };
 
 ModulatedDelayDSP::ModulatedDelayDSP(ModulatedDelayType type)
@@ -83,27 +83,27 @@ void ModulatedDelayDSP::deinit()
 
 #define CHUNKSIZE 8     // defines ramp interval
 
-void ModulatedDelayDSP::process(AUAudioFrameCount frameCount, AUAudioFrameCount bufferOffset)
+void ModulatedDelayDSP::process2(FrameRange range)
 {
     float *inBuffers[2], *outBuffers[2];
-    inBuffers[0]  = (float *)inputBufferLists[0]->mBuffers[0].mData  + bufferOffset;
-    inBuffers[1]  = (float *)inputBufferLists[0]->mBuffers[1].mData  + bufferOffset;
-    outBuffers[0] = (float *)outputBufferList->mBuffers[0].mData + bufferOffset;
-    outBuffers[1] = (float *)outputBufferList->mBuffers[1].mData + bufferOffset;
+    inBuffers[0]  = (float *)inputBufferLists[0]->mBuffers[0].mData  + range.start;
+    inBuffers[1]  = (float *)inputBufferLists[0]->mBuffers[1].mData  + range.start;
+    outBuffers[0] = (float *)outputBufferList->mBuffers[0].mData + range.start;
+    outBuffers[1] = (float *)outputBufferList->mBuffers[1].mData + range.start;
     unsigned channelCount = outputBufferList->mNumberBuffers;
 
     if (!isStarted)
     {
         // effect bypassed: just copy input to output
-        memcpy(outBuffers[0], inBuffers[0], frameCount * sizeof(float));
+        memcpy(outBuffers[0], inBuffers[0], range.count * sizeof(float));
         if (channelCount > 0)
-            memcpy(outBuffers[1], inBuffers[1], frameCount * sizeof(float));
+            memcpy(outBuffers[1], inBuffers[1], range.count * sizeof(float));
         return;
     }
 
     // process in chunks of maximum length CHUNKSIZE
-    for (int frameIndex = 0; frameIndex < frameCount; frameIndex += CHUNKSIZE) {
-        int chunkSize = frameCount - frameIndex;
+    for (int frameIndex = 0; frameIndex < range.count; frameIndex += CHUNKSIZE) {
+        int chunkSize = range.count - frameIndex;
         if (chunkSize > CHUNKSIZE) chunkSize = CHUNKSIZE;
 
         // ramp parameters
