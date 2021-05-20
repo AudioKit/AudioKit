fa965309953bb538121dabfefc6311a3f45c2c00
Aurelius Prochazka <aurephd@gmail.com>
2021-05-20 13:25:30 -0700

    Updated tap tests to not use operations

--- Tests/AudioKitTests/Analysis Tests/AmplitudeTapTests.swift
+++ Tests/AudioKitTests/Analysis Tests/AmplitudeTapTests.swift
@@ -1,160 +1,148 @@
-//// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
-//
-//import AudioKit
-//import XCTest
-//
-//class AmplitudeTapTests: XCTestCase {
-//
-//    func check(values: [Float], known: [Float]) {
-//        XCTAssertGreaterThan(values.count, known.count)
-//        if values.count > known.count {
-//            for i in 0..<known.count {
-//                XCTAssertEqual(values[i], known[i], accuracy: 0.001)
-//            }
-//        }
-//    }
-//
-//    func testDefault() {
-//
-//        let engine = AudioEngine()
-//
-//        var amplitudes: [Float] = []
-//
-//        let sine = OperationGenerator {
-//            let amplitude = Operation.sineWave(frequency: 0.25, amplitude: 1)
-//            return Operation.sineWave() * amplitude }
-//
-//        engine.output = sine
-//        sine.start()
-//
-//        let expect = expectation(description: "wait for amplitudes")
-//
-//        let tap = AmplitudeTap(sine) { amp in
-//            amplitudes.append(amp)
-//
-//            if amplitudes.count == 10 {
-//                expect.fulfill()
-//            }
-//        }
-//        tap.start()
-//
-//        let audio = engine.startTest(totalDuration: 1.0)
-//        audio.append(engine.render(duration: 1.0))
-//        testMD5(audio)
-//
-//        wait(for: [expect], timeout: 5.0)
-//
-//        let knownValues: [Float] = [0.01478241, 0.03954828, 0.06425185, 0.09090047, 0.11480384,
-//                                    0.14164367, 0.16560285, 0.19081590, 0.21635467, 0.23850754]
-//        check(values: amplitudes, known: knownValues)
-//    }
-//
-//    func testLeftStereoMode() {
-//
-//        let engine = AudioEngine()
-//
-//        var amplitudes: [Float] = []
-//
-//        let sine = OperationGenerator {
-//            let amplitude = Operation.sineWave(frequency: 0.25, amplitude: 1)
-//            return Operation.sineWave() * amplitude }
-//
-//        engine.output = sine
-//        sine.start()
-//
-//        let expect = expectation(description: "wait for amplitudes")
-//
-//        let tap = AmplitudeTap(sine) { amp in
-//            amplitudes.append(amp)
-//
-//            if amplitudes.count == 10 {
-//                expect.fulfill()
-//            }
-//        }
-//        tap.stereoMode = .left
-//        tap.start()
-//
-//        let audio = engine.startTest(totalDuration: 1.0)
-//        audio.append(engine.render(duration: 1.0))
-//        testMD5(audio)
-//
-//        wait(for: [expect], timeout: 5.0)
-//
-//        let knownValues: [Float] = [0.01478241, 0.03954828, 0.06425185, 0.09090047, 0.11480384,
-//                                    0.14164367, 0.16560285, 0.19081590, 0.21635467, 0.23850754]
-//        check(values: amplitudes, known: knownValues)
-//    }
-//
-//    func testRightStereoMode() {
-//
-//        let engine = AudioEngine()
-//
-//        var amplitudes: [Float] = []
-//
-//        let sine = OperationGenerator {
-//            let amplitude = Operation.sineWave(frequency: 0.25, amplitude: 1)
-//            return Operation.sineWave() * amplitude }
-//
-//        engine.output = sine
-//        sine.start()
-//
-//        let expect = expectation(description: "wait for amplitudes")
-//
-//        let tap = AmplitudeTap(sine) { amp in
-//            amplitudes.append(amp)
-//
-//            if amplitudes.count == 10 {
-//                expect.fulfill()
-//            }
-//        }
-//        tap.stereoMode = .right
-//        tap.start()
-//
-//        let audio = engine.startTest(totalDuration: 1.0)
-//        audio.append(engine.render(duration: 1.0))
-//        testMD5(audio)
-//
-//        wait(for: [expect], timeout: 5.0)
-//
-//        let knownValues: [Float] = [0.01478241, 0.03954828, 0.06425185, 0.09090047, 0.11480384,
-//                                    0.14164367, 0.16560285, 0.19081590, 0.21635467, 0.23850754]
-//        check(values: amplitudes, known: knownValues)
-//    }
-//
-//    func testPeakAnalysisMode() {
-//
-//        let engine = AudioEngine()
-//
-//        var amplitudes: [Float] = []
-//
-//        let sine = OperationGenerator {
-//            let amplitude = Operation.sineWave(frequency: 0.25, amplitude: 1)
-//            return Operation.sineWave() * amplitude }
-//
-//        engine.output = sine
-//        sine.start()
-//
-//        let expect = expectation(description: "wait for amplitudes")
-//
-//        let tap = AmplitudeTap(sine) { amp in
-//            amplitudes.append(amp)
-//
-//            if amplitudes.count == 10 {
-//                expect.fulfill()
-//            }
-//        }
-//        tap.analysisMode = .peak
-//        tap.start()
-//
-//        let audio = engine.startTest(totalDuration: 1.0)
-//        audio.append(engine.render(duration: 1.0))
-//        testMD5(audio)
-//
-//        wait(for: [expect], timeout: 5.0)
-//
-//        let knownValues: [Float] = [0.03505735, 0.07213809, 0.10766032, 0.1430245, 0.17815503,
-//                                    0.2166785, 0.251323, 0.2855623, 0.3196378, 0.3532541]
-//        check(values: amplitudes, known: knownValues)
-//    }
-//
-//}
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
+
+import AudioKit
+import XCTest
+
+class AmplitudeTapTests: XCTestCase {
+
+    func check(values: [Float], known: [Float]) {
+        XCTAssertGreaterThan(values.count, known.count)
+        if values.count > known.count {
+            for i in 0..<known.count {
+                XCTAssertEqual(values[i], known[i], accuracy: 0.001)
+            }
+        }
+    }
+
+    func testDefault() {
+
+        let engine = AudioEngine()
+
+        var amplitudes: [Float] = []
+
+        let oscillator = Oscillator(amplitude: 0.0)
+        engine.output = oscillator
+        oscillator.start()
+        oscillator.$amplitude.ramp(to: 1.0, duration: 1.0)
+
+        let expect = expectation(description: "wait for amplitudes")
+
+        let tap = AmplitudeTap(oscillator) { amp in
+            amplitudes.append(amp)
+
+            if amplitudes.count == 10 {
+                expect.fulfill()
+            }
+        }
+        tap.start()
+
+        let audio = engine.startTest(totalDuration: 1.0)
+        audio.append(engine.render(duration: 1.0))
+        testMD5(audio)
+
+        wait(for: [expect], timeout: 5.0)
+
+        let knownValues: [Float] = [0.00942282, 0.025220972, 0.041011177, 0.058098152, 0.07350557, 0.0908917, 0.10654987, 0.12315354, 0.14013591, 0.15510349, 0.17372803, 0.18744196, 0.20658945, 0.2207793, 0.23839833, 0.2550281, 0.26965025, 0.28930312, 0.30145812, 0.32247952, 0.33476913, 0.35403505, 0.36954698, 0.384581, 0.40462023, 0.41562736, 0.43839857, 0.44859052, 0.46998367, 0.48368293, 0.49992698, 0.5195799, 0.53006244, 0.55421007, 0.5623595, 0.58614916, 0.5974768, 0.61569136]
+        check(values: amplitudes, known: knownValues)
+    }
+
+    func testLeftStereoMode() {
+
+        let engine = AudioEngine()
+
+        var amplitudes: [Float] = []
+
+        let oscillator = Oscillator(amplitude: 0.0)
+        engine.output = oscillator
+        oscillator.start()
+        oscillator.$amplitude.ramp(to: 1.0, duration: 1.0)
+
+        let expect = expectation(description: "wait for amplitudes")
+
+        let tap = AmplitudeTap(oscillator) { amp in
+            amplitudes.append(amp)
+
+            if amplitudes.count == 10 {
+                expect.fulfill()
+            }
+        }
+        tap.stereoMode = .left
+        tap.start()
+
+        let audio = engine.startTest(totalDuration: 1.0)
+        audio.append(engine.render(duration: 1.0))
+        testMD5(audio)
+
+        wait(for: [expect], timeout: 5.0)
+
+        let knownValues: [Float] = [0.00942282, 0.025220972, 0.041011177, 0.058098152, 0.07350557, 0.0908917, 0.10654987, 0.12315354, 0.14013591, 0.15510349, 0.17372803, 0.18744196, 0.20658945, 0.2207793, 0.23839833, 0.2550281, 0.26965025, 0.28930312, 0.30145812, 0.32247952, 0.33476913, 0.35403505, 0.36954698, 0.384581, 0.40462023, 0.41562736, 0.43839857, 0.44859052, 0.46998367, 0.48368293, 0.49992698, 0.5195799, 0.53006244, 0.55421007, 0.5623595, 0.58614916, 0.5974768, 0.61569136]
+        check(values: amplitudes, known: knownValues)
+    }
+
+    func testRightStereoMode() {
+
+        let engine = AudioEngine()
+
+        var amplitudes: [Float] = []
+
+        let oscillator = Oscillator(amplitude: 0.0)
+        engine.output = oscillator
+        oscillator.start()
+        oscillator.$amplitude.ramp(to: 1.0, duration: 1.0)
+
+        let expect = expectation(description: "wait for amplitudes")
+
+        let tap = AmplitudeTap(oscillator) { amp in
+            amplitudes.append(amp)
+
+            if amplitudes.count == 10 {
+                expect.fulfill()
+            }
+        }
+        tap.stereoMode = .right
+        tap.start()
+
+        let audio = engine.startTest(totalDuration: 1.0)
+        audio.append(engine.render(duration: 1.0))
+        testMD5(audio)
+
+        wait(for: [expect], timeout: 5.0)
+
+        let knownValues: [Float] = [0.00942282, 0.025220972, 0.041011177, 0.058098152, 0.07350557, 0.0908917, 0.10654987, 0.12315354, 0.14013591, 0.15510349, 0.17372803, 0.18744196, 0.20658945, 0.2207793, 0.23839833, 0.2550281, 0.26965025, 0.28930312, 0.30145812, 0.32247952, 0.33476913, 0.35403505, 0.36954698, 0.384581, 0.40462023, 0.41562736, 0.43839857, 0.44859052, 0.46998367, 0.48368293, 0.49992698, 0.5195799, 0.53006244, 0.55421007, 0.5623595, 0.58614916, 0.5974768, 0.61569136]
+        check(values: amplitudes, known: knownValues)
+    }
+
+    func testPeakAnalysisMode() {
+
+        let engine = AudioEngine()
+
+        var amplitudes: [Float] = []
+
+        let oscillator = Oscillator(amplitude: 0.0)
+        engine.output = oscillator
+        oscillator.start()
+        oscillator.$amplitude.ramp(to: 1.0, duration: 1.0)
+
+        let expect = expectation(description: "wait for amplitudes")
+
+        let tap = AmplitudeTap(oscillator) { amp in
+            amplitudes.append(amp)
+
+            if amplitudes.count == 10 {
+                expect.fulfill()
+            }
+        }
+        tap.analysisMode = .peak
+        tap.start()
+
+        let audio = engine.startTest(totalDuration: 1.0)
+        audio.append(engine.render(duration: 1.0))
+        testMD5(audio)
+
+        wait(for: [expect], timeout: 5.0)
+
+        let knownValues: [Float] = [0.022348743, 0.04601721, 0.06875052, 0.09147034, 0.11415692, 0.13920411, 0.16191694, 0.18457419, 0.20736109, 0.23011333, 0.25499576, 0.27781528, 0.3005661, 0.32322842, 0.34783408, 0.37101626, 0.3936486, 0.41636539, 0.4391955, 0.46405852, 0.48682672, 0.50965476, 0.53233904, 0.55485994, 0.5801122, 0.6027659, 0.62528473, 0.64823896, 0.6731836, 0.69575673, 0.71870834, 0.741461, 0.763995, 0.7891771, 0.81189847, 0.83437896, 0.85721564, 0.8801084]
+        check(values: amplitudes, known: knownValues)
+    }
+
+}
--- Tests/AudioKitTests/Analysis Tests/PitchTapTests.swift
+++ Tests/AudioKitTests/Analysis Tests/PitchTapTests.swift
@@ -1,45 +1,41 @@
-//// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
-//
-//import AudioKit
-//import XCTest
-//
-//class PitchTapTests: XCTestCase {
-//
-//    func testBasic() {
-//        let engine = AudioEngine()
-//
-//        let sine = OperationGenerator {
-//            let s = Operation.sawtooth(frequency: 0.25, amplitude: 1, phase: 0) + 2
-//            return Operation.sineWave(frequency: 440 * s, amplitude: 1)
-//        }
-//
-//        sine.start()
-//
-//        var pitches: [Float] = []
-//        let knownValues: [Float] = [100.0, 447.32297, 455.59183, 481.56384, 497.71292, 519.39923, 542.7518, 555.37006, 583.9163, 602.96344]
-//
-//        engine.output = sine
-//
-//        let expect = expectation(description: "wait for amplitudes")
-//
-//        let tap = PitchTap(sine) {  (tapPitches, _) in
-//            pitches.append(tapPitches[0])
-//
-//            if pitches.count == knownValues.count {
-//                expect.fulfill()
-//            }
-//        }
-//        tap.start()
-//
-//        let audio = engine.startTest(totalDuration: 1.0)
-//        audio.append(engine.render(duration: 1.0))
-//        testMD5(audio)
-//
-//        wait(for: [expect], timeout: 5.0)
-//
-//        for i in 0..<knownValues.count {
-//            XCTAssertEqual(pitches[i], knownValues[i], accuracy: 0.001)
-//        }
-//    }
-//
-//}
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
+
+import AudioKit
+import XCTest
+
+class PitchTapTests: XCTestCase {
+
+    func testBasic() {
+        let engine = AudioEngine()
+
+        let oscillator = Oscillator(frequency: 440)
+        engine.output = oscillator
+        oscillator.start()
+        oscillator.$frequency.ramp(to: 660, duration: 1.0)
+
+        var pitches: [Float] = []
+        let knownValues: [Float] = [100.0, 447.32437, 455.5944, 481.5628, 497.71658, 517.4379, 542.6506, 555.4854, 581.54614, 597.6265]
+
+        let expect = expectation(description: "wait for amplitudes")
+
+        let tap = PitchTap(oscillator) {  (tapPitches, _) in
+            pitches.append(tapPitches[0])
+
+            if pitches.count == knownValues.count {
+                expect.fulfill()
+            }
+        }
+        tap.start()
+
+        let audio = engine.startTest(totalDuration: 1.0)
+        audio.append(engine.render(duration: 1.0))
+        testMD5(audio)
+
+        wait(for: [expect], timeout: 5.0)
+
+        for i in 0..<knownValues.count {
+            XCTAssertEqual(pitches[i], knownValues[i], accuracy: 0.001)
+        }
+    }
+
+}
