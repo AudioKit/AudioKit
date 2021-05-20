02fcf737edf1cea61bfbbf9503baa3be55a25fc3
Taylor Holliday <taylor@audulus.com>
2021-05-19 19:45:41 -0700

    remove import

--- Sources/AudioKit/Nodes/Playback/Samplers/Apple Sampler/PresetBuilder.swift
+++ Sources/AudioKit/Nodes/Playback/Samplers/Apple Sampler/PresetBuilder.swift
@@ -1,7 +1,6 @@
 // Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
 
 import AVFoundation
-import CAudioKit
 
 //swiftlint:disable all
 /// Builds presets for Apple sampler to read from
