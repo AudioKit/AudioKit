2e3496ddaa87e5a96214802f109252b926b41ed9
Taylor Holliday <taylor@audulus.com>
2021-05-22 10:17:35 -0700

    cleanup

--- Sources/AudioKit/Nodes/Effects/Reverb.swift
+++ Sources/AudioKit/Nodes/Effects/Reverb.swift
@@ -1,7 +1,6 @@
 // Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
 
 import AVFoundation
-import CAudioKit
 
 /// AudioKit version of Apple's Reverb Audio Unit
 ///
