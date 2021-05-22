1c99ee4160cd250759a72fa55f20de6be42a0150
Aurelius Prochazka <aurephd@gmail.com>
2021-05-21 19:31:40 -0700

    See if CI likes this file

--- Tests/AudioKitTests/Sequencing and Automation Tests/SequencerTrackTests.swift
+++ Tests/AudioKitTests/Sequencing and Automation Tests/SequencerTrackTests.swift
@@ -10,17 +10,23 @@ class SequencerTrackTests: XCTestCase {
     func getTestSequence() -> NoteEventSequence {
         var seq = NoteEventSequence()
         seq.add(noteNumber: 60, position: 0, duration: 0.5)
-        seq.add(noteNumber: 62, position: 1, duration: 0.5)
-        seq.add(noteNumber: 63, position: 2, duration: 0.5)
+        seq.add(noteNumber: 72, position: 1, duration: 0.5)
+        seq.add(noteNumber: 84, position: 2, duration: 0.5)
 
         return seq
     }
 
     func testEmptyTrack() {
         let sampler = AppleSampler()
+        let sampleURL = Bundle.module.url(forResource: "TestResources/sinechirp", withExtension: "wav")!
+        let audioFile = try! AVAudioFile(forReading: sampleURL)
+        try! sampler.loadAudioFile(audioFile)
         let seq = SequencerTrack(targetNode: sampler)
+        let engine = AudioEngine()
+        engine.output = sampler
+        let audio = engine.startTest(totalDuration: 5.0)
+        audio.append(engine.render(duration: 5.0))
         XCTAssertFalse(seq.isPlaying)
-
         XCTAssertEqual(seq.length, 4.0) // One measure
         XCTAssertEqual(seq.loopEnabled, true) // Loop on
     }
@@ -28,6 +34,9 @@ class SequencerTrackTests: XCTestCase {
     func testLoop() {
         let engine = AudioEngine()
         let sampler = AppleSampler()
+        let sampleURL = Bundle.module.url(forResource: "TestResources/sinechirp", withExtension: "wav")!
+        let audioFile = try! AVAudioFile(forReading: sampleURL)
+        try! sampler.loadAudioFile(audioFile)
 
         let track = SequencerTrack(targetNode: sampler)
         engine.output = sampler
@@ -45,6 +54,9 @@ class SequencerTrackTests: XCTestCase {
 
         let engine = AudioEngine()
         let sampler = AppleSampler()
+        let sampleURL = Bundle.module.url(forResource: "TestResources/sinechirp", withExtension: "wav")!
+        let audioFile = try! AVAudioFile(forReading: sampleURL)
+        try! sampler.loadAudioFile(audioFile)
 
         let track = SequencerTrack(targetNode: sampler)
         engine.output = sampler
@@ -62,6 +74,9 @@ class SequencerTrackTests: XCTestCase {
 
         let engine = AudioEngine()
         let sampler = AppleSampler()
+        let sampleURL = Bundle.module.url(forResource: "TestResources/sinechirp", withExtension: "wav")!
+        let audioFile = try! AVAudioFile(forReading: sampleURL)
+        try! sampler.loadAudioFile(audioFile)
 
         let track = SequencerTrack(targetNode: sampler)
         engine.output = sampler
@@ -79,6 +94,9 @@ class SequencerTrackTests: XCTestCase {
 
         let engine = AudioEngine()
         let sampler = AppleSampler()
+        let sampleURL = Bundle.module.url(forResource: "TestResources/sinechirp", withExtension: "wav")!
+        let audioFile = try! AVAudioFile(forReading: sampleURL)
+        try! sampler.loadAudioFile(audioFile)
 
         let track = SequencerTrack(targetNode: sampler)
         engine.output = sampler
