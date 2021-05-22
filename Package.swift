18509cd16b66849fe47057322d617ffa7f2facd0
Aurelius Prochazka <aurephd@gmail.com>
2021-05-21 17:21:40 -0700

    Remove exclude lines

--- Package.swift
+++ Package.swift
@@ -23,18 +23,11 @@ let package = Package(
             name: "CAudioKit",
             dependencies: ["Soundpipe"],
             exclude: [
-                "AudioKitCore/Modulated Delay/README.md",
-                "AudioKitCore/Sampler/Wavpack/license.txt",
-                "AudioKitCore/Common/README.md",
                 "Nodes/Effects/DiodeClipper.soul",
-                "AudioKitCore/Common/Envelope.hpp",
-                "AudioKitCore/Sampler/README.md",
-                "AudioKitCore/README.md",
             ],
             publicHeadersPath: "include",
             cxxSettings: [
                 .headerSearchPath("Internals"),
-                .headerSearchPath("AudioKitCore/Common"),
                 .headerSearchPath("Devoloop/include"),
                 .headerSearchPath(".")
             ]
@@ -44,21 +37,13 @@ let package = Package(
             dependencies: ["CAudioKit"],
             exclude: [
                 "Internals/Table/README.md",
-                "Nodes/Playback/Samplers/Sampler/Sampler.md",
-                "Nodes/Playback/Samplers/Apple Sampler/AppleSamplerNotes.md",
-                "Nodes/Playback/Samplers/Samplers.md",
-                "Nodes/Playback/Samplers/Sampler/README.md",
                 "Nodes/Effects/Guitar Processors/README.md",
                 "Nodes/Playback/Samplers/PreparingSampleSets.md",
                 "Internals/README.md",
                 "Nodes/Effects/Modulation/ModDelay.svg",
                 "MIDI/README.md",
                 "Taps/README.md",
-                "Nodes/Effects/Modulation/ModulatedDelayEffects.md",
-                "Nodes/Effects/Modulation/README.md",
-                "Nodes/Playback/Samplers/Apple Sampler/Skeleton.aupreset",
                 "Nodes/Effects/README.md",
-                "Operations/README.md",
                 "Nodes/README.md",
             ]),
         .testTarget(
