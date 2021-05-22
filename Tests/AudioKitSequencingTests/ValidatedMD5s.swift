d3a8921df28a9c5983912dbd8cd20c1c8e63c415
Taylor Holliday <taylor@audulus.com>
2021-05-22 09:56:56 -0700

    got hounded

--- Tests/AudioKitSequencingTests/ValidatedMD5s.swift
+++ Tests/AudioKitSequencingTests/ValidatedMD5s.swift
@@ -14,5 +14,5 @@ let validatedMD5s: [String: String] = [
     "-[SequencerTrackTests testChangeTempo]": "3e05405bead660d36ebc9080920a6c1e",
     "-[SequencerTrackTests testLoop]": "3a7ebced69ddc6669932f4ee48dabe2b",
     "-[SequencerTrackTests testOneShot]": "3fbf53f1139a831b3e1a284140c8a53c",
-    "-[SequencerTrackTests testTempo]": "1eb7efc6ea54eafbe616dfa8e1a3ef36",
+    "-[SequencerTrackTests testTempo]": "1eb7efc6ea54eafbe616dfa8e1a3ef36"
 ]
