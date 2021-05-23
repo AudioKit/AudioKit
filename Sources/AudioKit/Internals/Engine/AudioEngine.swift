cba097c3c1d1a04594e007abef4672e5ba722dcf
Evan Murray <evanmurray22@gmail.com>
2021-05-22 23:51:19 -0500

    Resolving Split Connections (implemented by fquarters)#2370

--- Sources/AudioKit/Internals/Engine/AudioEngine.swift
+++ Sources/AudioKit/Internals/Engine/AudioEngine.swift
@@ -16,6 +16,32 @@ extension AVAudioNode {
         }
     }
 
+    /// Break all connections, then reconnect to all nodes except target node
+    public func disconnectSplitConnection(from target: AVAudioNode, format: AVAudioFormat? = Settings.audioFormat) {
+        guard let engine = engine else { return }
+        let connections = engine.outputConnectionPoints(for: self, outputBus: 0)
+        let backupConnections = connections.filter { $0.node != .some(target) }
+        engine.disconnectNodeOutput(self, bus: 0)
+        engine.connect(self, to: backupConnections, fromBus: 0, format: format)
+    }
+
+    public func disconnectExpectingSplitConnections(input: AVAudioNode) {
+            guard let engine = engine else { return }
+            let inputTotalConnections = engine.outputConnectionPoints(for: input, outputBus: 0)
+            for bus in 0 ..< numberOfInputs {
+                if let cp = engine.inputConnectionPoint(for: self, inputBus: bus) {
+                    if cp.node === input {
+                        if inputTotalConnections.count > 1 {
+                            input.disconnectSplitConnection(from: self)
+                        } else {
+                           // current implementation goes here straight away
+                            engine.disconnectNodeInput(self, bus: bus)
+                        }
+                    }
+                }
+            }
+        }
+
     /// Make a connection without breaking other connections.
     public func connect(input: AVAudioNode, bus: Int, format: AVAudioFormat? = Settings.audioFormat) {
         if let engine = engine {
