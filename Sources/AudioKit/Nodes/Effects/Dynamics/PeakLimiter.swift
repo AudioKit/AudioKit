cfe55f35b042c0957d0905878157e44eae255f41
Jeff Cooper <jeff@southernworkstx.com>
2021-08-02 11:50:28 -0500

    fix weird line break

--- Sources/AudioKit/Nodes/Effects/Dynamics/PeakLimiter.swift
+++ Sources/AudioKit/Nodes/Effects/Dynamics/PeakLimiter.swift
@@ -7,8 +7,7 @@ import AVFoundation
 ///
 public class PeakLimiter: Node {
 
-    fileprivate let effectAU = AVAudioUnitEffect(appleEffect: kAudioUnitSubType_PeakLimiter
-    )
+    fileprivate let effectAU = AVAudioUnitEffect(appleEffect: kAudioUnitSubType_PeakLimiter)
 
     let input: Node
 
