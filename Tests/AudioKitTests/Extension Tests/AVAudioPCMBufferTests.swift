cbdcc8a5236808e2579d70e13c575c9cfefbcdf4
Taylor Holliday <taylor@audulus.com>
2021-12-05 15:41:02 -0800

    Fix #2584. Demonstrate how to append buffers.

--- Tests/AudioKitTests/Extension Tests/AVAudioPCMBufferTests.swift
+++ Tests/AudioKitTests/Extension Tests/AVAudioPCMBufferTests.swift
@@ -4,10 +4,14 @@ import AudioKit
 import XCTest
 
 class AVAudioPCMBufferTests: XCTestCase {
-  func testAppend() {
-    let path = Bundle.module.url(forResource: "TestResources/drumloop", withExtension: "wav")
-    let file = try? AVAudioFile(forReading: path!)
-    let loopBuffer = file!.toAVAudioPCMBuffer()!
-    XCTAssertNoThrow(loopBuffer.append(file!.toAVAudioPCMBuffer()!))
-  }
+    func testAppend() {
+        let path = Bundle.module.url(forResource: "TestResources/drumloop", withExtension: "wav")
+        let file = try! AVAudioFile(forReading: path!)
+
+        let fileBuffer = file.toAVAudioPCMBuffer()!
+        let loopBuffer = AVAudioPCMBuffer(pcmFormat: fileBuffer.format, frameCapacity: 2 * UInt32(file.length))!
+
+        loopBuffer.append(fileBuffer)
+        XCTAssertNoThrow(loopBuffer.append(fileBuffer))
+    }
 }
