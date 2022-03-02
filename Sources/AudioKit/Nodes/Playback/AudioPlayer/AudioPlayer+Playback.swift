d8002699a67b8230d072cc4eb2c9128967921f6a
Evan Murray <evan@auraaudio.io>
2022-03-02 18:06:01 -0500

    Address audio player reset logic

--- Sources/AudioKit/Nodes/Playback/AudioPlayer/AudioPlayer+Playback.swift
+++ Sources/AudioKit/Nodes/Playback/AudioPlayer/AudioPlayer+Playback.swift
@@ -43,11 +43,9 @@ extension AudioPlayer {
             return
         case .completed:
             // reset the status and play again if isLooping and not buffered
+            status = .stopped
             if isLooping && !isBuffered {
-                status = .stopped
                 play()
-            } else if !isLooping && !isBuffered {
-                status = .stopped
             }
         }
     }
