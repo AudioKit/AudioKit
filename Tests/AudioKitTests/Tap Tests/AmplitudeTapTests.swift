94d6624f8380c0b8557495d2d5f55ed742941cdd
Aurelius Prochazka <aurephd@gmail.com>
2022-06-08 10:26:17 -0700

    Added amplitude tap test

--- Tests/AudioKitTests/Tap Tests/AmplitudeTapTests.swift
+++ Tests/AudioKitTests/Tap Tests/AmplitudeTapTests.swift
@@ -19,4 +19,51 @@ class AmplitudeTapTests: XCTestCase {
 
         XCTAssertFalse(tap.isStarted)
     }
+
+    func check(values: [Float], known: [Float]) {
+        if values.count >= known.count {
+            for i in 0..<known.count {
+                XCTAssertEqual(values[i], 0.579 * known[i], accuracy: 0.01)
+            }
+        }
+        print(values)
+        print(known)
+    }
+
+    func testDefault() {
+
+        let engine = AudioEngine()
+
+        var detectedAmplitudes: [Float] = []
+        let targetAmplitudes: [Float] = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+
+        let noise = PlaygroundNoiseGenerator(amplitude: 0.0)
+        engine.output = noise
+        noise.start()
+
+        let expect = expectation(description: "wait for amplitudes")
+
+        let tap = AmplitudeTap(noise) { amp in
+            if abs(amp - (detectedAmplitudes.last ?? 0.0)) > 0.05 {
+                detectedAmplitudes.append(amp)
+                if detectedAmplitudes.count == 10 {
+                    expect.fulfill()
+                }
+            }
+
+        }
+        tap.start()
+
+        let audio = engine.startTest(totalDuration: 10.0)
+        for amplitude in targetAmplitudes {
+            noise.amplitude = amplitude
+            audio.append(engine.render(duration: 1.0))
+        }
+        wait(for: [expect], timeout: 10.0)
+
+        check(values: detectedAmplitudes, known: targetAmplitudes)
+
+    }
+
+
 }
