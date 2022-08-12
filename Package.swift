e839fce4b19469ddde483dafca769c2f70fc2ad1
Taylor Holliday <taylor@audulus.com>
2022-08-12 14:09:28 -0700

    Need Swift concurrency.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "AudioKit",
-    platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
+    platforms: [.macOS(.v10_15), .iOS(.v11), .tvOS(.v11)],
     products: [.library(name: "AudioKit", targets: ["AudioKit"])],
     targets: [
         .target(name: "AudioKit"),
