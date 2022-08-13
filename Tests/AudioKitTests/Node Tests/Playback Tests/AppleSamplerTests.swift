f3a52f47ac575a05e0693678ec458be7946cff4a
Taylor Holliday <taylor@audulus.com>
2022-08-12 18:46:47 -0700

    Disable intermittent failing tests.

--- Tests/AudioKitTests/Node Tests/Playback Tests/AppleSamplerTests.swift
+++ Tests/AudioKitTests/Node Tests/Playback Tests/AppleSamplerTests.swift
@@ -6,6 +6,7 @@ import XCTest
 
 // Commented out these tests due to intermittent failure on CI
 
+/*
 class AppleSamplerTests: XCTestCase {
     let sampler = AppleSampler()
     let engine = AudioEngine()
@@ -60,7 +61,6 @@ class AppleSamplerTests: XCTestCase {
     }
 
     // Repro case.
-    /*
     func testLoadEXS24_bug() throws {
         let engine = AVAudioEngine()
         let samplerUnit = AVAudioUnitSampler()
@@ -68,5 +68,5 @@ class AppleSamplerTests: XCTestCase {
         let exsURL = Bundle.module.url(forResource: "TestResources/Sampler Instruments/sawPiano1", withExtension: "exs")!
         try samplerUnit.loadInstrument(at: exsURL)
     }
-    */
 }
+*/
