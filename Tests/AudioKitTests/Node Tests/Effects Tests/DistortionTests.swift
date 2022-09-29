e9cd15c347524995da4fd0d0cc6e3117ca4d3828
NickCulbertson <nickculbertson@gmail.com>
2022-09-29 19:01:39 -0400

    Adding to Test

--- Tests/AudioKitTests/Node Tests/Effects Tests/DistortionTests.swift
+++ Tests/AudioKitTests/Node Tests/Effects Tests/DistortionTests.swift
@@ -10,8 +10,7 @@ class DistortionTests: XCTestCase {
         let engine = AudioEngine()
         let url = Bundle.module.url(forResource: "12345", withExtension: "wav", subdirectory: "TestResources")!
         let input = AudioPlayer(url: url)!
-//        engine.output = AppleDistortion(input)
-        engine.output = input
+        engine.output = AppleDistortion(input)
         let audio = engine.startTest(totalDuration: 1.0)
         input.start()
         audio.append(engine.render(duration: 1.0))
