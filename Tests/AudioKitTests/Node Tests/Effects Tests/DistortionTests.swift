cac1a413bc994db4412380c9ef1653423f78fc7d
NickCulbertson <nickculbertson@gmail.com>
2022-09-29 14:02:36 -0400

    Create DistortionTests.swift

---
+++ Tests/AudioKitTests/Node Tests/Effects Tests/DistortionTests.swift
@@ -0,0 +1,35 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
+
+@testable import AudioKit
+import XCTest
+import AVFAudio
+
+class DistortionTests: XCTestCase {
+
+    #if os(iOS)
+    func testDefault() {
+        let engine = AudioEngine()
+        let url = Bundle.module.url(forResource: "12345", withExtension: "wav", subdirectory: "TestResources")!
+        let input = AudioPlayer(url: url)!
+        engine.output = AppleDistortion(input)
+        let audio = engine.startTest(totalDuration: 1.0)
+        input.start()
+        audio.append(engine.render(duration: 1.0))
+        testMD5(audio)
+    }
+
+    func testMultiDistortedCubed() {
+        let engine = AudioEngine()
+        let url = Bundle.module.url(forResource: "12345", withExtension: "wav", subdirectory: "TestResources")!
+        let input = AudioPlayer(url: url)!
+        let effect = AppleDistortion(input)
+        engine.output = effect
+        effect.loadFactoryPreset(.multiDistortedCubed)
+        let audio = engine.startTest(totalDuration: 1.0)
+        input.start()
+        audio.append(engine.render(duration: 1.0))
+        testMD5(audio)
+    }
+    #endif
+
+}
