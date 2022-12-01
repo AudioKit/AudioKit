fec9b93d12b08cf1f59d265a3210521f7abca5d5
Taylor Holliday <wtholliday@gmail.com>
2022-11-30 19:19:04 -0800

    re #2804. Cleanup

--- Sources/AudioKit/Internals/Engine2/AudioEngine2.swift
+++ Sources/AudioKit/Internals/Engine2/AudioEngine2.swift
@@ -9,69 +9,73 @@ public class AudioEngine2 {
     
     public var output: Node? {
         didSet {
-            // Traverse the node graph to schedule
-            // audio units.
-            
-            if let output = output {
-                
-                // Generate a new schedule of AUs.
-                var scheduled = Set<ObjectIdentifier>()
-                var list: [Node] = []
-                
-                schedule(node: output, scheduled: &scheduled, list: &list)
-                
-                // Generate output buffers for each AU.
-                let format = AVAudioFormat(standardFormatWithSampleRate: 44100, channels: 2)!
-                var buffers: [ObjectIdentifier: AVAudioPCMBuffer] = [:]
-                
-                for node in list {
-                    // What should the frame capacity be?
-                    buffers[ObjectIdentifier(node)] = AVAudioPCMBuffer(pcmFormat: format, frameCapacity: 1024)
-                }
-                
-                // Pass the schedule to the engineAU
-                var execList: [EngineAudioUnit.AUExecInfo] = []
-                
-                for node in list {
-                    
-                    let nodeBuffer = buffers[ObjectIdentifier(node)]!
-                    
-                    let inputBuffers = node.connections.map { buffers[ObjectIdentifier($0)]! }
-                    let inputBufferLists = inputBuffers.map { $0.mutableAudioBufferList }
-                    
-                    let block = {
-                        (flags: UnsafeMutablePointer<AudioUnitRenderActionFlags>,
-                         timestamp: UnsafePointer<AudioTimeStamp>,
-                         frames: AUAudioFrameCount,
-                         bus: Int,
-                         outputBuffer: UnsafeMutablePointer<AudioBufferList>) in
-                        
-                        // We'd like to avoid actually copying samples, so just copy the ABL.
-                        let buffer = inputBufferLists[bus]
-                        
-                        assert(buffer.pointee.mNumberBuffers == outputBuffer.pointee.mNumberBuffers)
-                        let ablSize = MemoryLayout<AudioBufferList>.size + Int(buffer.pointee.mNumberBuffers) * MemoryLayout<AudioBuffer>.size
-                        memcpy(outputBuffer, buffer, ablSize)
-                        
-                        return noErr
-                    }
-                    
-                    let info = EngineAudioUnit.AUExecInfo(outputBuffer: nodeBuffer.mutableAudioBufferList,
-                                                          outputPCMBuffer: nodeBuffer,
-                                                          renderBlock: node.au.renderBlock,
-                                                          inputBlock: block)
-                    
-                    execList.append(info)
-                }
-                
-                // Update engine exec list.
-                engineAU.execList = execList
-                
-            }
+            compile()
         }
     }
     
     var engineAU: EngineAudioUnit!
+    
+    func compile() {
+        // Traverse the node graph to schedule
+        // audio units.
+        
+        if let output = output {
+            
+            // Generate a new schedule of AUs.
+            var scheduled = Set<ObjectIdentifier>()
+            var list: [Node] = []
+            
+            schedule(node: output, scheduled: &scheduled, list: &list)
+            
+            // Generate output buffers for each AU.
+            let format = AVAudioFormat(standardFormatWithSampleRate: 44100, channels: 2)!
+            var buffers: [ObjectIdentifier: AVAudioPCMBuffer] = [:]
+            
+            for node in list {
+                // What should the frame capacity be?
+                buffers[ObjectIdentifier(node)] = AVAudioPCMBuffer(pcmFormat: format, frameCapacity: 1024)
+            }
+            
+            // Pass the schedule to the engineAU
+            var execList: [EngineAudioUnit.AUExecInfo] = []
+            
+            for node in list {
+                
+                let nodeBuffer = buffers[ObjectIdentifier(node)]!
+                
+                let inputBuffers = node.connections.map { buffers[ObjectIdentifier($0)]! }
+                let inputBufferLists = inputBuffers.map { $0.mutableAudioBufferList }
+                
+                let block = {
+                    (flags: UnsafeMutablePointer<AudioUnitRenderActionFlags>,
+                     timestamp: UnsafePointer<AudioTimeStamp>,
+                     frames: AUAudioFrameCount,
+                     bus: Int,
+                     outputBuffer: UnsafeMutablePointer<AudioBufferList>) in
+                    
+                    // We'd like to avoid actually copying samples, so just copy the ABL.
+                    let buffer = inputBufferLists[bus]
+                    
+                    assert(buffer.pointee.mNumberBuffers == outputBuffer.pointee.mNumberBuffers)
+                    let ablSize = MemoryLayout<AudioBufferList>.size + Int(buffer.pointee.mNumberBuffers) * MemoryLayout<AudioBuffer>.size
+                    memcpy(outputBuffer, buffer, ablSize)
+                    
+                    return noErr
+                }
+                
+                let info = EngineAudioUnit.AUExecInfo(outputBuffer: nodeBuffer.mutableAudioBufferList,
+                                                      outputPCMBuffer: nodeBuffer,
+                                                      renderBlock: node.au.renderBlock,
+                                                      inputBlock: block)
+                
+                execList.append(info)
+            }
+            
+            // Update engine exec list.
+            engineAU.execList = execList
+            
+        }
+    }
     
     /// Recursively build a schedule of audio units to run.
     func schedule(node: Node,
