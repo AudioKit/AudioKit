1301a8b89871c933b62f90ad839bc222ac4571e1
Taylor Holliday <taylor@audulus.com>
2022-12-02 12:43:14 -0800

    re #2804. Add Swift Atomics.

--- Package.swift
+++ Package.swift
@@ -6,8 +6,9 @@ let package = Package(
     name: "AudioKit",
     platforms: [.macOS(.v11), .iOS(.v13), .tvOS(.v13)],
     products: [.library(name: "AudioKit", targets: ["AudioKit"])],
+    dependencies: [.package(url: "https://github.com/apple/swift-atomics", from: "1.0.0")],
     targets: [
-        .target(name: "AudioKit"),
+        .target(name: "AudioKit", dependencies: [.product(name: "Atomics", package: "swift-atomics")]),
         .testTarget(name: "AudioKitTests", dependencies: ["AudioKit"], resources: [.copy("TestResources/")]),
     ]
 )
