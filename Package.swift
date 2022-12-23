17e1de1c003345eb2ec71a369e4ee45afc0cd1f0
Taylor Holliday <taylor@audulus.com>
2022-12-22 16:59:44 -0800

    re #2804. Add MIDIKit

--- Package.swift
+++ Package.swift
@@ -6,9 +6,14 @@ let package = Package(
     name: "AudioKit",
     platforms: [.macOS(.v12), .iOS(.v13), .tvOS(.v13)],
     products: [.library(name: "AudioKit", targets: ["AudioKit"])],
-    dependencies: [.package(url: "https://github.com/apple/swift-atomics", from: "1.0.0")],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-atomics", from: "1.0.0"),
+        .package(url: "https://github.com/orchetect/MIDIKit", from: "0.7.2")
+    ],
     targets: [
-        .target(name: "AudioKit", dependencies: [.product(name: "Atomics", package: "swift-atomics")]),
+        .target(name: "AudioKit", dependencies: [
+            .product(name: "Atomics", package: "swift-atomics"),
+            "MIDIKit"]),
         .testTarget(name: "AudioKitTests", dependencies: ["AudioKit"], resources: [.copy("TestResources/")]),
     ]
 )
