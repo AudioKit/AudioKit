bea2f9ac78ebe17d0d84fd2099c3f67f116624a7
Taylor Holliday <taylor@audulus.com>
2022-12-24 12:32:10 -0800

    re #2804. Add comments.

--- Sources/AudioKit/Internals/Engine/sysex.swift
+++ Sources/AudioKit/Internals/Engine/sysex.swift
@@ -3,11 +3,18 @@
 import Foundation
 import AudioUnit
 
+/// Encode a value in a MIDI sysex message. Value must be plain-old-data.
 func encodeSysex<T>(_ value: T) -> [UInt8] {
 
     // Start with a sysex header.
     var result: [UInt8] = [0xF0, 0x00]
 
+    // Encode the value as a sequence of nibbles.
+    // There might be some more efficient way to do this,
+    // but we can't clash with the 0xF7 end-of-message.
+    // We may not actually need to encode a valid MIDI sysex
+    // message, but that could be implementation dependent
+    // and change over time. Best to be safe.
     withUnsafeBytes(of: value) { ptr in
         for byte in ptr {
             result.append( byte >> 4 )
@@ -19,6 +26,16 @@ func encodeSysex<T>(_ value: T) -> [UInt8] {
     return result
 }
 
+/// Decode a sysex message into a value. Value must be plain-old-data.
+///
+/// We can't return a value because we can't assume the value can be
+/// default constructed.
+///
+/// - Parameters:
+///   - bytes: the sysex message
+///   - count: number of bytes in message
+///   - value: the value we're writing to
+///
 func decodeSysex<T>(_ bytes: UnsafePointer<UInt8>, count: Int, _ value: inout T) {
 
     // Number of bytes should include sysex header (0xF0, 0x00) and terminator (0xF7).
@@ -31,7 +48,19 @@ func decodeSysex<T>(_ bytes: UnsafePointer<UInt8>, count: Int, _ value: inout T)
     }
 }
 
+/// Call a function with a pointer to the midi data in the AURenderEvent.
+///
+/// We need this function because event.pointee.MIDI.data is just a tuple of three midi bytes. This is
+/// fine for simple midi messages like note on/off, but some messages are longer, so we need
+/// access to the full array, which extends off the end of the structure (one of those variable-length C structs).
+///
+/// - Parameters:
+///   - event: pointer to the AURenderEvent
+///   - f: function to call
 func withMidiData(_ event: UnsafePointer<AURenderEvent>, _ f: (UnsafePointer<UInt8>) -> ()) {
+
+    let type = event.pointee.head.eventType
+    assert(type == .midiSysEx || type == .MIDI)
 
     let length = event.pointee.MIDI.length
     if let offset = MemoryLayout.offset(of: \AUMIDIEvent.data) {
