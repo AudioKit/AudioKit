01014fa0549664ad4eaf6b3a95e4ff577101cb4b
Aurelius Prochazka <aurephd@gmail.com>
2022-12-25 21:25:43 -0800

    re: #2804 Start Volume AudioUnit

---
+++ Sources/AudioKit/Nodes/Mixing/Volume.swift
@@ -0,0 +1,103 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
+
+import Foundation
+import AudioUnit
+import AVFoundation
+
+public class Volume: Node {
+    public let connections: [Node] = []
+
+    public let avAudioNode: AVAudioNode
+
+    let volumeAU: VolumeAudioUnit
+
+    // XXX: should be using parameters
+    public var volume: Float { get { volumeAU.volume } set { volumeAU.volume = newValue }}
+
+    public init() {
+
+        let componentDescription = AudioComponentDescription(effect: "volu")
+
+        AUAudioUnit.registerSubclass(VolumeAudioUnit.self,
+                                     as: componentDescription,
+                                     name: "Volume AU",
+                                     version: .max)
+        avAudioNode = instantiate(componentDescription: componentDescription)
+        volumeAU = avAudioNode.auAudioUnit as! VolumeAudioUnit
+    }
+}
+
+
+/// Renders a sine wave.
+class VolumeAudioUnit: AUAudioUnit {
+
+    private var inputBusArray: AUAudioUnitBusArray!
+    private var outputBusArray: AUAudioUnitBusArray!
+
+    let inputChannelCount: NSNumber = 2
+    let outputChannelCount: NSNumber = 2
+
+    override public var channelCapabilities: [NSNumber]? {
+        return [inputChannelCount, outputChannelCount]
+    }
+    
+    /// Initialize with component description and options
+    /// - Parameters:
+    ///   - componentDescription: Audio Component Description
+    ///   - options: Audio Component Instantiation Options
+    /// - Throws: error
+    override public init(componentDescription: AudioComponentDescription,
+                         options: AudioComponentInstantiationOptions = []) throws {
+        
+        try super.init(componentDescription: componentDescription, options: options)
+        
+        let format = AVAudioFormat(standardFormatWithSampleRate: 44100, channels: 2)!
+        inputBusArray = AUAudioUnitBusArray(audioUnit: self, busType: .input, busses: [])
+        outputBusArray = AUAudioUnitBusArray(audioUnit: self, busType: .output, busses: [try AUAudioUnitBus(format: format)])
+        
+        parameterTree = AUParameterTree.createTree(withChildren: [])
+    }
+    
+    override var inputBusses: AUAudioUnitBusArray {
+        inputBusArray
+    }
+    
+    override var outputBusses: AUAudioUnitBusArray {
+        outputBusArray
+    }
+    
+    override func allocateRenderResources() throws {}
+    
+    override func deallocateRenderResources() {}
+    
+    var volume: AUValue = 1.0
+
+    override var internalRenderBlock: AUInternalRenderBlock {
+        { (actionFlags: UnsafeMutablePointer<AudioUnitRenderActionFlags>,
+           timeStamp: UnsafePointer<AudioTimeStamp>,
+           frameCount: AUAudioFrameCount,
+           outputBusNumber: Int,
+           outputBufferList: UnsafeMutablePointer<AudioBufferList>,
+           renderEvents: UnsafePointer<AURenderEvent>?,
+           inputBlock: AURenderPullInputBlock?) in
+            
+            let ablPointer = UnsafeMutableAudioBufferListPointer(outputBufferList)
+
+            var inputFlags: AudioUnitRenderActionFlags = []
+            _ = inputBlock?(&inputFlags, timeStamp, frameCount, 0, outputBufferList)
+
+            for channel in 0..<ablPointer.count {
+
+                let outBuf = UnsafeMutableBufferPointer<Float>(ablPointer[channel])
+                for frame in 0..<Int(frameCount) {
+                    outBuf[frame] *= self.volume
+                }
+
+            }
+            
+            return noErr
+        }
+    }
+    
+}
+
