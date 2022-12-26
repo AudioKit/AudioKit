6dc040ab06f8b06ad7dd372dab8f9404cb2ea58d
Taylor Holliday <taylor@audulus.com>
2022-12-25 21:38:37 -0800

    re #2804. Add version of instantiate.

--- Sources/AudioKit/Internals/Audio Unit/AVAudioUnit+Helpers.swift
+++ Sources/AudioKit/Internals/Audio Unit/AVAudioUnit+Helpers.swift
@@ -2,11 +2,25 @@
 
 import AVFAudio
 
+/// Instantiate an AVAudioUnit.
 func instantiate(componentDescription: AudioComponentDescription) -> AVAudioUnit {
     let semaphore = DispatchSemaphore(value: 0)
     var result: AVAudioUnit!
     AVAudioUnit.instantiate(with: componentDescription) { avAudioUnit, _ in
         guard let au = avAudioUnit else { fatalError("Unable to instantiate AVAudioUnit") }
+        result = au
+        semaphore.signal()
+    }
+    _ = semaphore.wait(wallTimeout: .distantFuture)
+    return result
+}
+
+/// Sometimes we don't want an AVAudioUnit.
+func instantiateAU(componentDescription: AudioComponentDescription) -> AUAudioUnit {
+    let semaphore = DispatchSemaphore(value: 0)
+    var result: AUAudioUnit!
+    AUAudioUnit.instantiate(with: componentDescription) { auAudioUnit, _ in
+        guard let au = auAudioUnit else { fatalError("Unable to instantiate AUAudioUnit") }
         result = au
         semaphore.signal()
     }
