e962391e337805f15e6cd2f5a8aa6e41e91ec494
Aurelius Prochazka <aurephd@gmail.com>
2022-12-26 17:34:11 -0800

    re: #2804 Had to update to iOS15 for "overallGaIn"

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "AudioKit",
-    platforms: [.macOS(.v12), .iOS(.v13), .tvOS(.v13)],
+    platforms: [.macOS(.v12), .iOS(.v15), .tvOS(.v13)],
     products: [.library(name: "AudioKit", targets: ["AudioKit"])],
     dependencies: [],
     targets: [
