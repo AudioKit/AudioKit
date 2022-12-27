a72ebebfba9753058302745eb57e905c216ccc2e
Taylor Holliday <wtholliday@gmail.com>
2022-12-26 21:43:49 -0800

    re #2804. Add frameCapacity.

--- Sources/AudioKit/Nodes/Playback/Sampler.swift
+++ Sources/AudioKit/Nodes/Playback/Sampler.swift
@@ -8,6 +8,10 @@ extension AudioBuffer {
     func clear() {
         bzero(mData, Int(mDataByteSize))
     }
+
+    var frameCapacity: AVAudioFrameCount {
+        mDataByteSize / UInt32(MemoryLayout<Float>.size)
+    }
 }
 
 enum SamplerCommand {
