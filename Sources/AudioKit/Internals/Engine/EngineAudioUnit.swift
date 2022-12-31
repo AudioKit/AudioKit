65c20b593e5c1e10c3b927342bd37af432df4e59
Taylor Holliday <wtholliday@gmail.com>
2022-12-30 23:16:12 -0800

    Cleanup

--- Sources/AudioKit/Internals/Engine/EngineAudioUnit.swift
+++ Sources/AudioKit/Internals/Engine/EngineAudioUnit.swift
@@ -20,32 +20,6 @@ public struct ExecSchedule {
     var done: Bool = false
 }
 
-func set_realtime(period: UInt32, computation: UInt32, constraint: UInt32) -> Bool {
-    let TIME_CONSTRAINT_POLICY: UInt32 = 2
-    let TIME_CONSTRAINT_POLICY_COUNT = UInt32(MemoryLayout<thread_time_constraint_policy_data_t>.size / MemoryLayout<integer_t>.size)
-    let SUCCESS: Int32 = 0
-    var policy: thread_time_constraint_policy = .init()
-    var ret: Int32
-    let thread: thread_port_t = pthread_mach_thread_np(pthread_self())
-
-    policy.period = period
-    policy.computation = computation
-    policy.constraint = constraint
-    policy.preemptible = 1
-
-    ret = withUnsafeMutablePointer(to: &policy) {
-        $0.withMemoryRebound(to: integer_t.self, capacity: Int(TIME_CONSTRAINT_POLICY_COUNT)) {
-            thread_policy_set(thread, TIME_CONSTRAINT_POLICY, $0, TIME_CONSTRAINT_POLICY_COUNT)
-        }
-    }
-
-    if ret != SUCCESS {
-        print(stderr, "set_realtime() failed.\n")
-        return false
-    }
-    return true
-}
-
 class WorkerThread: Thread {
 
     var run = true
