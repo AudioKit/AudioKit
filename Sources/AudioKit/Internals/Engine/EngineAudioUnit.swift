265a1180acb46965b05c3aa1e0b648263f1656c5
Taylor Holliday <wtholliday@gmail.com>
2022-12-30 22:32:13 -0800

    Make worker threads realtime.
    Guessing at parameters for now.

--- Sources/AudioKit/Internals/Engine/EngineAudioUnit.swift
+++ Sources/AudioKit/Internals/Engine/EngineAudioUnit.swift
@@ -20,9 +20,49 @@ public struct ExecSchedule {
     var done: Bool = false
 }
 
+func set_realtime(period: UInt32, computation: UInt32, constraint: UInt32) -> Bool {
+    let TIME_CONSTRAINT_POLICY: UInt32 = 2
+    let TIME_CONSTRAINT_POLICY_COUNT = UInt32(MemoryLayout<thread_time_constraint_policy_data_t>.size / MemoryLayout<integer_t>.size)
+    let SUCCESS: Int32 = 0
+    var policy: thread_time_constraint_policy = .init()
+    var ret: Int32
+    let thread: thread_port_t = pthread_mach_thread_np(pthread_self())
+
+    policy.period = period
+    policy.computation = computation
+    policy.constraint = constraint
+    policy.preemptible = 1
+
+    ret = withUnsafeMutablePointer(to: &policy) {
+        $0.withMemoryRebound(to: integer_t.self, capacity: Int(TIME_CONSTRAINT_POLICY_COUNT)) {
+            thread_policy_set(thread, TIME_CONSTRAINT_POLICY, $0, TIME_CONSTRAINT_POLICY_COUNT)
+        }
+    }
+
+    if ret != SUCCESS {
+        print(stderr, "set_realtime() failed.\n")
+        return false
+    }
+    return true
+}
+
 class WorkerThread: Thread {
     override func main() {
         print ("worker thread")
+
+        var tbinfo = mach_timebase_info_data_t()
+        mach_timebase_info(&tbinfo)
+
+        let seconds = (Double(tbinfo.denom) / Double(tbinfo.numer)) * 1_000_000_000
+
+        // Guessing what the parameters would be for 128 frame buffer at 44.1kHz
+        let period = (128.0/44100.0) * seconds
+        let constraint = 0.5 * period
+        let comp = 0.5 * constraint
+
+        if !set_realtime(period: UInt32(period), computation: UInt32(comp), constraint: UInt32(constraint)) {
+            print("failed to set worker thread to realtime priority")
+        }
     }
 }
 
