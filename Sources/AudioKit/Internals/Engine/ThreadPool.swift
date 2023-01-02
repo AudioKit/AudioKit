6279abdac2f9592cd34d0c782a8825279e7d99a2
Taylor Holliday <wtholliday@gmail.com>
2023-01-02 11:54:43 -0800

    Cleanup

---
+++ Sources/AudioKit/Internals/Engine/ThreadPool.swift
@@ -0,0 +1,53 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
+
+import Foundation
+
+/// Pool of worker threads.
+///
+/// The CLAP host example uses two semaphores. See https://github.com/free-audio/clap-host/blob/56e5d267ac24593788ac1874e3643f670112cdaf/host/plugin-host.hh#L229
+class ThreadPool {
+
+    var prod: DispatchSemaphore
+    var done: DispatchSemaphore
+    var workers: [WorkerThread] = []
+
+    // Initial guess for the number of worker threads.
+    let workerCount = 4 // XXX: disable worker threads for now
+
+    init() {
+
+        prod = DispatchSemaphore(value: 0)
+        done = DispatchSemaphore(value: 0)
+
+        // Start workers.
+        for _ in 0..<workerCount {
+            let worker = WorkerThread(prod: prod, done: done)
+            worker.start()
+            workers.append(worker)
+        }
+    }
+
+    /// Wake the threads.
+    func start() {
+        for _ in 0..<workerCount {
+            prod.signal()
+        }
+    }
+
+    /// Wait for threads to finish work.
+    func wait() {
+        for _ in 0..<workerCount {
+            done.wait()
+        }
+    }
+
+    deinit {
+
+        // Shut down workers.
+        for worker in workers {
+            worker.run = false
+            worker.prod.signal()
+        }
+
+    }
+}
