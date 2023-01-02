1b9afba6c41b0f44f72f6f51afdbf83cfe569220
Taylor Holliday <taylor@audulus.com>
2023-01-01 16:05:12 -0800

    Use Atomics

--- Sources/AudioKit/Internals/Engine/AtomicList.swift
+++ Sources/AudioKit/Internals/Engine/AtomicList.swift
@@ -1,13 +1,14 @@
 // Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
 
 import Foundation
+import Atomics
 
 /// An atomic list we can use as a work queue among multiple threads.
 ///
 /// Note that item data is stored in a parallel array. Also, this list suffers from the ABA problem,
 /// but we will not be pushing a single item more than once.
 class AtomicList {
-    var head: Int = -1
+    var head = ManagedAtomic<Int>(-1)
     var items: [Int]
 
     /// Create an atomic list with a fixed number of potential items.
@@ -26,12 +27,18 @@ class AtomicList {
     func push(_ value: Int) {
         assert(value < items.count)
 
-        var oldHead = head
+        var oldHead = head.load(ordering: .relaxed)
         items[value] = oldHead
 
         // Spin until we successfully push.
-        while !OSAtomicCompareAndSwapLong(oldHead, value, &head) {
-            oldHead = head
+        while true {
+            let (exchanged, original) = head.compareExchange(expected: oldHead, desired: value, ordering: .relaxed)
+
+            if exchanged {
+                break
+            }
+
+            oldHead = original
             items[value] = oldHead
         }
 
@@ -40,14 +47,20 @@ class AtomicList {
     /// Pop an index.
     func pop() -> Int? {
 
-        var oldHead = head
+        var oldHead = head.load(ordering: .relaxed)
         if oldHead == -1 {
             return nil
         }
 
         // Spin until we successfullly pop.
-        while !OSAtomicCompareAndSwapLong(oldHead, items[oldHead], &head) {
-            oldHead = head
+        while true {
+            let (exchanged, original) = head.compareExchange(expected: oldHead, desired: items[oldHead], ordering: .relaxed)
+
+            if exchanged {
+                break
+            }
+
+            oldHead = original
             if oldHead == -1 {
                 return nil
             }
