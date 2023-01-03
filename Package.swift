ed003deae2959e6de0d46508d79f58c7b983ba69
Taylor Holliday <taylor@audulus.com>
2023-01-03 13:30:16 -0800

    Add -experimental-performance-annotations

--- Package.swift
+++ Package.swift
@@ -9,7 +9,10 @@ let package = Package(
     dependencies: [.package(url: "https://github.com/apple/swift-atomics", from: .init(1, 0, 3))],
     targets: [
         .target(name: "AudioKit",
-                dependencies: ["Utilities", .product(name: "Atomics", package: "swift-atomics")]),
+                dependencies: ["Utilities", .product(name: "Atomics", package: "swift-atomics")],
+                swiftSettings: [
+                  .unsafeFlags(["-experimental-performance-annotations"])
+                ]),
         .target(name: "Utilities"),
         .testTarget(name: "AudioKitTests", dependencies: ["AudioKit"], resources: [.copy("TestResources/")]),
     ]
