8fa00a2aa9b5b5ee9e02f61f6ec3c2453103ecd3
Taylor Holliday <taylor@audulus.com>
2023-01-05 20:45:53 -0800

    Fix typo

--- Tests/AudioKitTests/EngineRealtimeTests.swift
+++ Tests/AudioKitTests/EngineRealtimeTests.swift
@@ -3,7 +3,7 @@ import AudioKit
 import AVFoundation
 import XCTest
 
-class EngineRelatimeTests: XCTestCase {
+class EngineRealtimeTests: XCTestCase {
 
     func testBasicRealtime() throws {
         let engine = Engine()
