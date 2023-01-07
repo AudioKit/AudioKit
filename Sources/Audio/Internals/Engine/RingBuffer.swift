7a94a6df5c3be539b7d331c8aa4c78ced16e1998
Taylor Holliday <taylor@audulus.com>
2023-01-07 14:16:52 -0800

    Upgrade RingBuffer
    Fix memory ordering.

--- Sources/Audio/Internals/Engine/RingBuffer.swift
+++ Sources/Audio/Internals/Engine/RingBuffer.swift
@@ -3,89 +3,133 @@
 import Foundation
 import Atomics
 
-/// Lock-free FIFO based on TPCircularBuffer without the fancy VM mirroring stuff.
+/// Lock-free FIFO.
 public class RingBuffer<T> {
 
-    var head: Int32 = 0
-    var tail: Int32 = 0
-    var fillCount = ManagedAtomic<Int32>(0)
-    var buffer: UnsafeMutableBufferPointer<T>
+    private var _head = ManagedAtomic<Int32>(0)
+    private var _tail = ManagedAtomic<Int32>(0)
+    private var _buffer: UnsafeMutableBufferPointer<T>
 
     public init() {
-        buffer = .allocate(capacity: 1024)
+        _buffer = .allocate(capacity: 1024)
     }
 
     deinit {
-        buffer.deallocate()
+        _buffer.deallocate()
+    }
+
+    private func next(_ current: Int32) -> Int32 {
+        (current+1) % Int32(_buffer.count)
     }
 
     /// Push a single element
     /// - Parameter value: value to be pushed
     /// - Returns: whether the value could be pushed (or not enough space)
     public func push(_ value: T) -> Bool {
-        if Int32(buffer.count) - fillCount.load(ordering: .relaxed) > 0 {
-            buffer[Int(head)] = value
-            head = (head + 1) % Int32(buffer.count)
-            fillCount.wrappingIncrement(ordering: .relaxed)
-            return true
+        let head = _head.load(ordering: .relaxed)
+        let next_head = next(head)
+        if next_head == _tail.load(ordering: .acquiring) {
+            return false
         }
-        return false
+        _buffer[Int(head)] = value
+        _head.store(next_head, ordering: .releasing)
+        return true
+    }
+
+    private func write_available(_ head: Int32, _ tail: Int32) -> Int32 {
+        var ret = tail - head - 1;
+        if head >= tail {
+            ret += Int32(_buffer.count)
+        }
+        return ret
+    }
+
+    private func read_available(_ head: Int32, _ tail: Int32) -> Int32 {
+        if head >= tail {
+            return head - tail
+        }
+        return head + Int32(_buffer.count) - tail
     }
 
     /// Push elements from a buffer.
     /// - Parameter ptr: Buffer from which to read elements.
     /// - Returns: whether the elements could be pushed
     public func push(from ptr: UnsafeBufferPointer<T>) -> Bool {
-        if Int32(buffer.count) - fillCount.load(ordering: .relaxed) >= ptr.count {
-            for i in 0..<ptr.count {
-                buffer[Int(head)] = ptr[i]
-                head = (head + 1) % Int32(buffer.count)
-            }
-            fillCount.wrappingIncrement(by: Int32(ptr.count), ordering: .relaxed)
-            return true
+
+        let head = _head.load(ordering: .relaxed)
+        let tail = _tail.load(ordering: .acquiring)
+        let avail = write_available(head, tail)
+
+        if avail < ptr.count {
+            return false
         }
-        return false
+
+        for i in 0..<ptr.count {
+            _buffer[(Int(head) + i) % _buffer.count] = ptr[i]
+        }
+
+        let next_head = (Int(head) + ptr.count) % _buffer.count;
+        _head.store(Int32(next_head), ordering: .releasing);
+        return true
     }
 
     public func push(interleaving leftPtr: UnsafeBufferPointer<T>, and rightPtr: UnsafeBufferPointer<T>) -> Bool {
         assert(leftPtr.count == rightPtr.count)
-        if Int32(buffer.count) - fillCount.load(ordering: .relaxed) >= (leftPtr.count * 2) {
-            for i in 0..<leftPtr.count {
-                buffer[Int(head)] = leftPtr[i]
-                head = (head + 1) % Int32(buffer.count)
-                buffer[Int(head)] = rightPtr[i]
-                head = (head + 1) % Int32(buffer.count)
-            }
-            fillCount.wrappingIncrement(by: Int32(leftPtr.count * 2), ordering: .relaxed)
-            return true
+
+        var head = _head.load(ordering: .relaxed)
+        let tail = _tail.load(ordering: .acquiring)
+        let avail = write_available(head, tail)
+
+        if avail < (leftPtr.count * 2) {
+            return false
         }
-        return false
+
+        for i in 0..<leftPtr.count {
+            _buffer[Int(head)] = leftPtr[i]
+            head = (head + 1) % Int32(_buffer.count)
+            _buffer[Int(head)] = rightPtr[i]
+            head = (head + 1) % Int32(_buffer.count)
+        }
+
+        _head.store(Int32(head), ordering: .releasing)
+        return true
     }
 
     /// Pop off a single element
     /// - Returns: The element or nil if no elements were available.
     public func pop() -> T? {
-        if fillCount.load(ordering: .relaxed) > 0 {
-            let index = Int32(tail)
-            tail = (tail + 1) % Int32(buffer.count)
-            fillCount.wrappingDecrement(ordering: .relaxed)
-            return buffer[Int(index)]
+
+        let tail = _tail.load(ordering: .relaxed);
+        if tail == _head.load(ordering: .acquiring) {
+            return nil
         }
-        return nil
+
+        let value = _buffer[Int(tail)]
+        _tail.store(next(tail), ordering: .releasing)
+
+        return value
     }
 
     /// Pop elements into a buffer.
     /// - Parameter ptr: Buffer to store elements.
     /// - Returns: whether the elements could be popped
     public func pop(to ptr: UnsafeMutableBufferPointer<T>) -> Bool {
-        if fillCount.load(ordering: .relaxed) >= ptr.count {
-            for i in 0..<ptr.count {
-                ptr[i] = buffer[Int(tail)]
-                tail = (tail + 1) % Int32(buffer.count)
-            }
-            fillCount.wrappingDecrement(by: Int32(ptr.count), ordering: .relaxed)
-            return true
+
+        let head = _head.load(ordering: .acquiring)
+        var tail = _tail.load(ordering: .relaxed)
+
+        let avail = read_available(head, tail)
+
+        if avail < ptr.count {
+            return false
         }
-        return false
+
+        for i in 0..<ptr.count {
+            ptr[i] = _buffer[Int(tail)]
+            tail = (tail + 1) % Int32(_buffer.count)
+        }
+
+        _tail.store(tail, ordering: .releasing)
+        return true
     }
 }
