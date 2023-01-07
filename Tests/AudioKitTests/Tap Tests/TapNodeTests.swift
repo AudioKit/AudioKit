15f7ef7a1ef95d218efb8d97398f1088b140d35b
Aurelius Prochazka <aure@aure.com>
2023-01-07 01:58:30 -0800

    Simple tap processor

--- Tests/AudioKitTests/Tap Tests/TapNodeTests.swift
+++ Tests/AudioKitTests/Tap Tests/TapNodeTests.swift
@@ -4,18 +4,27 @@ import XCTest
 import AudioKit
 
 class TapNodeTests: XCTestCase {
+    func quickieAmplitudes(left: [Float], right: [Float]) -> (Float, Float) {
+        let leftAvg = left.reduce(0.0) { partialResult, current in
+            partialResult + abs(current) / Float(left.count)
+        }
+        let rightAvg = right.reduce(0.0) { partialResult, current in
+            partialResult + abs(current) / Float(left.count)
+        }
+        return (leftAvg, rightAvg)
+    }
 
     func testTapNode() async throws {
         let engine = Engine()
-        let osc = PlaygroundOscillator()
-        osc.amplitude = 0.1
-        let tapNode = TapNode(osc) { left, right in
-            print(left.count, right.count)
+        let noise = PlaygroundNoiseGenerator()
+        noise.amplitude = 0.1
+        let tapNode = TapNode(noise) { left, right in
+            print(self.quickieAmplitudes(left: left, right: right))
         }
         engine.output = tapNode
 
         try engine.start()
-        osc.play()
+        noise.play()
         sleep(1)
     }
 }
