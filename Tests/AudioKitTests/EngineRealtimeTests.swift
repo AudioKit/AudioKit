f0e931b3024f5380cee2916338ec70a1af21bcc7
Taylor Holliday <taylor@audulus.com>
2023-01-07 22:01:08 -0800

    Remove test.

--- Tests/AudioKitTests/EngineRealtimeTests.swift
+++ Tests/AudioKitTests/EngineRealtimeTests.swift
@@ -150,20 +150,4 @@ class EngineRealtimeTests: XCTestCase {
         sleep(2)
     }
 
-    func testManyOscillatorsOld() throws {
-        let engine = AudioEngine()
-
-        let mixer = Mixer()
-
-        for _ in 0..<100 {
-            mixer.addInput(TestOscillator())
-        }
-
-        mixer.volume = 0.001
-        engine.output = mixer
-
-        try engine.start()
-        sleep(2)
-    }
-
 }
