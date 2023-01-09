5d086aad2cff82a9fc26505fec563f2ed1af320b
Taylor Holliday <taylor@audulus.com>
2023-01-09 14:04:57 -0800

    Cleanup

--- Tests/AudioKitTests/Tap Tests/AmplitudeDetectionTests.swift
+++ Tests/AudioKitTests/Tap Tests/AmplitudeDetectionTests.swift
@@ -6,47 +6,21 @@ import AVFAudio
 
 class AmplitudeDectorTests: XCTestCase {
 
-    func check(values: [Float], known: [Float]) {
-        if values.count >= known.count {
-            for i in 0..<known.count {
-                XCTAssertEqual(values[i], 0.579 * known[i], accuracy: 0.03)
-            }
-        }
-    }
-
-    @available(iOS 13.0, *)
     func testDefault() {
 
-        let engine = Engine()
+        let sr = 44100
+        for i in 0..<10 {
 
-        var detectedAmplitudes: [Float] = []
-        let targetAmplitudes: [Float] = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-
-        let noise = PlaygroundNoiseGenerator(amplitude: 0.0)
-
-        let expect = expectation(description: "wait for amplitudes")
-
-        let tap = Tap(noise) { left, right in
-            let amp = detectAmplitude(left, right)
-            if abs(amp - (detectedAmplitudes.last ?? 0.0)) > 0.05 {
-                detectedAmplitudes.append(amp)
-                if detectedAmplitudes.count == 10 {
-                    expect.fulfill()
-                }
+            // One second of noise.
+            let noise: [Float] = (0..<sr).map { _ in
+                Float.random(in: -1...1) * Float(i) * 0.1
             }
-        }
-        engine.output = tap
 
-        let audio = engine.startTest(totalDuration: 10.0)
-        for amplitude in targetAmplitudes {
-            noise.amplitude = amplitude
-            audio.append(engine.render(duration: 1.0))
-        }
-        wait(for: [expect], timeout: 10.0)
+            let amp = detectAmplitude(noise)
 
-        check(values: detectedAmplitudes, known: targetAmplitudes)
+            XCTAssertEqual(amp, 0.579 * Float(i) * 0.1, accuracy: 0.03)
+        }
 
     }
-
 
 }
