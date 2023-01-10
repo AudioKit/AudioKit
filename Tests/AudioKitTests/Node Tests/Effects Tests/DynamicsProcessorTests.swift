f797475e9a1a32af966de4dc89f6a76800839b58
Aurelius Prochazka <aure@aure.com>
2023-01-09 21:36:17 -0800

    Removed redundant nodes

--- Tests/AudioKitTests/Node Tests/Effects Tests/DynamicsProcessorTests.swift
+++ Tests/AudioKitTests/Node Tests/Effects Tests/DynamicsProcessorTests.swift
@@ -5,12 +5,68 @@ import XCTest
 
 class DynamicsProcessorTests: XCTestCase {
     func testDefault() throws {
-        try XCTSkipIf(true, "TODO This test gives different results on local machines from what CI does")
         let engine = Engine()
         let sampler = Sampler()
         engine.output = DynamicsProcessor(sampler)
         sampler.play(url: URL.testAudio)
         let audio = engine.startTest(totalDuration: 1.0)
+        audio.append(engine.render(duration: 1.0))
+        testMD5(audio)
+    }
+
+    func testAttackTime() {
+        let engine = Engine()
+        let sampler = Sampler()
+        engine.output = DynamicsProcessor(sampler, attackTime: 0.1)
+        sampler.play(url: URL.testAudio)
+        let audio = engine.startTest(totalDuration: 1.0)
+        audio.append(engine.render(duration: 1.0))
+        testMD5(audio)
+    }
+
+    func testHeadRoom() {
+        let engine = Engine()
+        let sampler = Sampler()
+        engine.output = DynamicsProcessor(sampler, headRoom: 0)
+        let audio = engine.startTest(totalDuration: 1.0)
+        sampler.play(url: URL.testAudio)
+        audio.append(engine.render(duration: 1.0))
+        testMD5(audio)
+    }
+
+    func testMasterGain() {
+        let engine = Engine()
+        let sampler = Sampler()
+        engine.output = DynamicsProcessor(sampler, masterGain: 1)
+        let audio = engine.startTest(totalDuration: 1.0)
+        sampler.play(url: URL.testAudio)
+        audio.append(engine.render(duration: 1.0))
+        testMD5(audio)
+    }
+
+    func testParameters() {
+        let engine = Engine()
+        let sampler = Sampler()
+        engine.output = DynamicsProcessor(sampler,
+                                   threshold: -25,
+                                   headRoom: 10,
+                                   attackTime: 0.1,
+                                   releaseTime: 0.1,
+                                   masterGain: 1)
+        let audio = engine.startTest(totalDuration: 1.0)
+        sampler.play(url: URL.testAudio)
+        audio.append(engine.render(duration: 1.0))
+        testMD5(audio)
+    }
+
+    // Release time is not currently tested
+
+    func testThreshold() {
+        let engine = Engine()
+        let sampler = Sampler()
+        engine.output = DynamicsProcessor(sampler, threshold: -25)
+        let audio = engine.startTest(totalDuration: 1.0)
+        sampler.play(url: URL.testAudio)
         audio.append(engine.render(duration: 1.0))
         testMD5(audio)
     }
