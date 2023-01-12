b8cd02af37550d7e7a11d5b98345133433e688ca
Aurelius Prochazka <aure@aure.com>
2023-01-12 00:16:07 -0800

    Don't use engine to test Table

--- Tests/AudioKitTests/TableTests.swift
+++ Tests/AudioKitTests/TableTests.swift
@@ -2,59 +2,55 @@
 
 import AudioKit
 import XCTest
+import CryptoKit
 
-// XXX: don't need to use Engine to test Table
 class TableTests: XCTestCase {
-    @available(macOS 10.15, iOS 13.0, tvOS 13.0, *)
-    func testReverseSawtooth() {
-        let engine = Engine()
-        let osc = Oscillator(waveform: Table(.reverseSawtooth))
-        engine.output = osc
-        osc.start()
-        let audio = engine.startTest(totalDuration: 1.0)
-        audio.append(engine.render(duration: 1.0))
-        testMD5(audio)
+
+    func MD5(_ string: String) -> String {
+        let digest = Insecure.MD5.hash(data: string.data(using: .utf8) ?? Data())
+        return digest.map { String(format: "%02hhx", $0) }.joined()
     }
 
-    @available(macOS 10.15, iOS 13.0, tvOS 13.0, *)
+    func testPositiveSawtooth() {
+        XCTAssertEqual(MD5("\(Table(.positiveSawtooth).content)"), "b0d38e424a4f667b7213ddbeffb163ea")
+    }
+
+    func testPositiveSine() {
+        XCTAssertEqual(MD5("\(Table(.positiveSine).content)"), "6e6cf289adef24957d785c1b916215a2")
+    }
+
+    func testPositiveSquare() {
+        XCTAssertEqual(MD5("\(Table(.positiveSquare).content)"), "6b2a5e42d97b4472190d8d88a5078e08")
+    }
+
+
+    func testPositiveTriangle() {
+        XCTAssertEqual(MD5("\(Table(.positiveTriangle).content)"), "b8176e769d36f84e53bfa8c77875fac8")
+    }
+
+    func testReverseSawtooth() {
+        XCTAssertEqual(MD5("\(Table(.reverseSawtooth).content)"), "818da16ec1a9882218af2b24e7133369")
+    }
+
     func testSawtooth() {
-        let engine = Engine()
-        let input = Oscillator(waveform: Table(.sawtooth))
-        engine.output = input
-        input.start()
-        let audio = engine.startTest(totalDuration: 1.0)
-        audio.append(engine.render(duration: 1.0))
-        testMD5(audio)
+        XCTAssertEqual(MD5("\(Table(.sawtooth).content)"), "bf2f159da29e56bce563a43ec254bc44")
     }
 
     func testSine() {
-        let engine = Engine()
-        let input = Oscillator(waveform: Table(.sine))
-        engine.output = input
-        input.start()
-        let audio = engine.startTest(totalDuration: 1.0)
-        audio.append(engine.render(duration: 1.0))
-        testMD5(audio)
+        XCTAssertEqual(MD5("\(Table(.sine).content)"), "ca89fcc197408b4829fa946c86a42855")
+    }
+
+    func testSquare() {
+        XCTAssertEqual(MD5("\(Table(.square).content)"), "d105f98e99354e7476dd6bba9cadde66")
     }
 
     func testTriangle() {
-        let engine = Engine()
-        let input = Oscillator(waveform: Table(.triangle))
-        engine.output = input
-        input.start()
-        let audio = engine.startTest(totalDuration: 1.0)
-        audio.append(engine.render(duration: 1.0))
-        testMD5(audio)
+        XCTAssertEqual(MD5("\(Table(.triangle).content)"), "26dba54983ca6a960f1ac3abbe3ab9eb")
     }
 
     func testHarmonicWithPartialAmplitudes() {
-        let engine = Engine()
         let partialAmplitudes: [Float] = [0.8, 0.2, 0.3, 0.06, 0.12, 0.0015]
-        let input = Oscillator(waveform: Table(.harmonic(partialAmplitudes)))
-        engine.output = input
-        input.start()
-        let audio = engine.startTest(totalDuration: 1.0)
-        audio.append(engine.render(duration: 1.0))
-        testMD5(audio)
+        let table = Table(.harmonic(partialAmplitudes))
+        XCTAssertEqual(MD5("\(table.content)"), "2e5695816694e97c824fea9b7edf9d7f")
     }
 }
