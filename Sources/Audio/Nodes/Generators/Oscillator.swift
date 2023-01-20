8618ce2f495c78ac847d015570919469f7a3d2be
Taylor Holliday <taylor@audulus.com>
2023-01-19 16:01:38 -0800

    Formatting

--- Sources/Audio/Nodes/Generators/Oscillator.swift
+++ Sources/Audio/Nodes/Generators/Oscillator.swift
@@ -174,34 +174,34 @@ class OscillatorAudioUnit: AUAudioUnit {
            renderEvents: UnsafePointer<AURenderEvent>?,
            _: AURenderPullInputBlock?) in
 
-                self.processEvents(events: renderEvents)
+            self.processEvents(events: renderEvents)
 
-                let ablPointer = UnsafeMutableAudioBufferListPointer(outputBufferList)
+            let ablPointer = UnsafeMutableAudioBufferListPointer(outputBufferList)
 
-                let twoPi: AUValue = .init(2 * Double.pi)
-                let phaseIncrement = (twoPi / AUValue(Settings.sampleRate)) * self.frequency
-                for frame in 0 ..< Int(frameCount) {
-                    // Get signal value for this frame at time.
-                    let index = Int(self.currentPhase / twoPi * Float(self.table.count))
-                    let value = self.table[index] * self.amplitude
+            let twoPi: AUValue = .init(2 * Double.pi)
+            let phaseIncrement = (twoPi / AUValue(Settings.sampleRate)) * self.frequency
+            for frame in 0 ..< Int(frameCount) {
+                // Get signal value for this frame at time.
+                let index = Int(self.currentPhase / twoPi * Float(self.table.count))
+                let value = self.table[index] * self.amplitude
 
-                    // Advance the phase for the next frame.
-                    self.currentPhase += phaseIncrement
-                    if self.currentPhase >= twoPi { self.currentPhase -= twoPi }
-                    if self.currentPhase < 0.0 { self.currentPhase += twoPi }
-                    // Set the same value on all channels (due to the inputFormat we have only 1 channel though).
-                    for buffer in ablPointer {
-                        let buf = UnsafeMutableBufferPointer<Float>(buffer)
-                        assert(frame < buf.count)
-                        if self.shouldBypassEffect {
-                            buf[frame] = 0
-                        } else {
-                            buf[frame] = value
-                        }
+                // Advance the phase for the next frame.
+                self.currentPhase += phaseIncrement
+                if self.currentPhase >= twoPi { self.currentPhase -= twoPi }
+                if self.currentPhase < 0.0 { self.currentPhase += twoPi }
+                // Set the same value on all channels (due to the inputFormat we have only 1 channel though).
+                for buffer in ablPointer {
+                    let buf = UnsafeMutableBufferPointer<Float>(buffer)
+                    assert(frame < buf.count)
+                    if self.shouldBypassEffect {
+                        buf[frame] = 0
+                    } else {
+                        buf[frame] = value
                     }
                 }
+            }
 
-                return noErr
+            return noErr
         }
     }
 }
