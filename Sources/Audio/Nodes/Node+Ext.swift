cf9c3e16f763b12039402d046d9aef33af6269bb
Aurelius Prochazka <aure@aure.com>
2023-01-20 20:40:50 -0800

    import MIDIKit instead of MIDI

--- Sources/Audio/Nodes/Node+Ext.swift
+++ Sources/Audio/Nodes/Node+Ext.swift
@@ -1,7 +1,7 @@
 // Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
 
 import AVFoundation
-import MIDI
+import MIDIKit
 import Utilities
 
 public extension Node {
