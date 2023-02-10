ffc702f2c5a38f7c08285e83f96fa6458a645295
Taylor Holliday <wtholliday@gmail.com>
2023-02-10 00:00:34 -0800

    Enable warnings for slow-checking expressions.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,10 @@ let package = Package(
         .target(name: "AudioKit",
                 dependencies: ["Audio", "AudioFiles", "Utilities", "MIDI", "Taps"]),
         .target(name: "Audio",
-                dependencies: ["MIDI", "Utilities", .product(name: "Atomics", package: "swift-atomics")]),
+                dependencies: ["MIDI", "Utilities", .product(name: "Atomics", package: "swift-atomics")],
+                swiftSettings: [
+                    .unsafeFlags(["-Xfrontend", "-warn-long-expression-type-checking=10"])
+                ]),
         .target(name: "AudioFiles", dependencies: ["Utilities"]),
         .target(name: "Utilities"),
         .target(name: "MIDI", dependencies: ["Utilities", .product(name: "MIDIKit", package: "MIDIKit")]),
