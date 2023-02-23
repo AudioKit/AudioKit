a5758159f8fc580d52c92aa07c7fa3358669727a
Taylor Holliday <wtholliday@gmail.com>
2023-02-23 11:27:35 -0800

    Add test for extractPacket

--- Tests/AudioKitTests/MIDI Tests/MIDIPacketTests.swift
+++ Tests/AudioKitTests/MIDI Tests/MIDIPacketTests.swift
@@ -11,16 +11,28 @@ final class MIDIPacketTests: XCTestCase {
         var packet = MIDIPacket()
         packet.length = 0
         XCTAssertEqual(extractPacketData(&packet), [])
+        var result = extractPacket(&packet)
+        XCTAssertEqual(result.length, 0)
 
         packet.length = 1
         packet.data.0 = 1
         XCTAssertEqual(extractPacketData(&packet), [1])
+
+        result = extractPacket(&packet)
+        XCTAssertEqual(result.length, 1)
+        XCTAssertEqual(result.data.0, 1)
 
         packet.length = 3
         packet.data.0 = 1
         packet.data.1 = 2
         packet.data.2 = 3
         XCTAssertEqual(extractPacketData(&packet), [1,2,3])
+
+        result = extractPacket(&packet)
+        XCTAssertEqual(result.length, 3)
+        XCTAssertEqual(result.data.0, 1)
+        XCTAssertEqual(result.data.1, 2)
+        XCTAssertEqual(result.data.2, 3)
     }
 
 
