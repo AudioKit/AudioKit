8a35dad59998d0ab212ac807b4d80680a60e11bf
Taylor Holliday <wtholliday@gmail.com>
2023-02-23 11:24:36 -0800

    Add extractPacket

--- Sources/AudioKit/MIDI/Packets/MIDIPacketList+SequenceType.swift
+++ Sources/AudioKit/MIDI/Packets/MIDIPacketList+SequenceType.swift
@@ -51,4 +51,35 @@ public func extractPacketData(_ ptr: UnsafePointer<MIDIPacket>) -> [UInt8] {
     return array
 }
 
+/// We can't call pointee on a packet pointer without potentially reading off the end and
+/// triggering ASAN.
+///
+/// This is not ideal. We're using MIDIPacket directly and assuming that our packet length is less
+/// than the 256 bytes in MIDIPacket.
+public func extractPacket(_ ptr: UnsafePointer<MIDIPacket>) -> MIDIPacket {
+
+    var packet = MIDIPacket()
+    let raw = UnsafeRawPointer(ptr)
+    let lengthPtr = raw.advanced(by: MemoryLayout.offset(of: \MIDIPacket.length)!)
+    let timestampPtr = raw.advanced(by: MemoryLayout.offset(of: \MIDIPacket.timeStamp)!)
+    let dataPtr = raw.advanced(by: MemoryLayout.offset(of: \MIDIPacket.data)!)
+
+    let length = lengthPtr.withMemoryRebound(to: UInt16.self, capacity: 1) { pointer in
+        Int(pointer.pointee)
+    }
+
+    assert(length < 256)
+
+    packet.length = UInt16(length)
+    packet.timeStamp = timestampPtr.withMemoryRebound(to: MIDITimeStamp.self, capacity: 1, { pointer in
+        pointer.pointee
+    })
+
+    _ = withUnsafeMutableBytes(of: &packet.data) { ptr in
+        memcpy(ptr.baseAddress!, dataPtr, length)
+    }
+
+    return packet
+}
+
 #endif
