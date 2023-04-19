66c7134ab2da0445e7097d9bc291056993e4710e
cgetc <shigetoshi.komatsu@mixi.co.jp>
2023-04-19 17:25:57 +0900

    Remove files of FormatConverter.

--- Sources/AudioKit/Nodes/NamedNode.swift
+++
@@ -1,9 +0,0 @@
-// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
-
-import Foundation
-
-/// Protocol to allow nice printouts for debugging connections
-public protocol NamedNode where Self: Node {
-    /// User-friendly name for the node
-    var name: String { get set }
-}
