c85cc6f4035396ac6aad413084afe1367fa859d8
Aurelius Prochazka <aure@aure.com>
2023-05-05 10:08:55 -0700

    Fixed AppleSamplerTests leak

--- Tests/AudioKitTests/Node Tests/Playback Tests/AppleSamplerTests.swift
+++ Tests/AudioKitTests/Node Tests/Playback Tests/AppleSamplerTests.swift
@@ -7,24 +7,29 @@ import XCTest
 // Commented these out if still fail CI
 
 class AppleSamplerTests: XCTestCase {
-    let sampler = AppleSampler()
-    let engine = Engine()
 
-    override func setUpWithError() throws {
+    func testSamplePlayback() throws {
+        let sampler = AppleSampler()
+        let engine = Engine()
         let sampleURL = Bundle.module.url(forResource: "TestResources/sinechirp", withExtension: "wav")!
         let audioFile = try AVAudioFile(forReading: sampleURL)
         try sampler.loadAudioFile(audioFile)
         engine.output = sampler
-    }
 
-    func testSamplePlayback() {
         let audio = engine.startTest(totalDuration: 2.0)
         sampler.play(noteNumber: 50, velocity: 127, channel: 1)
         audio.append(engine.render(duration: 2.0))
         testMD5(audio)
     }
 
-    func testStop() {
+    func testStop() throws {
+        let sampler = AppleSampler()
+        let engine = Engine()
+        let sampleURL = Bundle.module.url(forResource: "TestResources/sinechirp", withExtension: "wav")!
+        let audioFile = try AVAudioFile(forReading: sampleURL)
+        try sampler.loadAudioFile(audioFile)
+        engine.output = sampler
+
         let audio = engine.startTest(totalDuration: 3.0)
         sampler.play()
         audio.append(engine.render(duration: 1.0))
@@ -35,7 +40,14 @@ class AppleSamplerTests: XCTestCase {
         testMD5(audio)
     }
 
-    func testVolume() {
+    func testVolume() throws {
+        let sampler = AppleSampler()
+        let engine = Engine()
+        let sampleURL = Bundle.module.url(forResource: "TestResources/sinechirp", withExtension: "wav")!
+        let audioFile = try AVAudioFile(forReading: sampleURL)
+        try sampler.loadAudioFile(audioFile)
+        engine.output = sampler
+
         sampler.volume = 0.8
         let audio = engine.startTest(totalDuration: 2.0)
         sampler.play(noteNumber: 50, velocity: 127, channel: 1)
@@ -43,7 +55,14 @@ class AppleSamplerTests: XCTestCase {
         testMD5(audio)
     }
 
-    func testPan() {
+    func testPan() throws {
+        let sampler = AppleSampler()
+        let engine = Engine()
+        let sampleURL = Bundle.module.url(forResource: "TestResources/sinechirp", withExtension: "wav")!
+        let audioFile = try AVAudioFile(forReading: sampleURL)
+        try sampler.loadAudioFile(audioFile)
+        engine.output = sampler
+
         sampler.pan = 1.0
         let audio = engine.startTest(totalDuration: 2.0)
         sampler.play(noteNumber: 50, velocity: 127, channel: 1)
@@ -51,7 +70,14 @@ class AppleSamplerTests: XCTestCase {
         testMD5(audio)
     }
 
-    func testAmplitude() {
+    func testAmplitude() throws {
+        let sampler = AppleSampler()
+        let engine = Engine()
+        let sampleURL = Bundle.module.url(forResource: "TestResources/sinechirp", withExtension: "wav")!
+        let audioFile = try AVAudioFile(forReading: sampleURL)
+        try sampler.loadAudioFile(audioFile)
+        engine.output = sampler
+
         sampler.amplitude = 12
         let audio = engine.startTest(totalDuration: 2.0)
         sampler.play(noteNumber: 50, velocity: 127, channel: 1)
