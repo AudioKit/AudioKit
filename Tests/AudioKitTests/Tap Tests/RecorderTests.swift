2b1cb76753ce99a69637abb4aae1ebfe3b700aa0
Taylor Holliday <wtholliday@gmail.com>
2023-05-07 17:54:48 -0700

    🧪 Test is no longer relevant
    We are removing audioDataCallback because it is now easy to have multiple taps

--- Tests/AudioKitTests/Tap Tests/RecorderTests.swift
+++ Tests/AudioKitTests/Tap Tests/RecorderTests.swift
@@ -44,37 +44,4 @@ class RecorderTests: AKTestCase {
         XCTAssertFalse(file.toAVAudioPCMBuffer()!.isSilent)
 
     }
-//
-//    func testCallback() throws {
-//        return // for now, tests are failing
-//        let engine = Engine()
-//        let sampler = Sampler()
-//        engine.output = sampler
-//        let recorder = try NodeRecorder(node: sampler)
-//
-//        // attach the callback handler
-//        var values = [Float]()
-//        recorder.audioDataCallback = { audioData, _ in
-//            values.append(contentsOf: audioData)
-//        }
-//
-//        // record a little audio
-//        try engine.start()
-//        sampler.play(url: .testAudio)
-//        try recorder.reset()
-//        try recorder.record()
-//        sleep(1)
-//
-//        // stop recording and load it into a player
-//        recorder.stop()
-//        let audioFileURL = recorder.audioFile!.url
-//        engine.stop()
-//        sampler.stop()
-//
-//        // test the result
-//        let audio = engine.startTest(totalDuration: 1.0)
-//        sampler.play(url: audioFileURL)
-//        audio.append(engine.render(duration: 1.0))
-//        XCTAssertEqual(values[5000], -0.027038574)
-//    }
 }
