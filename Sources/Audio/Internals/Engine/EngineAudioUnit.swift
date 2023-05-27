314fa3c4f892cab073feefebd78a958df5bcda09
Taylor Holliday <wtholliday@gmail.com>
2023-05-26 21:20:24 -0700

    Fix concurrency warnings

--- Sources/Audio/Internals/Engine/EngineAudioUnit.swift
+++ Sources/Audio/Internals/Engine/EngineAudioUnit.swift
@@ -18,7 +18,7 @@ public class EngineAudioUnit: AUAudioUnit {
 
     var cachedMIDIBlock: AUScheduleMIDIEventBlock?
 
-    public static var instanceCount = ManagedAtomic(0)
+    public static let instanceCount = ManagedAtomic(0)
 
     override public var channelCapabilities: [NSNumber]? {
         return [inputChannelCount, outputChannelCount]
