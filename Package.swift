b2dbaed8cd38077092b6b6e311699ddf5d94dbd7
Aurelius Prochazka <aure@aure.com>
2024-01-28 13:40:55 -0800

    Updated package

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "AudioKit",
-    platforms: [.macOS(.v11), .iOS(.v13), .tvOS(.v13)],
+    platforms: [.macOS(.v11), .iOS(.v13), .tvOS(.v13), .visionOS(.v1)],
     products: [.library(name: "AudioKit", targets: ["AudioKit"])],
     targets: [
         .target(name: "AudioKit"),
